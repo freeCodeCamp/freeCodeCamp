27788480430948f158086cb175eae5b5714f08c7
Sahat Yalkabov <sakhat@gmail.com>
2013-11-30 13:14:29 -0500

    Added sendgrid package and user/pass to config

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,3 +1,6 @@
+var config = require('../config/config.json');
+var sendgrid  = require('sendgrid')(config.sendgrid.user, config.sendgrid.password);
+
 exports.getContact = function(req, res) {
   res.render('contact', {
     title: 'Contact',
@@ -11,4 +14,14 @@ exports.postContact = function(req, res) {
   var lastName = req.body.lastName;
   var email = req.body.email;
   var body = req.body.contactBody;
+
+  sendgrid.send({
+    to:       'example@example.com',
+    from:     'other@example.com',
+    subject:  'Hello World',
+    text:     'My first email through SendGrid.'
+  }, function(err, json) {
+    if (err) { return console.error(err); }
+    console.log(json);
+  });
 };
