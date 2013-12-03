188539d4fe56431ee06fd9a7e02b44eaa81153df
Sahat Yalkabov <sakhat@gmail.com>
2013-12-03 17:36:13 -0500

    Removed angular.js related files which are no longer used

--- public/js/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
