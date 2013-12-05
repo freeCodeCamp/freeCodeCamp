6794f64a03e85239c438827b6460a00237c0356b
Sahat Yalkabov <sakhat@gmail.com>
2013-12-04 22:11:09 -0500

    Minor style change on foursquare venue detail, and removed old images

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRe
 
 app.get('/auth/github', passport.authenticate('github'));
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) { res.redirect('/'); });
+
 app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
 app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
 
