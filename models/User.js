52827c7c9b59ef4d265c7be3b9eecfc3118d4604
Sahat Yalkabov <sakhat@gmail.com>
2013-12-06 01:14:10 -0500

    removed picture selectors on account page

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ var userSchema = new mongoose.Schema({
     gender: { type: String, default: '' },
     location: { type: String, default: '' },
     website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+    picture: { type: String, default: 'http://bit.ly/1cppDAL' }
   },
 
   // API access tokens
