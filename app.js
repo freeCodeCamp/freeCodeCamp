75ef770e49847795bcf65d486ba04f3568806ef4
Sahat Yalkabov <sakhat@gmail.com>
2013-12-12 01:54:48 -0500

    Github page Ui modifications

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.locals.pretty = true;
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.cookieParser());
