5559f2f8a564dcd8bf2c4202299a4554b534370c
Sahat Yalkabov <sakhat@gmail.com>
2014-01-07 14:35:23 -0500

    Updated token unlinking

--- controllers/user.js
+++ controllers/user.js
@@ -174,7 +174,7 @@ exports.getOauthUnlink = function(req, res, next) {
     if (err) return next(err);
 
     user[provider] = undefined;
-    user.tokens = _.reject(user.tokens, function(token) { return token.kind === 'google'; });
+    user.tokens = _.reject(user.tokens, function(token) { return token.kind === provider; });
 
     user.save(function(err) {
       if (err) return next(err);
