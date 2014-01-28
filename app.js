0a0cc7493bf7ed3d9809b038d3f1a12eecf1cdd3
Sahat Yalkabov <sakhat@gmail.com>
2014-01-28 00:24:55 -0500

    Enable express.compress() middleware for "gzipping" content

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.compress());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.cookieParser());
