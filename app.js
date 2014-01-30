d31c00ec0ec7aaf7c0a8bf417e99fde16aa45bc7
Sahat Yalkabov <sakhat@gmail.com>
2014-01-30 04:16:50 -0500

    Add mode type to console.log when you start the server

--- app.js
+++ app.js
@@ -119,5 +119,5 @@ app.get('/auth/tumblr', passport.authorize('tumblr'));
 app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), function(req, res) { res.redirect('/api/tumblr'); });
 
 app.listen(app.get('port'), function() {
-  console.log('✔ Express server listening on port ' + app.get('port'));
+  console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
