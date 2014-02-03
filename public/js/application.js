db1a185343ea2240ad5c42d3d49fae13cf92bc92
Sahat Yalkabov <sakhat@gmail.com>
2014-02-03 08:35:18 -0500

    Add application.js for connect-assets to minify and concatenate JS files

---
+++ public/js/application.js
@@ -0,0 +1,14 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Connect Assets README (https://github.com/adunkman/connect-assets) for details
+// about supported directives.
+
+//= require lib/jquery-2.1.0.min
+//= require lib/bootstrap.min
+//= require main
