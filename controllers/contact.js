a987efea4a8507bcb7780eadefa75b5cc629fd1f
Sahat Yalkabov <sakhat@gmail.com>
2014-02-11 10:46:39 -0500

    JavaScript style formatting. SendGrid as a default service for contact form.

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,16 +1,16 @@
 var secrets = require('../config/secrets');
 var nodemailer = require("nodemailer");
-var smtpTransport = nodemailer.createTransport("SMTP", {
-    service: "Mailgun",
-    auth: {
-        user: secrets.mailgun.login,
-        pass: secrets.mailgun.password
-    }
-    // service: "Sendgrid",
-    // auth: {
-    //     user: secrets.sendgrid.user,
-    //     pass: secrets.sendgrid.password
-    // }
+var smtpTransport = nodemailer.createTransport('SMTP', {
+//  service: 'Mailgun',
+//  auth: {
+//    user: secrets.mailgun.login,
+//    pass: secrets.mailgun.password
+//  }
+  service: 'SendGrid',
+  auth: {
+       user: secrets.sendgrid.user,
+       pass: secrets.sendgrid.password
+  }
 });
 
 /**
@@ -47,7 +47,7 @@ exports.postContact = function(req, res) {
   var from = req.body.email;
   var name = req.body.name;
   var body = req.body.message;
-  var to = 'you@email.com';
+  var to = 'your@email.com';
   var subject = 'API Example | Contact Form';
 
   var mailOptions = {
