1dcbe0ec97565fb75ba9f522b492678547cde8a6
rockaBe <bernd@rockaBe.de>
2014-03-11 01:17:53 +0100

    stores lowercase email for gravatar

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
 
 var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, lowercase: true },
   password: String,
 
   facebook: String,
