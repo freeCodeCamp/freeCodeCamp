d082b2158b4989de6ad49f69b972b84b92988bdb
Sahat Yalkabov <sakhat@gmail.com>
2014-04-13 15:23:10 -0400

    Added Google add/remove generator

--- minimizer.js
+++ minimizer.js
@@ -34,22 +34,32 @@ inquirer.prompt({
         { name: 'Google', checked: true },
         { name: 'Twitter', checked: true },
         { name: 'Local', checked: true },
-        { name: 'LinkedIn' },
-        { name: 'Instagram' }
+        { name: 'LinkedIn', checked: true }
       ],
       validate: function(answer) {
-        if (answer.length < 1) {
-          return 'You must choose at least one authentication method.';
-        }
+        if (answer.length < 1) return 'You must choose at least one authentication method.';
         return true;
       }
     }, function(answer) {
-      var facebookStrategyRequire = "var FacebookStrategy = require('passport-facebook').Strategy;";
-      var facebookStrategy = M(function() {
-        /***
-         // Sign in with Facebook.
+      var index;
 
-         passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, refreshToken, profile, done) {
+      var passportConfigFile = 'config/passport.js';
+      var userModelFile = 'models/User.js';
+      var profileTemplateFile = 'views/account/profile.jade';
+      var loginTemplateFile = 'views/account/login.jade';
+
+      var passportConfig = fs.readFileSync(passportConfigFile).toString().split('\n');
+      var loginTemplate = fs.readFileSync(loginTemplateFile).toString().split('\n');
+      var profileTemplate = fs.readFileSync(profileTemplateFile).toString().split('\n');
+      var userModel = fs.readFileSync(userModelFile).toString().split('\n');
+
+      if (_.contains(answer.auth, 'Facebook')) {
+        var facebookStrategyRequire = "var FacebookStrategy = require('passport-facebook').Strategy;";
+        var facebookStrategy = M(function() {
+          /***
+          // Sign in with Facebook.
+
+          passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, refreshToken, profile, done) {
             if (req.user) {
               User.findOne({ $or: [{ facebook: profile.id }, { email: profile.email }] }, function(err, existingUser) {
                 if (existingUser) {
@@ -94,39 +104,26 @@ inquirer.prompt({
             }
           }));
 
-         ***/
-      });
-      var facebookButton = M(function() {
-        /***
-               a.btn.btn-block.btn-facebook.btn-social(href='/auth/facebook')
-                 i.fa.fa-facebook
-                 | Sign in with Facebook
-         ***/
-      });
-      var facebookLinkUnlink = M(function() {
-        /***
+          ***/
+        });
+        var facebookButton = M(function() {
+          /***
+           a.btn.btn-block.btn-facebook.btn-social(href='/auth/facebook')
+             i.fa.fa-facebook
+             | Sign in with Facebook
+           ***/
+        });
+        var facebookLinkUnlink = M(function() {
+          /***
 
            if user.facebook
              p: a.text-danger(href='/account/unlink/facebook') Unlink your Facebook account
            else
              p: a(href='/auth/facebook') Link your Facebook account
-         ***/
-      });
-      var facebookModel = '  facebook: String,';
+           ***/
+        });
+        var facebookModel = '  facebook: String,';
 
-      var passportConfigFile = 'config/passport.js';
-      var userModelFile = 'models/User.js';
-      var profileTemplateFile = 'views/account/profile.jade';
-      var loginTemplateFile = 'views/account/login.jade';
-
-      var passportConfig = fs.readFileSync(passportConfigFile).toString().split('\n');
-      var loginTemplate = fs.readFileSync(loginTemplateFile).toString().split('\n');
-      var profileTemplate = fs.readFileSync(profileTemplateFile).toString().split('\n');
-      var userModel = fs.readFileSync(userModelFile).toString().split('\n');
-
-      var index;
-
-      if (_.contains(answer.auth, 'FCodeacebook')) {
         if (passportConfig.indexOf(facebookStrategyRequire) < 0) {
 
           // config/passport.js (+)
@@ -147,7 +144,7 @@ inquirer.prompt({
 
           // models/User.js (+)
           index = userModel.indexOf('  tokens: Array,');
-          userModel.splice(index -1 , 0, facebookModel);
+          userModel.splice(index - 1, 0, facebookModel);
           fs.writeFileSync(userModelFile, userModel.join('\n'));
 
           console.log('Facebook authentication has been added.'.info);
@@ -179,6 +176,131 @@ inquirer.prompt({
         fs.writeFileSync(userModelFile, userModel.join('\n'));
 
         console.log('Facebook authentication has been removed.'.error);
+      }
+
+      if (_.contains(answer.auth, 'Google')) {
+        var googleStrategyRequire = "var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;";
+        var googleStrategy = M(function() {
+          /***
+          // Sign in with Google.
+
+          passport.use(new GoogleStrategy(secrets.google, function(req, accessToken, refreshToken, profile, done) {
+            if (req.user) {
+              User.findOne({ $or: [{ google: profile.id }, { email: profile.email }] }, function(err, existingUser) {
+                if (existingUser) {
+                  req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+                  done(err);
+                } else {
+                  User.findById(req.user.id, function(err, user) {
+                    user.google = profile.id;
+                    user.tokens.push({ kind: 'google', accessToken: accessToken });
+                    user.profile.name = user.profile.name || profile.displayName;
+                    user.profile.gender = user.profile.gender || profile._json.gender;
+                    user.profile.picture = user.profile.picture || profile._json.picture;
+                    user.save(function(err) {
+                      req.flash('info', { msg: 'Google account has been linked.' });
+                      done(err, user);
+                    });
+                  });
+                }
+              });
+            } else {
+              User.findOne({ google: profile.id }, function(err, existingUser) {
+                if (existingUser) return done(null, existingUser);
+                User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
+                  if (existingEmailUser) {
+                    req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+                    done(err);
+                  } else {
+                    var user = new User();
+                    user.email = profile._json.email;
+                    user.google = profile.id;
+                    user.tokens.push({ kind: 'google', accessToken: accessToken });
+                    user.profile.name = profile.displayName;
+                    user.profile.gender = profile._json.gender;
+                    user.profile.picture = profile._json.picture;
+                    user.save(function(err) {
+                      done(err, user);
+                    });
+                  }
+                });
+              });
+            }
+          }));
+
+          ***/
+        });
+
+        var googleButton = M(function() {
+          /***
+           a.btn.btn-block.btn-google-plus.btn-social(href='/auth/google')
+             i.fa.fa-google-plus
+             | Sign in with Google
+           ***/
+        });
+        var googleLinkUnlink = M(function() {
+          /***
+
+           if user.google
+             p: a.text-danger(href='/account/unlink/google') Unlink your Google account
+           else
+             p: a(href='/auth/google') Link your Google account
+           ***/
+        });
+        var googleModel = '  google: String,';
+
+        if (passportConfig.indexOf(googleStrategyRequire) < 0) {
+
+          // config/passport.js (+)
+          index = passportConfig.indexOf("var passport = require('passport');");
+          passportConfig.splice(index + 1, 0, googleStrategyRequire);
+          index = passportConfig.indexOf('passport.deserializeUser(function(id, done) {');
+          passportConfig.splice(index + 6, 0, googleStrategy);
+          fs.writeFileSync(passportConfigFile, passportConfig.join('\n'));
+
+          // views/account/login.jade (+)
+          loginTemplate.push(googleButton);
+          fs.writeFileSync(loginTemplateFile, loginTemplate.join('\n'));
+
+          // views/account/profile.jade (+)
+          index = profileTemplate.indexOf('    h3 Linked Accounts');
+          profileTemplate.splice(index + 1, 0, googleLinkUnlink);
+          fs.writeFileSync(profileTemplateFile, profileTemplate.join('\n'));
+
+          // models/User.js (+)
+          index = userModel.indexOf('  tokens: Array,');
+          userModel.splice(index - 1, 0, googleModel);
+          fs.writeFileSync(userModelFile, userModel.join('\n'));
+
+          console.log('Google authentication has been added.'.info);
+        } else {
+          console.log('Google authentication is already active.'.warn);
+        }
+      } else {
+
+        // config/passport.js (-)
+        index = passportConfig.indexOf(googleStrategyRequire);
+        passportConfig.splice(index, 1);
+        index = passportConfig.indexOf('// Sign in with Google.');
+        passportConfig.splice(index, 46);
+        fs.writeFileSync(passportConfigFile, passportConfig.join('\n'));
+
+        // views/account/login.jade (-)
+        index = loginTemplate.indexOf("      a.btn.btn-block.btn-google-plus.btn-social(href='/auth/google')");
+        loginTemplate.splice(index, 4);
+        fs.writeFileSync(loginTemplateFile, loginTemplate.join('\n'));
+
+        // views/account/profile.jade (-)
+        index = profileTemplate.indexOf("  if user.google");
+        profileTemplate.splice(index - 1, 5);
+        fs.writeFileSync(profileTemplateFile, profileTemplate.join('\n'));
+
+        // models/User.js (-)
+        index = userModel.indexOf('  google: String,');
+        userModel.splice(index, 1);
+        fs.writeFileSync(userModelFile, userModel.join('\n'));
+
+        console.log('Google authentication has been removed.'.error);
       }
     });
   }
