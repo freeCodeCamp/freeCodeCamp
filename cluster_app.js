94f4fa8b66d2e20fdbb5c3890965cdcbd10ecd88
Sahat Yalkabov <sakhat@gmail.com>
2014-04-17 03:37:35 -0400

    Cleaned up cluster_app.js

--- cluster_app.js
+++ cluster_app.js
@@ -1,27 +1,15 @@
-/**
- * Module dependencies.
- */
-
 var os = require('os');
 var cluster = require('cluster');
 
-/**
- * Cluster setup.
- */
-
-// Setup the cluster to use app.js
 cluster.setupMaster({
   exec: 'app.js'
 });
 
-// Listen for dying workers
 cluster.on('exit', function(worker) {
-  console.log('Worker ' + worker.id + ' died');
-  // Replace the dead worker
+  console.log('worker ' + worker.id + ' died');
   cluster.fork();
 });
 
-// Fork a worker for each available CPU
 for (var i = 0; i < os.cpus().length; i++) {
   cluster.fork();
 }
