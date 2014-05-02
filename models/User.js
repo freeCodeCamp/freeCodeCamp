d4192b2e0054bb083ff9deea1c0fd79046800dc5
Sahat Yalkabov <sakhat@gmail.com>
2014-05-02 16:16:44 -0400

    Refactored Gravatar mongoose method

--- models/User.js
+++ models/User.js
@@ -64,16 +64,15 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
  * Used in Navbar and Account Management page.
  */
 
-userSchema.methods.gravatar = function(size, defaults) {
+userSchema.methods.gravatar = function(size) {
   if (!size) size = 200;
-  if (!defaults) defaults = 'retro';
 
   if (!this.email) {
-    return 'https://gravatar.com/avatar/?s=' + size + '&d=' + defaults;
+    return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
   }
 
-  var md5 = crypto.createHash('md5').update(this.email);
-  return 'https://gravatar.com/avatar/' + md5.digest('hex').toString() + '?s=' + size + '&d=' + defaults;
+  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
 };
 
 module.exports = mongoose.model('User', userSchema);
