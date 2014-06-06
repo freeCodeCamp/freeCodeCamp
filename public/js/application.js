7621f4edf2bb21f8bb3ddedf3f62c6c228a6be5d
Sahat Yalkabov <sakhat@gmail.com>
2014-06-06 15:36:08 -0400

    Updated jQuery to 2.1.1

--- public/js/application.js
+++ public/js/application.js
@@ -7,8 +7,12 @@
  *
  * It's not advisable to add code directly here, but if you do, it will appear
  * at the bottom of the compiled file.
+ *
+ * If you are planning to include all custom JavaScript inside main.js then
+ * you don't have touch this file at all, otherwise "require" additional
+ * scripts down below using //= filename.js notation.
  */
 
-//= require lib/jquery-2.1.0.min
+//= require lib/jquery-2.1.1.min
 //= require lib/bootstrap.min
 //= require main
