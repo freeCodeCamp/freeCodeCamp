886b1d2d0d6b3203dc7c0e325519b11c23b0d25e
Sahat Yalkabov <sakhat@gmail.com>
2014-09-01 12:12:53 -0700

    use https profile image url in twitter auth

--- config/passport.js
+++ config/passport.js
@@ -204,7 +204,7 @@ passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tok
           user.tokens.push({ kind: 'twitter', accessToken: accessToken, tokenSecret: tokenSecret });
           user.profile.name = user.profile.name || profile.displayName;
           user.profile.location = user.profile.location || profile._json.location;
-          user.profile.picture = user.profile.picture || profile._json.profile_image_url;
+          user.profile.picture = user.profile.picture || profile._json.profile_image_url_https;
           user.save(function(err) {
             req.flash('info', { msg: 'Twitter account has been linked.' });
             done(err, user);
@@ -225,7 +225,7 @@ passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tok
       user.tokens.push({ kind: 'twitter', accessToken: accessToken, tokenSecret: tokenSecret });
       user.profile.name = profile.displayName;
       user.profile.location = profile._json.location;
-      user.profile.picture = profile._json.profile_image_url;
+      user.profile.picture = profile._json.profile_image_url_https;
       user.save(function(err) {
         done(err, user);
       });
