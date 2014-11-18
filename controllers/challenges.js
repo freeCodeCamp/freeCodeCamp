0fbee8bad3e348be0417677deb06bd02ec2e54bd
Michael Q Larson <michaelqlarson@gmail.com>
2014-11-17 19:26:55 -0800

    add new relic and potentially fix the challenges controller

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -2,17 +2,26 @@
  * GET /
  * Challenges.
  */
-var Challenge = require('./../models/Challenge')
+var Challenge = require('./../models/Challenge');
+var _ = require('lodash');
 
-exports.returnChallenge = function(req, res) {
-    Challenge.findOne({challengeNumber: req.params.challengeNumber}, function(err, c){
+exports.returnChallenge = function(req, res, next) {
+    var challengeNumber = req.params.challengeNumber;
+    if (!_.isNumber(challengeNumber)) {
+        challengeNumber = 0;
+    }
+    Challenge.findOne({challengeNumber: challengeNumber}, function(err, c){
+        if (err) {
+            console.error('Challenge err: ', err);
+            next(err);
+        }
         res.render('challenges/show', {
             title: 'Challenge',
             name: c.name,
             video: c.video,
             time: c.time,
             steps: c.steps,
-            cc: req.user.challengesCompleted || []
+            cc: req.user ? req.user.challengesCompleted : []
         });
     });
 };
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['freecodecamp'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '4b88e7cef87e9e99728be2d36c4d5f2a3862f5ae',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
