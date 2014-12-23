3ac4ecdd08f016012d7c7b87d26363143c8d675a
Michael Q Larson <michaelqlarson@gmail.com>
2014-12-23 12:16:23 -0800

    add twitter social functionality

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -7,6 +7,8 @@ var _ = require('lodash');
 
 exports.returnChallenge = function(req, res, next) {
     var challengeNumber = parseInt(req.params.challengeNumber) || 0;
+    var verbs = ['ACED', 'NAILED', 'ROCKED', 'SCORCHED', 'DEVASTATED', 'OWNED', 'CRUSHED', 'CONQUERED', 'KILLED', 'SHREDDED', 'ANNIHILATED', 'NUKED']
+    var phrases = ['Shout it from on top of a mountain', 'Tell everyone and their dogs', "Show them. Show them all!", "Inspire your friends", "Tell the world of your greatness", "Look accomplished on social media", 'Share news of your grand endeavor', 'Establish your alibi for the past two hours', "Prove to mom that computers aren't just for games"]
     if (challengeNumber > 59) { challengeNumber = 0; }
     Challenge.find({}, function (err, c) {
         if (err) {
@@ -19,7 +21,10 @@ exports.returnChallenge = function(req, res, next) {
             video: c[challengeNumber].video,
             time: c[challengeNumber].time,
             steps: c[challengeNumber].steps,
+            number: challengeNumber,
             cc: req.user ? req.user.challengesHash : undefined,
+            verb: verbs[Math.floor(Math.random()*verbs.length)],
+            phrase: phrases[Math.floor(Math.random()*phrases.length)],
             challenges: c
         });
     });
