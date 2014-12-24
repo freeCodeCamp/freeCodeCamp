fc2031062b212c10c9ea5abbf9613698efa89f41
Michael Q Larson <michaelqlarson@gmail.com>
2014-12-24 09:46:51 -0800

    add back in generic secrets.js file

---
+++ config/secrets.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+  db: process.env.MONGODB || process.env.MONGOHQ_URL,
+
+  sessionSecret: process.env.SESSION_SECRET,
+
+  mandrill: {
+    user: process.env.MANDRILL_USER,
+    password: process.env.MANDRILL_PASSWORD
+  },
+
+  facebook: {
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: '/auth/facebook/callback',
+    passReqToCallback: true
+  },
+
+  github: {
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: '/auth/github/callback',
+    passReqToCallback: true
+  },
+
+  twitter: {
+    consumerKey: process.env.TWITTER_KEY,
+    consumerSecret: process.env.TWITTER_SECRET,
+    callbackURL: '/auth/twitter/callback',
+    passReqToCallback: true
+  },
+
+  google: {
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
+  },
+
+  linkedin: {
+    clientID: process.env.LINKEDIN_ID,
+    clientSecret: process.env.LINKEDIN_SECRET,
+    callbackURL: '/auth/linkedin/callback',
+    scope: ['r_fullprofile', 'r_emailaddress'],
+    passReqToCallback: true
+  }
+};
