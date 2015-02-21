a41c37ec0334a46ff0433d9ae80d103dc49c1a1e
Michael Q Larson <michaelqlarson@gmail.com>
2015-02-21 01:17:54 -0800

    add together.js

--- app.js
+++ app.js
@@ -139,6 +139,11 @@ var trusted = [
     '*.ionicframework.com',
     'https://syndication.twitter.com',
     '*.youtube.com',
+    '*.jsdelivr.net',
+    '*.togetherjs.com',
+    'https://*.togetherjs.com',
+    'wss://hub.togetherjs.com',
+    '*.ytimg.com'
 ];
 
 app.use(helmet.contentSecurityPolicy({
