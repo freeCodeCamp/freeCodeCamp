62c59d89ba43d7165375e32b0aa7c6070efc71ad
Berkeley Martinez <Berkeley@robotie.com>
2015-03-01 14:23:50 -0800

    Partial save

--- common/models/User.js
+++ common/models/User.js
@@ -2,14 +2,23 @@ var R = require('ramda'),
     debug = require('debug')('freecc:models:user');
 
 module.exports = function(User) {
+  var app = User.app;
+  var UserIdentity = app.models.userIdentity;
 
   debug('setting up user hooks');
-  User.observe('before save', function setProfile(ctx, next) {
+  User.observe('before save', function setUsername(ctx, next) {
     debug('setting username', ctx.instance.username);
     ctx.instance.username =
       ctx.instance.username.match(/twitter/g) ?
-      ctx.instance.username.split('.').pop().toLowerCase() : ctx.instance.username;
+        ctx.instance.username.split('.').pop().toLowerCase() :
+        ctx.instance.username;
     debug('username set', ctx.instance.username);
     next();
   });
+
+
+  /*User.observe('after save', function setProfile(ctx, next) {
+    debug('setting profile/picture values', ctx.instance, ctx.data);
+    next();
+  });*/
 };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+'use strict';
 if (process.env.NODE_ENV === 'production') {
   require('newrelic');
 }
