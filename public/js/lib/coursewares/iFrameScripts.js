3fe2edcc82a70afafcd6edfee0df101817c3d670
Nathan Leniz <terakilobyte@gmail.com>
2015-03-03 10:34:16 +0900

    Fix indenting on iframescripts

--- public/js/lib/coursewares/iFrameScripts.js
+++ public/js/lib/coursewares/iFrameScripts.js
@@ -1,20 +1,20 @@
 /*eslint-disable no-unused-vars, no-undef, block-scoped-var, no-eval */
 (function() {
-    var expect = chai.expect;
-    var tests = parent.tests;
+  var expect = chai.expect;
+  var tests = parent.tests;
 
-    for (var i = 0; i < tests.length; i++) {
-        var thisTest = true;
-        try {
-            eval(parent.tests[i]);
-        } catch (err) {
-            allTestsGood = false;
-            thisTest = false;
-            parent.postError(JSON.stringify(tests[i]));
-        } finally {
-            if (thisTest) {
-                parent.postSuccess(JSON.stringify(tests[i]));
-            }
-        }
+  for (var i = 0; i < tests.length; i++) {
+    var thisTest = true;
+    try {
+      eval(parent.tests[i]);
+    } catch (err) {
+      allTestsGood = false;
+      thisTest = false;
+      parent.postError(JSON.stringify(tests[i]));
+    } finally {
+      if (thisTest) {
+        parent.postSuccess(JSON.stringify(tests[i]));
+      }
     }
+  }
 })();
