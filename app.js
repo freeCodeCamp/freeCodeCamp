1180a3132c4b54fe3d5761795e8f517b37feb85a
Michael Q Larson <michaelqlarson@gmail.com>
2015-03-07 02:00:21 -0800

    add express caching middleware

--- app.js
+++ app.js
@@ -208,6 +208,8 @@ app.use(
     express.static(path.join(__dirname, 'public'), {maxAge: 31557600000})
 );
 
+app.use(express.static(__dirname + '/public', { maxAge: 86400000 }));
+
 /**
  * Main routes.
  */
