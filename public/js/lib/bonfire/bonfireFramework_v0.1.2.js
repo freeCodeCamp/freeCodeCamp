c609294e1f281acc58b5882dda504756a113b9ce
Nathan Leniz <terakilobyte@gmail.com>
2015-03-15 17:06:05 +0900

    force merge conflict for stream viewers

--- public/js/lib/bonfire/bonfireFramework_v0.1.2.js
+++ public/js/lib/bonfire/bonfireFramework_v0.1.2.js
@@ -17,10 +17,13 @@ editor.setSize("100%", "auto");
 
 // Hijack tab key to enter two spaces intead
 editor.setOption("extraKeys", {
-    Tab: function(cm) {
-        var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-        cm.replaceSelection(spaces);
+    Tab: function(mc) {
+        var spaces = Array(mc.getOption("indentUnit") + 1).join(" ");
+        mc.replaceSelection(spaces);
     },
+
+
+
     "Ctrl-Enter": function() {
         bonfireExecute();
         return false;
