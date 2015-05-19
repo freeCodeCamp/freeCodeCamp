45d4954a433881508f7f0460a3a21a539ea503e8
Quincy Larson <michaelqlarson@gmail.com>
2015-05-18 22:25:48 -0700

    fix script tag importing issue in coursewares

--- public/js/lib/coursewares/coursewaresHCJQFramework_0.1.1.js
+++ public/js/lib/coursewares/coursewaresHCJQFramework_0.1.1.js
@@ -107,11 +107,11 @@ var tests = tests || [];
 var allSeeds = '';
 (function() {
     challengeSeed.forEach(function(elem) {
-        allSeeds += elem + '\n';
+        allSeeds += elem.replace(/fccss/g, '<script>').replace(/fcces/g,'</script>') + '\n';
     });
+  editor.setValue(allSeeds);
 })();
 
-editor.setValue(allSeeds);
 
 function doLinting () {
     editor.operation(function () {
