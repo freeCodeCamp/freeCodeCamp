40bc945a200869ca92caea79284caf694d43d9d0
Berkeley Martinez <Berkeley@robotie.com>
2015-06-01 17:11:23 -0700

    remove old bower components

--- bower_components/jsdom/lib/jsdom/level2/index.js
+++
@@ -1,7 +0,0 @@
-exports.dom = {
-  level2 : {
-    core   : require("./core").dom.level2.core,
-    events : require("./events").dom.level2.events,
-    html   : require("./html").dom.level2.html
-  }
-};
