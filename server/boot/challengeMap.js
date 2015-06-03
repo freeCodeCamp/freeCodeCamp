e7d01424e95ae76e9c9dce38355150c0721b0368
Berkeley Martinez <Berkeley@robotie.com>
2015-06-02 19:02:54 -0700

    refactor challenge and challengeMap

--- server/boot/challengeMap.js
+++ server/boot/challengeMap.js
@@ -1,65 +1,63 @@
 var R = require('ramda'),
-  express = require('express'),
   // debug = require('debug')('freecc:cntr:challengeMap'),
-  User = require('../../common/models/User'),
   resources = require('./../resources/resources'),
-  middleware = require('../resources/middleware'),
-  router = express.Router();
+  middleware = require('../resources/middleware');
 
-router.get('/map', middleware.userMigration, challengeMap);
 
-router.get('/learn-to-code', function(req, res) {
-  res.redirect(301, '/map');
-});
+module.exports = function(app) {
+  var User = app.models.User;
+  var router = app.Router();
 
-router.get('/about', function(req, res) {
-  res.redirect(301, '/map');
-});
-
-function challengeMap(req, res, next) {
-  var completedList = [];
-
-  if (req.user) {
-    completedList = req.user.completedChallenges;
-  }
-
-  var noDuplicatedChallenges = R.uniq(completedList);
-
-  var completedChallengeList = noDuplicatedChallenges
-    .map(function(challenge) {
-      return challenge._id;
-    });
-  var challengeList = resources.
-    getChallengeMapForDisplay(completedChallengeList);
-
-  Object.keys(challengeList).forEach(function(key) {
-    challengeList[key].completed = challengeList[key]
-      .challenges.filter(function(elem) {
-      return completedChallengeList.indexOf(elem._id) > -1;
-    });
+  router.get('/map', middleware.userMigration, challengeMap);
+  router.get('/learn-to-code', function(req, res) {
+    res.redirect(301, '/map');
+  });
+  router.get('/about', function(req, res) {
+    res.redirect(301, '/map');
   });
 
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
+  function challengeMap(req, res, next) {
+    var completedList = [];
 
-  var date1 = new Date('10/15/2014');
-  var date2 = new Date();
-  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-  var daysRunning = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-  User.count({}, function (err, camperCount) {
-    if (err) {
-      return next(err);
+    if (req.user) {
+      completedList = req.user.completedChallenges;
     }
-    res.render('challengeMap/show', {
-      daysRunning: daysRunning,
-      camperCount: numberWithCommas(camperCount),
-      title: "A map of all Free Code Camp's Challenges",
-      challengeList: challengeList,
-      completedChallengeList: completedChallengeList
-    });
-  });
-}
 
-module.exports = router;
+    var noDuplicatedChallenges = R.uniq(completedList);
+
+    var completedChallengeList = noDuplicatedChallenges
+      .map(function(challenge) {
+        return challenge._id;
+      });
+    var challengeList = resources.
+      getChallengeMapForDisplay(completedChallengeList);
+
+    Object.keys(challengeList).forEach(function(key) {
+      challengeList[key].completed = challengeList[key]
+        .challenges.filter(function(elem) {
+        return completedChallengeList.indexOf(elem._id) > -1;
+      });
+    });
+
+    function numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    var date1 = new Date('10/15/2014');
+    var date2 = new Date();
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var daysRunning = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    User.count(function(err, camperCount) {
+      if (err) { return next(err); }
+
+      res.render('challengeMap/show', {
+        daysRunning: daysRunning,
+        camperCount: numberWithCommas(camperCount),
+        title: "A map of all Free Code Camp's Challenges",
+        challengeList: challengeList,
+        completedChallengeList: completedChallengeList
+      });
+    });
+  }
+};
--- server/boot/jobs.js
+++ server/boot/jobs.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var Job = require('../../common/models/Job');
-var router = express.Router();
+module.exports = function(app) {
+  var Job = app.models.Job;
+  var router = app.Router();
 
-router.get('/jobs', jobsDirectory);
+  router.get('/jobs', jobsDirectory);
 
-function jobsDirectory(req, res, next) {
-  Job.find({}, function(err, jobs) {
-    if (err) { return next(err); }
+  function jobsDirectory(req, res, next) {
+    Job.find({}, function(err, jobs) {
+      if (err) { return next(err); }
 
-    res.render('jobs/directory', {
-      title: 'Junior JavaScript Engineer Jobs',
-      jobs: jobs
+      res.render('jobs/directory', {
+        title: 'Junior JavaScript Engineer Jobs',
+        jobs: jobs
+      });
     });
-  });
-}
-
-module.exports = router;
+  }
+};
