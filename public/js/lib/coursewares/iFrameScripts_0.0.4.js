7cfca0b5f8831b72c5c7e62c2570b3619d1a2f46
terakilobyte <terakilobyte@gmail.com>
2015-06-06 23:58:27 -0400

    Bump a delay on test runs and show pattern for expect tests

--- public/js/lib/coursewares/iFrameScripts_0.0.4.js
+++ public/js/lib/coursewares/iFrameScripts_0.0.4.js
@@ -3,18 +3,21 @@
   var tests = parent.tests;
   var editor = parent.editorValueForIFrame;
 
-  for (var i = 0; i < tests.length; i++) {
-    var thisTest = true;
-    try {
-      eval(parent.tests[i]);
-    } catch (err) {
-      allTestsGood = false;
-      thisTest = false;
-      parent.postError(JSON.stringify(err.message.split(':').shift()));
-    } finally {
-      if (thisTest) {
-        parent.postSuccess(JSON.stringify(tests[i].split(',').pop().replace(/\'/g, '').replace(/\)/, '')));
+  setTimeout(function() {
+    for (var i = 0; i < tests.length; i++) {
+      var thisTest = true;
+      try {
+        eval(parent.tests[i]);
+      } catch (err) {
+        allTestsGood = false;
+        thisTest = false;
+        parent.postError(JSON.stringify(err.message.split(':').shift()));
+      } finally {
+        if (thisTest) {
+          parent.postSuccess(JSON.stringify(tests[i].split(',').pop().replace(
+            /\'/g, '').replace(/\)/, '')));
+        }
       }
     }
-  }
+  }, 250);
 })();
