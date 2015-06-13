320d84c3c1ed8c35cf68c36dad9dee6b30d821c2
terakilobyte <terakilobyte@gmail.com>
2015-06-12 23:31:42 -0400

    Make bower automatic, add gulp build task to build less file into css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp'),
-    debug = require('debug')('freecc:gulp'),
-    bower = require('bower-main-files'),
-    nodemon = require('gulp-nodemon'),
-    sync = require('browser-sync'),
-    reload = sync.reload,
-    inject = require('gulp-inject'),
-    reloadDelay = 1000,
-    eslint = require('gulp-eslint');
+  debug = require('debug')('freecc:gulp'),
+  bower = require('bower-main-files'),
+  nodemon = require('gulp-nodemon'),
+  sync = require('browser-sync'),
+  reload = sync.reload,
+  inject = require('gulp-inject'),
+  reloadDelay = 1000,
+  less = require('gulp-less'),
+  path = require('path'),
+  eslint = require('gulp-eslint');
 
 var paths = {
-    server: './server/server.js',
-    serverIgnore: []
+  server: './server/server.js',
+  serverIgnore: []
 };
 
 gulp.task('inject', function() {
@@ -22,50 +24,58 @@ gulp.task('inject', function() {
 });
 
 gulp.task('serve', function(cb) {
-    var called = false;
-    nodemon({
-        script: paths.server,
-        ext: '.js',
-        ignore: paths.serverIgnore,
-        env: {
-            'NODE_ENV': 'development',
-            'DEBUG': process.env.DEBUG || 'freecc:*'
-        }
+  var called = false;
+  nodemon({
+    script: paths.server,
+    ext: '.js',
+    ignore: paths.serverIgnore,
+    env: {
+      'NODE_ENV': 'development',
+      'DEBUG': process.env.DEBUG || 'freecc:*'
+    }
+  })
+    .on('start', function() {
+      if (!called) {
+        called = true;
+        setTimeout(function() {
+          cb();
+        }, reloadDelay);
+      }
     })
-        .on('start', function() {
-            if (!called) {
-                called = true;
-                setTimeout(function() {
-                    cb();
-                }, reloadDelay);
-            }
-        })
-        .on('restart', function(files) {
-            if (files) {
-                debug('Files that changes: ', files);
-            }
-            setTimeout(function() {
-                debug('Restarting browsers');
-                reload();
-            }, reloadDelay);
-        });
+    .on('restart', function(files) {
+      if (files) {
+        debug('Files that changes: ', files);
+      }
+      setTimeout(function() {
+        debug('Restarting browsers');
+        reload();
+      }, reloadDelay);
+    });
 });
 
 gulp.task('sync', ['serve'], function() {
-    sync.init(null, {
-      proxy: 'http://localhost:3000',
-      logLeval: 'debug',
-      files: ['public/js/lib/*/*.{js, jsx}'],
-      port: 3001,
-      open: false,
-      reloadDelay: reloadDelay
-    });
+  sync.init(null, {
+    proxy: 'http://localhost:3000',
+    logLeval: 'debug',
+    files: ['public/js/lib/*/*.{js, jsx}'],
+    port: 3001,
+    open: false,
+    reloadDelay: reloadDelay
+  });
 });
 
 gulp.task('lint', function() {
   return gulp.src(['public/js/lib/**/*'])
-  .pipe(eslint())
-  .pipe(eslint.format());
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
-gulp.task('default', ['serve', 'sync']);
+gulp.task('build', function() {
+  return gulp.src('./public/css/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('default', ['build', 'serve', 'sync']);
