e782e0310f4aef3b5a47786aa9d51353229fc992
terakilobyte <terakilobyte@gmail.com>
2015-06-16 15:04:20 -0400

    Remove toString() on certificate reads as per http://stackoverflow.com/questions/20893025/cant-start-nodejs-https-server-v0-10-23

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -8,9 +8,9 @@ var fs = require('fs');
 if (process.env.NODE_ENV === 'production') {
   exports.privateKey =
     fs.readFileSync(path.join(__dirname,
-      '../../private/privatekey.pem')).toString();
+      '../../private/privatekey.pem'));
   exports.certificate =
     fs.readFileSync(path.join(__dirname,
-      '../../private/certificate.pem')).toString();
+      '../../private/certificate.pem'));
 
 }
