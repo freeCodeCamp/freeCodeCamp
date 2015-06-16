0a8356e50fe46f749a852663f5d12cff2b64ee95
terakilobyte <terakilobyte@gmail.com>
2015-06-16 14:30:16 -0400

    Configure production to run in https

---
+++ server/ssl-config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by nathanleniz on 6/16/15.
+ */
+
+var path = require('path');
+var fs = require('fs');
+
+if (process.env.NODE_ENV === 'production') {
+  exports.privateKey =
+    fs.readFileSync(path.join(__dirname,
+      '../../private/privatekey.pem')).toString();
+  exports.certificate =
+    fs.readFileSync(path.join(__dirname,
+      '../../private/certificate.pem')).toString();
+
+}
