4804d868c4f1d0c61fb045b05a6c818a61ed631a
Berkeley Martinez <Berkeley@robotie.com>
2015-06-21 18:10:17 -0700

    wrap server.listen in if module prevents server from starting durring testing/scripting

--- server/server.js
+++ server/server.js
@@ -301,20 +301,15 @@ if (true) { // eslint-disable-line
   });
 }
 
-/**
- * Start Express server.
- */
-
-
-app.listen(app.get('port'), function() {
-  console.log(
-    'FreeCodeCamp server listening on port %d in %s mode',
-    app.get('port'),
-    app.get('env')
-  );
-});
+module.exports = app;
 
 // start the server if `$ node server.js`
-
-
-module.exports = app;
+if (require.main === module) {
+  app.listen(app.get('port'), function() {
+    console.log(
+      'FreeCodeCamp server listening on port %d in %s mode',
+      app.get('port'),
+      app.get('env')
+    );
+  });
+}
