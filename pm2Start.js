c0eda90eb1c9fc67cae8fb5040e58b34a0bf56c3
Berkeley Martinez <Berkeley@robotie.com>
2015-07-24 22:22:40 -0700

    make production future ready This commit does the following:
    Production start should use the following command `npm run
    start-producion` this first runs bower install and builds the front end
    app(react). Then it will use the `pm2Start` script. This script will set
    up the pm2 daemons to run loopback in cluster mode. This script also use
    `production-start` script instead of the regular `server` script.
    
    The reasons are two fold: to ensure `server` is run in es7 mode, and to
    wait for handshake from DB or kill itself if no DB can be found within a
    certain amount of time.

--- pm2Start.js
+++ pm2Start.js
@@ -2,7 +2,7 @@ var pm2 = require('pm2');
 pm2.connect(function() {
   pm2.start({
     name: 'server',
-    script: 'server/server.js',
+    script: 'server/production-start.js',
     'exec_mode': 'cluster',
     instances: '2',
     'max_memory_restart': '900M'
---
+++ server/production-start.js
@@ -0,0 +1,31 @@
+// this ensures node understands the future
+require('babel/register');
+
+var startTime = Date.now();
+var timeoutHandler;
+// this is where server starts booting up
+var app = require('./server');
+console.log('waiting for db to connect');
+
+
+var onConnect = function() {
+  console.log('db connected in %s ms', Date.now() - startTime);
+  if (timeoutHandler) {
+    clearTimeout(timeoutHandler);
+  }
+  app.start();
+};
+
+var timeoutHandler = setTimeout(function() {
+  var message =
+    'db did not after  ' +
+    (Date.now() - startTime) +
+    ' ms connect crashing hard';
+
+  console.log(message);
+  // purposely shutdown server
+  // pm2 should restart this in production
+  throw new Error(message);
+}, 5000);
+
+app.dataSources.db.on('connected', onConnect);
