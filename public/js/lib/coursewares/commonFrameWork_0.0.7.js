39f636ae91f2df947d897a655227809b9c906141
Berkeley Martinez <Berkeley@robotie.com>
2015-08-26 15:17:19 -0700

    add loader to challenge submit

--- public/js/lib/coursewares/commonFrameWork_0.0.7.js
+++ public/js/lib/coursewares/commonFrameWork_0.0.7.js
@@ -273,13 +273,27 @@ function showCompletion() {
     var didCompleteWith = $('#completed-with').val() || null;
     $('#complete-courseware-dialog').modal('show');
     $('#submit-challenge').click(function(e) {
+      e.preventDefault();
 
       $('#submit-challenge')
         .attr('disabled', 'true')
         .removeClass('btn-primary')
         .addClass('btn-warning disabled');
 
-      e.preventDefault();
+      var $checkmarkContainer = $('#checkmark-container');
+      $checkmarkContainer.css({ height: $checkmarkContainer.innerHeight() });
+
+      $('#challenge-checkmark')
+        .addClass('zoomOutUp')
+        // .removeClass('zoomInDown')
+        .delay(1000)
+        .queue(function(next) {
+          $(this).replaceWith(
+            '<div id="challenge-spinner" class="animated zoomInUp inner-circles-loader">submitting...</div>'
+          );
+          next();
+        });
+
       $.post(
         '/completed-bonfire/', {
           challengeInfo: {
