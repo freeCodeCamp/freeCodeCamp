45c0c710e0bec64bb3d1df87a283653dfcd65b46
Berkeley Martinez <Berkeley@robotie.com>
2015-08-27 11:40:06 -0700

    lint sandbox

--- client/sandbox.js
+++ client/sandbox.js
@@ -1,87 +1,89 @@
+/* global jailed */
 var printCallback;
-// sends the input to the plugin for evaluation
-var submit = function(code,callback) {
-    printCallback = callback;
-    // postpone the evaluation until the plugin is initialized
-    plugin.whenConnected(
-        function() {
-            if (requests == 0) {
-                startLoading();
-            }
 
-            requests++;
-            plugin.remote.run(code);
-        }
-    );
-};
+// sends the input to the plugin for evaluation
+function submit(code, callback) {
+  printCallback = callback;
+
+  // postpone the evaluation until the plugin is initialized
+  plugin.whenConnected(function() {
+    if (requests === 0) {
+      startLoading();
+    }
+
+    requests++;
+    plugin.remote.run(code);
+  });
+}
 
 // puts the message on the terminal
 var print = function(cls, msg) {
-    printCallback(cls,msg);
+  printCallback(cls, msg);
 };
 
 
 // will restart the plugin if it does not respond
 var disconnectTimeout = null;
 var startLoading = function() {
-    disconnectTimeout = setTimeout(disconnect, 3000);
+  disconnectTimeout = setTimeout(disconnect, 3000);
 };
 
 var endLoading = function() {
-    clearTimeout(disconnectTimeout);
+  clearTimeout(disconnectTimeout);
 };
 
 var disconnect = function() {
-    plugin.disconnect();
+  plugin.disconnect();
 };
 
 
 // interface provided to the plugin
 var api = {
-    output: function(data) {
-      endLoading();
-      //print('input', data.input);
-      if (data.error) {
-          print('Error', data);
-          reset();
-      } else {
-          print(null, data);
-          reset();
-      }
+  output: function(data) {
+    endLoading();
+    // print('input', data.input);
+
+    if (data.error) {
+      print('Error', data);
+      reset();
+    } else {
+      print(null, data);
+      reset();
     }
+  }
 };
 
 
 // obtaining absolute path of this script
 var scripts = document.getElementsByTagName('script');
-var path = scripts[scripts.length-1].src
-        .split('?')[0]
-        .split('/')
-        .slice(0, -1)
-        .join('/')+'/';
-
-
+var path = scripts[scripts.length - 1].src
+  .split('?')[0]
+  .split('/')
+  .slice(0, -1)
+  .join('/') + '/';
 
 var requests;
 
 // (re)initializes the plugin
 var reset = function() {
-    requests = 0;
-    plugin = new jailed.Plugin(path + 'plugin.js', api);
-    plugin.whenDisconnected( function() {
-        // give some time to handle the last responce
-        setTimeout( function() {
-            endLoading();
-            console.log("resetting on fatal plugin error");
-            if(challengeType === 0){
-                codeOutput.setValue("Sorry, your code is either too slow, has a fatal error, or contains an infinite loop.");
-            }
-            reset();
-        }, 10);
-    });
+  requests = 0;
+  plugin = new jailed.Plugin(path + 'plugin.js', api);
+  plugin.whenDisconnected( function() {
+    // give some time to handle the last responce
+    setTimeout( function() {
+      endLoading();
+      console.log('resetting on fatal plugin error');
+
+      if (challengeType === 0) {
+        codeOutput.setValue(
+          'Sorry, your code is either too slow, has a fatal error, ' +
+          'or contains an infinite loop.'
+        );
+      }
+      reset();
+    }, 10);
+  });
 };
-
-
 
 // initialize everything
 var plugin = null;
