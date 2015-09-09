fab32391c69b58fc0f8c0cb2dd91b61d3ddf315b
Berkeley Martinez <Berkeley@robotie.com>
2015-09-09 14:44:13 -0700

    fix clicking on modal closes This prevents clicking on modal before modal has completed init closing modal prematurely at the expense of removing the ability to close the modal by clicking on the backdrop
    closes #2792

--- client/commonFramework.js
+++ client/commonFramework.js
@@ -433,7 +433,7 @@ function showCompletion() {
   );
   var bonfireSolution = myCodeMirror.getValue();
   var didCompleteWith = $('#completed-with').val() || null;
-  $('#complete-courseware-dialog').modal('show');
+  $('#complete-courseware-dialog').modal({ backdrop: 'static' });
   $('#complete-courseware-dialog .modal-header').click();
   $('#submit-challenge').click(function(e) {
     e.preventDefault();
