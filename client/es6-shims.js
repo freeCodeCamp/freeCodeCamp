db80c098e5521c8a61c8f24efc92dfa2f4cb1730
Berkeley Martinez <Berkeley@robotie.com>
2015-09-08 19:32:45 -0700

    add es6 shim file use Object.assign

---
+++ client/es6-shims.js
@@ -0,0 +1,1 @@
+require('object.assign').shim();
--- common/app/app-stream.jsx
+++ common/app/app-stream.jsx
@@ -1,5 +1,4 @@
 import Rx from 'rx';
-import assign from 'object.assign';
 import { Router } from 'react-router';
 import App from './App.jsx';
 import AppCat from './Cat';
@@ -8,7 +7,7 @@ import childRoutes from './routes';
 
 const router$ = Rx.Observable.fromNodeCallback(Router.run, Router);
 
-const routes = assign({ components: App }, childRoutes);
+const routes = Object.assign({ components: App }, childRoutes);
 
 export default function app$(location) {
   return router$(routes, location)
