bc6a9c6db24deade66515b452773a4f6cce5bf41
Berkeley Martinez <Berkeley@robotie.com>
2015-10-06 00:13:51 -0700

    Add redirect to user page on submit

--- server/boot/commit.js
+++ server/boot/commit.js
@@ -1,8 +1,20 @@
+import dedent from 'dedent';
+import {
+  ifNoUserRedirectTo
+} from '../utils/middleware';
+
+const sendNonUserToFront = ifNoUserRedirectTo('/');
+
 export default function commit(app) {
   const router = app.loopback.Router();
   router.get(
     '/commit',
     commitToNonprofit
+  );
+  router.get(
+    '/commit/pledge',
+    sendNonUserToFront,
+    pledge
   );
 
   app.use(router);
@@ -11,5 +23,24 @@ export default function commit(app) {
     res.render('commit/', {
       title: 'Commit to a nonprofit. Commit to your goal.'
     });
+  }
+
+  function pledge(req, res) {
+    const { user } = req;
+    const {
+      nonprofit = 'girl develop it',
+      amount = '5',
+      goal = 'front end certification'
+    } = req.query;
+
+    req.flash('success', {
+      msg: dedent`
+        Congratulations, you have commit to giving ${nonprofit} ${amount}
+        dollars a month until you have reached your goal
+        of completing your ${goal}
+      `
+    });
+
+    res.redirect('/' + user.username);
   }
 }
