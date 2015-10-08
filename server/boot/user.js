ba88def0359c86bf42237a3729405bcde7a47bd9
Berkeley Martinez <Berkeley@robotie.com>
2015-10-08 08:48:52 -0700

    Harden showCert against errors

--- server/boot/user.js
+++ server/boot/user.js
@@ -319,11 +319,12 @@ module.exports = function(app) {
             showFront && user.isFrontEndCert ||
             !showFront && user.isFullStackCert
           ) {
-            var { completedDate } = _.find(user.completedChallenges, {
-              id: showFront ?
-                frontEndChallangeId :
-                fullStackChallangeId
-            });
+            var { completedDate = new Date() } =
+              _.find(user.completedChallenges, {
+                id: showFront ?
+                  frontEndChallangeId :
+                  fullStackChallangeId
+              }) || {};
 
             return res.render(
               showFront ?
@@ -342,7 +343,7 @@ module.exports = function(app) {
               `Looks like user ${username} is not Front End certified` :
               `Looks like user ${username} is not Full Stack certified`
           });
-          res.redirect('/map');
+          res.redirect('back');
         },
         next
       );
