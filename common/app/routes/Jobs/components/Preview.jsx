4577a007f2b32d643fdbb9884cc5f8ef26661154
Berkeley Martinez <Berkeley@robotie.com>
2015-10-12 17:12:38 -0700

    Add goback and checkout buttons to preview job

--- common/app/routes/Jobs/components/Preview.jsx
+++ common/app/routes/Jobs/components/Preview.jsx
@@ -1,4 +1,6 @@
-// import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+import { History } from 'react-router';
+import { Well, Button, Row } from 'react-bootstrap';
 import { contain } from 'thundercats-react';
 import ShowJob from './ShowJob.jsx';
 
@@ -10,5 +12,37 @@ export default contain(
       return { job };
     }
   },
-  ShowJob
+  React.createClass({
+    displayName: 'Preview',
+
+    mixins: [History],
+
+    propTypes: {
+      job: PropTypes.object
+    },
+
+    render() {
+      const { job } = this.props;
+      const { history } = this;
+      return (
+        <div>
+          <ShowJob job={ job } />
+          <Row>
+            <Well>
+              <Button
+                block={ true }
+                className='signup-btn' >
+                Looks great! Let's Check Out
+              </Button>
+              <Button
+                block={ true }
+                onClick={ () => history.goBack() } >
+                Head back and make edits
+              </Button>
+            </Well>
+          </Row>
+        </div>
+      );
+    }
+  })
 );
