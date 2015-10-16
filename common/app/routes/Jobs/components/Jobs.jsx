979583e0c9293fd0d767b21e7a244d0bc897cae0
Berkeley Martinez <Berkeley@robotie.com>
2015-10-15 21:10:06 -0700

    Add styling to jobs list

--- common/app/routes/Jobs/components/Jobs.jsx
+++ common/app/routes/Jobs/components/Jobs.jsx
@@ -1,7 +1,7 @@
 import React, { cloneElement, PropTypes } from 'react';
 import { contain } from 'thundercats-react';
 import { History } from 'react-router';
-import { Button, Panel, Row } from 'react-bootstrap';
+import { Button, Panel, Row, Col } from 'react-bootstrap';
 
 import CreateJobModal from './CreateJobModal.jsx';
 import ListJobs from './List.jsx';
@@ -61,27 +61,39 @@ export default contain(
 
       return (
         <Panel>
-          <h1>Free Code Camps' Job Board</h1>
           <Row>
-            <p>
-              Need to find the best junior developers?
-              Want to find dedicated developers eager to join your company?
-              Sign up now to post your job!
-            </p>
-            <Button
-              bsSize='large'
-              className='signup-btn'
-              onClick={ jobActions.openModal }>
-              Post a job: $200 for 60 days.
-            </Button>
+            <Col
+              md={ 10 }
+              mdOffset= { 1 }
+              xs={ 12 }>
+              <h1 className='text-center'>Free Code Camps' Job Board</h1>
+              <Row>
+                <Col
+                  xs={ 10 }
+                  xsOffset={ 1 }>
+                  <p>
+                    Need to find the best junior developers?
+                    Post your job today!
+                  </p>
+                  <Button
+                    block={ true }
+                    bsSize='large'
+                    className='signup-btn'
+                    onClick={ jobActions.openModal }>
+                    Post a job: $200 for 60 days.
+                  </Button>
+                  <div className='spacer' />
+                </Col>
+              </Row>
+              <Row>
+              { this.renderChild(children, jobs) ||
+                this.renderList(this.handleJobClick, jobs) }
+              </Row>
+              <CreateJobModal
+                onHide={ jobActions.closeModal }
+                showModal={ showModal } />
+            </Col>
           </Row>
-          <Row>
-          { this.renderChild(children, jobs) ||
-            this.renderList(this.handleJobClick, jobs) }
-          </Row>
-          <CreateJobModal
-            onHide={ jobActions.closeModal }
-            showModal={ showModal } />
         </Panel>
       );
     }
