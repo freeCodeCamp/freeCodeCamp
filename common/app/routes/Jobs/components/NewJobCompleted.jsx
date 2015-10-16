c3776175a33098e8524cf6d6e2070b1dea469d6e
Berkeley Martinez <Berkeley@robotie.com>
2015-10-15 23:33:26 -0700

    Add styling to job completed view

--- common/app/routes/Jobs/components/NewJobCompleted.jsx
+++ common/app/routes/Jobs/components/NewJobCompleted.jsx
@@ -1,18 +1,41 @@
-import React, { PropTypes } from 'react';
-import { Well, Row } from 'react-bootstrap';
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Button, Panel, Col, Row } from 'react-bootstrap';
 
 export default React.createClass({
   displayName: 'NewJobCompleted',
+
   propTypes: {
   },
+
   render() {
     return (
       <div>
-        <Row>
-          <Well>
-            Congrats!
-          </Well>
-        </Row>
+        <Panel>
+          <Row>
+            <h1 className='text-center'>
+              Job under review
+            </h1>
+          </Row>
+          <Row>
+            <Col
+              md={ 6 }
+              mdOffset={ 3 }>
+            Congrats! Your job has been posted and is under review.
+            Once we review you job post we will publish it and you will receive
+            an email from us with a link to the listing.
+            </Col>
+          </Row>
+          <div className='spacer' />
+          <LinkContainer to={ '/jobs' }>
+            <Button
+              block={ true }
+              bsSize='large'
+              bsStyle='primary'>
+              Go to the job board
+            </Button>
+          </LinkContainer>
+        </Panel>
       </div>
     );
   }
