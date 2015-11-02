af30d4d56ea19ae26db95a3c81d62b7d5d547a3a
terakilobyte <terakilobyte@gmail.com>
2015-11-01 17:26:26 -0800

    Implements a 'releasedOn' flag and marks challenges less than 30 days old as NEW https://www.dropbox.com/s/43un2a908ldktpl/Screenshot%202015-11-01%2017.06.53.png

--- server/boot/challenge.js
+++ server/boot/challenge.js
@@ -537,6 +537,11 @@ module.exports = function(app) {
         if (completedChallenges.indexOf(challenge.id) !== -1) {
           challenge.completed = true;
         }
+        if (typeof(challenge.releasedOn) !== 'undefined'
+          && moment(challenge.releasedOn, 'MMM MMMM DD, YYYY').diff(moment(),
+            'days') >= -30) {
+          challenge.markNew = true;
+        }
         return challenge;
       })
       // group challenges by block | returns a stream of observables
