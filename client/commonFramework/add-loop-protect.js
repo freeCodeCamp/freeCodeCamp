18901149d9bc2bc8d326887f41144e6fbb1f942c
Geoff Wright <Geoff616@gmail.com>
2015-12-19 22:30:56 +0000

    changing loop error message to close issue 5345

--- client/commonFramework/add-loop-protect.js
+++ client/commonFramework/add-loop-protect.js
@@ -5,7 +5,11 @@ window.common = (function(global) {
   } = global;
 
   loopProtect.hit = function hit(line) {
-    var err = `Error: Exiting potential infinite loop at line ${line}.`;
+     var err = 'Error: Exiting potential infinite loop at line ' +
+     line +
+     '. To disable loop protection, write: \n\\/\\/ noprotect\nas the first' +
+     'line. Beware that if you do have an infinite loop in your code' +
+     'this will crash your browser.';
     console.error(err);
   };
 
