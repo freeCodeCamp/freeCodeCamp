12b2be9db8b8e5168d9c62415a73a3c5b63a9c96
Berkeley Martinez <Berkeley@robotie.com>
2016-01-15 06:08:54 -0800

    Remove old terminology

---
+++ server/middlewares/jade-helpers.js
@@ -0,0 +1,11 @@
+const challengesRegex = /^(bonfire|waypoint|zipline|basejump|checkpoint):\s/i;
+
+export default function jadeHelpers() {
+  return function jadeHelpersMiddleware(req, res, next) {
+    res.locals.removeOldTerms = function removeOldTerms(str) {
+      return str.replace(challengesRegex, '');
+    };
+
+    next();
+  };
+}
