c4ba7ac46a04a1b6f838aa4f2a196c4da81011bd
Berkeley Martinez <Berkeley@robotie.com>
2016-02-04 15:44:02 -0800

    Fix document title syncing

--- client/sagas/title-saga.js
+++ client/sagas/title-saga.js
@@ -2,14 +2,15 @@
 //     () =>
 //     (next: (action: Action) => Object) =>
 //     titleSage(action: Action) => Object|Void
-export default (doc) => () => next => {
+export default ({ doc }) => ({ getState }) => next => {
   return function titleSage(action) {
     // get next state
     const result = next(action);
-    if (action !== 'updateTitle') {
+    if (action.type !== 'app.updateTitle') {
       return result;
     }
-    const newTitle = result.app.title;
+    const state = getState();
+    const newTitle = state.app.title;
     doc.title = newTitle;
     return result;
   };
