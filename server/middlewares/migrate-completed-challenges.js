0354eeae16477d0b3e195f82c5baffff10873a06
Berkeley Martinez <Berkeley@robotie.com>
2016-02-10 16:34:09 -0800

    Move idMap to utils so it can be used elsewhere

--- server/middlewares/migrate-completed-challenges.js
+++ server/middlewares/migrate-completed-challenges.js
@@ -2,6 +2,8 @@ import { Observable, Scheduler } from 'rx';
 import { ObjectID } from 'mongodb';
 import debug from 'debug';
 
+import idMap from '../utils/bad-id-map';
+
 const log = debug('freecc:migrate');
 const challengeTypes = {
   html: 0,
@@ -13,26 +15,6 @@ const challengeTypes = {
   hikes: 6,
   step: 7,
   waypoint: 0
-};
-
-const idMap = {
-  bg9997c9c79feddfaeb9bdef: '56bbb991ad1ed5201cd392ca',
-  bg9995c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cb',
-  bg9994c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cc',
-  bg9996c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cd',
-  bg9997c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392ce',
-  bg9997c9c89feddfaeb9bdef: '56bbb991ad1ed5201cd392cf',
-  bg9998c9c99feddfaeb9bdef: '56bbb991ad1ed5201cd392d0',
-  bg9999c9c99feddfaeb9bdef: '56bbb991ad1ed5201cd392d1',
-  bg9999c9c99feedfaeb9bdef: '56bbb991ad1ed5201cd392d2',
-  bg9999c9c99fdddfaeb9bdef: '56bbb991ad1ed5201cd392d3',
-  bb000000000000000000001: '56bbb991ad1ed5201cd392d4',
-  bc000000000000000000001: '56bbb991ad1ed5201cd392d5',
-  bb000000000000000000002: '56bbb991ad1ed5201cd392d6',
-  bb000000000000000000003: '56bbb991ad1ed5201cd392d7',
-  bb000000000000000000004: '56bbb991ad1ed5201cd392d8',
-  bb000000000000000000005: '56bbb991ad1ed5201cd392d9',
-  bb000000000000000000006: '56bbb991ad1ed5201cd392da'
 };
 
 const challengeTypeReg = /^(waypoint|hike|zipline|basejump)/i;
@@ -90,8 +72,8 @@ function buildChallengeMap(userId, completedChallenges = [], User) {
     null,
     Scheduler.default
   )
-    .filter(({ id, _id }) => ObjectID.isValid(id || _id))
     .map(updateId)
+    .filter(({ id, _id }) => ObjectID.isValid(id || _id))
     .map(updateName)
     .reduce((challengeMap, challenge) => {
       const id = challenge.id || challenge._id;
