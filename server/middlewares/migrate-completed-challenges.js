5be6c4c92f0c9a779a8550c974fcc93864e18db7
Berkeley Martinez <Berkeley@robotie.com>
2016-02-10 16:31:59 -0800

    Fix lint errors

--- server/middlewares/migrate-completed-challenges.js
+++ server/middlewares/migrate-completed-challenges.js
@@ -16,23 +16,23 @@ const challengeTypes = {
 };
 
 const idMap = {
-  "bg9997c9c79feddfaeb9bdef": "56bbb991ad1ed5201cd392ca",
-  "bg9995c9c69feddfaeb9bdef": "56bbb991ad1ed5201cd392cb",
-  "bg9994c9c69feddfaeb9bdef": "56bbb991ad1ed5201cd392cc",
-  "bg9996c9c69feddfaeb9bdef": "56bbb991ad1ed5201cd392cd",
-  "bg9997c9c69feddfaeb9bdef": "56bbb991ad1ed5201cd392ce",
-  "bg9997c9c89feddfaeb9bdef": "56bbb991ad1ed5201cd392cf",
-  "bg9998c9c99feddfaeb9bdef": "56bbb991ad1ed5201cd392d0",
-  "bg9999c9c99feddfaeb9bdef": "56bbb991ad1ed5201cd392d1",
-  "bg9999c9c99feedfaeb9bdef": "56bbb991ad1ed5201cd392d2",
-  "bg9999c9c99fdddfaeb9bdef": "56bbb991ad1ed5201cd392d3",
-  "bb000000000000000000001": "56bbb991ad1ed5201cd392d4",
-  "bc000000000000000000001": "56bbb991ad1ed5201cd392d5",
-  "bb000000000000000000002": "56bbb991ad1ed5201cd392d6",
-  "bb000000000000000000003": "56bbb991ad1ed5201cd392d7",
-  "bb000000000000000000004": "56bbb991ad1ed5201cd392d8",
-  "bb000000000000000000005": "56bbb991ad1ed5201cd392d9",
-  "bb000000000000000000006": "56bbb991ad1ed5201cd392da"
+  bg9997c9c79feddfaeb9bdef: '56bbb991ad1ed5201cd392ca',
+  bg9995c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cb',
+  bg9994c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cc',
+  bg9996c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392cd',
+  bg9997c9c69feddfaeb9bdef: '56bbb991ad1ed5201cd392ce',
+  bg9997c9c89feddfaeb9bdef: '56bbb991ad1ed5201cd392cf',
+  bg9998c9c99feddfaeb9bdef: '56bbb991ad1ed5201cd392d0',
+  bg9999c9c99feddfaeb9bdef: '56bbb991ad1ed5201cd392d1',
+  bg9999c9c99feedfaeb9bdef: '56bbb991ad1ed5201cd392d2',
+  bg9999c9c99fdddfaeb9bdef: '56bbb991ad1ed5201cd392d3',
+  bb000000000000000000001: '56bbb991ad1ed5201cd392d4',
+  bc000000000000000000001: '56bbb991ad1ed5201cd392d5',
+  bb000000000000000000002: '56bbb991ad1ed5201cd392d6',
+  bb000000000000000000003: '56bbb991ad1ed5201cd392d7',
+  bb000000000000000000004: '56bbb991ad1ed5201cd392d8',
+  bb000000000000000000005: '56bbb991ad1ed5201cd392d9',
+  bb000000000000000000006: '56bbb991ad1ed5201cd392da'
 };
 
 const challengeTypeReg = /^(waypoint|hike|zipline|basejump)/i;
@@ -72,7 +72,7 @@ function updateName(challenge) {
 }
 
 function updateId(challenge) {
-  if(idMap.hasOwnProperty(challenge.id)) {
+  if (idMap.hasOwnProperty(challenge.id)) {
     challenge.id = idMap[challenge.id];
   }
   return challenge;
