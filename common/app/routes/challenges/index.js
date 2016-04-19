caa992ecff69cbf00f64baade758aea5a371eb9a
Berkeley Martinez <Berkeley@robotie.com>
2016-04-18 18:58:29 -0700

    Redirect /challenges to /map

--- common/app/routes/challenges/index.js
+++ common/app/routes/challenges/index.js
@@ -1,6 +1,12 @@
 import Challenges from './components/Challenges.jsx';
 
 export default {
-  path: 'challenges',
-  component: Challenges
+  path: 'challenges(/:dashedName)',
+  component: Challenges,
+  onEnter(nextState, replace) {
+    // redirect /challenges to /map
+    if (nextState.location.pathname === '/challenges') {
+      replace('/map');
+    }
+  }
 };
