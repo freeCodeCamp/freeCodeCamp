9ced160f47d22f70a653be26ec9367d5bbe97820
Quincy Larson <michaelqlarson@gmail.com>
2016-05-18 00:04:19 -0700

    Remove vestigial Trello and Blogger API keys and functions

--- server/config.development.js
+++ server/config.development.js
@@ -2,15 +2,6 @@ module.exports = {
   host: '127.0.0.1',
   sessionSecret: process.env.SESSION_SECRET,
 
-  trello: {
-    key: process.env.TRELLO_KEY,
-    secret: process.env.TRELLO_SECRET
-  },
-
-  blogger: {
-    key: process.env.BLOGGER_KEY
-  },
-
   github: {
     clientID: process.env.GITHUB_ID,
     clientSecret: process.env.GITHUB_SECRET
--- server/config.local.js
+++ server/config.local.js
@@ -4,15 +4,6 @@ module.exports = {
   restApiRoot: globalConfig.restApi,
   sessionSecret: process.env.SESSION_SECRET,
 
-  trello: {
-    key: process.env.TRELLO_KEY,
-    secret: process.env.TRELLO_SECRET
-  },
-
-  blogger: {
-    key: process.env.BLOGGER_KEY
-  },
-
   github: {
     clientID: process.env.GITHUB_ID,
     clientSecret: process.env.GITHUB_SECRET
