5fb539da2f1ac78e6d098e63f82acf98c043cff9
Andrew McKeever <andmckvr@gmail.com>
2016-06-12 09:25:02 +0200

    Add quotes

---
+++ issue8955.js
@@ -0,0 +1,23 @@
+2548: {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    2468: {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    1245: {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    5439: {
+      "album": "ABBA Gold"
+    }
