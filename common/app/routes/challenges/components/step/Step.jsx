6e653c65bd3ac071024d173d91272a3eb0e8d173
systimotic <timogos.k@live.nl>
2017-01-21 21:58:34 +0100

    Center images in introduction challenges

--- common/app/routes/challenges/components/step/Step.jsx
+++ common/app/routes/challenges/components/step/Step.jsx
@@ -191,6 +191,7 @@ export class StepChallenge extends PureComponent {
           >
           <Image
             alt={ imgAlt }
+            className='center-block'
             responsive={ true }
             src={ imgUrl }
           />
