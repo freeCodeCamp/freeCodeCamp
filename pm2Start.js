d963be60f3d9e392c2f1ba56a6c91bab03f684eb
Viqar <viqars@gmail.com>
2017-06-12 14:54:06 +0530

    code change
    Deleted lines of code for test purpose

--- pm2Start.js
+++ pm2Start.js
@@ -4,15 +4,6 @@ var instances = process.env.INSTANCES || 1;
 var serverName = process.env.SERVER_NAME || 'server';
 var maxMemory = process.env.MAX_MEMORY || '390M';
 
-pm2.connect(function() {
-  pm2.start({
-    name: serverName,
-    script: 'server/production-start.js',
-    'exec_mode': 'cluster',
-    instances: instances,
-    'max_memory_restart': maxMemory,
-    'NODE_ENV': 'production'
-  }, function() {
     console.log(
       'pm2 started %s with %s instances at %s max memory',
       serverName,
