aea4257d268103b76b96dac42d4fa9fc37deaa42
Berkeley Martinez <BerkeleyTrue@users.noreply.github.com>
2017-06-27 20:42:02 -0700

    fix(devops): Change keymetrics to use opbeat (#15556)

--- server/middlewares/production-metrics.js
+++ server/middlewares/production-metrics.js
@@ -1,9 +1,10 @@
-import pmx from 'pmx';
+import opbeat from 'opbeat';
 
 export default function keymetrics() {
   if (process.env.NODE_ENV !== 'production') {
     return (err, req, res, next) => next(err);
   }
+  const opbeatMiddleware = opbeat.middleware.express();
   return (err, req, res, next) => {
     if (res.statusCode < 400) { res.statusCode = 500; }
 
@@ -15,6 +16,6 @@ export default function keymetrics() {
     err.username = req.user ? req.user.username : 'anonymous';
     err.userId = req.user ? req.user.id : 'anonymous';
 
-    return next(pmx.notify(err));
+    return opbeatMiddleware(err, req, res, next);
   };
 }
