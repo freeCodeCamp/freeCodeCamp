2a3d8f745410c53f2f00b99cd879f60eba84a71c
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-06-29 22:15:09 -0700

    feat(server): Redirect non user to org
    Redirect non users to org directly. Logout users and redirect with ref=com randomly

--- server/middlewares/redirect-to-org.js
+++ server/middlewares/redirect-to-org.js
@@ -1,10 +1,11 @@
 import debug from 'debug';
 
 const log = debug('fcc:server:middlewares:redirectToOrg');
-const percent = parseInt(process.env.REDIRECT_PERCENT || '0', 0) / 100;
-const redirectStatus = parseInt(process.env.REDIRECT_PERMENT, 0) ?
+const percent = parseInt(process.env.REDIRECT_PERCENT || '0', 10) / 100;
+const redirectStatus = parseInt(process.env.REDIRECT_PERMENT || '0', 10) ?
   301 :
   302;
+const orgUrl = 'https://www.freecodecamp.org';
 const paths = [
   /^\/$/i,
   /^\/challenges\/.*/ig
@@ -14,24 +15,34 @@ log('redirect precentage: ', percent);
 log('redirect status: ', redirectStatus);
 export default () => {
   return function redirectToOrg(req, res, next) {
-    const method = req.method;
     const path = req.path;
+    const url = req.originalUrl;
+    const isGet = req.method === 'GET';
+    const isOnRightPath = paths.some(_path => _path.test(path));
+    // if not a matching path or a get request
+    // continue on
+    if (!isOnRightPath || !isGet) {
+      return next();
+    }
+    // if a user is not signed in, redirect them to
+    // org without any special treatment
+    if (!req.user) {
+      const redirectTo = `${orgUrl}${url}`;
+      log('redirecting to: ', redirectTo);
+      return res.redirect(redirectStatus, redirectTo);
+    }
     const group = Math.random();
-    log('method: ', method);
-    log('path: ', path);
     log('group: ', group);
-    if (
-      method === 'GET' &&
-      paths.some(_path => _path.test(path)) &&
-      group < percent
-    ) {
-      const paramsString = req.url.split('?')[1];
+    // if user is in random group, redirect them
+    if (group < percent) {
+      const paramsString = url.split('?')[1];
       let params = 'ref=com';
       if (paramsString) {
-        params = paramsString + '&ref=com';
+        params = paramsString + '&' + params;
       }
-      const redirectTo = `https://www.freecodecamp.org${path}?${params}`;
-      log('redirecting to: ', redirectTo);
+      const redirectTo = `${orgUrl}${path}?${params}`;
+      log('user redirecting to: ', redirectTo);
+      req.logOut();
       return res.redirect(redirectStatus, redirectTo);
     }
 
