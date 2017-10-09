239c06e3240ef8d3be0a0c8f77f8beb5d71e7ac6
Juan Luis Peña Wagner <juan3789@gmail.com>
2017-10-08 19:16:06 -0500

    fix(update-email): Update error handling in backend and use error message from JSON in frontend

--- client/epics/err-epic.js
+++ client/epics/err-epic.js
@@ -1,10 +1,16 @@
 import { makeToast } from '../../common/app/Toasts/redux';
 
-export default function errorSaga(actions) {
+export default function errorEpic(actions) {
   return actions.filter(({ error }) => !!error)
     .map(({ error }) => error)
     .doOnNext(error => console.error(error))
-    .map(() => makeToast({
-      message: 'Something went wrong, please try again later'
-    }));
+    .map((error) => {
+      // Get data according to normalized ajax error.
+      const data = error.xhr.responseText ? JSON.parse(error.xhr.responseText)
+      : {};
+      return makeToast({
+        message: data.message ? data.message :
+      'Something went wrong, please try again later'
+      });
+    });
 }
