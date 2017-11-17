110adf9d40cb385ebe07a9d4d02de401ba786135
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-11-17 15:19:18 -0800

    fix(Challenges): Add view type

--- common/app/routes/Challenges/utils.js
+++ common/app/routes/Challenges/utils.js
@@ -13,7 +13,8 @@ export const viewTypes = {
   [ challengeTypes.video ]: 'video',
   [ challengeTypes.step ]: 'step',
   [ challengeTypes.quiz ]: 'quiz',
-  backend: 'backend'
+  backend: 'backend',
+  modern: 'modern'
 };
 
 // determine the type of submit function to use for the challenge on completion
