3ec49e15a878ae432cbd3b81b52da99e1cde77f2
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-12-07 15:52:50 -0800

    fix(loadPreviousChallengeEpic): Refactor for RFR

--- common/app/Nav/redux/load-current-challenge-epic.js
+++ common/app/Nav/redux/load-current-challenge-epic.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { ofType } from 'redux-epic';
 
 import { types } from './';
@@ -8,23 +9,21 @@ import {
 } from '../../redux';
 import { onRouteChallenges } from '../../routes/Challenges/redux';
 import { entitiesSelector } from '../../entities';
+import { langSelector, pathnameSelector } from '../../Router/redux';
 
 export default function loadCurrentChallengeEpic(actions, { getState }) {
   return actions::ofType(types.clickOnLogo, types.clickOnMap)
     .debounce(500)
-    .map(() => {
+    .map(getState)
+    .map(state => {
       let finalChallenge;
-      const state = getState();
+      const lang = langSelector(state);
       const { id: currentlyLoadedChallengeId } = challengeSelector(state);
       const {
         challenge: challengeMap,
         challengeIdToName
       } = entitiesSelector(state);
-      const {
-        routing: {
-          locationBeforeTransitions: { pathname } = {}
-        }
-      } = state;
+      const pathname = pathnameSelector(state);
       const firstChallenge = firstChallengeSelector(state);
       const { currentChallengeId } = userSelector(state);
       const isOnAChallenge = (/^\/[^\/]{2,6}\/challenges/).test(pathname);
@@ -37,22 +36,23 @@ export default function loadCurrentChallengeEpic(actions, { getState }) {
         ];
       }
       return {
-        finalChallenge,
+        ..._.pick(finalChallenge, ['id', 'block', 'dashedName']),
+        lang,
         isOnAChallenge,
         currentlyLoadedChallengeId
       };
     })
     .filter(({
-      finalChallenge,
+      id,
       isOnAChallenge,
       currentlyLoadedChallengeId
     }) => (
       // data might not be there yet, filter out for now
-      !!finalChallenge &&
+      !!id &&
       // are we already on that challenge? if not load challenge
-      (!isOnAChallenge || finalChallenge.id !== currentlyLoadedChallengeId)
+      (!isOnAChallenge || id !== currentlyLoadedChallengeId)
       // don't reload if the challenge is already loaded.
       // This may change to toast to avoid user confusion
     ))
-    .map(({ finalChallenge }) => onRouteChallenges(finalChallenge));
+    .map(onRouteChallenges);
 }
