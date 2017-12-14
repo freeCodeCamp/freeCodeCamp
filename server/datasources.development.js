b0f91bd9bf5d7985969053259d8af9e91c7c2dd8
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-12-13 17:32:24 -0800

    feat(Development): Use SES for mail if defined

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,18 +1,26 @@
-module.exports = {
-    mail: {
-        connector: 'mail',
-        transport: {
-            type: 'smtp',
-            host: 'localhost',
-            secure: false,
-            port: 1025,
-            tls: {
-                rejectUnauthorized: false
-            }
-        },
-        auth: {
-            user: 'test',
-            pass: 'test'
-        }
+const debug = require('debug')('fcc:server:datasources');
+const dsLocal = require('./datasources.local');
+
+// use [MailHog](https://github.com/mailhog/MailHog)
+let mail = {
+  connector: 'mail',
+  Transport: {
+    type: 'smtp',
+    host: 'localhost',
+    secure: false,
+    port: 1025,
+    tls: {
+      rejectUnauthorized: false
     }
+  },
+  auth: {
+    user: 'test',
+    pass: 'test'
+  }
 };
+// use AWS SES if defined
+if (process.env.SES_ID) {
+  debug('using AWS SES to deliver emails');
+  mail = dsLocal.mail;
+}
+module.exports = { mail };
