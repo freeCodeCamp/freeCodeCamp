7be4110a65475debcca595b30a024509ea06f19b
KrisVos130 <krisvos130@gmail.com>
2017-12-14 22:29:42 +0100

    feat(profile): Started working on profile page. Added raw non-functional code.

---
+++ common/app/routes/Profile/redux/index.js
@@ -0,0 +1,60 @@
+import { isLocationAction } from 'redux-first-router';
+import {
+  addNS,
+  createAction,
+  createTypes
+} from 'berkeleys-redux-utils';
+
+import userUpdateEpic from './update-user-epic.js';
+import ns from '../ns.json';
+
+export const epics = [
+  userUpdateEpic
+];
+
+export const types = createTypes([
+  'toggleUserFlag',
+  'updateMyEmail',
+  'updateMyLang',
+  'onRouteProfile'
+], 'settings');
+
+
+export const onRouteProfile = createAction(types.onRouteProfile);
+export const toggleUserFlag = createAction(types.toggleUserFlag);
+export const updateMyEmail = createAction(types.updateMyEmail);
+
+export const updateMyLang = createAction(
+  types.updateMyLang,
+  (values) => values.lang
+);
+
+const defaultState = {
+  showUpdateEmailView: false
+};
+
+const getNS = state => state[ns];
+export const showUpdateEmailViewSelector =
+  state => getNS(state).showUpdateEmailView;
+
+export default addNS(
+  ns,
+  function profileRouteReducer(state = defaultState, action) {
+    if (isLocationAction(action)) {
+      const { type } = action;
+      if (type === types.onRouteUpdateEmail) {
+        return {
+          ...state,
+          showUpdateEmailView: true
+        };
+      }
+      if (state.showUpdateEmailView) {
+        return {
+          ...state,
+          showUpdateEmailView: false
+        };
+      }
+    }
+    return state;
+  }
+);
