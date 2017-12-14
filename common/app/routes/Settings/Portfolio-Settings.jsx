64f9f5b7254fdfb83252df7858b540d7b564f087
Jonathan Grah <30289773+jonathan-grah@users.noreply.github.com>
2017-12-14 21:58:18 +0000

    feat(settings): split up settings file into smaller modules

---
+++ common/app/routes/Settings/Portfolio-Settings.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Row, Col, FormControl } from 'react-bootstrap';
+
+const propTypes = {};
+
+export default function PortfolioSettings({}) {
+  return (
+    <Row>
+        <Col xs={ 5 }>
+            <FormControl
+                bsSize='sm'
+                className='portfolio-title'
+                placeholder='Title'
+                type='input'
+                value=''
+            />
+            <FormControl
+                bsSize='sm'
+                className='portfolio-url'
+                placeholder='URL'
+                type='input'
+                value=''
+            />
+        </Col>
+        <Col xs={ 7 }>
+            <FormControl
+                className='portfolio-description'
+                componentClass='textarea'
+                placeholder='Description'
+            />
+        </Col>
+    </Row>
+  );
+}
+
+PortfolioSettings.displayName = 'PortfolioSettings';
+PortfolioSettings.propTypes = propTypes;
