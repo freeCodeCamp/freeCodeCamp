42f9b6fc150fb35ff5c5cddb768f18685c1146f0
Stuart Taylor <staylor1012@gmail.com>
2018-01-16 18:09:29 +0000

    Remove error.message from Toast

--- client/epics/err-epic.js
+++ client/epics/err-epic.js
@@ -4,7 +4,7 @@ export default function errorSaga(actions) {
   return actions.filter(({ error }) => !!error)
     .map(({ error }) => error)
     .doOnNext(error => console.error(error))
-    .map(error => makeToast({
-      message: error.message || 'Something went wrong, please try again later'
+    .map(() => makeToast({
+      message: 'Something went wrong, please try again later'
     }));
 }
