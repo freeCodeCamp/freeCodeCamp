ff74df53f1c90c5669e2b83911626d4bc35b5353
Stuart Taylor <staylor1012@gmail.com>
2018-02-01 16:13:23 +0000

    feat(solutions): View Solutions from the profile page, with syntax highlighting

--- common/app/routes/Profile/components/Timeline.jsx
+++ common/app/routes/Profile/components/Timeline.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
 import format from 'date-fns/format';
 import _ from 'lodash';
 import {
+  Button,
+  Modal,
   Table
 } from 'react-bootstrap';
 
@@ -14,16 +16,18 @@ import { homeURL } from '../../../../utils/constantStrings.json';
 import blockNameify from '../../../utils/blockNameify';
 import { FullWidthRow } from '../../../helperComponents';
 import { Link } from '../../../Router';
+import SolutionViewer from '../../Settings/components/SolutionViewer.jsx';
 
 const mapStateToProps = createSelector(
   challengeIdToNameMapSelector,
   userByNameSelector,
   (
     idToNameMap,
-    { challengeMap: completedMap = {} }
+    { challengeMap: completedMap = {}, username }
   ) => ({
     completedMap,
-    idToNameMap
+    idToNameMap,
+    username
   })
 );
 
@@ -33,48 +37,96 @@ const propTypes = {
     completedDate: PropTypes.number,
     lastUpdated: PropTypes.number
   }),
-  idToNameMap: PropTypes.objectOf(PropTypes.string)
+  idToNameMap: PropTypes.objectOf(PropTypes.string),
+  username: PropTypes.string
 };
 
-function renderCompletion(idToNameMap, completed) {
-  const { id, completedDate, lastUpdated } = completed;
-  return (
-      <tr key={ id }>
-        <td>{ blockNameify(idToNameMap[id]) }</td>
-        <td>
-          <time dateTime={ format(completedDate, 'YYYY-MM-DDTHH:MM:SSZ') }>
-            {
-              format(completedDate, 'MMMM DD YYYY')
-            }
-          </time>
-        </td>
-        <td>{
-          lastUpdated ?
-          <time dateTime={ format(lastUpdated, 'YYYY-MM-DDTHH:MM:SSZ') }>
-            {
-              format(lastUpdated, 'MMMM DD YYYY')
-            }
-          </time> :
-          ''
-          }</td>
-      </tr>
-    );
-}
+class Timeline extends PureComponent {
+  constructor(props) {
+    super(props);
 
-function Timeline({ completedMap, idToNameMap }) {
-  if (!idToNameMap) {
-    return null;
+    this.state = {
+      solutionToView: null,
+      solutionOpen: false
+    };
+
+    this.closeSolution = this.closeSolution.bind(this);
+    this.renderCompletion = this.renderCompletion.bind(this);
+    this.viewSolution = this.viewSolution.bind(this);
   }
-  const renderCompletionWithIdNameMap = _.curry(renderCompletion)(idToNameMap);
 
-  return (
+  renderCompletion(completed) {
+    const { idToNameMap } = this.props;
+    const { id, completedDate, lastUpdated, files } = completed;
+    return (
+        <tr key={ id }>
+          <td>{ blockNameify(idToNameMap[id]) }</td>
+          <td>
+            <time dateTime={ format(completedDate, 'YYYY-MM-DDTHH:MM:SSZ') }>
+              {
+                format(completedDate, 'MMMM DD YYYY')
+              }
+            </time>
+          </td>
+          <td>
+            {
+              lastUpdated ?
+              <time dateTime={ format(lastUpdated, 'YYYY-MM-DDTHH:MM:SSZ') }>
+                {
+                  format(lastUpdated, 'MMMM DD YYYY')
+                }
+              </time> :
+              ''
+            }
+          </td>
+          <td>
+            {
+              files ?
+                <Button
+                  block={ true }
+                  bsStyle='primary'
+                  onClick={ () => this.viewSolution(id) }
+                  >
+                  View&nbsp;Solution
+                </Button> :
+                ''
+            }
+          </td>
+        </tr>
+      );
+  }
+
+  viewSolution(id) {
+    this.setState(state => ({
+      ...state,
+      solutionToView: id,
+      solutionOpen: true
+    }));
+  }
+
+  closeSolution() {
+    this.setState(state => ({
+      ...state,
+      solutionToView: null,
+      solutionOpen: false
+    }));
+  }
+
+  render() {
+    const { completedMap, idToNameMap, username } = this.props;
+    const { solutionToView: id, solutionOpen } = this.state;
+    if (!idToNameMap) {
+      return null;
+    }
+    console.log(completedMap);
+    return (
       <FullWidthRow>
         <h2 className='text-center'>Timeline</h2>
         {
           Object.keys(completedMap).length === 0 ?
           <p className='text-center'>
             No challenges have been completed yet.&nbsp;
-            <Link to={ `/${homeURL}` }>
+            <Link to={ homeURL }>
               Get started here.
             </Link>
           </p> :
@@ -84,6 +136,7 @@ function Timeline({ completedMap, idToNameMap }) {
                 <th>Challenge</th>
                 <th>First Completed</th>
                 <th>Last Changed</th>
+                <th />
               </tr>
             </thead>
             <tbody>
@@ -91,16 +144,37 @@ function Timeline({ completedMap, idToNameMap }) {
                 _.reverse(
                   _.sortBy(
                     Object.keys(completedMap).map(key => completedMap[key]),
-                  [ 'completedDate' ]
+                    [ 'completedDate' ]
                   )
                 )
-                .map(renderCompletionWithIdNameMap)
+                .map(this.renderCompletion)
               }
             </tbody>
           </Table>
         }
+        {
+          id &&
+          <Modal
+            aria-labelledby='contained-modal-title'
+            onHide={this.closeSolution}
+            show={ solutionOpen }
+            >
+            <Modal.Header closeButton={ true }>
+              <Modal.Title id='contained-modal-title'>
+                { `${username}'s Solution to ${blockNameify(idToNameMap[id])}` }
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <SolutionViewer files={ completedMap[id].files }/>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.closeSolution}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        }
       </FullWidthRow>
-  );
+    );
+  }
 }
 
 
--- common/app/routes/Settings/components/SolutionViewer.jsx
+++ common/app/routes/Settings/components/SolutionViewer.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel } from 'react-bootstrap';
+import Prism from 'prismjs';
+import Helmet from 'react-helmet';
+
+const prismLang = {
+  css: 'css',
+  js: 'javascript',
+  jsx: 'javascript',
+  html: 'markup'
+};
 
 function SolutionViewer({ files }) {
   return (
     <div>
+      <Helmet>
+        <link href='/css/prism.css' rel='stylesheet' />
+      </Helmet>
       {
         Object.keys(files)
           .map(key => files[key])
@@ -15,7 +27,17 @@ function SolutionViewer({ files }) {
               header={ file.ext.toUpperCase() }
               key={ file.ext }
               >
-              <pre>{file.contents.trim()}</pre>
+              <pre>
+                <code
+                className={ `language-${prismLang[file.ext]}` }
+                  dangerouslySetInnerHTML={{
+                    __html: Prism.highlight(
+                      file.contents.trim(),
+                      Prism.languages[prismLang[file.ext]]
+                    )
+                  }}
+                />
+              </pre>
             </Panel>
           ))
       }
