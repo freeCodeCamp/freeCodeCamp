2043765754966503b75520c53b8b8d2f63e807f5
Anh Tran <trana1906@gmail.com>
2018-02-15 13:38:27 -0500

    fix(challenges): Created a NotFound react component, added a redux
    Added new React component and added redux states for invalid challenge
    Fixed problems for requested changes with eslint long string and used
    the existing NotFound component + use Link component
    Used react-bootstrap for Not-Found component and changed the "Map" link into
    "current-challenge" link. Deleted 'invalid' state in Challenges.jsx
    
    BREAKING CHANGE: N/A
    
    Closes #16257
    
    fix(seed): Add Img to CSS Relative Positioning Challenge (#16728)
    
    fix(challenges): Call resetQuiz() when dashedName of the quiz chang (#16828)
    
    currentIndex, selectedChoice and correct state variables of the Quiz
    component were not changing when navigating to other quiz challenges
    through map. I fixed it by calling resetQuiz() whenever dashedName of
    the quiz changes.
    
    fix(BasicJS): Change accessing obj challenge (#16849)
    
    fix(BasicJS): Change accessing obj challenge
    
    Closes #16829
    
    fix(BasicJS): Change accessing obj challenge(#16849)

--- common/app/NotFound/Not-Found.jsx
+++ common/app/NotFound/Not-Found.jsx
@@ -7,6 +7,7 @@ const propTypes = {};
 export default function NotFound() {
   return (
     <Alert bsStyle='danger'>
+<<<<<<< 410bda2394575f78f8fd0e3405c10e013544425a
       <h4>We couldn't find a page for that address.</h4>
       <p>
         Head back to &thinsp;
@@ -15,6 +16,15 @@ export default function NotFound() {
         </a>
         .
       </p>
+=======
+      <h4>Oh snap! Page not found!</h4>
+      <p>
+        Head back to
+      </p>
+        <a href='/challenges/current-challenge'>
+            your current challenge
+        </a>
+>>>>>>> fix(challenges): Created a NotFound react component, added a redux
     </Alert>
   );
 }
