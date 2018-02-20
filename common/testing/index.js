3622cb87584d8b738360cc9e8aa8ff15cd56655f
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2018-02-19 17:55:48 -0800

    fix(testing/render): Require some files needed to build challenges

--- common/testing/index.js
+++ common/testing/index.js
@@ -2,26 +2,51 @@ const _path = require('path');
 const _ = require('lodash/fp');
 const dedent = require('dedent');
 
+const challengesPath = '../../common/app/routes/Challenges';
 module.exports = function renderTest({ path, outputPath, challengesByPath }) {
   const thisTestFilePath = _path.join(outputPath, path);
   const resolveRelativeRequire = dep => _path.relative(
     thisTestFilePath,
     _path.resolve(__dirname, dep)
   );
-  const buildFromFiles =
-    resolveRelativeRequire('../../common/app/routes/Challenges/utils/build.js');
+  const buildUtils =
+    resolveRelativeRequire(`${challengesPath}/utils/build.js`);
+  const frameUtils =
+    resolveRelativeRequire(`${challengesPath}/utils/frame.js`);
   const challenge = challengesByPath[path];
-  const { dashedName, solutions, seed } = challenge;
+  const { dashedName, solutions } = challenge;
   const solution = _.first(solutions);
   const testFile = dedent`
-    import test from 'ava';
-    import challenge from './data.json';
-    import { buildFromFiles } from '${buildFromFiles}'
+    require('babel-register');
+    const test = require('ava');
+    const { Subject } = require('rx');
+    const challenge = require('./data.json');
+    const {
+      buildFromFiles
+    } = require('${buildUtils}')
+
+    const {
+      runTestsInTestFrame,
+      createTestFramer,
+      createMainFramer
+    } = require('${frameUtils}');
+    const {
+      challengeType,
+      seed
+    } = challenge;
 
     test('${dashedName}', t => {
-      t.is(
-        ${seed},
-        ${solution}
+      const frameReady = new Subject();
+      const frameTests = createTestFramer(document, getState, frameReady);
+      const challengeResults = frameReady.flatMap(() => {
+        return runTestsInTestFrame(tests);
+      })
+      const buildAndFrameChallenges = buildFromFiles(state, false)
+        .do(frameTests);
+
+      return Observable.merge(
+        buildAndFrameChallenges,
+        challengeResults
       );
     });
   `;
