febaa517e07c4bf7fad075f2493c385eb5556b39
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2018-03-21 00:15:15 +0900

    feat(app): Add Motivational Quote Easter Egg
    Added a feature where a motivational quote is randomly selected from a
    static list and displayed whenever the user closes all the panes on the
    page. Also added a quotes.js file with an array of quote objects and a
    function that returns a random quote object when called.
    
    Edit: Forgot to add double quotes to the quote blocks after moving from
    test strings to random quote objects from the quotes.js file. Added
    them here as HTML encoded left and right double quotes.
    
    Made changes @Bouncey requested.
    
    Made further changes @Bouncey requested. Also added a componentDidUpdate
    method to both ensure the quote is only changed when panes open and close,
    and also to make sure the new quote is not the same as the one that
    was last displayed.
    
    Removed the fCC logo as it was redundant and clashed with the night mode.
    Simplified the layout so the quote block is in the middle of the page and
    changed the width so that it's more readable. Also added styling to change
    the quote text color in night mode.
    
    BREAKING CHANGE: None
    
    Closes #16382

--- common/app/Panes/Panes.jsx
+++ common/app/Panes/Panes.jsx
@@ -11,6 +11,7 @@ import {
 } from './redux';
 import Pane from './Pane.jsx';
 import Divider from './Divider.jsx';
+import { fetchRandomQuote } from './quotes';
 
 const mapStateToProps = createSelector(
   panesSelector,
@@ -44,56 +45,72 @@ const propTypes = {
   render: PropTypes.func.isRequired
 };
 
+let quoteObj = fetchRandomQuote();
+
 export class Panes extends PureComponent {
   componentDidMount() {
     this.props.panesMounted();
   }
+  componentDidUpdate(prevProps) {
+    if (prevProps.panes.length === 0 && this.props.panes.length === 1) {
+      const currentQuote = quoteObj.quote;
+      let newQuoteObj = fetchRandomQuote();
+      while (currentQuote === newQuoteObj.quote) {
+        newQuoteObj = fetchRandomQuote();
+      }
+      quoteObj = newQuoteObj;
+    }
+  }
+
   renderPanes() {
     const {
       render,
       panes
     } = this.props;
-    return panes.map(({ name, left, right, dividerLeft }) => {
-      const divider = dividerLeft ?
-        (
-          <Divider
-            key={ name + 'divider' }
-            left={ dividerLeft }
-            name={ name }
-          />
-        ) :
-        null;
+    if (panes.length > 0) {
+      return panes.map(({ name, left, right, dividerLeft }) => {
+        const divider = dividerLeft ?
+          (
+            <Divider
+              key={ name + 'divider' }
+              left={ dividerLeft }
+              name={ name }
+            />
+          ) :
+          null;
 
-      return [
-        <Pane
-          key={ name }
-          left={ left }
-          right={ right }
-          >
-          { render(name) }
-        </Pane>,
-        divider
-      ];
-    }).reduce((panes, pane) => panes.concat(pane), [])
-      .filter(Boolean);
+        return [
+          <Pane
+            key={ name }
+            left={ left }
+            right={ right }
+            >
+            { render(name) }
+          </Pane>,
+          divider
+        ];
+      }).reduce((panes, pane) => panes.concat(pane), [])
+        .filter(Boolean);
+    } else {
+      return this.renderQuote();
+    }
+  }
+
+  renderQuote() {
+    return (
+      <div className='outer-flex-container'>
+        <div className='quote-container'>
+          <h2 className='quote-style'>&ldquo;{quoteObj.quote}&rdquo;</h2>
+          <h2 className='author-style'><i>&mdash;{quoteObj.author}</i></h2>
+        </div>
+      </div>
+    );
   }
 
   render() {
-    const outerStyle = {
-      height: '100%',
-      position: 'relative',
-      width: '100%'
-    };
-    const innerStyle = {
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0
-    };
     return (
-      <div style={outerStyle}>
-        <div style={innerStyle}>
+      <div className='outer-style'>
+        <div className='inner-style'>
           { this.renderPanes() }
         </div>
       </div>
