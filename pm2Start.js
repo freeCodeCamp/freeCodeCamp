863042312e103fbab2f280e836664850c19a8ae1
Varun <varun995862@gmail.com>
2018-04-06 03:57:15 -0400

    fix(pm2): update scripts and add dev deps (#17030)

--- pm2Start.js
+++ pm2Start.js
@@ -1,21 +1,18 @@
 require('dotenv').load();
-var pm2 = require('pm2');
-var nodemailer = require('nodemailer');
-var moment = require('moment-timezone');
-var _ = require('lodash');
+const pm2 = require('pm2');
 
-var instances = process.env.INSTANCES || 1;
-var serverName = process.env.SERVER_NAME || 'server';
-var maxMemory = process.env.MAX_MEMORY || '390M';
+const instances = process.env.INSTANCES || 1;
+const serverName = process.env.SERVER_NAME || 'server';
+const maxMemory = process.env.MAX_MEMORY || '390M';
 
 pm2.connect(function() {
   pm2.start({
     name: serverName,
     script: 'server/production-start.js',
-    'exec_mode': 'cluster',
+    exec_mode: 'cluster',
     instances: instances,
-    'max_memory_restart': maxMemory,
-    'NODE_ENV': 'production'
+    max_memory_restart: maxMemory,
+    NODE_ENV: 'production'
   }, function() {
     console.log(
       'pm2 started %s with %s instances at %s max memory',
