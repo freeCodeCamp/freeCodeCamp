7afd20bd6ec9fa76349d476f2a0137a5b22c704c
Bunny Vishal <bunnyvishal6@gmail.com>
2018-05-14 20:32:54 +0530

    fix Misspelling in quote "Twenty years from now" by Mark Twain
    Misspelling in quote "Twenty years from now" by Mark Twain in beta.freecodecamp.org

--- common/app/Panes/quotes.js
+++ common/app/Panes/quotes.js
@@ -9,7 +9,7 @@ const quotes = [
     author: 'Winston Churchill'
   },
   {
-    quote: 'Twenty years from you you will be more disappointed by the ' +
+    quote: 'Twenty years from now you will be more disappointed by the ' +
       'things that you didn\'t do than by the ones you did do. So throw ' +
       'off the bowlines. Sail away from the safe harbor. Catch the trade ' +
       'winds in your sails.',
