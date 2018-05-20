4f292cd806323de8f52beb4e0b638cf2843d6cc4
Stuart Taylor <staylor1012@gmail.com>
2018-05-20 23:40:55 +0100

    chore(updateEmail): Remove update email until we sort out auth0

--- common/app/routes/Settings/components/Email-Settings.jsx
+++ common/app/routes/Settings/components/Email-Settings.jsx
@@ -13,12 +13,28 @@ import {
 } from 'react-bootstrap';
 
 import TB from '../Toggle-Button';
-import EmailForm from './EmailForm.jsx';
+// import EmailForm from './EmailForm.jsx';
 import { Link } from '../../../Router';
-import { FullWidthRow, Spacer } from '../../../helperComponents';
+import { FullWidthRow } from '../../../helperComponents';
 import SectionHeader from './SectionHeader.jsx';
 import { userSelector } from '../../../redux';
 import { onRouteUpdateEmail, updateMyEmail, updateUserBackend } from '../redux';
+
+/**
+  Removed functionality until we can update auth0 at the same time
+
+  <FullWidthRow>
+    <UpdateEmailButton />
+  </FullWidthRow>
+
+
+  <FullWidthRow>
+    <EmailForm
+      initialValues={{ email, confrimEmail: ''}}
+    />
+  </FullWidthRow>
+  <Spacer />
+ */
 
 const mapStateToProps = createSelector(
   userSelector,
@@ -101,9 +117,8 @@ class EmailSettings extends PureComponent {
               You do not have an email associated with this account.
             </p>
           </FullWidthRow>
-          <FullWidthRow>
-            <UpdateEmailButton />
-          </FullWidthRow>
+
+
         </div>
       );
     }
@@ -124,12 +139,8 @@ class EmailSettings extends PureComponent {
             </HelpBlock>
           </FullWidthRow>
         }
-        <FullWidthRow>
-          <EmailForm
-            initialValues={{ email, confrimEmail: ''}}
-          />
-        </FullWidthRow>
-        <Spacer />
+
+
         <FullWidthRow>
           <Row className='inline-form-field' key='sendQuincyEmail'>
             <Col sm={ 8 }>
