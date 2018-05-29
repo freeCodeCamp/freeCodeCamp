c7c59d4ee657fac0ec5395e8227c56e750448109
Stuart Taylor <staylor1012@gmail.com>
2018-05-29 09:30:34 +0100

    feat(reporting): Use rollbarjs for error reporting in production

--- server/middlewares/error-reporter.js
+++ server/middlewares/error-reporter.js
@@ -1,15 +1,30 @@
 import debug from 'debug';
-
+import Rollbar from 'rollbar';
 import {
   isHandledError,
   unwrapHandledError
 } from '../utils/create-handled-error.js';
 
+const { ROLLBAR_APP_ID } = process.env;
+
+const rollbar = new Rollbar(ROLLBAR_APP_ID);
 const log = debug('fcc:middlewares:error-reporter');
+
+const errTemplate = ({message, ...restError}, req) => `
+Time: ${new Date(Date.now()).toISOString()}
+Error: ${message}
+Is authenticated user: ${!!req.user}
+Route: ${JSON.stringify(req.route, null, 2)}
+
+${JSON.stringify(restError, null, 2)}
+
+`;
 
 export default function errrorReporter() {
   if (process.env.NODE_ENV !== 'production' && process.env.ERROR_REPORTER) {
     return (err, req, res, next) => {
+      console.error(errTemplate(err, req));
+
       if (isHandledError(err)) {
         // log out user messages in development
         const handled = unwrapHandledError(err);
@@ -18,5 +33,9 @@ export default function errrorReporter() {
       next(err);
     };
   }
-  return (err, req, res, next) => next(err);
+  return (err, req, res, next) => {
+    console.error(errTemplate(err, req));
+    rollbar.error(err.message, err);
+    next(err);
+  };
 }
