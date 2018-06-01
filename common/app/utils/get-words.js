bc0e213529366ae158b223150b531cdd39b9103e
Quincy Larson <QuincyLarson@users.noreply.github.com>
2018-06-01 17:02:37 -0500

    feat: Update quotes and quote logic (#17381)

--- common/app/utils/get-words.js
+++ common/app/utils/get-words.js
@@ -1,8 +1,11 @@
 import words from './words.json';
 
-
 function randomItem(arr) {
   return arr[ Math.floor(Math.random() * arr.length) ];
+}
+
+export function randomQuote() {
+  return randomItem(words.motivationalQuotes);
 }
 
 export function randomPhrase() {
