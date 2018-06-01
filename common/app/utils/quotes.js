993fd01fdecd63d9d707cd0ac162f01d228c8811
Anshu Raj <imanshuraj@gmail.com>
2018-06-01 17:37:39 +0530

    Fix for typo in quotes. (#17364)

--- common/app/utils/quotes.js
+++ common/app/utils/quotes.js
@@ -67,7 +67,7 @@ const quotes = [
   },
   {
     quote:
-    'I fear not the man who I fear not the man who has practiced 10,000 ' +
+    'I fear not the man who has practiced 10,000 ' +
     'kicks once, but I fear the man who has practiced one kick 10,000 times.',
     author: 'Bruce Lee'
   },
