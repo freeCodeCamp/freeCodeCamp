ebba8e9c64de1c2e0095ea09a549f1c3523c1ebc
Mrugesh Mohapatra <me@raisedadead.com>
2018-06-28 20:27:15 +0530

    fix(auth0): Unify authentication via auth0 as provider

--- server/passport-providers.js
+++ server/passport-providers.js
@@ -13,63 +13,6 @@ export default {
     session: true,
     failureFlash: true
   },
-  'facebook-login': {
-    provider: 'facebook',
-    module: 'passport-facebook',
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    authPath: '/auth/facebook',
-    callbackURL: '/auth/facebook/callback',
-    callbackPath: '/auth/facebook/callback',
-    useCustomCallback: true,
-    successRedirect: successRedirect,
-    failureRedirect: failureRedirect,
-    scope: ['email'],
-    failureFlash: true
-  },
-  'google-login': {
-    provider: 'google',
-    authScheme: 'oauth2',
-    module: 'passport-google-oauth2',
-    clientID: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    authPath: '/auth/google',
-    callbackURL: '/auth/google/callback',
-    callbackPath: '/auth/google/callback',
-    useCustomCallback: true,
-    successRedirect: successRedirect,
-    failureRedirect: failureRedirect,
-    scope: ['email', 'profile'],
-    failureFlash: true
-  },
-  'twitter-login': {
-    provider: 'twitter',
-    authScheme: 'oauth',
-    module: 'passport-twitter',
-    authPath: '/auth/twitter',
-    callbackURL: '/auth/twitter/callback',
-    callbackPath: '/auth/twitter/callback',
-    useCustomCallback: true,
-    successRedirect: successRedirect,
-    failureRedirect: failureRedirect,
-    consumerKey: process.env.TWITTER_KEY,
-    consumerSecret: process.env.TWITTER_SECRET,
-    failureFlash: true
-  },
-  'github-login': {
-    provider: 'github',
-    authScheme: 'oauth2',
-    module: 'passport-github',
-    authPath: '/auth/github',
-    callbackURL: '/auth/github/callback',
-    callbackPath: '/auth/github/callback',
-    useCustomCallback: true,
-    successRedirect: successRedirect,
-    failureRedirect: failureRedirect,
-    clientID: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
-    failureFlash: true
-  },
   'auth0-login': {
     provider: 'auth0',
     module: 'passport-auth0',
@@ -83,7 +26,7 @@ export default {
     useCustomCallback: true,
     successRedirect: successRedirect,
     failureRedirect: failureRedirect,
-    scope: ['openid email'],
+    scope: ['openid profile email'],
     failureFlash: true
   }
 };
