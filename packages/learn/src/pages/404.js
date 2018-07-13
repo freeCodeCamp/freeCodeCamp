f18e0079ed127d5502cbe3228e0226f0097f00c1
Jerome Estiller <jromestiller@gmail.com>
2018-07-13 23:13:01 +0800

    feat: update 404 page (#194)

--- packages/learn/src/pages/404.js
+++ packages/learn/src/pages/404.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+
+import './404.css';
+import notFoundLogo from '../../static/img/freeCodeCamp-404.svg';
+import { quotes } from '../../static/json/quotes.json';
+
+let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 const NotFoundPage = () => (
-  <div>
+  <div className='notfound-page-wrapper'>
+    <Helmet title='Page Not Found | freeCodeCamp' />
+
+    <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>We couldn't find what you were looking for, but here is a quote:</p>
+
+    <div className='quote-wrapper'>
+      <p className='quote'>
+        <span>&#8220;</span>
+        {randomQuote.quote}
+      </p>
+      <p className='author'>- {randomQuote.author}</p>
+    </div>
+
+    <Link className='btn-curriculum' to='/'>
+      View the Curriculum
+    </Link>
   </div>
 );
 
