e78afeea2de9a2bc1461511537431028c8e80a49
Stuart Taylor <staylor1012@gmail.com>
2018-07-22 14:35:36 +0100

    feat(env): Make learn links env dependent
    (cherry picked from commit e9206494aa17c2b55db41eb9b05abb568459107c)

---
+++ common/app/Nav/links.js
@@ -0,0 +1,13 @@
+import { homeLocation } from '../../../config/env';
+
+export default [
+  {
+    content: 'Curriculum',
+    link: `${homeLocation}/learn`
+  },
+  {
+    content: 'Forum',
+    link: 'https://forum.freecodecamp.org/',
+    target: '_blank'
+  }
+];
