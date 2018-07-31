379384a249660a2d95f44ce64294dedce5d51292
Stuart Taylor <staylor1012@gmail.com>
2018-07-31 11:11:20 +0100

    fix(lint): Apply Observable bindings to new Models

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,9 @@
-'use strict';
+import { Observable } from 'rx';
 
 module.exports = function(Article) {
-
+  Article.on('dataSourceAttached', () => {
+    Article.findOne$ = Observable.fromNodeCallback(Article.findOne, Article);
+    Article.findById$ = Observable.fromNodeCallback(Article.findById, Article);
+    Article.find$ = Observable.fromNodeCallback(Article.find, Article);
+  });
 };
