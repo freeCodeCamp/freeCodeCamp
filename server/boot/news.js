5cf5979563dc6e16d5da5084c03f4e376c92dbd2
Stuart Taylor <staylor1012@gmail.com>
2018-07-31 16:58:04 +0100

    feat(news): Initial SSR implementation

--- server/boot/news.js
+++ server/boot/news.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+import NewsApp from '../../news/NewsApp';
+
 export default function newsBoot(app) {
   const router = app.loopback.Router();
 
   router.get('/', (req, res) => {
-    res.render('layout-news', {title: 'Hello News?'});
+    const markup = renderToString(<NewsApp />);
+    return res.render('layout-news', {title: 'Hello News?', markup});
   });
 
   app.use('/news', router);
