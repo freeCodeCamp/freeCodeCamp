f3207fa9024bab3cf34cfdf064cbbea7312b5b60
Stuart Taylor <staylor1012@gmail.com>
2018-07-31 11:11:20 +0100

    fix(lint): Apply Observable bindings to new Models

--- common/models/popularity.js
+++ common/models/popularity.js
@@ -1,5 +1,15 @@
-'use strict';
+import { Observable } from 'rx';
 
 module.exports = function(Popularity) {
-
+  Popularity.on('dataSourceAttached', () => {
+    Popularity.findOne$ = Observable.fromNodeCallback(
+      Popularity.findOne,
+      Popularity
+    );
+    Popularity.findById$ = Observable.fromNodeCallback(
+      Popularity.findById,
+      Popularity
+    );
+    Popularity.find$ = Observable.fromNodeCallback(Popularity.find, Popularity);
+  });
 };
