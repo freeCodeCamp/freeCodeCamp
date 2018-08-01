fbad53a2c447741b47d0ae909ba3e7634e0a2cc2
Stuart Taylor <staylor1012@gmail.com>
2018-08-01 12:40:44 +0100

    feat(client-routes): Add react-router-dom for client-side routing

---
+++ news/routes/Featured/Featured.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const propTypes = {};
+
+function Featured() {
+  return (
+    <h2>Featured</h2>
+  );
+}
+
+Featured.displayName = 'Featured';
+Featured.propTypes = propTypes;
+
+export default Featured;
---
+++ news/routes/Featured/index.js
@@ -0,0 +1,1 @@
+export { default } from './Featured';
