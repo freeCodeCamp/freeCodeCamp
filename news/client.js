f0582efbd5da5363d2fbf32afc98a6d8281d9db9
Stuart Taylor <staylor1012@gmail.com>
2018-08-01 12:42:00 +0100

    feat(nav): Add a Nav to news and update www nav links

--- news/client.js
+++ news/client.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import { render } from 'react-dom';
 
 import NewsApp from './NewsApp';
 
 const newsMountPoint = document.getElementById('news-app-mount');
 
+const App = (
+  <BrowserRouter basename='/news'>
+    <NewsApp />
+  </BrowserRouter>
+);
+
 render(
-  <NewsApp/>,
+  App,
   newsMountPoint,
   () => console.log('react has rendered and is ready to go go go go go go!!')
 );
