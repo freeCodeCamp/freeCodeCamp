1b173bc97c18b530e42542b8a9f61fd30d0dc845
Stuart Taylor <staylor1012@gmail.com>
2018-09-04 18:14:00 +0100

    chore(google): Add google scripts (#283)

--- packages/learn/gatsby-ssr.js
+++ packages/learn/gatsby-ssr.js
@@ -26,6 +26,20 @@ exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       async='true'
       key='chai-CDN'
       src='https://cdnjs.cloudflare.com/ajax/libs/chai/4.1.2/chai.min.js'
+    />,
+    <script
+      async={true}
+      src='https://www.googletagmanager.com/gtag/js?id=AW-795617839'
+    />,
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-795617839');
+        `
+      }}
     />
   ]);
 };
