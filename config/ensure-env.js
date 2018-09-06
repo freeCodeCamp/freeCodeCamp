8cf674a5b61b64a17958cdf919abc7e820d0e8b8
Bouncey <staylor1012@gmail.com>
2018-09-06 14:58:51 +0100

    feat(env): Make env vars available to client app

--- config/ensure-env.js
+++ config/ensure-env.js
@@ -1,25 +1,30 @@
-var fs = require('fs');
+require('dotenv').config();
 
-fs.access(
-  './server/rev-manifest.json',
-  function(err) {
-    if (err) {
-      console.log('\n\ncreating manifest\n\n');
-      return fs.writeFileSync('server/rev-manifest.json', '{}');
-    }
-    console.log('\n\nrev-manifest present\n\n');
-    return null;
-  }
-);
+const fs = require('fs-extra');
 
-fs.access(
-  './server/resources/pathMigration.json',
-  err => {
-    if (err) {
-      console.log('\n\ncreating pathMigration\n\n');
-      return fs.writeFileSync('server/resources/pathMigration.json', '{}');
-    }
-    console.log('\n\npathMigration present\n\n');
-    return null;
+const env = require('./env');
+
+fs.access('./api-server/server/rev-manifest.json', function(err) {
+  if (err) {
+    console.log('\n\ncreating manifest\n\n');
+    return fs.writeFileSync('./api-server/server/rev-manifest.json', '{}');
   }
+  console.log('\n\nrev-manifest present\n\n');
+  return null;
+});
+
+fs.access('./api-server/server/resources/pathMigration.json', err => {
+  if (err) {
+    console.log('\n\ncreating pathMigration\n\n');
+    return fs.writeFileSync(
+      './api-server/server/resources/pathMigration.json',
+      '{}'
+    );
+  }
+  console.log('\n\npathMigration present\n\n');
+  return null;
+});
+
+fs.ensureDir('./client/config/').then(() =>
+  fs.writeFileSync('./client/config/env.json', JSON.stringify(env))
 );
