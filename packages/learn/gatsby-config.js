9b1df5128f4c8718689228ff12428a111c6032b7
ValeraS <ValeraS@users.noreply.github.com>
2018-09-11 15:31:51 +0300

    update gatsby dependecies

--- packages/learn/gatsby-config.js
+++ packages/learn/gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     url: 'http://localhost:3000'
   },
   plugins: [
-    'gatsby-plugin-react-next',
+    'gatsby-plugin-layout',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'fcc-source-challenges',
