00cda68456c42a4727b52426bbea01a56fb475e1
ValeraS <ValeraS@users.noreply.github.com>
2018-09-12 11:41:27 +0300

    update frame-runner webpack config

--- packages/learn/webpack-frame-runner.js
+++ packages/learn/webpack-frame-runner.js
@@ -1,72 +1,36 @@
-const webpack = require('webpack');
 const path = require('path');
-const UglifyPlugin = require('uglifyjs-webpack-plugin');
 
-const __DEV__ = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-  mode: __DEV__ ? 'development' : 'production',
-  entry: './src/client/frame-runner.js',
-  devtool: __DEV__ ? 'inline-source-map' : 'source-map',
-  node: {
-    // Mock Node.js modules that Babel require()s but that we don't
-    // particularly care about.
-    fs: 'empty',
-    module: 'empty',
-    net: 'empty'
-  },
-  output: {
-    filename: __DEV__ ? 'frame-runner.js' : 'frame-runner-[hash].js',
-    path: path.join(__dirname, './static/js')
-  },
-  stats: {
-    // Examine all modules
-    maxModules: Infinity,
-    // Display bailout reasons
-    optimizationBailout: true
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      include: [ path.join(__dirname, 'client/') ],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          babelrc: false,
-          presets: [
-            [ 'es2015', { modules: false }],
-            [ 'stage-3' ]
-          ],
-          plugins: [
-            'transform-runtime',
-            'lodash'
-          ]
+module.exports = (env = {}) => {
+  const __DEV__ = env.production !== true;
+  return {
+    mode: __DEV__ ? 'development' : 'production',
+    entry: './src/client/frame-runner.js',
+    devtool: __DEV__ ? 'inline-source-map' : 'source-map',
+    output: {
+      filename: 'frame-runner.js',
+      path: path.join(__dirname, './static/js')
+    },
+    stats: {
+      // Display bailout reasons
+      optimizationBailout: true
+    },
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        include: [ path.join(__dirname, 'src/client/') ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              [ '@babel/preset-env', { modules: false } ]
+            ],
+            plugins: [
+              '@babel/plugin-transform-runtime'
+            ]
+          }
         }
-      }
-    }]
-  },
-  externals: {
-    rxjs: 'Rx'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(__DEV__ ? 'development' : 'production')
-      },
-      __DEVTOOLS__: !__DEV__
-    }),
-    // Use browser version of visionmedia-debug
-    new webpack.NormalModuleReplacementPlugin(
-      /debug\/node/,
-      'debug/src/browser'
-    )
-  ]
+      }]
+    }
+  };
 };
-
-module.exports.plugins.push(
-  new UglifyPlugin({
-    test: /\.js($|\?)/i,
-    cache: true,
-    sourceMap: true
-  })
-);
