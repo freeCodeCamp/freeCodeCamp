ec57716fa6ffd38a44f0369086c20bfa4bcd70a9
Bouncey <staylor1012@gmail.com>
2018-09-13 18:27:14 +0100

    feat(settings) Scaffold /settings route

--- client/src/components/helpers/ButtonSpacer.js
+++ client/src/components/helpers/ButtonSpacer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function ButtonSpacer() {
-  return <div className='button-spacer' />;
+  return <div className='button-spacer' style={{ padding: '5px 0' }} />;
 }
 
 ButtonSpacer.displayName = 'ButtonSpacer';
