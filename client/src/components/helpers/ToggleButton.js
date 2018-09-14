5e6e07c550b20bb0689c7084a18dae3890c5cd57
Bouncey <staylor1012@gmail.com>
2018-09-14 13:20:51 +0100

    feat(theme): Add theme settings UI

---
+++ client/src/components/helpers/ToggleButton.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import {
+  ToggleButtonGroup as BSBG,
+  ToggleButton as TB
+} from '@freecodecamp/react-bootstrap';
+
+import './toggle-button.css';
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  offLabel: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onLabel: PropTypes.string,
+  value: PropTypes.bool.isRequired
+};
+
+function getActiveClass(condition) {
+  return condition ? 'active' : 'not-active';
+}
+
+export default function ToggleButton({
+  name,
+  onChange,
+  value,
+  onLabel = 'On',
+  offLabel = 'Off'
+}) {
+  return (
+    <Fragment>
+      <BSBG name={name} onChange={onChange} type='radio'>
+        <TB
+          bsSize='lg'
+          bsStyle='primary'
+          className={`toggle-${getActiveClass(value)}`}
+          disabled={value}
+          type='radio'
+          value={1}
+          >
+          {onLabel}
+        </TB>
+        <TB
+          bsSize='lg'
+          bsStyle='primary'
+          className={`toggle-${getActiveClass(!value)}`}
+          disabled={!value}
+          type='radio'
+          value={2}
+          >
+          {offLabel}
+        </TB>
+      </BSBG>
+    </Fragment>
+  );
+}
+
+ToggleButton.displayName = 'ToggleButton';
+ToggleButton.propTypes = propTypes;
