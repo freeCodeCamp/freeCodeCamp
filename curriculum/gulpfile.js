2723a943c9cbf00980c331e2eed5047633b1b1cc
Bouncey <staylor1012@gmail.com>
2018-10-04 14:45:22 +0100

    feat(getChallenges): Get Challenges by Lang

--- curriculum/gulpfile.js
+++ curriculum/gulpfile.js
@@ -1,15 +1,6 @@
 const gulp = require('gulp');
-const util = require('gulp-util');
-const jsonMinify = require('gulp-json-minify');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
-
-gulp.task('json:minify', function() {
-  return gulp.src('./challenges/**/*.json')
-      .pipe(jsonMinify())
-      .pipe(gulp.dest('dist/challenges/'))
-      .on('error', util.log);
-});
 
 gulp.task('babel-getChallenges', () =>
   gulp.src('./getChallenges.js')
