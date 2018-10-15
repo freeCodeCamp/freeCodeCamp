d0da33f1d3907cb55a9fb794559fcadfea909ea9
Nischal Dutt <nischaldutt01@gmail.com>
2018-10-15 16:48:27 +0530

    fix: update Certificates.jsx file

--- common/app/routes/Profile/components/Certificates.jsx
+++ common/app/routes/Profile/components/Certificates.jsx
@@ -78,11 +78,6 @@ const mapStateToProps = createSelector(
     ],
     legacyCerts: [
       {
-        show: isFullStackCert,
-        title: 'Full Stack Certification',
-        showURL: 'legacy-full-stack'
-      },
-      {
         show: isFrontEndCert,
         title: 'Front End Certification',
         showURL: 'legacy-front-end'
