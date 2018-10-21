3f701438f9b3a757e9e2533757f3ba67ea4c4c42
hattila87 <44334249+hattila87@users.noreply.github.com>
2018-10-21 09:41:54 +0200

    unregisted test task

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-
+ unregistedtest4
   db: process.env.MONGODB || process.env.MONGOHQ_URL,
 
   sessionSecret: process.env.SESSION_SECRET,
@@ -17,7 +17,7 @@ module.exports = {
     callbackURL: '/auth/github/callback',
     passReqToCallback: true
   },
-
+34
   twitter: {
     consumerKey: process.env.TWITTER_KEY,
     consumerSecret: process.env.TWITTER_SECRET,
