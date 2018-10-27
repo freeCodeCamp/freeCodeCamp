876f41a2299927355bfa0b1007cf7a2097306641
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2018-10-28 01:07:00 +0530

    Create power_of_2.cpp

---
+++ guide/english/algorithms/bit-manipulation/power_of_2/power_of_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int isPowerOfTwo (int x) {
+    /**
+     * Function to check if a given number is a power of 2 
+     *      x              : checks if x == 0 and 
+     *      !(x & (x - 1)) : checks if x is a power of 2 
+     */
+    return (x && !(x & (x - 1)));
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (isPowerOfTwo(num)) 
+        printf("Given number is a power of 2\n");
+    else 
+        printf("Given number is not a power of 2\n");
+
+    return 0;
+}
