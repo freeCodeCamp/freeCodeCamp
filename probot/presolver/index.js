14471dc4fdece81a3d65ad8c6f374c6b324b61de
tbushman <tbushman@pu.bli.sh>
2018-11-01 00:55:56 -0600

    non-working pullRequest createComment

--- probot/presolver/index.js
+++ probot/presolver/index.js
@@ -27,16 +27,81 @@ function getPullRequest(context) {
 
 module.exports = probotPlugin;
 */
+function getPullRequest(context) {
+  return context.payload.pull_request || context.payload.review.pull_request;
+}
 module.exports = async app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('pull_request.labeled', async context => {
-    //const [requestedAction, prNumber] = context.payload.requested_action.identifier.split(':')
-    return context.payload.pullRequest.createComment({body:'probot label test'})
+    //const prNumber = context.payload.pull_request.number;
+    const { title, head, number, id } = getPullRequest(context);
+    const owner = context.repo().owner;
+    const repo = context.repo().repo;
+    /*const reviews =
+      (await context.github.pullRequests.getReviews({ owner, repo, number }))
+        .data || [];
+    
+    const uniqueReviews = reviews
+      .filter(review => review.commit_id === sha)
+      .reduce((reviewObj, review) => {
+        if (
+          reviewObj[review.user.id] === null ||
+          reviewObj[review.user.id] === undefined
+        ) {
+          reviewObj[review.user.id] = {
+            state: review.state,
+            submitted_at: review.submitted_at
+          };
+        } else {
+          const a = new Date(
+            reviewObj[review.user.id]["submitted_at"]
+          ).getTime();
+          const b = new Date(review.submitted_at).getTime();
+          if (a < b) {
+            reviewObj[review.user.id] = {
+              state: review.state,
+              submitted_at: review.submitted_at
+            };
+          }
+        }
+        return reviewObj;
+      }, {});
+
+      return Object.values(uniqueReviews);
+    }*/
+    //const params = 
+    const commit_id = head.sha;
+    const body = 'probot comment test';
+    const filepath = './app.js'
+    const pos = 4;
+    const event = 'COMMENT'
+    const comments = {
+      path: './app.js',
+      position: 4,
+      body: 'probot test'
+    }
+    const path = 
+      //comments[].path = 
+      filepath;
+    const position = 
+      //comments[].path = 
+      pos;
+    const reviewers = ['tbushman']
+    /*return await context.github.pullRequests.createReview({
+      owner,
+      repo,
+      number,
+      commit_id,
+      body,
+      event,
+      comments
+
+    })
     /*const issueComment = context.issue({ body: 'probot label test' })
     return context.github.issues.createComment(issueComment)*/
-    
+    return await context.github.pullRequests.createReviewRequest({owner, repo, number, reviewers})
   })
 
   // For more information on building apps:
@@ -45,3 +110,4 @@ module.exports = async app => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 }
+
--- probot/presolver/test/index.test.js
+++ probot/presolver/test/index.test.js
@@ -3,7 +3,7 @@ const nock = require('nock')
 const myProbotApp = require('..')
 const { Probot } = require('probot')
 // Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
+const payload = require('./fixtures/pullRequests.labeled')
 const issueCreatedBody = { body: 'Thanks for opening this issue!' }
 
 nock.disableNetConnect()
@@ -21,21 +21,20 @@ describe('My Probot app', () => {
   })
 
   test('creates a comment when an issue is opened', async () => {
-    // Test that we correctly return a test token
     nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
+      .post('/app/installations/421598/access_tokens')
       .reply(200, { token: 'test' })
 
     // Test that a comment is posted
-    nock('https://api.github.com')
+    /*nock('https://api.github.com')
       .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
         expect(body).toMatchObject(issueCreatedBody)
         return true
       })
-      .reply(200)
+      .reply(200)*/
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({name: 'pull_request', payload: payload})
   })
 })
 
