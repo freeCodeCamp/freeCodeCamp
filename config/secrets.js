f1f8b13a10e049e5d568152795f0c292a6640676
manishkaa <35163295+manishkaa@users.noreply.github.com>
2018-11-03 10:32:13 +0530

    update

--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,20 @@
+version: '1.0'
+steps:
+  build_image:
+    type: build
+    description: Building the image...
+    image-name: myuser/myservice
+    tag: develop # ${{CF_BRANCH}}
+
+  perform_tests:
+    image: node:5
+    working_directory: ${{main_clone}}
+    description: Performing unit tests...
+    commands:
+      - npm install gulp -g 
+      - npm install
+      - gulp unit_test
+
 const {
   MONGODB,
   MONGOHQ_URL,
