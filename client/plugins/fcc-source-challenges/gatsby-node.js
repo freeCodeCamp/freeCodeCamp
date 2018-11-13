a8fe9388954cbf7b56e5b77a28a7e57fb53e832d
Bouncey <staylor1012@gmail.com>
2018-11-13 10:07:41 +0000

    feat: Refresh challenge nodes on challenge markdown edit

--- client/plugins/fcc-source-challenges/gatsby-node.js
+++ client/plugins/fcc-source-challenges/gatsby-node.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const chokidar = require('chokidar');
+
+const curriculumPath = path.resolve(__dirname, '../../../curriculum');
+
 const { createChallengeNodes } = require('./create-Challenge-nodes');
 
 exports.sourceNodes = function sourceChallengesSourceNodes(
@@ -6,26 +11,36 @@ exports.sourceNodes = function sourceChallengesSourceNodes(
 ) {
   if (typeof pluginOptions.source !== 'function') {
     reporter.panic(`
-"source" is a required option for fcc-source-challenges. It must be a function
-that delivers challenge files to the plugin
-      `);
+    "source" is a required option for fcc-source-challenges. It must be a
+    function that delivers challenge objects to the plugin
+    `);
   }
-  // TODO: Add live seed updates
   const { createNode } = actions;
+  const watcher = chokidar.watch(curriculumPath, {
+    ignored: [/(^|[\/\\])\../, /node_modules/],
+    persistent: true
+  });
 
-  const { source } = pluginOptions;
-  return source()
-    .then(challenges =>
-      challenges
-        .filter(challenge => challenge.superBlock !== 'Certificates')
-        .map(challenge => createChallengeNodes(challenge, reporter))
-        .map(node => createNode(node))
-    )
-    .catch(e =>
-      reporter.panic(`fcc-source-challenges
+  watcher.on('ready', sourceAndCreateNodes).on('change', sourceAndCreateNodes);
+
+  function sourceAndCreateNodes(filePath) {
+    if (filePath) {
+      reporter.info(`File ${filePath} has been changed`);
+    }
+    const { source } = pluginOptions;
+    return source()
+      .then(challenges =>
+        challenges
+          .filter(challenge => challenge.superBlock !== 'Certificates')
+          .map(challenge => createChallengeNodes(challenge, reporter))
+          .map(node => createNode(node))
+      )
+      .catch(e =>
+        reporter.panic(`fcc-source-challenges
 
   ${e.message}
 
   `)
-    );
+      );
+  }
 };
--- client/utils/buildChallenges.js
+++ client/utils/buildChallenges.js
@@ -1,7 +1,7 @@
-const { getChallengesForLang } = require('@freecodecamp/curriculum');
 const _ = require('lodash');
 
-const utils = require('../utils');
+const { getChallengesForLang } = require('../../curriculum/getChallenges');
+const utils = require('./');
 const { locale } = require('../config/env.json');
 
 const dasherize = utils.dasherize;
@@ -11,7 +11,7 @@ const arrToString = arr =>
   Array.isArray(arr) ? arr.join('\n') : _.toString(arr);
 
 exports.buildChallenges = async function buildChallenges() {
-  const curriculum = await getChallengesForLang( locale );
+  const curriculum = await getChallengesForLang(locale);
   const superBlocks = Object.keys(curriculum);
   const blocks = superBlocks
     .map(superBlock => curriculum[superBlock].blocks)
@@ -20,54 +20,57 @@ exports.buildChallenges = async function buildChallenges() {
       return blocks.concat(_.flatten(currentBlocks));
     }, []);
 
-  const builtChallenges = blocks.filter(block => !block.isPrivate).map(({ meta, challenges }) => {
-    const {
-      order,
-      time,
-      template,
-      required,
-      superBlock,
-      superOrder,
-      isPrivate,
-      dashedName: blockDashedName,
-      fileName
-    } = meta;
+  const builtChallenges = blocks
+    .filter(block => !block.isPrivate)
+    .map(({ meta, challenges }) => {
+      const {
+        order,
+        time,
+        template,
+        required,
+        superBlock,
+        superOrder,
+        isPrivate,
+        dashedName: blockDashedName,
+        fileName
+      } = meta;
 
-    return challenges.map(challenge => {
-      challenge.name = nameify(challenge.title);
+      return challenges.map(challenge => {
+        challenge.name = nameify(challenge.title);
 
-      challenge.dashedName = dasherize(challenge.name);
+        challenge.dashedName = dasherize(challenge.name);
 
-      if (challenge.files) {
-        challenge.files = _.reduce(
-          challenge.files,
-          (map, file) => {
-            map[file.key] = {
-              ...file,
-              head: arrToString(file.head),
-              contents: arrToString(file.contents),
-              tail: arrToString(file.tail)
-            };
-            return map;
-          },
-          {}
-        );
-      }
-      challenge.fileName = fileName;
-      challenge.order = order;
-      challenge.block = blockDashedName;
-      challenge.isPrivate = challenge.isPrivate || isPrivate;
-      challenge.isRequired = !!challenge.isRequired;
-      challenge.time = time;
-      challenge.superOrder = superOrder;
-      challenge.superBlock = superBlock
-        .split('-')
-        .map(word => _.capitalize(word))
-        .join(' ');
-      challenge.required = required;
-      challenge.template = template;
-      return challenge;
-    });
-  }).reduce((accu, current) => accu.concat(current), [])
+        if (challenge.files) {
+          challenge.files = _.reduce(
+            challenge.files,
+            (map, file) => {
+              map[file.key] = {
+                ...file,
+                head: arrToString(file.head),
+                contents: arrToString(file.contents),
+                tail: arrToString(file.tail)
+              };
+              return map;
+            },
+            {}
+          );
+        }
+        challenge.fileName = fileName;
+        challenge.order = order;
+        challenge.block = blockDashedName;
+        challenge.isPrivate = challenge.isPrivate || isPrivate;
+        challenge.isRequired = !!challenge.isRequired;
+        challenge.time = time;
+        challenge.superOrder = superOrder;
+        challenge.superBlock = superBlock
+          .split('-')
+          .map(word => _.capitalize(word))
+          .join(' ');
+        challenge.required = required;
+        challenge.template = template;
+        return challenge;
+      });
+    })
+    .reduce((accu, current) => accu.concat(current), []);
   return builtChallenges;
 };
