80879883a902d182a3a17fb00a0034e0b1a0c073
Randell Dawson <rdawson@onepathtech.com>
2018-11-22 10:41:19 -0800

    fix: deleted typo

--- pr-tasks/index.js
+++ pr-tasks/index.js
@@ -1,5 +1,5 @@
 const { addComment } = require('./add-comment');
 const { addLabels } = require('./add-labels');
 const { closeOpen } = require('./close-open');
-L
+
 module.exports = { addComment, addLabels, closeOpen };
