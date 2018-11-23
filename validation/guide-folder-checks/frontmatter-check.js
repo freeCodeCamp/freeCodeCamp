d4aa7c0b562e90b3a761a117bec44bffb19075f0
Randell Dawson <rdawson@onepathtech.com>
2018-11-22 23:40:32 -0800

    fix: added catch for unexpected frontmatter error

--- validation/guide-folder-checks/frontmatter-check.js
+++ validation/guide-folder-checks/frontmatter-check.js
@@ -1,22 +1,31 @@
 const matter = require('gray-matter');
 const _ = require('lodash');
-const dedent = require('dedent');
 
 const frontmatterCheck = (fullPath, isTranslation, fileContent) => {
-  const { data: frontmatter } = matter(fileContent);
   let errors = [];
-  if (!frontmatter || _.isEmpty(frontmatter) || !frontmatter.title) {
+  try {
+    const { data: frontmatter } = matter(fileContent);
+    if (!frontmatter || _.isEmpty(frontmatter) || !frontmatter.title) {
+      errors.push({
+        msg: `Misplaced or missing \`title\` keyword in the front matter block. Review the [style guide](https://github.com/freeCodeCamp/freeCodeCamp/blob/master/docs/style-guide-for-guide-articles.md#title) for more details.`,
+        fullPath
+      });
+    }
+    if (isTranslation && !frontmatter.localeTitle) {
+      errors.push({
+        msg: `Missing \`localeTitle\` keyword in the front matter block. Review the [style guide](https://github.com/freeCodeCamp/freeCodeCamp/blob/master/docs/style-guide-for-guide-articles.md#title) for more details.`,
+        fullPath
+      });
+    }
+  }
+  catch(err) {
     errors.push({
-      msg: `Misplaced or missing \`title\` keyword in the front matter block. Review the [style guide](https://github.com/freeCodeCamp/freeCodeCamp/blob/master/docs/style-guide-for-guide-articles.md#title) for more details.`,
+      msg: `Unexpected syntax found in the front matter block. Review Travis CI build Details link above for more details.`,
       fullPath
     });
   }
-  if (isTranslation && !frontmatter.localeTitle) {
-    errors.push({
-      msg: `Missing \`localeTitle\` keyword in the front matter block. Review the [style guide](https://github.com/freeCodeCamp/freeCodeCamp/blob/master/docs/style-guide-for-guide-articles.md#title) for more details.`,
-      fullPath
-    });
-  }
+
+
   return errors;
 };
 
