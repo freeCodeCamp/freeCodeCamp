6f1b3145d72379ae37997aac078a3b9ea27c3611
Bouncey <staylor1012@gmail.com>
2018-11-25 10:48:40 +0000

    fix: Use handleAPIError utility

--- client/src/templates/News/redux/shortId-saga.js
+++ client/src/templates/News/redux/shortId-saga.js
@@ -2,26 +2,22 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 
 import { getArticleById } from '../../../utils/ajax';
 import { resolveShortIdComplete, resolveShortIdError } from './';
-import { handle400Error, handle500Error } from '../../../utils/handled-error';
+import { handleAPIError, wrapHandledError } from '../../../utils/handled-error';
 
 function* fetchArticleByIdSaga({ payload }) {
   try {
     const { data } = yield call(getArticleById, payload);
     yield put(resolveShortIdComplete(data));
   } catch (e) {
-    const { status } = e.response;
-    if (status >= 400 && status < 500) {
-      const handledError = handle400Error(e, { redirectTo: '/news' });
-      yield put(resolveShortIdError(handledError));
-      return;
-    }
-    if (status >= 500) {
-      const handledError = handle500Error(e, { redirectTo: '/news' });
+    const { response: { status } = {} } = e;
+    if (typeof status !== 'undefined') {
+      const handledError = wrapHandledError(
+        handleAPIError(e, { redirectTo: '/news' })
+      );
       yield put(resolveShortIdError(handledError));
       return;
     }
     yield put(resolveShortIdError(e));
-    return;
   }
 }
 
