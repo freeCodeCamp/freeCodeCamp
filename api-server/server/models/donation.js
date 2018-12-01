c3d00ba538fc9758720b7c39d4dcd0183de62dbf
Bouncey <staylor1012@gmail.com>
2018-12-01 11:22:34 +0000

    feat: Use InMemoryCache to reducve load on db

--- api-server/server/models/donation.js
+++ api-server/server/models/donation.js
@@ -1,18 +1,50 @@
 import { Observable } from 'rx';
+import debug from 'debug';
+
+import InMemoryCache from '../utils/in-memory-cache';
+
+const log = debug('fcc:boot:donate');
+const fiveMinutes = 1000 * 60 * 5;
 
 export default function(Donation) {
+  let activeDonationUpdateInterval = null;
+  const activeDonationCountCacheTTL = fiveMinutes;
+  const activeDonationCountCache = InMemoryCache(0);
+  const activeDonationsQuery$ = () =>
+    Donation.find$({
+      // eslint-disable-next-line no-undefined
+      where: { endDate: undefined }
+    }).map(instances => instances.length);
+  function cleanUp() {
+    if (activeDonationUpdateInterval) {
+      clearInterval(activeDonationUpdateInterval);
+    }
+    return;
+  }
+
+  process.on('exit', cleanUp);
   Donation.on('dataSourceAttached', () => {
     Donation.find$ = Observable.fromNodeCallback(Donation.find.bind(Donation));
     Donation.findOne$ = Observable.fromNodeCallback(
       Donation.findOne.bind(Donation)
     );
+    activeDonationsQuery$().subscribe(count => {
+      log('activeDonator count: %d', count);
+      return activeDonationCountCache.update(() => count);
+    });
+
+    activeDonationUpdateInterval = setInterval(
+      () =>
+        activeDonationsQuery$().subscribe(count => {
+          log('activeDonator count: %d', count);
+          return activeDonationCountCache.update(() => count);
+        }),
+      activeDonationCountCacheTTL
+    );
   });
 
   function getCurrentActiveDonationCount$() {
-    // eslint-disable-next-line no-undefined
-    return Donation.find$({ where: { endDate: undefined } }).map(
-      instances => instances.length
-    );
+    return Observable.of(activeDonationCountCache.get());
   }
 
   Donation.getCurrentActiveDonationCount$ = getCurrentActiveDonationCount$;
