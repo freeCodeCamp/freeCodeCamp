98101ad315c96f5092996c7debe7e9c77a3afa96
David Gisser <dgisser@gmail.com>
2018-12-05 04:33:21 -0500

    fix: fix console errors

--- client/src/templates/Challenges/redux/execute-save-lesson-epic.js
+++ client/src/templates/Challenges/redux/execute-save-lesson-epic.js
@@ -1,44 +1,26 @@
 //import { ofType } from 'redux-observable';
-import {of } from 'rxjs';
-import { ofType } from 'redux-observable';
+import { ofType} from 'redux-observable';
+import { of } from 'rxjs';
 import {
-  switchMap,
-  ignoreElements,
-  catchError
+  startWith,
+  switchMap
 } from 'rxjs/operators';
 
 import {
   types,
-  disableJSOnError
 } from './'
 
-// maybe create new challengetype import { backend } from '../../../../utils/challengeTypes';
 
 function executeSaveLessonEpic(action$, state$, { document }) {
-  return of(document).pipe(
+  return action$.pipe(
+    ofType(types.executeSaveLesson),
     switchMap(() => {
-      const saveOutput = action$.pipe(
-        ofType(types.executeSaveLesson),
-        //debounceTime(executeDebounceTimeout),
-        //filter(() => isJSEnabledSelector(state$.value)),
-        switchMap(() => {
-          console.log('// console output') //send console outputs
-          localStorage.setItem(state$.value.challenge.challengeMeta.id,
-            state$.value.challenge.challengeFiles.indexhtml.contents)
-
-        }),
-        catchError(err => {
-          console.error(err);
-          return of(disableJSOnError(err));
-        })
-      );
-      return saveOutput;
+      console.log('// console output') //send console outputs
+      localStorage.setItem(state$.value.challenge.challengeMeta.id,
+        state$.value.challenge.challengeFiles.indexhtml.contents)
+      return of()
     })
   );
 }
-//   return of(document).pipe(
-//     logsToConsole('// console output')
-//   );
-// }
 
 export default executeSaveLessonEpic;
