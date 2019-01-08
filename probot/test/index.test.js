3f3746e2b260f0a6bf2e17ae1b9ae14236a1d818
tbushman <tbushman@pu.bli.sh>
2019-01-08 13:42:12 -0700

    init tests

--- probot/test/index.test.js
+++ probot/test/index.test.js
@@ -1,7 +1,11 @@
 const expect = require('expect');
 const { Probot } = require('probot');
-const prSuccessEvent = require('./events/pullRequests.opened');
-const prExisting = require('./events/pullRequests.existing');
+const prOpened = require('./payloads/events/pullRequests.opened');
+const prExisting = require('./payloads/events/pullRequests.existing');
+const prUnrelated = require('./payloads/events/pullRequests.unrelated');
+const prOpenedFiles = require('./payloads/files/files.opened');
+const prExistingFiles = require('./payloads/files/files.existing');
+const prUnrelatedFiles = require('./payloads/files/files.unrelated');
 const probotPlugin = require('..');
 
 describe('Presolver', () => {
@@ -15,10 +19,12 @@ describe('Presolver', () => {
     // https://probot.github.io/docs/testing/
     github = {
       issues: {
+        /* eslint-disable no-undef */
         createComment: jest.fn().mockReturnValue(Promise.resolve({})),
         addLabels: jest.fn(),
         getLabel: jest.fn().mockImplementation(() => Promise.resolve([])),
         createLabel: jest.fn()
+        /* eslint-enable no-undef */
       },
       repos: {
         getContent: () =>
@@ -32,12 +38,34 @@ describe('Presolver', () => {
           })
       },
       pullRequests: {
-        getFiles: jest.fn().mockImplementation(() => ({
-          data: [{ filename: 'test.txt' }]
-        })),
+        /* eslint-disable no-undef */
+        getFiles: jest.fn().mockImplementation((issue) => {
+          const { number } = issue;
+          let data;
+          switch (number) {
+            case 31525:
+              data = prOpenedFiles;
+              break;
+            case 33363:
+              data = prExistingFiles;
+              break;
+            case 34559:
+              data = prUnrelatedFiles;
+              break;
+            default:
+              data = prExistingFiles;
+          }
+          return { data };
+        }),
+        /* eslint-disable no-undef */
         getAll: jest
+        /* eslint-enable no-undef */
           .fn()
-          .mockImplementation(() => ({ data: [prExisting.pull_request] }))
+          .mockImplementation(() => ({ data: [
+            prExisting.pull_request,
+            prOpened.pull_request,
+            prUnrelated.pull_request
+          ] }))
       }
     };
     app.auth = () => Promise.resolve(github);
@@ -45,11 +73,20 @@ describe('Presolver', () => {
     // app.app = () => 'test'
   });
 
-  test('adds a label if a PR has changes to files targeted by an existing PR', async () => {
+  test(`adds a label if a PR has changes to files targeted by an 
+    existing PR`, async () => {
     // Receive a webhook event
     await probot.receive({
       name: 'pull_request.opened',
-      payload: prSuccessEvent
+      payload: prOpened
+    });
+    expect(github.issues.addLabels).toHaveBeenCalled();
+  });
+
+  test('does not add a label when files do not coincide', async () => {
+    await probot.receive({
+      name: 'pull_request.opened',
+      payload: prUnrelated
     });
     expect(github.issues.addLabels).toHaveBeenCalled();
   });
