dd914d978c06b9f32e55fe5552989da527435edf
Ron Sogueco <ron@soguecode.io>
2019-01-26 18:04:38 -0500

    feat: remove react-d3-library package

--- client/src/components/Calculators/CodingBootcampCostCalculator.js
+++ client/src/components/Calculators/CodingBootcampCostCalculator.js
@@ -1,10 +1,6 @@
 import React from 'react';
-// import rd3 from 'react-d3-library';
 import CostCalculator from './CostCalculator';
-// import { updateCalculator } from './calculator';
 import './codingbootcampcostcalculator.css';
-
-// const RD3Component = rd3.Component;
 
 class CodingBootcampCostCalculator extends React.Component {
   constructor(props) {
