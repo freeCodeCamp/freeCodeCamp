bc9eaceba1bfe0ca6ab1e45016b7ea2093e499d5
Mrugesh Mohapatra <me@raisedadead.com>
2019-02-07 21:59:01 +0530

    fix(donate): extract active donors from selectors

--- client/src/components/Donation/components/DonateText.js
+++ client/src/components/Donation/components/DonateText.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
-class DonateText extends Component {
-  render() {
-    return (
-      <div className='text-center'>
-        <p>
-          freeCodeCamp.org is a tiny nonprofit that's helping millions of
-          people learn to code for free.
-        </p>
-        <p>
-          Join <strong>4,180</strong> supporters.
-        </p>
-        <p>
-          Your $5 / month donation will help keep tech education free and
-          open.
-        </p>
-      </div>
-    );
-  }
-}
+import { activeDonationsSelector } from '../../../redux';
 
-export default DonateText;
+const propTypes = {
+  activeDonations: PropTypes.number
+};
+
+const mapStateToProps = createSelector(
+  activeDonationsSelector,
+  activeDonations => ({ activeDonations })
+);
+
+const DonateText = ({ activeDonations }) => {
+  const donationsLocale = activeDonations.toLocaleString();
+  return (
+    <div className='text-center'>
+      <p>
+        freeCodeCamp.org is a tiny nonprofit that's helping millions of
+        people learn to code for free.
+      </p>
+      <p>
+        Join <strong>{donationsLocale}</strong> supporters.
+      </p>
+      <p>
+        Your $5 / month donation will help keep tech education free and
+        open.
+      </p>
+    </div>
+  );
+};
+
+DonateText.displayName = 'DonateText';
+DonateText.propTypes = propTypes;
+
+export default connect(mapStateToProps)(DonateText);
