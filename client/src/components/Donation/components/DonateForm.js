260d09383bbb4223481a89ab3c478db36784441f
Bouncey <staylor1012@gmail.com>
2019-02-15 20:52:11 +0000

    fix: Revert to relative ajax urls

--- client/src/components/Donation/components/DonateForm.js
+++ client/src/components/Donation/components/DonateForm.js
@@ -121,7 +121,7 @@ class DonateForm extends Component {
     }));
 
     const chargeStripePath = isSignedIn ?
-      `${apiLocation}/internal/donate/charge-stripe` :
+      `/internal/donate/charge-stripe` :
       `${apiLocation}/unauthenticated/donate/charge-stripe`;
     return postJSON$(chargeStripePath, {
       token,
