71cf52144e22f14bf7de1b9eccded99ede25e40a
Mrugesh Mohapatra <me@raisedadead.com>
2019-02-17 16:26:10 +0530

    fix(footer): show only on non-learn pages

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -6,7 +6,7 @@ import { createStore } from './src/redux/createStore';
 import AppMountNotifier from './src/components/AppMountNotifier';
 import GuideNavContextProvider from './src/contexts/GuideNavigationContext';
 import DefaultLayout from './src/components/layouts/Default';
-import GuideLayout from './src/components/layouts/GuideLayout';
+import GuideLayout from './src/components/layouts/Guide';
 
 const store = createStore();
 
@@ -30,15 +30,26 @@ export const wrapPageElement = ({ element, props }) => {
   } = props;
   if (pathname === '/') {
     return (
-      <DefaultLayout disableSettings={true} landingPage={true}>
+      <DefaultLayout
+        disableSettings={true}
+        landingPage={true}
+        nonLearnPage={true}
+        >
         {element}
       </DefaultLayout>
     );
   }
   if ((/^\/guide(\/.*)*/).test(pathname)) {
     return (
-      <DefaultLayout>
+      <DefaultLayout nonLearnPage={true}>
         <GuideLayout>{element}</GuideLayout>
+      </DefaultLayout>
+    );
+  }
+  if (false === (/^\/learn(\/.*)*/).test(pathname)) {
+    return (
+      <DefaultLayout nonLearnPage={true}>
+        {element}
       </DefaultLayout>
     );
   }
