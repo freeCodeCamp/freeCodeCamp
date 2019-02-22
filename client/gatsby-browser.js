293e58cc235db6d313a16e03e18739362f89a250
mrugesh mohapatra <1884376+raisedadead@users.noreply.github.com>
2019-02-22 20:36:47 +0530

    fix(certs): remove footer and nav from certs (#35333)
    * fix(certs): remove footer and nav from certs
    
    * fix: revert spacing on cert

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux';
 import { createStore } from './src/redux/createStore';
 import AppMountNotifier from './src/components/AppMountNotifier';
 import GuideNavContextProvider from './src/contexts/GuideNavigationContext';
-import DefaultLayout from './src/components/layouts/Default';
-import GuideLayout from './src/components/layouts/Guide';
+import {
+  CertificationLayout,
+  DefaultLayout,
+  GuideLayout
+} from './src/components/layouts';
 
 const store = createStore();
 
@@ -34,6 +37,9 @@ export const wrapPageElement = ({ element, props }) => {
         {element}
       </DefaultLayout>
     );
+  }
+  if (/^\/certification(\/.*)*/.test(pathname)) {
+    return <CertificationLayout>{element}</CertificationLayout>;
   }
   if (/^\/guide(\/.*)*/.test(pathname)) {
     return (
