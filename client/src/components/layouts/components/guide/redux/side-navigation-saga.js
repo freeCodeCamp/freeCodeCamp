f91157886e1fc30996329de3b92e31bf5ed7c3be
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-02-28 22:25:38 +0300

    fix: moved sidenav logic to saga

--- client/src/components/layouts/components/guide/redux/side-navigation-saga.js
+++ client/src/components/layouts/components/guide/redux/side-navigation-saga.js
@@ -1,14 +1,31 @@
-import { takeEvery, put } from 'redux-saga/effects';
+import { takeEvery, put, all } from 'redux-saga/effects';
 
-// import { put, takeEvery, take } from 'redux-saga/effects';
-
-import { openDonationModal } from '../../../../../redux';
+import { toggleExpandedState } from './';
+import { types as appTypes } from '../../../../../redux';
 
 function* showDonateModalSaga() {
-  console.log('hello');
-  yield put(openDonationModal());
+  let guideRegex = /^\/guide\//;
+  let onGuide = guideRegex.test(window.location.pathname);
+  if (onGuide && typeof window !== 'undefined') {
+    const pathMap = window.location.pathname
+      .slice(1)
+      .split('/')
+      .slice(0, -1)
+      .reduce((map, current, i, pathArray) => {
+        const path =
+          i !== 0 ? map[pathArray[i - 1]] + `/${current}` : `/${current}`;
+        return {
+          ...map,
+          [current]: path
+        };
+      }, {});
+
+    let routes = Object.keys(pathMap).map(key => pathMap[key]);
+
+    yield all(routes.map(route => put(toggleExpandedState(route))));
+  }
 }
 
-export function createSideNavigationSaga(types) {
-  takeEvery(types.toggleDisplaySideNav, showDonateModalSaga);
+export function createSideNavigationSaga() {
+  return [takeEvery(appTypes.appMount, showDonateModalSaga)];
 }
