07ec7045bc46851e1f1dc5a16a622c9be7587c13
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-03-06 15:00:37 +0300

    fix: optimize font usage

--- client/src/assets/fonts/index.js
+++ client/src/assets/fonts/index.js
@@ -1,11 +1,4 @@
-import saxmonoRegularWoff from './saxmono-webfont.woff';
-import saxmonoRegularTff from './saxmono-webfont.ttf';
 import hackRegularWoff from './Hack-Regular-webfont.woff';
 import hackRegularTff from './Hack-Regular-webfont.ttf';
 
-export {
-  saxmonoRegularWoff,
-  saxmonoRegularTff,
-  hackRegularWoff,
-  hackRegularTff
-};
+export { hackRegularWoff, hackRegularTff };
--- client/src/assets/styles/index.js
+++ client/src/assets/styles/index.js
@@ -7,22 +7,12 @@ const GlobalStyle = createGlobalStyle`
     --primary-color: ${props => (props.darkMode ? 'black' : 'white')};
     --secondary-color: ${props => (props.darkMode ? '#0a0a2e' : '#f5f6f7')};
     --tertiary-color: ${props => (props.darkMode ? '#292948' : '#e8e8eb')};
-    --quaternary-color: ${props => (props.darkMode ? '#d4d4da' : '#43435d')};
-    --quaternary-canvas: ${props => (props.darkMode ? '#43435d' : '#d4d4da')};
+    --quaternary-color: ${props => (props.darkMode ? '#43435d' : '#d4d4da')};
+    --quaternary-canvas: ${props => (props.darkMode ? '#d4d4da' : '#43435d')};
     --tertiary-canvas: ${props => (props.darkMode ? '#e8e8eb' : '#292948')};
     --secondary-canvas: ${props => (props.darkMode ? '#f5f6f7' : '#0a0a2e')};
     --primary-canvas: ${props => (props.darkMode ? 'white' : '#0a0a2e')};
     --theme-color:#0a0a2e;
-  }
-
-  @font-face {
-    font-family: "saxmonoregular";
-    font-style: normal;
-    font-weight: normal;
-    src: local("saxmonoregular"),
-    local("saxmonoregular"),
-    url(${fontFiles.saxmonoRegularTff}) format("truetype"),
-    url(${fontFiles.saxmonoRegularWoff}) format("woff");
   }
 
   @font-face {
