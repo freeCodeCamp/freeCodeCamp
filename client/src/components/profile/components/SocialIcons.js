cbaea46504850a398adf11e63d6ec0fa90754dbc
Mrugesh Mohapatra <me@raisedadead.com>
2019-04-02 22:10:07 +0530

    fix: rollback gatsby-remark-prismjs & more updates

--- client/src/components/profile/components/SocialIcons.js
+++ client/src/components/profile/components/SocialIcons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from '@freecodecamp/react-bootstrap';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faLinkedin,
   faGithub,
