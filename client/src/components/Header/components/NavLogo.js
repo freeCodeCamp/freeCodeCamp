18ee2587b079ad08f4fae67ce2fcf53571b00210
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2019-04-30 18:18:08 +0200

    Restore smaller logo

--- client/src/components/Header/components/NavLogo.js
+++ client/src/components/Header/components/NavLogo.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const fCClogo = 'https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg';
+const fCCglyph = 'https://s3.amazonaws.com/freecodecamp/FFCFire.png';
 
 function NavLogo() {
   return (
-    <img
-      alt='learn to code at freeCodeCamp logo'
-      className='nav-logo logo'
-      src={fCClogo}
-    />
+    <>
+      <img
+        alt='learn to code at freeCodeCamp logo'
+        className='nav-logo logo'
+        src={fCClogo}
+      />
+      <img
+        alt='learn to code at freeCodeCamp logo'
+        className='nav-logo logo-glyph'
+        src={fCCglyph}
+      />
+    </>
   );
 }
 
