698ffbe92bc0f6e367075b3cf7dcc135f66ed9a4
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2019-04-30 14:39:48 +0200

    Remove smaller logo

--- client/src/components/Header/components/NavLogo.js
+++ client/src/components/Header/components/NavLogo.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import Media from 'react-responsive';
 
 const fCClogo = 'https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg';
-const fCCglyph = 'https://s3.amazonaws.com/freecodecamp/FFCFire.png';
 
 function NavLogo() {
   return (
-    <>
-      <Media minWidth={735}>
-        <img
-          alt='learn to code at freeCodeCamp logo'
-          className='nav-logo logo'
-          src={fCClogo}
-        />
-      </Media>
-      <Media maxWidth={734}>
-        <img
-          alt='learn to code at freeCodeCamp logo'
-          className='nav-logo logo-glyph'
-          src={fCCglyph}
-        />
-      </Media>
-    </>
+    <img
+      alt='learn to code at freeCodeCamp logo'
+      className='nav-logo logo'
+      src={fCClogo}
+    />
   );
 }
 
