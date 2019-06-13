db69c9b071b8ed3761f005e6b48e1f638bed40ca
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2019-06-13 11:26:08 +0200

    fix: Only lint staged files on commit

---
+++ curriculum/tools/lint.js
@@ -0,0 +1,28 @@
+const markdownlint = require('markdownlint');
+
+const lintYAML = require('./markdown-yaml');
+const lintConfig = require('./.markdownlintrc.js');
+const argv = require('yargs').argv;
+
+const isMDRE = /.*\.md$/;
+
+const files = argv._.filter(arg => isMDRE.test(arg));
+files.forEach(path => lint({ path: path }));
+
+function lint(file, next) {
+  const options = {
+    files: [file.path],
+    config: lintConfig,
+    customRules: [lintYAML]
+  };
+  markdownlint(options, function callback(err, result) {
+    const resultString = (result || '').toString();
+    if (resultString) {
+      process.exitCode = 1;
+      console.log(resultString);
+    }
+    if (next) next(err, file);
+  });
+}
+
+module.exports = lint;
