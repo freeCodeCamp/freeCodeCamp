55e4bfd8f799c2f43bf80f02e40c727f320e8afc
Ryan Agard <jragard@gmail.com>
2019-06-25 00:07:09 -0400

    fix(client): add warning on exceeding forum post length limit
    Co-authored-by: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>

--- client/src/templates/Challenges/redux/create-question-epic.js
+++ client/src/templates/Challenges/redux/create-question-epic.js
@@ -44,28 +44,63 @@ function createQuestionEpic(action$, state$, { window }) {
         navigator: { userAgent },
         location: { href }
       } = window;
+
       const textMessage = [
         "**Tell us what's happening:**\n\n\n\n",
         '**Your code so far**\n',
         filesToMarkdown(files),
         '**Your browser information:**\n\n',
-        'User Agent is: `',
+        'User Agent is: <code>',
         userAgent,
-        '`.\n\n',
+        '</code>.\n\n',
         '**Challenge:**\n',
         challengeTitle,
         '\n**Link to the challenge:**\n',
         href
       ].join('');
-      window.open(
-        'https://forum.freecodecamp.org/new-topic' +
-          '?category=' +
-          window.encodeURIComponent(helpCategory[challengeType] || 'Help') +
-          '&title=' +
-          '&body=' +
-          window.encodeURIComponent(textMessage),
-        '_blank'
+
+      const altTextMessage = [
+        "**Tell us what's happening:**\n\n\n\n",
+        '**Your code so far**\n\n',
+        'WARNING\n\n',
+        'The challenge seed code and/or your solution exceeded the maximum',
+        ' length we can port over from the challenge.\n\n',
+        'You will need to take an additional step here so the code you wrote',
+        'presents in an easy to read format.\n\n',
+        'Please copy/paste all the editor code showing in the challenge ',
+        'from where you just linked.\n\n',
+        '```js\n\n',
+        'Replace these two sentences with your copied code. \n',
+        'Please leave the ```js line above and the ``` line below, ',
+        'because they allow your code to properly format in the post.\n\n',
+        '```\n\n',
+        '**Your browser information:**\n\n',
+        'User Agent is: <code>',
+        userAgent,
+        '</code>.\n\n',
+        '**Challenge:**\n',
+        challengeTitle,
+        '\n\n**Link to the challenge:**\n',
+        href
+      ].join('');
+
+      const category = window.encodeURIComponent(
+        helpCategory[challengeType] || 'Help'
       );
+
+      const studentCode = window.encodeURIComponent(textMessage);
+      const altStudentCode = window.encodeURIComponent(altTextMessage);
+
+      const defaultURI = `https://www.freecodecamp.org/forum/new-topic?category=${category}
+                          &title=&body=${studentCode}`;
+      const altURI = `https://www.freecodecamp.org/forum/new-topic?category=${category}
+      &title=&body=${altStudentCode}`;
+
+      if (defaultURI.length < 8000) {
+        window.open(defaultURI, '_blank');
+      } else {
+        window.open(altURI, '_blank');
+      }
     }),
     mapTo(closeModal('help'))
   );
