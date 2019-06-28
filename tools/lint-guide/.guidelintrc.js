924f995af398cc4b8883bd508908201d51411d78
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2019-06-28 14:46:08 +0200

    fix(tools): Relax rules for the guide

--- tools/lint-guide/.guidelintrc.js
+++ tools/lint-guide/.guidelintrc.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   "default": false,    // the guide has a more permissive set of rules
-  "MD031": true,       // fenced blocks do need surrounding by newlines
+  "MD031": false,      // code fences do not need surrounding by newlines
+                       // unlike the curriculum, they are not embedded in html.
   "MD040": true,       // code fence languages should be specificed
   "prism-langs": true, // code fence languages should be supported by PrismJS
 }
