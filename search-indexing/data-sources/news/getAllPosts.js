1c876ad14063e50f12cef1afd4481537ee67dbc2
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-07-25 23:01:18 +0900

    feat: Add script to poll Ghost for all published posts and construct index for Algolia.

--- search-indexing/data-sources/news/getAllPosts.js
+++ search-indexing/data-sources/news/getAllPosts.js
@@ -3,14 +3,17 @@ const path = require('path');
 const envPath = path.resolve(__dirname, '../../../.env');
 require('dotenv').config({ path: envPath });
 
+const { GHOST_CLIENT_KEY } = process.env;
+
 const axios = require('axios');
 const fs = require('fs');
 
-const getJson = async (url) => {
-  return axios.get(url)
-  .then(res => res.data )
-  .catch(err => console.log(err));
-}
+const getJson = async url => {
+  return axios
+    .get(url)
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
 
 const constructIndex = async () => {
   let currPage = 1;
@@ -19,7 +22,9 @@ const constructIndex = async () => {
   const posts = [];
 
   while (currPage && currPage <= lastPage) {
-    const data = await getJson(`https://www.freecodecamp.org/news/ghost/api/v2/content/posts/?key=${process.env.GHOST_CLIENT_KEY}&include=tags,authors&page=${currPage}`);
+    const data = await getJson(
+      `https://www.freecodecamp.org/news/ghost/api/v2/content/posts/?key=${GHOST_CLIENT_KEY}&include=tags,authors&page=${currPage}`
+    );
 
     data.posts.forEach(post => {
       const thisPost = {
@@ -27,19 +32,19 @@ const constructIndex = async () => {
         author: {
           name: post.primary_author.name,
           url: post.primary_author.url,
-          profile_image: post.primary_author.profile_image
+          profileImage: post.primary_author.profile_image
         },
         tags: post.tags.map(obj => {
           return {
             name: obj.name,
             url: obj.url
-          }
+          };
         }),
         url: post.url,
-        feature_image: post.feature_image,
-        ghost_id: post.id,
-        published_at: post.published_at
-      }
+        featureImage: post.feature_image,
+        ghostId: post.id,
+        publishedAt: post.published_at
+      };
 
       posts.push(thisPost);
     });
@@ -52,6 +57,6 @@ const constructIndex = async () => {
     fs.writeFileSync('posts.json', JSON.stringify(posts, null, 2));
     await delay(1000);
   }
-}
+};
 
 constructIndex();
