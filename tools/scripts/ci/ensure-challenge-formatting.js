31f337f2bd1e750ecc0171f303e936969ca1ced3
Mrugesh Mohapatra <me@raisedadead.com>
2019-08-01 20:13:05 +0530

    fix(tools): update challege formatting check

--- tools/scripts/ci/ensure-challenge-formatting.js
+++ tools/scripts/ci/ensure-challenge-formatting.js
@@ -51,21 +51,26 @@ const challengeFrontmatterValidator = file => frontmatter => {
 
     `);
   }
+
   const { videoUrl } = frontmatter;
-
   let validVideoUrl = false;
-  if (isEmpty(videoUrl)) {
+  if (!isEmpty(videoUrl) && !scrimbaUrlRE.test(videoUrl)) {
     validVideoUrl = true;
-  } else {
-    validVideoUrl = scrimbaUrlRE.test(videoUrl);
-
-    if (!validVideoUrl) {
-      console.log(`
-  ${fullPath} contains an invalid videoUrl
-`);
-    }
+    console.log(`
+        ${fullPath} contains an invalid videoUrl
+      `);
   }
-  return hasRequiredProperties && validVideoUrl;
+
+  const { forumTopicId } = frontmatter;
+  let validForumTopicId = false;
+  if (!isEmpty(forumTopicId) && !isNumber(forumTopicId)) {
+    validForumTopicId = true;
+    console.log(`
+      ${fullPath} contains an invalid forumTopicId
+    `);
+  }
+
+  return hasRequiredProperties && validVideoUrl && validForumTopicId;
 };
 
 function isChallengeParseable(file) {
