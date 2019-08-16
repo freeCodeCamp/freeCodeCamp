3891ec72de53117f17650ade9abc6fedd6f2129f
Randell Dawson <rdawson@onepathtech.com>
2019-08-16 14:09:20 -0700

    fix: added forumTopicId to ChallengeNode PropTypes

--- client/src/redux/propTypes.js
+++ client/src/redux/propTypes.js
@@ -20,6 +20,7 @@ export const MarkdownRemark = PropTypes.shape({
 
 export const ChallengeNode = PropTypes.shape({
   block: PropTypes.string,
+  challengeOrder: PropTypes.number,
   challengeType: PropTypes.number,
   dashedName: PropTypes.string,
   description: PropTypes.string,
@@ -31,9 +32,9 @@ export const ChallengeNode = PropTypes.shape({
     slug: PropTypes.string,
     blockName: PropTypes.string
   }),
+  forumTopicId: PropTypes.number,
   guideUrl: PropTypes.string,
   head: PropTypes.arrayOf(PropTypes.string),
-  challengeOrder: PropTypes.number,
   instructions: PropTypes.string,
   isBeta: PropTypes.bool,
   isComingSoon: PropTypes.bool,
