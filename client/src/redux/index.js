8ebbde1e98bfdaf655eda514901e2f1bcf18abce
lancerutkin <lance.rutkin@gmail.com>
2019-08-18 21:34:06 -0400

    Track if donation modal has opened in a session

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -28,6 +28,7 @@ export const defaultFetchState = {
 const initialState = {
   appUsername: '',
   completionCount: 0,
+  donationRequested: false,
   showCert: {},
   showCertFetchState: {
     ...defaultFetchState
@@ -122,6 +123,7 @@ export const completedChallengesSelector = state =>
 export const completionCountSelector = state => state[ns].completionCount;
 export const currentChallengeIdSelector = state =>
   userSelector(state).currentChallengeId || '';
+  export const donationRequestedSelector = state => state[ns].donationRequested;
 
 export const isOnlineSelector = state => state[ns].isOnline;
 export const isSignedInSelector = state => !!state[ns].appUsername;
@@ -136,16 +138,17 @@ export const showDonationSelector = state => {
   const completedChallenges = completedChallengesSelector(state);
   const completionCount = completionCountSelector(state);
   const currentCompletedLength = completedChallenges.length;
+  const donationRequested = donationRequestedSelector(state);
   // the user has not completed 9 challenges in total yet
   if (currentCompletedLength < 9) {
     return false;
   }
   // this will mean we are on the 10th submission in total for the user
-  if (completedChallenges.length === 9) {
+  if (completedChallenges.length === 9 && donationRequested === false) {
     return true;
   }
   // this will mean we are on the 3rd submission for this browser session
-  if (completionCount === 2) {
+  if (completionCount === 2 && donationRequested === false) {
     return true;
   }
   return false;
@@ -260,7 +263,8 @@ export const reducer = handleActions(
     }),
     [types.openDonationModal]: state => ({
       ...state,
-      showDonationModal: true
+      showDonationModal: true,
+      donationRequested: true
     }),
     [types.resetUserData]: state => ({
       ...state,
