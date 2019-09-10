32dd61f590d987f5a101ebcb7c5fcd4aa3a9481b
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-09-10 19:41:58 +0900

    feat: Passing hits from SearchHits to parent SearchBar to keep track of with the keyboard. Moved all logic for number of hits to WithInstantSearch.js

--- client/src/components/search/searchBar/SearchBar.js
+++ client/src/components/search/searchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -50,6 +51,12 @@ class SearchBar extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handlePageClick = this.handlePageClick.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleHits = this.handleHits.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.state = {
+      currentHits: [],
+      cursor: 0
+    };
   }
 
   componentDidMount() {
@@ -81,20 +88,68 @@ class SearchBar extends Component {
     const { toggleSearchDropdown, updateSearchQuery } = this.props;
     // disable the search dropdown
     toggleSearchDropdown(false);
-    if (query) {
-      updateSearchQuery(query);
+    if (!query) {
+      query = e.currentTarget.children[0].value;
     }
+    updateSearchQuery(query);
+
     // For Learn search results page
     // return navigate('/search');
 
     // Temporary redirect to News search results page
-    return window.location.assign(
-      `https://freecodecamp.org/news/search/?query=${query}`
-    );
+    // return window.location.assign(
+    //   `https://freecodecamp.org/news/search/?query=${query}`
+    // );
+  }
+
+  handleHits(hits) {
+    const { toggleSearchDropdown } = this.props;
+    if (hits.length !== this.state.currentHits.length) {
+      // Toggle dropdown if the length of hits suddenly changes
+      // because the dropdown is open while the window
+      // height moves above / below 768 px
+      toggleSearchDropdown(false);
+    }
+
+    // console.log(hits, this.state.currentHits);
+    this.setState({ currentHits: hits });
+  }
+
+  handleKeyDown(e) {
+    const { cursor, currentHits } = this.state;
+    const upKey = e.keyCode === 38;
+    const downKey = e.keyCode === 40;
+    if (upKey || downKey) {
+      // Prevent cursor from jumping to
+      // beginning or end of search bar
+      e.preventDefault();
+    }
+
+    if (upKey && cursor > 0) {
+      this.setState(prevState => ({
+        cursor: prevState.cursor - 1
+      }));
+    } else if (downKey && cursor < currentHits.length - 1) {
+      this.setState(prevState => ({
+        cursor: prevState.cursor + 1
+      }));
+    }
   }
 
   render() {
     const { isDropdownEnabled, isSearchFocused } = this.props;
+    // const { cursor, currentHits } = this.state;
+    const { cursor } = this.state;
+    // if (isDropdownEnabled, isSearchFocused) {
+    const hitsTest = document.querySelectorAll('.ais-Hits-item');
+    const selectedHit = hitsTest[cursor];
+    if (selectedHit) {
+      // console.log(cursor, hitsTest[cursor].childNodes[0]);
+      console.log(ReactDOM.findDOMNode(this));
+    }
+    // }
+    // console.log(cursor, currentHits[cursor], currentHits);
+
     return (
       <div
         className='fcc_searchBar'
@@ -107,12 +162,16 @@ class SearchBar extends Component {
           </label>
           <SearchBox
             onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
             onSubmit={this.handleSearch}
             showLoadingIndicator={true}
             translations={{ placeholder }}
           />
           {isDropdownEnabled && isSearchFocused && (
-            <SearchHits handleSubmit={this.handleSearch} />
+            <SearchHits
+              getHits={this.handleHits}
+              handleSubmit={this.handleSearch}
+            />
           )}
         </div>
       </div>
--- client/src/components/search/searchBar/SearchHits.js
+++ client/src/components/search/searchBar/SearchHits.js
@@ -3,48 +3,55 @@ import { connectStateResults, connectHits } from 'react-instantsearch-dom';
 import isEmpty from 'lodash/isEmpty';
 import Suggestion from './SearchSuggestion';
 
-const CustomHits = connectHits(({ hits, currentRefinement, handleSubmit }) => {
-  const shortenedHits = hits.filter((hit, i) => i < 8);
-  const defaultHit = [
-    {
-      objectID: `default-hit-${currentRefinement}`,
-      query: currentRefinement,
-      _highlightResult: {
-        query: {
-          value: `
+const CustomHits = connectHits(
+  ({ hits, currentRefinement, handleSubmit, getHits }) => {
+    const footer = [
+      {
+        objectID: `default-hit-${currentRefinement}`,
+        query: currentRefinement,
+        _highlightResult: {
+          query: {
+            value: `
             See all results for
             <ais-highlight-0000000000>
             ${currentRefinement}
             </ais-highlight-0000000000>
           `
+          }
         }
       }
-    }
-  ];
-  return (
-    <div className='ais-Hits'>
-      <ul className='ais-Hits-list'>
-        {shortenedHits.concat(defaultHit).map(hit => (
-          <li
-            className='ais-Hits-item'
-            data-fccobjectid={hit.objectID}
-            key={hit.objectID}
-          >
-            <Suggestion handleSubmit={handleSubmit} hit={hit} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-});
+    ];
+    const allHits = hits.concat(footer);
+    getHits(allHits);
 
-const SearchHits = connectStateResults(({ handleSubmit, searchState }) => {
-  return isEmpty(searchState) || !searchState.query ? null : (
-    <CustomHits
-      currentRefinement={searchState.query}
-      handleSubmit={handleSubmit}
-    />
-  );
-});
+    return (
+      <div className='ais-Hits'>
+        <ul className='ais-Hits-list'>
+          {allHits.map(hit => (
+            <li
+              className='ais-Hits-item'
+              data-fccobjectid={hit.objectID}
+              key={hit.objectID}
+            >
+              <Suggestion handleSubmit={handleSubmit} hit={hit} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+);
+
+const SearchHits = connectStateResults(
+  ({ handleSubmit, getHits, searchState }) => {
+    return isEmpty(searchState) || !searchState.query ? null : (
+      <CustomHits
+        currentRefinement={searchState.query}
+        getHits={getHits}
+        handleSubmit={handleSubmit}
+      />
+    );
+  }
+);
 
 export default SearchHits;
