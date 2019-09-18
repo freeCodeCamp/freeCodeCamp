3f5dd55e8f2cdab9a9a11e961c02f2e0bf91492c
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-09-18 21:57:51 +0530

    prettier fix

--- client/src/components/landing/index.js
+++ client/src/components/landing/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Grid, Row, Col, Image } from '@freecodecamp/react-bootstrap';
+import { Grid, Row, Col } from '@freecodecamp/react-bootstrap';
 import Helmet from 'react-helmet';
 
 import Login from '../Header/components/Login';
