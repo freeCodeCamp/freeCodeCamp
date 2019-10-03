77c35d3f3a4ff3fe42dc463c264bcc1ad84d3304
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-10-03 12:48:50 +0300

    feat: re-add toggle menu

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,17 +5,56 @@ import UniversalNav from './components/UniversalNav';
 
 import './header.css';
 
-export function Header() {
-  return (
-    <>
-      <Helmet>
-        <style>{':root{--header-height: 38px}'}</style>
-      </Helmet>
-      <header>
-        <UniversalNav />
-      </header>
-    </>
-  );
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayMenu: false
+    };
+    this.menuButtonRef = React.createRef();
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.toggleDisplayMenu = this.toggleDisplayMenu.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside);
+  }
+
+  handleClickOutside(event) {
+    if (
+      this.state.displayMenu &&
+      this.menuButtonRef.current &&
+      !this.menuButtonRef.current.contains(event.target) &&
+      event.target.id !== 'fcc_instantsearch'
+    ) {
+      this.toggleDisplayMenu();
+    }
+  }
+
+  toggleDisplayMenu() {
+    this.setState(({ displayMenu }) => ({ displayMenu: !displayMenu }));
+  }
+  render() {
+    const { displayMenu } = this.state;
+    return (
+      <>
+        <Helmet>
+          <style>{':root{--header-height: 38px}'}</style>
+        </Helmet>
+        <header>
+          <UniversalNav
+            displayMenu={displayMenu}
+            ref={this.menuButtonRef}
+            toggleDisplayMenu={this.toggleDisplayMenu}
+          />
+        </header>
+      </>
+    );
+  }
 }
 
 Header.displayName = 'Header';
