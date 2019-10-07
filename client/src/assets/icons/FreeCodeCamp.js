a73802a066e5909b778184bdaafa3ceac6eba584
Sanskar Gupta <Sanskar95@users.noreply.github.com>
2019-10-07 14:26:14 +0530

    lint fix

--- client/src/assets/icons/FreeCodeCamp.js
+++ client/src/assets/icons/FreeCodeCamp.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 
 function FreeCodeCamp(props) {
+  const altText="freeCodeCamp.org";
   return (
     <Fragment>
       <svg
-        xmlns:xlink='https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg'>
-        {...props}
+        xmlns:xlink="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg">
       >
-      <title>freeCodeCamp.org</title>
+      <title>{altText}</title>
       </svg>
     </Fragment>
   );
