f8b545d26a551b48e21044ce6e58752badc2dbe7
Tse, Christopher <ctse@netsuite.com>
2019-10-23 21:05:13 -0500

    fix: set access control to * during preflight request

--- api-server/server/middlewares/constant-headers.js
+++ api-server/server/middlewares/constant-headers.js
@@ -2,7 +2,11 @@ import { homeLocation } from '../../../config/env';
 
 export default function constantHeaders() {
   return function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', homeLocation);
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', '*');
+    } else {
+      res.header('Access-Control-Allow-Origin', homeLocation);
+    }
     res.header('Access-Control-Allow-Credentials', true);
     res.header(
       'Access-Control-Allow-Headers',
