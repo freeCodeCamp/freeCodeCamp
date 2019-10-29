64bcbd32fe70062353e8502fb292e35170d87567
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-10-29 17:33:28 +0530

    check against correct variable

--- client/src/components/settings/Username.js
+++ client/src/components/settings/Username.js
@@ -63,7 +63,7 @@ class UsernameSettings extends Component {
       submitClicked: false,
       // eslint-disable-next-line max-len
       isUserNew: /^fcc[0-9|a-f]{8}-[0-9|a-f]{4}-[0-9|a-f]{4}-[0-9|a-f]{4}-[0-9|a-f]{12}$/.test(
-        prevUsername
+        props.username
       )
     };
 
@@ -84,7 +84,7 @@ class UsernameSettings extends Component {
         submitClicked: false,
         // eslint-disable-next-line max-len
         isUserNew: /^fcc[0-9|a-f]{8}-[0-9|a-f]{4}-[0-9|a-f]{4}-[0-9|a-f]{4}-[0-9|a-f]{12}$/.test(
-          prevUsername
+          formValue
         )
       });
     }
