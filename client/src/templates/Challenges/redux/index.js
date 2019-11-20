9cf56b992289773a3372728398ff4071adb5d84c
Tom <20648924+moT01@users.noreply.github.com>
2019-11-20 10:41:47 -0600

    Update client/src/templates/Challenges/redux/index.js
    Co-Authored-By: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/templates/Challenges/redux/index.js
+++ client/src/templates/Challenges/redux/index.js
@@ -160,7 +160,6 @@ export const challengeTestsSelector = state => state[ns].challengeTests;
 export const consoleOutputSelector = state => state[ns].consoleOut;
 export const completedChallengesIds = state =>
   completedChallengesSelector(state).map(node => node.id);
-  const completedChallenges = completedChallengesSelector(state);
   const completedChallengesIds = completedChallenges.map(node => node.id);
   return completedChallengesIds;
 };
