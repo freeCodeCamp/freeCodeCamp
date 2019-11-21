3fff299b2a5410de1a4bdcb86fe68ec721270c9d
Mrugesh Mohapatra <me@raisedadead.com>
2019-11-21 21:50:12 +0530

    fix(client,server): usernames should not be a http error code

--- utils/validate.js
+++ utils/validate.js
@@ -1,13 +1,31 @@
-const validCharsRE = /^[a-zA-Z0-9\-_+]+$/;
 const invalidCharError = {
   valid: false,
-  error: 'contains invalid characters'
+  error: 'contains invalid characters.'
 };
 const validationSuccess = { valid: true, error: null };
-const usernameTooShort = { valid: false, error: 'is too short' };
+const usernameTooShort = { valid: false, error: 'is too short.' };
+const usernameIsHttpStatusCode = {
+  valid: false,
+  error: 'is a reserved error code.'
+};
 
-exports.validate = str => {
+const isNumeric = num => !isNaN(num);
+const validCharsRE = /^[a-zA-Z0-9\-_+]+$/;
+const isHttpStatusCode = str =>
+  isNumeric(str) && (parseInt(str, 10) >= 100 && parseInt(str, 10) <= 599);
+const isValidUsername = str => {
   if (str.length < 3) return usernameTooShort;
+  if (isHttpStatusCode(str)) return usernameIsHttpStatusCode;
   if (!validCharsRE.test(str)) return invalidCharError;
   return validationSuccess;
+};
+
+module.exports = {
+  isNumeric,
+  isHttpStatusCode,
+  isValidUsername,
+  validationSuccess,
+  usernameTooShort,
+  usernameIsHttpStatusCode,
+  invalidCharError
 };
