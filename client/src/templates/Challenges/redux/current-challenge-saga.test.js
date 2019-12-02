a4596b9e7dc5fb969b0849d387b6d4d371036228
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-12-02 14:20:49 +0300

    Update client/src/templates/Challenges/redux/current-challenge-saga.test.js
    Co-Authored-By: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/templates/Challenges/redux/current-challenge-saga.test.js
+++ client/src/templates/Challenges/redux/current-challenge-saga.test.js
@@ -3,7 +3,7 @@ import { canRequestDonationSaga } from './current-challenge-saga';
 import { types as appTypes } from '../../../redux';
 
 describe('canRequestDonationSaga', () => {
-  it('should call canRequestDonation', () => {
+  it('should call allowDonationRequests', () => {
     const gen = allowDonationRequestsSaga();
     expect(gen.next().value.payload.action.type).toEqual(
       appTypes.canRequestDonation
