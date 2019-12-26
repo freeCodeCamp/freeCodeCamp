91308e01b36dd0ee7fc30b6fc6f233ca7ad5e734
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-12-26 20:25:19 +0300

    feat: add GA to year end gift page

--- client/src/pages/year-end-gift.js
+++ client/src/pages/year-end-gift.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Grid } from '@freecodecamp/react-bootstrap';
+import ga from '../analytics';
 
 import { Spacer, FullWidthRow } from '../components/helpers';
 import YearEndDonationForm from '../components/YearEndGift/YearEndDonationForm';
 
 function YearEndGiftPage() {
+  const handleProcessing = (duration, amount) => {
+    ga.event({
+      category: 'donation',
+      action: 'year-end-gift strip form submission',
+      label: duration,
+      value: amount
+    });
+  };
   return (
     <>
       <Helmet title='Support our nonprofit | freeCodeCamp.org' />
@@ -13,7 +22,10 @@ function YearEndGiftPage() {
         <main>
           <Spacer />
           <FullWidthRow>
-            <YearEndDonationForm defaultTheme='light' />
+            <YearEndDonationForm
+              defaultTheme='light'
+              handleProcessing={handleProcessing}
+            />
           </FullWidthRow>
           <Spacer />
           <Spacer />
