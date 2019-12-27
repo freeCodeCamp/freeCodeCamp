b50a25845ab4612483ece53a80250e9ad8231a8d
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-12-27 13:30:47 +0300

    feat: adjust tests

--- client/src/components/Map/components/Block.test.js
+++ client/src/components/Map/components/Block.test.js
@@ -44,6 +44,7 @@ test('<Block expanded snapshot', () => {
 test('<Block />  should handle toggle clicks correctly', async () => {
   const toggleSpy = jest.fn();
   const toggleMapSpy = jest.fn();
+  const reportGaEvent = jest.fn();
 
   const props = {
     blockDashedName: 'block-a',
@@ -51,6 +52,7 @@ test('<Block />  should handle toggle clicks correctly', async () => {
     completedChallenges: mockCompleted,
     intro: mockIntroNodes[0],
     isExpanded: false,
+    reportGaEvent: reportGaEvent,
     toggleBlock: toggleSpy,
     toggleMapModal: toggleMapSpy
   };
