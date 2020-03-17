a24c470d2bf3079f2f4f7011fb87109a5606e8a9
Jacob Robinson <39199632+jakethecoder95@users.noreply.github.com>
2020-03-16 22:37:42 -0700

    feat(client): add space for flash message (#38327)

--- client/src/components/Flash/index.js
+++ client/src/components/Flash/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Alert } from '@freecodecamp/react-bootstrap';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
@@ -7,14 +7,44 @@ import './flash.css';
 
 function Flash({ flashMessage, onClose }) {
   const { type, message, id } = flashMessage;
+  const [flashMessageHeight, setFlashMessageHeight] = useState(null);
+
+  useEffect(() => {
+    setFlashMessageHeight(
+      document.querySelector('.flash-message').offsetHeight
+    );
+    document.documentElement.style.setProperty(
+      '--flash-message-height',
+      flashMessageHeight + 'px'
+    );
+  }, [flashMessageHeight]);
+
+  function handleClose() {
+    document.documentElement.style.setProperty('--flash-message-height', '0px');
+    onClose();
+  }
+
   return (
-    <TransitionGroup>
-      <CSSTransition classNames='flash-message' key={id} timeout={500}>
-        <Alert bsStyle={type} className='flash-message' onDismiss={onClose}>
-          {message}
-        </Alert>
-      </CSSTransition>
-    </TransitionGroup>
+    <>
+      <TransitionGroup>
+        <CSSTransition classNames='flash-message' key={id} timeout={500}>
+          <Alert
+            bsStyle={type}
+            className='flash-message'
+            onDismiss={handleClose}
+          >
+            {message}
+          </Alert>
+        </CSSTransition>
+      </TransitionGroup>
+      {flashMessage && (
+        <div
+          style={{
+            height: flashMessageHeight + 'px'
+          }}
+        ></div>
+      )}
+    </>
   );
 }
 
