49fdade90bde39b4e06a5930b8c53855ebbacbac
Mrugesh Mohapatra <me@raisedadead.com>
2020-03-19 18:48:58 +0530

    feat: add paypal donation to modals and certificates

--- client/src/components/Donation/MinimalDonateForm.js
+++ client/src/components/Donation/MinimalDonateForm.js
@@ -130,17 +130,18 @@ class MinimalDonateForm extends Component {
     return (
       <Row>
         <Col lg={8} lgOffset={2} sm={10} smOffset={1} xs={12}>
+          <Spacer />
+          <b>Confirm your donation of $60 / year with PayPal:</b>
+          <Spacer />
           <PaypalButton
             handleProcessing={handleProcessing}
             onDonationStateChange={this.onDonationStateChange}
           />
         </Col>
-        <Col sm={10} smOffset={1} xs={12}>
+        <Col lg={8} lgOffset={2} sm={10} smOffset={1} xs={12}>
           <Spacer />
           <b>Or donate with a credit card:</b>
           <Spacer />
-        </Col>
-        <Col lg={8} lgOffset={2} sm={10} smOffset={1} xs={12}>
           <StripeProvider stripe={stripe}>
             <Elements>
               <DonateFormChildViewForHOC
@@ -148,7 +149,7 @@ class MinimalDonateForm extends Component {
                 donationAmount={donationAmount}
                 donationDuration={donationDuration}
                 getDonationButtonLabel={() =>
-                  `Confirm your donation of $5 / month`
+                  `Confirm your donation of $60 / year`
                 }
                 handleProcessing={handleProcessing}
               />
