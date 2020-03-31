33036fb263d47cddcab1801723fceb59d1a2cf0c
Tom <20648924+moT01@users.noreply.github.com>
2020-03-31 15:34:54 -0500

    feat: paginate heatmap + calculate streaks on client (#38318)
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/jest.test.js
+++
@@ -1,6 +0,0 @@
-/* global expect */
-describe('Timezones', () => {
-  it('should always be UTC', () => {
-    expect(new Date().getTimezoneOffset()).toBe(0);
-  });
-});
--- client/src/components/profile/components/HeatMap.test.js
+++ client/src/components/profile/components/HeatMap.test.js
@@ -6,23 +6,27 @@ import { render } from '@testing-library/react';
 
 import HeatMap from './HeatMap';
 
+// offset is used to shift the dates so that the calendar renders (for testing
+// purposes only) the same way in each timezone.
+const offset = new Date().getTimezoneOffset() * 60;
+const date1 = 1580497504 + offset;
+const date2 = 1580597504 + offset;
+const date3 = 1580729769 + offset;
+
 const props = {
-  calendar: {
-    1580393017: 1,
-    1580397504: 1
-  },
-  streak: {
-    current: 2,
-    longest: 2
-  }
+  calendar: {}
 };
+
+props.calendar[date1] = 1;
+props.calendar[date2] = 1;
+props.calendar[date3] = 1;
 
 let dateNowMockFn;
 
 beforeEach(() => {
   dateNowMockFn = jest
     .spyOn(Date, 'now')
-    .mockImplementation(() => 1580729769714);
+    .mockImplementation(() => 1580729769714 + offset * 1000);
 });
 
 afterEach(() => {
@@ -33,5 +37,19 @@ describe('<HeatMap/>', () => {
   it('renders correctly', () => {
     const { container } = render(<HeatMap {...props} />);
     expect(container).toMatchSnapshot();
+  });
+
+  it('calculates the correct longest streak', () => {
+    const { getByTestId } = render(<HeatMap {...props} />);
+    expect(getByTestId('longest-streak').textContent).toContain(
+      'Longest Streak: 2'
+    );
+  });
+
+  it('calculates the correct current streak', () => {
+    const { getByTestId } = render(<HeatMap {...props} />);
+    expect(getByTestId('current-streak').textContent).toContain(
+      'Current Streak: 1'
+    );
   });
 });
