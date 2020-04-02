00c475f663d21cb5f245a9a4b5d6bf8a33ccee75
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2020-04-02 17:26:21 +0530

    chore: update meta and description (#38419)

--- client/src/components/landing/index.js
+++ client/src/components/landing/index.js
@@ -28,7 +28,7 @@ export const Landing = ({ edges }) => {
   return (
     <Fragment>
       <Helmet>
-        <title>Learn to code | freeCodeCamp.org</title>
+        <title>Learn to code at home | freeCodeCamp.org</title>
       </Helmet>
       <main className='landing-page'>
         <Spacer />
@@ -39,7 +39,9 @@ export const Landing = ({ edges }) => {
                 Welcome to freeCodeCamp.org
               </h1>
               <Spacer />
-              <h2 className='medium-heading text-center'>Learn to code.</h2>
+              <h2 className='medium-heading text-center'>
+                Learn to code at home.
+              </h2>
               <h2 className='medium-heading text-center'>Build projects.</h2>
               <h2 className='medium-heading text-center'>
                 Earn certifications.
