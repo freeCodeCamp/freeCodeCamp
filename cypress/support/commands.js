92ffa1a92ed889cd3068fe22fda39bb9352d50a6
MarceloDamian <60354250+MarceloDamian@users.noreply.github.com>
2020-05-04 04:05:06 -0400

    fixed comment error (#38724)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,5 +29,5 @@
 // );
 //
 //
-// -- This is will overwrite an existing command --
+// -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => {});
