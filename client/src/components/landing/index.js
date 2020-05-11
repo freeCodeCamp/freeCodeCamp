9a25ba2633794b64d62d1d4cdd52696b643a6d27
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2020-05-11 20:26:12 +0700

    fix(learn): improve landing page (#38695)

--- client/src/components/landing/index.js
+++ client/src/components/landing/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+
 import { Grid, Row, Col } from '@freecodecamp/react-bootstrap';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
@@ -6,6 +7,8 @@ import { Link } from 'gatsby';
 import { uniq } from 'lodash';
 import { Spacer } from '../helpers';
 import Login from '../Header/components/Login';
+import CompanyLogos from './components/CompanyLogos';
+import { AsFeatureLogo } from '../../assets/images';
 
 import './landing.css';
 import '../Map/map.css';
@@ -16,10 +19,21 @@ const propTypes = {
 
 const BigCallToAction = () => (
   <Row>
-    <Col md={6} mdOffset={3} sm={10} smOffset={1} xs={12}>
+    <Col md={6} mdOffset={3} sm={8} smOffset={2} xs={12}>
       <Login block={true} data-test-label='landing-big-cta'>
-        Sign in and get started (it's free)
+        Get started (it's free)
       </Login>
+    </Col>
+  </Row>
+);
+
+const AsFeaturedSection = () => (
+  <Row>
+    <Col sm={8} smOffset={2} xs={12}>
+      <div className='text-center'>
+        <h2 className='medium-heading'>As Featured In:</h2>
+        <AsFeatureLogo />
+      </div>
     </Col>
   </Row>
 );
@@ -55,18 +69,13 @@ export const Landing = ({ edges }) => {
                 Since 2014, more than 40,000 freeCodeCamp.org graduates have
                 gotten jobs at tech companies including:
               </h2>
-              <div className='logo-row'>
-                <h2 className='medium-heading'>Apple</h2>
-                <h2 className='medium-heading'>Google</h2>
-                <h2 className='medium-heading'>Amazon</h2>
-                <h2 className='medium-heading'>Microsoft</h2>
-                <h2 className='medium-heading'>Spotify</h2>
-              </div>
+              <CompanyLogos />
             </Col>
           </Row>
           <Spacer />
           <BigCallToAction />
           <Spacer />
+          <AsFeaturedSection />
           <Row>
             <Col sm={10} smOffset={1} xs={12}>
               <h2 className='medium-heading'>Certifications:</h2>
