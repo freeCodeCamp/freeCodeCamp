27456c1d6dfdedd378c9f25a6ba5feb9809e55ef
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-06-01 18:28:22 +0200

    feat: add simple challengeFile sorter

---
+++ client/src/templates/Challenges/utils/__fixtures__/challenges.js
@@ -0,0 +1,38 @@
+export const challengeFiles = {
+  indexcss: {
+    contents: 'some css',
+    error: null,
+    ext: 'css',
+    head: '',
+    history: ['index.css'],
+    key: 'indexcss',
+    name: 'index',
+    path: 'index.css',
+    seed: 'some css',
+    tail: ''
+  },
+  indexhtml: {
+    contents: 'some html',
+    error: null,
+    ext: 'html',
+    head: '',
+    history: ['index.html'],
+    key: 'indexhtml',
+    name: 'index',
+    path: 'index.html',
+    seed: 'some html',
+    tail: ''
+  },
+  indexjs: {
+    contents: 'some js',
+    error: null,
+    ext: 'js',
+    head: '',
+    history: ['index.js'],
+    key: 'indexjs',
+    name: 'index',
+    path: 'index.js',
+    seed: 'some js',
+    tail: ''
+  }
+};
