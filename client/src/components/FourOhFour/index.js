cf2d568f3f878fafede9319f97f8688a869a46cb
rdm100 <robertode_marco@hotmail.com>
2020-06-11 13:44:59 +0100

    Changed path to words.json for C:\Users\1\Desktop\freeCodeCamp\client\src\components\FourOhFour\index.js

--- client/src/components/FourOhFour/index.js
+++ client/src/components/FourOhFour/index.js
@@ -4,7 +4,7 @@ import Spinner from 'react-spinkit';
 import { Link } from 'gatsby';
 
 import notFoundLogo from '../../assets/images/freeCodeCamp-404.svg';
-import words from '../../../../../utils/words.json';
+import words from '../../../../utils/words.json';
 hello
 import './404.css';
 
