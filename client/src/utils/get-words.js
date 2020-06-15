885538d9e113e0e7f689ca825693f7d921ffd0fa
rdm100 <robertode_marco@hotmail.com>
2020-06-15 14:12:27 +0100

    fix: updated and consolidated quotes (#39069)
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/utils/get-words.js
+++ client/src/utils/get-words.js
@@ -1,4 +1,4 @@
-import words from './words.json';
+import words from '../../../config/motivational-quotes.json';
 
 function randomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -6,14 +6,6 @@ function randomItem(arr) {
 
 export function randomQuote() {
   return randomItem(words.motivationalQuotes);
-}
-
-export function randomPhrase() {
-  return randomItem(words.phrases);
-}
-
-export function randomVerb() {
-  return randomItem(words.verbs);
 }
 
 export function randomCompliment() {
