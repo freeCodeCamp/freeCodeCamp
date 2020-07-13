5ea35f2e9f954536319e285e6721a5278b960faa
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-07-13 18:59:40 +0200

    fix: unify single and multifile testing

--- tools/challenge-md-parser/solution-to-data.js
+++ tools/challenge-md-parser/solution-to-data.js
@@ -2,23 +2,27 @@ const visit = require('unist-util-visit');
 const { selectAll } = require('hast-util-select');
 
 const { sectionFilter } = require('./utils');
-const { createCodeGetter } = require('./challengeSeed-to-data');
+const { createCodeGetter, defaultFile } = require('./challengeSeed-to-data');
+const { isEmpty } = require('lodash');
 
 const solutionRE = /(.+)-solution$/;
+
+function indexByKey(obj) {
+  return { [obj.key]: { ...obj } };
+}
 
 function createPlugin() {
   return function transformer(tree, file) {
     function visitor(node) {
       if (sectionFilter(node, 'solution')) {
         // fallback for single-file challenges
-        const solutions = selectAll('code', node).map(
-          element => element.children[0].value
-        );
-        file.data = {
-          ...file.data,
-          solutions
-        };
+        const rawSolutions = selectAll('code', node).map(element => ({
+          lang: element.properties.className[0].split('-')[1],
+          contents: element.children[0].value
+        }));
+
         const solutionFiles = {};
+
         const codeDivs = selectAll('div', node);
         const solutionContainers = codeDivs.filter(({ properties: { id } }) =>
           solutionRE.test(id)
@@ -27,11 +31,20 @@ function createPlugin() {
           createCodeGetter('contents', solutionRE, solutionFiles)
         );
 
+        const solutionsAsFiles = rawSolutions
+          .map(({ lang, contents }) => ({
+            ...defaultFile(lang),
+            contents
+          }))
+          .map(indexByKey);
+
+        const solutions = isEmpty(solutionFiles)
+          ? solutionsAsFiles
+          : [solutionFiles];
+
         file.data = {
           ...file.data,
-          solutionFiles: Object.keys(solutionFiles).map(
-            lang => solutionFiles[lang]
-          )
+          solutions
         };
       }
     }
