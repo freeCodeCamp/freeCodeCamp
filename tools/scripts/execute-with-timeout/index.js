5d075853842c93df1de9a6376f459fc025eccbd7
Twaha Rahman <mahi6703890@gmail.com>
2020-08-27 19:46:27 +0600

    Update execute-with-timeout script

--- tools/scripts/execute-with-timeout/index.js
+++ tools/scripts/execute-with-timeout/index.js
@@ -1,15 +1,45 @@
+const fs = require('fs');
 const execa = require('execa');
 
+const cliArgs = process.argv;
+
+const scriptToRun = cliArgs[2].replace(/-/g, '');
+
+const timeoutArg = cliArgs[3].replace(/-/g, '');
+
+const timeout = Number(timeoutArg) * 60 * 1000;
+
 (async () => {
-  // const subprocess = await execa('npm', ['run', 'develop']);
+  try {
+    const subprocess = execa('npm', ['run', scriptToRun]);
+    subprocess.stdout.pipe(process.stdout);
 
-  const subprocess = execa('npm', ['run', 'develop']);
+    const writableStream = fs.createWriteStream(
+      `${scriptToRun}-script-log.txt`
+    );
 
-  setTimeout(() => {
-    subprocess.kill('SIGTERM', {
-      forceKillAfterTimeout: 2000
-    });
-  }, 1000);
+    subprocess.stdout.pipe(writableStream);
 
-  subprocess.stdout.pipe(process.stdout);
+    setTimeout(() => {
+      fs.readFile(`${scriptToRun}-script-log.txt`, 'utf8', (err, data) => {
+        if (err) throw new Error('failed to read data');
+
+        const regexChecheckFirst = RegExp(
+          'You can now view @freecodecamp/client in the browser.'
+        );
+
+        const regexChecheckSecond = RegExp('http://localhost:8000/___graphql');
+
+        if (regexChecheckFirst.test(data) || regexChecheckSecond.test(data)) {
+          // eslint-disable-next-line
+          return process.exit(0);
+        } else {
+          // eslint-disable-next-line
+          return process.exit(1);
+        }
+      });
+    }, timeout);
+  } catch (error) {
+    process.exitCode(1);
+  }
 })();
