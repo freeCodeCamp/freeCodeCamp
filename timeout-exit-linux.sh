46b9882fc289fcb7747091d18328982b08b3ef04
Twaha Rahman <mahi6703890@gmail.com>
2020-08-28 22:12:15 +0600

    Test build script

--- timeout-exit-linux.sh
+++ timeout-exit-linux.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-timeout -k 6m 5m npm run develop
+timeout -k 8m 7m npm run develop
+
+case $? in
+
+  124 | 137)
+    exit 0
+    ;;
+
+  *)
+    exit 1
+    ;;
+esac
+
+timeout -k 11m 10m npm run build
 
 case $? in
 
--- timeout-exit-macos.sh
+++ timeout-exit-macos.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-gtimeout -k 6m 5m npm run develop
+gtimeout -k 8m 7m npm run develop
+
+case $? in
+
+  124 | 137)
+    exit 0
+    ;;
+
+  *)
+    exit 1
+    ;;
+esac
+
+gtimeout -k 11m 10m npm run build
 
 case $? in
 
