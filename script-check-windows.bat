021980e05c566c6de23dc69a16a06c4f9568d4f7
Twaha Rahman <mahi6703890@gmail.com>
2020-08-29 15:44:29 +0600

    Change exit code

--- script-check-windows.bat
+++ script-check-windows.bat
@@ -2,6 +2,6 @@
 
 echo "start timeout 7m npm run develop"
 
-if %errorlevel%==124 (echo "npm run develop runs successfully") else (EXIT /B 1)
+if %errorlevel%==124 (echo "npm run develop runs successfully") else (EXIT /B 123)
 
 echo "start npm run build"
