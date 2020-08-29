cb32249e5f3b3787254e93f5548c6b609870c2f7
Twaha Rahman <mahi6703890@gmail.com>
2020-08-29 15:28:46 +0600

    Test error code

--- script-check-windows.bat
+++ script-check-windows.bat
@@ -2,6 +2,6 @@
 
 start timeout 7m npm run develop
 
-if %errorlevel%==124 (echo "npm run develop runs successfully") else (EXIT /B 1)
+if %errorlevel%==124 (echo "npm run develop runs successfully") else (EXIT /B 123)
 
 start npm run build
