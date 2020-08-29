ae78bb3aecb1c8509f5279b2cf11680219278c39
Twaha Rahman <mahi6703890@gmail.com>
2020-08-29 16:05:21 +0600

    Debug batch file

--- script-check-windows.bat
+++ script-check-windows.bat
@@ -1,9 +1,3 @@
 @ECHO OFF
 
-echo "start timeout 7m npm run develop"
-
-if errorlevel 124 (
-   echo "npm run develop runs successfully"
-) else (EXIT /B 123)
-
-start npm run build
+start timeout 7m npm run develop
