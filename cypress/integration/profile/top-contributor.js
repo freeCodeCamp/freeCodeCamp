c4ecc5c8eb85b4afaf202852a95ee2d6de892d24
Twaha Rahman <mahi6703890@gmail.com>
2020-09-26 18:21:20 +0600

    Initial test

---
+++ cypress/integration/profile/top-contributor.js
@@ -0,0 +1,31 @@
+/* global cy */
+
+describe('Top contributor in user profile', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.contains('Profile').click();
+    cy.contains('Preview custom 404 page').click();
+  });
+
+  it('Should show `Top Contributor` text with badge', () => {
+    cy.contains('Top Contributor')
+      .parent()
+      .within(() => {
+        cy.contains('Top Contributor').should('be.visible');
+        cy.get('svg').should('be.visible');
+      });
+  });
+
+  // eslint-disable-next-line max-len
+  it('Should take user to `Top Contributor` page when `Top Contributor` gets clicked', () => {
+    cy.contains('Top Contributor').should(
+      'have.attr',
+      'href',
+      '/top-contributors'
+    );
+  });
+
+  it('Should show years when it was achieved', () => {
+    cy.contains('2017, 2018 and 2019').should('be.visible');
+  });
+});
