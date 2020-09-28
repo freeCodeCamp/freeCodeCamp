1da92cbc9ccd30aa0344cd5a06a47a24f813dee5
Twaha Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2020-09-28 17:35:35 +0600

    Add tests for top contributor badge

---
+++ cypress/integration/top-contributor.js
@@ -0,0 +1,33 @@
+/* global cy */
+
+describe('Top contributor in user profile', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.contains('Profile').click();
+
+    // The following line is only required if you want to test it in development
+    // cy.contains('Preview custom 404 page').click();
+  });
+
+  it('Should show `Top Contributor` text with badge', () => {
+    cy.contains('Top Contributor')
+      .parent()
+      .within(() => {
+        cy.contains('Top Contributor').should('be.visible');
+        cy.get('svg').should('be.visible');
+      });
+  });
+
+  // eslint-disable-next-line max-len
+  it('Should take user to `Top Contributor` page when `Top Contributor` gets clicked', () => {
+    cy.contains('Top Contributor').should(
+      'have.attr',
+      'href',
+      '/top-contributors'
+    );
+  });
+
+  it('Should show years when it was achieved', () => {
+    cy.contains('2017, 2018 and 2019').should('be.visible');
+  });
+});
