7874baaeb75bf11849323cb641839589a56b8e4c
Twaha Rahman <mahi6703890@gmail.com>
2020-09-28 18:54:30 +0600

    Change navbar test to check for green border

--- cypress/integration/learn/common-components/navbar.js
+++ cypress/integration/learn/common-components/navbar.js
@@ -87,7 +87,7 @@ describe('Navbar', () => {
   it('Should have a profile image with class `default-border`', () => {
     cy.login()
       .get(selectors.avatarContainer)
-      .should('have.class', 'default-border');
+      .should('have.class', 'green-border');
     cy.get(selectors.defaultAvatar).should('exist');
   });
 });
