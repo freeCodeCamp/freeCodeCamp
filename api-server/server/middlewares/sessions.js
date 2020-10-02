0555482449079d927832377206f714f291eb59f5
matvii <matvii@hodovani.uk>
2020-10-02 18:23:02 +0300

    Add secure attribute to cookie
    Cookie [:11] misses the Secure attribute (it is false by default). Set it to true to protect the cookie from man-in-the-middle attacks.

--- api-server/server/middlewares/sessions.js
+++ api-server/server/middlewares/sessions.js
@@ -8,7 +8,10 @@ const url = process.env.MONGODB || process.env.MONGOHQ_URL;
 export default function sessionsMiddleware() {
   return session({
     // 900 day session cookie
-    cookie: { maxAge: 900 * 24 * 60 * 60 * 1000 },
+    cookie: { 
+      maxAge: 900 * 24 * 60 * 60 * 1000,
+      secure: true
+    },
     // resave forces session to be resaved
     // regardless of whether it was modified
     // this causes race conditions during parallel req
