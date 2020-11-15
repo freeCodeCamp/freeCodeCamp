0865ea8709494ae89842ebb0bd2aa94a0676baf9
Nullaferius <Cafercotel@gmail.com>
2020-11-15 19:28:57 +0300

    Update client/src/components/OfflineWarning/OfflineWarning.js
    Co-authored-by: Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>

--- client/src/components/OfflineWarning/OfflineWarning.js
+++ client/src/components/OfflineWarning/OfflineWarning.js
@@ -25,7 +25,7 @@ function OfflineWarning({ isOnline, isSignedIn }) {
   }
 
   return showWarning ? (
-    <div className='offline-warning'>
+    <div className='offline-warning alert-info'>
       <div className='alert-info'>
         You appear to be offline, your progress may not be being saved.
       </div>
