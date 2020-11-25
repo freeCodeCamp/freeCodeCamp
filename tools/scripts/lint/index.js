5e06e7211645034aed99f690184f7a66abadceb9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-11-25 19:25:42 +0100

    fix: update linter for new challenge format
    Also uses yaml for better vscode integration

--- tools/scripts/lint/index.js
+++ tools/scripts/lint/index.js
@@ -1,10 +1,18 @@
-const lintRules = require('./.markdownlintrc');
-const linter = require('./linter');
+const YAML = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
 const argv = require('yargs').argv;
+const linter = require('./linter');
 
+const CONFIG_PATH = path.resolve(
+  __dirname,
+  '../../../curriculum/challenges/.markdownlint.yaml'
+);
 const isMDRE = /.*\.md$/;
 
-const lint = linter(lintRules);
+const lintRules = fs.readFileSync(CONFIG_PATH, 'utf8');
+
+const lint = linter(YAML.load(lintRules));
 
 const files = argv._.filter(arg => isMDRE.test(arg));
 files.forEach(path => lint({ path: path }));
