a0c6ca0d3bb01818dc8d4525820960615b981b5f
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-11-27 21:11:15 +0000

    feat(dev): update GitPod testing

--- api-server/server/utils/getSetAccessToken.test.js
+++ api-server/server/utils/getSetAccessToken.test.js
@@ -134,7 +134,7 @@ describe('getSetAccessToken', () => {
         expectedJWT,
         {
           signed: false,
-          domain: 'localhost',
+          domain: process.env.COOKIE_DOMAIN || 'localhost',
           maxAge: accessToken.ttl
         }
       ]);
@@ -147,37 +147,20 @@ describe('getSetAccessToken', () => {
       // expect.assertions(4);
       const req = mockReq();
       const res = mockRes();
+      const jwtOptions = { signed: false, domain: process.env.COOKIE_DOMAIN };
 
       removeCookies(req, res);
 
       expect(res.clearCookie.getCall(0).args).toEqual([
         'jwt_access_token',
-        {
-          signed: false,
-          domain: 'localhost'
-        }
+        jwtOptions
       ]);
       expect(res.clearCookie.getCall(1).args).toEqual([
         'access_token',
-        {
-          signed: false,
-          domain: 'localhost'
-        }
+        jwtOptions
       ]);
-      expect(res.clearCookie.getCall(2).args).toEqual([
-        'userId',
-        {
-          signed: false,
-          domain: 'localhost'
-        }
-      ]);
-      expect(res.clearCookie.getCall(3).args).toEqual([
-        '_csrf',
-        {
-          signed: false,
-          domain: 'localhost'
-        }
-      ]);
+      expect(res.clearCookie.getCall(2).args).toEqual(['userId', jwtOptions]);
+      expect(res.clearCookie.getCall(3).args).toEqual(['_csrf', jwtOptions]);
     });
   });
 });
