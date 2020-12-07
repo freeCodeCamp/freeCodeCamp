c90aa3c9381cb5de9042bbc4899b4ef3c7a0b82a
moT01 <20648924+moT01@users.noreply.github.com>
2020-12-07 11:21:04 -0600

    fix: filename to uppercase

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import Link from '../helpers/Link';
-
 import LanguageMenu from './LanguageMenu';
 import './footer.css';
 
