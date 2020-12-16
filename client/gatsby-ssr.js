510065b4fab211d39a3cb54639f394a721018965
Tom <20648924+moT01@users.noreply.github.com>
2020-12-16 12:39:52 -0600

    fix(client): translation hydration (#40469)
    * fix: translation hydration

--- client/gatsby-ssr.js
+++ client/gatsby-ssr.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 
+import i18n from './i18n/config';
 import headComponents from './src/head';
 import { createStore } from './src/redux/createStore';
 
@@ -10,7 +12,11 @@ import layoutSelector from './utils/gatsby/layoutSelector';
 const store = createStore();
 
 export const wrapRootElement = ({ element }) => {
-  return <Provider store={store}>{element}</Provider>;
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>{element}</I18nextProvider>
+    </Provider>
+  );
 };
 
 wrapRootElement.propTypes = {
