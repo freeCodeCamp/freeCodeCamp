203442cf2cd6d2db19ad86d20f997d51fd5178c2
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2020-12-22 10:22:12 +0300

    feat: make the component declerative

--- client/src/components/Donation/DonationOptionsText.js
+++ client/src/components/Donation/DonationOptionsText.js
@@ -4,27 +4,31 @@ import PropTypes from 'prop-types';
 const DonationOptionsText = ({ isAlert }) => {
   const Title =
     'Want to make a bigger one-time donation, mail us a check, or give in other ways?';
+  const ArticleLink = (
+    <a
+      href={'https://www.freecodecamp.org/news/how-to-donate-to-free-code-camp'}
+    >
+      other ways we could support our non-profit's mission
+    </a>
+  );
 
-  return (
+  const AlertDonationOptionsText = (
+    <p className='donation-options'>
+      {Title} Here are many {ArticleLink}.
+    </p>
+  );
+
+  const DefaultDonationOptionsText = (
     <>
-      {!isAlert && (
-        <h4>
-          <b>{Title}</b>
-        </h4>
-      )}
-      <p className='donation-options'>
-        {isAlert && Title} Here are many{' '}
-        <a
-          href={
-            'https://www.freecodecamp.org/news/how-to-donate-to-free-code-camp'
-          }
-        >
-          other ways we could support our non-profit's mission
-        </a>
-        .
-      </p>
+      <h4>
+        <b>{Title}</b>
+      </h4>
+      <p className='donation-options'>Here are many {ArticleLink}.</p>
     </>
   );
+
+  if (isAlert) return AlertDonationOptionsText;
+  else return DefaultDonationOptionsText;
 };
 
 DonationOptionsText.propTypes = {
