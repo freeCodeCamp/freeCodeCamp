09232a38fa1648bc6712fbab7e76a1860b8aadd6
miyaliu666 <miyaliu66666@gmail.com>
2020-12-28 20:48:08 +0800

    fix(i18n): translated comments to Chinese (#40526)
    Co-authored-by: Randell Dawson <5313213+RandellDawson@users.noreply.github.com>
    Co-authored-by: Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>

--- curriculum/dictionaries/chinese/comments.js
+++ curriculum/dictionaries/chinese/comments.js
@@ -6,435 +6,438 @@
 const TRANSLATIONS = [
   {
     id: 'hyek8f',
-    text: 'comment placeholder (needs translation)'
+    text: '24.44 摄氏度'
   },
   {
     id: 'rscjup',
-    text: 'comment placeholder (needs translation)'
+    text: '26 摄氏度'
   },
   {
     id: 'am2xch',
-    text: 'comment placeholder (needs translation)'
+    text: '一个展示需要完成的任务的字符串列表：'
   },
   {
     id: '6rztdg',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里添加一条记录'
   },
   {
     id: 'to1vwe',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里添加 handleChange() 和 submitMessage() 方法'
   },
   {
     id: '31b7ey',
-    text: 'comment placeholder (needs translation)'
+    text: '在这行上面添加代码'
   },
   {
     id: 'c24by8',
-    text: 'comment placeholder (needs translation)'
+    text: '在这行下面添加代码'
   },
   {
     id: 'jbrt8k',
-    text: 'comment placeholder (needs translation)'
+    text: '添加第 m 行到 newArray'
   },
   {
     id: 'zkh12d',
-    text: 'comment placeholder (needs translation)'
+    text: '使用 myString 通过挑战后，了解分组如何运行'
   },
   {
     id: 'mobihi',
-    text: 'comment placeholder (needs translation)'
+    text: '给 oopsGlobal 赋值 5'
   },
   {
     id: 'v3ups9',
-    text: 'comment placeholder (needs translation)'
+    text: '调用 scale，传入一个参数'
   },
   {
     id: 'iw4a3a',
-    text: 'comment placeholder (needs translation)'
+    text: '示例 1：Target 没有子级'
   },
   {
     id: '463xp8',
-    text: 'comment placeholder (needs translation)'
+    text: '示例 2：Target 有一个子级'
   },
   {
     id: 'u3inrm',
-    text: 'comment placeholder (needs translation)'
+    text: '示例 3：Target 有两个子级'
   },
   {
     id: 'axnbgg',
-    text: 'comment placeholder (needs translation)'
+    text: '修改这行上面的代码'
   },
   {
     id: 'i2kck7',
-    text: 'comment placeholder (needs translation)'
+    text: '修改这行下面的代码'
   },
   {
     id: 'dlbobn',
-    text: 'comment placeholder (needs translation)'
+    text: '修改这一行'
   },
   {
     id: 'v127zb',
-    text: 'comment placeholder (needs translation)'
+    text:
+      '检查两个控制台的差异。freeCodeCamp 控制台应该打印两次变量，分别对应这个挑战的每一个测试。浏览器控制台应该只打印一次变量，因为你清除了第一次的打印结果。'
   },
   {
     id: 'ejm0ql',
-    text: 'comment placeholder (needs translation)'
+    text: '关闭视频窗口的第三个选项卡，并合并'
   },
   {
     id: 'iwch6t',
-    text: 'comment placeholder (needs translation)'
+    text: '完成下面的方法，其余的执行类似'
   },
   {
     id: 'hihhyz',
-    text: 'comment placeholder (needs translation)'
+    text: '完成 return 语句'
   },
   {
     id: 'sdxti5',
-    text: 'comment placeholder (needs translation)'
+    text: '计数 target 的子节点，并删掉'
   },
   {
     id: 'wfw6sc',
-    text: 'comment placeholder (needs translation)'
+    text: '创建 x 和 y 轴'
   },
   {
     id: 'sjw6f4',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里创建轴'
   },
   {
     id: 'nupsh2',
-    text: 'comment placeholder (needs translation)'
+    text: '创建一个二维数组，有 m 行 n 列，元素均为 0'
   },
   {
     id: 'xfjb3s',
-    text: 'comment placeholder (needs translation)'
+    text: '在这行下面声明 myGlobal 变量'
   },
   {
     id: 'htpjk7',
-    text: 'comment placeholder (needs translation)'
+    text: '为 decrement action types 定义一个常量'
   },
   {
     id: 'tfzdsp',
-    text: 'comment placeholder (needs translation)'
+    text: '为 increment action types 定义一个常量'
   },
   {
     id: 'zh20mi',
-    text: 'comment placeholder (needs translation)'
+    text: '定义 ADD、addMessage()、messageReducer() 并在这里存储：'
   },
   {
     id: '43qs4c',
-    text: 'comment placeholder (needs translation)'
+    text: '为自减运算定义一个动作创建器'
   },
   {
     id: 'nen3qo',
-    text: 'comment placeholder (needs translation)'
+    text: '为自增运算定义一个动作创建器'
   },
   {
     id: '0cwyam',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义一个动作创建器：'
   },
   {
     id: 'fq0wsg',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义一个动作'
   },
   {
     id: 'tegkqa',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义容器组件：'
   },
   {
     id: 'b5oihn',
-    text: 'comment placeholder (needs translation)'
+    text: '定义 counter reducer，根据接收到的动作递增或递减 state'
   },
   {
     id: '91y4pd',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义 Redux store，传入 reducers'
   },
   {
     id: 'eie1vk',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义 root reducer'
   },
   {
     id: '5s7nnl',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里定义 store here：'
   },
   {
     id: '34qe2q',
-    text: 'comment placeholder (needs translation)'
+    text: '字典将包含集合的元素'
   },
   {
     id: '2c1wra',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里发送接收到的 data action'
   },
   {
     id: '923cpg',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里发送 request action'
   },
   {
     id: 'picsyf',
-    text: 'comment placeholder (needs translation)'
+    text: '在这里发送 action：'
   },
   {
     id: 'ysjr1s',
-    text: 'comment placeholder (needs translation)'
+    text: '显示代码'
   },
   {
     id: 'kjd1am',
-    text: 'comment placeholder (needs translation)'
+    text: '这里不能修改 state，否则测试不能通过'
   },
   {
     id: '5tx4ow',
-    text: 'comment placeholder (needs translation)'
+    text: '娱乐网站'
   },
   {
     id: '9yu58b',
-    text: 'comment placeholder (needs translation)'
+    text: '示例库存清单'
   },
   {
     id: 'ciddtb',
-    text: 'comment placeholder (needs translation)'
+    text: '找到目标值及其父级'
   },
   {
     id: 'ixx548',
-    text: 'comment placeholder (needs translation)'
+    text: '修改下方代码，使其结果为 true'
   },
   {
     id: '6mbhjj',
-    text: 'comment placeholder (needs translation)'
+    text: '例如：Redux.createStore()'
   },
   {
     id: 'jshtzq',
-    text: 'comment placeholder (needs translation)'
+    text: '函数返回表示“一杯红茶（black tea）”的字符串'
   },
   {
     id: 'cw1ghf',
-    text: 'comment placeholder (needs translation)'
+    text: '函数返回表示“一杯绿茶（green tea）”的字符串'
   },
   {
     id: 'iuccln',
-    text: 'comment placeholder (needs translation)'
+    text: '返回一个随机填充的数组'
   },
   {
     id: 'bm2mop',
-    text: 'comment placeholder (needs translation)'
+    text: '点击之后获取 tabs'
   },
   {
     id: 'kchz5k',
-    text: 'comment placeholder (needs translation)'
+    text: '点击之前获取 tabs'
   },
   {
     id: 'bfd23c',
-    text: 'comment placeholder (needs translation)'
+    text:
+      '有一个函数（代表茶的种类）和需要几杯茶，下面的函数返回一个数组，包含字符串（每个字符串表示一杯特别种类的茶）。'
   },
   {
     id: 'ead98i',
-    text: 'comment placeholder (needs translation)'
+    text: '全局计数变量：'
   },
   {
     id: '7zf0i2',
-    text: 'comment placeholder (needs translation)'
+    text: '将它们合并起来'
   },
   {
     id: '5j2l88',
-    text: 'comment placeholder (needs translation)'
+    text: '我们创建三个浏览器窗口'
   },
   {
     id: 'e843r9',
-    text: 'comment placeholder (needs translation)'
+    text: '我们现在打开一个新的选项卡'
   },
   {
     id: '5fvehh',
-    text: 'comment placeholder (needs translation)'
+    text: '在 myLocalScope 之外，未定义 myVar'
   },
   {
     id: 'qn720a',
-    text: 'comment placeholder (needs translation)'
+    text:
+      '现在，在 console.log() 之前添加 console.clear()，清除浏览器控制台的内容，通过测试。'
   },
   {
     id: 'j86mef',
-    text: 'comment placeholder (needs translation)'
+    text: '现在执行打开选项卡，关闭选项卡和其他操作'
   },
   {
     id: 'mk7rvy',
-    text: 'comment placeholder (needs translation)'
+    text: '现在移除 console log 这一行，通过测试'
   },
   {
     id: 'n7vm1s',
-    text: 'comment placeholder (needs translation)'
+    text: '只修改这一行上面的代码'
   },
   {
     id: 'cvh4x7',
-    text: 'comment placeholder (needs translation)'
+    text: '只修改这一行下面的代码'
   },
   {
     id: 'lvmnm7',
-    text: 'comment placeholder (needs translation)'
+    text: '打开一个新的选项卡，显示猫的图片'
   },
   {
     id: 'avpx79',
-    text: 'comment placeholder (needs translation)'
+    text: '打开你的浏览器控制台。'
   },
   {
     id: '0b5ps6',
-    text: 'comment placeholder (needs translation)'
+    text: '画布边线和图表之间的 padding'
   },
   {
     id: 'uemoej',
-    text: 'comment placeholder (needs translation)'
+    text: '将 n 个 0 推入当前行以创建列'
   },
   {
     id: 'lm86nf',
-    text: 'comment placeholder (needs translation)'
+    text: '将当前行（已有 n 个 0）推送到数组'
   },
   {
     id: 'qscelx',
-    text: 'comment placeholder (needs translation)'
+    text: '可从 Redux 对象获得 Redux 方法'
   },
   {
     id: 'atqiig',
-    text: 'comment placeholder (needs translation)'
+    text: '在这一行下面渲染一个输入框（input），按钮（button）和列表（ul）'
   },
   {
     id: 'yq81wg',
-    text: 'comment placeholder (needs translation)'
+    text: '在这一行下面渲染 Provider'
   },
   {
     id: 'kxio9j',
-    text: 'comment placeholder (needs translation)'
+    text: 'responseFromServer 设置为 false，表示从服务器获得无效响应'
   },
   {
     id: 'alh6pw',
-    text: 'comment placeholder (needs translation)'
+    text: 'responseFromServer 设置为 true，表示从服务器获得有效响应'
   },
   {
     id: '1cfidd',
-    text: 'comment placeholder (needs translation)'
+    text: 'responseFromServer 表示从服务器获得一个响应'
   },
   {
     id: '96tntk',
-    text: 'comment placeholder (needs translation)'
+    text: '返回 30'
   },
   {
     id: '58a5g7',
-    text: 'comment placeholder (needs translation)'
+    text: '运行并检查控制台'
   },
   {
     id: '71bus9',
-    text: 'comment placeholder (needs translation)'
+    text: '运行测试，查看两个控制台的差异。'
   },
   {
     id: '7wp46n',
-    text: 'comment placeholder (needs translation)'
+    text: '设置为华氏刻度'
   },
   {
     id: 'oefvg5',
-    text: 'comment placeholder (needs translation)'
+    text: '设置'
   },
   {
     id: 'mnt4d3',
-    text: 'comment placeholder (needs translation)'
+    text: "应该显示 'carrot'"
   },
   {
     id: 'fhe9m4',
-    text: 'comment placeholder (needs translation)'
+    text: '社交网站'
   },
   {
     id: 'za434b',
-    text: 'comment placeholder (needs translation)'
+    text: '每周训练的堆积条形图'
   },
   {
     id: '7c1fv9',
-    text: 'comment placeholder (needs translation)'
+    text: '堆积条形图在这里显示'
   },
   {
     id: 'r44ovx',
-    text: 'comment placeholder (needs translation)'
+    text: 'tabs 是在窗口中打开的每个站点的 title 的数组'
   },
   {
     id: 'cl8peb',
-    text: 'comment placeholder (needs translation)'
+    text: '测试数组：'
   },
   {
     id: '1xi3cv',
-    text: 'comment placeholder (needs translation)'
+    text: '全局变量'
   },
   {
     id: '3gc01a',
-    text: 'comment placeholder (needs translation)'
+    text: 'main.scss 文件'
   },
   {
     id: '14kfog',
-    text: 'comment placeholder (needs translation)'
+    text: '这是交集方法'
   },
   {
     id: 'd1shtt',
-    text: 'comment placeholder (needs translation)'
+    text: '这是并集方法'
   },
   {
     id: 'pqq6sy',
-    text: 'comment placeholder (needs translation)'
+    text: '此方法添加一个元素到集合中'
   },
   {
     id: 'nd2oxy',
-    text: 'comment placeholder (needs translation)'
+    text: '此方法将检查元素是否存在，并返回 true 或 false'
   },
   {
     id: 'ocm81t',
-    text: 'comment placeholder (needs translation)'
+    text: '此方法将从集合中删除一个元素'
   },
   {
     id: 'or9p5p',
-    text: 'comment placeholder (needs translation)'
+    text: '此方法将返回集合中的所有值'
   },
   {
     id: 'g1608f',
-    text: 'comment placeholder (needs translation)'
+    text: '此方法将返回集合的大小'
   },
   {
     id: 'bheu99',
-    text: 'comment placeholder (needs translation)'
+    text: '这将保存一个集合'
   },
   {
     id: 'x1djjr',
-    text: 'comment placeholder (needs translation)'
+    text: '在下一行使用 console.clear()，清除浏览器控制台的内容'
   },
   {
     id: '22ta95',
-    text: 'comment placeholder (needs translation)'
+    text: '使用 console.log() 打印输出变量'
   },
   {
     id: 'w43c7l',
-    text: 'comment placeholder (needs translation)'
+    text: '使用 s = [2, 5, 7] 将无效'
   },
   {
     id: 'pgckoj',
-    text: 'comment placeholder (needs translation)'
+    text: '变量赋值'
   },
   {
     id: '2xiqvv',
-    text: 'comment placeholder (needs translation)'
+    text: '变量声明'
   },
   {
     id: '2sx8zg',
-    text: 'comment placeholder (needs translation)'
+    text: '我们记录对象内部的数组'
   },
   {
     id: 'xmjfd8',
-    text: 'comment placeholder (needs translation)'
+    text: '当你关闭一个选项卡时'
   },
   {
     id: 'es69h6',
-    text: 'comment placeholder (needs translation)'
+    text: '当你将两个窗口合并为一个窗口时'
   },
   {
     id: 'fho5t5',
-    text: 'comment placeholder (needs translation)'
+    text: '当你在最后打开一个选项卡时'
   },
   {
     id: '00kcrm',
-    text: 'comment placeholder (needs translation)'
+    text: '输出 true'
   },
   {
     id: 'sxpg2a',
-    text: 'comment placeholder (needs translation)'
+    text: '你的邮箱、Google Drive 和其他工作地点'
   }
 ];
 
