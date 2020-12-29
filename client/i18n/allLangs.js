0de8dcdadeda57801cef58aacb08bececdd8f613
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2020-12-29 00:36:39 -0800

    feat(client): Add news translation (#40577)

--- client/i18n/allLangs.js
+++ client/i18n/allLangs.js
@@ -8,7 +8,7 @@ const availableLangs = {
 
 // Each client language needs an entry in the rest of the variables below
 
-/* These strings set the i18next langauge. It needs to be the two character
+/* These strings set the i18next language. It needs to be the two character
  * string for the language to take advantage of available functionality.
  * Use a 639-1 code here https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
  */
