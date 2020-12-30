31e59c77276b0ca999fb8e367cd1cb3e826e6694
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2020-12-31 00:46:59 +0530

    fix(i18n): use `withPrefix` for custom paths (#40591)

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import { withPrefix } from 'gatsby';
 
 import RedirectHome from '../components/RedirectHome';
 import ShowUser from '../client-only-routes/ShowUser';
@@ -7,7 +8,7 @@ import ShowUser from '../client-only-routes/ShowUser';
 function User() {
   return (
     <Router>
-      <ShowUser path='/user/:username/report-user' />
+      <ShowUser path={withPrefix('/user/:username/report-user')} />
       <RedirectHome default={true} />
     </Router>
   );
