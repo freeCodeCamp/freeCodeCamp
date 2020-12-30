52b88b8c970f5b1fe124c17ace5beca945aaf29e
Mrugesh Mohapatra <me@raisedadead.com>
2020-12-30 16:34:45 +0000

    fix(i18n): use `withPrefix` for custom paths

--- client/src/pages/challenges.js
+++ client/src/pages/challenges.js
@@ -1,11 +1,7 @@
 // this exists purely to redirect legacy challenge paths to /learn
 import React from 'react';
 import { Router } from '@reach/router';
-import { navigate } from 'gatsby';
-
-import createRedirect from '../components/createRedirect';
-
-const RedirectToLearn = createRedirect('/learn');
+import { navigate, withPrefix } from 'gatsby';
 
 const Redirect = props => {
   if (typeof window !== 'undefined') {
@@ -15,18 +11,18 @@ const Redirect = props => {
 };
 
 const Challenges = () => (
-  <Router basepath='/challenges'>
+  <Router basepath={withPrefix('/challenges')}>
     <Redirect path='/:superBlock/' />
     <Redirect path='/:superBlock/:block/' />
     <Redirect path='/:superBlock/:block/:challenge' />
-    <RedirectToLearn default={true} />
+    <Redirect default={true} />
   </Router>
 );
 
 Challenges.displayName = 'Challenges';
 
 export function toLearnPath({ superBlock, block, challenge }) {
-  let path = '/learn';
+  let path = withPrefix('/learn');
   if (superBlock) path += `/${superBlock}`;
   if (block) path += `/${block}`;
   if (challenge) path += `/${challenge}`;
