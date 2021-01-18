f37dd6ff84e36837c718d6e6ea2dc2a76d8c5802
Randell Dawson <5313213+RandellDawson@users.noreply.github.com>
2021-01-18 11:20:29 -0700

    fix(tools) Improve efficiency of the Hide Non-translated Strings GitHub action (#40721)
    * fix: changed update strings logic
    
    * fix: pull all strings instead of strings by file
    
    * fix: changed console.log message

--- tools/crowdin/actions/hide-non-translated-strings/index.js
+++ tools/crowdin/actions/hide-non-translated-strings/index.js
@@ -4,31 +4,47 @@ const fs = require('fs');
 const path = require('path');
 const matter = require('gray-matter');
 const { getFiles } = require('../../utils/files');
-const { updateFileStrings } = require('../../utils/strings');
+const { getStrings, updateFileString } = require('../../utils/strings');
+
+const createChallengeTitleLookup = (
+  lookup,
+  { fileId, path: crowdinFilePath }
+) => {
+  const challengeFilePath = path.join(
+    __dirname,
+    '/../../../../',
+    crowdinFilePath
+  );
+  try {
+    const challengeContent = fs.readFileSync(challengeFilePath);
+    const {
+      data: { title: challengeTitle }
+    } = matter(challengeContent);
+    return { ...lookup, [fileId]: challengeTitle };
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+  }
+  return lookup;
+};
 
 const hideNonTranslatedStrings = async projectId => {
-  console.log('start hiding non-translated strings...');
+  console.log('hide non-translated strings...');
   const crowdinFiles = await getFiles(projectId);
   if (crowdinFiles && crowdinFiles.length) {
-    for (let { fileId, path: crowdinFilePath } of crowdinFiles) {
-      const challengeFilePath = path.join(
-        __dirname,
-        '/../../../../',
-        crowdinFilePath
-      );
-      try {
-        const challengeContent = fs.readFileSync(challengeFilePath);
-        const {
-          data: { title: challengeTitle }
-        } = matter(challengeContent);
-        await updateFileStrings({ projectId, fileId, challengeTitle });
-      } catch (err) {
-        console.log(err.name);
-        console.log(err.message);
+    const challengeTitleLookup = crowdinFiles.reduce(
+      createChallengeTitleLookup,
+      {}
+    );
+    const crowdinStrings = await getStrings({ projectId });
+    if (crowdinStrings && crowdinStrings.length) {
+      for (let string of crowdinStrings) {
+        const challengeTitle = challengeTitleLookup[string.data.fileId];
+        await updateFileString({ projectId, string, challengeTitle });
       }
     }
   }
-  console.log('hiding non-translated strings complete');
+  console.log('complete');
 };
 
 const projectId = process.env.CROWDIN_PROJECT_ID;
