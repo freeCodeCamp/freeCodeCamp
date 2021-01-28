3a921b208a226d39b697cd22d96d1d16dd4edff7
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-01-28 19:16:30 +0900

    feat: update footers for all locales

--- client/i18n/trending-schema.js
+++ client/i18n/trending-schema.js
@@ -4,96 +4,95 @@
  */
 
 const trendingSchema = {
-  article1title: 'Git Clone',
+  article0title: 'What is Docker?',
+  article0link:
+    'https://www.freecodecamp.org/news/what-is-docker-used-for-a-docker-container-tutorial-for-beginners/',
+  article1title: 'TCP/IP Model',
   article1link:
-    'https://www.freecodecamp.org/news/git-clone-branch-how-to-clone-a-specific-branch/',
-  article2title: 'Agile Methods',
+    'https://www.freecodecamp.org/news/what-is-tcp-ip-layers-and-protocols-explained/',
+  article2title: 'RTF File',
   article2link:
-    'https://www.freecodecamp.org/news/agile-methods-and-methodology-for-beginners/',
-  article3title: 'Python Main',
-  article3link:
-    'https://www.freecodecamp.org/news/if-name-main-python-example/',
-  article4title: 'Callback',
+    'https://www.freecodecamp.org/news/rtf-file-what-is-the-rich-text-format/',
+  article3title: 'CSS Transition',
+  article3link: 'https://www.freecodecamp.org/news/css-transition-examples/',
+  article4title: 'How to Use Instagram?',
   article4link:
-    'https://www.freecodecamp.org/news/javascript-callback-functions-what-are-callbacks-in-js-and-how-to-use-them/',
-  article5title: 'Debounce',
+    'https://www.freecodecamp.org/news/how-to-use-instagram-like-a-pro/',
+  article5title: 'MBR VS GPT',
   article5link:
-    'https://www.freecodecamp.org/news/debounce-javascript-tutorial-how-to-make-your-js-wait-up/',
-  article6title: 'URL Encode',
+    'https://www.freecodecamp.org/news/mbr-vs-gpt-whats-the-difference-between-an-mbr-partition-and-a-gpt-partition-solved/',
+  article6title: 'FAT32 Format',
   article6link:
-    'https://www.freecodecamp.org/news/javascript-url-encode-example-how-to-use-encodeuricomponent-and-encodeuri/',
-  article7title: 'Blink HTML',
+    'https://www.freecodecamp.org/news/how-to-format-a-usb-drive-to-fat32-on-windows-10/',
+  article7title: 'Error 503 Code',
   article7link:
-    'https://www.freecodecamp.org/news/make-it-blink-html-tutorial-how-to-use-the-blink-tag-with-code-examples/',
-  article8title: 'Python Tuple',
+    'https://www.freecodecamp.org/news/http-error-503-service-unavailable-explained-what-the-503-error-code-means/',
+  article8title: 'Windows Hosts File',
   article8link:
-    'https://www.freecodecamp.org/news/python-returns-multiple-values-how-to-return-a-tuple-list-dictionary/',
-  article9title: 'JavaScript Push',
+    'https://www.freecodecamp.org/news/how-to-find-and-edit-a-windows-hosts-file/',
+  article9title: 'Mobi to PDF',
   article9link:
-    'https://www.freecodecamp.org/news/javascript-array-insert-how-to-add-to-an-array-with-the-push-unshift-and-concat-functions/',
-  article10title: 'Java List',
+    'https://www.freecodecamp.org/news/mobi-to-pdf-how-to-convert-to-and-from-a-mobi-file/',
+  article10title: 'What is STEM?',
   article10link:
-    'https://www.freecodecamp.org/news/java-list-tutorial-util-list-api-example/',
-  article11title: 'UX',
+    'https://www.freecodecamp.org/news/what-is-stem-the-meaning-of-an-acronym-youll-hear-a-lot-in-school/',
+  article11title: 'JavaScript Void 0',
   article11link:
-    'https://www.freecodecamp.org/news/learn-ux-design-self-taught-user-experience-designer/',
-  article12title: 'Design Thinking',
+    'https://www.freecodecamp.org/news/javascript-void-keyword-explained/',
+  article12title: 'SQL Delete Row',
   article12link:
-    'https://www.freecodecamp.org/news/what-is-design-thinking-an-introduction-to-the-design-process-for-entrepreneurs-and-developers/',
-  article13title: 'Prime Number List',
+    'https://www.freecodecamp.org/news/sql-delete-row-statement-examples/',
+  article13title: 'JavaScript Replace',
   article13link:
-    'https://www.freecodecamp.org/news/prime-numbers-list-chart-of-primes/',
-  article14title: 'Product Design',
+    'https://www.freecodecamp.org/news/javascript-string-replace-example-with-regex/',
+  article14title: 'Python JSON Parser',
   article14link:
-    'https://www.freecodecamp.org/news/product-design-explained-in-plain-english/',
-  article15title: 'Digital Design',
+    'https://www.freecodecamp.org/news/python-read-json-file-how-to-load-json-from-a-file-and-parse-dumps/',
+  article15title: 'cmd Delete Folder',
   article15link:
-    'https://www.freecodecamp.org/news/what-is-digital-design-and-why-does-it-matter/',
-  article16title: 'Coding Games',
+    'https://www.freecodecamp.org/news/cmd-delete-folder-how-to-remove-files-and-folders-in-windows/',
+  article16title: 'What is NFC?',
   article16link:
-    'https://www.freecodecamp.org/news/best-coding-games-online-adults-learn-to-code/',
-  article17title: 'SVM',
+    'https://www.freecodecamp.org/news/what-is-nfc-near-field-communication-uses-chips-tags-and-readers-explained/',
+  article17title: 'Content Type JSON',
   article17link:
-    'https://www.freecodecamp.org/news/svm-machine-learning-tutorial-what-is-the-support-vector-machine-algorithm-explained-with-code-examples/',
-  article18title: 'JavaScript forEach',
+    'https://www.freecodecamp.org/news/what-is-the-correct-content-type-for-json-request-header-mime-type-explained/',
+  article18title: 'Convert HEIC to JPG',
   article18link:
-    'https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/',
-  article19title: 'Google BERT',
+    'https://www.freecodecamp.org/news/heic-to-jpg-how-to-convert-images-on-a-mac/',
+  article19title: 'Math Random Java',
   article19link:
-    'https://www.freecodecamp.org/news/google-bert-nlp-machine-learning-tutorial/',
-  article20title: 'Create Table SQL',
+    'https://www.freecodecamp.org/news/generate-random-numbers-java/',
+  article20title: 'WordPress for Beginners',
   article20link:
-    'https://www.freecodecamp.org/news/sql-create-table-statement-with-example-syntax/',
-  article21title: 'Responsive Web Design',
+    'https://www.freecodecamp.org/news/how-to-start-a-blog-with-wordpress/',
+  article21title: 'Qualitative VS Quantitative',
   article21link:
-    'https://www.freecodecamp.org/news/responsive-web-design-how-to-make-a-website-look-good-on-phones-and-tablets/',
-  article22title: 'What Is an SVG File?',
+    'https://www.freecodecamp.org/news/qualitative-vs-quantitative-definition-research-methods-and-data/',
+  article22title: 'JavaScript Split String',
   article22link:
-    'https://www.freecodecamp.org/news/svg-basics-what-are-scalable-vector-graphics-and-how-do-you-use-them/',
-  article23title: 'PDF Password Remover',
+    'https://www.freecodecamp.org/news/javascript-split-string-example/',
+  article23title: 'Accented Letters on Mac',
   article23link:
-    'https://www.freecodecamp.org/news/pdf-password-remover-guide-how-to-remove-password-protection-from-a-pdf/',
-  article24title: 'What Is a PDF?',
+    'https://www.freecodecamp.org/news/how-to-type-letters-with-accents-on-mac/',
+  article24title: 'Windows 10 Product Key',
   article24link:
-    'https://www.freecodecamp.org/news/what-is-a-pdf-file-and-how-do-you-open-it-solved/',
-  article25title: 'What Is Python?',
+    'https://www.freecodecamp.org/news/how-to-find-a-windows-10-product-key/',
+  article25title: 'Google Docs Landscape',
   article25link:
-    'https://www.freecodecamp.org/news/what-is-python-used-for-10-coding-uses-for-the-python-programming-language/',
-  article26title: 'What Is TLS?',
+    'https://www.freecodecamp.org/news/google-docs-landscape-tutorial-how-to-make-a-google-doc-landscape-orientation/',
+  article26title: 'Antimalware Executable',
   article26link:
-    'https://www.freecodecamp.org/news/what-is-tls-transport-layer-security-encryption-explained-in-plain-english/',
-  article27title: 'What Is a LAN?',
+    'https://www.freecodecamp.org/news/antimalware-service-executable-what-is-msmpeng-exe-and-why-is-it-running-in-windows-10/',
+  article27title: 'Windows 10 Start Menu',
   article27link:
-    'https://www.freecodecamp.org/news/what-is-a-lan-local-area-network-explained-in-plain-english/',
-  article28title: 'What Is npm?',
+    'https://www.freecodecamp.org/news/windows-10-start-menu-not-working-solved/',
+  article28title: 'Windows 10 Command Line',
   article28link:
-    'https://www.freecodecamp.org/news/what-is-npm-a-node-package-manager-tutorial-for-beginners/',
-  article29title: 'RSync Examples',
+    'https://www.freecodecamp.org/news/how-to-open-the-command-prompt-in-windows-10/',
+  article29title: 'Google Account Recovery',
   article29link:
-    'https://www.freecodecamp.org/news/rsync-examples-rsync-options-and-how-to-copy-files-over-ssh/',
-  article30title: 'Random Forest',
-  article30link:
-    'https://www.freecodecamp.org/news/how-to-use-the-tree-based-algorithm-for-machine-learning/'
+    'https://www.freecodecamp.org/news/google-account-recovery-change-gmail-password/'
 };
 
 exports.trendingSchema = trendingSchema;
