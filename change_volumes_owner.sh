77a271320d5a735524215ff6d4621cb3af16fef8
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-02-03 11:04:11 +0100

    chore: remove old Docker volume script

--- change_volumes_owner.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-chown node:staff node_modules
-chown node:staff api-server/node_modules
-chown node:staff client/node_modules
-chown node:staff client/plugins/fcc-create-nav-data/node_modules
-chown node:staff curriculum/node_modules
-chown node:staff tools/challenge-parser/node_modules
-chown node:staff tools/scripts/seed/node_modules
