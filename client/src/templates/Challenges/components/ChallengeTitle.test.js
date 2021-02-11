89b1b1325c2ab47110f12e1fca43bd4463487808
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-11 09:52:54 -0600

    fix: update snapshot

--- client/src/templates/Challenges/components/ChallengeTitle.test.js
+++ client/src/templates/Challenges/components/ChallengeTitle.test.js
@@ -6,8 +6,10 @@ import renderer from 'react-test-renderer';
 import ChallengeTitle from './Challenge-Title';
 
 const baseProps = {
+  block: 'fake block',
   children: 'title text',
-  isCompleted: true
+  isCompleted: true,
+  superBlock: 'fake superblock'
 };
 
 describe('<ChallengeTitle/>', () => {
