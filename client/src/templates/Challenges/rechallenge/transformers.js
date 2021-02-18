ff92d9674e8a681075cdad0dbc24d3d7d49eff1a
Shaun Hamilton <2397860h@student.gla.ac.uk>
2021-02-18 17:20:39 +0000

    return protectTimeout to 100

--- client/src/templates/Challenges/rechallenge/transformers.js
+++ client/src/templates/Challenges/rechallenge/transformers.js
@@ -19,8 +19,8 @@ import createWorker from '../utils/worker-executor';
 // eslint-disable-next-line import/no-unresolved
 import { filename as sassCompile } from '../../../../../config/client/sass-compile';
 
-const protectTimeout = 400;
-const recursionTimeout = 700;
+const protectTimeout = 100;
+const recursionTimeout = 900;
 const testProtectTimeout = 1500;
 const loopsPerTimeoutCheck = 2000;
 
