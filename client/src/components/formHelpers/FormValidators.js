bea773f51033530357df906a8df357286fd3cfbb
Shaun Hamilton <2397860h@student.gla.ac.uk>
2021-02-18 15:43:59 +0000

    add fCCRegex to validators

--- client/src/components/formHelpers/FormValidators.js
+++ client/src/components/formHelpers/FormValidators.js
@@ -1,9 +1,13 @@
 // Matches editor links for: Repl.it, Glitch, CodeSandbox, GitHub
 const editorRegex = /repl\.it\/(@|join\/)|glitch\.com\/edit\/#!|codesandbox\.io\/s\/|github\.com/;
+const fCCRegex = /codepen\.io\/freecodecamp|freecodecamp\.rocks|github\.com\/freecodecamp/i;
 const localhostRegex = /localhost:/;
 
 export const editorValidator = value =>
   editorRegex.test(value) ? 'Remember to submit the Live App URL.' : null;
+
+export const fCCValidator = value =>
+  fCCRegex.test(value) ? 'Remember to submit your own work.' : null;
 
 export const localhostValidator = value =>
   localhostRegex.test(value)
