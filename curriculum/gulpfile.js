1c8d63953383d40ae948f3217abd7ee2f11d2435
Mrugesh Mohapatra <me@raisedadead.com>
2021-02-26 01:04:27 +0530

    fix(api): decouple api from curriculum
    This reverts commit 8f0e441644c6e31e2e837e2b3d95efb8a6622787 and
    introduces the implementations from #40703.

--- curriculum/gulpfile.js
+++ curriculum/gulpfile.js
@@ -1,31 +1,12 @@
-const fs = require('fs-extra');
 const gulp = require('gulp');
 const through2 = require('through2');
 
-const { curriculumLocale } = require('../config/env.json');
-const { getChallengesForLang } = require('./getChallenges');
 const { testedLang } = require('./utils');
 const lintMarkdown = require('../tools/scripts/lint');
 
 /**
  * Tasks
  **/
-
-function generateCurriculum(done) {
-  return getChallengesForLang(curriculumLocale)
-    .then(curriculum => {
-      fs.ensureFileSync(`./build/curriculum-${curriculumLocale}.json`);
-      fs.writeFile(
-        `./build/curriculum-${curriculumLocale}.json`,
-        JSON.stringify(curriculum)
-      );
-    })
-    .then(done);
-}
-
-function watchFiles() {
-  return gulp.watch('./challenges/**/*.md', generateCurriculum);
-}
 
 function lint() {
   return gulp.src(globLang(testedLang()), { read: false }).pipe(
@@ -35,8 +16,6 @@ function lint() {
   );
 }
 
-const defaultTask = gulp.series(generateCurriculum, watchFiles);
-
 /**
  * Helper functions
  **/
@@ -45,6 +24,4 @@ function globLang(lang) {
   return `./challenges/${lang}/**/*.md`;
 }
 
-gulp.task('default', defaultTask);
-gulp.task('build', generateCurriculum);
 gulp.task('lint', lint);
