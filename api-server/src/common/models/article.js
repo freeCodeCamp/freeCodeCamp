0d5cd99a718fdef2ca7856a9cebd1ddffa85f3d7
Mrugesh Mohapatra <me@raisedadead.com>
2021-03-02 12:36:08 +0530

    chore(api): remove unused code and endpoints

--- api-server/src/common/models/article.js
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rx';
-
-module.exports = function(Article) {
-  Article.on('dataSourceAttached', () => {
-    Article.findOne$ = Observable.fromNodeCallback(Article.findOne, Article);
-    Article.findById$ = Observable.fromNodeCallback(Article.findById, Article);
-    Article.find$ = Observable.fromNodeCallback(Article.find, Article);
-  });
-};
