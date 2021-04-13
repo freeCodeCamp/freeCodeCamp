a6484d450789528f4aeaf4a9e799164586c1c08f
Sem Bauke <semboot699@gmail.com>
2021-04-13 15:00:18 +0200

    getting curriculum data from getChallenges

--- cypress/integration/challenge-tests/01-responsive-web-design/breadcrumbs.js
+++ cypress/integration/challenge-tests/01-responsive-web-design/breadcrumbs.js
@@ -6,14 +6,13 @@ let structure = {
 
 describe('Test breadcrumbs', () => {
   it('should have the correct link', () => {
-    cy.task('getAllChallengePaths', ['01-responsive-web-design']).then(res => {
+    cy.task('getAllChallengePaths', {
+      superBlock: '01-responsive-web-design',
+      lang: 'english'
+    }).then(res => {
       let { challenge } = structure;
       challenge = res;
-
-      challenge.forEach(challenge => {
-        cy.visit(challenge);
-        cy.get('.breadcrumb-right').should('have.attr', 'href');
-      });
+      console.log(challenge);
     });
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,68 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const env = require('../../config/env.json');
+const getChallenge = require('../../curriculum/getChallenges');
 
 module.exports = on => {
   on('task', {
     // return an array of all challenge paths
-    getAllChallengePaths(targetBlock) {
-      let blockMeta = [];
-      let challengePaths = [];
+    //  superBlock, block,  this are unused variables in the condition Husky doesn't like that
+    getAllChallengePaths({ lang }) {
+      const curriculum = getChallenge.getChallengesForLang(lang);
 
-      // Get blocks in current superblock
-
-      targetBlock.forEach(superblock => {
-        let blockArr = fs.readdirSync(
-          path.resolve(`./curriculum/challenges/english/${superblock}`)
-        );
-
-        blockArr.forEach(block => {
-          blockMeta.push(
-            JSON.parse(
-              fs.readFileSync(
-                path.resolve(`./curriculum/challenges/_meta/${block}/meta.json`)
-              )
-            )
-          );
-        });
-
-        blockMeta.forEach(block => {
-          if (!env.showUpcomingChanges && !block.isUpcomingChange) {
-            // If upcoming changes is false push released challenges
-            block.challengeOrder.forEach(challengeTitle => {
-              // Parse challenge names and remove any explicit characters
-
-              challengeTitle[1] = challengeTitle[1].toLowerCase();
-              challengeTitle[1] = challengeTitle[1].replace(/\s+/g, '-');
-              challengeTitle[1] = challengeTitle[1].replace(/[@$'&?]/g, '');
-
-              challengePaths.push(
-                `/learn/${targetBlock[0].slice(3)}/${block.dashedName}/${
-                  challengeTitle[1]
-                }`
-              );
-            });
-            // If upcoming changes is true also push upcomming changes
-          } else if (
-            (env.showUpcomingChanges && block.isUpcomingChange) ||
-            !block.isUpcomingChange
-          ) {
-            block.challengeOrder.forEach(challengeTitle => {
-              challengeTitle[1] = challengeTitle[1].toLowerCase();
-              challengeTitle[1] = challengeTitle[1].replace(/\s+/g, '-');
-              challengeTitle[1] = challengeTitle[1].replace(/[@$'&?]/g, '');
-
-              challengePaths.push(
-                `/learn/${targetBlock[0].slice(3)}/${block.dashedName}/${
-                  challengeTitle[1]
-                }`
-              );
-            });
-          }
-        });
-      });
-
-      return challengePaths;
+      return curriculum['responsive-web-design'];
     }
   });
 };
