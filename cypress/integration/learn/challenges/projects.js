073fd4128a24d9506d8476e8842c9ef1ef4edc1c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-04-22 13:30:42 +0200

    test: wait for next challenge render

--- cypress/integration/learn/challenges/projects.js
+++ cypress/integration/learn/challenges/projects.js
@@ -4,25 +4,41 @@ const projects = {
   superBlock: 'machine-learning-with-python',
   block: 'machine-learning-with-python-projects',
   challenges: [
-    'book-recommendation-engine-using-knn',
-    'cat-and-dog-image-classifier',
-    'linear-regression-health-costs-calculator',
-    'neural-network-sms-text-classifier',
-    'rock-paper-scissors'
+    {
+      slug: 'book-recommendation-engine-using-knn',
+      nextChallengeText: 'Linear Regression Health Costs Calculator'
+    },
+    {
+      slug: 'cat-and-dog-image-classifier',
+      nextChallengeText: 'Book Recommendation Engine using KNN'
+    },
+    {
+      slug: 'linear-regression-health-costs-calculator',
+      nextChallengeText: 'Neural Network SMS Text Classifier'
+    },
+    {
+      slug: 'neural-network-sms-text-classifier',
+      nextChallengeText: 'Find the Symmetric Difference'
+    },
+    {
+      slug: 'rock-paper-scissors',
+      nextChallengeText: 'Cat and Dog Image Classifier'
+    }
   ]
 };
 describe('project submission', () => {
   // NOTE: this will fail once challenge tests are added.
   it('Should be possible to submit Python projects', () => {
     const { superBlock, block, challenges } = projects;
-    challenges.forEach(challenge => {
-      cy.visit(`/learn/${superBlock}/${block}/${challenge}`);
+    challenges.forEach(({ slug, nextChallengeText }) => {
+      cy.visit(`/learn/${superBlock}/${block}/${slug}`);
       cy.get('#dynamic-front-end-form')
         .get('#solution')
         .type('https://repl.it/@camperbot/python-project#main.py');
 
       cy.contains("I've completed this challenge").click();
       cy.contains('Go to next challenge').click();
+      cy.get('.title-text').should('include.text', nextChallengeText);
     });
   });
 });
