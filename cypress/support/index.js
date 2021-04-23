d97f59885248a39084b1b1463039640b15903e01
Sem Bauke <semboot699@gmail.com>
2021-04-23 14:35:35 +0200

    make cypress not fail test on uncaught exception due chunk errors

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,11 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
+/* global cy */
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+cy.on('uncaught:exception', () => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
