85903c820d17bcd8191daedf238120367cd82b19
Sem Bauke <semboot699@gmail.com>
2021-04-29 16:57:46 +0200

    feat(Cypress): Add test for email change

---
+++ cypress/integration/settings/email-change.js
@@ -0,0 +1,43 @@
+/* global cy */
+describe('Email input field', () => {
+  before(() => {
+    cy.login();
+    cy.visit('/settings');
+  });
+
+  it('Should be possible to type new email', () => {
+    cy.get('[id=new-email]')
+      .type('bar@foo.com')
+      .should('have.attr', 'value', 'bar@foo.com');
+  });
+
+  it('Should be possible to confirm email', () => {
+    cy.get('[id=confirm-email]')
+      .type('bar@foo.com')
+      .should('have.attr', 'value', 'bar@foo.com');
+  });
+
+  it('Should be possible to save new email', () => {
+    cy.contains('Save').click({ force: true });
+  });
+
+  it('Displays an error message when emails do not match', () => {
+    cy.get('[id=confirm-email]').clear().type('foo@bar.com');
+
+    cy.get('[class=help-block]').contains(
+      'Both new email addresses must be the same'
+    );
+  });
+
+  it('Displays an error message when the email is the same as current email', () => {
+    cy.get('[id=new-email]').clear().type('foo@bar.com');
+
+    cy.get('[class=help-block]').contains(
+      'This email is the same as your current email'
+    );
+  });
+
+  it('Should be possible to get Quincys weekly email', () => {
+    cy.contains('Yes please').click({ force: true });
+  });
+});
