7f6f386f356ebadf53490fda0f6b142df3101e81
Sem Bauke <semboot699@gmail.com>
2021-04-29 18:54:12 +0200

    Add suggestions from oliver

--- cypress/integration/settings/email-change.js
+++ cypress/integration/settings/email-change.js
@@ -5,33 +5,27 @@ describe('Email input field', () => {
     cy.visit('/settings');
   });
 
-  it('Should be possible to type new email', () => {
+  it('Should be possible to submit the new email', () => {
     cy.get('[id=new-email]')
       .type('bar@foo.com')
       .should('have.attr', 'value', 'bar@foo.com');
-  });
 
-  it('Should be possible to confirm email', () => {
     cy.get('[id=confirm-email]')
       .type('bar@foo.com')
       .should('have.attr', 'value', 'bar@foo.com');
-  });
 
-  it('Should be possible to save new email', () => {
     cy.get('[id=form-update-email]').within(() => {
       cy.contains('Save').click();
     });
   });
 
-  it('Displays an error message when emails do not match', () => {
+  it('Displays an error message when there are problems with the submitted emails', () => {
     cy.get('[id=confirm-email]').clear().type('foo@bar.com');
 
     cy.get('[class=help-block]').contains(
       'Both new email addresses must be the same'
     );
-  });
 
-  it('Displays an error message when the email is the same as current email', () => {
     cy.get('[id=new-email]').clear().type('foo@bar.com');
 
     cy.get('[class=help-block]').contains(
@@ -40,6 +34,6 @@ describe('Email input field', () => {
   });
 
   it('Should be possible to get Quincys weekly email', () => {
-    cy.contains('Yes please').click({ force: true });
+    cy.contains('Yes please').click();
   });
 });
