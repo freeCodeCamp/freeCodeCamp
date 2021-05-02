3c796710109ac507501b22eb1d47f57ba64e6972
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2021-05-03 01:35:46 +0700

    Revert "feat(client): basic Storybook setup"
    This reverts commit f55e2493265e9fde971200fb649f15f8896e03b9.

--- client/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-postcss'
-  ]
-};
--- client/.storybook/preview.js
+++
@@ -1,9 +0,0 @@
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
-    }
-  }
-};
