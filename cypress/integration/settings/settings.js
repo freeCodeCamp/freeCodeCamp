eb31c14188a17c727314fa5cb840a1df170b0611
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-07 09:41:39 +0200

    test(e2e): test that progress can be reset

--- cypress/integration/settings/settings.js
+++ cypress/integration/settings/settings.js
@@ -1,8 +1,15 @@
-/* global cy */
+/* global cy expect */
+
 describe('Settings', () => {
-  it('should be possible to visit the settings page', () => {
+  it('should be possible to reset your progress', () => {
     cy.visit('/');
-    cy.contains("Get started (it's free)").click({ force: true });
+    cy.contains("Get started (it's free)").click();
     cy.visit('/settings');
+    cy.contains('Reset all of my progress').click();
+    cy.contains('Reset everything. I want to start from the beginning').click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/');
+    });
+    cy.contains('Your progress has been reset');
   });
 });
