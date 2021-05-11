f50285f05a93ce9a9ee8afc7e5b5b418ae7b13c4
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-11 18:21:09 +0200

    Revert "fix(client): use strip-comments library for js comments removal (#41960)"
    This reverts commit b16fadb4361ea8b753c9c6213ce47d92d943dd49.

--- client/src/utils/curriculum-helpers.js
+++ client/src/utils/curriculum-helpers.js
@@ -1,16 +1,27 @@
-import strip from 'strip-comments';
-
-export const removeJSComments = codeStr => {
-  try {
-    return strip(codeStr);
-  } catch (err) {
-    return codeStr;
-  }
-};
+import { parse } from '@babel/parser';
+import generate from '@babel/generator';
 
 const removeHtmlComments = str => str.replace(/<!--[\s\S]*?(-->|$)/g, '');
 
 const removeCssComments = str => str.replace(/\/\*[\s\S]+?\*\//g, '');
+
+export const removeJSComments = codeStr => {
+  // Note: removes trailing new lines and tailing spaces at end of lines
+  const options = {
+    comments: false,
+    retainLines: true,
+    compact: false,
+    concise: false,
+    minified: false
+  };
+  try {
+    const ast = parse(codeStr);
+    const { code } = generate(ast, options, codeStr);
+    return code;
+  } catch (err) {
+    return codeStr;
+  }
+};
 
 const removeWhiteSpace = (str = '') => {
   return str.replace(/\s/g, '');
