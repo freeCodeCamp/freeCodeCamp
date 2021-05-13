205f0ad080a26081d8669c4e1815aaadcd1d99c0
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-13 12:45:15 +0200

    feat: convert Flash to typescript

--- client/src/components/Flash/index.tsx
+++ client/src/components/Flash/index.tsx
@@ -6,15 +6,25 @@ import { useTranslation } from 'react-i18next';
 
 import './flash.css';
 
-function Flash({ flashMessage, onClose }) {
+type FlashProps = {
+  flashMessage: {
+    type: string;
+    message: string;
+    id: string;
+    variables: Record<string, unknown>;
+  };
+  onClose: () => void;
+};
+
+function Flash({ flashMessage, onClose }: FlashProps): JSX.Element {
   const { type, message, id, variables = {} } = flashMessage;
   const { t } = useTranslation();
-  const [flashMessageHeight, setFlashMessageHeight] = useState(null);
+  const [flashMessageHeight, setFlashMessageHeight] = useState(0);
 
   useEffect(() => {
-    setFlashMessageHeight(
-      document.querySelector('.flash-message').offsetHeight
-    );
+    const flashMessageElem: HTMLElement | null =
+      document.querySelector('.flash-message');
+    setFlashMessageHeight(flashMessageElem?.offsetHeight || 0);
     document.documentElement.style.setProperty(
       '--flash-message-height',
       flashMessageHeight + 'px'
---
+++ client/src/declarations.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/unambiguous
+declare module '@freecodecamp/react-bootstrap';
