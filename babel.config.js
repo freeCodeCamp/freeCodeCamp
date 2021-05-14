cd70b069778754cc0ee1447a11a0baff94051c68
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-14 17:20:33 +0200

    refactor: bring api jest tests to root

---
+++ babel.config.js
@@ -0,0 +1,1 @@
+module.exports = { babelrcRoots: ['.', './api-server'] };
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,21 @@
-// TODO: remove this once /tools/dashboard's tests are configured correctly
-
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/tools/dashboard']
+  // TODO: remove /tools/dashboard when the tests are configured correctly
+  testPathIgnorePatterns: ['/node_modules/', '/tools/dashboard'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/src/__mocks__/fileMock.js',
+    // Plain CSS - match css files that don't end with
+    // '.module.css' https://regex101.com/r/VzwrKH/4
+    '^(?!.*\\.module\\.css$).*\\.css$': '<rootDir>/src/__mocks__/styleMock.js',
+    // CSS Modules - match files that end with 'module.css'
+    '\\.module\\.css$': 'identity-obj-proxy',
+    analytics: '<rootDir>/src/__mocks__/analyticsMock.js'
+  },
+  globals: {
+    __PATH_PREFIX__: ''
+  },
+  verbose: true,
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)']
 };
