d49f6042d3c475c7d8c73e4310ef1da172fca89d
ChrisPrzR <62666682+ChrisPrzR@users.noreply.github.com>
2021-05-21 09:10:25 -0500

    Apply suggestions from code review
    Co-authored-by: Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>

--- tools/ui-components/tailwind.config.js
+++ tools/ui-components/tailwind.config.js
@@ -14,17 +14,17 @@ module.exports = {
       gray80: '#2a2a40',
       gray85: '#1b1b32',
       gray90: '#0a0a23',
-      //Accent Colors - Primary
+      // Accent Colors - Primary
       blue: '#99c9ff',
       green: '#acd157',
       purple: '#dbb8ff',
       yellow: '#f1be32',
-      //Accent Colors - Secondary
+      // Accent Colors - Secondary
       darkBlue: '#002ead',
       darkGreen: '#00471b',
       darkPurple: '#5a01a7',
       darkYellow: '#4d3800',
-      //Colors in variables.css
+      // Colors in variables.css
       blueMid: '#198eee',
       blueTranslucent: 'rgba(153, 201, 255, 0.3)',
       darkBlueTranslucent: 'rgba(0, 46, 173, 0.3)',
@@ -34,9 +34,9 @@ module.exports = {
       editorBackground: '#fffffe',
       love: '#f8577c',
       purpleMid: '#9400d3',
-      red: '#ffadad', 
+      red: '#ffadad',
       yellowGold: '#ffbf00',
-      yellowLight: '#ffc300',
+      yellowLight: '#ffc300'
     }
   },
   variants: {
