36a04dc00de85ec638565b9409ca91e405d0f974
Sem Bauke <46919888+Sembauke@users.noreply.github.com>
2021-05-22 19:59:54 +0200

    Update cypress/integration/settings/image-picture-check.js

--- cypress/integration/settings/image-picture-check.js
+++ cypress/integration/settings/image-picture-check.js
@@ -2,7 +2,7 @@
 
 describe('Picture input field', () => {
   beforeEach(() => {
-    cy.login()
+    cy.login();
     cy.visit('/settings');
     // Setting aliases here
     cy.get('input#about-picture').as('pictureInput');
