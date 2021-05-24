daaff33c49ef2c49a5a0c66d7550b515af24005e
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-05-24 15:54:03 +0900

    feat: update titles for certification pages

--- client/src/templates/Introduction/SuperBlockIntro.js
+++ client/src/templates/Introduction/SuperBlockIntro.js
@@ -148,11 +148,14 @@ class SuperBlockIntroductionPage extends Component {
     const nodesForSuperBlock = edges.map(({ node }) => node);
     const blockDashedNames = uniq(nodesForSuperBlock.map(({ block }) => block));
     const i18nSuperBlock = t(`intro:${superBlock}.title`);
+    const i18nCertText = t(`intro:misc-text.certification`, {
+      cert: i18nSuperBlock
+    });
 
     return (
       <>
         <Helmet>
-          <title>{i18nSuperBlock} | freeCodeCamp.org</title>
+          <title>{i18nCertText} | freeCodeCamp.org</title>
         </Helmet>
         <Grid>
           <Row className='super-block-intro-page'>
