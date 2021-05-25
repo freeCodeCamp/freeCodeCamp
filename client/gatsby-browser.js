5331e698ae206efaf3f77882a6ba5043f3d53154
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-25 12:20:45 +0200

    feat: only regenerate csrf tokens as needed

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -30,7 +30,9 @@ export const wrapPageElement = layoutSelector;
 export const disableCorePrefetching = () => true;
 
 export const onClientEntry = () => {
-  // purge the _csrf cookie, rather than relying what the browser decides a
+  // purge the csrf cookies, rather than relying what the browser decides a
   // Session duration is
   cookies.erase('_csrf');
+  // the token must be erased since it is only valid for the old _csrf secret
+  cookies.erase('csrf_token');
 };
