3bc8eab6b2c999c88394500343234151b35431df
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-05-27 14:53:55 +0200

    test: update for new findIndexHtml

--- client/src/templates/Challenges/rechallenge/builders.test.js
+++ client/src/templates/Challenges/rechallenge/builders.test.js
@@ -2,19 +2,19 @@
 import { findIndexHtml } from './builders.js';
 
 const withHTML = [
-  { includes: [], contents: 'the index html' },
-  { contents: 'the style file' }
+  { history: ['index.html'], contents: 'the index html' },
+  { history: ['index.css', 'index.html'], contents: 'the style file' }
 ];
 
 const withoutHTML = [
-  { contents: 'the js file' },
-  { contents: 'the style file' }
+  { history: ['index.css', 'index.html'], contents: 'the js file' },
+  { history: ['index.js', 'index.html'], contents: 'the style file' }
 ];
 
 const tooMuchHTML = [
-  { includes: [], contents: 'the index html' },
-  { includes: [], contents: 'index html two' },
-  { includes: [], contents: 'index html three' }
+  { history: ['index.html'], contents: 'the index html' },
+  { history: ['index.css', 'index.html'], contents: 'index html two' },
+  { history: ['index.html'], contents: 'index html three' }
 ];
 
 // TODO: write tests for concatHtml instead, since findIndexHtml should not be
@@ -25,15 +25,15 @@ describe('findIndexHtml', () => {
     expect.assertions(1);
 
     expect(findIndexHtml(withHTML)).toStrictEqual({
-      includes: [],
+      history: ['index.html'],
       contents: 'the index html'
     });
   });
 
-  it('should return null when the index.html file is missing', () => {
+  it('should return undefined when the index.html file is missing', () => {
     expect.assertions(1);
 
-    expect(findIndexHtml(withoutHTML)).toBe(null);
+    expect(findIndexHtml(withoutHTML)).toBeUndefined();
   });
 
   it('should throw if there are two or more index.htmls', () => {
