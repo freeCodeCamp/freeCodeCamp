fd2bebe8be5c8d71ca3ca17cd91963c6ed4050ad
Dripcoding <dripcoding@gmail.com>
2021-05-30 21:41:18 -0700

    feat: migrate Profile to typescript

--- client/src/components/helpers/Spacer.tsx
+++ client/src/components/helpers/Spacer.tsx
@@ -4,23 +4,24 @@ interface ISpacerProps {
   size?: number;
 }
 
-const styles = {padding: '15px 0', height: '1px'};
+const styles = { padding: '15px 0', height: '1px' };
 
-const Comp = (props: any): JSX.Element => <div className='spacer' style={styles} {...props} />;
+const Comp = (props: any): JSX.Element => (
+  <div className='spacer' style={styles} {...props} />
+);
 
-const Spacer = ({size = 1}: ISpacerProps): JSX.Element => (
+const Spacer = ({ size = 1 }: ISpacerProps): JSX.Element =>
   size === 1 ? (
-    <Comp/>
+    <Comp />
   ) : (
     <>
-      {
-        '#'
-          .repeat(size)
-          .split('')
-          .map((_, i) => <Comp key={`spacer_${i}`}/>)
-      }
+      {'#'
+        .repeat(size)
+        .split('')
+        .map((_, i) => (
+          <Comp key={`spacer_${i}`} />
+        ))}
     </>
-  )
-);
+  );
 
 export default Spacer;
