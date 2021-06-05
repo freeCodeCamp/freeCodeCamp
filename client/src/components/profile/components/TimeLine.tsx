ae90387f4fb7a48494d5cad5f09778f6015d9f6a
Dripcoding <dripcoding@gmail.com>
2021-06-04 20:21:06 -0700

    feat: migrate TimeLine to typescript

--- client/src/components/profile/components/TimeLine.tsx
+++ client/src/components/profile/components/TimeLine.tsx
@@ -1,5 +1,4 @@
 import React, { Component, useMemo } from 'react';
-import PropTypes from 'prop-types';
 import { reverse, sortBy } from 'lodash-es';
 import {
   Button,
@@ -9,7 +8,7 @@ import {
   MenuItem
 } from '@freecodecamp/react-bootstrap';
 import { useStaticQuery, graphql } from 'gatsby';
-import { withTranslation } from 'react-i18next';
+import { TFunction, withTranslation } from 'react-i18next';
 
 import './timeline.css';
 import TimelinePagination from './TimelinePagination';
@@ -25,59 +24,73 @@ import { maybeUrlRE } from '../../../utils';
 import CertificationIcon from '../../../assets/icons/CertificationIcon';
 
 import { langCodes } from '../../../../../config/i18n/all-langs';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import envData from '../../../../../config/env.json';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const { clientLocale } = envData;
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
 const localeCode = langCodes[clientLocale];
 
 // Items per page in timeline.
 const ITEMS_PER_PAGE = 15;
 
-const propTypes = {
-  completedMap: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      completedDate: PropTypes.number,
-      challengeType: PropTypes.number,
-      solution: PropTypes.string,
-      files: PropTypes.arrayOf(
-        PropTypes.shape({
-          ext: PropTypes.string,
-          contents: PropTypes.string
-        })
-      )
-    })
-  ),
-  t: PropTypes.func.isRequired,
-  username: PropTypes.string
-};
+interface IFile {
+  ext: string;
+  contents: string;
+}
 
-const innerPropTypes = {
-  ...propTypes,
-  idToNameMap: PropTypes.objectOf(
-    PropTypes.shape({
-      challengePath: PropTypes.string,
-      challengeTitle: PropTypes.string
-    })
-  ).isRequired,
-  sortedTimeline: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      completedDate: PropTypes.number,
-      files: PropTypes.arrayOf(
-        PropTypes.shape({
-          ext: PropTypes.string,
-          contents: PropTypes.string
-        })
-      )
-    })
-  ).isRequired,
-  totalPages: PropTypes.number.isRequired
-};
+interface ICompletedMap {
+  id: string;
+  completedDate: number;
+  challengeType: number;
+  solution: string;
+  files: IFile[];
+  githubLink: string;
+}
 
-class TimelineInner extends Component {
-  constructor(props) {
+interface ITimelineProps {
+  completedMap: ICompletedMap[];
+  t: TFunction<'translation'>;
+  username: string;
+}
+
+interface IFile {
+  ext: string;
+  contents: string;
+}
+
+interface ISortedTimeline {
+  id: string;
+  completedDate: number;
+  files: IFile[];
+  githubLink: string;
+  solution: string;
+}
+
+interface ITimelineInnerProps extends ITimelineProps {
+  idToNameMap: Map<string, string>;
+  sortedTimeline: ISortedTimeline[];
+  totalPages: number;
+}
+
+interface ITimeLineInnerState {
+  solutionToView: string | null;
+  solutionOpen: boolean;
+  pageNo: number;
+  solution: string | null;
+  files: IFile[] | null;
+}
+
+class TimelineInner extends Component<
+  ITimelineInnerProps,
+  ITimeLineInnerState
+> {
+  constructor(props: ITimelineInnerProps) {
     super(props);
 
     this.state = {
@@ -98,7 +111,12 @@ class TimelineInner extends Component {
     this.renderViewButton = this.renderViewButton.bind(this);
   }
 
-  renderViewButton(id, files, githubLink, solution) {
+  renderViewButton(
+    id: string,
+    files: IFile[],
+    githubLink: string,
+    solution: string
+  ): React.ReactNode {
     const { t } = this.props;
     if (files && files.length) {
       return (
@@ -160,10 +178,12 @@ class TimelineInner extends Component {
     }
   }
 
-  renderCompletion(completed) {
+  renderCompletion(completed: ISortedTimeline): JSX.Element {
     const { idToNameMap, username } = this.props;
     const { id, files, githubLink, solution } = completed;
     const completedDate = new Date(completed.completedDate);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const { challengeTitle, challengePath, certPath } = idToNameMap.get(id);
     return (
       <tr className='timeline-row' key={id}>
@@ -194,7 +214,7 @@ class TimelineInner extends Component {
       </tr>
     );
   }
-  viewSolution(id, solution, files) {
+  viewSolution(id: string, solution: string, files: IFile[]): void {
     this.setState(state => ({
       ...state,
       solutionToView: id,
@@ -281,13 +301,17 @@ class TimelineInner extends Component {
             <Modal.Header closeButton={true}>
               <Modal.Title id='contained-modal-title'>
                 {`${username}'s Solution to ${
+                  // @ts-ignore
                   idToNameMap.get(id).challengeTitle
                 }`}
               </Modal.Title>
             </Modal.Header>
             <Modal.Body>
+              {/* @ts-ignore */}
               <SolutionViewer
+                // @ts-ignore
                 files={this.state.files}
+                // @ts-ignore
                 solution={this.state.solution}
               />
             </Modal.Body>
@@ -311,9 +335,7 @@ class TimelineInner extends Component {
   }
 }
 
-TimelineInner.propTypes = innerPropTypes;
-
-function useIdToNameMap() {
+function useIdToNameMap(): Map<string, string> {
   const {
     allChallengeNode: { edges }
   } = useStaticQuery(graphql`
@@ -341,8 +363,11 @@ function useIdToNameMap() {
   edges.forEach(
     ({
       node: {
+        // @ts-ignore
         id,
+        // @ts-ignore
         title,
+        // @ts-ignore
         fields: { slug }
       }
     }) => {
@@ -352,7 +377,7 @@ function useIdToNameMap() {
   return idToNameMap;
 }
 
-const Timeline = props => {
+const Timeline = (props: ITimelineProps): JSX.Element => {
   const idToNameMap = useIdToNameMap();
   const { completedMap } = props;
   // Get the sorted timeline along with total page count.
@@ -374,8 +399,6 @@ const Timeline = props => {
     />
   );
 };
-
-Timeline.propTypes = propTypes;
 
 Timeline.displayName = 'Timeline';
 
