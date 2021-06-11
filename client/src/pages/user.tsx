1d27732bf590b3ba465f0ddc534a5c80fdd772e1
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-06-10 22:36:44 -0700

    Migrated user to TS

--- client/src/pages/user.tsx
+++ client/src/pages/user.tsx
@@ -5,10 +5,12 @@ import { withPrefix } from 'gatsby';
 import RedirectHome from '../components/RedirectHome';
 import ShowUser from '../client-only-routes/show-user';
 
-function User() {
+function User(): JSX.Element {
   return (
     <Router>
       <ShowUser path={withPrefix('/user/:username/report-user')} />
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
       <RedirectHome default={true} />
     </Router>
   );
