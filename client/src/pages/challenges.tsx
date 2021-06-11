f187d72ad307251b1577165c3ab0d0c776a61008
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-06-10 17:23:37 -0700

    Partially migrated challenges to TS
    TODO: Ask about Redirect props

--- client/src/pages/challenges.tsx
+++ client/src/pages/challenges.tsx
@@ -5,21 +5,39 @@ import { navigate, withPrefix } from 'gatsby';
 
 import toLearnPath from '../utils/to-learn-path';
 
-const Redirect = props => {
+// interface RedirectProps1 {
+//   superBlock: string;
+//   block: string;
+//   challenge: string;
+// }
+
+// interface RedirectProps2 {
+//   path?: string;
+//   default?: boolean;
+// }
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+function Redirect(props) {
   if (typeof window !== 'undefined') {
-    navigate(toLearnPath(props));
+    void navigate(toLearnPath(props));
   }
   return null;
-};
+}
+// Unsure about Redirect props shape:
+// toLearnPath() takes required superBlock, block, and challenge props
+// but usage below has optional path and default props
 
-const Challenges = () => (
-  <Router basepath={withPrefix('/challenges')}>
-    <Redirect path='/:superBlock/' />
-    <Redirect path='/:superBlock/:block/' />
-    <Redirect path='/:superBlock/:block/:challenge' />
-    <Redirect default={true} />
-  </Router>
-);
+function Challenges(): JSX.Element {
+  return (
+    <Router basepath={withPrefix('/challenges')}>
+      <Redirect path='/:superBlock/' />
+      <Redirect path='/:superBlock/:block/' />
+      <Redirect path='/:superBlock/:block/:challenge' />
+      <Redirect default={true} />
+    </Router>
+  );
+}
 
 Challenges.displayName = 'Challenges';
 
