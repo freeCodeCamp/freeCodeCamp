dee6081f45f996b068bd612779963f93ca0fffd1
PrashantSakre <prashantsakre@Prashants-mac-mini.local>
2021-06-15 22:29:12 +0530

    fix(client): Change cert name to "Front End Development Libraries" on settings page

--- client/src/resources/certAndProjectMap.js
+++ client/src/resources/certAndProjectMap.js
@@ -361,7 +361,7 @@ const certMap = [
   },
   {
     id: '561acd10cb82ac38a17513bc',
-    title: 'Front End Libraries',
+    title: 'Front End Development Libraries',
     certSlug: 'front-end-libraries',
     flag: 'isFrontEndLibsCert',
     projects: [
