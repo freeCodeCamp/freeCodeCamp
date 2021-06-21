f6d2a86b8ae40c5595971faf30454c9f7bd7da5c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-21 17:01:01 +0200

    test: add more settings tests

--- cypress/integration/settings/settings.js
+++ cypress/integration/settings/settings.js
@@ -1,8 +1,17 @@
 /* global cy */
 
 describe('Settings', () => {
-  it('should be possible to reset your progress', () => {
+  beforeEach(() => {
+    cy.visit('/learn');
+    // check we've arrived
+    cy.contains('Please slow down and read this.');
     cy.login();
+    cy.contains('Menu').click();
+    cy.contains('Sign out').click();
+    // check we've arrived
+    cy.contains("Get started (it's free)");
+  });
+  it('should be possible to reset your progress', () => {
     cy.visit('/settings');
     cy.contains('Reset all of my progress').click();
     cy.contains('Reset everything. I want to start from the beginning').click();
@@ -10,5 +19,10 @@ describe('Settings', () => {
       expect(loc.pathname).to.eq('/');
     });
     cy.contains('Your progress has been reset');
+  });
+  it('should automatically log you in when you navigate to /settings', () => {
+    cy.visit('/settings');
+    cy.url().should('match', /settings/);
+    cy.contains('Account Settings');
   });
 });
