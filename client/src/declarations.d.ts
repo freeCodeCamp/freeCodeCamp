ddb8d2a7cf287898d1d4470adfd430762e59eee3
Niraj Nandish <nirajnandish@icloud.com>
2021-06-21 23:03:33 +0400

    feat(client): add required types packages

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,3 +1,6 @@
 // eslint-disable-next-line import/unambiguous
 declare module '@freecodecamp/react-bootstrap';
 declare module '@freecodecamp/strip-comments';
+declare module '@types/react-redux';
+declare module '@types/validator';
+declare module '@types/lodash-es';
