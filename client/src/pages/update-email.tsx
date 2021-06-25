e54e2588bfcbf4bb0c40111bb73c17c378f1068e
awu43 <46470763+awu43@users.noreply.github.com>
2021-06-25 08:32:57 -0700

    feat(client): ts-migrate client/src/pages (#42445)
    * Renamed .js files to .tsx
    
    * Migrated 404 to TS
    
    TODO: Ask about default prop
    
    * Migrated certification to TS
    
    Converted to functional component
    
    * Partially migrated challenges to TS
    
    TODO: Ask about Redirect props
    
    * Installed @types/react-helmet and @types/react-redux
    
    Prep for migrating donate, this caused two new TS errors on 404 and certification
    
    * Migrated donate to TS
    
    TODO: Ask about prop spreading
    
    * Migrated email-sign-up to TS
    
    Converted to functional component and removed unused isSignedIn prop
    
    * Migrated index to TS
    
    Removed unused props
    
    * Migrated learn to TS
    
    * Installed @types/react-instantsearch-dom
    
    Prep for migrating search
    
    * Migrated search to TS
    
    Converted to functional component
    
    * Migrated settings to TS
    
    * Migrated unsubscribed to TS
    
    * Installed @types/validator and @types/lodash-es
    
    Prep for migrating update-email
    
    * Migrated update-email to TS
    
    Converted to functional component
    
    * Migrated user to TS
    
    * Updated effect hook dependencies
    
    Also removed unnecessary comments from 404
    
    * Renamed challenges.test.tsx to .ts
    
    * remove search.tsx, as search.js was removed
    
    * revert: packages
    
    * revert: packages
    
    Co-authored-by: Shaun Hamilton <shauhami020@gmail.com>
    Co-authored-by: moT01 <20648924+moT01@users.noreply.github.com>

---
+++ client/src/pages/update-email.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import type { FormEvent, ChangeEvent } from 'react';
+import { Link } from 'gatsby';
+import { bindActionCreators } from 'redux';
+import type { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Grid,
+  Row,
+  Col,
+  Button
+} from '@freecodecamp/react-bootstrap';
+import Helmet from 'react-helmet';
+import isEmail from 'validator/lib/isEmail';
+import { isString } from 'lodash-es';
+import { withTranslation } from 'react-i18next';
+
+import { Spacer } from '../components/helpers';
+import './update-email.css';
+import { userSelector } from '../redux';
+import { updateMyEmail } from '../redux/settings';
+import { maybeEmailRE } from '../utils';
+
+interface UpdateEmailProps {
+  isNewEmail: boolean;
+  t: (s: string) => string;
+  updateMyEmail: (e: string) => void;
+}
+
+const mapStateToProps = createSelector(
+  userSelector,
+  ({ email, emailVerified }: { email: string; emailVerified: boolean }) => ({
+    isNewEmail: !email || emailVerified
+  })
+);
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators({ updateMyEmail }, dispatch);
+
+function UpdateEmail({ isNewEmail, t, updateMyEmail }: UpdateEmailProps) {
+  const [emailValue, setEmailValue] = useState('');
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    updateMyEmail(emailValue);
+  }
+
+  function onChange(event: ChangeEvent) {
+    const change = (event.target as HTMLInputElement).value;
+    if (!isString(change)) {
+      return null;
+    }
+    setEmailValue(change);
+    return null;
+  }
+
+  function getEmailValidationState() {
+    if (maybeEmailRE.test(emailValue)) {
+      return isEmail(emailValue) ? 'success' : 'error';
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>{t('misc.update-email-1')} | freeCodeCamp.org</title>
+      </Helmet>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <Spacer />
+      <h2 className='text-center'>{t('misc.update-email-2')}</h2>
+      <Grid>
+        <Row>
+          <Col sm={6} smOffset={3}>
+            <Row>
+              <Form horizontal={true} onSubmit={handleSubmit}>
+                <FormGroup
+                  controlId='emailInput'
+                  validationState={getEmailValidationState()}
+                >
+                  <Col
+                    className='email-label'
+                    // TODO
+                    componentClass={ControlLabel as unknown}
+                    sm={2}
+                  >
+                    {t('misc.email')}
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl
+                      onChange={onChange}
+                      placeholder='camperbot@example.com'
+                      required={true}
+                      type='email'
+                    />
+                  </Col>
+                </FormGroup>
+                <Button
+                  block={true}
+                  bsSize='lg'
+                  bsStyle='primary'
+                  disabled={getEmailValidationState() !== 'success'}
+                  type='submit'
+                >
+                  {isNewEmail
+                    ? t('buttons.update-email')
+                    : t('buttons.verify-email')}
+                </Button>
+              </Form>
+              <p className='text-center'>
+                <Link to='/signout'>{t('buttons.sign-out')}</Link>
+              </p>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    </>
+  );
+}
+
+UpdateEmail.displayName = 'Update-Email';
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(UpdateEmail));
