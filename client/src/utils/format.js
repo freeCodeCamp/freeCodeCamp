0a8f7f075e1d11d15a9fb6ff5aedf46e1bff317e
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-07-06 12:22:03 -0700

    revert format to JS

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -1,6 +1,6 @@
 import { inspect } from 'util';
 
-export function format(x: unknown): string {
+export function format(x) {
   // we're trying to mimic console.log, so we avoid wrapping strings in quotes:
   if (typeof x === 'string') return x;
   return inspect(x);
