957a0b255eeb9e896224699a6fe821d54764f343
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-07-14 19:04:56 -0700

    refactor(client): config-for-test to typescript

--- client/i18n/config-for-tests.ts
+++ client/i18n/config-for-tests.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18n.use(initReactI18next).init({
   debug: true,
   defaultNS: 'translations',
