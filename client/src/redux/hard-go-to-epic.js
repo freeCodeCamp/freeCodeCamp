49b4e1f2b3220dcd93255ea171cccfe40d1fa802
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-07-27 17:35:14 +0200

    fix: remove HOME_PATH (#43034)
    It was only used as a default for hardGoTo, but that function is only
    called once and does not need the default

--- client/src/redux/hard-go-to-epic.js
+++ client/src/redux/hard-go-to-epic.js
@@ -1,4 +1,3 @@
-/* global HOME_PATH */
 import { ofType } from 'redux-observable';
 import { tap, ignoreElements } from 'rxjs/operators';
 
@@ -7,7 +6,7 @@ import { types } from './';
 export default function hardGoToEpic(action$, _, { location }) {
   return action$.pipe(
     ofType(types.hardGoTo),
-    tap(({ payload = HOME_PATH }) => {
+    tap(({ payload }) => {
       location.href = payload;
     }),
     ignoreElements()
