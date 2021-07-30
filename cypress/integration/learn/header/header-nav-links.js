6d0b33cf0944b1e099db2995bf95ccfa5402d93d
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-07-30 10:54:11 -0700

    fix(tools): use single it

--- cypress/integration/learn/header/header-nav-links.js
+++ cypress/integration/learn/header/header-nav-links.js
@@ -1,22 +1,13 @@
 /* global cy */
 
 describe('Navigation links', () => {
-  it('should render menu button', () => {
+  it('should render the expected forum and news links.', () => {
     cy.visit('/learn');
     cy.get('.toggle-button-nav').should('be.visible');
-  });
-
-  it('should have forum link', () => {
-    cy.visit('/learn');
     cy.get('.toggle-button-nav').click();
     cy.get('.nav-list')
       .contains('Forum')
       .should('have.attr', 'href', 'https://forum.freecodecamp.org/');
-  });
-
-  it('should have news link', () => {
-    cy.visit('/learn');
-    cy.get('.toggle-button-nav').click();
     cy.get('.nav-list')
       .contains('News')
       .should('have.attr', 'href', 'https://freecodecamp.org/news/');
