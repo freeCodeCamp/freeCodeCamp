44eb2369555c969b205db9e19cbac9c9f1f7ff2e
moT01 <20648924+moT01@users.noreply.github.com>
2021-08-03 16:01:36 -0500

    feat(curriculum): add icon for rdbms

---
+++ client/src/assets/icons/Database-icon.tsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+function DatabaseIcon(
+  props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <span className='sr-only'>{t('icons.database')}</span>
+      <svg
+        height='300'
+        preserveAspectRatio='xMidYMid meet'
+        version='1.1'
+        viewBox='99 39 204 304'
+        width='200'
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        {...props}
+      >
+        <defs>
+          <path
+            d='M300 103.16C300 120.59 255.19 134.74 200 134.74C144.81 134.74 100 120.59 100 103.16C100 85.73 144.81 71.58 200 71.58C255.19 71.58 300 85.73 300 103.16Z'
+            id='b6zIOCWcs8'
+          />
+          <path
+            d='M100 103.16L300 103.16L300 150.53L100 150.53L100 103.16Z'
+            id='b11SHPoncU'
+          />
+          <path
+            d='M300 87.37C300 104.8 255.19 118.95 200 118.95C144.81 118.95 100 104.8 100 87.37C100 69.94 144.81 55.79 200 55.79C255.19 55.79 300 69.94 300 87.37Z'
+            id='a3N68R6LQ'
+          />
+          <path
+            d='M300 71.58C300 89.01 255.19 103.16 200 103.16C144.81 103.16 100 89.01 100 71.58C100 54.15 144.81 40 200 40C255.19 40 300 54.15 300 71.58Z'
+            id='b1EF3WGwOS'
+          />
+          <path
+            d='M300 182.11C300 199.53 255.19 213.68 200 213.68C144.81 213.68 100 199.53 100 182.11C100 164.68 144.81 150.53 200 150.53C255.19 150.53 300 164.68 300 182.11Z'
+            id='b39qGnN3c'
+          />
+          <path
+            d='M100 182.11L300 182.11L300 229.47L100 229.47L100 182.11Z'
+            id='i42pZSRmoV'
+          />
+          <path
+            d='M300 166.32C300 183.74 255.19 197.89 200 197.89C144.81 197.89 100 183.74 100 166.32C100 148.89 144.81 134.74 200 134.74C255.19 134.74 300 148.89 300 166.32Z'
+            id='ejrkTr2Ls'
+          />
+          <path
+            d='M300 150.53C300 167.96 255.19 182.11 200 182.11C144.81 182.11 100 167.96 100 150.53C100 133.1 144.81 118.95 200 118.95C255.19 118.95 300 133.1 300 150.53Z'
+            id='b6uTehlJg'
+          />
+          <path
+            d='M300 261.05C300 278.48 255.19 292.63 200 292.63C144.81 292.63 100 278.48 100 261.05C100 243.62 144.81 229.47 200 229.47C255.19 229.47 300 243.62 300 261.05Z'
+            id='gEKK01Fz6'
+          />
+          <path
+            d='M100 261.05L300 261.05L300 308.42L100 308.42L100 261.05Z'
+            id='biyMS5phv'
+          />
+          <path
+            d='M300 245.26C300 262.69 255.19 276.84 200 276.84C144.81 276.84 100 262.69 100 245.26C100 227.83 144.81 213.68 200 213.68C255.19 213.68 300 227.83 300 245.26Z'
+            id='arj70FYhm'
+          />
+          <path
+            d='M300 229.47C300 246.9 255.19 261.05 200 261.05C144.81 261.05 100 246.9 100 229.47C100 212.04 144.81 197.89 200 197.89C255.19 197.89 300 212.04 300 229.47Z'
+            id='aaazahdS'
+          />
+          <path
+            d='M300 308.42C300 325.85 255.19 340 200 340C144.81 340 100 325.85 100 308.42C100 290.99 144.81 276.84 200 276.84C255.19 276.84 300 290.99 300 308.42Z'
+            id='g1HT9VPdlO'
+          />
+        </defs>
+        <g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#b6zIOCWcs8'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#b11SHPoncU'
+            />
+          </g>
+          <g>
+            <use
+              fill='#ffffff'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#a3N68R6LQ'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#b1EF3WGwOS'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#b39qGnN3c'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#i42pZSRmoV'
+            />
+          </g>
+          <g>
+            <use
+              fill='#ffffff'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#ejrkTr2Ls'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#b6uTehlJg'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#gEKK01Fz6'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#biyMS5phv'
+            />
+          </g>
+          <g>
+            <use
+              fill='#ffffff'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#arj70FYhm'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#aaazahdS'
+            />
+          </g>
+          <g>
+            <use
+              fill='#0a0a23'
+              fillOpacity='1'
+              opacity='1'
+              xlinkHref='#g1HT9VPdlO'
+            />
+          </g>
+        </g>
+      </svg>
+    </>
+  );
+}
+
+DatabaseIcon.displayName = 'Database';
+
+export default DatabaseIcon;
