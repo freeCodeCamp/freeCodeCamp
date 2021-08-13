81611eca33042b2468bafea9337f479146ce656f
Shaun Hamilton <shauhami020@gmail.com>
2021-08-13 18:23:36 +0000

    plese cypress

--- cypress/integration/legacy/redirects/adding-development.js
+++ cypress/integration/legacy/redirects/adding-development.js
@@ -63,7 +63,7 @@ describe('Legacy redirects', () => {
     );
     cy.location().should(loc => {
       expect(loc.pathname).to.eq(
-        '/learn/back-end-development-and-apis/managing-packages-with-npm/how-to-use-package-json-the-core-of-any-node-js-project-or-npm-package/'
+        '/learn/back-end-development-and-apis/managing-packages-with-npm/how-to-use-package-json-the-core-of-any-node-js-project-or-npm-package'
       );
     });
 
