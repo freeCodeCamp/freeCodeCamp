1d0d10db67896c70eff734261e8d54ebb3b8f8ff
Sem Bauke <semboot699@gmail.com>
2021-08-22 18:05:40 +0200

    fix: use Ahmads logos

--- client/src/components/landing/components/LandingTop.js
+++ client/src/components/landing/components/LandingTop.js
@@ -40,7 +40,7 @@ function LandingTop({ page }) {
             <GoogleLogo />
             <MicrosoftLogo />
 
-            {clientLocale === 'chinese' ? (
+            {clientLocale === 'english' ? (
               <>
                 <TencentLogo />
                 <AlibabaLogo />
