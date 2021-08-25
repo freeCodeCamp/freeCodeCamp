a0e229ca6ea4ebfc1f7f72a13d5c6bb427565d58
Sem Bauke <semboot699@gmail.com>
2021-08-25 09:59:01 +0200

    feat: include chinese traditional

--- client/src/components/landing/components/LandingTop.js
+++ client/src/components/landing/components/LandingTop.js
@@ -40,7 +40,7 @@ function LandingTop({ page }) {
             <GoogleLogo />
             <MicrosoftLogo />
 
-            {clientLocale === 'chinese' ? (
+            {clientLocale === ('chinese' || 'chinese-traditional') ? (
               <>
                 <TencentLogo />
                 <AlibabaLogo />
