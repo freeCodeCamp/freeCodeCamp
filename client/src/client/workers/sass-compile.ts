c8303c687d3a3b733e8947699d5782d355fdecc5
Meron Ogbai <okbaymeron@gmail.com>
2021-08-29 02:14:57 +0300

    fix(ts-migrate): use ctx alias in sass-compile.ts

--- client/src/client/workers/sass-compile.ts
+++ client/src/client/workers/sass-compile.ts
@@ -1,8 +1,17 @@
 /* eslint-disable import/unambiguous */
 // work around for SASS error in Edge
 // https://github.com/medialize/sass.js/issues/96#issuecomment-424386171
-if (!self.crypto) {
-  (self.crypto as unknown) = {
+interface WorkerWithSass extends Worker {
+  Sass: {
+    compile(data: unknown, callback: unknown): void;
+  };
+}
+
+const ctx: WorkerWithSass & typeof globalThis =
+  self as unknown as WorkerWithSass & typeof globalThis;
+
+if (!ctx.crypto) {
+  (ctx.crypto as unknown) = {
     getRandomValues: function (array: number[]) {
       for (let i = 0, l = array.length; i < l; i++) {
         array[i] = Math.floor(Math.random() * 256);
@@ -12,29 +21,20 @@ if (!self.crypto) {
   };
 }
 
-self.importScripts('/js/sass.sync.js');
+ctx.importScripts('/js/sass.sync.js');
 
-interface WorkerWithSass {
-  Sass: {
-    compile(data: unknown, callback: unknown): void;
-  };
-}
-
-self.onmessage = e => {
+ctx.onmessage = e => {
   const data: unknown = e.data;
-  (self as WorkerGlobalScope & typeof globalThis & WorkerWithSass).Sass.compile(
-    data,
-    (result: Record<string, unknown>) => {
-      if (result.status === 0) {
-        self.postMessage(result.text);
-      } else {
-        self.postMessage({
-          type: 'error',
-          data: { message: result.formatted }
-        });
-      }
+  ctx.Sass.compile(data, (result: Record<string, unknown>) => {
+    if (result.status === 0) {
+      ctx.postMessage(result.text);
+    } else {
+      ctx.postMessage({
+        type: 'error',
+        data: { message: result.formatted }
+      });
     }
-  );
+  });
 };
 
-self.postMessage({ type: 'contentLoaded' });
+ctx.postMessage({ type: 'contentLoaded' });
