270dd564acab80fbb8a06773fced8571df7bb239
Vishwas Navada <navadavishwas@hotmail.com>
2021-09-20 18:27:41 +0530

    feat(client): ts-migrate client/src/components/formHelpers/form*

--- client/src/components/formHelpers/index.tsx
+++ client/src/components/formHelpers/index.tsx
@@ -1,27 +1,44 @@
 import normalizeUrl from 'normalize-url';
+import { ValidatedValues } from '../../templates/Challenges/projects/solution-form';
+import { FormOptions } from './form';
 import {
   localhostValidator,
   editorValidator,
   composeValidators,
   fCCValidator,
   httpValidator
-} from './FormValidators';
+} from './form-validators';
 
 export { default as BlockSaveButton } from './block-save-button';
 export { default as BlockSaveWrapper } from './block-save-wrapper';
-export { default as Form } from './Form.js';
-export { default as FormFields } from './form-fields.js';
+export { default as Form } from './form';
+export { default as FormFields } from './form-fields';
 
 const normalizeOptions = {
   stripWWW: false
 };
 
-export function formatUrlValues(values, options) {
+type validatedValuesType = {
+  errors: string[];
+  invalidValues: string[];
+  values: Record<string, unknown>;
+};
+
+export function formatUrlValues(
+  values: ValidatedValues,
+  options: FormOptions
+): validatedValuesType {
   const { isEditorLinkAllowed, isLocalLinkAllowed, types } = options;
-  const validatedValues = { values: {}, errors: [], invalidValues: [] };
+  const validatedValues: validatedValuesType = {
+    values: {},
+    errors: [],
+    invalidValues: []
+  };
   const urlValues = Object.keys(values).reduce((result, key) => {
-    let value = values[key];
-    const nullOrWarning = composeValidators(
+    // eslint-disable-next-line
+    //@ts-ignore
+    let value: string = values[key] as string;
+    const nullOrWarning: string = composeValidators(
       fCCValidator,
       httpValidator,
       isLocalLinkAllowed ? null : localhostValidator,
@@ -30,12 +47,14 @@ export function formatUrlValues(values, options) {
     if (nullOrWarning) {
       validatedValues.invalidValues.push(nullOrWarning);
     }
-    if (value && types[key] === 'url') {
+    if (value && types && types[key] === 'url') {
       try {
         value = normalizeUrl(value, normalizeOptions);
       } catch (err) {
         // Not a valid URL for testing or submission
-        validatedValues.errors.push({ error: err, value });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        validatedValues.errors.push({ error: err, value }); // eslint-disable-line  @typescript-eslint/no-unsafe-assignment
       }
     }
     return { ...result, [key]: value };
