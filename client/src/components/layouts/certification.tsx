07097835673d60781d7ffcda2ff1dd36ba8db647
moT01 <20648924+moT01@users.noreply.github.com>
2021-09-21 11:59:45 -0500

    fix: reorder imports

--- client/src/components/layouts/certification.tsx
+++ client/src/components/layouts/certification.tsx
@@ -1,5 +1,5 @@
-import Helmet from 'react-helmet';
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 
 import { createSelector } from 'reselect';
