169896503a64a5bcbe984c44753d97289b22513f
Hamza Waleed <hamza.waleed@arbisoft.com>
2021-09-23 00:43:51 +0500

    Add TS configs and improve dir structure

--- tools/ui-components/.storybook/main.js
+++ tools/ui-components/.storybook/main.js
@@ -4,5 +4,18 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-postcss'
-  ]
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
 };
