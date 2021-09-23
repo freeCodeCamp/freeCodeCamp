09402a868af6bdf73322fc9cb0e0afa4bfd32c94
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-09-23 12:21:07 +0200

    fix: make DesktopLayout aware of ActionRow
    The ReflexContainer seems to need to be inside a display: flex component
    to correctly compute the sizes of it's children.

--- client/src/templates/Challenges/classic/DesktopLayout.js
+++ client/src/templates/Challenges/classic/DesktopLayout.js
@@ -87,7 +87,7 @@ class DesktopLayout extends Component {
       layoutState;
 
     return (
-      <>
+      <div className='desktop-layout'>
         {projectBasedChallenge && (
           <ActionRow
             block={block}
@@ -96,75 +96,71 @@ class DesktopLayout extends Component {
             superBlock={superBlock}
           />
         )}
-        <ReflexContainer className='desktop-layout' orientation='horizontal'>
-          <ReflexElement flex={8} {...reflexProps} {...resizeProps}>
-            <ReflexContainer orientation='vertical'>
-              {!projectBasedChallenge && (
-                <ReflexElement
-                  flex={instructionPane.flex}
-                  name='instructionPane'
-                  {...resizeProps}
-                >
-                  {instructions}
-                </ReflexElement>
-              )}
-              {!projectBasedChallenge && (
-                <ReflexSplitter propagate={true} {...resizeProps} />
-              )}
+        <ReflexContainer orientation='vertical'>
+          {!projectBasedChallenge && (
+            <ReflexElement
+              flex={instructionPane.flex}
+              name='instructionPane'
+              {...resizeProps}
+            >
+              {instructions}
+            </ReflexElement>
+          )}
+          {!projectBasedChallenge && (
+            <ReflexSplitter propagate={true} {...resizeProps} />
+          )}
 
-              <ReflexElement
-                flex={editorPane.flex}
-                name='editorPane'
-                {...resizeProps}
+          <ReflexElement
+            flex={editorPane.flex}
+            name='editorPane'
+            {...resizeProps}
+          >
+            {challengeFile && showUpcomingChanges && !hasEditableBoundries && (
+              <EditorTabs />
+            )}
+            {challengeFile && (
+              <ReflexContainer
+                key={challengeFile.fileKey}
+                orientation='horizontal'
               >
-                {challengeFile &&
-                  showUpcomingChanges &&
-                  !hasEditableBoundries && <EditorTabs />}
-                {challengeFile && (
-                  <ReflexContainer
-                    key={challengeFile.fileKey}
-                    orientation='horizontal'
-                  >
-                    <ReflexElement
-                      flex={codePane.flex}
-                      name='codePane'
-                      {...reflexProps}
-                      {...resizeProps}
-                    >
-                      {editor}
-                    </ReflexElement>
-                    {isConsoleDisplayable && (
-                      <ReflexSplitter propagate={true} {...resizeProps} />
-                    )}
-                    {isConsoleDisplayable && (
-                      <ReflexElement
-                        flex={testsPane.flex}
-                        name='testsPane'
-                        {...reflexProps}
-                        {...resizeProps}
-                      >
-                        {testOutput}
-                      </ReflexElement>
-                    )}
-                  </ReflexContainer>
-                )}
-              </ReflexElement>
-              {isPreviewDisplayable && (
-                <ReflexSplitter propagate={true} {...resizeProps} />
-              )}
-              {isPreviewDisplayable && (
                 <ReflexElement
-                  flex={previewPane.flex}
-                  name='previewPane'
+                  flex={codePane.flex}
+                  name='codePane'
+                  {...reflexProps}
                   {...resizeProps}
                 >
-                  {preview}
+                  {editor}
                 </ReflexElement>
-              )}
-            </ReflexContainer>
+                {isConsoleDisplayable && (
+                  <ReflexSplitter propagate={true} {...resizeProps} />
+                )}
+                {isConsoleDisplayable && (
+                  <ReflexElement
+                    flex={testsPane.flex}
+                    name='testsPane'
+                    {...reflexProps}
+                    {...resizeProps}
+                  >
+                    {testOutput}
+                  </ReflexElement>
+                )}
+              </ReflexContainer>
+            )}
           </ReflexElement>
+          {isPreviewDisplayable && (
+            <ReflexSplitter propagate={true} {...resizeProps} />
+          )}
+          {isPreviewDisplayable && (
+            <ReflexElement
+              flex={previewPane.flex}
+              name='previewPane'
+              {...resizeProps}
+            >
+              {preview}
+            </ReflexElement>
+          )}
         </ReflexContainer>
-      </>
+      </div>
     );
   }
 }
