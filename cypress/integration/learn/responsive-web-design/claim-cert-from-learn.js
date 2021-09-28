ed23e6b40f9356603e64e6d2ae52bb6a75fa40ba
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-09-28 13:13:16 +0200

    test: improve claim cert tests
    Without npm run seed, retries will be in a different state.
    
    Check the scroll position to (hopefully) catch the weird cert claim bug.

--- cypress/integration/learn/responsive-web-design/claim-cert-from-learn.js
+++ cypress/integration/learn/responsive-web-design/claim-cert-from-learn.js
@@ -49,6 +49,7 @@ describe('Responsive Web Design Superblock', () => {
   });
   describe('After submitting all 5 projects', () => {
     before(() => {
+      cy.exec('npm run seed');
       cy.login();
       cy.toggleAll();
       const { superBlock, block, challenges } = projects;
@@ -81,8 +82,11 @@ describe('Responsive Web Design Superblock', () => {
       cy.contains('Ask me later').click();
       cy.get('.donation-modal').should('not.exist');
       // directed to claim-cert-block section
-      cy.contains('Claim Your Certification').should('be.visible');
       cy.url().should('include', '#claim-cert-block');
+      // make sure that the window has not snapped to the top (a weird bug that
+      // we never figured out and so could randomly reappear)
+      cy.window().its('scrollY').should('not.equal', 0);
+      cy.contains('Claim Your Certification');
       cy.contains('Claim Certification').should('not.be.disabled').click();
       cy.contains('Show Certification').click();
       cy.location().should(loc => {
