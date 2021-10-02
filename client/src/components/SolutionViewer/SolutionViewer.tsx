14609f5542ae09c287d8ca747eb9143436618bf3
Roberto Sousa <beto.sousa22@gmail.com>
2021-10-02 23:42:49 +0100

    migrate solution view

---
+++ client/src/components/SolutionViewer/SolutionViewer.tsx
@@ -0,0 +1,75 @@
+import { Panel } from '@freecodecamp/react-bootstrap';
+import Prism from 'prismjs';
+import React from 'react';
+
+import { ChallengeFile } from '../../redux/prop-types';
+
+const prismLang = {
+  css: 'css',
+  js: 'javascript',
+  jsx: 'javascript',
+  html: 'markup'
+};
+
+interface Props {
+  challengeFiles: ChallengeFile[];
+  solution: string;
+}
+
+const SolutionViewer = ({
+  challengeFiles,
+  solution = '// The solution is not available for this project'
+}: Props): JSX.Element =>
+  challengeFiles?.length > 0 ? (
+    <>
+      {challengeFiles.map(challengeFile => (
+        <Panel
+          bsStyle='primary'
+          className='solution-viewer'
+          key={challengeFile.ext}
+        >
+          <Panel.Heading>{challengeFile.ext.toUpperCase()}</Panel.Heading>
+          <Panel.Body>
+            <pre>
+              <code
+                className={`language-${prismLang[challengeFile.ext]}`}
+                dangerouslySetInnerHTML={{
+                  __html: Prism.highlight(
+                    challengeFile.contents.trim(),
+                    Prism.languages[prismLang[challengeFile.ext]],
+                    prismLang[challengeFile.ext]
+                  )
+                }}
+              />
+            </pre>
+          </Panel.Body>
+        </Panel>
+      ))}
+    </>
+  ) : (
+    <Panel
+      bsStyle='primary'
+      className='solution-viewer'
+      key={solution.slice(0, 10)}
+    >
+      <Panel.Heading>JS</Panel.Heading>
+      <Panel.Body>
+        <pre>
+          <code
+            className='language-markup'
+            dangerouslySetInnerHTML={{
+              __html: Prism.highlight(
+                solution.trim(),
+                Prism.languages.js,
+                'javascript'
+              )
+            }}
+          />
+        </pre>
+      </Panel.Body>
+    </Panel>
+  );
+
+SolutionViewer.displayName = 'SolutionViewer';
+
+export default SolutionViewer;
