79d79e167d01b663f2bce2dfc95b20b042a043f3
moT01 <20648924+moT01@users.noreply.github.com>
2021-10-03 20:23:01 -0500

    fix: remove file I didn't use

--- api-server/src/server/models/webhook-token.js
+++
@@ -1,9 +0,0 @@
-/* import { Observable } from 'rx';
-
-export default function initializeWebhookToken(WebhookToken) {
-  WebhookToken.on('dataSourceAttached', () => {
-    WebhookToken.findOne$ = Observable.fromNodeCallback(
-      WebhookToken.findOne.bind(WebhookToken)
-    );
-  });
-}*/
