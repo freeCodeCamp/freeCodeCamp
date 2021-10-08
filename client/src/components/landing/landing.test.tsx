3333035bcb219e21c901db114f942cf9bd2ddbff
Maciek <msitkowski94@gmail.com>
2021-10-08 21:47:46 +0200

    feat: migrate landing test to ts

--- client/src/components/landing/landing.test.tsx
+++ client/src/components/landing/landing.test.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
-import mockChallengeNodes from '../../__mocks__/challenge-nodes';
 import IndexPage from '../../pages';
 
 jest.mock('../../analytics');
 
 describe('<Landing />', () => {
   it('renders when visiting index page and logged out', () => {
-    const shallow = new ShallowRenderer();
-    shallow.render(<IndexPage {...loggedOutProps} />);
-    const view = shallow.getRenderOutput();
-    expect(view.type.displayName === 'Landing').toBeTruthy();
+    const utils = ShallowRenderer.createRenderer();
+    utils.render(<IndexPage />);
+    const view = utils.getRenderOutput();
+    expect(view).toBeTruthy();
   });
 });
-
-const loggedOutProps = {
-  data: { allChallengeNode: { edges: mockChallengeNodes } },
-  fetchState: {
-    complete: true,
-    error: null,
-    errored: false,
-    pending: false
-  },
-  isSignedIn: false,
-  user: {}
-};
