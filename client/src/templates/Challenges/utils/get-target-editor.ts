6237dac11e4d6da6716da3f2abde610f9371d460
Niraj Nandish <nirajnandish@icloud.com>
2021-10-10 20:52:52 +0400

    feat(client): migrate getTargetEditor

--- client/src/templates/Challenges/utils/get-target-editor.ts
+++ client/src/templates/Challenges/utils/get-target-editor.ts
@@ -1,14 +1,36 @@
 import { isEmpty } from 'lodash-es';
 import { sortChallengeFiles } from '../../../../../utils/sort-challengefiles';
 
-export function getTargetEditor(challengeFiles) {
+// Check whether to take "ext" and "fileKey" from prop-types.ts
+type ChallengeFilesType = {
+  fileKey: string;
+  head: string;
+  tail: string;
+  editableRegionBoundaries: number[];
+  history: string[];
+  name: string;
+  ext: string;
+  path: string;
+  contents: string;
+  error: string | null;
+  seed: string;
+  editableContents: string;
+  seedEditableRegionBoundaries: number[];
+};
+
+export function getTargetEditor(
+  challengeFiles: ChallengeFilesType[]
+): string | null {
   if (isEmpty(challengeFiles)) return null;
   else {
-    let targetEditor = challengeFiles.find(
+    const targetEditor = challengeFiles.find(
       ({ editableRegionBoundaries }) => !isEmpty(editableRegionBoundaries)
     )?.fileKey;
 
     // fallback for when there is no editable region.
-    return targetEditor || sortChallengeFiles(challengeFiles)[0].fileKey;
+    return (
+      targetEditor ||
+      (sortChallengeFiles(challengeFiles) as ChallengeFilesType[])[0].fileKey
+    );
   }
 }
