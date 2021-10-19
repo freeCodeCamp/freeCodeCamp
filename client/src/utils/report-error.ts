45b2b83dc16271340c3448418ffca7f8aa7feea0
Tristan Toye <tristan@careerjsm.com>
2021-10-18 21:07:01 -0400

    Add Sentry to client for error handling

--- client/src/utils/report-error.ts
+++ client/src/utils/report-error.ts
@@ -1,7 +1,13 @@
-// TODO: integrate with Sentry?
+import * as Sentry from '@sentry/react';
+import { sentry } from '../../../config/secrets';
+
 export function reportClientSideError(
   e: Error,
-  message = 'Unhandled error'
-): void {
-  return console.error(`Client: ${message}`, e);
+  message = sentry.dns === 'dsn_from_sentry_dashboard'
+    ? 'Unhandled error'
+    : 'Error sent to Sentry'
+): string | void {
+  return sentry.dns === 'dsn_from_sentry_dashboard'
+    ? console.error(`Client: ${message}`, e)
+    : Sentry.captureException(e);
 }
