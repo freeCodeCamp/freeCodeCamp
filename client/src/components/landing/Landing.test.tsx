2737a9ec794b8bd120b8ead8bca8e7e862892b54
JasmineSun33 <jasminesun19@gmail.com>
2021-10-19 09:41:31 -0400

    change to specific annotation

--- client/src/components/landing/Landing.test.tsx
+++ client/src/components/landing/Landing.test.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable */
-// @ts-nocheck Likely need to not use ShallowRenderer
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import * as ShallowRenderer from 'react-test-renderer/shallow';
 
 import mockChallengeNodes from '../../__mocks__/challenge-nodes';
 import IndexPage from '../../pages';
@@ -10,9 +8,12 @@ jest.mock('../../analytics');
 
 describe('<Landing />', () => {
   it('renders when visiting index page and logged out', () => {
-    const shallow = new ShallowRenderer();
+    // eslint-disable-next-line testing-library/render-result-naming-convention
+    const shallow = ShallowRenderer.createRenderer();
+    // @ts-expect-error Type definition mismatch
     shallow.render(<IndexPage {...loggedOutProps} />);
     const view = shallow.getRenderOutput();
+    // @ts-expect-error Type definition mismatch
     expect(view.type.displayName === 'Landing').toBeTruthy();
   });
 });
