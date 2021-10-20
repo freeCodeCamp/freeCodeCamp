d100132f75b9b3642dcc21b1960f390967a26169
Shaun Hamilton <shauhami020@gmail.com>
2021-10-20 18:58:10 +0100

    feat(client): refactor Flash/redux into TS (#42725)
    * feat(client): ts-migrate Flash/redux
    
    * add app types
    
    * convert Flash/redux/index to vanilla TS
    
    * update redux types.ts
    
    * use FlashState type over State type
    
    * update typing
    
    * fix: prettier errors I caused?
    
    * fix: re-add comment I removed
    
    * remove comment
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    * fix type to not include {}
    
    * remove commented out code for future use
    
    * remove unused initialState object
    
    * rename Flash onClose prop to match action name
    
    * directly type reducer to return state
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    Co-authored-by: moT01 <20648924+moT01@users.noreply.github.com>
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/client-only-routes/show-settings.tsx
+++ client/src/client-only-routes/show-settings.tsx
@@ -29,7 +29,7 @@ const { apiLocation } = envData;
 
 // TODO: update types for actions
 interface IShowSettingsProps {
-  createFlashMessage: (paylaod: string[]) => void;
+  createFlashMessage: typeof createFlashMessage;
   isSignedIn: boolean;
   navigate: (location: string) => void;
   showLoading: boolean;
