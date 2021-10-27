293667399931177d323ad446d38f3af95748cdb0
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-10-27 16:32:37 +0200

    test: extract-js-comments error

--- curriculum/test/utils/extract-js-comments.test.js
+++ curriculum/test/utils/extract-js-comments.test.js
@@ -13,6 +13,8 @@ var y = '// single line comment';
 
 `;
 
+const someInvalidJS = `const isChange;`;
+
 describe('extractJSComments', () => {
   it('should return an object with comment keys and count values', () => {
     const commentCounts = {
@@ -20,5 +22,14 @@ describe('extractJSComments', () => {
       'a multiline comment': 1
     };
     expect(extractJSComments(someJS)).toEqual(commentCounts);
+  });
+
+  it('should throw an informative error if the JS is invalid', () => {
+    expect(() => extractJSComments(someInvalidJS)).toThrow(
+      `extract-js-comments could not parse the code below, this challenge have invalid syntax:
+
+${someInvalidJS}
+`
+    );
   });
 });
