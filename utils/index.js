e0142b9768fc49b6438743184278078e2ea26d1c
vijaychandar30 <72193841+vijaychandar30@users.noreply.github.com>
2021-10-29 20:40:47 +0530

    Update on index.js

--- utils/index.js
+++ utils/index.js
@@ -47,3 +47,15 @@ for (const [id, title] of idToTitle) {
 export const getCertIds = () => idToPath.keys();
 export const getPathFromID = id => idToPath.get(id);
 export const getTitleFromId = id => idToTitle.get(id);
+
+#my changes
+Here's our logo (hover to see the title text):
+
+Inline-style: 
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+
+Reference-style: 
+![alt text][logo]
+
+[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
+
