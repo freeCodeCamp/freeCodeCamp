f3585965277589d501e758453e015bb23c11d5d9
KushGabani <kushgabz2687@gmail.com>
2021-11-06 18:03:01 +0530

    tsmigrate: client/src/__tests__/integration/handled-error.test.js --> .ts

--- client/src/__tests__/integration/handled-error.test.ts
+++ client/src/__tests__/integration/handled-error.test.ts
@@ -1,37 +1,49 @@
 import {
   wrapHandledError,
-  unwrapHandledError
+  unwrapHandledError,
+  HandledError,
+  ErrorData
 } from '../../utils/handled-error';
 
+type handledType = {
+  type?: string;
+  message?: string;
+  redirectTo?: string;
+};
+
 describe('handled-error integration', () => {
-  const handledA = {
+  const handledA: handledType = {
     type: 'info',
     message: 'something helpful',
     redirectTo: '/a-path-we-choose'
   };
-  const handledB = {
+  const handledB: handledType = {
     type: 'danger',
     message: 'Oh noes!',
     redirectTo: '/whoops'
   };
-  const handledC = {
+  const handledC: handledType = {
     type: 'success',
     message: 'great news!',
     redirectTo: '/awesome'
   };
-  const handledD = {};
+  const handledD: handledType = {};
 
   it('can wrap and unwrap handled errors', () => {
     expect.assertions(4);
-    const wrappedA = wrapHandledError(new Error(), handledA);
-    const wrappedB = wrapHandledError(new Error(), handledB);
-    const wrappedC = wrapHandledError(new Error(), handledC);
-    const wrappedD = wrapHandledError(new Error(), handledD);
+    const wrappedA: HandledError = wrapHandledError(new Error(), handledA);
+    const wrappedB: HandledError = wrapHandledError(new Error(), handledB);
+    const wrappedC: HandledError = wrapHandledError(new Error(), handledC);
+    const wrappedD: HandledError = wrapHandledError(new Error(), handledD);
 
-    const unwrappedA = unwrapHandledError(wrappedA);
-    const unwrappedB = unwrapHandledError(wrappedB);
-    const unwrappedC = unwrapHandledError(wrappedC);
-    const unwrappedD = unwrapHandledError(wrappedD);
+    const unwrappedA: ErrorData | Record<string, never> =
+      unwrapHandledError(wrappedA);
+    const unwrappedB: ErrorData | Record<string, never> =
+      unwrapHandledError(wrappedB);
+    const unwrappedC: ErrorData | Record<string, never> =
+      unwrapHandledError(wrappedC);
+    const unwrappedD: ErrorData | Record<string, never> =
+      unwrapHandledError(wrappedD);
 
     expect(unwrappedA).toEqual(handledA);
     expect(unwrappedB).toEqual(handledB);
