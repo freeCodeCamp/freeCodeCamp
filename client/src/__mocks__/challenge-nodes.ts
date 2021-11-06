c9d51736e2881eb1a5480c090bf086adb8c4b631
KushGabani <kushgabz2687@gmail.com>
2021-11-06 17:32:52 +0530

    tsmigrate: client/src/__mocks__/challenge-nodes.js -> /ts

--- client/src/__mocks__/challenge-nodes.ts
+++ client/src/__mocks__/challenge-nodes.ts
@@ -1,4 +1,17 @@
-const mockChallengeNodes = [
+interface mockChallengeNodesType {
+  fields: {
+    slug: string;
+    blockName: string;
+  };
+  id: string;
+  block: string;
+  title: string;
+  isPrivate: boolean;
+  superBlock: string;
+  dashedName: string;
+}
+
+const mockChallengeNodes: mockChallengeNodesType[] = [
   {
     fields: {
       slug: '/super-block-one/block-a/challenge-one',
