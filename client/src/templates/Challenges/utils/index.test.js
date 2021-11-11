7e2c1eb57b500f098869193d5c4952f3faf29ccc
Shaun Hamilton <shauhami020@gmail.com>
2021-11-11 10:53:32 +0000

    add tests for removeUrlParamters

--- client/src/templates/Challenges/utils/index.test.js
+++ client/src/templates/Challenges/utils/index.test.js
@@ -1,8 +1,13 @@
 import envData from '../../../../../config/env.json';
 
-const { forumLocation } = envData;
+const { forumLocation, homeLocation } = envData;
 
-const { getGuideUrl } = require('./');
+const { getGuideUrl, removeUrlParameters } = require('./');
+
+const exampleUrls = {
+  plainLesson: `${homeLocation}/learn/back-end-development-and-apis/mongodb-and-mongoose/install-and-set-up-mongoose`,
+  paramLesson: `${homeLocation}/learn/back-end-development-and-apis/mongodb-and-mongoose/install-and-set-up-mongoose?messages=success%5B0%5D%3Dflash.signin-success`
+};
 
 describe('index', () => {
   describe('getGuideUrl', () => {
@@ -21,6 +26,16 @@ describe('index', () => {
       expect(value).toEqual(
         `${forumLocation}/search?q=%26%20a%20sample%20title%3F%20in%3Atitle%20order%3Aviews`
       );
+    });
+  });
+  describe('removeUrlParameters', () => {
+    it('should not change a url without parameters', () => {
+      const value = removeUrlParameters(exampleUrls.plainLesson);
+      expect(value).toEqual(exampleUrls.plainLesson);
+    });
+    it('should remove only url parameters', () => {
+      const value = removeUrlParameters(exampleUrls.paramLesson);
+      expect(value).toEqual(exampleUrls.plainLesson);
     });
   });
 });
