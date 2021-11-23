68fbc9753896971558420b1874b08f6e13d28d07
Fabien <fabien-luc.lallemand@epitech.eu>
2021-11-23 19:07:48 +0400

    refactor: refactor the file landing/index.js into typescript

--- client/src/components/landing/components/certifications.tsx
+++ client/src/components/landing/components/certifications.tsx
@@ -6,7 +6,7 @@ import { Spacer } from '../../helpers';
 import BigCallToAction from './big-call-to-action';
 
 interface CertificationProps {
-  pageName: string;
+  pageName?: string;
 }
 
 const Certifications = ({
