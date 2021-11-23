26abd7bea35506083add3b66ea224cb89c05e881
moT01 <20648924+moT01@users.noreply.github.com>
2021-11-23 12:15:53 -0600

    fix: clean up notes components and use prism formatting

--- client/src/templates/Challenges/components/notes.tsx
+++ client/src/templates/Challenges/components/notes.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-// import { withTranslation } from 'react-i18next';
-// import { connect } from 'react-redux';
-// import { bindActionCreators, Dispatch } from 'redux';
-
-// import PrismFormatted from './prism-formatted';
-// import './notes.css';
-
-// const mainId = 'fcc-main-frame';
-
-/* const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      previewMounted
-    },
-    dispatch
-  );*/
+import PrismFormatted from './prism-formatted';
 
 interface NotesProps {
   notes?: string;
 }
 
 function Notes({ notes }: NotesProps): JSX.Element {
-  return <>{notes && <div dangerouslySetInnerHTML={{ __html: notes }} />}</>;
+  return <>{notes && <PrismFormatted text={notes} />}</>;
 }
 
 Notes.displayName = 'Notes';
