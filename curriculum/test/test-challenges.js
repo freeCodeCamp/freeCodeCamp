1729fc02b9f126652028b2a9a130cd37c2bd8bd2
moT01 <20648924+moT01@users.noreply.github.com>
2021-11-23 18:08:01 -0600

    he's heating up

--- curriculum/test/test-challenges.js
+++ curriculum/test/test-challenges.js
@@ -299,7 +299,7 @@ function populateTestsForLang({ lang, challenges, meta }) {
   });
 
   describe(`Check challenges (${lang})`, function () {
-    this.timeout(5000);
+    this.timeout(10000);
     challenges.forEach((challenge, id) => {
       const dashedBlockName = challenge.block;
       // TODO: once certifications are not included in the list of challenges,
