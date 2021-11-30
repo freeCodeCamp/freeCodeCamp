5b61c8a56f4af7a00b9350e97ae80ccfc69d41b0
Vinay <79531287+foolhardy21@users.noreply.github.com>
2021-11-30 22:07:00 +0530

    refactor(client): ts-migration of i18n/validate-keys.js (#44292)
    * refactor - file extenstion change to .ts
    
    * fix - imports syntax
    
    * fix - migration complete
    
    * fix - lint:prettier warn
    
    * refactor - validate-keys in tools/scripts/lint/
    
    * refactor - validate-keys.ts moved to tools/scripts/lint and added in tsconfig.json
    
    * refactor - tsconfig.json file for tools
    
    * fix - lint:ts script update and validate-keys.js added to .eslintignore
    
    * fix - lint:prettier and ts compilation
    
    * fix - ts compilation
    
    * refactor - changes in .gitignore and validate-keys script

--- tools/scripts/lint/validate-keys.ts
+++ tools/scripts/lint/validate-keys.ts
@@ -1,28 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const introObject = require('./locales/english/intro.json');
-const linksObject = require('./locales/english/links.json');
-const metaObject = require('./locales/english/meta-tags.json');
-const motivationObject = require('./locales/english/motivation.json');
-const translationsObject = require('./locales/english/translations.json');
-const trendingObject = require('./locales/english/trending.json');
+import fs from 'fs';
+import path from 'path';
+import introObject from '../../../client/i18n/locales/english/intro.json';
+import linksObject from '../../../client/i18n/locales/english/links.json';
+import metaObject from '../../../client/i18n/locales/english/meta-tags.json';
+import motivationObject from '../../../client/i18n/locales/english/motivation.json';
+import translationsObject from '../../../client/i18n/locales/english/translations.json';
+import trendingObject from '../../../client/i18n/locales/english/trending.json';
 
 /**
  * Function to flatten a nested object. Written specifically for
  * our translation flow, the `namespace` value is used to create the
  * property chains that are used in the i18n replacement scripts.
- * @param {Object} obj
- * @param {string} namespace
  */
-const flattenAnObject = (obj, namespace = '') => {
-  const flattened = {};
-  Object.keys(obj).forEach(key => {
+const flattenAnObject = (
+  obj: Record<string, unknown>,
+  namespace = ''
+): Record<string, unknown> => {
+  const flattened: Record<string, unknown> = {};
+  Object.keys(obj).forEach((key: string) => {
     if (Array.isArray(obj[key])) {
       flattened[namespace ? `${namespace}.${key}` : key] = obj[key];
     } else if (typeof obj[key] === 'object') {
       Object.assign(
         flattened,
-        flattenAnObject(obj[key], namespace ? `${namespace}.${key}` : key)
+        flattenAnObject(
+          obj[key] as Record<string, unknown>,
+          namespace ? `${namespace}.${key}` : key
+        )
       );
     } else {
       flattened[namespace ? `${namespace}.${key}` : key] = obj[key];
@@ -31,25 +35,26 @@ const flattenAnObject = (obj, namespace = '') => {
   return flattened;
 };
 
-const translationKeys = Object.keys(flattenAnObject(translationsObject));
-const metaKeys = Object.keys(flattenAnObject(metaObject));
-const motivationKeys = Object.keys(flattenAnObject(motivationObject));
-const introKeys = Object.keys(flattenAnObject(introObject));
-const trendingKeys = Object.keys(flattenAnObject(trendingObject));
-const linksKeys = Object.keys(flattenAnObject(linksObject));
+const translationKeys: string[] = Object.keys(
+  flattenAnObject(translationsObject)
+);
+const metaKeys: string[] = Object.keys(flattenAnObject(metaObject));
+const motivationKeys: string[] = Object.keys(flattenAnObject(motivationObject));
+const introKeys: string[] = Object.keys(flattenAnObject(introObject));
+const trendingKeys: string[] = Object.keys(flattenAnObject(trendingObject));
+const linksKeys: string[] = Object.keys(flattenAnObject(linksObject));
 
 /**
  * Recursively read through the directory, grabbing .js files
  * in each nested subdirectory and concatenating them all in
  * to one string.
- * @param {String} filePath
  */
-const readComponentCode = filePath => {
+const readComponentCode = (filePath: string): string => {
   let code = '';
-  const isItFolder = fs.lstatSync(filePath).isDirectory();
+  const isItFolder: boolean = fs.lstatSync(filePath).isDirectory();
   if (isItFolder) {
-    const contents = fs.readdirSync(filePath);
-    contents.forEach(file => {
+    const contents: string[] = fs.readdirSync(filePath);
+    contents.forEach((file: string) => {
       code += readComponentCode(path.join(filePath + '/' + file));
     });
   } else {
@@ -61,8 +66,10 @@ const readComponentCode = filePath => {
   return code;
 };
 
-const clientCodebase = readComponentCode(path.join(process.cwd() + '/src'));
-const serverCodebase = readComponentCode(
+const clientCodebase: string = readComponentCode(
+  path.join(process.cwd() + '/src')
+);
+const serverCodebase: string = readComponentCode(
   path.join(process.cwd() + '/../api-server/src/server')
 );
 
