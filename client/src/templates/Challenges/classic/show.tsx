748d1dd2a66be13905fce0e34d88fb9049d06a5a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-11-30 15:27:20 +0100

    fix: use the previewMounted connected to redux

--- client/src/templates/Challenges/classic/show.tsx
+++ client/src/templates/Challenges/classic/show.tsx
@@ -103,6 +103,7 @@ interface ShowClassicProps {
   updateChallengeMeta: (arg0: ChallengeMeta) => void;
   openModal: (modal: string) => void;
   setEditorFocusability: (canFocus: boolean) => void;
+  previewMounted: () => void;
 }
 
 interface ShowClassicState {
@@ -378,7 +379,7 @@ class ShowClassic extends Component<ShowClassicProps, ShowClassicState> {
       <Preview
         className='full-height'
         disableIframe={this.state.resizing}
-        previewMounted={previewMounted}
+        previewMounted={this.props.previewMounted}
       />
     );
   }
