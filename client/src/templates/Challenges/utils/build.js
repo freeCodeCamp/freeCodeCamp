9b80e722e4afdbd277ff52d79aa54df63dba2c4c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-11-30 16:21:28 +0100

    fix: only include frame-runner when running tests

--- client/src/templates/Challenges/utils/build.js
+++ client/src/templates/Challenges/utils/build.js
@@ -145,12 +145,13 @@ async function getDOMTestRunner(buildData, { proxyLogger }, document) {
     runTestInTestFrame(document, testString, testTimeout);
 }
 
-export function buildDOMChallenge({
-  challengeFiles,
-  required = [],
-  template = ''
-}) {
-  const finalRequires = [...globalRequires, ...required, ...frameRunner];
+export function buildDOMChallenge(
+  { challengeFiles, required = [], template = '' },
+  { usesTestRunner }
+) {
+  const finalRequires = [...globalRequires, ...required];
+  if (usesTestRunner) finalRequires.push(...frameRunner);
+
   const loadEnzyme = challengeFiles.some(
     challengeFile => challengeFile.ext === 'jsx'
   );
