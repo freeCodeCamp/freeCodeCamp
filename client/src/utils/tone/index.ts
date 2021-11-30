5d711be987a848bac92ec3230db32762bcc84e8b
Shaun Hamilton <shauhami020@gmail.com>
2021-11-30 18:49:28 +0000

    remove redundant type

--- client/src/utils/tone/index.ts
+++ client/src/utils/tone/index.ts
@@ -49,7 +49,7 @@ const toneUrls = {
   [FlashMessages.WentWrong]: TRY_AGAIN
 } as const;
 
-type ToneStates = keyof typeof toneUrls & FlashMessages;
+type ToneStates = keyof typeof toneUrls;
 
 export async function playTone(state: ToneStates): Promise<void> {
   const playSound = !!store.get('fcc-sound');
