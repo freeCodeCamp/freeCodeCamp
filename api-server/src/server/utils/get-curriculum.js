736cf839bd1e893a26237fe2b4e9f6b8710714a1
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-08 18:52:03 +0100

    fix: ensure the api always gets id not challengeId

--- api-server/src/server/utils/get-curriculum.js
+++ api-server/src/server/utils/get-curriculum.js
@@ -1,4 +1,4 @@
-import { flatten } from 'lodash';
+import { flatten, omit } from 'lodash';
 
 // TODO: keeping curriculum in memory is handy if we want to field requests that
 // need to 'query' the curriculum, but if we force the client to handle
@@ -13,9 +13,15 @@ export function getChallenges() {
   return Object.keys(curriculum)
     .map(key => curriculum[key].blocks)
     .reduce((challengeArray, superBlock) => {
-      const challengesForBlock = Object.keys(superBlock).map(
-        key => superBlock[key].challenges
-      );
-      return [...challengeArray, ...flatten(challengesForBlock)];
+      const challengesForBlock = flatten(
+        Object.keys(superBlock).map(key => superBlock[key].challenges)
+      ).map(challenge => ({
+        ...omit(challenge, 'challengeId'),
+        // challengeId is used by the client, but the api expects to work with
+        // id
+        id: challenge.challengeId
+      }));
+
+      return [...challengeArray, ...challengesForBlock];
     }, []);
 }
