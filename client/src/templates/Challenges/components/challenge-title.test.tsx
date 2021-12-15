e6b1b2b8c0ca49a2411d4b86c10b9b44c461970c
moT01 <20648924+moT01@users.noreply.github.com>
2021-12-14 20:21:35 -0600

    revert: snapshot fix

--- client/src/templates/Challenges/components/challenge-title.test.tsx
+++ client/src/templates/Challenges/components/challenge-title.test.tsx
@@ -7,7 +7,6 @@ const baseProps = {
   block: 'fake-block',
   children: 'title text',
   isCompleted: true,
-  showBreadCrumbs: true,
   superBlock: 'fake-superblock',
   translationPending: false
 };
