bce2345b98564dc8a3ad65ba2ccfb65877b2dbfa
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-16 07:50:29 +0100

    fix: remove duplicate projects from /settings

--- client/src/resources/cert-and-project-map.ts
+++ client/src/resources/cert-and-project-map.ts
@@ -328,44 +328,6 @@ const certMap = [
   },
 
   {
-    id: '561add10cb82ac38a17513bc',
-    title: 'Responsive Web Design Two',
-    certSlug: SuperBlocks.RespWebDesign,
-    flag: 'isRespWebDesignCert',
-    projects: [
-      {
-        id: 'bd7158d8c442eddfaeb5bd18',
-        title: 'Build a Tribute Page',
-        link: `${responsiveWebBase}/build-a-tribute-page`,
-        certSlug: SuperBlocks.RespWebDesign
-      },
-      {
-        id: '587d78af367417b2b2512b03',
-        title: 'Build a Survey Form',
-        link: `${responsiveWebBase}/build-a-survey-form`,
-        certSlug: SuperBlocks.RespWebDesign
-      },
-      {
-        id: '587d78af367417b2b2512b04',
-        title: 'Build a Product Landing Page',
-        link: `${responsiveWebBase}/build-a-product-landing-page`,
-        certSlug: SuperBlocks.RespWebDesign
-      },
-      {
-        id: '587d78b0367417b2b2512b05',
-        title: 'Build a Technical Documentation Page',
-        link: `${responsiveWebBase}/build-a-technical-documentation-page`,
-        certSlug: SuperBlocks.RespWebDesign
-      },
-      {
-        id: 'bd7158d8c242eddfaeb5bd13',
-        title: 'Build a Personal Portfolio Webpage',
-        link: `${responsiveWebBase}/build-a-personal-portfolio-webpage`,
-        certSlug: SuperBlocks.RespWebDesign
-      }
-    ]
-  },
-  {
     id: '561abd10cb81ac38a17513bc',
     title: 'JavaScript Algorithms and Data Structures',
     certSlug: SuperBlocks.JsAlgoDataStruct,
