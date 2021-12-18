c42134b2dca4708e95e9697a46623ae1bb61e347
moT01 <20648924+moT01@users.noreply.github.com>
2021-12-17 23:10:16 -0600

    fix: add check before saving

--- client/src/templates/Challenges/redux/save-challenge-saga.js
+++ client/src/templates/Challenges/redux/save-challenge-saga.js
@@ -2,24 +2,27 @@ import { call, takeEvery, put, select } from 'redux-saga/effects';
 import { postSaveChallenge } from '../../../utils/ajax';
 import { challengeDataSelector, challengeMetaSelector } from './';
 import { createFlashMessage } from '../../../components/Flash/redux';
+import { challengeTypes } from '../../../../utils/challenge-types';
 
 export function* saveChallengeSaga() {
-  try {
-    const { id } = yield select(challengeMetaSelector);
-    const { challengeFiles } = yield select(challengeDataSelector);
+  const { id, challengeType } = yield select(challengeMetaSelector);
+  const { challengeFiles } = yield select(challengeDataSelector);
 
-    const response = yield call(postSaveChallenge, { id, challengeFiles });
+  if (challengeType === challengeTypes.multiFileCertProject) {
+    try {
+      const response = yield call(postSaveChallenge, { id, challengeFiles });
 
-    if (response?.message) {
-      yield put(createFlashMessage(response));
+      if (response?.message) {
+        yield put(createFlashMessage(response));
+      }
+    } catch (e) {
+      yield put(
+        createFlashMessage({
+          type: 'danger',
+          message: 'flash.save-code-error'
+        })
+      );
     }
-  } catch (e) {
-    yield put(
-      createFlashMessage({
-        type: 'danger',
-        message: 'flash.save-code-error'
-      })
-    );
   }
 }
 
