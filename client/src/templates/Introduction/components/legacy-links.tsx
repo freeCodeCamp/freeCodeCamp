fb4cfd1ef35be5b3060e76f3ddcff1125f74fc09
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-21 16:47:41 +0100

    feat: only show the copy on the new superblock

--- client/src/templates/Introduction/components/legacy-links.tsx
+++ client/src/templates/Introduction/components/legacy-links.tsx
@@ -1,41 +1,22 @@
 import React from 'react';
-import { Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { SuperBlocks } from '../../../../../config/certification-settings';
-import envData from '../../../../../config/env.json';
 import { Link } from '../../../components/helpers';
-
-const { showNewCurriculum } = envData;
 
 interface LegacyLinksProps {
   superBlock: SuperBlocks;
 }
 
 function LegacyLinks({ superBlock }: LegacyLinksProps): JSX.Element {
+  const { t } = useTranslation();
   return (
     <>
       {superBlock === SuperBlocks.RespWebDesignNew && (
         <h2>
-          <Trans i18nKey='intro:link-to-legacy-rwd'>
-            Looking for the old Responsive Web Design curriculum? Go{' '}
-            <Link className='inline' to={`/learn/${SuperBlocks.RespWebDesign}`}>
-              here
-            </Link>
-            .
-          </Trans>
-        </h2>
-      )}
-      {superBlock === SuperBlocks.RespWebDesign && showNewCurriculum && (
-        <h2>
-          <Trans i18nKey='intro:link-to-rwd'>
-            There is a newer version of this curriculum available{' '}
-            <Link
-              className='inline'
-              to={`/learn/${SuperBlocks.RespWebDesignNew}`}
-            >
-              here
-            </Link>
-            .
-          </Trans>
+          {t('intro:viewing-upcoming-change')}
+          <Link className='inline' to={`/learn/`}>
+            {t('intro:go-back-to-learn')}
+          </Link>
         </h2>
       )}
     </>
