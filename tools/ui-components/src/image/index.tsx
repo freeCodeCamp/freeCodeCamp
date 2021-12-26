30b86b504b8296baf844848b3d6882829a93db51
IsmailTlemcani <ismail.tlemcani@gmail.com>
2021-12-26 12:18:59 +0100

    feat: add image component

---
+++ tools/ui-components/src/image/index.tsx
@@ -0,0 +1,1 @@
+export { Image } from './image';
--- tools/ui-components/src/index.ts
+++ tools/ui-components/src/index.ts
@@ -1,3 +1,4 @@
 // Use this file as the entry point for component export
 export { Button } from './button';
 export { Alert } from './alert';
+export { Image } from './image';
