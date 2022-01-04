cc8665404801db1c042b0b4d52dc18439a3b50b1
Aleksa Kis <59057989+aleksaKis@users.noreply.github.com>
2022-01-04 12:19:31 +0100

    chore(challenges-utils): Typescript migration of Challenges util (#44663)

--- client/src/templates/Challenges/utils/index.ts
+++ client/src/templates/Challenges/utils/index.ts
@@ -2,19 +2,24 @@ import envData from '../../../../../config/env.json';
 
 const { forumLocation } = envData;
 
-export function getGuideUrl({ forumTopicId, title = '' }) {
+interface GuideData {
+  forumTopicId: number;
+  title?: string;
+}
+
+export function getGuideUrl({ forumTopicId, title = '' }: GuideData): string {
   title = encodeURIComponent(title);
   return forumTopicId
     ? `https://forum.freecodecamp.org/t/${forumTopicId}`
     : `${forumLocation}/search?q=${title}%20in%3Atitle%20order%3Aviews`;
 }
 
-export function isGoodXHRStatus(status) {
+export function isGoodXHRStatus(status: string): boolean {
   const statusInt = parseInt(status, 10);
   return (statusInt >= 200 && statusInt < 400) || statusInt === 402;
 }
 
-export function transformEditorLink(url) {
+export function transformEditorLink(url: string): string {
   return url
     .replace(
       /(\/\/)(?<projectname>[^.]+)\.(?<username>[^.]+)\.repl\.co\/?/,
