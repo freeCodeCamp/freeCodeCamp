2a24d3a53640b31f5c7742b4397bfcec3246250c
Nicholas Carrigan <nhcarrigan@gmail.com>
2022-01-05 11:10:13 -0800

    feat: script to rename challenges

---
+++ tools/challenge-helper-scripts/rename-challenge-files.js
@@ -0,0 +1,34 @@
+const { readdir, readFile } = require('fs').promises;
+const { join } = require('path');
+const gray = require('gray-matter');
+const { exec } = require('child_process');
+const { promisify } = require('util');
+(async () => {
+  const asyncExec = promisify(exec);
+  const blocks = await readdir(
+    join(
+      process.cwd(),
+      'curriculum/challenges/english/14-responsive-web-design-22'
+    )
+  );
+  for (const block of blocks) {
+    const files = await readdir(
+      join(
+        process.cwd(),
+        `curriculum/challenges/english/14-responsive-web-design-22/${block}`
+      )
+    );
+    for (const file of files) {
+      const fileData = await readFile(
+        join(
+          process.cwd(),
+          `curriculum/challenges/english/14-responsive-web-design-22/${block}/${file}`
+        )
+      );
+      const challengeId = await gray(fileData).data.id;
+      await asyncExec(
+        `git mv curriculum/challenges/english/14-responsive-web-design-22/${block}/${file} curriculum/challenges/english/14-responsive-web-design-22/${block}/${challengeId}.md`
+      );
+    }
+  }
+})();
