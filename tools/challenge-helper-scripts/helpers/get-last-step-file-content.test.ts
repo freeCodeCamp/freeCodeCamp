f1810abbc82c2e5ed76c4c013e026ee41eef9231
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-01-12 13:09:09 +0100

    test: provide mock meta.json for test

--- tools/challenge-helper-scripts/helpers/get-last-step-file-content.test.ts
+++ tools/challenge-helper-scripts/helpers/get-last-step-file-content.test.ts
@@ -3,7 +3,14 @@ import { getLastStepFileContent } from './get-last-step-file-content';
 
 jest.mock('./get-project-info', () => {
   return {
-    getProjectPath: jest.fn(() => 'mock-project/')
+    getProjectPath: jest.fn(() => 'mock-project/'),
+    getProjectName: jest.fn(() => 'mock-project')
+  };
+});
+
+jest.mock('./get-project-meta-path', () => {
+  return {
+    getProjectMetaPath: jest.fn(() => '_meta/mock-project/meta.json')
   };
 });
 
@@ -38,6 +45,12 @@ describe('getLastStepFileContent helper', () => {
         'step-001.md': 'Lorem ipsum...',
         'step-002.md': 'Lorem ipsum...',
         'step-003.md': 'Lorem ipsum...'
+      },
+      '_meta/mock-project': {
+        'meta.json': `{
+        "id": "mock-id",
+        "challengeOrder": [["1","step1"], ["2","step2"], ["1","step3"]]}
+        `
       }
     });
     // it feels like an off-by-one error, but I think it's assuming that
@@ -49,8 +62,13 @@ describe('getLastStepFileContent helper', () => {
         lorem: 'ipsum'
       }
     };
+    // Add mock to test condition
+    process.env.CALLING_DIR = 'mock-project/';
 
     expect(getLastStepFileContent()).toEqual(expected);
+
+    // Remove mock to not affect other tests
+    delete process.env.CALLING_DIR;
   });
 
   afterEach(() => {
