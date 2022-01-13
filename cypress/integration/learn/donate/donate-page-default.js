b4b36d7e4aac2b475b775744d8a6216fc3098c1f
ahmad abdolsaheb <home@mac-16.local>
2022-01-13 17:43:30 +0300

    fix: adjust tests

--- cypress/integration/learn/donate/donate-page-default.js
+++ cypress/integration/learn/donate/donate-page-default.js
@@ -15,21 +15,19 @@ describe('Donate page', () => {
   });
 
   it('Should have FAQ section', () => {
-    cy.contains('Frequently asked questions').should('be.visible');
-    cy.contains('How can I get help with my donations?').should('be.visible');
-    cy.contains('How transparent is freeCodeCamp.org?').should('be.visible');
-    cy.contains('How efficient is freeCodeCamp?').should('be.visible');
-    cy.contains('How can I make a one-time donation?').should('be.visible');
+    cy.contains('Frequently asked questions');
+    cy.contains('How can I get help with my donations?');
+    cy.contains('How transparent is freeCodeCamp.org?');
+    cy.contains('How efficient is freeCodeCamp?');
+    cy.contains('How can I make a one-time donation?');
     cy.contains(
       'Does freeCodeCamp accept donations in Bitcoin or other cryptocurrencies?'
-    ).should('be.visible');
-    cy.contains('Can I mail a physical check?').should('be.visible');
+    );
+    cy.contains('Can I mail a physical check?');
     cy.contains(
       'How can I set up matching gifts from my employer, or payroll deductions?'
-    ).should('be.visible');
-    cy.contains(
-      'How can I set up an Endowment Gift to freeCodeCamp.org?'
-    ).should('be.visible');
+    );
+    cy.contains('How can I set up an Endowment Gift to freeCodeCamp.org?');
     cy.contains('How can I set up a Legacy gift to freeCodeCamp.org?').should(
       'be.visible'
     );
@@ -38,9 +36,9 @@ describe('Donate page', () => {
     );
     cy.contains(
       'I set up a monthly donation, but I need to update or pause the monthly recurrence. How can I do this?'
-    ).should('be.visible');
+    );
     cy.contains(
       'Is there anything else I can learn about donating to freeCodeCamp.org?'
-    ).should('be.visible');
+    );
   });
 });
