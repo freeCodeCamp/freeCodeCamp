9b62a266c873ffcaf974147effe71320ea4d6b94
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2022-01-22 22:39:29 +0300

    feat: replace blocks with grid bloks for new RWD

--- client/src/templates/Introduction/components/block.tsx
+++ client/src/templates/Introduction/components/block.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withTranslation, TFunction } from 'react-i18next';
+import { ProgressBar } from '@freecodecamp/react-bootstrap';
 import { connect } from 'react-redux';
 import ScrollableAnchor from 'react-scrollable-anchor';
 import { bindActionCreators, Dispatch } from 'redux';
@@ -10,11 +11,12 @@ import { isAuditedCert } from '../../../../../utils/is-audited';
 import Caret from '../../../assets/icons/caret';
 import GreenNotCompleted from '../../../assets/icons/green-not-completed';
 import GreenPass from '../../../assets/icons/green-pass';
-import { Link } from '../../../components/helpers';
+import { Link, Spacer } from '../../../components/helpers';
 import { completedChallengesSelector, executeGA } from '../../../redux';
 import { ChallengeNode, CompletedChallenge } from '../../../redux/prop-types';
 import { playTone } from '../../../utils/tone';
 import { makeExpandedBlockSelector, toggleBlock } from '../redux';
+import IsNewRespCert from '../../../utils/is-new-responsive-web-design-cert';
 import Challenges from './challenges';
 
 const { curriculumLocale } = envData;
@@ -90,6 +92,10 @@ export class Block extends Component<BlockProps> {
     );
   }
 
+  renderProgressBar(): JSX.Element {
+    return <ProgressBar now={25} label={`${25}%`} />;
+  }
+
   render(): JSX.Element {
     const {
       blockDashedName,
@@ -99,6 +105,8 @@ export class Block extends Component<BlockProps> {
       superBlock,
       t
     } = this.props;
+
+    const isNewResponsiveWebDesign = IsNewRespCert(superBlock);
 
     let completedCount = 0;
     const challengesWithCompleted = challenges.map(({ challenge }) => {
@@ -142,88 +150,149 @@ export class Block extends Component<BlockProps> {
       collapse: string;
     } = t('intro:misc-text');
 
-    return isProjectBlock ? (
-      <ScrollableAnchor id={blockDashedName}>
-        <div className='block'>
-          <div className='block-title-wrapper'>
-            <a className='block-link' href={`#${blockDashedName}`}>
-              <span className='cert-tag'>Certification Project</span>
-              <h3 className='big-block-title'>
-                {blockTitle}
-                <span className='block-link-icon'>#</span>
-              </h3>
-            </a>
-            {!isAuditedCert(curriculumLocale, superBlock) && (
-              <div className='block-cta-wrapper'>
-                <Link
-                  className='block-title-translation-cta'
-                  to={t('links:help-translate-link-url')}
-                >
-                  {t('misc.translation-pending')}
-                </Link>
-              </div>
-            )}
-          </div>
-          {this.renderBlockIntros(blockIntroArr)}
-          <Challenges
-            challengesWithCompleted={challengesWithCompleted}
-            isProjectBlock={isProjectBlock}
-            superBlock={superBlock}
-          />
-        </div>
-      </ScrollableAnchor>
-    ) : (
-      <ScrollableAnchor id={blockDashedName}>
-        <div className={`block ${isExpanded ? 'open' : ''}`}>
-          <div className='block-title-wrapper'>
-            <a className='block-link' href={`#${blockDashedName}`}>
-              <h3 className='big-block-title'>
-                {blockTitle}
-                <span className='block-link-icon'>#</span>
-              </h3>
-            </a>
-            {!isAuditedCert(curriculumLocale, superBlock) && (
-              <div className='block-cta-wrapper'>
-                <Link
-                  className='block-title-translation-cta'
-                  to={t('links:help-translate-link-url')}
-                >
-                  {t('misc.translation-pending')}
-                </Link>
-              </div>
-            )}
-          </div>
-          {this.renderBlockIntros(blockIntroArr)}
-          <button
-            aria-expanded={isExpanded}
-            className='map-title'
-            onClick={() => {
-              this.handleBlockClick();
-            }}
-          >
-            <Caret />
-            <h4 className='course-title'>
-              {`${isExpanded ? collapseText : expandText}`}
-            </h4>
-            <div className='map-title-completed course-title'>
-              {this.renderCheckMark(
-                completedCount === challengesWithCompleted.length
+    const ProjectBlock = (
+      <>
+        <ScrollableAnchor id={blockDashedName}>
+          <div className='block'>
+            <div className='block-header'>
+              <a className='block-link' href={`#${blockDashedName}`}>
+                <span className='cert-tag'>Certification Project</span>
+                <h3 className='big-block-title'>
+                  {blockTitle}
+                  <span className='block-link-icon'>#</span>
+                </h3>
+              </a>
+              {!isAuditedCert(curriculumLocale, superBlock) && (
+                <div className='block-cta-wrapper'>
+                  <Link
+                    className='block-title-translation-cta'
+                    to={t('links:help-translate-link-url')}
+                  >
+                    {t('misc.translation-pending')}
+                  </Link>
+                </div>
               )}
-              <span className='map-completed-count'>{`${completedCount}/${challengesWithCompleted.length}`}</span>
             </div>
-          </button>
-          {isExpanded && (
+            {this.renderBlockIntros(blockIntroArr)}
             <Challenges
               challengesWithCompleted={challengesWithCompleted}
               isProjectBlock={isProjectBlock}
               superBlock={superBlock}
             />
-          )}
-        </div>
-      </ScrollableAnchor>
+          </div>
+        </ScrollableAnchor>
+      </>
+    );
+
+    const Block = (
+      <>
+        {' '}
+        <ScrollableAnchor id={blockDashedName}>
+          <div className={`block ${isExpanded ? 'open' : ''}`}>
+            <div className='block-header'>
+              <a className='block-link' href={`#${blockDashedName}`}>
+                <h3 className='big-block-title'>
+                  {blockTitle}
+                  <span className='block-link-icon'>#</span>
+                </h3>
+              </a>
+              {!isAuditedCert(curriculumLocale, superBlock) && (
+                <div className='block-cta-wrapper'>
+                  <Link
+                    className='block-title-translation-cta'
+                    to={t('links:help-translate-link-url')}
+                  >
+                    {t('misc.translation-pending')}
+                  </Link>
+                </div>
+              )}
+            </div>
+            {this.renderBlockIntros(blockIntroArr)}
+            <button
+              aria-expanded={isExpanded}
+              className='map-title'
+              onClick={() => {
+                this.handleBlockClick();
+              }}
+            >
+              <Caret />
+              <h4 className='course-title'>
+                {`${isExpanded ? collapseText : expandText}`}
+              </h4>
+              <div className='map-title-completed course-title'>
+                {this.renderCheckMark(
+                  completedCount === challengesWithCompleted.length
+                )}
+                <span className='map-completed-count'>{`${completedCount}/${challengesWithCompleted.length}`}</span>
+              </div>
+            </button>
+            {isExpanded && (
+              <Challenges
+                challengesWithCompleted={challengesWithCompleted}
+                isProjectBlock={isProjectBlock}
+                superBlock={superBlock}
+              />
+            )}
+          </div>
+        </ScrollableAnchor>
+      </>
+    );
+    const blockrenderer = () => {
+      if (isProjectBlock) return ProjectBlock;
+      return isNewResponsiveWebDesign ? AltBlock : Block;
+    };
+
+    const AltBlock = (
+      <>
+        {' '}
+        <ScrollableAnchor id={blockDashedName}>
+          <div className={`block block-grid ${isExpanded ? 'open' : ''}`}>
+            <a
+              className='block-header'
+              onClick={() => {
+                this.handleBlockClick();
+              }}
+              href={`#${blockDashedName}`}
+            >
+              <div className='title-wrapper map-title'>
+                {this.renderCheckMark(
+                  completedCount === challengesWithCompleted.length
+                )}
+                <h3>{blockTitle}</h3>
+                <Caret />
+              </div>
+              {!isExpanded && (
+                <div className='progress-wrapper'>
+                  {this.renderProgressBar()}
+                </div>
+              )}
+            </a>
+            {isExpanded && (
+              <>
+                {this.renderBlockIntros(blockIntroArr)}
+                <Challenges
+                  challengesWithCompleted={challengesWithCompleted}
+                  isProjectBlock={isProjectBlock}
+                  superBlock={superBlock}
+                />
+              </>
+            )}
+          </div>
+        </ScrollableAnchor>
+      </>
+    );
+
+    return (
+      <>
+        {blockrenderer()}
+        {isNewResponsiveWebDesign && !isProjectBlock ? null : <Spacer />}
+      </>
     );
   }
 }
+
+// space if project block and new cert
+// blockDashedName !== 'project-euler'
 
 Block.displayName = 'Block';
 
