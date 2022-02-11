0c2da8baaf5675f48336b363eb6ca12a47206ee9
gikf <60067306+gikf@users.noreply.github.com>
2022-02-11 20:31:45 +0100

    chore: tsmigrate

--- client/src/templates/Challenges/utils/get-target-editor.ts
+++ client/src/templates/Challenges/utils/get-target-editor.ts
@@ -1,14 +1,17 @@
 import { isEmpty } from 'lodash-es';
 import { sortChallengeFiles } from '../../../../../utils/sort-challengefiles';
+import { ChallengeFiles, FileKey } from '../../../redux/prop-types';
 
-export function getTargetEditor(challengeFiles) {
+export function getTargetEditor(
+  challengeFiles: ChallengeFiles
+): FileKey | null {
   if (isEmpty(challengeFiles)) return null;
-  else {
-    let targetEditor = challengeFiles.find(
-      ({ editableRegionBoundaries }) => !isEmpty(editableRegionBoundaries)
-    )?.fileKey;
 
-    // fallback for when there is no editable region.
-    return targetEditor || sortChallengeFiles(challengeFiles)[0].fileKey;
-  }
+  const targetEditor = challengeFiles?.find(
+    ({ editableRegionBoundaries }) => !isEmpty(editableRegionBoundaries)
+  )?.fileKey;
+
+  // fallback for when there is no editable region.
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return */
+  return targetEditor || sortChallengeFiles(challengeFiles)[0].fileKey;
 }
