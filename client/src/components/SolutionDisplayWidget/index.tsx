a4874c0b347d1c597633383b30002a291f009397
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-02-16 10:19:07 +0100

    refactor: reorganise ShowDisplayWidget

--- client/src/components/SolutionDisplayWidget/index.tsx
+++ client/src/components/SolutionDisplayWidget/index.tsx
@@ -30,7 +30,94 @@ export function SolutionDisplayWidget({
     displayContext === 'settings'
       ? t('buttons.show-solution')
       : t('buttons.view');
-  const missingSolutionComponent =
+
+  const ShowFilesSolutionForCertification = (
+    <button
+      className='project-link-button-override'
+      data-cy={`${projectTitle ?? ''} solution`}
+      onClick={showFilesSolution}
+    >
+      {t('certification.project.solution')}
+    </button>
+  );
+  const ShowProjectAndGithubLinkForCertification = (
+    <>
+      <a href={solution ?? ''} rel='noopener noreferrer' target='_blank'>
+        {t('certification.project.solution')}
+      </a>
+      ,{' '}
+      <a href={githubLink} rel='noopener noreferrer' target='_blank'>
+        {t('certification.project.source')}
+      </a>
+    </>
+  );
+  const ShowProjectLinkForCertification = (
+    <a
+      className='btn-invert'
+      href={solution ?? ''}
+      rel='noopener noreferrer'
+      target='_blank'
+    >
+      {t('certification.project.solution')}
+    </a>
+  );
+  const MissingSolutionComponentForCertification = (
+    <>{t('certification.project.no-solution')}</>
+  );
+  const ShowFilesSolution = (
+    <Button
+      block={true}
+      bsStyle='primary'
+      className='btn-invert'
+      data-cy={projectTitle}
+      id={`btn-for-${id}`}
+      onClick={showFilesSolution}
+    >
+      {t('buttons.show-code')}
+    </Button>
+  );
+  const ShowProjectAndGithubLinks = (
+    <div className='solutions-dropdown'>
+      <DropdownButton
+        block={true}
+        bsStyle='primary'
+        className='btn-invert'
+        id={`dropdown-for-${id}`}
+        title={dropdownTitle}
+      >
+        <MenuItem
+          bsStyle='primary'
+          href={solution}
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          {t('buttons.frontend')}
+        </MenuItem>
+        <MenuItem
+          bsStyle='primary'
+          href={githubLink}
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          {t('buttons.backend')}
+        </MenuItem>
+      </DropdownButton>
+    </div>
+  );
+  const ShowProjectLink = (
+    <Button
+      block={true}
+      bsStyle='primary'
+      className='btn-invert'
+      href={solution}
+      id={`btn-for-${id}`}
+      rel='noopener noreferrer'
+      target='_blank'
+    >
+      {projectLinkText}
+    </Button>
+  );
+  const MissingSolutionComponent =
     displayContext === 'settings' ? (
       <>{t('certification.project.no-solution')}</>
     ) : null;
@@ -38,97 +125,16 @@ export function SolutionDisplayWidget({
   const displayComponents =
     displayContext === 'certification'
       ? {
-          showFilesSolution: (
-            <button
-              className='project-link-button-override'
-              data-cy={`${projectTitle ?? ''} solution`}
-              onClick={showFilesSolution}
-            >
-              {t('certification.project.solution')}
-            </button>
-          ),
-          showProjectAndGitHubLinks: (
-            <>
-              <a
-                href={solution ?? ''}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                {t('certification.project.solution')}
-              </a>
-              ,{' '}
-              <a href={githubLink} rel='noopener noreferrer' target='_blank'>
-                {t('certification.project.source')}
-              </a>
-            </>
-          ),
-          showProjectLink: (
-            <a
-              className='btn-invert'
-              href={solution ?? ''}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              {t('certification.project.solution')}
-            </a>
-          ),
-          none: <>{t('certification.project.no-solution')}</>
+          showFilesSolution: ShowFilesSolutionForCertification,
+          showProjectAndGitHubLinks: ShowProjectAndGithubLinkForCertification,
+          showProjectLink: ShowProjectLinkForCertification,
+          none: MissingSolutionComponentForCertification
         }
       : {
-          showFilesSolution: (
-            <Button
-              block={true}
-              bsStyle='primary'
-              className='btn-invert'
-              data-cy={projectTitle}
-              id={`btn-for-${id}`}
-              onClick={showFilesSolution}
-            >
-              {t('buttons.show-code')}
-            </Button>
-          ),
-          showProjectAndGitHubLinks: (
-            <div className='solutions-dropdown'>
-              <DropdownButton
-                block={true}
-                bsStyle='primary'
-                className='btn-invert'
-                id={`dropdown-for-${id}`}
-                title={dropdownTitle}
-              >
-                <MenuItem
-                  bsStyle='primary'
-                  href={solution}
-                  rel='noopener noreferrer'
-                  target='_blank'
-                >
-                  {t('buttons.frontend')}
-                </MenuItem>
-                <MenuItem
-                  bsStyle='primary'
-                  href={githubLink}
-                  rel='noopener noreferrer'
-                  target='_blank'
-                >
-                  {t('buttons.backend')}
-                </MenuItem>
-              </DropdownButton>
-            </div>
-          ),
-          showProjectLink: (
-            <Button
-              block={true}
-              bsStyle='primary'
-              className='btn-invert'
-              href={solution}
-              id={`btn-for-${id}`}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              {projectLinkText}
-            </Button>
-          ),
-          none: missingSolutionComponent
+          showFilesSolution: ShowFilesSolution,
+          showProjectAndGitHubLinks: ShowProjectAndGithubLinks,
+          showProjectLink: ShowProjectLink,
+          none: MissingSolutionComponent
         };
 
   return displayComponents[getSolutionDisplayType(completedChallenge)];
