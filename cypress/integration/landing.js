ba4575f69cb2ee4ffde967806eb2dda1a90f1353
moT01 <20648924+moT01@users.noreply.github.com>
2022-03-02 12:19:29 -0600

    fix: cypress tests

--- cypress/integration/landing.js
+++ cypress/integration/landing.js
@@ -17,7 +17,8 @@ const certifications = [
   'Data Analysis with Python',
   'Information Security',
   'Machine Learning with Python',
-  'Responsive Web Design (Beta)'
+  'Responsive Web Design (Beta)',
+  'Relational Database (Beta)'
 ];
 
 describe('Landing page', () => {
