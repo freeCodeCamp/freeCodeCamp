56efebc091036b2871eb160e0363792970d11a55
Naomi Carrigan <nhcarrigan@gmail.com>
2022-03-21 14:03:59 -0700

    fix: prettier issue

--- tools/challenge-editor/api/routes/blockRoute.ts
+++ tools/challenge-editor/api/routes/blockRoute.ts
@@ -3,7 +3,7 @@ import { getSteps } from '../utils/getSteps';
 
 export const blockRoute = async (req: Request, res: Response) => {
   const { superblock, block } = req.params;
-  
+
   const steps = await getSteps(superblock, block);
 
   res.json(steps);
