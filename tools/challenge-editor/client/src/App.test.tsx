00779717263848fa0db0f706664423852df9026a
Naomi Carrigan <nhcarrigan@gmail.com>
2022-03-21 13:59:50 -0700

    chore: remove test files for now

--- tools/challenge-editor/client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- tools/challenge-editor/client/src/components/header/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-test('Renders root link', () => {
-  render(<Header />);
-  const linkElement = screen.getByText(/freeCodeCamp Challenge Editor/i);
-  expect(linkElement).toBeInTheDocument();
-});
