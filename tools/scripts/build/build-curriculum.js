d993d63fb170d74af15fee84b8928fd95a93f484
Shaun Hamilton <shauhami020@gmail.com>
2022-03-22 20:01:15 +0000

    fix(tests): do not build mobile on i18n tests

--- tools/scripts/build/build-curriculum.js
+++ tools/scripts/build/build-curriculum.js
@@ -4,13 +4,17 @@ const path = require('path');
 const { getChallengesForLang } = require('../../../curriculum/getChallenges');
 const { buildMobileCurriculum } = require('./build-mobile-curriculum');
 
+const { CURRICULUM_LOCALE } = process.env;
+
 const globalConfigPath = path.resolve(__dirname, '../../../config');
 
 // We are defaulting to English because the ids for the challenges are same
 // across all languages.
 getChallengesForLang('english')
   .then(result => {
-    buildMobileCurriculum(result);
+    if (CURRICULUM_LOCALE === 'english') {
+      buildMobileCurriculum(result);
+    }
     return result;
   })
   .then(JSON.stringify)
