f47b8fc8999f792556653e75d3fd25e0ecb645e4
Mrugesh Mohapatra <hi@mrugesh.dev>
2022-03-28 23:21:42 +0530

    revert(client): sign up for email list directly
    This reverts commit b2a96e0a09adffaf178b88177757186cf0aab534.

--- client/src/pages/email-sign-up.tsx
+++ client/src/pages/email-sign-up.tsx
@@ -9,27 +9,21 @@ import { createSelector } from 'reselect';
 import IntroDescription from '../components/Intro/components/IntroDescription';
 import createRedirect from '../components/create-redirect';
 import { ButtonSpacer, Spacer } from '../components/helpers';
-import { apiLocation } from '../../../config/env.json';
 
-import { acceptTerms, userSelector, isSignedInSelector } from '../redux';
+import { acceptTerms, userSelector } from '../redux';
 
 import './email-sign-up.css';
+
 interface AcceptPrivacyTermsProps {
   acceptTerms: (accept: boolean | null) => void;
   acceptedPrivacyTerms: boolean;
-  isSignedIn: boolean;
   t: TFunction;
 }
 
 const mapStateToProps = createSelector(
   userSelector,
-  isSignedInSelector,
-  (
-    { acceptedPrivacyTerms }: { acceptedPrivacyTerms: boolean },
-    isSignedIn: boolean
-  ) => ({
-    acceptedPrivacyTerms,
-    isSignedIn
+  ({ acceptedPrivacyTerms }: { acceptedPrivacyTerms: boolean }) => ({
+    acceptedPrivacyTerms
   })
 );
 const mapDispatchToProps = (dispatch: Dispatch) =>
@@ -39,12 +33,10 @@ const RedirectToLearn = createRedirect('/learn');
 function AcceptPrivacyTerms({
   acceptTerms,
   acceptedPrivacyTerms,
-  isSignedIn,
   t
 }: AcceptPrivacyTermsProps) {
   const acceptedPrivacyRef = useRef(acceptedPrivacyTerms);
   const acceptTermsRef = useRef(acceptTerms);
-
   useEffect(() => {
     acceptedPrivacyRef.current = acceptedPrivacyTerms;
     acceptTermsRef.current = acceptTerms;
@@ -65,55 +57,6 @@ function AcceptPrivacyTerms({
 
   function onClick(isWeeklyEmailAccepted: boolean) {
     acceptTerms(isWeeklyEmailAccepted);
-  }
-
-  function renderEmailListOptin(isSignedIn: boolean) {
-    if (isSignedIn) {
-      return (
-        <Row>
-          <Col md={4} mdOffset={2} sm={5} smOffset={1} xs={12}>
-            <Button
-              block={true}
-              bsSize='lg'
-              bsStyle='primary'
-              className='big-cta-btn'
-              onClick={() => onClick(true)}
-            >
-              {t('buttons.yes-please')}
-            </Button>
-            <ButtonSpacer />
-          </Col>
-          <Col md={4} sm={5} xs={12}>
-            <Button
-              block={true}
-              bsSize='lg'
-              bsStyle='primary'
-              className='big-cta-btn'
-              onClick={() => onClick(false)}
-            >
-              {t('buttons.no-thanks')}
-            </Button>
-            <ButtonSpacer />
-          </Col>
-        </Row>
-      );
-    } else {
-      return (
-        <Col md={8} mdOffset={2} sm={10} smOffset={1} xs={12}>
-          <ButtonSpacer />
-          <Button
-            block={true}
-            bsSize='lg'
-            bsStyle='primary'
-            className='big-cta-btn'
-            href={`${apiLocation}/signin`}
-          >
-            {t('buttons.sign-up-email-list')}
-          </Button>
-          <ButtonSpacer />
-        </Col>
-      );
-    }
   }
 
   return acceptedPrivacyTerms ? (
@@ -138,7 +81,30 @@ function AcceptPrivacyTerms({
             <p>{t('misc.email-blast')}</p>
             <Spacer />
           </Col>
-          {renderEmailListOptin(isSignedIn)}
+          <Col md={4} mdOffset={2} sm={5} smOffset={1} xs={12}>
+            <Button
+              block={true}
+              bsSize='lg'
+              bsStyle='primary'
+              className='big-cta-btn'
+              onClick={() => onClick(true)}
+            >
+              {t('buttons.yes-please')}
+            </Button>
+            <ButtonSpacer />
+          </Col>
+          <Col md={4} sm={5} xs={12}>
+            <Button
+              block={true}
+              bsSize='lg'
+              bsStyle='primary'
+              className='big-cta-btn'
+              onClick={() => onClick(false)}
+            >
+              {t('buttons.no-thanks')}
+            </Button>
+            <ButtonSpacer />
+          </Col>
           <Col xs={12}>
             <Spacer />
           </Col>
