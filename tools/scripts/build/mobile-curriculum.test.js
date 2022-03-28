1febd29e7fdc9c8a5d26634a4a35ce9a561c0617
SemBauke <semboot699@gmail.com>
2022-03-28 17:41:04 +0200

    fix: test mobile build conditionally

--- tools/scripts/build/mobile-curriculum.test.js
+++ tools/scripts/build/mobile-curriculum.test.js
@@ -4,6 +4,7 @@ const { AssertionError } = require('chai');
 const { getChallengesForLang } = require('../../../curriculum/getChallenges');
 const { buildMobileCurriculum } = require('./build-mobile-curriculum');
 const { mobileSchemaValidator } = require('./mobileSchema');
+const { CURRICULUM_LOCALE } = process.env;
 
 describe('mobile curriculum build', () => {
   const mobileStaticPath = path.resolve(__dirname, '../../../client/static');
@@ -16,38 +17,52 @@ describe('mobile curriculum build', () => {
 
   let curriculum;
 
+  const runCondition = CURRICULUM_LOCALE === 'english';
+  const itif = condition => (condition ? it : it.skip);
+
   beforeAll(async () => {
-    curriculum = await getChallengesForLang('english');
-    await buildMobileCurriculum(curriculum);
+    if (runCondition) {
+      curriculum = await getChallengesForLang('english');
+      buildMobileCurriculum(curriculum);
+    }
   }, 20000);
 
-  test('the mobile curriculum should have a static folder with multiple files', () => {
-    expect(fs.existsSync(`${mobileStaticPath}/mobile`)).toBe(true);
+  itif(runCondition)(
+    'the mobile curriculum should have a static folder with multiple files',
+    () => {
+      expect(fs.existsSync(`${mobileStaticPath}/mobile`)).toBe(true);
 
-    expect(fs.readdirSync(`${mobileStaticPath}/mobile`).length).toBeGreaterThan(
-      0
-    );
-  });
+      expect(
+        fs.readdirSync(`${mobileStaticPath}/mobile`).length
+      ).toBeGreaterThan(0);
+    }
+  );
 
-  test('the mobile curriculum should have access to the intro.json file', () => {
-    expect(fs.existsSync(blockIntroPath)).toBe(true);
-  });
+  itif(runCondition)(
+    'the mobile curriculum should have access to the intro.json file',
+    () => {
+      expect(fs.existsSync(blockIntroPath)).toBe(true);
+    }
+  );
 
-  test('the files generated should have the correct schema', () => {
-    const fileArray = fs.readdirSync(`${mobileStaticPath}/mobile`);
+  itif(runCondition)(
+    'the files generated should have the correct schema',
+    () => {
+      const fileArray = fs.readdirSync(`${mobileStaticPath}/mobile`);
 
-    fileArray
-      .filter(fileInArray => fileInArray !== 'availableSuperblocks.json')
-      .forEach(fileInArray => {
-        const fileContent = fs.readFileSync(
-          `${mobileStaticPath}/mobile/${fileInArray}`
-        );
+      fileArray
+        .filter(fileInArray => fileInArray !== 'availableSuperblocks.json')
+        .forEach(fileInArray => {
+          const fileContent = fs.readFileSync(
+            `${mobileStaticPath}/mobile/${fileInArray}`
+          );
 
-        const result = validateMobileSuperBlock(JSON.parse(fileContent));
+          const result = validateMobileSuperBlock(JSON.parse(fileContent));
 
-        if (result.error) {
-          throw new AssertionError(result.error, `file: ${fileInArray}`);
-        }
-      });
-  });
+          if (result.error) {
+            throw new AssertionError(result.error, `file: ${fileInArray}`);
+          }
+        });
+    }
+  );
 });
