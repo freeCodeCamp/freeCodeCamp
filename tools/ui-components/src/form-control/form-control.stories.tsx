2896b40dfa4e5ca3a6b4eb304ee4727ec9242d09
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2022-03-30 19:56:01 +0300

    feat: add initial stories

--- tools/ui-components/src/form-control/form-control.stories.tsx
+++ tools/ui-components/src/form-control/form-control.stories.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { FormControl, FormControlProps } from '.';
+import { FormControl, FormControlProps, FormControlVariationProps } from '.';
 
 const story = {
   title: 'Example/FormControl',
   component: FormControl
 };
 
-const Template: Story<FormControlProps> = args => {
+const DefaultTemplate: Story<FormControlProps> = args => {
   return <FormControl {...args} />;
 };
 
-export const Default = Template.bind({});
+export const Default = DefaultTemplate.bind({});
 Default.args = {
   // default props go here
+};
+
+const StaticTemplate: Story<FormControlVariationProps> = args => {
+  return <FormControl.Static {...args} />;
+};
+
+export const Static = StaticTemplate.bind({});
+Static.args = {
+  children: 'foo@bar.com'
+};
+
+const FeedBackTemplate: Story<FormControlVariationProps> = args => {
+  return <FormControl.Feedback {...args} />;
+};
+
+export const Feedback = FeedBackTemplate.bind({});
+Feedback.args = {
+  children: '✔'
 };
 
 export default story;
--- tools/ui-components/src/form-control/index.ts
+++ tools/ui-components/src/form-control/index.ts
@@ -1,2 +1,2 @@
 export { FormControl } from './form-control';
-export type { FormControlProps } from './types';
+export type { FormControlProps, FormControlVariationProps } from './types';
