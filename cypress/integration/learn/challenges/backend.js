c4b189ccb13e5106f2fa0a694207ac5da9a25a4d
moT01 <20648924+moT01@users.noreply.github.com>
2022-03-31 21:47:32 -0500

    Revert "fix: cypress"
    This reverts commit 18c95c3bde8045345b04761582011449e23672ee.

--- cypress/integration/learn/challenges/backend.js
+++ cypress/integration/learn/challenges/backend.js
@@ -26,15 +26,9 @@ describe('Backend challenge', function () {
 
   it('does not generate unhandled errors on submission', () => {
     cy.visit(locations.index);
-    cy.get(selectors.input).type('https://example.com');
-
-    // temporary fix until https://github.com/cypress-io/cypress/issues/20562 is fixed
-    cy.contains(`I've completed this challenge`)
-      .click()
-
-      // revert to this when it is
-      // .type('{enter}')
-
+    cy.get(selectors.input)
+      .type('https://example.com')
+      .type('{enter}')
       .then(() => {
         cy.get(selectors.defaultOutput)
           .contains(runningOutput)
--- cypress/integration/learn/challenges/codeally.js
+++ cypress/integration/learn/challenges/codeally.js
@@ -9,14 +9,9 @@ describe('CodeAlly cert challenge', function () {
     });
 
     it('should not allow you to submit a URL', function () {
-      cy.get('input[name="solution"]').type('https://example.com');
-
-      // temporary fix until https://github.com/cypress-io/cypress/issues/20562 is fixed
-      cy.contains(`I've completed this challenge`).click();
-
-      // revert to this when it is
-      // .type('{enter}')
-
+      cy.get('input[name="solution"]')
+        .type('https://example.com')
+        .type('{enter}');
       cy.contains('You must complete the project first.');
     });
   });
@@ -31,14 +26,9 @@ describe('CodeAlly cert challenge', function () {
     });
 
     it('should allow you to submit a URL', function () {
-      cy.get('input[name="solution"]').type('https://example.com');
-
-      // temporary fix until https://github.com/cypress-io/cypress/issues/20562 is fixed
-      cy.contains(`I've completed this challenge`).click();
-
-      // revert to this when it is
-      // .type('{enter}')
-
+      cy.get('input[name="solution"]')
+        .type('https://example.com')
+        .type('{enter}');
       cy.get('.completion-modal-body');
     });
   });
