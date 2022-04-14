35ac1b074e89bb0e76927aca0afea3a0832e41d5
Naomi Carrigan <nhcarrigan@gmail.com>
2022-04-14 09:40:17 -0700

    feat: include all superblocks

---
+++ tools/challenge-editor/api/configs/superBlockList.ts
@@ -0,0 +1,54 @@
+export const superBlockList = [
+  {
+    name: 'Responsive Web Design',
+    path: '01-responsive-web-design'
+  },
+  {
+    name: 'JavaScript Algorithms and Data Structures',
+    path: '02-javascript-algorithms-and-data-structures'
+  },
+  {
+    name: 'Front End Development Libraries',
+    path: '03-front-end-development-libraries'
+  },
+  {
+    name: 'Data Visualization',
+    path: '04-data-visualization'
+  },
+  {
+    name: 'Back End Development and APIs',
+    path: '05-back-end-development-and-apis'
+  },
+  {
+    name: 'Quality Assurance',
+    path: '06-quality-assurance'
+  },
+  {
+    name: 'Scientific Computing with Python',
+    path: '07-scientific-computing-with-python'
+  },
+  {
+    name: 'Data Analysis with Python',
+    path: '08-data-analysis-with-python'
+  },
+  {
+    name: 'Information Security',
+    path: '09-information-security'
+  },
+  {
+    name: 'Coding Interview Prep',
+    path: '10-coding-interview-prep'
+  },
+  {
+    name: 'Machine Learning with Python',
+    path: '11-machine-learning-with-python'
+  },
+  {
+    name: 'Relational Databases',
+    path: '13-relational-databases'
+  },
+  {
+    name: 'Responsive Web Design (Beta)',
+    path: '14-responsive-web-design-22'
+  }
+];
--- tools/challenge-editor/api/routes/indexRoute.ts
+++ tools/challenge-editor/api/routes/indexRoute.ts
@@ -1,12 +1,6 @@
 import { Request, Response } from 'express';
+import { superBlockList } from '../configs/superBlockList';
 
 export const indexRoute = (req: Request, res: Response) => {
-  const supers = [
-    {
-      name: 'Responsive Web Design',
-      path: '14-responsive-web-design-22'
-    }
-  ];
-
-  res.json(supers);
+  res.json(superBlockList);
 };
