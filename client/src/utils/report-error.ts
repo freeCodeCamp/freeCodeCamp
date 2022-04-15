b532720cb428e78aac1cf5512e380266369936d6
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-04-15 09:48:30 +0200

    fix: use env.json in report-error

--- client/src/utils/report-error.ts
+++ client/src/utils/report-error.ts
@@ -1,13 +1,15 @@
 import * as Sentry from '@sentry/react';
-import { sentry } from '../../../config/secrets';
+import envData from '../../../config/env.json';
+
+const { sentryClientDSN } = envData;
 
 export function reportClientSideError(
   e: Error,
-  message = sentry.dns === 'dsn_from_sentry_dashboard'
+  message = sentryClientDSN === null
     ? 'Unhandled error'
     : 'Error sent to Sentry'
 ): string | void {
-  return sentry.dns === 'dsn_from_sentry_dashboard'
+  return sentryClientDSN === null
     ? console.error(`Client: ${message}`, e)
     : Sentry.captureException(e);
 }
