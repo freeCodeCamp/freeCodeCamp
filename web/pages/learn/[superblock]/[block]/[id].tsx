c0538892fa32d49ea4f5c2fe4e495add433dbace
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-04-20 13:51:10 +0200

    feat: extend ISR demo to use params

--- web/pages/learn/[superblock]/[block]/[id].tsx
+++ web/pages/learn/[superblock]/[block]/[id].tsx
@@ -1,11 +1,36 @@
-import { GetStaticPaths, GetStaticPropsResult } from 'next';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
 interface Props {
-  rwd?: Record<string, unknown>;
-  js?: Record<string, unknown>;
+  rwd?: Record<string, any>;
+  js?: Record<string, any>;
 }
 
 export default function Challenge({ rwd, js }: Props) {
+  const router = useRouter();
+  const { superblock, block, id } = router.query;
+
+  if (!superblock || !block || !id) return null;
+  if (typeof block !== 'string') return null;
+  if (!rwd || !js) return null;
+
+  if (superblock === 'responsive-web-design') {
+    const challenge = rwd[block].challenges.filter(
+      (c: { dashedName: string }) => c.dashedName == id
+    )[0].description;
+
+    return <div dangerouslySetInnerHTML={{ __html: challenge }} />;
+  } else if (superblock === 'javascript-algorithms-and-data-structures') {
+    const challenge = js[block].challenges.filter(
+      (c: { dashedName: string }) => c.dashedName == id
+    )[0].description;
+
+    return <div dangerouslySetInnerHTML={{ __html: challenge }} />;
+  }
   return (
     <div>
       <ul>{rwd && Object.keys(rwd).map(name => <li key={name}>{name}</li>)}</ul>
@@ -14,7 +39,7 @@ export default function Challenge({ rwd, js }: Props) {
   );
 }
 
-export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+export const getStaticProps: GetStaticProps = async () => {
   const rwd = await fetch('http://localhost:3000/responsive-web-design');
   const js = await fetch(
     'http://localhost:3000/javascript-algorithms-and-data-structures'
@@ -23,8 +48,9 @@ export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
     .blocks;
   const jsBlocks = ((await js.json()) as { blocks: Record<string, unknown> })
     .blocks;
+
   return { props: { rwd: rwdBlocks, js: jsBlocks }, revalidate: 5 };
-}
+};
 
 export const getStaticPaths: GetStaticPaths = () => ({
   paths: [],
