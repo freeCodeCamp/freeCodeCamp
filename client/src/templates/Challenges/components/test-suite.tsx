53d64e938789d12050ad27cc7daf87fe1229b90d
Bruce B <bbsmooth@gmail.com>
2022-04-28 13:38:29 -0700

    fix: challenge test suite accessibility

--- client/src/templates/Challenges/components/test-suite.tsx
+++ client/src/templates/Challenges/components/test-suite.tsx
@@ -20,47 +20,37 @@ interface TestSuiteProps {
   tests: Test[];
 }
 
-function getAccessibleText(text: string, err?: string, pass?: boolean) {
-  let accessibleText = 'Waiting';
-  const cleanText = text.replace(/<\/?code>/g, '');
-
-  // Determine test status (i.e. icon)
-  if (err) {
-    accessibleText = 'Error';
-  } else if (pass) {
-    accessibleText = 'Pass';
-  }
-
-  // Append the text itself
-  return accessibleText + ' - ' + cleanText;
-}
-
 function TestSuite({ tests }: TestSuiteProps): JSX.Element {
   const testSuiteTests = tests.filter(isTestSuiteTest);
 
   return (
-    <div className='challenge-test-suite'>
-      {testSuiteTests.map(({ err, pass = false, text = '' }, index) => {
-        const isInitial = !pass && !err;
-        const statusIcon = pass && !err ? <GreenPass /> : <Fail />;
-        return (
-          <div
-            aria-label={getAccessibleText(text, err, pass)}
-            className='test-result'
-            key={text.slice(-6) + String(index)}
-          >
-            <div className='test-status-icon'>
-              {isInitial ? <Initial /> : statusIcon}
-            </div>
-            <div
-              aria-hidden='true'
-              className='test-output'
-              dangerouslySetInnerHTML={{ __html: text }}
-            />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h2 className='challenge-test-suite-heading'>Tests</h2>
+      <ul className='challenge-test-suite'>
+        {testSuiteTests.map(({ err, pass = false, text = '' }, index) => {
+          const isInitial = !pass && !err;
+          const statusIcon = pass && !err ? <GreenPass /> : <Fail />;
+          const initialText = 'Waiting';
+          const statusText = pass && !err ? 'Passed' : 'Failed';
+          // Remove opening/closing <p> so screen reader will read both
+          // status message and test text as one block.
+          text = text.replace(/^<p>|<\/p>$/g, '');
+          return (
+            <li className='test-result' key={text.slice(-6) + String(index)}>
+              <div className='test-status-icon' aria-hidden='true'>
+                {isInitial ? <Initial /> : statusIcon}
+              </div>
+              <div className='test-output'>
+                <span className='sr-only'>
+                  {isInitial ? initialText : statusText}:{' '}
+                </span>
+                <span dangerouslySetInnerHTML={{ __html: text }} />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
