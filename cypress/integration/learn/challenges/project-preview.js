c56e69e16dec2b99aa0c2c119378f0386d42a76f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-05-02 11:34:42 +0200

    feat: create 4 character ids

--- cypress/integration/learn/challenges/project-preview.js
+++ cypress/integration/learn/challenges/project-preview.js
@@ -1,126 +1,126 @@
 const practiceProjectsFirstSteps = [
   {
     title: 'Accessibility Quiz',
-    url: '/learn/2022/responsive-web-design/learn-accessibility-by-building-a-quiz/step-1/a5d36e91-af08-44e7-9aa7-d03cf858baf4/'
+    url: '/learn/2022/responsive-web-design/learn-accessibility-by-building-a-quiz/step-1/MVPV/'
   },
   {
     title: 'Cafe Menu',
-    url: '/learn/2022/responsive-web-design/learn-basic-css-by-building-a-cafe-menu/step-1/e159a8ab-7978-423e-9b2e-bae0b7517cb2/'
+    url: '/learn/2022/responsive-web-design/learn-basic-css-by-building-a-cafe-menu/step-1/hFCk/'
   },
   {
     title: 'Ferris Wheel',
-    url: '/learn/2022/responsive-web-design/learn-css-animation-by-building-a-ferris-wheel/step-1/3bfab848-cde3-405c-b2a4-a469ae632b34/'
+    url: '/learn/2022/responsive-web-design/learn-css-animation-by-building-a-ferris-wheel/step-1/r5pC/'
   },
   {
     title: 'Colored Markers',
-    url: '/learn/2022/responsive-web-design/learn-css-colors-by-building-a-set-of-colored-markers/step-1/0c1cb4e2-63b7-43ed-a24a-6c9fbb735411/'
+    url: '/learn/2022/responsive-web-design/learn-css-colors-by-building-a-set-of-colored-markers/step-1/5nWc/'
   },
   {
     title: 'Photo Gallery',
-    url: '/learn/2022/responsive-web-design/learn-css-flexbox-by-building-a-photo-gallery/step-1/0884ac01-ed22-44f5-8802-2031ce1012e8/'
+    url: '/learn/2022/responsive-web-design/learn-css-flexbox-by-building-a-photo-gallery/step-1/2Tw5/'
   },
   {
     title: 'Magazine',
-    url: '/learn/2022/responsive-web-design/learn-css-grid-by-building-a-magazine/step-1/4c840e76-0a68-46f1-b359-09dc9a72b60d/'
+    url: '/learn/2022/responsive-web-design/learn-css-grid-by-building-a-magazine/step-1/jl2k/'
   },
   {
     title: 'Penguin',
-    url: '/learn/2022/responsive-web-design/learn-css-transforms-by-building-a-penguin/step-1/7e5b7d65-9669-4ada-b083-4949dab2cb9f/'
+    url: '/learn/2022/responsive-web-design/learn-css-transforms-by-building-a-penguin/step-1/rYHN/'
   },
   {
     title: 'City Skyline',
-    url: '/learn/2022/responsive-web-design/learn-css-variables-by-building-a-city-skyline/step-1/b15e829b-220f-4120-8c04-1917c815f87e/'
+    url: '/learn/2022/responsive-web-design/learn-css-variables-by-building-a-city-skyline/step-1/qMqs/'
   },
   {
     title: 'CatPhotoApp',
-    url: '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-1/df3e64bb-fb58-4f09-875f-e0fd1e6f5ca4/'
+    url: '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-1/9Qb7/'
   },
   {
     title: 'Registration Form',
-    url: '/learn/2022/responsive-web-design/learn-html-forms-by-building-a-registration-form/step-1/73a3299b-a244-4c42-ada2-876984af45c6/'
+    url: '/learn/2022/responsive-web-design/learn-html-forms-by-building-a-registration-form/step-1/wBSk/'
   },
   {
     title: 'Picasso Painting',
-    url: '/learn/2022/responsive-web-design/learn-intermediate-css-by-building-a-picasso-painting/step-1/1c2fac60-9617-4504-bfd7-da55c58b3500/'
+    url: '/learn/2022/responsive-web-design/learn-intermediate-css-by-building-a-picasso-painting/step-1/JzCF/'
   },
   {
     title: 'Balance Sheet',
-    url: '/learn/2022/responsive-web-design/learn-more-about-css-pseudo-selectors-by-building-a-balance-sheet/step-1/5b7ed753-b0cd-4f7b-83ed-721a88f8479c/'
+    url: '/learn/2022/responsive-web-design/learn-more-about-css-pseudo-selectors-by-building-a-balance-sheet/step-1/LpRs/'
   },
   {
     title: 'Piano',
-    url: '/learn/2022/responsive-web-design/learn-responsive-web-design-by-building-a-piano/step-1/060df0e6-9742-4528-af64-48832fbcb52b/'
+    url: '/learn/2022/responsive-web-design/learn-responsive-web-design-by-building-a-piano/step-1/cRMp/'
   },
   {
     title: 'Rothko Painting',
-    url: '/learn/2022/responsive-web-design/learn-the-css-box-model-by-building-a-rothko-painting/step-1/a5cfa392-821c-4175-af85-374502b993c6/'
+    url: '/learn/2022/responsive-web-design/learn-the-css-box-model-by-building-a-rothko-painting/step-1/FfV5/'
   },
   {
     title: 'Nutrition Label',
-    url: '/learn/2022/responsive-web-design/learn-typography-by-building-a-nutrition-label/step-1/04570082-7e6b-40d7-8d1c-ad50dd9f08da/'
+    url: '/learn/2022/responsive-web-design/learn-typography-by-building-a-nutrition-label/step-1/JV9R/'
   }
 ];
 
 const practiceProjectsSecondSteps = [
   {
     title: 'Accessibility Quiz',
-    url: '/learn/2022/responsive-web-design/learn-accessibility-by-building-a-quiz/step-2/c94c45d9-2970-441b-b10a-f0aa1adfd036/'
+    url: '/learn/2022/responsive-web-design/learn-accessibility-by-building-a-quiz/step-2/myqT/'
   },
   {
     title: 'Cafe Menu',
-    url: '/learn/2022/responsive-web-design/learn-basic-css-by-building-a-cafe-menu/step-2/51ae372e-e3bc-4aff-86f2-4f05d037bb17/'
+    url: '/learn/2022/responsive-web-design/learn-basic-css-by-building-a-cafe-menu/step-2/nVVT/'
   },
   {
     title: 'Ferris Wheel',
-    url: '/learn/2022/responsive-web-design/learn-css-animation-by-building-a-ferris-wheel/step-2/f9b84487-3d58-4da2-b2af-889478e6fa5b/'
+    url: '/learn/2022/responsive-web-design/learn-css-animation-by-building-a-ferris-wheel/step-2/lXxg/'
   },
   {
     title: 'Colored Markers',
-    url: '/learn/2022/responsive-web-design/learn-css-colors-by-building-a-set-of-colored-markers/step-2/bef03efb-89a8-46f7-a504-e0826cebca59/'
+    url: '/learn/2022/responsive-web-design/learn-css-colors-by-building-a-set-of-colored-markers/step-2/t5FB/'
   },
   {
     title: 'Photo Gallery',
-    url: '/learn/2022/responsive-web-design/learn-css-flexbox-by-building-a-photo-gallery/step-2/8268e457-c42b-41ba-a511-5d46c04f5663/'
+    url: '/learn/2022/responsive-web-design/learn-css-flexbox-by-building-a-photo-gallery/step-2/vBwt/'
   },
   {
     title: 'Magazine',
-    url: '/learn/2022/responsive-web-design/learn-css-grid-by-building-a-magazine/step-2/b3110a5e-e28d-4bd5-8689-4d05861e2f7c/'
+    url: '/learn/2022/responsive-web-design/learn-css-grid-by-building-a-magazine/step-2/h692/'
   },
   {
     title: 'Penguin',
-    url: '/learn/2022/responsive-web-design/learn-css-transforms-by-building-a-penguin/step-2/b68794a9-4a07-4ab8-894b-bff165833757/'
+    url: '/learn/2022/responsive-web-design/learn-css-transforms-by-building-a-penguin/step-2/KrBb/'
   },
   {
     title: 'City Skyline',
-    url: '/learn/2022/responsive-web-design/learn-css-variables-by-building-a-city-skyline/step-2/5282784b-f9f0-47e8-a4d4-ead482942d86/'
+    url: '/learn/2022/responsive-web-design/learn-css-variables-by-building-a-city-skyline/step-2/Yk5M/'
   },
   {
     title: 'CatPhotoApp',
-    url: '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2/9fa664a3-6820-4c3a-8f9e-3691b108feeb/'
+    url: '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2/HJ9V/'
   },
   {
     title: 'Registration Form',
-    url: '/learn/2022/responsive-web-design/learn-html-forms-by-building-a-registration-form/step-2/d68d48ca-0aaa-43e9-bd92-bd7cb3831fd2/'
+    url: '/learn/2022/responsive-web-design/learn-html-forms-by-building-a-registration-form/step-2/W5dR/'
   },
   {
     title: 'Picasso Painting',
-    url: '/learn/2022/responsive-web-design/learn-intermediate-css-by-building-a-picasso-painting/step-2/83452733-c470-4eaf-bf5b-6ed812845b5b/'
+    url: '/learn/2022/responsive-web-design/learn-intermediate-css-by-building-a-picasso-painting/step-2/tQrm/'
   },
   {
     title: 'Balance Sheet',
-    url: '/learn/2022/responsive-web-design/learn-more-about-css-pseudo-selectors-by-building-a-balance-sheet/step-2/4cca8c85-f574-4784-ba55-d8345106b972/'
+    url: '/learn/2022/responsive-web-design/learn-more-about-css-pseudo-selectors-by-building-a-balance-sheet/step-2/TZpL/'
   },
   {
     title: 'Piano',
-    url: '/learn/2022/responsive-web-design/learn-responsive-web-design-by-building-a-piano/step-2/6fae0be3-0ac4-49a2-bdf2-d18a879b8836/'
+    url: '/learn/2022/responsive-web-design/learn-responsive-web-design-by-building-a-piano/step-2/Zgkg/'
   },
   {
     title: 'Rothko Painting',
-    url: '/learn/2022/responsive-web-design/learn-the-css-box-model-by-building-a-rothko-painting/step-2/0e57c930-a00f-4560-9141-e557d93ebc33/'
+    url: '/learn/2022/responsive-web-design/learn-the-css-box-model-by-building-a-rothko-painting/step-2/7Rwf/'
   },
   {
     title: 'Nutrition Label',
-    url: '/learn/2022/responsive-web-design/learn-typography-by-building-a-nutrition-label/step-2/e463349e-abb8-489b-ba08-4923a4ca4235/'
+    url: '/learn/2022/responsive-web-design/learn-typography-by-building-a-nutrition-label/step-2/WTMD/'
   }
 ];
 
