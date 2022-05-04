dcdd80542a1d885b0c71071347023951532d86a0
Naomi Carrigan <nhcarrigan@gmail.com>
2022-05-04 14:19:11 -0700

    feat: replace beta banner with legacy banner

--- client/src/utils/is-a-cert.ts
+++ client/src/utils/is-a-cert.ts
@@ -4,6 +4,10 @@ export function isNewRespCert(superBlock: string): boolean {
   return superBlock === SuperBlocks.RespWebDesignNew;
 }
 
+export function isOldRespCert(superBlock: string): boolean {
+  return superBlock === SuperBlocks.RespWebDesign;
+}
+
 export function isRelationalDbCert(superBlock: string): boolean {
   return superBlock === SuperBlocks.RelationalDb;
 }
