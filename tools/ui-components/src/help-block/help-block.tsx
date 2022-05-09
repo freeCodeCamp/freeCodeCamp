fa59d4a107d1f55a74cc78332fac0227f762316c
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2022-05-09 20:28:25 +0700

    chore(ui-components): simplify color tokens (#45848)
    * chore: rename color tokens
    
    * chore: clean up hover tokens
    
    * docs: add a note on colors usage

--- tools/ui-components/src/help-block/help-block.tsx
+++ tools/ui-components/src/help-block/help-block.tsx
@@ -3,7 +3,7 @@ import { HelpBlockProps } from './types';
 
 export const HelpBlock = React.forwardRef<HTMLSpanElement, HelpBlockProps>(
   ({ className, children }, ref): JSX.Element => {
-    const defaultClasses = 'block mt-1 mb-2 text-default-foreground-quaternary';
+    const defaultClasses = 'block mt-1 mb-2 text-foreground-quaternary';
     const classes = [defaultClasses, className].join(' ');
     return (
       <span ref={ref} data-testid='help-block' className={classes}>
