8494e7af3bbffde0273b86591f61de3324b7100b
Naomi Carrigan <nhcarrigan@gmail.com>
2022-05-12 08:34:46 -0700

    Revert "chore: tighten restriction on update-user-flag"
    This reverts commit 088a38698e83f662f306f63a5bae00776bdf81d6.

--- api-server/src/server/boot/settings.js
+++ api-server/src/server/boot/settings.js
@@ -262,6 +262,11 @@ function handleInvalidUpdate(res) {
 function updateUserFlag(req, res, next) {
   const { user, body: update } = req;
   const allowedKeys = [
+    'theme',
+    'sound',
+    'isHonest',
+    'portfolio',
+    'sendQuincyEmail',
     'isGithub',
     'isLinkedIn',
     'isTwitter',
