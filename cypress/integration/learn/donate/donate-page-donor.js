ed865269a65d7b684c2f35739ddf861c21660b68
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-06-01 00:06:35 +0200

    test: fix donate page for existing donors (#46255)

--- cypress/integration/learn/donate/donate-page-donor.js
+++ cypress/integration/learn/donate/donate-page-donor.js
@@ -1,41 +1,24 @@
-const selectors = {
-  donateAlert: {
-    firstText: '.alert-info p:first-child',
-    secondText: '.alert-info p:last-child',
-    link: '.alert-info a'
-  }
-};
-
 describe('Donate page', () => {
-  before(() => {
-    cy.clearCookies();
+  beforeEach(() => {
     cy.exec('npm run seed -- --donor');
     cy.login();
-    cy.visit('/donate');
-  });
-
-  after(() => {
-    cy.exec('npm run seed');
   });
 
   it('Donor alert should be visible for donor', () => {
-    cy.get('.alert-info').should('be.visible');
-  });
+    cy.visit('/donate');
 
-  it('Donor should see alert message', () => {
-    cy.contains(
-      selectors.donateAlert.firstText,
+    cy.get('[data-cy="donate-alert"]').should('be.visible');
+
+    cy.get('[data-cy="donate.thank-you"]').should(
+      'have.text',
       'Thank you for being a supporter of freeCodeCamp. You currently have a recurring donation.'
     );
-    cy.contains(
-      selectors.donateAlert.lastText,
+    cy.get('[data-cy="donate.bigger-donation"]').should(
+      'have.text',
       "Want to make a bigger one-time donation, mail us a check, or give in other ways? Here are many other ways you can support our non-profit's mission."
     );
-  });
-
-  it('Donor alert section should have donation link', () => {
-    cy.get(selectors.donateAlert.link).should(
-      'have.attr',
+    cy.get('[data-cy="donate-link"]').should(
+      'contain.attr',
       'href',
       'https://www.freecodecamp.org/news/how-to-donate-to-free-code-camp'
     );
