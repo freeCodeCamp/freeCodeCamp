d2332093f6383faf9db9eae9cb681fcce194f5dd
Bruce Blaser <bbsmooth@gmail.com>
2022-06-01 00:39:26 -0700

    fix(a11y): main menu a11y updates (#45137)
    * fix: main menu a11y updates
    
    * update font menu
    
    * fix: sign in button text alignment
    
    * fix: disabled button font
    
    * fix: language menu font
    
    * remove sign-in from main menu
    
    * fix: add section dividers to menu
    
    * fix: height on language selector menu
    
    * fix: alignment of Menu button text
    
    * fix: language globe icon
    
    * refactor: remove dup selector
    
    * update language menu colors
    
    * refactor: clearer name for language menu display state
    
    * fix: don't close onBlur if Menu button is clicked
    
    * refactor: move globe icon styling to CSS
    
    * refactor: get rid of switch statements
    
    * refactor: remove try catch block
    
    * fix: translate Change language button
    
    * fix: move search into nav menu for mobile layout
    
    * fix: forgot a merge
    
    * refactor: updates for changes in i18n/all-langs
    
    * fix: prevent menu from collapsing when focus is on change language button and user clicks into search
    
    * fix: translate cancel change option in language picker
    
    * feat: add cypress tests
    
    * feat: display the complete language list
    
    * fix: fix TS typing
    
    * fix: force scrollbar on lang menu
    
    * fix: remove scroll bar from lang menu
    
    * fix: close menu when user tabs away from last menu item
    
    * add list role to navigation list to appease Safari
    
    * chore: capitalize Change Language/remove CSS comment
    
    * fix: right side search box alignment in narrow view
    
    * remove extraneous list role
    
    * fix: cypress
    
    Co-authored-by: ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
    Co-authored-by: moT01 <20648924+moT01@users.noreply.github.com>

--- client/src/components/Header/components/nav-links.tsx
+++ client/src/components/Header/components/nav-links.tsx
@@ -16,19 +16,21 @@ import {
   faExternalLinkAlt
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createRef, Ref } from 'react';
 import { TFunction, withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import envData from '../../../../../config/env.json';
 import {
   availableLangs,
-  getLangName
+  LangNames,
+  LangCodes
 } from '../../../../../config/i18n/all-langs';
 import { hardGoTo as navigate } from '../../../redux';
 import { updateUserFlag } from '../../../redux/settings';
 import createLanguageRedirect from '../../create-language-redirect';
 import { Link } from '../../helpers';
 import { Themes } from '../../settings/theme';
+import LanguageGlobe from '../../../assets/icons/language-globe';
 
 const { clientLocale, radioLocation, apiLocation } = envData;
 
@@ -36,13 +38,17 @@ const locales = availableLangs.client;
 
 export interface NavLinksProps {
   displayMenu?: boolean;
+  isLanguageMenuDisplayed?: boolean;
   fetchState?: { pending: boolean };
   i18n: Object;
   t: TFunction;
-  toggleDisplayMenu?: React.MouseEventHandler<HTMLButtonElement>;
+  hideMenu: () => void;
   toggleNightMode: (x: any) => any;
   user?: Record<string, unknown>;
   navigate?: (location: string) => void;
+  showLanguageMenu?: (elementToFocus: HTMLButtonElement) => void;
+  hideLanguageMenu?: () => void;
+  menuButtonRef: Ref<HTMLButtonElement>;
 }
 
 const mapDispatchToProps = {
@@ -52,10 +58,22 @@ const mapDispatchToProps = {
 
 export class NavLinks extends Component<NavLinksProps, {}> {
   static displayName: string;
+  langButtonRef: React.RefObject<HTMLButtonElement>;
+  firstLangOptionRef: React.RefObject<HTMLElement>;
+  lastLangOptionRef: React.RefObject<HTMLElement>;
 
   constructor(props: NavLinksProps) {
     super(props);
+    this.langButtonRef = createRef();
+    this.firstLangOptionRef = createRef();
+    this.lastLangOptionRef = createRef();
     this.handleLanguageChange = this.handleLanguageChange.bind(this);
+    this.handleLanguageMenuKeyDown = this.handleLanguageMenuKeyDown.bind(this);
+    this.handleLanguageButtonClick = this.handleLanguageButtonClick.bind(this);
+    this.handleLanguageButtonKeyDown =
+      this.handleLanguageButtonKeyDown.bind(this);
+    this.handleMenuKeyDown = this.handleMenuKeyDown.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   toggleTheme(currentTheme = Themes.Default, toggleNightMode: any) {
@@ -64,23 +82,172 @@ export class NavLinks extends Component<NavLinksProps, {}> {
     );
   }
 
-  handleLanguageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ): void => {
-    const { toggleDisplayMenu, navigate } = this.props;
-    toggleDisplayMenu();
+  getPreviousMenuItem(target: HTMLElement): HTMLElement {
+    const { menuButtonRef } = this.props;
+    const previousSibling =
+      target.closest('.nav-list > li')?.previousElementSibling;
+    return previousSibling?.querySelector('a, button') ?? menuButtonRef.current;
+  }
 
+  handleLanguageChange = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    const { hideMenu, hideLanguageMenu, menuButtonRef, navigate } = this.props;
+    const newLanguage = event.target.dataset.value as string;
+    // If user selected cancel then close menu and put focus on button
+    if (newLanguage === 'exit-lang-menu') {
+      // Set focus to language button first so we don't lose focus
+      // for screen readers.
+      this.langButtonRef.current.focus();
+      hideLanguageMenu();
+      return;
+    }
+    // Put focus on menu button first so we don't lose focus
+    // for screen readers.
+    menuButtonRef.current.focus();
+    hideMenu();
+    // If user selected the current language then we just close the menu
+    if (newLanguage === clientLocale) {
+      return;
+    }
     const path = createLanguageRedirect({
       clientLocale,
-      lang: event.target.value
+      lang: newLanguage
     });
 
     return navigate(path);
   };
 
+  handleMenuKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>): void => {
+    const { menuButtonRef, hideMenu } = this.props;
+    if (event.key === 'Escape') {
+      menuButtonRef.current.focus();
+      hideMenu();
+      event.preventDefault();
+    }
+  };
+
+  handleLanguageButtonClick = (): void => {
+    const { isLanguageMenuDisplayed, hideLanguageMenu, showLanguageMenu } =
+      this.props;
+    if (isLanguageMenuDisplayed) {
+      hideLanguageMenu();
+    } else {
+      showLanguageMenu(this.firstLangOptionRef.current);
+    }
+  };
+
+  handleLanguageButtonKeyDown = (
+    event: React.KeyboardEvent<HTMLButtonElement>
+  ): void => {
+    const { menuButtonRef, showLanguageMenu, hideMenu } = this.props;
+    /* eslint-disable @typescript-eslint/naming-convention */
+    const doKeyPress = {
+      Escape: () => {
+        menuButtonRef.current.focus();
+        hideMenu();
+        event.preventDefault();
+      },
+      ArrowDown: () => {
+        showLanguageMenu(this.firstLangOptionRef.current);
+        event.preventDefault();
+      },
+      ArrowUp: () => {
+        showLanguageMenu(this.lastLangOptionRef.current);
+        event.preventDefault();
+      }
+    };
+    /* eslint-enable @typescript-eslint/naming-convention */
+    doKeyPress[event.key]?.();
+  };
+
+  handleLanguageMenuKeyDown = (
+    event: React.KeyboardEvent<HTMLButtonElement>
+  ): void => {
+    const { hideLanguageMenu, hideMenu } = this.props;
+    const focusFirstLanguageMenuItem = () => {
+      this.firstLangOptionRef.current.focus();
+      event.preventDefault();
+    };
+    const focusLastLanguageMenuItem = () => {
+      this.lastLangOptionRef.current.focus();
+      event.preventDefault();
+    };
+    /* eslint-disable @typescript-eslint/naming-convention */
+    const doKeyPress = {
+      Tab: () => {
+        if (!event.shiftKey) {
+          // Let the Tab work as normal.
+          hideLanguageMenu();
+          // Close the menu if focus is now outside of the menu. This will
+          // happen when there is no Sign Out menu item.
+          setTimeout(() => {
+            const currentlyFocusedElement = document.activeElement;
+            if (
+              currentlyFocusedElement &&
+              !currentlyFocusedElement.closest('.nav-list')
+            ) {
+              hideMenu();
+            }
+          }, 200);
+          return;
+        }
+        // Because FF adds an extra tab stop to the lang menu (because it
+        // is scrollable) we need to manually focus the previous menu item.
+        this.getPreviousMenuItem(this.langButtonRef.current).focus();
+        hideLanguageMenu();
+        event.preventDefault();
+      },
+      ArrowUp: () => {
+        const arrowUpItemToFocus =
+          event.target === this.firstLangOptionRef.current
+            ? this.lastLangOptionRef.current
+            : (event.target.parentNode.previousSibling
+                .firstChild as HTMLElement);
+        arrowUpItemToFocus.focus();
+        event.preventDefault();
+      },
+      ArrowDown: () => {
+        const arrowDownItemToFocus =
+          event.target === this.lastLangOptionRef.current
+            ? this.firstLangOptionRef.current
+            : (event.target.parentNode.nextSibling.firstChild as HTMLElement);
+        arrowDownItemToFocus.focus();
+        event.preventDefault();
+      },
+      Escape: () => {
+        // Set focus to language button first so we don't lose focus
+        // for screen readers.
+        this.langButtonRef.current.focus();
+        hideLanguageMenu();
+        event.preventDefault();
+      },
+      Home: focusFirstLanguageMenuItem,
+      PageUp: focusFirstLanguageMenuItem,
+      End: focusLastLanguageMenuItem,
+      PageDown: focusLastLanguageMenuItem
+    };
+    /* eslint-enable @typescript-eslint/naming-convention */
+    doKeyPress[event.key]?.();
+  };
+
+  // Added to the last item in the nav menu. Will close the menu if
+  // the user Tabs out of the menu.
+  handleBlur = (event: React.FocusEvent<HTMLButtonElement>): void => {
+    const { hideMenu, menuButtonRef } = this.props;
+    console.log('handleBlur: relatedTarget = ', event.relatedTarget);
+    if (
+      event.relatedTarget &&
+      !event.relatedTarget.closest('.nav-list') &&
+      event.relatedTarget !== menuButtonRef.current
+    ) {
+      hideMenu();
+    }
+  };
+
   render() {
     const {
       displayMenu,
+      isLanguageMenuDisplayed,
       fetchState,
       t,
       toggleNightMode,
@@ -92,132 +259,214 @@ export class NavLinks extends Component<NavLinksProps, {}> {
     return pending ? (
       <div className='nav-skeleton' />
     ) : (
-      <div className={'nav-list' + (displayMenu ? ' display-menu' : '')}>
+      <ul
+        aria-labelledby='toggle-button-nav'
+        className={`nav-list${displayMenu ? ' display-menu' : ''}${
+          isLanguageMenuDisplayed ? ' display-lang-menu' : ''
+        }`}
+      >
         {isDonating ? (
-          <div className='nav-link nav-link-flex nav-link-header' key='donate'>
-            <span>{t('donate.thanks')}</span>
-            <FontAwesomeIcon icon={faHeart} />
-          </div>
+          <li key='donate'>
+            <div className='nav-link nav-link-flex nav-link-header'>
+              <span>{t('donate.thanks')}</span>
+              <FontAwesomeIcon icon={faHeart} />
+            </div>
+          </li>
         ) : (
-          <Link className='nav-link' key='donate' sameTab={false} to='/donate'>
-            {t('buttons.donate')}
-          </Link>
+          <li key='donate'>
+            <Link
+              className='nav-link'
+              onKeyDown={this.handleMenuKeyDown}
+              sameTab={false}
+              to='/donate'
+            >
+              {t('buttons.donate')}
+            </Link>
+          </li>
         )}
-        {!username && (
-          <a
-            className='nav-link nav-link-sign-in'
-            href={`${apiLocation}/signin`}
-            key='signin'
+        <li key='learn'>
+          <Link
+            className='nav-link'
+            onKeyDown={this.handleMenuKeyDown}
+            to='/learn'
           >
-            {t('buttons.sign-in')}
-          </a>
-        )}
-        <Link className='nav-link' key='learn' to='/learn'>
-          {t('buttons.curriculum')}
-        </Link>
+            {t('buttons.curriculum')}
+          </Link>
+        </li>
         {username && (
           <Fragment key='profile-settings'>
-            <Link
-              className='nav-link'
-              key='profile'
-              sameTab={false}
-              to={`/${username}`}
-            >
-              {t('buttons.profile')}
-            </Link>
-            <Link
-              className='nav-link'
-              key='settings'
-              sameTab={false}
-              to={`/settings`}
-            >
-              {t('buttons.settings')}
-            </Link>
+            <li key='profile'>
+              <Link
+                className='nav-link'
+                onKeyDown={this.handleMenuKeyDown}
+                sameTab={false}
+                to={`/${username}`}
+              >
+                {t('buttons.profile')}
+              </Link>
+            </li>
+            <li key='settings'>
+              <Link
+                className='nav-link'
+                onKeyDown={this.handleMenuKeyDown}
+                sameTab={false}
+                to={`/settings`}
+              >
+                {t('buttons.settings')}
+              </Link>
+            </li>
           </Fragment>
         )}
-        <hr className='nav-line' />
-        <Link
-          className='nav-link nav-link-flex'
-          external={true}
-          key='forum'
-          sameTab={false}
-          to={t('links:nav.forum')}
-        >
-          <span>{t('buttons.forum')}</span>
-          <FontAwesomeIcon icon={faExternalLinkAlt} />
-        </Link>
-        <Link
-          className='nav-link nav-link-flex'
-          external={true}
-          key='news'
-          sameTab={false}
-          to={t('links:nav.news')}
-        >
-          <span>{t('buttons.news')}</span>
-          <FontAwesomeIcon icon={faExternalLinkAlt} />
-        </Link>
-        <Link
-          className='nav-link nav-link-flex'
-          external={true}
-          key='radio'
-          sameTab={false}
-          to={radioLocation}
-        >
-          <span>{t('buttons.radio')}</span>
-          <FontAwesomeIcon icon={faExternalLinkAlt} />
-        </Link>
-        <hr className='nav-line' />
-        <button
-          className={
-            'nav-link nav-link-flex' + (!username ? ' nav-link-header' : '')
-          }
-          disabled={!username}
-          key='theme'
-          onClick={() => this.toggleTheme(String(theme), toggleNightMode)}
-        >
-          {username ? (
-            <>
-              <span>{t('settings.labels.night-mode')}</span>
-              {theme === Themes.Night ? (
-                <FontAwesomeIcon icon={faCheckSquare} />
-              ) : (
-                <FontAwesomeIcon icon={faSquare} />
-              )}
-            </>
-          ) : (
-            <span className='nav-link-dull'>{t('misc.change-theme')}</span>
-          )}
-        </button>
-        <div className='nav-link nav-link-header' key='lang-header'>
-          {t('footer.language')}
-        </div>
-
-        <div className='nav-link dropdown-nav-link' key='language-dropdown'>
-          <select
-            className='nav-link-lang-dropdown'
-            onChange={this.handleLanguageChange}
-            value={clientLocale}
+        <li key='forum' className='nav-line'>
+          <Link
+            className='nav-link nav-link-flex'
+            external={true}
+            onKeyDown={this.handleMenuKeyDown}
+            sameTab={false}
+            to={t('links:nav.forum')}
           >
-            {locales.map(lang => (
-              <option key={'lang-' + lang} value={lang}>
-                {getLangName(lang)}
-              </option>
-            ))}
-          </select>
-        </div>
+            <span>{t('buttons.forum')}</span>
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </Link>
+        </li>
+        <li key='news'>
+          <Link
+            className='nav-link nav-link-flex'
+            external={true}
+            onKeyDown={this.handleMenuKeyDown}
+            sameTab={false}
+            to={t('links:nav.news')}
+          >
+            <span>{t('buttons.news')}</span>
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </Link>
+        </li>
+        <li key='radio'>
+          <Link
+            className='nav-link nav-link-flex'
+            external={true}
+            onKeyDown={this.handleMenuKeyDown}
+            sameTab={false}
+            to={radioLocation}
+          >
+            <span>{t('buttons.radio')}</span>
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </Link>
+        </li>
+        <li className='nav-line' key='theme'>
+          <button
+            {...(!username && { 'aria-describedby': 'theme-sign-in' })}
+            aria-disabled={!username}
+            aria-pressed={theme === Themes.Night ? 'true' : 'false'}
+            className={
+              'nav-link nav-link-flex' + (!username ? ' nav-link-header' : '')
+            }
+            onClick={() => {
+              if (username) {
+                this.toggleTheme(String(theme), toggleNightMode);
+              }
+            }}
+            onKeyDown={this.handleMenuKeyDown}
+          >
+            {username ? (
+              <>
+                <span>{t('settings.labels.night-mode')}</span>
+                {theme === Themes.Night ? (
+                  <FontAwesomeIcon icon={faCheckSquare} />
+                ) : (
+                  <FontAwesomeIcon icon={faSquare} />
+                )}
+              </>
+            ) : (
+              <Fragment key='night-mode'>
+                <span className='sr-only'>
+                  {t('settings.labels.night-mode')}
+                </span>
+                <span
+                  aria-hidden='true'
+                  className='nav-link-dull'
+                  id='theme-sign-in'
+                >
+                  {t('misc.change-theme')}
+                </span>
+              </Fragment>
+            )}
+          </button>
+        </li>
+        <li key='lang-menu'>
+          <div className='nav-lang' key='language-dropdown'>
+            <button
+              aria-controls='nav-lang-menu'
+              {...(isLanguageMenuDisplayed && { 'aria-expanded': true })}
+              aria-haspopup='true'
+              className='nav-link nav-lang-button'
+              id='nav-lang-button'
+              onBlur={this.handleBlur}
+              onClick={this.handleLanguageButtonClick}
+              onKeyDown={this.handleLanguageButtonKeyDown}
+              ref={this.langButtonRef}
+            >
+              <span>{t('buttons.change-language')}</span>
+              <LanguageGlobe />
+            </button>
+            <ul
+              aria-labelledby='nav-lang-button'
+              className={'nav-lang-menu' + (username ? ' logged-in' : '')}
+              id='nav-lang-menu'
+              role='menu'
+            >
+              <li key='lang-menu-exit' role='none'>
+                <button
+                  className='nav-link nav-lang-menu-option'
+                  data-value='exit-lang-menu'
+                  onClick={this.handleLanguageChange}
+                  onKeyDown={this.handleLanguageMenuKeyDown}
+                  ref={this.firstLangOptionRef}
+                  role='menuitem'
+                  tabIndex='-1'
+                >
+                  {t('buttons.cancel-change')}
+                </button>
+              </li>
+              {locales.map((lang, index) => (
+                <li key={'lang-' + lang} role='none'>
+                  <button
+                    {...(clientLocale === lang && { 'aria-current': true })}
+                    className='nav-link nav-lang-menu-option'
+                    data-value={lang}
+                    {...(LangCodes[lang] && {
+                      lang: LangCodes[lang] as string
+                    })}
+                    onClick={this.handleLanguageChange}
+                    onKeyDown={this.handleLanguageMenuKeyDown}
+                    {...(index === locales.length - 1 && {
+                      ref: this.lastLangOptionRef
+                    })}
+                    role='menuitem'
+                    tabIndex='-1'
+                  >
+                    {LangNames[lang]}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </li>
         {username && (
           <Fragment key='signout-frag'>
-            <hr className='nav-line-2' />
-            <a
-              className='nav-link'
-              href={`${apiLocation}/signout`}
-              key='sign-out'
-            >
-              {t('buttons.sign-out')}
-            </a>
+            <li className='nav-line' key='sign-out'>
+              <a
+                className='nav-link nav-link-signout'
+                href={`${apiLocation}/signout`}
+                onBlur={this.handleBlur}
+                onKeyDown={this.handleMenuKeyDown}
+              >
+                {t('buttons.sign-out')}
+              </a>
+            </li>
           </Fragment>
         )}
-      </div>
+      </ul>
     );
   }
 }
--- client/src/components/Header/components/universal-nav.tsx
+++ client/src/components/Header/components/universal-nav.tsx
@@ -6,29 +6,41 @@
 import Loadable from '@loadable/component';
 import React, { Ref } from 'react';
 import { useTranslation } from 'react-i18next';
+import Media from 'react-responsive';
 import { isLanding } from '../../../utils/path-parsers';
 import { Link, SkeletonSprite } from '../../helpers';
 import MenuButton from './menu-button';
 import NavLinks from './nav-links';
 import NavLogo from './nav-logo';
 import './universal-nav.css';
+import AuthOrProfile from './auth-or-profile';
 
 const SearchBar = Loadable(() => import('../../search/searchBar/search-bar'));
 const SearchBarOptimized = Loadable(
   () => import('../../search/searchBar/search-bar-optimized')
 );
 
+const MAX_MOBILE_WIDTH = 980;
+
 export interface UniversalNavProps {
   displayMenu?: boolean;
+  isLanguageMenuDisplayed?: boolean;
   fetchState?: { pending: boolean };
   menuButtonRef?: Ref<HTMLButtonElement> | undefined;
   searchBarRef?: unknown;
-  toggleDisplayMenu?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  showMenu?: () => void;
+  hideMenu?: () => void;
+  showLanguageMenu?: (elementToFocus: HTMLButtonElement) => void;
+  hideLanguageMenu?: () => void;
   user?: Record<string, unknown>;
 }
 export const UniversalNav = ({
   displayMenu,
-  toggleDisplayMenu,
+  isLanguageMenuDisplayed,
+  showMenu,
+  hideMenu,
+  showLanguageMenu,
+  hideLanguageMenu,
   menuButtonRef,
   searchBarRef,
   user,
@@ -47,15 +59,13 @@ export const UniversalNav = ({
   return (
     <nav
       aria-label={t('aria.primary-nav')}
-      className={'universal-nav' + (displayMenu ? ' expand-nav' : '')}
+      className={`universal-nav${displayMenu ? ' expand-nav' : ''}`}
       id='universal-nav'
     >
       <div
-        className={
-          'universal-nav-left' + (displayMenu ? ' display-search' : '')
-        }
+        className={`universal-nav-left${displayMenu ? ' display-search' : ''}`}
       >
-        {search}
+        <Media minWidth={MAX_MOBILE_WIDTH + 1}>{search}</Media>
       </div>
       <div className='universal-nav-middle'>
         <Link id='universal-nav-logo' to='/learn'>
@@ -68,21 +78,32 @@ export const UniversalNav = ({
             <SkeletonSprite />
           </div>
         ) : (
-          <MenuButton
-            displayMenu={displayMenu}
-            innerRef={menuButtonRef}
-            onClick={toggleDisplayMenu}
-            user={user}
-          />
+          <>
+            <MenuButton
+              displayMenu={displayMenu}
+              hideMenu={hideMenu}
+              innerRef={menuButtonRef}
+              showMenu={showMenu}
+              user={user}
+            />
+            <Media maxWidth={MAX_MOBILE_WIDTH}>{search}</Media>
+            <NavLinks
+              displayMenu={displayMenu}
+              fetchState={fetchState}
+              isLanguageMenuDisplayed={isLanguageMenuDisplayed}
+              hideLanguageMenu={hideLanguageMenu}
+              hideMenu={hideMenu}
+              menuButtonRef={menuButtonRef}
+              showLanguageMenu={showLanguageMenu}
+              showMenu={showMenu}
+              user={user}
+            />
+            <div className='navatar'>
+              <AuthOrProfile user={user} />
+            </div>
+          </>
         )}
       </div>
-
-      <NavLinks
-        displayMenu={displayMenu}
-        fetchState={fetchState}
-        toggleDisplayMenu={toggleDisplayMenu}
-        user={user}
-      />
     </nav>
   );
 };
