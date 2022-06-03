0b41df8b04a3dfdabc91d1c392552dabcad5d5f0
Naomi Carrigan <nhcarrigan@gmail.com>
2022-06-03 10:15:37 -0700

    feat: restrict update flag endpoint

--- api-server/src/server/boot/settings.js
+++ api-server/src/server/boot/settings.js
@@ -269,11 +269,6 @@ function handleInvalidUpdate(res) {
 function updateUserFlag(req, res, next) {
   const { user, body: update } = req;
   const allowedKeys = [
-    'theme',
-    'sound',
-    'isHonest',
-    'portfolio',
-    'sendQuincyEmail',
     'isGithub',
     'isLinkedIn',
     'isTwitter',
