b06cebb8aa14befd6c0e1b07fed87a7c171a38eb
SemBauke <semboot699@gmail.com>
2022-06-24 14:55:55 +0200

    test: commit hook

--- commit-message-hook.js
+++ commit-message-hook.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
+
 const main = async () => {
   const commitFile = path.resolve('.git/COMMIT_EDITMSG');
   const commitMessage = fs.readFileSync(commitFile, 'utf8');
@@ -9,8 +10,8 @@ const main = async () => {
   const match = commitMessage.match(regex);
 
   if (!match) {
-    console.log('please use the following format:');
-    console.log('fix: some fix');
+    console.log('please use the following format: type: description \n commit message: ' + commitMessage);
+    process.exit(1);
   } else {
     console.log('commit message ok');
   }
