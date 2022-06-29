14f4d3414255a22acb718c093918b39ebb2b281d
Sboonny <MuhammedElruby@gmail.com>
2022-06-29 14:14:54 +0000

    will come back after the meeting

--- client/src/components/Header/components/SignOutModal.tsx
+++ client/src/components/Header/components/SignOutModal.tsx
@@ -1,57 +1,55 @@
 // import { Modal } from '@freecodecamp/react-bootstrap';
-// import React from 'react';
-// import envData from '../../../../../config/env.json';
+// import React, { useState } from 'react';
 // import { TFunction, useTranslation } from 'react-i18next';
+// import envData from '../../../../../config/env.json';
 
 // const { apiLocation } = envData;
-// const { t } = useTranslation();
 
 // export interface SignOutProps {
-//   i18n: Object;
+//   i18n: unknown;
 //   t: TFunction;
 //   hideModal: () => void;
 //   isSignedOut?: boolean;
 // }
 
-// constructor(props: SignOutProps ){
-//   this.handleMenuKeyDown = this.handleMenuKeyDown.bind(this);
-//   this.handleBlur = this.handleBlur.bind(this);
-// }
+// // constructor(){
+// //   this.handleMenuKeyDown = this.handleMenuKeyDown.bind(this);
+// //   this.handleBlur = this.handleBlur.bind(this);
+// // }
 
-// function SignOutModal(
-// ): JSX.Element {
+// class SignOutModal extends React.FunctionComponent<SignOutProps>  {
+// //     const { t } = useTranslation();
+// //     const [show, setShow] = useState(false);
 
-//   const [show, setShow] = useState(false);
-//   return (
-//     <>
-//      <button
-//         className='nav-link nav-link-signout'
-//         onClick={() => setShow(true)}
-//       >
-//         {t('buttons.sign-out')}
-//       </button>
-//     </li>
-//     <Modal>
-//       <Modal.Header closeButton />
-//         <Modal.Body>
-//           <p>
-//             you will signout in 10 seconds
-//           </p>
-//           <a
-//               className='nav-link nav-link-signout'
-//               href={`${apiLocation}/signout`}
-//               onBlur={this.handleBlur}
-//               onKeyDown={this.handleMenuKeyDown}
-//             >
-//               {t('buttons.sign-out')}
-//           </a>
-//           <button>
-//             Cansel Signing out
-//           </button>
-//        </Modal.Body>
-//     </Modal>
-//   </>
-//   );
+// //   return (
+// //     <li>
+// //      <button
+// //         className='nav-link nav-link-signout'
+// //         onClick={() => setShow(true)}
+// //       >
+// //         {t('buttons.sign-out')}
+// //       </button>
+// //     <Modal>
+// //       <Modal.Header closeButton />
+// //         <Modal.Body>
+// //           <p>
+// //             you will signout in 10 seconds
+// //           </p>
+// //           <a
+// //               className='nav-link nav-link-signout'
+// //               href={`${apiLocation}/signout`}
+// //               onBlur={this.handleBlur}
+// //               onKeyDown={this.handleMenuKeyDown}
+// //             >
+// //               {t('buttons.sign-out')}
+// //           </a>
+// //           <button>
+// //             Cansel Signing out
+// //           </button>
+// //        </Modal.Body>
+// //     </Modal>
+// //     </li>
+// //   );
 // }
 
 // SignOutModal.displayName = 'SignOutModal';
