0664d4498298045a07591300b3d9f721045acade
moT01 <20648924+moT01@users.noreply.github.com>
2022-07-12 13:38:01 -0500

    fix: multifile.js

--- cypress/integration/learn/challenges/multifile.js
+++ cypress/integration/learn/challenges/multifile.js
@@ -2,6 +2,7 @@ import translations from '../../../../client/i18n/locales/english/translations.j
 const location =
   '/learn/2022/responsive-web-design/learn-accessibility-by-building-a-quiz/step-2';
 const selectors = {
+  editor: '.monaco-editor',
   testButton: '#test-button',
   monacoTabs: '.monaco-editor-tabs',
   signInButton: '#action-buttons-container a[href$="/signin"]'
@@ -30,7 +31,11 @@ describe('Challenge with multifile editor', () => {
   // Page will change after this test. If your test requires page used in previous
   // tests make sure it is above this one
   it('prompts unauthenticated user to sign in to save progress', () => {
-    cy.focused().type('{end}{enter}<meta charset="UTF-8" />{ctrl+enter}');
+    cy.get(selectors.editor, { timeout: 15000 })
+      .first()
+      .click()
+      .focused()
+      .type('<meta charset="UTF-8" />{ctrl+enter}');
     cy.get(selectors.signInButton).contains(translations.learn['sign-in-save']);
     cy.get(selectors.signInButton).click();
     cy.go('back');
