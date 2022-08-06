a916b94d1affbf75d7f9cebc36a2ebfd1952ffba
AlreyYoss <shaosian0202@gmail.com>
2022-08-06 03:36:23 +0000

    test: (Beginner) added some test code in extract-html-comments

--- curriculum/test/utils/extract-html-comments.test.js
+++ curriculum/test/utils/extract-html-comments.test.js
@@ -19,3 +19,36 @@ describe('extractHTMLComments', () => {
     expect(extractHTMLComments(someHTML)).toEqual(commentCounts);
   });
 });
+
+// Test with Invalid value
+describe('extractHTMLComments', () => {
+  it('should return an object with comment keys and count values', () => {
+    const commentCounts = {
+      'a comment': 'a',
+      'another comment': 1
+    };
+    expect(extractHTMLComments(someHTML)).toEqual(commentCounts);
+  });
+});
+
+// Test with negative values
+describe('extractHTMLComments', () => {
+  it('should return an object with comment keys and count values', () => {
+    const commentCounts = {
+      'a comment': -2,
+      'another comment': -1
+    };
+    expect(extractHTMLComments(someHTML)).toEqual(commentCounts);
+  });
+});
+
+//Test with null value
+describe('extractHTMLComments', () => {
+  it('should return an object with comment keys and count values', () => {
+    const commentCounts = {
+      'a comment': null,
+      'another comment': null
+    };
+    expect(extractHTMLComments(someHTML)).toEqual(commentCounts);
+  });
+});
