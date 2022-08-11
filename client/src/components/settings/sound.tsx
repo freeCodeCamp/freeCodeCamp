b972141481ea6090efc408b17440e3c7d023c9de
sembauke <semboot699@gmail.com>
2022-08-11 12:09:55 +0200

    feat: volume slider campire mode

--- client/src/components/settings/sound.tsx
+++ client/src/components/settings/sound.tsx
@@ -1,7 +1,11 @@
 import { Form } from '@freecodecamp/react-bootstrap';
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import store from 'store';
 import { useTranslation } from 'react-i18next';
+import { Spacer } from '../helpers';
 
+import './sound.css';
+import { playTone } from '../../utils/tone';
 import ToggleSetting from './toggle-setting';
 
 type SoundProps = {
@@ -14,6 +18,23 @@ export default function SoundSettings({
   toggleSoundMode
 }: SoundProps): JSX.Element {
   const { t } = useTranslation();
+  const [volumeDisplay, setVolumeDisplay] = useState(100);
+
+  function handleVolumeChange(event: ChangeEvent<HTMLInputElement>) {
+    const inputValue = Number(event.target.value);
+
+    store.set('soundVolume', inputValue);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    setVolumeDisplay(store.get('soundVolume') ?? 0);
+
+    void playTone('tests-completed');
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    setVolumeDisplay(store.get('soundVolume') ?? 0);
+  }, []);
 
   return (
     <Form inline={true} onSubmit={(e: React.FormEvent) => e.preventDefault()}>
@@ -28,6 +49,18 @@ export default function SoundSettings({
           toggleSoundMode(sound ? false : true);
         }}
       />
+      <input
+        type='range'
+        min='10'
+        max='100'
+        defaultValue={store.get('soundVolume') as number}
+        className='soundbar'
+        onChange={handleVolumeChange}
+      />
+      <div>
+        {t('settings.sound-volume')} {volumeDisplay}
+      </div>
+      <Spacer></Spacer>
     </Form>
   );
 }
