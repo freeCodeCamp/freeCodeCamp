77697d8fa79c175414ec5eb290c3de3c59fd985e
Sboonny <muhammed@freecodecamp.org>
2022-08-12 12:27:23 +0000

    changed validation format

--- tools/ui-components/src/form-group/form-group.tsx
+++ tools/ui-components/src/form-group/form-group.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import { FormGroupProps } from './types';
+import { FormGroupProps, ValidationStatesProp } from './types';
+
+const ValidationStateClasses: Record<ValidationStatesProp, string> = {
+  success: 'text-green-700 bg-green-50 border-green-100',
+  warning: 'text-blue-700 bg-blue-50 border-blue-100',
+  error: 'text-yellow-700 bg-yellow-50 border-yellow-100'
+};
 
 const FormGroup = React.forwardRef<HTMLLabelElement, FormGroupProps>(
-  ({ children, className, validationState, controlId }): JSX.Element => {
+  ({ children, className, controlId }): JSX.Element => {
     const defaultClasses = 'mb-3.5';
     // ToDo: validationState should change when more Form components added.
     // It should account for input value and add class to formgroup, depending on formgroup state
-    const classes = [defaultClasses, className, validationState].join(' ');
+    const classes = [defaultClasses, className, ValidationStateClasses].join(
+      ' '
+    );
 
     return (
       <div className={classes} id={controlId}>
--- tools/ui-components/src/form-group/types.ts
+++ tools/ui-components/src/form-group/types.ts
@@ -1,8 +1,10 @@
 import React, { ReactNode } from 'react';
 
+export type ValidationStatesProp = 'success' | 'warning' | 'error';
+
 export interface FormGroupProps extends React.HTMLAttributes<HTMLElement> {
   children?: ReactNode;
   className?: string;
   controlId?: string;
-  validationState?: ['success', 'warning', 'error', null];
+  validationState?: ValidationStatesProp;
 }
