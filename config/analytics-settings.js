de346d6499ea48748fb27702baeeee89175c7236
Brooke <brooke.souza@topcoder.com>
2022-08-25 14:00:52 -0700

    TCA-314 #comment This commit removes heap from non-prod environments #time5m

--- config/analytics-settings.js
+++ config/analytics-settings.js
@@ -2,8 +2,7 @@ exports.prodAnalyticsId = null;
 exports.devAnalyticsId = null;
 
 exports.prodSegmentId = '8fCbi94o3ruUUGxRRGxWu194t6iVq9LH';
-// TODO: TCA-371 set this to null so we're not tracking non-prod
-exports.devSegmentId = '8fCbi94o3ruUUGxRRGxWu194t6iVq9LH';
+exports.devSegmentId = null;
 
 exports.prodTagManagerId = 'GTM-MXXQHG8';
 exports.devTagManagerId = 'GTM-W7B537Z';
