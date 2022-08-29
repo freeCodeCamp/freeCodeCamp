1f02742cdd64fbedb9f0f6bde61fd52269fb29c5
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-08-29 15:54:21 +0300

    lint

--- client/src/templates/Challenges/components/prism-formatted.tsx
+++ client/src/templates/Challenges/components/prism-formatted.tsx
@@ -14,24 +14,40 @@ interface PrismFormattedProps {
  * @param showLineNumbers
  * @param darkTheme
  */
-const addFormattingClassesForPres = (container: HTMLElement, showLineNumbers = true, darkTheme = true) => {
-  const codeBlocks: HTMLElement[] = [].slice.call(container.querySelectorAll('[class*="language-"]'));
+const addFormattingClassesForPres = (
+  container: HTMLElement,
+  showLineNumbers = true,
+  darkTheme = true
+) => {
+  const codeBlocks: HTMLElement[] = [].slice.call(
+    container.querySelectorAll('[class*="language-"]')
+  );
   // we want to formatt the <pre> element, not the <code>, get parent if current element is not PRE
-  const preElements: HTMLPreElement[] = codeBlocks.map(c => (c.nodeName === 'PRE' ? c : c.parentElement) as HTMLPreElement);
+  const preElements: HTMLPreElement[] = codeBlocks.map(
+    c => (c.nodeName === 'PRE' ? c : c.parentElement) as HTMLPreElement
+  );
 
-  for(const pre of preElements) {
+  for (const pre of preElements) {
     pre.classList.toggle('line-numbers', showLineNumbers);
     pre.classList.toggle('dark-palette', darkTheme);
   }
-}
+};
 
-function PrismFormatted({ className, text, ...props }: PrismFormattedProps): JSX.Element {
+function PrismFormatted({
+  className,
+  text,
+  ...props
+}: PrismFormattedProps): JSX.Element {
   const instructionsRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Just in case 'current' has not been created, though it should have been.
     if (instructionsRef.current) {
-      addFormattingClassesForPres(instructionsRef.current, props.lineNumbers, props.darkTheme);
+      addFormattingClassesForPres(
+        instructionsRef.current,
+        props.lineNumbers,
+        props.darkTheme
+      );
       Prism.highlightAllUnder(instructionsRef.current);
     }
   }, [props.darkTheme, props.lineNumbers]);
