111c0b7f7d501010f6e63eea931dfea57f9ae988
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2022-09-13 13:39:47 +0300

    feat: pull footer from cdn

---
+++ tools/scripts/build/download-trending.ts
@@ -0,0 +1,32 @@
+import { writeFileSync } from 'fs';
+import fetch from 'node-fetch';
+import yaml from 'js-yaml';
+
+import envData from '../../../config/env.json';
+const { clientLocale } = envData;
+
+const cdnUrlCreator = (lang: string) =>
+  `https://cdn.freecodecamp.org/universal/trending/${lang}.yaml`;
+
+(async () => {
+  const url = cdnUrlCreator('german');
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(
+      `
+      ----------------------------------------------------
+      Error: The CDN is missing the trending YAML file.
+      ----------------------------------------------------
+      Unable to fetch the ${clientLocale} footer: ${res.statusText}
+      `
+    );
+  }
+
+  const data = await res.text();
+  const trendingJSON = JSON.stringify(yaml.load(data));
+  const trendingLocation = `./client/i18n/locales/${clientLocale}/trending.json`;
+  console.log(trendingJSON);
+  writeFileSync(trendingLocation, trendingJSON);
+})().catch(err => {
+  console.log(err);
+});
