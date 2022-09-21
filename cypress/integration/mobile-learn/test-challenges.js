968cee348f81ce7e25809c46de2fb809b8274aab
Niraj Nandish <nirajnandish@icloud.com>
2022-09-22 03:38:28 +0530

    feat: test all generated challenges

--- cypress/integration/mobile-learn/test-challenges.js
+++ cypress/integration/mobile-learn/test-challenges.js
@@ -1,16 +1,28 @@
-let superBlock = '2022/responsive-web-design';
-let block = 'learn-html-by-building-a-cat-photo-app';
-let challenges = ['5dc17d3bf86c76b9248c6eb4', '5dc17dc8f86c76b9248c6eb5'];
+import currData from '../../../config/curriculum.json';
+import { orderedSuperBlockInfo } from '../../../tools/scripts/build/build-external-curricula-data';
+
+const publicSB = orderedSuperBlockInfo
+  .filter(sb => sb.public === true)
+  .map(sb => sb.dashedName);
 
 describe('Test challenges in mobile', () => {
-  for (let challenge of challenges) {
-    it('Challenge: ${challenge}', () => {
-      cy.visit(`/${superBlock}/${block}/${challenge}`, {
-        onBeforeLoad(win) {
-          cy.spy(win.console, 'log').as('consoleLog');
+  for (let superBlock of publicSB) {
+    for (let currBlock of Object.values(currData[superBlock]['blocks'])) {
+      describe(`Block ${currBlock['meta']['name']}`, () => {
+        for (let currChallenge of currBlock['challenges']) {
+          it(`Challenge: ${currChallenge['title']}(${currChallenge['id']})`, () => {
+            cy.visit(
+              `/${superBlock}/${currChallenge['block']}/${currChallenge['id']}`,
+              {
+                onBeforeLoad(win) {
+                  cy.spy(win.console, 'log').as('consoleLog');
+                }
+              }
+            );
+            cy.get('@consoleLog').should('be.calledWith', 'completed');
+          });
         }
       });
-      cy.get('@consoleLog').should('be.calledWith', 'completed');
-    });
+    }
   }
 });
