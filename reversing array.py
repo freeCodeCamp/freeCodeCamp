e2aa6535eff507e31cea2df015973837d030d7b9
NIVED P V <92683901+nivedpv21040@users.noreply.github.com>
2022-10-01 23:27:10 +0530

    reversing array

---
+++ reversing array.py
@@ -0,0 +1,6 @@
+from array import *
+array_num = array('i', [1, 3, 5, 3, 7, 1, 9, 3])
+print("Original array: "+str(array_num))
+array_num.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))
