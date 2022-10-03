efac8acfde7376e86f025d5a61513566d52effdc
Danny Greenberg <greenberg08@gmail.com>
2022-10-03 16:07:47 -0400

    Include BoldItalic font

--- client/src/components/layouts/default.tsx
+++ client/src/components/layouts/default.tsx
@@ -8,9 +8,11 @@ import { createSelector } from 'reselect';
 import latoBoldURL from '../../../static/fonts/lato/Lato-Bold.woff';
 import latoLightURL from '../../../static/fonts/lato/Lato-Light.woff';
 import latoRegularURL from '../../../static/fonts/lato/Lato-Regular.woff';
+import hackZeroSlashBoldItalicURL from '../../../static/fonts/hack-zeroslash/Hack-ZeroSlash-BoldItalic.woff';
 import hackZeroSlashBoldURL from '../../../static/fonts/hack-zeroslash/Hack-ZeroSlash-Bold.woff';
 import hackZeroSlashItalicURL from '../../../static/fonts/hack-zeroslash/Hack-ZeroSlash-Italic.woff';
 import hackZeroSlashRegularURL from '../../../static/fonts/hack-zeroslash/Hack-ZeroSlash-Regular.woff';
+
 import { isBrowser } from '../../../utils';
 import {
   fetchUser,
@@ -192,6 +194,13 @@ class DefaultLayout extends Component<DefaultLayoutProps> {
             as='font'
             crossOrigin='anonymous'
             href={hackZeroSlashItalicURL}
+            rel='preload'
+            type='font/woff'
+          />
+          <link
+            as='font'
+            crossOrigin='anonymous'
+            href={hackZeroSlashBoldItalicURL}
             rel='preload'
             type='font/woff'
           />
