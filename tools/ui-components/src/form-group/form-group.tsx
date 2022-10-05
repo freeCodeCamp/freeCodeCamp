4cd58052f8ab7374adafcf563f2bd25253ec1a1c
Sboonny <muhammed@freecodecamp.org>
2022-10-05 00:02:43 +0000

    WIP: valdiation state

--- tools/ui-components/src/form-group/form-group.tsx
+++ tools/ui-components/src/form-group/form-group.tsx
@@ -1,30 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect } from 'react';
 
 import { FormGroupProps } from './types';
 
 const FormGroup = React.forwardRef<HTMLLabelElement, FormGroupProps>(
-  (
-    { children, className, validationState, controlId },
-    isValid
-  ): JSX.Element => {
-    const defaultClasses = 'mb-3.5';
-
+  ({ children, className, validationState, controlId }): JSX.Element => {
+    const classes = [defaultClasses, className];
     useEffect(() => {
-      const controlLabel = document
-        .querySelector('input')
-        ?.getAttribute('value');
-      if (controlLabel === isValid) {
-        validationState = ['success'];
-      } else {
-        validationState = ['warning'];
-      }
-    }, [validationState]);
-
-    const classes = [defaultClasses, className, validationState].join(' ');
+      let defaultClasses = 'mb-3.5';
+      if (validationState === 'success') defaultClasses = 'mb-3.5 success';
+      else if (validationState === 'warning') defaultClasses = 'mb-3.5 warning';
+      else if (validationState === 'error') defaultClasses = 'mb-3.5 error';
+      else defaultClasses = 'mb-3.5';
+    }, [classes]);
 
     return (
-      <div className={classes} id={controlId}>
+      <div className={classes.join()} id={controlId}>
         {children}
       </div>
     );
