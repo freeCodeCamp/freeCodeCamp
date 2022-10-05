8aedde8d2a5d5afc902234ac8bf269434849c83d
Sboonny <muhammed@freecodecamp.org>
2022-10-05 00:14:43 +0000

    WIP: validation state

--- tools/ui-components/src/form-group/form-group.tsx
+++ tools/ui-components/src/form-group/form-group.tsx
@@ -1,18 +1,22 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 
 import { FormGroupProps } from './types';
 
 const FormGroup = React.forwardRef<HTMLLabelElement, FormGroupProps>(
   ({ children, className, validationState, controlId }): JSX.Element => {
-    const classes = [defaultClasses, className];
+    const classes = [className];
     useEffect(() => {
-      let defaultClasses = 'mb-3.5';
-      if (validationState === 'success') defaultClasses = 'mb-3.5 success';
-      else if (validationState === 'warning') defaultClasses = 'mb-3.5 warning';
-      else if (validationState === 'error') defaultClasses = 'mb-3.5 error';
-      else defaultClasses = 'mb-3.5';
+      const setDefaultClass = () => {
+        let defaultClasses = 'mb-3.5';
+        if (validationState === 'success') defaultClasses = 'mb-3.5 success';
+        else if (validationState === 'warning')
+          defaultClasses = 'mb-3.5 warning';
+        else if (validationState === 'error') defaultClasses = 'mb-3.5 error';
+        else defaultClasses = 'mb-3.5';
+        return classes.concat([defaultClasses]);
+      };
     }, [classes]);
 
     return (
