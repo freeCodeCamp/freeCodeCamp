66e06ec1c41c795692aef5b4dd3a76c70d4b2900
Sboonny <muhammed@freecodecamp.org>
2022-10-08 09:28:37 +0000

    WIP: Add validationstate story in formgroup stor

--- tools/ui-components/src/form-group/form-group.stories.tsx
+++ tools/ui-components/src/form-group/form-group.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import { Button } from '../button';
+import { FormControl } from '../form-control';
 import { FormGroup, FormGroupProps } from '.';
 
 const story = {
@@ -13,8 +15,52 @@ const story = {
   }
 };
 
-const Template: Story<FormGroupProps> = args => {
-  return <FormGroup {...args} />;
+// function getValidationForConfirmEmail() {
+//   const { confirmNewEmail, newEmail } = emailForm;
+//   if (!maybeEmailRE.test(newEmail)) {
+//     return {
+//       state: null,
+//       message: ''
+//     };
+//   }
+//   const isMatch = newEmail === confirmNewEmail;
+//   if (maybeEmailRE.test(confirmNewEmail)) {
+//     return {
+//       state: isMatch ? 'success' : 'error',
+//       message: isMatch ? '' : t('validation.email-mismatch')
+//     };
+//   } else {
+//     return {
+//       state: null,
+//       message: ''
+//     };
+//   }
+// }
+const Template: Story<FormGroupProps> = ({
+  controlId,
+  className,
+  validationState
+}) => {
+  return (
+    <div>
+      {/* when a function runs and confirm a value it should push vaidation state into formGroup */}
+      <Button onClick={() => (validationState = 'success')}>Success</Button>
+      <Button onClick={() => (validationState = 'warning')}>Warning</Button>
+      <Button onClick={() => (validationState = 'error')}>Error</Button>
+      <FormGroup
+        controlId={controlId}
+        className={className}
+        validationState={validationState}
+      >
+        <FormControl
+          type={'text'}
+          placeholder='Hello World'
+          value='Hello World'
+          required={true}
+        />
+      </FormGroup>
+    </div>
+  );
 };
 
 export const Default = Template.bind({});
