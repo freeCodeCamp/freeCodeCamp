5be9570e9f46876439bd508788c43e031e700a6f
Shaun Hamilton <shauhami020@gmail.com>
2022-10-12 14:52:01 +0000

    chore(client): remove ts-disables in growth-book-wrapper

--- client/src/components/growth-book/growth-book-wrapper.tsx
+++ client/src/components/growth-book/growth-book-wrapper.tsx
@@ -1,18 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import React, { ReactNode, useEffect } from 'react';
 import sha1 from 'sha-1';
-import { GrowthBook, GrowthBookProvider } from '@growthbook/growthbook-react';
-
+import {
+  FeatureDefinition,
+  GrowthBook,
+  GrowthBookProvider
+} from '@growthbook/growthbook-react';
 import { connect } from 'react-redux';
-
 import { createSelector } from 'reselect';
-
 import { isSignedInSelector, userSelector } from '../../redux/selectors';
-
 import envData from '../../../../config/env.json';
 import {
   prodGrowthbookKey,
@@ -20,20 +15,14 @@ import {
 } from '../../../../config/growthbook-settings';
 import { User } from '../../redux/prop-types';
 
-const {
-  deploymentEnv,
-  clientLocale
-}: { deploymentEnv: 'staging' | 'live'; clientLocale: string } = envData as {
-  deploymentEnv: 'staging' | 'live';
-  clientLocale: string;
-};
+const { deploymentEnv, clientLocale } = envData;
 
 const growthbook = new GrowthBook();
 
 const mapStateToProps = createSelector(
   isSignedInSelector,
   userSelector,
-  (isSignedIn, user) => ({
+  (isSignedIn, user: User) => ({
     isSignedIn,
     user
   })
@@ -59,11 +48,14 @@ const GrowthBookWrapper = ({
 
       const apiEndPoint = `https://api.gb.freecodecamp.org/api/features/${growthBookKey}`;
 
-      void fetch(apiEndPoint)
-        .then(res => res.json())
-        .then(json => {
-          growthbook.setFeatures(json.features);
-        });
+      void (async () => {
+        const res = await fetch(apiEndPoint);
+        const data = (await res.json()) as {
+          features: Record<string, FeatureDefinition>;
+        };
+        growthbook.setFeatures(data.features);
+      })();
+
       growthbook.setAttributes({
         id: sha1(user.email),
         staff: true,
