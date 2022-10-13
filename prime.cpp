c3d45890db8db03091408f1eba3d073dff18706d
darkmiracle2023 <115684369+darkmiracle2023@users.noreply.github.com>
2022-10-13 10:22:45 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int i, n;
+  bool is_prime = true;
+
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  // loop to check if n is prime
+  for (i = 2; i <= n/2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
+
+  if (is_prime)
+    cout << n << " is a prime number";
+  else
+    cout << n << " is not a prime number";
+
+  return 0;
+}
