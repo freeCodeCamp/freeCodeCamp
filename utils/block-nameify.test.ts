981cdc9640547dcebb1090a3f902fdcc25f9ee64
Shaun Hamilton <shauhami020@gmail.com>
2022-10-17 19:34:18 +0100

    fix test types [skip ci]

--- utils/block-nameify.test.ts
+++ utils/block-nameify.test.ts
@@ -7,17 +7,21 @@ describe('blockNameify', () => {
   });
 
   it('should not format prepositions', () => {
-    const result = blockNameify('and-for-of-the-up-with');
-    expect(result).toBe('and for of the up with');
+    const result = blockNameify('basic-html-and-html5');
+    expect(result).toBe('Basic Html and Html5');
   });
 
   it('should format javascript to JavaScript', () => {
-    const result = blockNameify('javascript');
-    expect(result).toBe('JavaScript');
+    const result = blockNameify('basic-javascript-rpg-game');
+    expect(result).toBe('Basic JavaScript RPG Game');
   });
 
   it('should transform "-" to " " and uppercase each word', () => {
-    const result = blockNameify('hello-world');
-    expect(result).toBe('Hello World');
+    const result = blockNameify(
+      'learn-more-about-css-pseudo-selectors-by-building-a-balance-sheet'
+    );
+    expect(result).toBe(
+      'Learn More About CSS Pseudo Selectors By Building A Balance Sheet'
+    );
   });
 });
