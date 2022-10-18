b14cc4444918b71a20a9e2ddcf1e22ecd59f4600
Anu <91693845+Ladkaherohai@users.noreply.github.com>
2022-10-19 04:14:44 +0530

    Create linear.java
    hacktoberfest

---
+++ linear.java
@@ -0,0 +1,15 @@
+public class LinearSearchExample{    
+public static int linearSearch(int[] arr, int key){    
+        for(int i=0;i<arr.length;i++){    
+            if(arr[i] == key){    
+                return i;    
+            }    
+        }    
+        return -1;    
+    }    
+    public static void main(String a[]){    
+        int[] a1= {10,20,30,50,70,90};    
+        int key = 50;    
+        System.out.println(key+" is found at index: "+linearSearch(a1, key));    
+    }    
+}    
