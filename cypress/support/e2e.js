7f1b136e40e281785f7e48040d6063ce40faf255
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2022-10-21 23:06:43 +0545

    test(e2e): add tests for navigating Menu with keyboard (#47791)

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -13,6 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 import 'cypress-plugin-stripe-elements';
+import 'cypress-plugin-tab';
 
 // Import commands.js using ES2015 syntax:
 import './commands';
