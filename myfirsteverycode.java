4bac0eb1a1fbf474ea8908c0a254fc8eb01854e2
nishitpanchal6377 <116424167+nishitpanchal6377@users.noreply.github.com>
2022-10-22 12:37:49 +0530

    Create myfirsteverycode.java

---
+++ myfirsteverycode.java
@@ -0,0 +1,12 @@
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
