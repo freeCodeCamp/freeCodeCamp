ea0bc358e2b3b25855fdca41a453a8d1961c2060
Upeksha Fernando <upekshafernando18@gmail.com>
2022-10-23 23:04:25 +0530

    upekshaF updating 2022_sample_index_page

---
+++ .github/2022_sample_index_page/sign_in_form.js
@@ -0,0 +1,9 @@
+// Sign In Form
+
+function openForm(){
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm(){
+    document.getElementById("myForm").style.display = "none";
+}
