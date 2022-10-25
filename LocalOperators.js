9c61d3d15cf92156f2f4165d47f0a2aa11270e07
MyName49 <33026946+MyName49@users.noreply.github.com>
2022-10-25 17:12:26 +0700

    Create LocalOperators.js

---
+++ LocalOperators.js
@@ -0,0 +1,23 @@
+let a = 10;
+let b = 12;
+
+/* AND operator */
+console.log(a < 15 && b > 10); // (true && true) -> true
+console.log(a > 15 && b > 10); // (false && true) -> false
+
+/* OR operator */
+console.log(a < 15 || b > 10); // (true || true) -> true
+console.log(a > 15 || b > 10); // (false || true) -> true
+
+/* NOT operator */
+console.log(!(a < 15)); // !(true) -> false
+console.log(!(a < 15 && b > 10)); // !(true && true) -> !(true) -> false
+
+/* output
+true
+false
+true
+true
+false
+false
+*/
