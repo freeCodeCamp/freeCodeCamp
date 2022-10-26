e07e95b3833627397475f42b370ca1c572c50875
Muhammed Mustafa <MuhammedElruby@gmail.com>
2022-10-26 12:32:00 +0200

    broader expected result type
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/components/helpers/link.tsx
+++ client/src/components/helpers/link.tsx
@@ -1,7 +1,7 @@
 import { GatsbyLinkProps, Link as GatsbyLink } from 'gatsby';
 import React from 'react';
 
-interface LinkProps extends Omit<GatsbyLinkProps<string>, 'ref'> {
+interface LinkProps extends Omit<GatsbyLinkProps<Record<string, unknown>>, 'ref'> {
   children?: React.ReactNode;
   external?: boolean;
   sameTab?: boolean;
