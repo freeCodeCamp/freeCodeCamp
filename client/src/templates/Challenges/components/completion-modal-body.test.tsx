70c937e2d3bfb4acfee27ec9fc95a93fb0fee7bd
Shaun Hamilton <shauhami020@gmail.com>
2022-10-26 15:24:38 +0000

    fix: update test and snapshot
    [skip ci]

--- client/src/templates/Challenges/components/completion-modal-body.test.tsx
+++ client/src/templates/Challenges/components/completion-modal-body.test.tsx
@@ -8,7 +8,9 @@ import CompletionModalBody from './completion-modal-body';
 const props = {
   block: 'basic-html-and-html5',
   completedPercent: Math.floor(Math.random() * 101),
-  completedProjects: 'Completed 5 of 10 challenges',
+  completedChallengesInBlock: 2,
+  totalChallengesInBlock: 5,
+  currentChallengeId: '',
   superBlock: SuperBlocks.RespWebDesign
 };
 
