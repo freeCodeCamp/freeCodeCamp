b7dbf404a4f170861288753eb5148d95fc5fb009
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2022-10-28 00:32:55 +0530

    Create FabNumber.py

---
+++ FabNumber.py
@@ -0,0 +1,17 @@
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
