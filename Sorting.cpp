c58dd901200d8a8f454db833ebee03a1adbda7f0
ypro <106179997+ypro-cs1@users.noreply.github.com>
2022-10-29 00:55:25 +0530

    Insertion sort

---
+++ Sorting.cpp
@@ -0,0 +1,50 @@
+// C++ program for insertion sort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to sort an array using
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+		// Move elements of arr[0..i-1],
+		// that are greater than key, to one
+		// position ahead of their
+		// current position
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+// A utility function to print an array
+// of size n
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6 };
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	insertionSort(arr, N);
+	printArray(arr, N);
+
+	return 0;
+}
+// This is code is contributed by rathbhupendra
