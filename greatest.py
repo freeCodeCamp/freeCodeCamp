067d48afe22d07cb78f263d967015e13d3815fe3
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2022-10-31 19:52:23 +0530

    Create greatest.py
    this is the program to find greatest among 3 numbers first we check num1 is greater than other 2 numbers if it is false we check whether num2 is greater than other two numbers if it is again false we declare that num3 is the greatest.

---
+++ greatest.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+print("The largest of all number is", largest)
