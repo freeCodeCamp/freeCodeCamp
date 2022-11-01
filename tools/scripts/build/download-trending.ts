a1a33501385db0c3f14272f14973223ce77c778e
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2022-11-01 15:20:51 +0900

    fix: download trending.json based on clientLocale, cleanup

--- tools/scripts/build/download-trending.ts
+++ tools/scripts/build/download-trending.ts
@@ -9,7 +9,7 @@ const cdnUrlCreator = (lang: string) =>
   `https://cdn.freecodecamp.org/universal/trending/${lang}.yaml`;
 
 (async () => {
-  const url = cdnUrlCreator('german');
+  const url = cdnUrlCreator(clientLocale);
   const res = await fetch(url);
   if (!res.ok) {
     throw new Error(
@@ -25,7 +25,6 @@ const cdnUrlCreator = (lang: string) =>
   const data = await res.text();
   const trendingJSON = JSON.stringify(yaml.load(data));
   const trendingLocation = `./client/i18n/locales/${clientLocale}/trending.json`;
-  console.log(trendingJSON);
   writeFileSync(trendingLocation, trendingJSON);
 })().catch(err => {
   console.log(err);
