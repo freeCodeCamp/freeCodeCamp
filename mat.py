06e89db0acfb2ed3f7277879717c49513e3185a1
sanidhya3001 <117115879+sanidhya3001@users.noreply.github.com>
2022-11-03 22:08:15 +0530

    Create mat.py

---
+++ mat.py
@@ -0,0 +1,14 @@
+A = [[1, 4, 5, 12], 
+    [-5, 8, 9, 0],
+    [-6, 7, 11, 19]]
+
+print("A =", A) 
+print("A[1] =", A[1])      # 2nd row
+print("A[1][2] =", A[1][2])   # 3rd element of 2nd row
+print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
+
+column = [];        # empty list
+for row in A:
+  column.append(row[2])   
+
+print("3rd column =", column)
