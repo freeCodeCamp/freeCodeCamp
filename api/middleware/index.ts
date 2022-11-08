1731baa4a452dfc85adce890f71136f10074b26c
Shaun Hamilton <shauhami020@gmail.com>
2022-11-08 19:09:59 +0000

    feat(api): example middleware (#48434)
    * feat(api): add middleware example
    
    * feat(api): add `@fastify/middie`, reorder for alphabetness
    
    * [skip-ci] [skip ci]

--- api/middleware/index.ts
+++ api/middleware/index.ts
@@ -1,3 +1,16 @@
+import { NextFunction } from '../utils';
+
 export async function auth0Verify() {
   // Verify user authorization code with Auth0
+}
+
+export function testMiddleware(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
+  //
+  console.log('Test middleware running');
+  console.log(req.headers);
+  next();
 }
