f6a95023467a624e72cd2631ddc8a175aa18f724
Sboonny <muhammed@freecodecamp.org>
2022-11-11 07:05:17 +0000

    WIP: add children style

--- tools/ui-components/src/form-group/form-group.tsx
+++ tools/ui-components/src/form-group/form-group.tsx
@@ -3,6 +3,27 @@ import { FormContext } from '../form-context';
 import { FormGroupProps } from './types';
 
 let variantClass = '';
+const hasSuccess = `
+[&_.help-block, &_.control-label, &_.radio, &_.checkbox, &_.radio-inline, &_.checkbox-inline, &_.help-block, &_.help-block, &_.help-block,]: color-var(--highlight-color) 
+[&.form-control]:bordercolor-var(--quaternary-background) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075) 
+[&.form-control:focus]:bordercolor-var(--tertiary-color) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 6px #67b168 
+[&.input-group-addon]: color-#3c763d background-color-#dff0d8 border-color-#3c763d
+[&.form-control-feedback]: color-#3c763d
+`;
+const hasWarning = `
+[&_.help-block, &_.control-label, &_.radio, &_.checkbox, &_.radio-inline, &_.checkbox-inline, &_.help-block, &_.help-block, &_.help-block,]: color-var(--highlight-color) 
+[&.form-control]:bordercolor-var(--quaternary-background) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075) 
+[&.form-control:focus]:bordercolor-var(--tertiary-color) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 6px #67b168 
+[&.input-group-addon]: color-#3c763d background-color-#dff0d8 border-color-#3c763d
+[&.form-control-feedback]: color-#3c763d
+`;
+const hasError = `
+[&_.help-block, &_.control-label, &_.radio, &_.checkbox, &_.radio-inline, &_.checkbox-inline, &_.help-block, &_.help-block, &_.help-block,]: var(--danger-color) 
+[&.form-control]:bordercolor-var(--quaternary-background) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075) 
+[&.form-control:focus]:bordercolor-var(--tertiary-color) box-shadow-inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 6px #67b168 
+[&.input-group-addon]: color-#8a6d3b background-color-#fcf8e3 border-color-#8a6d3b
+[&.form-control-feedback]: color-#8a6d3b
+`;
 
 const FormGroup = React.forwardRef<HTMLDivElement, FormGroupProps>(
   (
@@ -21,9 +42,9 @@ const FormGroup = React.forwardRef<HTMLDivElement, FormGroupProps>(
     // this works on render but the class doesn't change
     useEffect(() => {
       const setDefaultClass = (validValue?: string) => {
-        if (validValue === 'success') return (variantClass = 'bg-green');
-        else if (validValue === 'warning') return (variantClass = 'bg-blue');
-        else if (validValue === 'error') return (variantClass = 'bg-red');
+        if (validValue === 'success') return (variantClass = hasSuccess);
+        else if (validValue === 'warning') return (variantClass = hasWarning);
+        else if (validValue === 'error') return (variantClass = hasError);
       };
       setDefaultClass(validationState);
     }, [validationState]);
