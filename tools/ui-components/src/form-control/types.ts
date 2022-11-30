9a7edf196dec6b0febda9197ad52fce0a5d6c7a9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-11-30 13:08:17 +0100

    feat: use xor type to allow specific prop sets

--- tools/ui-components/src/form-control/types.ts
+++ tools/ui-components/src/form-control/types.ts
@@ -2,20 +2,34 @@ import React from 'react';
 
 export type FormControlElement = HTMLInputElement | HTMLTextAreaElement;
 
-export interface FormControlProps
-  extends React.HTMLAttributes<FormControlElement> {
-  className?: string;
-  id?: string;
-  testId?: string;
-  onChange?: React.ChangeEventHandler<FormControlElement>;
-  value?: string;
-  componentClass?: typeof React.Component;
-  placeholder?: string;
-  name?: string;
-  required?: boolean;
-  rows?: number;
-  type?: 'text' | 'email' | 'url';
-}
+type Values =
+  | {
+      value: string;
+      onChange: (event: React.ChangeEvent<FormControlElement>) => void;
+      defaultValue?: never;
+    }
+  | {
+      value?: never;
+      onChange?: never;
+      defaultValue: string;
+    }
+  | {
+      value: string;
+      readOnly: true;
+    };
+
+export type FormControlProps = Values &
+  React.HTMLAttributes<FormControlElement> & {
+    className?: string;
+    id?: string;
+    testId?: string;
+    componentClass?: typeof React.Component;
+    placeholder?: string;
+    name?: string;
+    required?: boolean;
+    rows?: number;
+    type?: 'text' | 'email' | 'url';
+  };
 
 export interface FormControlVariationProps {
   className?: string;
