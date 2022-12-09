7640c5baa82599c5d24dfa11ceb0b403c7c88c89
Sboonny <muhammed@freecodecamp.org>
2022-12-09 14:57:32 +0000

    Added null and use elements to select the styles

--- tools/ui-components/src/form-group/types.ts
+++ tools/ui-components/src/form-group/types.ts
@@ -4,5 +4,5 @@ export interface FormGroupProps extends React.HTMLAttributes<HTMLElement> {
   componentClass?: typeof React.Component;
   className?: string;
   controlId?: string;
-  validationState?: 'success' | 'warning' | 'error';
+  validationState?: 'success' | 'warning' | 'error' | null;
 }
