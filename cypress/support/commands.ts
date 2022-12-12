c6ed8282775512fdf79366dbc5e03856a80cd4c0
sembauke <semboot699@gmail.com>
2022-12-12 09:55:28 +0100

    feat(Cypress): give basic structure to cypress typescript files

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,38 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => {});
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add(
-//   'drag',
-//   { prevSubject: 'element' },
-//   (subject, options) => {}
-// );
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add(
-//   'dismiss',
-//   { prevSubject: 'optional' },
-//   (subject, options) => {}
-// );
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => {});
-
 Cypress.Commands.add('login', () => {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   cy.visit(`${Cypress.env('API_LOCATION')}/signin`);
   cy.contains('Welcome back');
 });
