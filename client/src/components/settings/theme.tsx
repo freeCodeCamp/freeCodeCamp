36e18fb8a029f15eb9d7f237b08082429698ce10
Sboonny <muhammed@freecodecamp.org>
2022-12-15 16:35:01 +0000

    Create three button, two of them change the theme

--- client/src/components/settings/theme.tsx
+++ client/src/components/settings/theme.tsx
@@ -1,12 +1,21 @@
-import { Form } from '@freecodecamp/react-bootstrap';
+import { Form, Button } from '@freecodecamp/react-bootstrap';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import Spacer from '../../assets/icons/spacer';
+import ToggleCheck from '../../assets/icons/toggle-check';
 import ToggleTheme from './ToggleTheme';
+
+const checkIconStyle = {
+  height: '15px',
+  paddingTop: '5',
+  width: '20px'
+};
 
 export enum Themes {
   Night = 'night',
-  Default = 'default'
+  Default = 'default',
+  System = 'system'
 }
 
 type ThemeProps = {
@@ -36,6 +45,48 @@ export default function ThemeSettings({
           );
         }}
       />
+      <Button
+        className={`${
+          currentTheme !== 'night' ? 'toggle-not-active' : 'toggle-active'
+        }`}
+        disabled={currentTheme === 'night'}
+        onClick={() => toggleNightMode((currentTheme = Themes.Night))}
+      >
+        {currentTheme === 'night' ? (
+          <ToggleCheck style={checkIconStyle} />
+        ) : (
+          <Spacer style={checkIconStyle} />
+        )}
+        dark
+      </Button>
+      <Button
+        className={`${
+          currentTheme !== 'default' ? 'toggle-not-active' : 'toggle-active'
+        }}`}
+        disabled={currentTheme === 'default'}
+        onClick={() => toggleNightMode((currentTheme = Themes.Default))}
+      >
+        light
+        {currentTheme ? (
+          <ToggleCheck style={checkIconStyle} />
+        ) : (
+          <Spacer style={checkIconStyle} />
+        )}
+      </Button>
+      <Button
+        className={`${
+          currentTheme !== null ? 'toggle-not-active' : 'toggle-active'
+        }`}
+        disabled={currentTheme === null}
+        onClick={() => toggleNightMode((currentTheme = Themes.System))}
+      >
+        system
+        {currentTheme === null ? (
+          <ToggleCheck style={checkIconStyle} />
+        ) : (
+          <Spacer style={checkIconStyle} />
+        )}
+      </Button>
     </Form>
   );
 }
