a555e27e41c3d4252eaa46bf95c4471772651292
Tom Fattah <35440960+t-fa@users.noreply.github.com>
2022-12-14 19:29:20 -0500

    Update utils/validate.ts
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- utils/validate.ts
+++ utils/validate.ts
@@ -14,8 +14,6 @@ export const usernameIsHttpStatusCode = {
   error: 'is a reserved error code'
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export const isNumeric = (num: any) => !isNaN(num);
 const validCharsRE = /^[a-zA-Z0-9\-_+]*$/;
 export const isHttpStatusCode = (str: string) =>
   isNumeric(str) && parseInt(str, 10) >= 100 && parseInt(str, 10) <= 599;
