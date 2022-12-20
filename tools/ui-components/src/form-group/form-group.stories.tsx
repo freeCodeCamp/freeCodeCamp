9d2d1bed1e0aee90d27e8047cfeeece8e4db93a6
Sboonny <muhammed@freecodecamp.org>
2022-12-20 13:43:50 +0000

    Clean the type and story

--- tools/ui-components/src/form-group/form-group.stories.tsx
+++ tools/ui-components/src/form-group/form-group.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { Button } from '../button';
 import { FormControl } from '../form-control';
 
 import { FormGroup, FormGroupProps } from '.';
@@ -22,27 +21,16 @@ const Template: Story<FormGroupProps> = ({
   validationState
 }) => {
   return (
-    <div>
-      {/* when a function runs and confirm a value it should push vaidation state into formGroup */}
-      <Button onClick={() => (validationState = 'success')}>Success</Button>
-      <Button onClick={() => (validationState = 'warning')}>Warning</Button>
-      <Button onClick={() => (validationState = 'error')}>Error</Button>
-      <FormGroup
-        controlId={controlId}
-        className={className}
-        validationState={validationState}
-      >
-        <FormControl
-          type={'text'}
-          placeholder='Hello World'
-          defaultValue='Hello World'
-          required={true}
-        >
-          {' '}
-          HelloWorld
-        </FormControl>
-      </FormGroup>
-    </div>
+    <FormGroup
+      controlId={controlId}
+      className={className}
+      validationState={validationState}
+    >
+      <FormControl type={'text'} placeholder='Hello World'>
+        {' '}
+        HelloWorld
+      </FormControl>
+    </FormGroup>
   );
 };
 
--- tools/ui-components/src/form-group/types.ts
+++ tools/ui-components/src/form-group/types.ts
@@ -2,7 +2,6 @@ import React from 'react';
 
 export interface FormGroupProps extends React.HTMLAttributes<HTMLElement> {
   componentClass?: typeof React.Component;
-  className?: string;
   controlId?: string;
   validationState?: 'success' | 'warning' | 'error' | null;
 }
