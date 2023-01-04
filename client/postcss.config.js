416e3de173c152307767b81a75fbe29e6a045b98
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2023-01-04 20:16:56 +0900

    fix: refactor the searchbar from flex to grid

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: {
     autoprefixer: {
       overrideBrowserslist: ['last 2 versions']
+    },
+    'postcss-preset-env': {
+      features: {
+        'has-pseudo-class': true
+      }
     }
   }
 };
