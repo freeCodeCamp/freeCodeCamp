dead9684e223a14647251f2095b7c47fab008755
Niraj Nandish <nirajnandish@icloud.com>
2023-01-04 16:24:03 +0400

    fix(api): no response body
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- api-server/src/server/boot/authentication.js
+++ api-server/src/server/boot/authentication.js
@@ -225,8 +225,8 @@ function mobileLogin(app) {
 
     return User.findOne$({ where: { email: email } })
       .do(async user => {
-        const token = await user.mobileLoginByRequest(req, res);
-        return res.json(token);
+        await user.mobileLoginByRequest(req, res);
+        res.end();
       })
       .subscribe(() => {}, next);
   };
