236591c393a8933f187380af0ece14082f2c874d
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2023-01-05 16:43:32 +0900

    fix: refactor the way of displaying searchbar input outline

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,11 +2,6 @@ module.exports = {
   plugins: {
     autoprefixer: {
       overrideBrowserslist: ['last 2 versions']
-    },
-    'postcss-preset-env': {
-      features: {
-        'has-pseudo-class': true
-      }
     }
   }
 };
