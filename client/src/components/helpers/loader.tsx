e91a6a4b6990eb652e324556bf42de1e9444283f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-01-05 10:37:09 +0100

    feat: show error if page fails to load

--- client/src/components/helpers/loader.tsx
+++ client/src/components/helpers/loader.tsx
@@ -9,18 +9,37 @@ interface Props {
 }
 function Loader({ fullScreen, delay }: Props): JSX.Element {
   const [showSpinner, setShowSpinner] = useState(!delay);
+  const [showError, setShowError] = useState(false);
+
   useEffect(() => {
-    if (!showSpinner) {
+    if (fullScreen) {
+      const timerId = setTimeout(() => {
+        setShowError(true);
+        setShowSpinner(false);
+      }, 5000);
+      return () => clearTimeout(timerId);
+    }
+  }, [fullScreen]);
+
+  useEffect(() => {
+    if (delay) {
       const timerId = setTimeout(() => setShowSpinner(true), delay);
       return () => clearTimeout(timerId);
     }
-  }, [setShowSpinner, showSpinner, delay]);
+  }, [delay]);
+
   return (
     <div
       className={`fcc-loader ${fullScreen ? 'full-screen-wrapper' : ''}`}
       data-testid='fcc-loader'
     >
       {showSpinner && <Spinner name='line-scale-pulse-out' />}
+      {showError && (
+        <div>
+          It looks like this is taking too long to load. Please try refreshing
+          the page.
+        </div>
+      )}
     </div>
   );
 }
