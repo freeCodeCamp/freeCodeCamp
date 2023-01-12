95fa8193abf93e4d62839d257412163e84ba1f19
Sboonny <muhammed@freecodecamp.org>
2023-01-12 10:27:15 +0000

    feat(client): clean some extra code in honesty policy section

--- client/src/components/settings/honesty.tsx
+++ client/src/components/settings/honesty.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import HonestyPolicy from '../../resources/honesty-policy';
-import { FullWidthRow } from '../helpers';
+import { FullWidthRow, Spacer } from '../helpers';
 import SectionHeader from './section-header';
 
 import './honesty.css';
@@ -16,13 +16,8 @@ type HonestyProps = {
 const Honesty = ({ isHonest, updateIsHonest }: HonestyProps): JSX.Element => {
   const { t } = useTranslation();
   const button = isHonest ? (
-    <Button
-      block={true}
-      bsStyle='primary'
-      className='disabled-agreed'
-      disabled={true}
-    >
-      <p>{t('buttons.accepted-honesty')}</p>
+    <Button block={true} bsStyle='primary' disabled={true}>
+      {t('buttons.accepted-honesty')}
     </Button>
   ) : (
     <Button
@@ -34,13 +29,13 @@ const Honesty = ({ isHonest, updateIsHonest }: HonestyProps): JSX.Element => {
     </Button>
   );
   return (
-    <section className='honesty-policy' id='honesty-policy'>
+    <section id='honesty-policy'>
       <SectionHeader>{t('settings.headings.honesty')}</SectionHeader>
       <FullWidthRow>
         <Panel className='honesty-panel'>
           <HonestyPolicy />
         </Panel>
-        <br />
+        <Spacer />
         {button}
       </FullWidthRow>
     </section>
