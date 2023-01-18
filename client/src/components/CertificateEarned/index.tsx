ecaf687153093a205025dd1d2cddbc27fcef34b6
Billy Arante <arantebillywilson@gmail.com>
2023-01-18 10:03:02 +0800

    feat(component): display the check mark if cert was earned

--- client/src/components/CertificateEarned/index.tsx
+++ client/src/components/CertificateEarned/index.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
+import { createSelector } from 'reselect';
+import { connect } from 'react-redux';
 import { SuperBlocks } from '../../../../config/certification-settings';
 import ToggleCheck from '../../assets/icons/toggle-check';
+import {
+  isSignedInSelector,
+  userSelector,
+  userFetchStateSelector
+} from '../../redux/selectors';
+
+interface FetchState {
+  pending: boolean;
+  complete: boolean;
+  errored: boolean;
+}
+
+interface User {
+  isRespWebDesignCert: boolean;
+  is2018DataVisCert: boolean;
+  isApisMicroservicesCert: boolean;
+  isBackEndCert: boolean;
+  isDataAnalysisPyCertV7: boolean;
+  isDataVisCert: boolean;
+  isFrontEndCert: boolean;
+  isFrontEndLibsCert: boolean;
+  isFullStackCert: boolean;
+  isInfosecCertV7: boolean;
+  isInfosecQaCert: boolean;
+  isJsAlgoDataStructCert: boolean;
+  isMachineLearningPyCertV7: boolean;
+  isQaCertV7: boolean;
+  isRelationalDatabaseCertV8: boolean;
+  isSciCompPyCertV7: boolean;
+}
+
+const mapStateToProps = createSelector(
+  userFetchStateSelector,
+  isSignedInSelector,
+  userSelector,
+  (fetchState: FetchState, isSignedIn: boolean, user: User) => ({
+    fetchState,
+    isSignedIn,
+    user
+  })
+);
 
 const certsMap = {
   [SuperBlocks.RespWebDesignNew]: false,
@@ -21,12 +64,54 @@ const certsMap = {
 
 interface CertificateEarnedProps {
   superBlock: SuperBlocks;
+  user: User;
 }
 
 const CertificateEarned = (props: CertificateEarnedProps): JSX.Element => {
-  const { superBlock } = props;
+  const { superBlock, user } = props;
+
+  // Update the values of certsMap with fetched user data
+  switch (superBlock) {
+    case SuperBlocks.RespWebDesign:
+    case SuperBlocks.RespWebDesignNew:
+      certsMap[superBlock] = user.isRespWebDesignCert;
+      break;
+    case SuperBlocks.JsAlgoDataStruct:
+    case SuperBlocks.JsAlgoDataStructNew:
+      certsMap[superBlock] = user.isJsAlgoDataStructCert;
+      break;
+    case SuperBlocks.FrontEndDevLibs:
+      certsMap[superBlock] = user.isFrontEndLibsCert;
+      break;
+    case SuperBlocks.DataVis:
+      certsMap[superBlock] = user.isDataVisCert;
+      break;
+    case SuperBlocks.BackEndDevApis:
+      certsMap[superBlock] = user.isBackEndCert;
+      break;
+    case SuperBlocks.RelationalDb:
+      certsMap[superBlock] = user.isRelationalDatabaseCertV8;
+      break;
+    case SuperBlocks.QualityAssurance:
+      certsMap[superBlock] = user.isQaCertV7;
+      break;
+    case SuperBlocks.SciCompPy:
+      certsMap[superBlock] = user.isSciCompPyCertV7;
+      break;
+    case SuperBlocks.DataAnalysisPy:
+      certsMap[superBlock] = user.isDataAnalysisPyCertV7;
+      break;
+    case SuperBlocks.InfoSec:
+      certsMap[superBlock] = user.isInfosecCertV7;
+      break;
+    case SuperBlocks.MachineLearningPy:
+      certsMap[superBlock] = user.isMachineLearningPyCertV7;
+      break;
+    default:
+      break;
+  }
 
   return certsMap[superBlock] ? <ToggleCheck /> : <></>;
 };
 
-export default CertificateEarned;
+export default connect(mapStateToProps)(CertificateEarned);
