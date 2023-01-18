75e6b0681c108046d24b950b5e652b4c1a5afa3f
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2023-01-18 14:19:15 +0300

    feat(client): add ES GTM id

--- client/src/analytics/index.ts
+++ client/src/analytics/index.ts
@@ -2,14 +2,21 @@ import TagManager from 'react-gtm-module';
 
 import {
   devAnalyticsId,
-  prodAnalyticsId
+  prodAnalyticsId,
+  prodAnalyticsESId
 } from '../../../config/analytics-settings';
 
 import envData from '../../../config/env.json';
 
-const { deploymentEnv } = envData;
+const { deploymentEnv, clientLocale } = envData;
 
-const gtmId = deploymentEnv === 'staging' ? devAnalyticsId : prodAnalyticsId;
+const analyticsIDSelecor = () => {
+  if (deploymentEnv === 'staging') return devAnalyticsId;
+  else if (clientLocale === 'espanol') return prodAnalyticsESId;
+  else return prodAnalyticsId;
+};
+
+const gtmId = analyticsIDSelecor();
 
 if (typeof document !== `undefined`) {
   TagManager.initialize({ gtmId });
