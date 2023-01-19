9a0d26fd64a189df674e4e82f0f03b815ad9c219
Bruce Blaser <bbsmooth@gmail.com>
2023-01-19 05:28:17 -0800

    fix: add personal information heading (#49060)

--- client/src/components/settings/about.tsx
+++ client/src/components/settings/about.tsx
@@ -15,6 +15,7 @@ import SoundSettings from './sound';
 import ThemeSettings, { Themes } from './theme';
 import UsernameSettings from './username';
 import KeyboardShortcutsSettings from './keyboard-shortcuts';
+import SectionHeader from './section-header';
 
 type FormValues = {
   name: string;
@@ -212,6 +213,7 @@ class AboutSettings extends Component<AboutProps, AboutState> {
       <div className='about-settings'>
         <UsernameSettings username={username} />
         <br />
+        <SectionHeader>{t('settings.headings.personal-info')}</SectionHeader>
         <FullWidthRow>
           <form id='camper-identity' onSubmit={this.handleSubmit}>
             <FormGroup controlId='about-name'>
