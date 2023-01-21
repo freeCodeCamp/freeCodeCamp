f6544beecce4ed463949cf9292673cad63233824
Sboonny <muhammed@freecodecamp.org>
2023-01-21 10:48:11 +0000

    feat(client): allow full width to carry key props

--- client/src/components/helpers/full-width-row.tsx
+++ client/src/components/helpers/full-width-row.tsx
@@ -1,22 +1,22 @@
 import { Row, Col } from '@freecodecamp/react-bootstrap';
 import React from 'react';
 
-function FullWidthRow({
-  children,
-  className
-}: {
+interface FullWidthRowProps {
   children?: React.ReactNode;
   className?: string;
-}): JSX.Element {
+  key?: string;
+}
+
+export const FullWidthRow = ({
+  children,
+  className,
+  key
+}: FullWidthRowProps) => {
   return (
-    <Row className={className}>
+    <Row className={className} key={key}>
       <Col sm={8} smOffset={2} xs={12}>
         {children}
       </Col>
     </Row>
   );
-}
-
-FullWidthRow.displayName = 'FullWidthRow';
-
-export default FullWidthRow;
+};
