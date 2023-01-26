37b33a4c53a6c1e070b7cc3a9b9c203fc02b31c5
Sam Rice <samsrice@gmail.com>
2023-01-25 17:48:29 -0800

    resolve compiler errors in settings/username-change.ts

--- cypress/e2e/default/settings/username-change.ts
+++ cypress/e2e/default/settings/username-change.ts
@@ -5,7 +5,8 @@ describe('Username input field', () => {
   });
 
   it('Should be possible to type', () => {
-    cy.typeUsername('twaha').should('have.attr', 'value', 'twaha');
+    cy.typeUsername('twaha');
+    cy.get('@usernameInput').should('have.attr', 'value', 'twaha');
   });
 
   it('Should show message when validating name', () => {
