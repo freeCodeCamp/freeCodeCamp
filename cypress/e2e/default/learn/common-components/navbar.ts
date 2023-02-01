d94c8c2b84d3426a4a617d701641eb78b6593b15
Aaron <a.rcottrill521@gmail.com>
2023-01-31 20:06:55 -0500

    Chore: Rename navbar

--- cypress/e2e/default/learn/common-components/navbar.ts
+++ cypress/e2e/default/learn/common-components/navbar.ts
@@ -1,4 +1,4 @@
-const selectors = {
+const navBarselectors = {
   heading: "[data-test-label='landing-header']",
   smallCallToAction: "[data-test-label='landing-small-cta']",
   navigationLinks: '.nav-list',
@@ -8,15 +8,15 @@ const selectors = {
   avatarImage: '.avatar-container .avatar'
 };
 
-let appHasStarted;
-function spyOnListener(win) {
+let appHasStarted: boolean;
+function spyOnListener(win: Cypress.AUTWindow) {
   const addListener = win.EventTarget.prototype.addEventListener;
-  win.EventTarget.prototype.addEventListener = function (name) {
+  win.EventTarget.prototype.addEventListener = function (name: string) {
     if (name === 'click') {
       appHasStarted = true;
       win.EventTarget.prototype.addEventListener = addListener;
     }
-    return addListener.apply(this, arguments);
+    return addListener.apply(this, Array.from(win));
   };
 }
 
@@ -24,7 +24,7 @@ function waitForAppStart() {
   return new Promise(resolve => {
     const isReady = () => {
       if (appHasStarted) {
-        return resolve();
+        return resolve(null);
       }
       return setTimeout(isReady, 0);
     };
@@ -47,12 +47,12 @@ describe('Navbar when logged out', () => {
 
   it(
     'Should have `Sign in` link on landing and learn pages' +
-      ' when not signed in',
+    ' when not signed in',
     () => {
-      cy.contains(selectors.smallCallToAction, 'Sign in');
-      cy.get(selectors.menuButton).click();
-      cy.get(selectors.navigationLinks).contains('Curriculum').click();
-      cy.contains(selectors.smallCallToAction, 'Sign in');
+      cy.contains(navBarselectors.smallCallToAction, 'Sign in');
+      cy.get(navBarselectors.menuButton).click();
+      cy.get(navBarselectors.navigationLinks).contains('Curriculum').click();
+      cy.contains(navBarselectors.smallCallToAction, 'Sign in');
     }
   );
 });
@@ -85,31 +85,31 @@ describe('Navbar Logged in', () => {
   // have the curriculum and CTA on landing and /learn pages.
   it(
     'Should have `Radio`, `Forum`, and `Curriculum` links on landing and learn pages' +
-      'page when not signed in',
+    'page when not signed in',
     () => {
-      cy.get(selectors.menuButton).click();
-      cy.get(selectors.navigationLinks).contains('Forum');
-      cy.get(selectors.navigationLinks).contains('Curriculum').click();
+      cy.get(navBarselectors.menuButton).click();
+      cy.get(navBarselectors.navigationLinks).contains('Forum');
+      cy.get(navBarselectors.navigationLinks).contains('Curriculum').click();
       cy.url().should('include', '/learn');
-      cy.get(selectors.navigationLinks).contains('Curriculum');
-      cy.get(selectors.navigationLinks).contains('Forum');
-      cy.get(selectors.navigationLinks).contains('Radio');
+      cy.get(navBarselectors.navigationLinks).contains('Curriculum');
+      cy.get(navBarselectors.navigationLinks).contains('Forum');
+      cy.get(navBarselectors.navigationLinks).contains('Radio');
     }
   );
 
   it('Should have `Profile` link when user is signed in', () => {
-    cy.get(selectors.menuButton).click();
-    cy.get(selectors.navigationLinks).contains('Profile').click();
+    cy.get(navBarselectors.menuButton).click();
+    cy.get(navBarselectors.navigationLinks).contains('Profile').click();
     cy.url().should('include', '/developmentuser');
   });
 
   it('Should have a profile image with class `default-border`', () => {
-    cy.get(selectors.avatarContainer).should('have.class', 'default-border');
-    cy.get(selectors.defaultAvatar).should('exist');
+    cy.get(navBarselectors.avatarContainer).should('have.class', 'default-border');
+    cy.get(navBarselectors.defaultAvatar).should('exist');
   });
 
   it('Should have a profile image with dimensions that are <= 31px', () => {
-    cy.get(selectors.avatarImage).invoke('width').should('lte', 31);
-    cy.get(selectors.avatarImage).invoke('height').should('lte', 31);
+    cy.get(navBarselectors.avatarImage).invoke('width').should('lte', 31);
+    cy.get(navBarselectors.avatarImage).invoke('height').should('lte', 31);
   });
 });
