534b09826cabf1892278f95d6fff325c136a12d7
Sboonny <muhammed@freecodecamp.org>
2023-02-01 04:13:31 +0000

    refactor(client): delete block save wrapper

--- client/src/components/formHelpers/index.tsx
+++ client/src/components/formHelpers/index.tsx
@@ -1,5 +1,4 @@
 import { default as Form, ValidatedValues } from './form';
 
 export { default as BlockSaveButton } from './block-save-button';
-export { default as BlockSaveWrapper } from './block-save-wrapper';
 export { Form, ValidatedValues };
