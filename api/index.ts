2a1d4ef7fdbd12811d08d732dae2dbc6d816368a
Niraj Nandish <nirajnandish@icloud.com>
2023-02-11 22:41:26 +0530

    feat(next-api): accept privacy endpoint with scopes support

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 config({ path: '../.env' });
 import fastifyAuth0 from 'fastify-auth0-verify';
+import jwtAuthz from 'fastify-jwt-authz';
 import Fastify from 'fastify';
 import middie from '@fastify/middie';
 
@@ -25,6 +26,7 @@ const start = async () => {
     domain: process.env.AUTH0_DOMAIN,
     audience: process.env.AUTH0_AUDIENCE
   });
+  void fastify.register(jwtAuthz);
 
   // Auth0 plugin
   void fastify.register(fastifyAuth0, {
