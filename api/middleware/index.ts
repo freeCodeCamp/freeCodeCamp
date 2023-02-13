4bd30561f488795a2da135b73451f9e62d6f001f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-02-13 12:17:55 +0100

    refactor: clean up types and tsconfig (#49360)

--- api/middleware/index.ts
+++ api/middleware/index.ts
@@ -1,16 +1,20 @@
-import { NextFunction } from '../utils';
+import type { NextFunction, NextHandleFunction } from '@fastify/middie';
 
 export async function auth0Verify() {
   // Verify user authorization code with Auth0
 }
 
+type MiddieRequest = Parameters<NextHandleFunction>[0];
+type MiddieResponse = Parameters<NextHandleFunction>[1];
+
 export function testMiddleware(
-  req: Request,
-  _res: Response,
+  req: MiddieRequest,
+  res: MiddieResponse,
   next: NextFunction
 ) {
-  //
   console.log('Test middleware running');
   console.log(req.headers);
+  console.log(req.query);
+  res.setHeader('X-Test-Header', 'test');
   next();
 }
