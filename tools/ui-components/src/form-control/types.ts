f9c016439012620319494359cc949a089881da42
Sboonny <88248797+Sboonny@users.noreply.github.com>
2023-02-17 10:57:24 +0200

    clean extra export

--- tools/ui-components/src/form-control/types.ts
+++ tools/ui-components/src/form-control/types.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export type FormControlElement = HTMLInputElement | HTMLTextAreaElement;
+type FormControlElement = HTMLInputElement | HTMLTextAreaElement;
 
 type ChangibleValues =
   | {
