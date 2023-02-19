85e9fbb8df8ee582223c69edd8b04391e7d5a59d
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-02-19 13:46:53 +0200

    refactor(client): clean extra component and extra unneeded imports (#49391)
    * feat: remove extra unnecessary code
    
    * clean the unique way that component was handled
    
    * make bsSize optional
    
    * update snapshot to account for the other button styles
    
    * add comment for our future selves
    
    * clean the changes file

--- client/src/components/helpers/form/block-save-button.tsx
+++ client/src/components/helpers/form/block-save-button.tsx
@@ -4,17 +4,21 @@ import { useTranslation } from 'react-i18next';
 
 function BlockSaveButton({
   children,
+  bgSize,
   ...restProps
 }: {
   children?: React.ReactNode;
   disabled?: boolean;
+  bgSize?: string;
 }): JSX.Element {
   const { t } = useTranslation();
 
   return (
     <Button
       block={true}
-      bsSize='lg'
+      // the button is used to submit solutions in projects that require external URL
+      // these buttons don't use bgSize, that's why the bgSize is optional.
+      bsSize={bgSize || 'lg'}
       bsStyle='primary'
       type='submit'
       {...restProps}
