0971b6a8a35246e6196c0e955ca3a46a0308ed80
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-02-22 19:05:08 +0200

    refactor(client): remove react lazy loader library (#49468)
    * feat(client): remove react lazy loader library
    
    * fix: use loading='lazy' to defer loading

--- client/src/components/landing/components/campers-image.tsx
+++ client/src/components/landing/components/campers-image.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Media from 'react-responsive';
 import wideImg from '../../../assets/images/landing/wide-image.png';
-import { Spacer, ImageLoader } from '../../helpers';
+import { Spacer, LazyImage } from '../../helpers';
 
 const LARGE_SCREEN_SIZE = 1200;
 
@@ -24,18 +24,16 @@ const landingImageSize = {
 function CampersImage({ pageName }: CampersImageProps): JSX.Element {
   const { t } = useTranslation();
 
-  const { spacerSize, height, width } =
+  const { spacerSize } =
     pageName === 'donate' ? donateImageSize : landingImageSize;
 
   return (
     <Media minWidth={LARGE_SCREEN_SIZE}>
       <Spacer size={spacerSize} />
-      <ImageLoader
+      <LazyImage
         alt={t('landing.hero-img-description')}
         className='landing-page-image'
-        height={height}
         src={wideImg}
-        width={width}
       />
       <p className='text-center caption'>{t('landing.hero-img-description')}</p>
     </Media>
