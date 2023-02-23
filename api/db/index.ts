047e146968f471635dea375f2c4c8de55316fa3d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-02-23 09:11:30 +0100

    refactor: clean up errors + unused code (#49473)
    * refactor: use await/done to mollify eslint
    
    * refactor: clean up unused code
    
    It's also more of an express pattern. Fastify routes should be
    registered as plugins.

--- api/db/index.ts
+++ api/db/index.ts
@@ -3,10 +3,10 @@ import fastifyMongo from '@fastify/mongodb';
 import { FastifyInstance } from 'fastify';
 
 const URI = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/freecodecamp';
-// eslint-disable-next-line @typescript-eslint/require-await
+
 async function connect(fastify: FastifyInstance) {
   fastify.log.info(`Connecting to : ${URI}`);
-  void fastify.register(fastifyMongo, {
+  await fastify.register(fastifyMongo, {
     url: URI
   });
 }
