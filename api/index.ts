7199f033fb16b7d9127dac556bab62fd664984d5
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2023-02-24 12:36:53 +0530

    feat(next-api): basic authentication setup (#49378)
    * feat(next-api): add fastify-auth0-verify plugin
    
    * feat(next-api): add fastify-jwt-authz plugin
    
    * feat(next-api): accept privacy endpoint with scopes support
    
    * fix(next-api): ignore eslint and ts errors
    
    They will be fixed in a future PR when the package with errors has been updated
    
    Co-authored-by: Niraj Nandish <nirajnandish@icloud.com>
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,13 @@
 import { config } from 'dotenv';
 config({ path: '../.env' });
+import fastifyAuth0 from 'fastify-auth0-verify';
 import Fastify from 'fastify';
 import middie from '@fastify/middie';
+
+import jwtAuthz from './plugins/fastify-jwt-authz';
 import { testRoutes } from './routes/test';
 import { dbConnector } from './db';
-import { testMiddleware } from './middleware';
+import { auth0Verify, testMiddleware } from './middleware';
 
 const fastify = Fastify({
   logger: { level: process.env.NODE_ENV === 'development' ? 'debug' : 'fatal' }
@@ -18,7 +21,17 @@ const start = async () => {
   // NOTE: Awaited to ensure `.use` is registered on `fastify`
   await fastify.register(middie);
 
+  // Auth0 plugin
+  void fastify.register(fastifyAuth0, {
+    domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE
+  });
+  void fastify.register(jwtAuthz);
+
   void fastify.use('/test', testMiddleware);
+
+  // Hooks
+  void fastify.addHook('preValidation', auth0Verify);
 
   void fastify.register(dbConnector);
   void fastify.register(testRoutes);
--- api/routes/test.ts
+++ api/routes/test.ts
@@ -1,4 +1,4 @@
-import { FastifyPluginCallback } from 'fastify';
+import { FastifyPluginCallback, FastifyRequest } from 'fastify';
 
 export const testRoutes: FastifyPluginCallback = (fastify, _options, done) => {
   const collection = fastify.mongo.db?.collection('user');
@@ -10,5 +10,48 @@ export const testRoutes: FastifyPluginCallback = (fastify, _options, done) => {
     const user = await collection?.findOne({ email: 'bar@bar.com' });
     return { user };
   });
+
+  fastify.put(
+    '/update-privacy-terms',
+    {
+      preHandler: [
+        function (
+          req: FastifyRequest<{ Body: { quincyEmails: boolean } }>,
+          _res,
+          done
+        ) {
+          void req.jwtAuthz(['write:user'], done);
+        }
+      ],
+      schema: {
+        body: {
+          required: ['quincyEmails'],
+          properties: {
+            quincyEmails: { type: 'boolean' }
+          }
+        }
+      }
+    },
+    (req, res) => {
+      const {
+        body: { quincyEmails }
+      } = req;
+
+      const update = {
+        acceptedPrivacyTerms: true,
+        sendQuincyEmail: !!quincyEmails
+      };
+
+      return collection
+        ?.updateOne({ email: 'bar@bar.com' }, { $set: update })
+        .then(() => {
+          void res.code(200).send({ msg: 'Successfully updated' });
+        })
+        .catch(err => {
+          fastify.log.error(err);
+          void res.code(500).send({ msg: 'Something went wrong' });
+        });
+    }
+  );
   done();
 };
