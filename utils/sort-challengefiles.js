e0d40e9ef15e533ab73520a8d2619901a85c2d81
DDeerrpp <raidon69869@icloud.com>
2023-02-26 22:08:16 -0800

    small change to sort-challengefiles

--- utils/sort-challengefiles.js
+++ utils/sort-challengefiles.js
@@ -1,14 +1,21 @@
 exports.sortChallengeFiles = function sortChallengeFiles(challengeFiles) {
   const xs = challengeFiles.slice();
   xs.sort((a, b) => {
-    if (a.history[0] === 'index.html') return -1;
-    if (b.history[0] === 'index.html') return 1;
-    if (a.history[0] === 'styles.css') return -1;
-    if (b.history[0] === 'styles.css') return 1;
-    if (a.history[0] === 'index.jsx') return -1;
-    if (b.history[0] === 'index.jsx') return 1;
-    if (a.history[0] === 'script.js') return -1;
-    if (b.history[0] === 'script.js') return 1;
+    var hisA = a.history[0]
+    var hisB = b.history[0]
+
+    if(hisA === 'index.html' ||
+       hisA === 'styles.css' ||
+       hisA === 'index.jsx'  ||
+       hisA === 'script.js'
+      ) {return -1;}
+
+    if(hisB === 'index.html' ||
+       hisB === 'styles.css' ||
+       hisB === 'index.jsx'  ||
+       hisB === 'script.js'
+      ) {return 1;}
+      
     return 0;
   });
   return xs;
