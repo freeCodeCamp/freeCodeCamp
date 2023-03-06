7a29e99b75791e7a517ee6ea017f038b6e363814
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-03-06 14:34:48 +0100

    Revert "fix: show loopback where strong-error-handler is"
    This reverts commit aac067bd7d81cce69b3151510264d7bb8231510e.

--- api-server/ecosystem.config.js
+++ api-server/ecosystem.config.js
@@ -5,18 +5,12 @@ const dotenv = require('dotenv');
 
 const filePath = path.resolve('..', '.env');
 const env = dotenv.parse(fs.readFileSync(filePath));
-// without this, loopback cannot find strong-error-handler. Node can, so we know
-// there's no _real_ issue, but loopback is not able to find it.
-const loopbackModuleResolutionHack = path.resolve(
-  __dirname,
-  '../node_modules/.pnpm/node_modules'
-);
 
 module.exports = {
   apps: [
     {
       script: `./lib/production-start.js`,
-      env: { ...env, NODE_PATH: loopbackModuleResolutionHack },
+      env,
       max_memory_restart: '600M',
       instances: 'max',
       exec_mode: 'cluster',
