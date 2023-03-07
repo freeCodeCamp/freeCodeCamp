c1daf99554b2ec3e6744580b730aa37603659ea3
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-03-07 15:58:13 +0100

    feat(api): validate environment variables before use
    This is similar in concept to ensure-env, but a little simpler since
    there is no need to put the data in a file before the api can consume
    it.

---
+++ api/utils/env.ts
@@ -0,0 +1,31 @@
+import assert from 'node:assert';
+import path from 'node:path';
+import { config } from 'dotenv';
+
+const envPath = path.resolve(__dirname, '../../.env');
+const { error } = config({ path: envPath });
+
+if (error) {
+  console.warn(`
+  ----------------------------------------------------
+  Warning: .env file not found.
+  ----------------------------------------------------
+  Please copy sample.env to .env
+
+  You can ignore this warning if using a different way
+  to setup this environment.
+  ----------------------------------------------------
+  `);
+}
+
+assert.ok(process.env.NODE_ENV);
+assert.ok(process.env.AUTH0_DOMAIN);
+assert.ok(process.env.AUTH0_AUDIENCE);
+if (process.env.NODE_ENV !== 'development') {
+  assert.ok(process.env.PORT);
+}
+
+export const NODE_ENV = process.env.NODE_ENV;
+export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
+export const PORT = process.env.PORT || '3000';
