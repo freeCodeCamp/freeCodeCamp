e264536a635bd5e578d0334e587359b9b4b77f68
Niraj Nandish <nirajnandish@icloud.com>
2023-03-08 18:06:01 +0530

    fix: shorter function type
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- api/plugins/session-auth.ts
+++ api/plugins/session-auth.ts
@@ -1,36 +1,23 @@
-import {
-  DoneFuncWithErrOrRes,
-  FastifyPluginCallback,
-  FastifyReply,
-  FastifyRequest
-} from 'fastify';
+import { FastifyPluginCallback, onRequestHookHandler } from 'fastify';
 import fp from 'fastify-plugin';
 
 const sessionAuth: FastifyPluginCallback = (fastify, _opts, done) => {
-  fastify.decorate('authenticateSession', authenticateSession);
-
-  function authenticateSession(
-    req: FastifyRequest,
-    res: FastifyReply,
-    done: DoneFuncWithErrOrRes
-  ) {
+  const authenticateSession: onRequestHookHandler = (req, res, done) => {
     if (!req.session.user) {
       res.statusCode = 401;
       return res.send({ msg: 'Unauthorized' });
     }
     done();
-  }
+  };
+
+  fastify.decorate('authenticateSession', authenticateSession);
 
   done();
 };
 
 declare module 'fastify' {
   interface FastifyInstance {
-    authenticateSession: (
-      req: FastifyRequest,
-      res: FastifyReply,
-      done: DoneFuncWithErrOrRes
-    ) => void;
+    authenticateSession: onRequestHookHandler;
   }
 }
 
