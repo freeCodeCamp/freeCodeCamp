6de9ad655752936367ae56f9d06c91d99973a2ea
Sboonny <muhammed@freecodecamp.org>
2023-03-09 13:14:07 +0000

    add tthe styles for the panel

--- tools/ui-components/src/panel/panel.tsx
+++ tools/ui-components/src/panel/panel.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import { PanelProps } from './types';
 
-const styles = 'border border-solid shadow-sm mb-6';
+const styles = 'border border-solid border-foreground-primary shadow-sm mb-6';
 const primaryStyle = 'border-background-primary';
+const dangerStyle = 'border-foreground-danger';
+const infoStyle = 'border-sky-300';
+// const infoHeadingStyle = 'bg-sky-200 text-sky-500'
 
-export const Panel = ({ children, className }: PanelProps): JSX.Element => {
-  // need to swap primaryStyles with bsStyle later
-  const panelClassed = [styles, primaryStyle, className].join(' ');
+export const Panel = ({
+  children,
+  className,
+  bsStyle
+}: PanelProps): JSX.Element => {
+  const buttonStyles = styles;
+  if (bsStyle === 'primary') [buttonStyles].concat(primaryStyle).join(' ');
+  else if (bsStyle === 'danger') [buttonStyles].concat(dangerStyle).join(' ');
+  else if (bsStyle === 'info') [buttonStyles].concat(infoStyle).join(' ');
+
+  const panelClassed = [buttonStyles, className].join(' ');
   return <div className={panelClassed}>{children}</div>;
 };
