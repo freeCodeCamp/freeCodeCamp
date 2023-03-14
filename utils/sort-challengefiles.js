a1ea1dcdc8eec5d5d32beda4601784197edb34d1
Divine Adeyeye <87317463+DivineAde@users.noreply.github.com>
2023-03-14 09:41:01 +0100

    Update sort-challengefiles.js

--- utils/sort-challengefiles.js
+++ utils/sort-challengefiles.js
@@ -1,7 +1,7 @@
 exports.sortChallengeFiles = function sortChallengeFiles(challengeFiles) {
-  const xs = challengeFiles.slice();
-  xs.sort((a, b) => {
-    if (a.history[0] === 'index.html') return -1;
+  const xs = challengeFiles.slice();               //This function creates a new array xs that is a copy of the original challengeFiles array using the slice() method to avoid modifying the original array
+  xs.sort((a, b) => {                              //It then uses the sort() method on xs, which takes a comparison function as its argument.
+    if (a.history[0] === 'index.html') return -1;  //The comparison function checks the first history entry of each element and returns -1, 1, or 0 
     if (b.history[0] === 'index.html') return 1;
     if (a.history[0] === 'styles.css') return -1;
     if (b.history[0] === 'styles.css') return 1;
@@ -11,5 +11,5 @@ exports.sortChallengeFiles = function sortChallengeFiles(challengeFiles) {
     if (b.history[0] === 'script.js') return 1;
     return 0;
   });
-  return xs;
+  return xs;                                        //Finally, the function returns the sorted array xs.
 };
