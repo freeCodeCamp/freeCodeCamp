d52ed1c990355151ce432e8a7ba5fb713f47298b
Muhammed Mustafa <MuhammedElruby@gmail.com>
2023-03-20 10:13:16 +0200

    Clean file changes

--- tools/ui-components/src/control-label/types.ts
+++ tools/ui-components/src/control-label/types.ts
@@ -2,7 +2,6 @@ import React from 'react';
 
 export interface ControlLabelProps
   extends React.LabelHTMLAttributes<HTMLLabelElement> {
-  className?: string;
   htmlFor?: string;
   srOnly?: boolean;
 }
