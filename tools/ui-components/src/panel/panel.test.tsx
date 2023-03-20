35e668b3e1dc1a72cf909df0d50896b63308ee97
Sboonny <muhammed@freecodecamp.org>
2023-03-20 05:43:16 +0000

    remove duplication in the test

--- tools/ui-components/src/panel/panel.test.tsx
+++ tools/ui-components/src/panel/panel.test.tsx
@@ -4,17 +4,13 @@ import { render, screen } from '@testing-library/react';
 import { Panel, PanelHeading } from '.';
 
 describe('<Panel />', () => {
-  it('should render correctly', () => {
-    render(<Panel role='article' />);
-    expect(screen.getByRole('article')).toBeInTheDocument();
-  });
-
   it('PanelHead should inherit bsStyle', () => {
     render(
       <Panel role='article' bsStyle='primary'>
         <PanelHeading>Test</PanelHeading>
       </Panel>
     );
+    expect(screen.getByRole('article')).toBeInTheDocument();
     expect(screen.getByText('Test')).toHaveClass(
       'border-b-1 border-solid border-foreground-primary text-foreground-primary'
     );
