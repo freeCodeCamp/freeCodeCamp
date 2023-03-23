9d4bc0c51089186518286ac985a480bbecbf453a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-03-23 12:50:07 +0100

    refactor: split browser scripts into own workspace

--- tools/client-plugins/gatsby-remark-node-identity/gatsby-node.js
+++ tools/client-plugins/gatsby-remark-node-identity/gatsby-node.js
@@ -4,12 +4,12 @@ exports.onCreateNode = function remarkNodeIdentityOnCreateNode(
 ) {
   if (typeof predicate !== 'function') {
     reporter.panic(
-      'Please supply a predicate function to `gatsby-plugin-identity`'
+      'Please supply a predicate function to `gatsby-remark-node-identity`'
     );
   }
   if (typeof identity !== 'string' || identity.length === 0) {
     reporter.panic(
-      '`gatsby-plugin-identity` requires an identify string to add to nodes ' +
+      '`gatsby-remark-node-identity` requires an identify string to add to nodes ' +
         'that match the predicate'
     );
   }
