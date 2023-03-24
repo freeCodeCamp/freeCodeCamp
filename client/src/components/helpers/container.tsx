ee455e319f116704b4fd443bdf9a24f842ca75ac
Sboonny <muhammedelruby@gmail.com>
2023-03-24 17:17:17 +0200

    feat(client): remake grid component to container helper component

---
+++ client/src/components/helpers/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const styles = {
+  width: 'min(100% - 2rem, 20em)',
+  margin: '0 auto'
+};
+
+export const Container = ({
+  children
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
+  return <div style={styles}>{children}</div>;
+};
