6a69ee6ca3c6fc220ec3e316b41479d504eba5d0
Sboonny <muhammedelruby@gmail.com>
2023-03-24 19:20:40 +0200

    clean the exports

--- tools/ui-components/src/panel/index.ts
+++ tools/ui-components/src/panel/index.ts
@@ -1,5 +1,2 @@
 export { Panel } from './panel';
-export { PanelHeading } from './panel-heading';
-export { PanelTitle } from './panel-title';
-export { PanelBody } from './panel-body';
 export type { PanelProps } from './types';
