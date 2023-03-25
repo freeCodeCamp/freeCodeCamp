a640f90a057722ce730ff47b0b2f5a86a4f12af6
Sboonny <muhammedelruby@gmail.com>
2023-03-25 15:10:08 +0200

    refactor(client): align nav-links types with universal-nav types

--- client/src/components/Header/components/nav-links.tsx
+++ client/src/components/Header/components/nav-links.tsx
@@ -7,8 +7,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Fragment, useRef } from 'react';
 import Media from 'react-responsive';
-import type { TFunction } from 'i18next';
-import { withTranslation } from 'react-i18next';
+import { useTranslation, withTranslation } from 'react-i18next';
 import { useFeature } from '@growthbook/growthbook-react';
 import { connect } from 'react-redux';
 import { clientLocale, radioLocation } from '../../../../../config/env.json';
@@ -31,12 +30,10 @@ const locales = availableLangs.client.filter(
   lang => !hiddenLangs.includes(lang)
 );
 
-interface NavLinksProps {
+export interface NavLinksProps {
   displayMenu: boolean;
   isLanguageMenuDisplayed: boolean;
   fetchState: { pending: boolean };
-  i18n: Record<string, unknown>;
-  t: TFunction;
   showMenu: () => void;
   hideMenu: () => void;
   toggleNightMode: (theme: Themes) => Themes;
@@ -45,7 +42,6 @@ interface NavLinksProps {
   showLanguageMenu: (elementToFocus: HTMLButtonElement | null) => void;
   hideLanguageMenu: () => void;
   menuButtonRef: React.RefObject<HTMLButtonElement>;
-  openSignoutModal: () => void;
 }
 
 const mapDispatchToProps = {
@@ -57,7 +53,6 @@ const mapDispatchToProps = {
 interface DonateButtonProps {
   isUserDonating: boolean | undefined;
   handleMenuKeyDown: (event: React.KeyboardEvent<HTMLAnchorElement>) => void;
-  t: TFunction;
 }
 
 type DonateItemProps = Pick<DonateButtonProps, 'handleMenuKeyDown'> & {
@@ -88,9 +83,9 @@ const ThankYouMessage = ({ message }: { message: string }) => (
 
 const DonateButton = ({
   isUserDonating,
-  handleMenuKeyDown,
-  t
+  handleMenuKeyDown
 }: DonateButtonProps) => {
+  const { t } = useTranslation();
   const exposeUniversalDonateButton = useFeature('expose_donate_button').on;
   if (isUserDonating) return <ThankYouMessage message={t('donate.thanks')} />;
   else if (exposeUniversalDonateButton)
@@ -111,32 +106,36 @@ const DonateButton = ({
     );
 };
 
+const toggleTheme = (
+  currentTheme = Themes.Default,
+  toggleNightMode: (theme: Themes) => Themes
+) => {
+  toggleNightMode(
+    currentTheme === Themes.Night ? Themes.Default : Themes.Night
+  );
+};
+
 function NavLinks({
   menuButtonRef,
   hideLanguageMenu,
-  openSignoutModal,
   hideMenu,
   showLanguageMenu,
   isLanguageMenuDisplayed,
   displayMenu,
   fetchState,
-  t,
   toggleNightMode,
   user,
   navigate
 }: NavLinksProps) {
+  const { t } = useTranslation();
   const langButtonRef = useRef<HTMLButtonElement>(null);
   const firstLangOptionRef = useRef<HTMLButtonElement>(null);
   const lastLangOptionRef = useRef<HTMLButtonElement>(null);
+  const { pending } = fetchState;
 
-  const toggleTheme = (
-    currentTheme = Themes.Default,
-    toggleNightMode: (theme: Themes) => Themes
-  ) => {
-    toggleNightMode(
-      currentTheme === Themes.Night ? Themes.Default : Themes.Night
-    );
-  };
+  const isUserDonating = user?.isDonating;
+  const currentUserName = user?.username;
+  const currentUserTheme = user?.theme;
 
   const getPreviousMenuItem = (target: HTMLButtonElement | null) => {
     const previousSibling =
@@ -152,7 +151,7 @@ function NavLinks({
   ): void => {
     event.preventDefault();
 
-    const newLanguage = event.currentTarget.dataset.value as string;
+    const newLanguage = event.currentTarget.dataset.value;
     // If user selected cancel then close menu and put focus on button
     if (newLanguage === 'exit-lang-menu') {
       // Set focus to language button first so we don't lose focus
@@ -169,12 +168,14 @@ function NavLinks({
     if (newLanguage === clientLocale) {
       return;
     }
-    const path = createLanguageRedirect({
-      clientLocale,
-      lang: newLanguage
-    });
-    if (typeof navigate !== 'undefined') {
-      return navigate(path);
+    if (newLanguage !== undefined) {
+      const path = createLanguageRedirect({
+        clientLocale,
+        lang: newLanguage
+      });
+      if (typeof navigate !== 'undefined') {
+        return navigate(path);
+      }
     }
   };
 
@@ -374,11 +375,6 @@ function NavLinks({
     openSignoutModal();
   };
 
-  const isUserDonating = user?.isDonating;
-  const currentUserName = user?.username;
-  const currentUserTheme = user?.theme;
-  const { pending } = fetchState;
-
   return pending ? (
     <div className='nav-skeleton' />
   ) : (
@@ -389,7 +385,6 @@ function NavLinks({
       }`}
     >
       <DonateButton
-        t={t}
         isUserDonating={isUserDonating}
         handleMenuKeyDown={handleMenuKeyDown}
       />
