3329271972bf913125f753d2840e874b48f1fd95
Sboonny <muhammedelruby@gmail.com>
2023-03-27 16:13:29 +0200

    fix(tools): sort small issues in formControl

--- tools/ui-components/src/form-control/types.ts
+++ tools/ui-components/src/form-control/types.ts
@@ -9,31 +9,26 @@ type ChangibleValues =
       readonly?: never;
     }
   | {
-      value: string;
+      value?: string;
       onChange?: never;
       readonly: boolean;
     }
   | {
-      value: string;
+      value?: string;
       onChange: (event: React.ChangeEvent<FormControlElement>) => void;
       readonly?: never;
     };
 
 export type FormControlProps = React.HTMLAttributes<FormControlElement> & {
-  id?: string;
-  className?: string;
   testId?: string;
   componentClass?: 'textarea' | 'input';
-  placeholder?: string;
   name?: string;
   required?: boolean;
   rows?: number;
   type?: 'text' | 'email' | 'url';
 } & ChangibleValues;
 
-export interface FormControlVariationProps {
-  className?: string;
-  children?: React.ReactNode;
-  id?: string;
-  testId?: string;
-}
+export type FormControlVariationProps = Pick<
+  FormControlProps,
+  'className' | 'children' | 'id' | 'testId'
+>;
