865d32b7917bba04417ae151d4e2751cf8b74211
Muhammed Mustafa <MuhammedElruby@gmail.com>
2023-03-27 20:09:48 +0200

    remove extra types

--- tools/ui-components/src/panel/types.ts
+++ tools/ui-components/src/panel/types.ts
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export interface PanelProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
-  children?: React.ReactNode;
   bsStyle?: 'primary' | 'info' | 'danger';
 }
