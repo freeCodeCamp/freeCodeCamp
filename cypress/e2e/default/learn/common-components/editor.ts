913f7acab2af5f30e46e7abbd8c649a3bd26030e
Aaron <a.rcottrill521@gmail.com>
2023-03-28 12:00:33 -0400

    Create a test for dark mode.

--- cypress/e2e/default/learn/common-components/editor.ts
+++ cypress/e2e/default/learn/common-components/editor.ts
@@ -27,3 +27,57 @@ describe('Editor Shortcuts', () => {
       .should('have.value', '<h1>Hello</h1>');
   });
 });
+
+const visit = (darkAppearance: boolean) =>
+  cy.visit(
+    'learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-23',
+    {
+      onLoad(win) {
+        cy.stub(win, 'matchMedia')
+          .withArgs('(prefers-color-scheme: dark)')
+          .returns({
+            matches: darkAppearance
+          });
+      }
+    }
+  );
+
+describe('Editor displays light theme', () => {
+  beforeEach(() => {
+    visit(false);
+  });
+
+  it('Displays when signed out', () => {
+    cy.get(editorElement.editor, { timeout: 15000 }).should(
+      'have.class',
+      'light-palette'
+    );
+  });
+
+  it('Displays when signed in', () => {
+    cy.login();
+    cy.get(editorElement.editor, { timeout: 15000 })
+      .first()
+      .should('have.class', 'light-palette');
+  });
+});
+
+describe('Editor displays dark theme', () => {
+  beforeEach(() => {
+    visit(true);
+  });
+
+  it('When signed out', () => {
+    cy.get(editorElement.editor, { timeout: 15000 }).should(
+      'have.class',
+      'light-palette'
+    );
+  });
+
+  it('When signed in', () => {
+    cy.login();
+    cy.get(editorElement.editor, { timeout: 15000 })
+      .first()
+      .should('have.class', 'dark-palette');
+  });
+});
