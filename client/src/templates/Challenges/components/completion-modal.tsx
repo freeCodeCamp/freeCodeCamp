f61700dc48c44dc7169aeab020dfa07394b35d14
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-03-30 20:24:10 +0200

    fix(client): import TFunction from i18next in forms (#49817)
    fix(client): import tfunction from i18next

--- client/src/templates/Challenges/components/completion-modal.tsx
+++ client/src/templates/Challenges/components/completion-modal.tsx
@@ -3,7 +3,7 @@
 import { Button, Modal } from '@freecodecamp/react-bootstrap';
 import { noop } from 'lodash-es';
 import React, { Component } from 'react';
-import { TFunction } from 'i18next';
+import type { TFunction } from 'i18next';
 import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
