6112f0ad9b854821aba8811ef28766c38378b818
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-03-30 22:37:04 +0200

    fix(client): align multifileEditor and editor types (#49825)
    * fix(client): align multifileEditor editor and hotkeys types
    
    * sort the last types
    
    * sort the last types
    
    * clean wrong types
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    * clean wrong types
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    * Change the comment
    
    ---------
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/templates/Challenges/classic/editor.tsx
+++ client/src/templates/Challenges/classic/editor.tsx
@@ -28,7 +28,6 @@ import {
 import {
   ChallengeFiles,
   Dimensions,
-  Ext,
   FileKey,
   ResizeProps,
   Test
@@ -68,22 +67,18 @@ import './editor.css';
 
 const MonacoEditor = Loadable(() => import('react-monaco-editor'));
 
-interface EditorProps {
+export interface EditorProps {
   attempts: number;
   canFocus: boolean;
   challengeFiles: ChallengeFiles;
   challengeType: number;
   containerRef: MutableRefObject<HTMLElement | undefined>;
-  contents: string;
   description: string;
-  dimensions: Dimensions;
+  dimensions?: Dimensions;
   editorRef: MutableRefObject<editor.IStandaloneCodeEditor | undefined>;
   executeChallenge: (options?: { showCompletionModal: boolean }) => void;
-  ext: Ext;
   fileKey: FileKey;
   canFocusOnMountRef: MutableRefObject<boolean>;
-  initialEditorContent: string;
-  initialExt: string;
   initTests: (tests: Test[]) => void;
   initialTests: Test[];
   isMobileLayout: boolean;
