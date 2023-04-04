394af64618f9dcb6312a7d4d96288d1eb74a8004
Sboonny <muhammedelruby@gmail.com>
2023-04-04 14:04:38 +0100

    remove the row that case overflow

--- client/src/components/landing/components/certifications.tsx
+++ client/src/components/landing/components/certifications.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from '@freecodecamp/react-bootstrap';
+import { Col } from '@freecodecamp/react-bootstrap';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Map from '../../Map/index';
@@ -15,15 +15,20 @@ const Certifications = ({
   const { t } = useTranslation();
 
   return (
-    <Row className='certification-section'>
-      <Col md={8} mdOffset={2} sm={10} smOffset={1} xs={12}>
-        <h1 className='big-heading'>{t('landing.certification-heading')}</h1>
-        <Map forLanding={true} />
-        <Spacer size='medium' />
-        <BigCallToAction pageName={pageName} />
-        <Spacer size='medium' />
-      </Col>
-    </Row>
+    <Col
+      className='certification-section'
+      md={8}
+      mdOffset={2}
+      sm={10}
+      smOffset={1}
+      xs={12}
+    >
+      <h1 className='big-heading'>{t('landing.certification-heading')}</h1>
+      <Map forLanding={true} />
+      <Spacer size='medium' />
+      <BigCallToAction pageName={pageName} />
+      <Spacer size='medium' />
+    </Col>
   );
 };
 
