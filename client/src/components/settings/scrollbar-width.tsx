01c45d3264306cee2071fd634657618ad31be4df
Bruce B <bbsmooth@gmail.com>
2023-04-07 18:22:54 -0700

    add tick boxes

--- client/src/components/settings/scrollbar-width.tsx
+++ client/src/components/settings/scrollbar-width.tsx
@@ -1,13 +1,16 @@
 import { Form } from '@freecodecamp/react-bootstrap';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState, useRef } from 'react';
 import store from 'store';
 import { useTranslation } from 'react-i18next';
 import { Spacer } from '../helpers';
 import './scrollbar-width.css';
 
+const ticks = [5, 10, 15, 20, 25];
+
 export default function ScrollbarWidthSettings(): JSX.Element {
   const { t } = useTranslation();
   const [scrollbarWidth, setScrollbarWidth] = useState(getScrollbarWidth());
+  const rangeRef = useRef<HTMLInputElement>(null);
 
   function getScrollbarWidth() {
     const storedWidth = parseInt(store.get('monacoScrollbarWidth'));
@@ -20,35 +23,61 @@ export default function ScrollbarWidthSettings(): JSX.Element {
     store.set('monacoScrollbarWidth', inputValue);
   }
 
+  function handleClick(event: React.MouseEvent<HTMLSpanElement>) {
+    const target = event.target as HTMLSpanElement;
+    const newScrollbarWidth = Number(target.dataset.value);
+
+    if (!rangeRef?.current || !ticks.includes(newScrollbarWidth)) return;
+    rangeRef.current.focus();
+
+    if (newScrollbarWidth === scrollbarWidth) return;
+    rangeRef.current.value = String(newScrollbarWidth);
+    setScrollbarWidth(newScrollbarWidth);
+    store.set('monacoScrollbarWidth', newScrollbarWidth);
+  }
+
+  /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
+
   return (
     <Form inline={true} onSubmit={(e: React.FormEvent) => e.preventDefault()}>
       <label htmlFor='scrollbar-width-slider'>
         {t('settings.scrollbar-width')}
       </label>
-      <input
-        type='range'
-        min='5'
-        max='25'
-        step='5'
-        id='scrollbar-width-slider'
-        className='scrollbar-width'
-        defaultValue={scrollbarWidth}
-        onInput={handleChange}
-      />
-      <div className='scrollbar-width-numbers' aria-hidden='true'>
-        <span {...(scrollbarWidth === 5 && { className: 'selected' })}>5</span>
-        <span {...(scrollbarWidth === 10 && { className: 'selected' })}>
-          10
-        </span>
-        <span {...(scrollbarWidth === 15 && { className: 'selected' })}>
-          15
-        </span>
-        <span {...(scrollbarWidth === 20 && { className: 'selected' })}>
-          20
-        </span>
-        <span {...(scrollbarWidth === 25 && { className: 'selected' })}>
-          25
-        </span>
+      <div className='scrollbar-width-container'>
+        <div className='scrollbar-width-ticks' aria-hidden='true'>
+          {ticks.map(tick => (
+            <span
+              className='tick'
+              onClick={handleClick}
+              data-value={tick}
+              key={`tick${tick}`}
+              {...(scrollbarWidth === tick && { 'data-current': true })}
+            />
+          ))}
+        </div>
+        <input
+          type='range'
+          min='5'
+          max='25'
+          step='5'
+          id='scrollbar-width-slider'
+          className='scrollbar-width'
+          defaultValue={scrollbarWidth}
+          onInput={handleChange}
+          ref={rangeRef}
+        />
+        <div className='scrollbar-width-numbers' aria-hidden='true'>
+          {ticks.map(tick => (
+            <span
+              onClick={handleClick}
+              data-value={tick}
+              key={`num${tick}`}
+              {...(scrollbarWidth === tick && { className: 'selected' })}
+            >
+              {tick}
+            </span>
+          ))}
+        </div>
       </div>
       <Spacer size='medium'></Spacer>
     </Form>
