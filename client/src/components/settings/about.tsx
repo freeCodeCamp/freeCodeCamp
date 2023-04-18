0dd8282cf8bc8180004329a47e0e0073248ebefe
Bruce Blaser <bbsmooth@gmail.com>
2023-04-18 05:36:03 -0700

    feat(client): adjustable scrollbar width in code editor (#49975)

--- client/src/components/settings/about.tsx
+++ client/src/components/settings/about.tsx
@@ -18,6 +18,7 @@ import ThemeSettings, { Themes } from './theme';
 import UsernameSettings from './username';
 import KeyboardShortcutsSettings from './keyboard-shortcuts';
 import SectionHeader from './section-header';
+import ScrollbarWidthSettings from './scrollbar-width';
 
 type FormValues = {
   name: string;
@@ -288,6 +289,7 @@ class AboutSettings extends Component<AboutProps, AboutState> {
             keyboardShortcuts={keyboardShortcuts}
             toggleKeyboardShortcuts={toggleKeyboardShortcuts}
           />
+          <ScrollbarWidthSettings />
         </FullWidthRow>
       </>
     );
