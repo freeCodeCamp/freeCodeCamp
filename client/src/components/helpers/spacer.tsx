6df7b110fa07d39cf357c24406d4f648e04c244f
Vansh Baghel <vanshbaghel912@gmail.com>
2023-04-22 13:10:41 +0000

    Children in spacer, replaced padding with padding-block and fixed calculation

--- client/src/components/helpers/spacer.tsx
+++ client/src/components/helpers/spacer.tsx
@@ -10,13 +10,15 @@ const Padding = Object.freeze({
 type PaddingKeys = keyof typeof Padding;
 interface SpacerProps {
   size: PaddingKeys;
+  children?: React.ReactNode;
 }
 
-const Spacer = ({ size }: SpacerProps): JSX.Element => (
-  <div
-    className='spacer'
-    style={{ padding: `${Padding[size]}px 0`, height: '1px' }}
-  />
+const Spacer = ({ size, children }: SpacerProps): JSX.Element => (
+  <div className='spacer' style={{ paddingBlock: `${Padding[size]}px` }}>
+    {children && (
+      <div style={{ paddingBlock: `${Padding[size]}px` }}>{children}</div>
+    )}
+  </div>
 );
 
 export default Spacer;
