88926dbb4fe6cc65b67ff5d8d637fa50c86917a1
Vansh Baghel <vanshbaghel912@gmail.com>
2023-04-24 12:46:45 +0000

    Changed calculation, removed extra div

--- client/src/components/helpers/spacer.tsx
+++ client/src/components/helpers/spacer.tsx
@@ -14,10 +14,13 @@ interface SpacerProps {
 }
 
 const Spacer = ({ size, children }: SpacerProps): JSX.Element => (
-  <div className='spacer' style={{ paddingBlock: `${Padding[size]}px` }}>
-    {children && (
-      <div style={{ paddingBlock: `${Padding[size]}px` }}>{children}</div>
-    )}
+  <div
+    className='spacer'
+    style={{
+      paddingBlock: children ? `${Padding[size] * 2}px` : `${Padding[size]}px`
+    }}
+  >
+    {children}
   </div>
 );
 
