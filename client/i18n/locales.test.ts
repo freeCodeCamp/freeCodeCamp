dccacf559eb0cddbc3408890f89d438171438776
Naomi Carrigan <nhcarrigan@gmail.com>
2023-04-24 17:01:46 -0700

    chore(tools): test intro file structure

--- client/i18n/locales.test.ts
+++ client/i18n/locales.test.ts
@@ -1,8 +1,22 @@
 import fs from 'fs';
 import { setup } from 'jest-json-schema-extended';
 import { availableLangs, LangNames, LangCodes } from '../../config/i18n';
+import intro from './locales/english/intro.json';
 
 setup();
+
+interface Intro {
+  [superBlock: string]: {
+    title: string;
+    intro: string[];
+    blocks: {
+      [block: string]: {
+        title: string;
+        intro: string[];
+      };
+    };
+  };
+}
 
 const filesThatShouldExist = [
   {
@@ -50,6 +64,23 @@ describe('Locale tests:', () => {
             .includes(lang)
         ).toBe(true);
       });
+    });
+  });
+});
+
+describe('Intro file structure tests:', () => {
+  const typedIntro = intro as unknown as Intro;
+  const superblocks = Object.keys(typedIntro).filter(
+    key => key !== 'misc-text'
+  );
+  superblocks.forEach(superBlock => {
+    expect(typeof typedIntro[superBlock].title).toBe('string');
+    expect(typedIntro[superBlock].intro).toBeInstanceOf(Array);
+    expect(typedIntro[superBlock].blocks).toBeInstanceOf(Object);
+    const blocks = Object.keys(typedIntro[superBlock].blocks);
+    blocks.forEach(block => {
+      expect(typeof typedIntro[superBlock].blocks[block].title).toBe('string');
+      expect(typedIntro[superBlock].blocks[block].intro).toBeInstanceOf(Array);
     });
   });
 });
