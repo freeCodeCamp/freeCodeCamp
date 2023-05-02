8e9f2418555be7f9c520a7996d95a68a340b6a34
moT01 <20648924+moT01@users.noreply.github.com>
2023-05-02 08:00:05 -0500

    fix: add review suggestions

--- client/src/components/landing/components/faq.tsx
+++ client/src/components/landing/components/faq.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from '@freecodecamp/react-bootstrap';
+import { Col } from '@freecodecamp/react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Spacer } from '../../helpers';
 import BigCallToAction from './big-call-to-action';
@@ -11,29 +11,26 @@ interface FaqItem {
 
 const Faq = (): JSX.Element => {
   const { t } = useTranslation();
-  const faqItems: FaqItem[] = t('landing.faqs', { returnObjects: true });
+  const faqItems = t<string, string & FaqItem[]>('landing.faqs');
 
   return (
-    <Row className='landing-faq'>
-      <Col sm={8} smOffset={2} xs={10} xsOffset={1}>
-        <h1 className='big-heading'>{t('landing.faq')}</h1>
-        <Spacer size='medium' />
-        {faqItems.map((faq, i) => (
-          <div data-test-label='landing-page-faq' key={i}>
-            <h3>{faq.question}</h3>
-            {faq.answer.map((answer, i) => (
-              <p key={i}>{answer}</p>
-            ))}
-            <Spacer size='small' />
-            <Spacer size='small' />
-          </div>
-        ))}
-        <h3>{t('landing.happy-coding')}</h3>
-        <Spacer size='medium' />
-        <BigCallToAction />
-        <Spacer size='large' />
-      </Col>
-    </Row>
+    <Col sm={8} smOffset={2} xs={10} xsOffset={1}>
+      <h1 className='big-heading'>{t('landing.faq')}</h1>
+      <Spacer size='small' />
+      {faqItems.map((faq, i) => (
+        <div data-test-label='landing-page-faq' key={i}>
+          <p className='faq-question'>{faq.question}</p>
+          {faq.answer.map((answer, i) => (
+            <p key={i}>{answer}</p>
+          ))}
+          <Spacer size='small' />
+        </div>
+      ))}
+      <h3>{t('learn.read-this.p12')}</h3>
+      <Spacer size='medium' />
+      <BigCallToAction />
+      <Spacer size='large' />
+    </Col>
   );
 };
 
