4df747a34da6ede9524e071f24d161a326a0df7d
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2023-05-13 16:49:50 +0530

    Update index.js
    Update index.js make some changes also add comments and make code readable manner.

--- api-server/src/server/index.js
+++ api-server/src/server/index.js
@@ -1,14 +1,11 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
-
 const Sentry = require('@sentry/node');
-// const Tracing = require('@sentry/tracing');
 const createDebugger = require('debug');
 const _ = require('lodash');
 const loopback = require('loopback');
 const boot = require('loopback-boot');
 const morgan = require('morgan');
-
 const { sentry } = require('../../../config/secrets');
 const { setupPassport } = require('./component-passport');
 
@@ -17,15 +14,21 @@ const reqLogFormat = ':date[iso] :status :method :response-time ms - :url';
 
 const app = loopback();
 
+// Set app configuration options
 app.set('state namespace', '__fcc__');
 app.set('port', process.env.API_PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+
+// Use middleware
 app.use(loopback.token());
-app.use(
-  morgan(reqLogFormat, { stream: { write: msg => log(_.split(msg, '\n')[0]) } })
-);
+app.use(morgan(reqLogFormat, { 
+  stream: { 
+    write: msg => log(_.split(msg, '\n')[0])
+  } 
+}));
 app.disable('x-powered-by');
 
+// Helper function to log only once
 const createLogOnce = () => {
   let called = false;
   return str => {
@@ -38,26 +41,28 @@ const createLogOnce = () => {
 };
 const logOnce = createLogOnce();
 
+// Boot LoopBack app
 boot(app, __dirname, err => {
   if (err) {
-    // rethrowing the error here because any error thrown in the boot stage
-    // is silent
+    // Rethrow error because any error thrown in the boot stage is silent
     logOnce('The below error was thrown in the boot stage');
     throw err;
   }
 });
 
+// Setup passport middleware
 setupPassport(app);
 
+// Log when database is connected
 const { db } = app.datasources;
-db.on(
-  'connected',
-  _.once(() => log('db connected'))
-);
+db.on('connected', _.once(() => log('db connected')));
 
+// Start the app
 app.start = _.once(function () {
   const server = app.listen(app.get('port'), function () {
+    // Emit event when app is started
     app.emit('started');
+    // Log app info
     log(
       'freeCodeCamp server listening on port %d in %s',
       app.get('port'),
@@ -66,6 +71,7 @@ app.start = _.once(function () {
     log(`connecting to db at ${db.settings.url}`);
   });
 
+  // Handle SIGINT signal to gracefully shut down the server
   process.on('SIGINT', () => {
     log('Shutting down server');
     server.close(() => {
@@ -74,35 +80,28 @@ app.start = _.once(function () {
     log('closing db connection');
     db.disconnect().then(() => {
       log('DB connection closed');
-      // exit process
-      // this may close kept alive sockets
-      // eslint-disable-next-line no-process-exit
+      // Exit process
       process.exit(0);
     });
   });
 });
 
+// Initialize Sentry error tracking
 if (sentry.dsn === 'dsn_from_sentry_dashboard') {
   log('Sentry reporting disabled unless DSN is provided.');
 } else {
   Sentry.init({
     dsn: sentry.dsn
-    // integrations: [
-    //   new Sentry.Integrations.Http({ tracing: true }),
-    //   new Tracing.Integrations.Express({
-    //     app
-    //   })
-    // ],
-    // // Capture 20% of transactions to avoid
-    // // overwhelming Sentry and remain within
-    // // the usage quota
-    // tracesSampleRate: 0.2
+    // Add integrations and set tracesSampleRate here if needed
   });
   log('Sentry initialized');
 }
 
+// Export the app
 module.exports = app;
 
+// Start the app if this module is the main module
 if (require.main === module) {
   app.start();
 }
+
