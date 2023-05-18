18c1adb97822c47f4b06f209cf3f10806761868d
Niraj Nandish <nirajnandish@icloud.com>
2023-05-18 19:59:39 +0400

    feat: reset progress endpoint

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -49,5 +49,69 @@ export const userRoutes: FastifyPluginCallbackTypebox = (
     }
   );
 
+  fastify.post(
+    '/account/reset',
+    {
+      schema: {
+        response: {
+          200: Type.Object({}),
+          500: Type.Object({
+            message: Type.Literal(
+              'Oops! Something went wrong. Please try again in a moment or contact support@freecodecamp.org if the error persists.'
+            ),
+            type: Type.Literal('danger')
+          })
+        }
+      }
+    },
+    async (req, reply) => {
+      try {
+        await fastify.prisma.userToken.deleteMany({
+          where: { userId: req.session.user.id }
+        });
+        await fastify.prisma.user.update({
+          where: { id: req.session.user.id },
+          data: {
+            progressTimestamps: [Date.now()],
+            currentChallengeId: '',
+            isRespWebDesignCert: false,
+            is2018DataVisCert: false,
+            isFrontEndLibsCert: false,
+            isJsAlgoDataStructCert: false,
+            isApisMicroservicesCert: false,
+            isInfosecQaCert: false,
+            isQaCertV7: false,
+            isInfosecCertV7: false,
+            is2018FullStackCert: false,
+            isFrontEndCert: false,
+            isBackEndCert: false,
+            isDataVisCert: false,
+            isFullStackCert: false,
+            isSciCompPyCertV7: false,
+            isDataAnalysisPyCertV7: false,
+            isMachineLearningPyCertV7: false,
+            isRelationalDatabaseCertV8: false,
+            // TODO: check if schema should be updated with below field
+            // isCollegeAlgebraPyCertV8: false
+            completedChallenges: [],
+            savedChallenges: [],
+            partiallyCompletedChallenges: [],
+            needsModeration: false
+          }
+        });
+
+        return {};
+      } catch (err) {
+        fastify.log.error(err);
+        void reply.code(500);
+        return {
+          message:
+            'Oops! Something went wrong. Please try again in a moment or contact support@freecodecamp.org if the error persists.',
+          type: 'danger'
+        };
+      }
+    }
+  );
+
   done();
 };
