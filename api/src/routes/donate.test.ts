1a11a435a398256f24e7e06c30f1294a13aacc41
sembauke <semboot699@gmail.com>
2023-05-19 13:19:44 +0200

    feat: add /add-donation endpoint

---
+++ api/src/routes/donate.test.ts
@@ -0,0 +1,63 @@
+import request from 'supertest';
+
+import { setupServer, superRequest } from '../../jest.utils';
+
+const baseProfileUI = {
+  isLocked: false,
+  showAbout: false,
+  showCerts: false,
+  showDonation: false,
+  showHeatMap: false,
+  showLocation: false,
+  showName: false,
+  showPoints: false,
+  showPortfolio: false,
+  showTimeLine: false
+};
+
+describe('Donate', () => {
+  setupServer();
+
+  describe('Authenticated User', () => {
+    let setCookies: string[];
+
+    // Authenticate user
+    beforeAll(async () => {
+      await fastifyTestInstance.prisma.user.updateMany({
+        where: { email: 'foo@bar.com' },
+        data: { profileUI: baseProfileUI }
+      });
+      const res = await request(fastifyTestInstance.server).get(
+        '/auth/dev-callback'
+      );
+      setCookies = res.get('Set-Cookie');
+    });
+
+    describe('POST /add-donation', () => {
+      it('should return 200 and update the user', async () => {
+        const response = await superRequest('/add-donation', {
+          method: 'POST',
+          setCookies
+        }).send({
+          isDonating: true
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+          message: 'success',
+          type: 'success'
+        });
+      });
+
+      // it should error if the body is empty
+      it('should return 400 if the body is empty', async () => {
+        const response = await superRequest('/add-donation', {
+          method: 'POST',
+          setCookies
+        }).send({});
+
+        expect(response.status).toBe(400);
+      });
+    });
+  });
+});
---
+++ api/src/routes/donate.ts
@@ -0,0 +1,61 @@
+import {
+  Type,
+  type FastifyPluginCallbackTypebox
+} from '@fastify/type-provider-typebox';
+
+export const donateRoutes: FastifyPluginCallbackTypebox = (
+  fastify,
+  _options,
+  done
+) => {
+  // The order matters here, since we want to reject invalid cross site requests
+  // before checking if the user is authenticated.
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  fastify.addHook('onRequest', fastify.csrfProtection);
+  fastify.addHook('onRequest', fastify.authenticateSession);
+
+  fastify.post(
+    '/add-donation',
+    {
+      schema: {
+        body: Type.Object({
+          isDonating: Type.Boolean()
+        }),
+        response: {
+          200: Type.Object({
+            message: Type.String(),
+            type: Type.Literal('success')
+          }),
+          500: Type.Object({
+            message: Type.Literal('Something went wrong.'),
+            type: Type.Literal('danger')
+          })
+        }
+      }
+    },
+    async (req, reply) => {
+      try {
+        await fastify.prisma.user.update({
+          where: { id: req.session.user.id },
+          data: {
+            isDonating: req.body.isDonating
+          }
+        });
+
+        return {
+          message: 'success',
+          type: 'success'
+        } as const;
+      } catch (error) {
+        fastify.log.error(error);
+        void reply.code(500);
+        return {
+          type: 'danger',
+          message: 'Something went wrong.'
+        } as const;
+      }
+    }
+  );
+
+  done();
+};
