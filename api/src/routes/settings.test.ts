f6736010346b6ca1aafd484e2a5afa778a9f733b
Sboonny <muhammed@freecodecamp.org>
2023-05-23 07:44:39 +0000

    sort the test"

--- api/src/routes/settings.test.ts
+++ api/src/routes/settings.test.ts
@@ -488,13 +488,12 @@ describe('settingRoutes', () => {
           where: { email: 'foo@bar.com' }
         });
 
-        expect(user?.about).toEqual({
-          about: 'Teacher at freeCodeCamp',
-          name: 'Quincy Larson',
-          location: 'USA',
-          picture:
-            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.eL2QwHH67XasjwwbkaUtVgHaHa%26pid%3DApi&f=1&ipt=498bbdee8dcdfc71d325b1751c901e4d8b5978b202c556b06f635d79ae1951b4&ipo=images'
-        });
+        expect(user?.about).toEqual('Teacher at freeCodeCamp');
+        expect(user?.name).toEqual('Quincy Larson');
+        expect(user?.location).toEqual('USA');
+        expect(user?.picture).toEqual(
+          'https://cdn.freecodecamp.org/platform/english/images/quincy-larson-signature.svg'
+        );
       });
 
       test('PUT updates the values in about settings without image', async () => {
