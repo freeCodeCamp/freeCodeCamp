da40a35e6d94374941c174c1ef4ccd03fdd9f4ef
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-05-25 11:34:19 +0200

    fix: handle no progressTimestamp case

--- api/src/utils/progress.test.ts
+++ api/src/utils/progress.test.ts
@@ -3,13 +3,13 @@ import { getCalendar } from './progress';
 describe('utils/progress', () => {
   describe('getCalendar', () => {
     it('should return an empty object if no timestamps are passed', () => {
-      const calendar = getCalendar([]);
-      expect(calendar).toEqual({});
+      expect(getCalendar([])).toEqual({});
+      expect(getCalendar()).toEqual({});
     });
     it('should take timestamps and return a calendar object', () => {
       const timestamps = [-1111001, 0, 1111000, 1111500, 1113000, 9999999];
-      const calendar = getCalendar(timestamps);
-      expect(calendar).toEqual({
+
+      expect(getCalendar(timestamps)).toEqual({
         '-1112': 1,
         0: 1,
         1111: 1,
@@ -20,8 +20,8 @@ describe('utils/progress', () => {
 
     it('should handle null, { timestamp: number } and float entries', () => {
       const timestamps = [null, { timestamp: 1113000 }, 1111000.5];
-      const calendar = getCalendar(timestamps);
-      expect(calendar).toEqual({
+
+      expect(getCalendar(timestamps)).toEqual({
         1111: 1,
         1113: 1
       });
--- api/src/utils/progress.ts
+++ api/src/utils/progress.ts
@@ -1,11 +1,11 @@
 export type ProgressTimestamp = number | { timestamp: number } | null;
 
 export const getCalendar = (
-  progressTimestamps: ProgressTimestamp[]
+  progressTimestamps?: ProgressTimestamp[]
 ): Record<string, 1> => {
   const calendar: Record<string, 1> = {};
 
-  progressTimestamps.forEach(progress => {
+  progressTimestamps?.forEach(progress => {
     if (progress === null) return;
     if (typeof progress === 'number') {
       calendar[Math.floor(progress / 1000)] = 1;
