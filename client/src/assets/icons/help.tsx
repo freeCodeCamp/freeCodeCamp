0851f3db91bf1abba0bbcf7c0b3b9c15020a9af5
Bruce B <bbsmooth@gmail.com>
2023-05-26 22:55:08 -0700

    refine styles

--- client/src/assets/icons/help.tsx
+++ client/src/assets/icons/help.tsx
@@ -5,20 +5,13 @@ const wrapper = {
   justifyContent: 'space-between',
   alignItems: 'center',
   position: 'relative',
-  padding: 'inherit',
-  width: '25px'
-} as React.CSSProperties;
-
-const speechBubbleIcon = {
-  position: 'absolute',
-  height: '60rem',
-  left: '-0.15rem'
+  padding: '0'
 } as React.CSSProperties;
 
 const questionMarkIcon = {
   position: 'absolute',
-  bottom: '8%',
-  left: '0.25rem'
+  top: 'calc(50% - 0.5rem)',
+  left: 'calc(50% - 0.225rem)'
 } as React.CSSProperties;
 
 function Help(
@@ -27,11 +20,10 @@ function Help(
   return (
     <div style={wrapper}>
       <svg
-        style={speechBubbleIcon}
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 512 512'
         width='1.25rem'
-        height='60rem'
+        height='1.3rem'
         fill='currentColor'
         {...props}
       >
@@ -42,7 +34,7 @@ function Help(
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 320 512'
         fill='currentColor'
-        width='.5rem'
+        width='.45rem'
         {...props}
       >
         <path d='M64 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H128C57.3 32 0 89.3 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z' />
--- client/src/assets/icons/reset.tsx
+++ client/src/assets/icons/reset.tsx
@@ -7,8 +7,8 @@ function Reset(
     <svg
       fill='currentColor'
       xmlns='http://www.w3.org/2000/svg'
-      height='24'
-      width='24'
+      height='1.325rem'
+      width='1.1rem'
       aria-hidden='true'
       {...props}
     >
