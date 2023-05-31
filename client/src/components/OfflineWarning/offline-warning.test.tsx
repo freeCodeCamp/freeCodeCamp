e83bfbbf55377a8b2b5ceb0aa62c8faf0209be13
Sky Jacobson <sky.jacobson231@gmail.com>
2023-05-31 13:17:13 -0700

    Create offline-warning.test.tsx
    Created unit tests for OfflineWarning component.

---
+++ client/src/components/OfflineWarning/offline-warning.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, act, screen } from '@testing-library/react';
+
+import OfflineWarning from './offline-warning';
+
+jest.useFakeTimers();
+
+describe('<OfflineWarning />', () => {
+  it('renders null when isOnline, isServerOnline and isSignedIn are true', () => {
+    const { container } = render(
+      <OfflineWarning isOnline={true} isServerOnline={true} isSignedIn={true} />
+    );
+    act(() => jest.runOnlyPendingTimers());
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('renders the misc.offline message when isOnline is false', () => {
+    render(
+      <OfflineWarning
+        isOnline={false}
+        isServerOnline={true}
+        isSignedIn={true}
+      />
+    );
+    act(() => jest.runOnlyPendingTimers());
+    expect(screen.getByText('misc.offline')).toBeInTheDocument();
+  });
+
+  it('renders the placeholder anchor tag', () => {
+    render(
+      <OfflineWarning
+        isOnline={true}
+        isServerOnline={false}
+        isSignedIn={true}
+      />
+    );
+    act(() => jest.runOnlyPendingTimers());
+    expect(screen.getByText('placeholder').tagName).toBe('A');
+  });
+
+  it('renders null when isSignedIn is false', () => {
+    const { container } = render(
+      <OfflineWarning
+        isOnline={true}
+        isServerOnline={true}
+        isSignedIn={false}
+      />
+    );
+    act(() => jest.runOnlyPendingTimers());
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('renders the misc.offline message when isOnline and isServerOnline are false', () => {
+    render(
+      <OfflineWarning
+        isOnline={false}
+        isServerOnline={false}
+        isSignedIn={true}
+      />
+    );
+    act(() => jest.runOnlyPendingTimers());
+    expect(screen.getByText('misc.offline')).toBeInTheDocument();
+  });
+});
