56456baa6495bac40344b8addc8a7a9deede768b
jacques metevier <jmetevier@gmail.com>
2023-06-04 16:40:52 -0700

    chore(deps) upgrade eslint

--- api/src/plugins/fastify-sentry.ts
+++ api/src/plugins/fastify-sentry.ts
@@ -2,6 +2,7 @@ import Sentry, { SentryPluginOptions } from '@immobiliarelabs/fastify-sentry';
 import { FastifyPluginCallback } from 'fastify';
 import fp from 'fastify-plugin';
 
+/* eslint-disable @typescript-eslint/no-misused-promises */
 const fastifySentry: FastifyPluginCallback<SentryPluginOptions> = async (
   fastify,
   options
--- client/src/utils/scrollbar-width.ts
+++ client/src/utils/scrollbar-width.ts
@@ -1,6 +1,6 @@
 import store from 'store';
 
 export function getScrollbarWidth(): number {
-  const storedWidth = parseInt(store.get('monacoScrollbarWidth'));
+  const storedWidth = parseInt(store.get('monacoScrollbarWidth') as string);
   return storedWidth >= 5 || storedWidth <= 25 ? storedWidth : 5;
 }
