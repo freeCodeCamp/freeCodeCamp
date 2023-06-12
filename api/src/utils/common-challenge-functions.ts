59e8c5edf5db017c029f0c3359c7cb56eafc1b5c
Niraj Nandish <nirajnandish@icloud.com>
2023-06-12 16:06:12 +0400

    fix: type issues

--- api/src/utils/common-challenge-functions.ts
+++ api/src/utils/common-challenge-functions.ts
@@ -1,4 +1,3 @@
-
 import { omit, pick } from 'lodash';
 import { user } from '@prisma/client';
 import { FastifyInstance } from 'fastify';
@@ -6,27 +5,53 @@ import { FastifyInstance } from 'fastify';
 import { getChallenges } from './get-challenges';
 
 const jsCertProjectIds = [
-    'aaa48de84e1ecc7c742e1124',
-    'a7f4d8f2483413a6ce226cac',
-    '56533eb9ac21ba0edf2244e2',
-    'aff0395860f5d3034dc0bfc9',
-    'aa2e6f85cab2ab736c9a9b24'
+  'aaa48de84e1ecc7c742e1124',
+  'a7f4d8f2483413a6ce226cac',
+  '56533eb9ac21ba0edf2244e2',
+  'aff0395860f5d3034dc0bfc9',
+  'aa2e6f85cab2ab736c9a9b24'
 ];
 
 const multifileCertProjectIds = getChallenges()
-    .filter(challenge => challenge.challengeType === 14)
-    .map(challenge => challenge.id);
+  .filter(challenge => challenge.challengeType === 14)
+  .map(challenge => challenge.id);
 
 const savableChallenges = getChallenges()
-    .filter(challenge => challenge.challengeType === 14)
-    .map(challenge => challenge.id);
+  .filter(challenge => challenge.challengeType === 14)
+  .map(challenge => challenge.id);
 
 type SavedChallengeFile = {
-    key: string;
-    ext: string;
-    name: string;
-    history?: string[];
-    contents: string;
+  key: string;
+  ext: string; // NOTE: This is Ext type in client
+  name: string;
+  history?: string[];
+  contents: string;
+};
+
+type SavedChallenge = {
+  id: string;
+  files?: SavedChallengeFile[];
+};
+
+// TODO: Confirm this type - read comments below
+type CompletedChallengeFile = {
+  key: string;
+  ext: string; // NOTE: This is Ext type in client
+  name: string;
+  contents: string;
+
+  // These values are present in prop-types and ajax.ts builds with it
+  // editableRegionBoundaries?: number[];
+  // usesMultifileEditor?: boolean;
+  // error: null | string | unknown;
+  // head: string;
+  // tail: string;
+  // seed: string;
+  // id: string;
+  // history: string[];
+
+  // This value is present in prisma schema
+  path?: string | null;
 };
 
 type CompletedChallenge = {
@@ -36,160 +61,169 @@ type CompletedChallenge = {
   challengeType?: number | null;
   completedDate: number;
   isManuallyApproved?: boolean | null;
-  files?: SavedChallengeFile[];
-}
+  files?: CompletedChallengeFile[];
+};
 
 export async function buildUserUpdate(
-    fastify: FastifyInstance,
-    user: user,
-    challengeId: string,
-    _userCompletedChallenge: CompletedChallenge,
-    timezone?: string,
+  fastify: FastifyInstance,
+  user: user,
+  challengeId: string,
+  _completedChallenge: CompletedChallenge,
+  timezone?: string // TODO: is this required as its not given as a arg anywhere
 ) {
-    const { files, completedDate: newProgressTimeStamp = Date.now() } = _userCompletedChallenge;
-    
-    let completedChallenge: CompletedChallenge;
-    
-    if (
-        jsCertProjectIds.includes(challengeId) ||
-        multifileCertProjectIds.includes(challengeId)
-    ) {
-        completedChallenge = {
-            ..._userCompletedChallenge,
-            files: files?.map(file =>
-                pick(file, ['contents', 'key', 'index', 'name', 'path', 'ext']) as SavedChallengeFile
-            )
-        };
-    } else {
-        completedChallenge = omit(_userCompletedChallenge, ['files']);
+  const { files, completedDate: newProgressTimeStamp = Date.now() } =
+    _completedChallenge;
+  let completedChallenge: CompletedChallenge;
+
+  if (
+    jsCertProjectIds.includes(challengeId) ||
+    multifileCertProjectIds.includes(challengeId)
+  ) {
+    completedChallenge = {
+      ..._completedChallenge,
+      files: files?.map(
+        file =>
+          pick(file, [
+            'contents',
+            'key',
+            'index',
+            'name',
+            'path',
+            'ext'
+          ]) as CompletedChallengeFile
+      )
+    };
+  } else {
+    completedChallenge = omit(_completedChallenge, ['files']);
+  }
+  let finalChallenge = {} as CompletedChallenge;
+
+  // Since these values are destuctured for easier updating, collectively update before returning
+  const {
+    timezone: userTimezone,
+    completedChallenges = [],
+    needsModeration = false,
+    savedChallenges = [],
+    progressTimestamps = [],
+    partiallyCompletedChallenges = []
+  } = user;
+
+  const userCompletedChallenges: CompletedChallenge[] = completedChallenges;
+  const userSavedChallenges: SavedChallenge[] = savedChallenges;
+  const userProgressTimestamps = progressTimestamps;
+  const userPartiallyCompletedChallenges = partiallyCompletedChallenges;
+
+  const oldIndex = userCompletedChallenges.findIndex(
+    ({ id }) => challengeId === id
+  );
+
+  const alreadyCompleted = oldIndex !== -1;
+  const oldChallenge = alreadyCompleted
+    ? userCompletedChallenges[oldIndex]
+    : null;
+
+  if (alreadyCompleted && oldChallenge) {
+    const challengeWithOldDate = userCompletedChallenges[oldIndex];
+
+    if (challengeWithOldDate) {
+      challengeWithOldDate.completedDate = oldChallenge.completedDate;
+      finalChallenge = {
+        ...completedChallenge,
+        ...challengeWithOldDate
+      };
     }
-    let finalChallenge = {} as CompletedChallenge;
-
-    // Since these values are destuctured for easier updating, collectively update before returning
-    const {
-        timezone: userTimezone,
-        completedChallenges = [],
-        needsModeration = false,
-        savedChallenges = [],
-        progressTimestamps = [],
-        partiallyCompletedChallenges = []
-    }:{
-        timezone: string | null;
-        completedChallenges?: CompletedChallenge[];
-        needsModeration: boolean | null;
-        savedChallenges?: SavedChallengeFile[];
-        progressTimestamps?: number[];
-        partiallyCompletedChallenges?: CompletedChallenge[];
-    } = user;
-
-    const oldIndex = completedChallenges.findIndex(
-        ({ id }) => challengeId === id
-    );
-
-    const alreadyCompleted = oldIndex !== -1;
-    const oldChallenge = alreadyCompleted ? completedChallenges[oldIndex] : null;
-
-    if (alreadyCompleted && oldChallenge) {
-        const challenges = completedChallenges;
-        const challengeWithOldDate = challenges[oldIndex];
-
-        if (challengeWithOldDate) {
-            challengeWithOldDate.completedDate = oldChallenge.completedDate;
-            finalChallenge = challengeWithOldDate;  
-        }
-
-
-        await fastify.prisma.user.update({
-            where: { id: user.id },
-            data: {
-                completedChallenges: challenges
-            }
-        });
-    } else {
-        finalChallenge = {
-            ...completedChallenge
-        };
-        // $push.progressTimestamps = completedDate;
-        if (progressTimestamps && Array.isArray(progressTimestamps)) {
-            progressTimestamps.push(newProgressTimeStamp);
-        }
-        // $push.completedChallenges = finalChallenge;
-        completedChallenges.push(finalChallenge);
-        await fastify.prisma.user.update({
-            where: { id: user.id },
-            data: {
-                progressTimestamps: progressTimestamps,
-                completedChallenges: completedChallenges
-            }
-        });
-    }
-
-    if (savableChallenges.includes(challengeId)) {
-        const challengeToSave: SavedChallenge = {
-            id: challengeId,
-            lastSavedDate: completedDate,
-            files: files?.map(file =>
-                pick(file, ['contents', 'key', 'name', 'e3xt', 'history'])
-            )
-        };
-
-        const savedIndex = savedChallenges.findIndex(
-            ({ id }) => challengeId === id
-        );
-
-        if (savedIndex >= 0) {
-            // $set[`savedChallenges.${savedIndex}`] = challengeToSave;
-            savedChallenges[savedIndex] = challengeToSave;
-        } else {
-            // $push.savedChallenges = challengeToSave;
-            savedChallenges.push(challengeToSave);
-        }
-    }
-
-    // remove from partiallyCompleted on submit
-    // $pull.partiallyCompletedChallenges = { id: challengeId };
-    partiallyCompletedChallenges.filter((challenge) => challenge.id !== challengeId)
-
-    if (
-        timezone &&
-        timezone !== 'UTC' &&
-        (!userTimezone || userTimezone === 'UTC')
-    ) {
-        timezone = userTimezone;
-        await fastify.prisma.user.update({
-            where: { id: user.id },
-            data: {
-                timezone
-            }
-        });
-    }
-
-    // Doesnt make sense as if false it wont be executed and when true sets "true" again
-    // if (needsModeration) {
-    //     await fastify.prisma.user.update({
-    //         where: { id: user.id },
-    //         data: {
-    //             needsModeration: true
-    //         }
-    //     });
-    // }
 
     await fastify.prisma.user.update({
-        where: { id: user.id },
-        data: {
-        completedChallenges,
-        needsModeration,
-        savedChallenges,
-        progressTimestamps,
-        partiallyCompletedChallenges
-    }});
-
-    const updateData = {};
-
-    return {
-        alreadyCompleted,
-        updateData,
-        completedDate: finalChallenge.completedDate,
-        savedChallenges
+      where: { id: user.id },
+      data: {
+        completedChallenges: userCompletedChallenges
+      }
+    });
+  } else {
+    finalChallenge = {
+      ...completedChallenge
     };
+    if (userProgressTimestamps && Array.isArray(userProgressTimestamps)) {
+      userProgressTimestamps.push(newProgressTimeStamp);
+    }
+    userCompletedChallenges.push(finalChallenge);
+    await fastify.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        progressTimestamps: userProgressTimestamps,
+        completedChallenges: userCompletedChallenges
+      }
+    });
+  }
+
+  if (savableChallenges.includes(challengeId)) {
+    const challengeToSave: SavedChallenge = {
+      id: challengeId,
+      lastSavedDate: newProgressTimeStamp,
+      files: files?.map(file =>
+        pick(file, ['contents', 'key', 'name', 'ext', 'history'])
+      )
+    };
+
+    const savedIndex = userSavedChallenges.findIndex(
+      ({ id }) => challengeId === id
+    );
+
+    if (savedIndex >= 0) {
+      userSavedChallenges[savedIndex] = challengeToSave;
+    } else {
+      userSavedChallenges.push(challengeToSave);
+    }
+  }
+
+  // remove from partiallyCompleted on submit
+  const updatedPartiallyCompletedChallenges =
+    userPartiallyCompletedChallenges.filter(
+      challenge => challenge.id !== challengeId
+    );
+
+  if (
+    timezone &&
+    timezone !== 'UTC' &&
+    !userTimezone &&
+    userTimezone === 'UTC'
+  ) {
+    timezone = userTimezone;
+    await fastify.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        timezone
+      }
+    });
+  }
+
+  // Doesnt make sense as if false it wont be executed and when true sets "true" again
+  // if (needsModeration) {
+  //     await fastify.prisma.user.update({
+  //         where: { id: user.id },
+  //         data: {
+  //             needsModeration: true
+  //         }
+  //     });
+  // }
+
+  await fastify.prisma.user.update({
+    where: { id: user.id },
+    data: {
+      completedChallenges: userCompletedChallenges,
+      needsModeration,
+      savedChallenges: userSavedChallenges,
+      progressTimestamps: userProgressTimestamps,
+      partiallyCompletedChallenges: updatedPartiallyCompletedChallenges
+    }
+  });
+
+  const updateData = {};
+
+  return {
+    alreadyCompleted,
+    updateData, // Might need to remove this variable as we're updating user object in this function now instead of in the endpoint handler
+    completedDate: finalChallenge.completedDate,
+    userSavedChallenges
+  };
 }
