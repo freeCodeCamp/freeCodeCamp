3043eec438dffaa4b7781e78091b99e3ac5a5ca9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-06-13 14:33:47 +0200

    feat: reject invalid ObjectIds

--- api/src/routes/challenge.ts
+++ api/src/routes/challenge.ts
@@ -1,4 +1,8 @@
-import { FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
+import {
+  type FastifyPluginCallbackTypebox,
+  Type
+} from '@fastify/type-provider-typebox';
+import { isObjectID } from '../utils/validation';
 
 export const challengeRoutes: FastifyPluginCallbackTypebox = (
   fastify,
@@ -9,9 +13,34 @@ export const challengeRoutes: FastifyPluginCallbackTypebox = (
   fastify.addHook('onRequest', fastify.csrfProtection);
   fastify.addHook('onRequest', fastify.authenticateSession);
 
-  fastify.post('/project-completed', () => {
-    return { done: true };
-  });
+  fastify.post(
+    '/project-completed',
+    {
+      schema: {
+        body: Type.Object({ id: Type.String() }),
+        response: {
+          // TODO: update to correct schema and test success case.
+          200: Type.Object({ done: Type.Boolean() }),
+          400: Type.Object({
+            type: Type.Literal('error'),
+            message: Type.Literal(
+              'That does not appear to be a valid challenge submission.'
+            )
+          })
+        }
+      }
+    },
+    (req, reply) => {
+      if (!isObjectID(req.body.id)) {
+        void reply.code(400);
+        return {
+          type: 'error',
+          message: 'That does not appear to be a valid challenge submission.'
+        } as const;
+      }
+      return { done: true };
+    }
+  );
 
   done();
 };
