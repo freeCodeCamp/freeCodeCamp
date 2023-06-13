0b80293b905f9bdb4745fe285b395247a6aac35a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-06-13 14:05:25 +0200

    test: specify the error responses

--- api/src/routes/challenge.test.ts
+++ api/src/routes/challenge.test.ts
@@ -24,11 +24,74 @@ describe('challengeRoutes', () => {
           setCookies
         }).send({ id: 'not-a-valid-id' });
 
-        expect(response?.body).toStrictEqual(
-          isValidChallengeCompletionErrorMsg
-        );
-        expect(response?.statusCode).toBe(400);
+        expect(response.body).toStrictEqual(isValidChallengeCompletionErrorMsg);
+        expect(response.statusCode).toBe(400);
       });
+
+      it('POST rejects requests with invalid challengeTypes', async () => {
+        const response = await superRequest('/project-completed', {
+          method: 'POST',
+          setCookies
+        }).send({
+          id: 'bd7123c8c441eddfaeb5bdef',
+          challengeType: 'not-a-valid-challenge-type'
+        });
+
+        expect(response.body).toStrictEqual(isValidChallengeCompletionErrorMsg);
+        expect(response.statusCode).toBe(400);
+      });
+
+      it('POST rejects requests without solutions', async () => {
+        const response = await superRequest('/project-completed', {
+          method: 'POST',
+          setCookies
+        }).send({
+          id: 'bd7123c8c441eddfaeb5bdef',
+          challengeType: 3
+        });
+
+        expect(response.body).toStrictEqual({
+          type: 'error',
+          message:
+            'You have not provided the valid links for us to inspect your work.'
+        });
+        expect(response.statusCode).toBe(400);
+      });
+
+      it('POST rejects requests with solutions that are not urls', async () => {
+        const response = await superRequest('/project-completed', {
+          method: 'POST',
+          setCookies
+        }).send({
+          id: 'bd7123c8c441eddfaeb5bdef',
+          challengeType: 3,
+          solution: 'not-a-valid-solution'
+        });
+
+        expect(response.body).toStrictEqual(isValidChallengeCompletionErrorMsg);
+        expect(response.statusCode).toBe(400);
+      });
+
+      it('POST rejects codeAlly projects when the user has not completed the required challenges', async () => {
+        const response = await superRequest('/project-completed', {
+          method: 'POST',
+          setCookies
+        }).send({
+          id: 'bd7123c8c441eddfaeb5bdef', // not a codeally challenge id, but does not matter
+          challengeType: 13, // this does matter, however, since there's special logic for that challenge type
+          solution: 'https://any.valid/url'
+        });
+
+        expect(response.body).toStrictEqual({
+          type: 'error',
+          message:
+            'You have to complete the project before you can submit a URL.'
+        });
+      });
+
+      // tests to add: successfully codeAlly project, successfully non-codeAlly
+      // project, successfully non-project challenge, resubmission, submission
+      // of savable challenges.
     });
   });
   describe('Unauthenticated user', () => {
