9716534a1af3cabc381a791313fd6be579bfc25e
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-06-15 09:53:11 +0200

    feat: add progress util
    This was created in the get-session-user PR but I need it here.

---
+++ api/src/utils/progress.ts
@@ -0,0 +1,22 @@
+export type ProgressTimestamp = number | { timestamp: number } | null;
+
+export const getCalendar = (
+  progressTimestamps?: ProgressTimestamp[]
+): Record<string, 1> => {
+  const calendar: Record<string, 1> = {};
+
+  progressTimestamps?.forEach(progress => {
+    if (progress === null) return;
+    if (typeof progress === 'number') {
+      calendar[Math.floor(progress / 1000)] = 1;
+    } else {
+      calendar[Math.floor(progress.timestamp / 1000)] = 1;
+    }
+  });
+
+  return calendar;
+};
+
+export const getPoints = (progressTimestamps?: ProgressTimestamp[]): number => {
+  return progressTimestamps?.length ?? 1;
+};
