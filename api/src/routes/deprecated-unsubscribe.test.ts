f8d4c9d0303cae08aa3afd7d614386a1bd551850
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-06-15 12:55:38 +0200

    test: update unsubscribe tests

--- api/src/routes/deprecated-unsubscribe.test.ts
+++ api/src/routes/deprecated-unsubscribe.test.ts
@@ -1,25 +1,22 @@
-import request from 'supertest';
-import { setupServer } from '../../jest.utils';
+import { setupServer, superRequest, ORIGIN } from '../../jest.utils';
 
 import { unsubscribeEndpoints } from './deprecated-unsubscribe';
+
+const urlEncodedMessage =
+  '?messages=info%5B0%5D%3DWe%2520are%2520no%2520longer%2520able%2520to%2520process%2520this%2520unsubscription%2520request.%2520Please%2520go%2520to%2520your%2520settings%2520to%2520update%2520your%2520email%2520preferences';
 
 describe('Deprecated unsubscribeEndpoints', () => {
   setupServer();
 
   unsubscribeEndpoints.forEach(([endpoint, method]) => {
-    test(`${method} ${endpoint} returns 410 status code with "info" message`, async () => {
-      const response = await request(fastifyTestInstance.server)[
-        method.toLowerCase() as 'get' | 'post'
-      ](endpoint);
+    test(`${method} ${endpoint} redirects to origin with "info" message`, async () => {
+      const response = await superRequest(endpoint, { method });
 
-      expect(response.status).toBe(410);
-      expect(response.body).toStrictEqual({
-        message: {
-          type: 'info',
-          message:
-            'We are no longer able to process this unsubscription request. Please go to your settings to update your email preferences'
-        }
-      });
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(response.headers.location).toStrictEqual(
+        ORIGIN + urlEncodedMessage
+      );
+      expect(response.status).toBe(302);
     });
   });
 });
