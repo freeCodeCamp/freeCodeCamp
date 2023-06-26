3efac104963d9a53f78140c18bd92f60e0ec3da2
Bruce B <bbsmooth@gmail.com>
2023-06-26 12:55:36 -0700

    fix: keyboard navigation on mobile step tabs

--- client/src/templates/Challenges/classic/mobile-layout.tsx
+++ client/src/templates/Challenges/classic/mobile-layout.tsx
@@ -1,6 +1,6 @@
 import { TabPane, Tabs } from '@freecodecamp/react-bootstrap';
 import i18next from 'i18next';
-import React, { Component, ReactElement, SyntheticEvent } from 'react';
+import React, { Component, ReactElement } from 'react';
 import { faWindowRestore } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { createSelector } from 'reselect';
@@ -85,14 +85,7 @@ class MobileLayout extends Component<MobileLayoutProps, MobileLayoutState> {
     currentTab: this.props.hasEditableBoundaries ? Tab.Editor : Tab.Instructions
   };
 
-  switchTab = (tab: Tab, e: SyntheticEvent): void => {
-    const portalButton = document.getElementById('portal-button');
-    // prevent switching to preview tab if pressing portal button
-    if (portalButton?.contains(e.target as Node)) {
-      e.preventDefault();
-      return;
-    }
-
+  switchTab = (tab: Tab): void => {
     this.setState({
       currentTab: tab
     });
@@ -261,21 +254,25 @@ class MobileLayout extends Component<MobileLayoutProps, MobileLayoutState> {
           {hasPreview && (
             <TabPane
               eventKey={Tab.Preview}
-              title={
-                <>
-                  {i18next.t('learn.editor-tabs.preview')}
-                  <button
-                    id='portal-button'
-                    aria-expanded={!!showPreviewPortal}
-                    onClick={() => togglePane('showPreviewPortal')}
-                  >
-                    <span className='sr-only'>{getPortalBtnSrText()}</span>
-                    <FontAwesomeIcon icon={faWindowRestore} />
-                  </button>
-                </>
-              }
+              title={i18next.t('learn.editor-tabs.preview')}
             >
+              <button
+                id='portal-button'
+                aria-expanded={!!showPreviewPortal}
+                onClick={() => togglePane('showPreviewPortal')}
+                tabIndex={this.state.currentTab === 'preview' ? 0 : -1}
+              >
+                <span>
+                  <span className='sr-only'>{getPortalBtnSrText()}</span>
+                  <FontAwesomeIcon icon={faWindowRestore} />
+                </span>
+              </button>
               {displayPreviewPane && preview}
+              {showPreviewPortal && (
+                <p className='preview-external-window'>
+                  Preview currently showing in external window
+                </p>
+              )}
             </TabPane>
           )}
           {!hasEditableBoundaries && (
