33647feccaf90ecf53e988631d58307c64f250b9
Mateo Guzmán <20783123+mateoguzmana@users.noreply.github.com>
2023-06-26 18:22:24 +0200

    fix(types): adding route component props to create-redirect
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/components/create-redirect.ts
+++ client/src/components/create-redirect.ts
@@ -2,7 +2,7 @@ import { navigate } from 'gatsby';
 import type { RouteComponentProps } from "@reach/router"
 
 const createRedirect =
-  (to = '/'): (() => JSX.Element | null) =>
+  (to = '/'): ((_props: RouteComponentProps) => JSX.Element | null) =>
   () => {
     if (typeof window !== 'undefined') {
       void navigate(to);
