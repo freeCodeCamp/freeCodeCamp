323158569b29d576c238145a10f0745295bb6d0e
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-07-11 18:23:59 +0300

    feat(tools): clean unneeded code in the menu items (#50341)

--- tools/ui-components/src/drop-down/menu-item/menu-item.tsx
+++ tools/ui-components/src/drop-down/menu-item/menu-item.tsx
@@ -1,90 +1,9 @@
 import { Menu } from '@headlessui/react';
 import React from 'react';
-import type { ButtonProps } from '../../button';
+import { type ButtonProps, HeadlessButton } from '../../button';
 
 export type MenuItemsProps = React.ComponentPropsWithoutRef<typeof Menu.Items> &
   ButtonProps;
-
-const Button = React.forwardRef<React.ElementRef<'button'>, ButtonProps>(
-  ({ className, onClick, disabled, children, ...rest }, ref) => {
-    // Manually prevent click event if the button is disabled
-    // as `aria-disabled` marks the element disabled but still registers the click event.
-    // Ref: https://css-tricks.com/making-disabled-buttons-more-inclusive/#aa-the-difference-between-disabled-and-aria-disabled
-
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      const ariaDisabled = event.currentTarget.getAttribute('aria-disabled');
-      if (!ariaDisabled && onClick) {
-        onClick(event);
-      }
-    };
-
-    return (
-      <button
-        className={className}
-        onClick={handleClick}
-        aria-disabled={disabled}
-        ref={ref}
-        {...rest}
-      >
-        {children}
-      </button>
-    );
-  }
-);
-
-const Link = React.forwardRef<React.ElementRef<'a'>, ButtonProps>(
-  ({ className, href, download, target, children, ...rest }, ref) => {
-    return (
-      <a
-        className={className}
-        download={download}
-        target={target}
-        ref={ref}
-        href={href ?? undefined}
-        {...rest}
-      >
-        {children}
-      </a>
-    );
-  }
-);
-
-export const HeadlessButton = React.forwardRef<
-  React.ElementRef<'button' | 'a'>,
-  ButtonProps
->(
-  (
-    { onClick, className, children, disabled, href, download, target, ...rest },
-    ref
-  ) => {
-    if (href && !disabled) {
-      return (
-        <Link
-          className={className}
-          href={href}
-          download={download}
-          target={target}
-          ref={ref as React.Ref<HTMLAnchorElement>}
-          {...rest}
-        >
-          {children}
-        </Link>
-      );
-    } else {
-      return (
-        <Button
-          className={className}
-          onClick={onClick}
-          aria-disabled={disabled}
-          ref={ref as React.Ref<HTMLButtonElement>}
-          {...rest}
-        >
-          {children}
-        </Button>
-      );
-    }
-  }
-);
 
 const defaultClass =
   'block text-center no-underline border-none px-4 py-1.5 focus:bg-background-secondary focus:text-foreground-secondary hover:text-foreground-secondary hover:bg-background-secondary w-full';
@@ -110,7 +29,3 @@ export const MenuItem = ({
     </Menu.Item>
   );
 };
-
-Button.displayName = 'Button';
-Link.displayName = 'Link';
-HeadlessButton.displayName = 'HeadlessButton';
