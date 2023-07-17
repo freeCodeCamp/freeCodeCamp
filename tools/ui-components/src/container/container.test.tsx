e8fe3f2511af5b230f3423eb117e4085a8b422c7
Muhammed Mustafa <muhammed@freecodecamp.org>
2023-07-17 12:41:56 +0300

    fix the test selector

--- tools/ui-components/src/container/container.test.tsx
+++ tools/ui-components/src/container/container.test.tsx
@@ -9,7 +9,7 @@ describe('<Container />', () => {
       <Container fluid={true}>Learn to code for free.</Container>
     );
     expect(
-      screen.getByText('Random text to test the element width')
+      screen.getByText('Learn to code for free.')
     ).toHaveClass('mx-auto px-[15px] ');
   });
   it('should add className to it', () => {
@@ -19,7 +19,7 @@ describe('<Container />', () => {
       </Container>
     );
     expect(
-      screen.getByText('Random text to test the element width')
+      screen.getByText('Learn to code for free.')
     ).toHaveClass(
       'mx-auto px-[15px] my-0 md:w-[750px] min-[992px]:w-[970px] min-[1200px]:w-[1170px] certificate-outer-wrapper'
     );
