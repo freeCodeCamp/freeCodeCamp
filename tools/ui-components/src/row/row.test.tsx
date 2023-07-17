e2a42ddabfc504687ab8ed9206717531f59b13a2
Sboonny <muhammedelruby@gmail.com>
2023-07-17 13:08:07 +0300

    change the test text

--- tools/ui-components/src/row/row.test.tsx
+++ tools/ui-components/src/row/row.test.tsx
@@ -5,9 +5,9 @@ import { Row } from '.';
 
 describe('<Row />', () => {
   it('Row can accept className', () => {
-    render(<Row className='h-full'>Random text to test the element width</Row>);
-    expect(
-      screen.getByText('Random text to test the element width')
-    ).toHaveClass('mx-[-15px] h-full');
+    render(<Row className='h-full'>Learn to code for free.</Row>);
+    expect(screen.getByText('Learn to code for free.')).toHaveClass(
+      'mx-[-15px] h-full'
+    );
   });
 });
