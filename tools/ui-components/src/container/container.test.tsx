be17c35e6f14811fcb489956033eb27d3bb2e364
Sboonny <muhammedelruby@gmail.com>
2023-07-17 13:09:02 +0300

    format

--- tools/ui-components/src/container/container.test.tsx
+++ tools/ui-components/src/container/container.test.tsx
@@ -5,12 +5,10 @@ import { Container } from '.';
 
 describe('<Container />', () => {
   it('remove width when the container is fluid', () => {
-    render(
-      <Container fluid={true}>Learn to code for free.</Container>
+    render(<Container fluid={true}>Learn to code for free.</Container>);
+    expect(screen.getByText('Learn to code for free.')).toHaveClass(
+      'mx-auto px-[15px] '
     );
-    expect(
-      screen.getByText('Learn to code for free.')
-    ).toHaveClass('mx-auto px-[15px] ');
   });
   it('should add className to it', () => {
     render(
@@ -18,9 +16,7 @@ describe('<Container />', () => {
         Learn to code for free.
       </Container>
     );
-    expect(
-      screen.getByText('Learn to code for free.')
-    ).toHaveClass(
+    expect(screen.getByText('Learn to code for free.')).toHaveClass(
       'mx-auto px-[15px] my-0 md:w-[750px] min-[992px]:w-[970px] min-[1200px]:w-[1170px] certificate-outer-wrapper'
     );
   });
