2112c72de5f3c26c8df214bf20c1c846aa200a67
Muhammed Mustafa <MuhammedElruby@gmail.com>
2023-07-17 14:08:29 +0300

    applying suggestion
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- tools/ui-components/src/row/row.tsx
+++ tools/ui-components/src/row/row.tsx
@@ -3,8 +3,5 @@ import React from 'react';
 import { RowProps } from './types';
 
 export const Row = ({ className, children }: RowProps) => {
-  if (!className) {
-    className = '';
-  }
-  return <div className={`mx-[-15px] ${className}`}>{children}</div>;
+  return <div className={`mx-[-15px] ${className ?? ''}`}>{children}</div>;
 };
