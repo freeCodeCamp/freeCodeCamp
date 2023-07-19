aad454f2f76a30740b650dbb4ca1dea8dbb59bb5
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-07-19 16:18:34 +0200

    feat: upgrade warning to error

--- client/src/utils/missing-trophy-warning.ts
+++ client/src/utils/missing-trophy-warning.ts
@@ -1,6 +1,6 @@
 import { FlashMessages } from '../components/Flash/redux/flash-messages';
 
 export const trophyMissingMessage = {
-  type: 'warn',
+  type: 'danger',
   message: FlashMessages.MSTrophyMissing
 };
