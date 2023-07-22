2909bfd140b634b1bfad6b1cb573eb6d2b54df38
Sboonny <muhammedelruby@gmail.com>
2023-07-22 21:37:29 +0300

    WIP add types of the sizes

--- tools/ui-components/src/col/col.tsx
+++ tools/ui-components/src/col/col.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { ColProps } from './types';
 
-export const Col = ({ className, children, ...props }: ColProps) => {
+export const Col = ({ className, children, xs, ...props }: ColProps) => {
   return (
     <div
-      className={`relative min-h-[1px] px-[15px] ${className ?? ''}`}
+      className={`relative min-h-[1px] px-[15px] float-left ${
+        className ?? ''
+      } ${xs ?? 'sm:w-full'}`}
       {...props}
     >
       {children}
--- tools/ui-components/src/col/types.ts
+++ tools/ui-components/src/col/types.ts
@@ -1,4 +1,8 @@
 export interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
   children?: React.ReactNode;
+  xs?: 8 | 12;
+  sm?: 2 | 4 | 6 | 8 | 10 | 12;
+  md?: 4 | 6 | 8 | 10;
+  lg?: 6 | 8 | 10;
 }
