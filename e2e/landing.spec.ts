a7e0a7aaa966d3714e2b564fb7ea3b14362ab428
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-08-02 13:02:26 +0500

    feat(e2e): adding remaining landing page tests

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -5,10 +5,32 @@ const landingPageElements = {
   callToAction: 'landing-big-cta',
   certifications: 'certifications',
   curriculumBtns: 'curriculum-map-button',
-  testimonials: 'testimonial-cards',
+  testimonials: 'testimonial-card',
   landingPageImage: 'landing-page-figure',
   faq: 'landing-page-faq'
 } as const;
+
+type LandingPageTypes<T> = T[keyof T];
+
+type LandingPageLogs = LandingPageTypes<typeof landingPageElements>;
+
+const superBlocks = [
+  'Responsive Web Design',
+  'JavaScript Algorithms and Data Structures',
+  'Front End Development Libraries',
+  'Data Visualization',
+  'Relational Database',
+  'Back End Development and APIs',
+  'Quality Assurance',
+  'Scientific Computing with Python',
+  'Data Analysis with Python',
+  'Information Security',
+  'Machine Learning with Python',
+  'College Algebra with Python',
+  'Coding Interview Prep',
+  'Project Euler',
+  'Legacy Responsive Web Design'
+];
 
 test.describe('Landing page', () => {
   test.beforeEach(async ({ page }) => {
@@ -21,18 +43,21 @@ test.describe('Landing page', () => {
     );
 
     const callToAction = page.getByTestId(landingPageElements.callToAction);
+    // const callToActionHeader = page.getByTestId(landingPageElements.callToAction).nth(1);
 
-    expect(await callToAction.first().innerText()).toBe(
-      "Get started (it's free)"
-    );
+    const callToActionHeader = page.locator('a .login-btn-text').nth(1);
 
-    expect(await callToAction.count()).toBe(4);
+    await expect(callToActionHeader).toHaveText("Get started (it's free)");
+
+    await expect(callToAction).toHaveCount(4);
   });
 
   test('Has visible header and sub-header', async ({ page }) => {
     const heading = page.getByTestId(landingPageElements.heading);
 
-    expect(await heading.innerText()).toBe('Learn to code — for free.');
+    // expect(await heading.innerText()).toBe('Learn to code — for free.');
+    await expect(heading).toContainText('Learn to code — for free.');
+
     expect(await page.isVisible('text=Build projects.')).toBeTruthy();
     expect(await page.isVisible('text=Earn certifications.')).toBeTruthy();
 
@@ -62,17 +87,33 @@ test.describe('Landing page', () => {
     page,
     isMobile
   }) => {
+    const landingPageImage = page.getByTestId(
+      landingPageElements.landingPageImage
+    );
     if (!isMobile) {
-      const landingPageImage = page.getByTestId(
-        landingPageElements.landingPageImage
-      );
-
       expect(await landingPageImage.isVisible()).toBeTruthy();
+    } else {
+      expect(await landingPageImage.isVisible()).toBeFalsy();
     }
   });
 
-  // test('Has `certifications` section', async ({ page }) => {
-  //   expect(await page.getByText('certifications').isVisible()).toBeTruthy();
-  //   expect(await page.locator('#certifications').isVisible()).toBeTruthy();
-  // });
+  test('Has links to all superblocks', async ({ page }) => {
+    const curriculumBtns = page.getByTestId(landingPageElements.curriculumBtns);
+    await expect(curriculumBtns).toHaveCount(15);
+
+    superBlocks.map(async (cert: LandingPageLogs, i) => {
+      const btn = curriculumBtns.nth(i);
+      await expect(btn).toContainText(cert);
+    });
+  });
+
+  test('Has 3 testimonial cards', async ({ page }) => {
+    const testimonials = page.locator(`.${landingPageElements.testimonials}`);
+    await expect(testimonials).toHaveCount(3);
+  });
+
+  test('Has FAQ section', async ({ page }) => {
+    const faqs = page.getByTestId(landingPageElements.faq);
+    await expect(faqs).toHaveCount(9);
+  });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -45,20 +45,20 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] }
     },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] }
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
 
     /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
     }
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] }
-    // }
 
     /* Test against branded browsers. */
     // {
