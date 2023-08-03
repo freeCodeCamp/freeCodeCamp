59818b1ff29182cc2f3ba5def8c0357fe7dfef84
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-04 02:30:53 +0530

    Update redirect-gen.ts

--- tools/scripts/redirect-gen.ts
+++ tools/scripts/redirect-gen.ts
@@ -1,36 +1,47 @@
 import fs from 'fs';
 import path from 'path';
 
-/* This can be used to create NGINX maps for redirects. After running this
-   script with `npx ts-node redirect-gen`, the map should appear in the current
-   directory.
-*/
-
 function createRedirectMap(): void {
-  const basePath = '../../../curriculum/challenges/english/18-project-euler';
-  const directories = fs.readdirSync(path.resolve(__dirname, basePath));
+  try {
+    // Specify the base path where the directories to be processed are located.
+    const basePath = path.resolve(__dirname, '../../../curriculum/challenges/english/18-project-euler');
 
-  let mapObject = '';
+    // Read the list of directories within the specified base path.
+    const directories = fs.readdirSync(basePath);
 
-  for (let i = 0; i < directories.length; i++) {
-    const files = fs.readdirSync(
-      path.resolve(__dirname, `${basePath}/${directories[i]}`)
-    );
+    // Initialize an empty string to store the NGINX map rules.
+    let mapObject = '';
 
-    for (let j = 0; j < files.length; j++) {
-      const fileName = path.parse(files[j]).name;
-      mapObject += `~^/learn/coding-interview-prep/project-euler/${fileName}/?$ /learn/project-euler/${directories[i]}/${fileName}; \n`;
+    // Loop through each directory found in the base path.
+    for (let i = 0; i < directories.length; i++) {
+      // Get the list of files within the current directory.
+      const files = fs.readdirSync(path.resolve(basePath, directories[i]));
+
+      // Loop through each file in the current directory.
+      for (let j = 0; j < files.length; j++) {
+        // Extract the file name without the extension.
+        const fileName = path.parse(files[j]).name;
+
+        // Generate the NGINX map rule for the current file and directory.
+        mapObject += `~^/learn/coding-interview-prep/project-euler/${fileName}/?$ /learn/project-euler/${directories[i]}/${fileName}; \n`;
+      }
     }
+
+    // Write the generated NGINX map rules to a file named "redirectMap.map".
+    fs.writeFile('redirectMap.map', mapObject, 'utf8', function (err) {
+      if (err) {
+        // Handle any errors that occur during file writing.
+        console.error('An error occurred while writing MAP redirect file', err);
+      } else {
+        console.log('Map file has been saved.');
+      }
+    });
+  } catch (err) {
+    // Handle any errors that occur during the execution of the script.
+    console.error('An error occurred while processing the redirect map', err);
   }
-
-  fs.writeFile('redirectMap.map', mapObject, 'utf8', function (err) {
-    if (err) {
-      console.log('An error occurred while writing MAP redirect file', err);
-      return console.log(err);
-    }
-
-    console.log('Map file has been saved.');
-  });
 }
 
+// Call the function to create the NGINX map.
 createRedirectMap();
+                 
