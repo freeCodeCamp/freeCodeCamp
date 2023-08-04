57714c1e60dee36d1fea91fe274a85211f30521d
Ahmadkashif <ahmadkashif983@gmail.com>
2023-08-04 20:57:25 +0500

    feat(e2e): reiterating playwright config for single threaded execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,16 +12,18 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 15 * 1000,
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://127.0.0.1:8000',
@@ -59,8 +61,8 @@ export default defineConfig({
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] }
     }
-/* Uncomment the blocks out if you want to enable the mentioned features */
-/* ====================================================== */
+    /* Uncomment the blocks out if you want to enable the mentioned features */
+    /* ====================================================== */
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
