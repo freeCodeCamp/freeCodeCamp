04a22343a3aa060c6af32de02e83570093669526
Ahmadkashif <ahmadkashif983@gmail.com>
2023-08-04 20:55:39 +0500

    feat(e2e): reiterating tests for single threaded execution

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
 const landingPageElements = {
   heading: 'landing-header',
@@ -13,6 +13,8 @@ const landingPageElements = {
 type LandingPageTypes<T> = T[keyof T];
 
 type LandingPageLogs = LandingPageTypes<typeof landingPageElements>;
+
+let page: Page;
 
 const superBlocks = [
   'Responsive Web Design',
@@ -32,87 +34,61 @@ const superBlocks = [
   'Legacy Responsive Web Design'
 ];
 
-test.describe('Landing page', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto('/');
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test('Should render', async () => {
+  await expect(page).toHaveTitle(
+    'Learn to Code — For Free — Coding Courses for Busy People'
+  );
+  const callToAction = page.getByTestId(landingPageElements.callToAction);
+  const callToActionHeader = page.locator('a .login-btn-text').nth(1);
+  await expect(callToActionHeader).toHaveText("Get started (it's free)");
+  await expect(callToAction).toHaveCount(4);
+});
+
+test('Has visible header and sub-header', async () => {
+  const heading = page.getByTestId(landingPageElements.heading);
+  await expect(heading).toContainText('Learn to code — for free.');
+  expect(await page.isVisible('text=Build projects.')).toBeTruthy();
+  expect(await page.isVisible('text=Earn certifications.')).toBeTruthy();
+  expect(
+    await page.isVisible(
+      'text=Since 2014, more than 40,000 freeCodeCamp.org ' +
+        'graduates have gotten jobs at tech companies including:'
+    )
+  ).toBeTruthy();
+});
+
+test('Has 5 brand logos', async () => {
+  await expect(page.locator('#featured-logos')).toBeVisible();
+});
+
+test('Has `as seen in` section', async () => {
+  await expect(page.locator('.as-seen-in')).toBeVisible();
+});
+
+test('Has links to all superblocks', async () => {
+  const curriculumBtns = page.getByTestId(landingPageElements.curriculumBtns);
+  await expect(curriculumBtns).toHaveCount(15);
+  superBlocks.map(async (cert: LandingPageLogs, i) => {
+    const btn = curriculumBtns.nth(i);
+    await expect(btn).toContainText(cert);
   });
+});
 
-  test('Should render', async ({ page }) => {
-    await expect(page).toHaveTitle(
-      'Learn to Code — For Free — Coding Courses for Busy People'
-    );
+test('Has 3 testimonial cards', async () => {
+  const testimonials = page.locator(`.${landingPageElements.testimonials}`);
+  await expect(testimonials).toHaveCount(3);
+});
 
-    const callToAction = page.getByTestId(landingPageElements.callToAction);
-
-    const callToActionHeader = page.locator('a .login-btn-text').nth(1);
-
-    await expect(callToActionHeader).toHaveText("Get started (it's free)");
-
-    await expect(callToAction).toHaveCount(4);
-  });
-
-  test('Has visible header and sub-header', async ({ page }) => {
-    const heading = page.getByTestId(landingPageElements.heading);
-
-    // expect(await heading.innerText()).toBe('Learn to code — for free.');
-    await expect(heading).toContainText('Learn to code — for free.');
-
-    expect(await page.isVisible('text=Build projects.')).toBeTruthy();
-    expect(await page.isVisible('text=Earn certifications.')).toBeTruthy();
-
-    expect(
-      await page.isVisible(
-        'text=Since 2014, more than 40,000 freeCodeCamp.org ' +
-          'graduates have gotten jobs at tech companies including:'
-      )
-    ).toBeTruthy();
-  });
-
-  test('Has 5 brand logos', async ({ page }) => {
-    const logoRowLength = await page
-      .locator('.logo-row')
-      .locator('svg')
-      .count();
-
-    expect(logoRowLength).toBe(5);
-  });
-
-  test('Has `as seen in` section', async ({ page }) => {
-    expect(await page.getByText('as seen in').isVisible()).toBeTruthy();
-    expect(await page.locator('#featured-logos').isVisible()).toBeTruthy();
-  });
-
-  test('Has a visible large image on large viewports', async ({
-    page,
-    isMobile
-  }) => {
-    const landingPageImage = page.getByTestId(
-      landingPageElements.landingPageImage
-    );
-    if (!isMobile) {
-      expect(await landingPageImage.isVisible()).toBeTruthy();
-    } else {
-      expect(await landingPageImage.isVisible()).toBeFalsy();
-    }
-  });
-
-  test('Has links to all superblocks', async ({ page }) => {
-    const curriculumBtns = page.getByTestId(landingPageElements.curriculumBtns);
-    await expect(curriculumBtns).toHaveCount(15);
-
-    superBlocks.map(async (cert: LandingPageLogs, i) => {
-      const btn = curriculumBtns.nth(i);
-      await expect(btn).toContainText(cert);
-    });
-  });
-
-  test('Has 3 testimonial cards', async ({ page }) => {
-    const testimonials = page.locator(`.${landingPageElements.testimonials}`);
-    await expect(testimonials).toHaveCount(3);
-  });
-
-  test('Has FAQ section', async ({ page }) => {
-    const faqs = page.getByTestId(landingPageElements.faq);
-    await expect(faqs).toHaveCount(9);
-  });
+test('Has FAQ section', async () => {
+  const faqs = page.getByTestId(landingPageElements.faq);
+  await expect(faqs).toHaveCount(9);
 });
