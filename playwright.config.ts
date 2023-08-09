fa6ec782afa0e45b09649fd445748f3c0909fb39
Ahmadkashif <ahmadkashif983@gmail.com>
2023-08-09 07:56:59 +0500

    feat(e2e): enabling baseURL switch between gitpod and local env

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: process.env.HOME_LOCATION || 'http://127.0.0.1:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
