7d0ccf7858d4767b4d9b031c138044999835586a
CallmeHongmaybe <s3757165@rmit.edu.vn>
2023-08-09 15:55:15 +0700

    ref: Improvement from feedback (v4)

--- client/src/assets/icons/completion-ribbon.tsx
+++ client/src/assets/icons/completion-ribbon.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
-import { userSelector } from '../../redux/selectors';
-import { Themes } from '../../components/settings/theme';
-
-const mapStateToProps = createSelector(
-  userSelector,
-  ({ theme = Themes.Default }: { theme: Themes }) => ({
-    theme
-  })
-);
+import React from 'react'
 
 interface RibbonProps {
   value: number;
-  theme: Themes;
 }
 
-const RibbonIcon = ({ value, theme }: RibbonProps): JSX.Element => {
-  const fillColor = theme === Themes.Night ? 'white' : 'black';
-
+const RibbonIcon = ({ value }: RibbonProps): JSX.Element => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -30,11 +16,11 @@ const RibbonIcon = ({ value, theme }: RibbonProps): JSX.Element => {
     >
       <path
         d='M25 35.3418L35.4851 28L44.5957 41.0113L36.2658 39.7151L34.1106 48.353L25 35.3418Z'
-        fill={fillColor}
+        className='map-icon'
       />
       <path
         d='M9.11059 29L19.5957 36.3418L10.4851 49.353L8.85418 41.0821L-4.67677e-07 42.0113L9.11059 29Z'
-        fill={fillColor}
+        className='map-icon'
       />
       <circle cx='21.9999' cy='21' r='20' stroke='black' strokeWidth='2' />
       <circle
@@ -49,7 +35,7 @@ const RibbonIcon = ({ value, theme }: RibbonProps): JSX.Element => {
         x='22.5'
         y='21'
         fontFamily='Verdana'
-        color={fillColor}
+        color='black'
         fontSize='15'
         fill='#fff'
         textAnchor='middle'
@@ -63,4 +49,4 @@ const RibbonIcon = ({ value, theme }: RibbonProps): JSX.Element => {
 
 RibbonIcon.displayName = 'RibbonIcon';
 
-export default connect(mapStateToProps)(RibbonIcon);
+export default RibbonIcon
