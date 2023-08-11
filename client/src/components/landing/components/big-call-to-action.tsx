aae2da13b25e6eafa328d89f0ccc7c97a1f4ea88
Max Zimmer <zimmemax@oregonstate.edu>
2023-08-11 01:26:30 +0000

    fixed text on landing page call to action button based on cogintive walkthrough findings

--- client/src/components/landing/components/big-call-to-action.tsx
+++ client/src/components/landing/components/big-call-to-action.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Login from '../../Header/components/login';
 
 const BigCallToAction = (): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <Login block={true} data-test-label='landing-big-cta'>
-      {t('buttons.logged-in-cta-btn')}
+      Create Account (it&apos;s free!)
     </Login>
   );
 };
