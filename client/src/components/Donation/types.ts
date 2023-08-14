239a028ad59f9ccae29d1402ebd119ac9ee9aa78
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-08-14 13:45:12 +0200

    refactor: move types to config from client
    To KISS the dependency relationship should always be one-way: client
    depends on config.

--- client/src/components/Donation/types.ts
+++ client/src/components/Donation/types.ts
@@ -8,13 +8,6 @@ export type HandleAuthentication = (
   paymentMethod: string
 ) => Promise<PaymentIntentResult | { error: { type: string } }>;
 
-export type DonationAmount = 500 | 1000 | 2000 | 3000 | 4000 | 5000;
-export type DonationDuration = 'one-time' | 'month';
-export interface DonationConfig {
-  donationAmount: DonationAmount;
-  donationDuration: DonationDuration;
-}
-
 export interface PostPayment {
   paymentProvider: PaymentProvider;
   data?: DonationApprovalData;
