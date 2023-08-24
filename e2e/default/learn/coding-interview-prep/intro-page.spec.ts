82be1dffb2eb71327fba2be0a9074ea94f14ac46
munzah-shah <munzah.shah00@gmail.com>
2023-08-24 16:40:58 +0500

    refactor(e2e): added an extra comment

--- e2e/default/learn/coding-interview-prep/intro-page.spec.ts
+++ e2e/default/learn/coding-interview-prep/intro-page.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+// Testing: Remove this comment later
 
 test.describe('Certification intro page', () => {
   let page: Page;
