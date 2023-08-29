6df2626a6cde036f56a5b36da164fd9d0d8a3be4
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-08-29 13:54:23 +0200

    fix: use env.ts to get JWT_SECRET
    api should not rely on secrets.js (as it is an api-server specific
    contrivance)

--- api/src/routes/challenge.ts
+++ api/src/routes/challenge.ts
@@ -1,12 +1,12 @@
 import { type FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
 import jwt from 'jsonwebtoken';
 import { uniqBy } from 'lodash';
-import { jwtSecret } from '../../../api-server/config/secrets';
 import { getChallenges } from '../utils/get-challenges';
 import { updateUserChallengeData } from '../utils/common-challenge-functions';
 import { formatValidationError } from '../utils/error-formatting';
 import { schemas } from '../schemas';
 import { getPoints, ProgressTimestamp } from '../utils/progress';
+import { JWT_SECRET } from '../utils/env';
 import { challengeTypes } from '../../../config/challenge-types';
 import {
   canSubmitCodeRoadCertProject,
@@ -65,8 +65,11 @@ export const challengeRoutes: FastifyPluginCallbackTypebox = (
       }
 
       try {
-        if (typeof encodedUserToken === 'string' && jwtSecret) {
-          const payload = jwt.verify(encodedUserToken, jwtSecret) as JwtPayload;
+        if (typeof encodedUserToken === 'string') {
+          const payload = jwt.verify(
+            encodedUserToken,
+            JWT_SECRET
+          ) as JwtPayload;
           userToken = payload.userToken;
         }
       } catch {
