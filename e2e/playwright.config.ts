9ccd9ebf96e59464283285de18601bf205568544
Ahmadkashif <ahmadkashif983@gmail.com>
2023-09-07 18:56:43 +0500

    feat(e2e): testing if parallelism works on CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
