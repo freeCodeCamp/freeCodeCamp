3f764dc6853b1d8e5a56343358137feb9e9cb532
Ahmadkashif <ahmadkashif983@gmail.com>
2023-09-07 20:26:40 +0500

    feat(e2e): finalizing touches for PR

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
