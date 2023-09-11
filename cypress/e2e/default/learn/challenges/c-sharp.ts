b6d51d699883c2dece75772c8225327d7ed1d0e7
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-11 09:12:18 -0700

    chore: should be visible
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- cypress/e2e/default/learn/challenges/c-sharp.ts
+++ cypress/e2e/default/learn/challenges/c-sharp.ts
@@ -12,8 +12,8 @@ describe('C Sharp Challenges', () => {
 
   it('renders the correct buttons', () => {
     cy.get('.btn-primary').should('have.length', 2);
-    cy.contains('.btn-primary', 'Check your answer').should('exist');
-    cy.contains('.btn-primary', 'Ask for Help').should('exist');
+    cy.contains('.btn-primary', 'Check your answer').should('be.visible');
+    cy.contains('.btn-primary', 'Ask for Help').should('be.visible');
   });
 });
 
@@ -25,8 +25,8 @@ describe('C Sharp Trophy Challenges', () => {
 
   it('renders the correct buttons', () => {
     cy.get('.btn-primary').should('have.length', 3);
-    cy.contains('.btn-primary', 'Link Account').should('exist');
-    cy.contains('.btn-primary', 'Verify Trophy').should('exist');
-    cy.contains('.btn-primary', 'Ask for Help').should('exist');
+    cy.contains('.btn-primary', 'Link Account').should('be.visible');
+    cy.contains('.btn-primary', 'Verify Trophy').should('be.visible');
+    cy.contains('.btn-primary', 'Ask for Help').should('be.visible');
   });
 });
