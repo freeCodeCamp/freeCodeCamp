fbfdfdb360278032fab25406f9152701e8f126bf
Fahdkassim <fahdkassim@gmail.com>
2023-09-13 15:10:03 +0500

    fix(e2e): untracked the config and utils folders

--- config/misc.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SEARCH_EXPOSED_WIDTH = exports.TOOL_PANEL_HEIGHT = exports.MAX_MOBILE_WIDTH = void 0;
-exports.MAX_MOBILE_WIDTH = 767;
-exports.TOOL_PANEL_HEIGHT = 37;
-exports.SEARCH_EXPOSED_WIDTH = 980;
