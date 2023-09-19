7167787447dbeccdc4075b6a0ad06fa7faf1070b
mot01 <tmondloch01@gmail.com>
2023-09-19 09:52:50 -0500

    feat: set jest max-workers

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,11 +1,20 @@
+import path from 'node:path';
 import type { Config } from 'jest';
+
+import { config as envConfig } from 'dotenv';
+
+const envPath = path.resolve(__dirname, '../.env');
+envConfig({ path: envPath });
 
 const config: Config = {
   verbose: true,
   testRegex: '\\.test\\.ts$',
   transform: {
     '^.+\\.ts$': 'ts-jest'
-  }
+  },
+  ...(process.env.FREECODECAMP_NODE_ENV == 'development'
+    ? { maxWorkers: 5 }
+    : {})
 };
 
 export default config;
