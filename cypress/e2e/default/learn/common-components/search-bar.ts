9a72d81ecd3e20fb6f12454797b7a054af9fd1ac
Parvez Khan <parvez.cs20@aitrc.edu.in>
2023-09-22 05:43:09 +0000

    Refactor Cypress Tests: Improve Comments, Test Descriptions, and Error Handling

--- cypress/e2e/default/learn/common-components/search-bar.ts
+++ cypress/e2e/default/learn/common-components/search-bar.ts
@@ -1,3 +1,4 @@
+// Utility functions
 const search = (query: string) => {
   cy.get('.ais-SearchBox').within(() => {
     cy.get('input').type(query);
@@ -12,6 +13,7 @@ const clear = () => {
   });
 };
 
+// Search bar optimized describe block
 describe('Search bar optimized', () => {
   before(() => {
     cy.visit('/');
@@ -21,15 +23,18 @@ describe('Search bar optimized', () => {
     clear();
   });
 
-  it('Should render properly', () => {
+  // Test: Search bar should be visible
+  it('Search bar should be visible', () => {
     cy.get('[data-cy=ais-SearchBox]').should('be.visible');
   });
 
+  // Test: Should not display hits
   it('Should not display hits', () => {
     search('freeCodeCamp');
     cy.get('[data-cy=ais-Hits-list]').should('not.exist');
   });
 
+  // Test: Should open a new tab
   it('Should open a new tab ', () => {
     cy.visit('/', {
       onBeforeLoad(win) {
@@ -44,6 +49,7 @@ describe('Search bar optimized', () => {
   });
 });
 
+// Search bar describe block
 describe('Search bar', () => {
   before(() => {
     cy.visit('/learn');
@@ -53,59 +59,54 @@ describe('Search bar', () => {
     clear();
   });
 
-  it('Should render properly', () => {
+  // Test: Search bar should be visible
+  it('Search bar should be visible', () => {
     cy.get('.ais-SearchBox').should('be.visible');
   });
 
+  // Test: Should accept input and display hits
   it('Should accept input and display hits', () => {
     search('freeCodeCamp');
-
     cy.get('[data-cy=ais-Hits-list]')
       .children()
       .should('to.have.length.of.at.least', 1);
   });
 
+  // Test: Should clear hits when input is cleared
   it('Should clear hits when input is cleared', () => {
     search('freeCodeCamp');
-
     cy.get('[data-cy=ais-Hits-list]')
       .children()
       .should('to.have.length.of.at.least', 1);
-
     clear();
-
     cy.get('div.ais-Hits').should('not.exist');
   });
 
+  // Test: Should show up to 8 hits when height >= 768px
   it('Should show up to 8 hits when height >= 768px', () => {
     cy.viewport(1300, 768);
-
     search('freeCodeCamp');
-
     cy.get('[data-cy=ais-Hits-list]').children().should('to.have.length.of', 8);
   });
 
+  // Test: Should show up to 5 hits when height < 768px
   it('Should show up to 5 hits when height < 768px', () => {
     cy.viewport(1300, 767);
-
     search('freeCodeCamp');
-
     cy.get('[data-cy=ais-Hits-list]').children().should('to.have.length.of', 5);
   });
 
+  // Test: Should show no hits for queries that do not exist in the Algolia index
   it('Should show no hits for queries that do not exist in the Algolia index', () => {
     search('testtttt');
-
     cy.get('[data-cy=ais-Hits-list]').children().should('to.have.length.of', 0);
-
     cy.contains('No tutorials found');
   });
 
+  // Test: Should not redirect to the News search page if there are no hits
   it('Should not redirect to the News search page if there are no hits', () => {
     search('testtttt');
-
     cy.get('.ais-SearchBox-form').submit();
-
     cy.location().should(loc => {
       expect(loc.pathname).to.eq('/learn/');
     });
