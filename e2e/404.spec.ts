e06e2c76535c7fe90e4eeb647fb2ecbe55157a5e
Subhrajit <subhrajitdutta007@gmail.com>
2023-10-01 15:56:48 +0530

    feat(e2e,playwright): added test file for 404.tsx

---
+++ e2e/404.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect, chromium, Page } from '@playwright/test';
+
+test.describe('404 Page', () => {
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('/404');
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('Should have a relevant page title', async () => {
+    await expect(page).toHaveTitle('404 Page Not Found');
+  });
+
+  test('Should display a message indicating a 404 error', async () => {
+    await expect(
+      page.locator("text=The page you're looking for does not exist.")
+    ).toBeVisible();
+  });
+
+  test('Should have a link to the homepage', async () => {
+    const homepageLink = await page.locator(
+      '[data-playwright-test-label="homepage-link"]'
+    );
+    await expect(homepageLink).toBeVisible();
+    await expect(homepageLink).toHaveText('Go to Homepage');
+  });
+});
