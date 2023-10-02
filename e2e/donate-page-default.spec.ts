29c32931a7bd775ed6d494218ab4942dfea10a26
HARSH ANAND <harsh.anand.ggl@gmail.com>
2023-10-02 15:34:32 +0000

    feat(e2e,playwright): created test file for donate-page-default.ts

--- e2e/donate-page-default.spec.ts
+++ e2e/donate-page-default.spec.ts
@@ -1,73 +1,69 @@
 import { test, expect, type Page } from '@playwright/test';
 
-test.describe('Donate page', () => {
-  let page: Page;
-  test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-    await page.goto('/donate');
-  });
+const frequentlyAskedQuestions = [
+  'How can I get help with my donations?',
+  'How transparent is freeCodeCamp.org?',
+  'How efficient is freeCodeCamp?',
+  'How can I make a one-time donation?',
+  'Does freeCodeCamp accept donations in Bitcoin or other cryptocurrencies?',
+  'Can I mail a physical check?',
+  'How can I set up matching gifts from my employer, or payroll deductions?',
+  'How can I set up an Endowment Gift to freeCodeCamp.org?',
+  'How can I set up a Legacy gift to freeCodeCamp.org?',
+  'How can I donate stock to freeCodeCamp.org?',
+  'I set up a monthly donation, but I need to update or pause the monthly recurrence. How can I do this?',
+  'Is there anything else I can learn about donating to freeCodeCamp.org?'
+];
 
-  test.afterAll(async () => {
-    await page.close();
-  });
+let page: Page;
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto('/donate');
+});
 
-  test('Should have a relevant page title', async () => {
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.describe('Should render the donation page correctly', () => {
+  test('should display the correct title', async () => {
     await expect(page).toHaveTitle('Support our charity | freeCodeCamp.org');
   });
 
-  test('Should render the donation page correctly', async () => {
-    await expect(
-      page.locator('text=Confirm your donation of $5 / month:')
-    ).toBeVisible();
+  test('should display the main heading', async () => {
+    const mainHeading = page.getByTestId('main-head');
+    expect(mainHeading).not.toBeNull();
+    expect(await mainHeading.textContent()).toBe('Help us do more');
+  });
 
-    expect(page.locator('text=Frequently asked questions'));
-
-    expect(page.locator('text=How can I get help with my donations?'));
-
-    expect(page.locator('text=How transparent is freeCodeCamp.org?'));
-
-    expect(page.locator('text=How efficient is freeCodeCamp?'));
-
-    expect(page.locator('text=How can I make a one-time donation?'));
-
-    expect(
-      page.locator(
-        'text=Does freeCodeCamp accept donations in Bitcoin or other cryptocurrencies?'
-      )
+  test('should display donation page upper text 1', async () => {
+    const donateText1 = page.getByTestId('donate-text-1');
+    await expect(donateText1).toHaveText(
+      'freeCodeCamp is a highly efficient education charity.'
     );
-
-    expect(page.locator('text=Can I mail a physical check?'));
-
-    expect(
-      page.locator(
-        'text=How can I set up matching gifts from my employer, or payroll deductions?'
-      )
+  });
+  test('should display donation page benefit 2', async () => {
+    const donateText2 = page.getByTestId('donate-text-2');
+    await expect(donateText2).toHaveText(
+      'When you donate to freeCodeCamp, you help people learn new skills and provide for their families.'
     );
-
-    expect(
-      page.locator(
-        'text=How can I set up an Endowment Gift to freeCodeCamp.org?'
-      )
+  });
+  test('should display donation page benefit 3', async () => {
+    const donateText3 = page.getByTestId('donate-text-3');
+    await expect(donateText3).toHaveText(
+      'You also help us create new resources for you to use to expand your own technology skills.'
     );
+  });
 
-    await expect(
-      page.locator('text=How can I set up a Legacy gift to freeCodeCamp.org?')
-    ).toBeVisible();
+  test('should display the faq heading', async () => {
+    const faqHead = page.getByTestId('faq-head');
+    expect(faqHead).not.toBeNull();
+    expect(await faqHead.textContent()).toBe('Frequently asked questions');
+  });
 
-    await expect(
-      page.locator('text=How can I donate stock to freeCodeCamp.org?')
-    ).toBeVisible();
-
-    expect(
-      page.locator(
-        'text=I set up a monthly donation, but I need to update or pause the monthly recurrence. How can I do this?'
-      )
-    );
-
-    expect(
-      page.locator(
-        'text=Is there anything else I can learn about donating to freeCodeCamp.org?'
-      )
-    );
+  test('Frequently asked question list buttons', async () => {
+    for (const i of frequentlyAskedQuestions) {
+      await page.getByRole('button', { name: `${i}` }).click();
+    }
   });
 });
