777f780b491bb3b7f27043a2f0be0c0df44f2111
HARSH ANAND <harsh.anand.ggl@gmail.com>
2023-10-03 08:51:45 +0000

    feat(e2e,playwright): created test file for donate-page-default.ts

--- e2e/donate-page-default.spec.ts
+++ e2e/donate-page-default.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect, type Page } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
 
 const frequentlyAskedQuestions = [
-  'How can I get help with my donations?',
-  'How transparent is freeCodeCamp.org?',
-  'How efficient is freeCodeCamp?',
-  'How can I make a one-time donation?',
-  'Does freeCodeCamp accept donations in Bitcoin or other cryptocurrencies?',
-  'Can I mail a physical check?',
-  'How can I set up matching gifts from my employer, or payroll deductions?',
-  'How can I set up an Endowment Gift to freeCodeCamp.org?',
-  'How can I set up a Legacy gift to freeCodeCamp.org?',
-  'How can I donate stock to freeCodeCamp.org?',
-  'I set up a monthly donation, but I need to update or pause the monthly recurrence. How can I do this?',
-  'Is there anything else I can learn about donating to freeCodeCamp.org?'
+  translations.donate['get-help'],
+  translations.donate['how-transparent'],
+  translations.donate['how-efficient'],
+  translations.donate['how-one-time'],
+  translations.donate['does-crypto'],
+  translations.donate['can-check'],
+  translations.donate['how-matching-gift'],
+  translations.donate['how-endowment'],
+  translations.donate['how-legacy'],
+  translations.donate['how-stock'],
+  translations.donate['how-update'],
+  translations.donate['anything-else']
 ];
 
 let page: Page;
@@ -25,7 +26,7 @@ test.afterAll(async () => {
   await page.close();
 });
 
-test.describe('Should render the donation page correctly', () => {
+test.describe('Donate Page', () => {
   test('should display the correct title', async () => {
     await expect(page).toHaveTitle('Support our charity | freeCodeCamp.org');
   });
@@ -42,12 +43,14 @@ test.describe('Should render the donation page correctly', () => {
       'freeCodeCamp is a highly efficient education charity.'
     );
   });
+
   test('should display donation page benefit 2', async () => {
     const donateText2 = page.getByTestId('donate-text-2');
     await expect(donateText2).toHaveText(
       'When you donate to freeCodeCamp, you help people learn new skills and provide for their families.'
     );
   });
+
   test('should display donation page benefit 3', async () => {
     const donateText3 = page.getByTestId('donate-text-3');
     await expect(donateText3).toHaveText(
@@ -63,7 +66,7 @@ test.describe('Should render the donation page correctly', () => {
 
   test('Frequently asked question list buttons', async () => {
     for (const i of frequentlyAskedQuestions) {
-      await page.getByRole('button', { name: `${i}` }).click();
+      await page.getByRole('button', { name: `${i}` }).isVisible();
     }
   });
 });
