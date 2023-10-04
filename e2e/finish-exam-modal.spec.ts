b89b18732265d3116db97b1a18ec1310dd3263e6
Dmitry Drepin <lixefer@gmail.com>
2023-10-04 22:49:28 +0200

    test(e2e): finish exam modal

--- e2e/finish-exam-modal.spec.ts
+++ e2e/finish-exam-modal.spec.ts
@@ -1,22 +1,49 @@
-import { test, expect } from '@playwright/test';
-
+import { test, expect, type Page } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
-const currentUrlPath = ''; //TODO add url
+let page: Page;
+const startUrl = '/learn';
+const examUrl =
+  '/learn/foundational-c-sharp-with-microsoft/foundational-c-sharp-with-microsoft-certification-exam/foundational-c-sharp-with-microsoft-certification-exam';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(currentUrlPath);
-  //TODO navigate to the desired page
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto(startUrl);
+  //sign-in
+  await page
+    .getByRole('link', { name: translations.buttons['sign-in'], exact: true })
+    .click();
 });
 
-test.afterEach(async ({ page }) => {
+test.beforeEach(async () => {
+  await page.goto(examUrl);
+  await page
+    .getByRole('button', {
+      name: translations.buttons['click-start-exam']
+    })
+    .click();
+  for (let i = 0; i < 4; i++) {
+    await page.getByTestId('exam-answer-label-0').check();
+    await page
+      .getByRole('button', {
+        name: translations.buttons['next-question']
+      })
+      .click();
+  }
+  await page.getByTestId('exam-answer-label-0').check();
+  await page
+    .getByRole('button', {
+      name: translations.buttons['finish-exam']
+    })
+    .click();
+});
+
+test.afterAll(async () => {
   await page.close();
 });
 
 test.describe('Finish Exit exam Modal E2E Test Suite', () => {
-  test('Verifies the Correct Rendering of the Finish Exit exam Modal', async ({
-    page
-  }) => {
+  test('Verifies the Correct Rendering of the Finish Exit exam Modal', async () => {
     await expect(
       page.getByText(translations.learn.exam['finish-header'])
     ).toBeVisible();
@@ -38,39 +65,33 @@ test.describe('Finish Exit exam Modal E2E Test Suite', () => {
     ).toBeVisible();
   });
 
-  test('Closes the Finish Exit exam Modal When the User clicks on finish-no button', async ({
-    page
-  }) => {
+  test('Closes the Finish Exit exam Modal When the User clicks on finish-no button', async () => {
     await page
       .getByRole('button', { name: translations.learn.exam['exit-no'] })
       .click();
     await expect(
       page.getByText(translations.learn.exam['exit-header'])
     ).not.toBeVisible();
-    await expect(page).toHaveURL(currentUrlPath);
+    await expect(page).toHaveURL(examUrl);
   });
 
-  test('Closes the Modal when the User clicks on finish-yes button', async ({
-    page
-  }) => {
+  test('Closes the Modal when the User clicks on finish-yes button', async () => {
     await page
       .getByRole('button', { name: translations.learn.exam['finish-yes'] })
       .click();
     await expect(
       page.getByText(translations.learn.exam['finish-header'])
     ).not.toBeVisible();
-    await expect(page).toHaveURL(currentUrlPath);
+    await expect(page).toHaveURL(examUrl);
   });
 
-  test('Closes the Modal when the User clicks on X button', async ({
-    page
-  }) => {
+  test('Closes the Modal when the User clicks on X button', async () => {
     await page
       .getByRole('button', { name: translations.buttons.close })
       .click();
     await expect(
       page.getByText(translations.learn.exam['finish-header'])
     ).not.toBeVisible();
-    await expect(page).toHaveURL(currentUrlPath);
+    await expect(page).toHaveURL(examUrl);
   });
 });
