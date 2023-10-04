347d5b2138e2b781b150ffa0b35e79498575333e
Tauseef Hilal Tantary <tantary.tauseef.atwork@gmail.com>
2023-10-04 15:18:33 +0530

    test(e2e,playwright) - more robust testing in donation page (#51783)

--- e2e/donate-page-default.spec.ts
+++ e2e/donate-page-default.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect, type Page } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
+const pageElements = {
+  mainHeading: 'main-head',
+  donateText1: 'donate-text-1',
+  donateText2: 'donate-text-2',
+  donateText3: 'donate-text-3',
+  faqHeading: 'faq-head',
+  campersImage: 'landing-page-figure'
+};
+
 const frequentlyAskedQuestions = [
   translations.donate['get-help'],
   translations.donate['how-transparent'],
@@ -28,45 +37,52 @@ test.afterAll(async () => {
 
 test.describe('Donate Page', () => {
   test('should display the correct title', async () => {
-    await expect(page).toHaveTitle('Support our charity | freeCodeCamp.org');
+    await expect(page).toHaveTitle(
+      `${translations.donate.title} | freeCodeCamp.org`
+    );
   });
 
   test('should display the main heading', async () => {
-    const mainHeading = page.getByTestId('main-head');
-    expect(mainHeading).not.toBeNull();
-    expect(await mainHeading.textContent()).toBe('Help us do more');
+    const mainHeading = page.getByTestId(pageElements.mainHeading);
+    await expect(mainHeading).toHaveText(translations.donate['help-more']);
   });
 
   test('should display donation page upper text 1', async () => {
-    const donateText1 = page.getByTestId('donate-text-1');
-    await expect(donateText1).toHaveText(
-      'freeCodeCamp is a highly efficient education charity.'
-    );
+    const donateText1 = page.getByTestId(pageElements.donateText1);
+    await expect(donateText1).toHaveText(translations.donate.efficiency);
   });
 
   test('should display donation page benefit 2', async () => {
-    const donateText2 = page.getByTestId('donate-text-2');
-    await expect(donateText2).toHaveText(
-      'When you donate to freeCodeCamp, you help people learn new skills and provide for their families.'
-    );
+    const donateText2 = page.getByTestId(pageElements.donateText2);
+    await expect(donateText2).toHaveText(translations.donate['why-donate-1']);
   });
 
   test('should display donation page benefit 3', async () => {
-    const donateText3 = page.getByTestId('donate-text-3');
-    await expect(donateText3).toHaveText(
-      'You also help us create new resources for you to use to expand your own technology skills.'
-    );
+    const donateText3 = page.getByTestId(pageElements.donateText3);
+    await expect(donateText3).toHaveText(translations.donate['why-donate-2']);
+  });
+
+  test('should render the campers image in desktop browsers', async ({
+    isMobile
+  }) => {
+    const figure = page.getByTestId(pageElements.campersImage);
+
+    if (isMobile) {
+      await expect(figure).not.toBeVisible();
+      return;
+    }
+
+    await expect(figure).toBeVisible();
   });
 
   test('should display the faq heading', async () => {
-    const faqHead = page.getByTestId('faq-head');
-    expect(faqHead).not.toBeNull();
-    expect(await faqHead.textContent()).toBe('Frequently asked questions');
+    const faqHead = page.getByTestId(pageElements.faqHeading);
+    await expect(faqHead).toHaveText(translations.donate.faq);
   });
 
-  test('Frequently asked question list buttons', async () => {
-    for (const i of frequentlyAskedQuestions) {
-      await page.getByRole('button', { name: `${i}` }).isVisible();
+  test('should display the faq list with buttons', async () => {
+    for (const question of frequentlyAskedQuestions) {
+      await page.getByRole('button', { name: question }).isVisible();
     }
   });
 });
