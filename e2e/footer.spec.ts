3f38693d53e92ecabdfb5325e8b7cae0db7b6a86
Umang Patel <umang.patel@lastyard.com>
2023-10-05 14:03:05 +0800

    added playwright tests for footer component

--- e2e/footer.spec.ts
+++ e2e/footer.spec.ts
@@ -1,4 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
+import links from '../client/i18n/locales/english/links.json';
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto('/');
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
 
 const footerComponentElements = {
   descriptions: 'footer-desc-col',
@@ -58,4 +71,133 @@ test('Has 11 nonprofits', async ({ page }) => {
   for (const footerBottomLink of await footerBottomLinks.all()) {
     await expect(footerBottomLink).toBeVisible();
   }
+});
+
+test.describe('Footer component left section', () => {
+  test('should render footer tax exempt statement text', async () => {
+    const footerTaxExemptText = page.getByTestId('footer-tax-exempt-status');
+    await expect(footerTaxExemptText).toBeVisible();
+    await expect(footerTaxExemptText).toContainText(
+      translations.footer['tax-exempt-status']
+    );
+  });
+
+  test('should render footer mission statement text', async () => {
+    const footerMissionStatement = page.getByTestId('footer-mission-statement');
+    await expect(footerMissionStatement).toBeVisible();
+    await expect(footerMissionStatement).toContainText(
+      translations.footer['mission-statement']
+    );
+  });
+
+  test('should render footer donation initiative text', async () => {
+    const footerDonationInitiative = page.getByTestId(
+      'footer-donation-initiatives-text'
+    );
+    await expect(footerDonationInitiative).toBeVisible();
+    await expect(footerDonationInitiative).toContainText(
+      translations.footer['donation-initiatives']
+    );
+  });
+
+  test('should correctly link to donations page', async () => {
+    const donationPageLink = page.getByTestId('footer-donation-page-link');
+    await expect(donationPageLink).toHaveAttribute('href', '/donate');
+  });
+});
+
+test.describe('Footer bottom links', () => {
+  test('should display correct link to about us page', async () => {
+    const link = page.getByTestId('footer-about-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['about-url']);
+    await expect(link).toHaveText(translations.footer.links.about);
+  });
+
+  test('should display correct link to alumni page', async () => {
+    const link = page.getByTestId('footer-alumni-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute(
+      'href',
+      'https://www.linkedin.com/school/free-code-camp/people/'
+    );
+    await expect(link).toHaveText(translations.footer.links.alumni);
+  });
+
+  test('should display correct link to open-source page', async () => {
+    const link = page.getByTestId('footer-open-source-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute(
+      'href',
+      'https://github.com/freeCodeCamp/'
+    );
+    await expect(link).toHaveText(translations.footer.links['open-source']);
+  });
+
+  test('should display correct link to shop', async () => {
+    const link = page.getByTestId('footer-shop-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['shop-url']);
+    await expect(link).toHaveText(translations.footer.links.shop);
+  });
+
+  test('should display correct link to support page', async () => {
+    const link = page.getByTestId('footer-support-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['support-url']);
+    await expect(link).toHaveText(translations.footer.links.support);
+  });
+
+  test('should display correct link to sponsors page', async () => {
+    const link = page.getByTestId('footer-sponsors-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['sponsors-url']);
+    await expect(link).toHaveText(translations.footer.links.sponsors);
+  });
+
+  test('should display correct link to honesty page', async () => {
+    const link = page.getByTestId('footer-honesty-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['honesty-url']);
+    await expect(link).toHaveText(translations.footer.links.honesty);
+  });
+
+  test('should display correct link to coc page', async () => {
+    const link = page.getByTestId('footer-coc-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['coc-url']);
+    await expect(link).toHaveText(translations.footer.links.coc);
+  });
+
+  test('should display correct link to privacy page', async () => {
+    const link = page.getByTestId('footer-privacy-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['privacy-url']);
+    await expect(link).toHaveText(translations.footer.links.privacy);
+  });
+
+  test('should display correct link to tos page', async () => {
+    const link = page.getByTestId('footer-tos-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['tos-url']);
+    await expect(link).toHaveText(translations.footer.links.tos);
+  });
+
+  test('should display correct link to copyright page', async () => {
+    const link = page.getByTestId('footer-copyright-url');
+
+    expect(await link.isVisible()).toBe(true);
+    await expect(link).toHaveAttribute('href', links.footer['copyright-url']);
+    await expect(link).toHaveText(translations.footer.links.copyright);
+  });
 });
