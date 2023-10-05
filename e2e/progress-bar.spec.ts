00c803aa098efa2e439bd65aa08f12b31397fb66
AMAN SINGH <amansingh33849@gmail.com>
2023-10-05 07:35:07 +0000

    progress bar test changes

--- e2e/progress-bar.spec.ts
+++ e2e/progress-bar.spec.ts
@@ -1,27 +1,35 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto(
+    '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
+  );
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
 
 test.describe('Should show the progress bar showing the completed percent on  challenges', () => {
-  test('Should enter code in the editor and show the progress bar', async ({
-    page
-  }) => {
-
-    await page.goto(
-      '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
-    );
-    const lowerCheckButton = page.getByText('Check Your Code (Ctrl + Enter)');
-
+  test('Should enter code in the editor and show the progress bar', async () => {
     const monacoEditor = page.locator(
       '.react-monaco-editor-container textarea'
     );
     await monacoEditor.click();
-    await monacoEditor.focus();
-    await page.keyboard.press('Control+KeyA');
-    await monacoEditor.clear();
-    await page.keyboard.type(
+
+    await page.keyboard.press('Control+A');
+    await page.keyboard.press('Meta+A');
+    await page.keyboard.press('Backspace');
+
+    await page.keyboard.insertText(
       '<html><body><h1>CatPhotoApp</h1><h2>Cat Photos</h2></body></html>'
     );
-    await lowerCheckButton.click();
+    const lowerCheckButton = page.getByText('Check Your Code (Ctrl + Enter)');
 
+    await lowerCheckButton.click({ force: true });
     const progressBarContanier = page.getByTestId('progress-bar-contanier');
     await expect(progressBarContanier).toContainText(
       'Learn HTML by Building a Cat Photo App'
