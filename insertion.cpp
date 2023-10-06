f539d403fe2fb27453487b718da408388d3c3b64
Abhishek Kumar <101486437+abhi33781@users.noreply.github.com>
2023-10-06 23:39:52 +0530

    Create insertion.cpp

---
+++ insertion.cpp
@@ -0,0 +1,13 @@
+
+void iSort(int arr[],int n){
+    
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
