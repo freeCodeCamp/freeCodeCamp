42c009175d58d895602308cbbb683fea8d4918f3
Dmitry Drepin <lixefer@gmail.com>
2023-10-09 12:34:23 +0200

    exam results share page

--- e2e/exam-results.spec.ts
+++ e2e/exam-results.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
 test.use({ storageState: 'playwright/.auth/certified-user.json' });
@@ -7,8 +7,9 @@ const examUrl =
   '/learn/foundational-c-sharp-with-microsoft/foundational-c-sharp-with-microsoft-certification-exam/foundational-c-sharp-with-microsoft-certification-exam';
 const exitUrl =
   '/learn/foundational-c-sharp-with-microsoft/#foundational-c-sharp-with-microsoft-certification-exam';
+let page: Page;
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async () => {
   await page.goto(examUrl);
   await page
     .getByRole('button', {
@@ -36,10 +37,16 @@ test.beforeEach(async ({ page }) => {
     .click();
 });
 
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
 test.describe('Exam Results E2E Test Suite', () => {
-  test('Verifies the Correct Rendering of the Exam results', async ({
-    page
-  }) => {
+  test('Verifies the Correct Rendering of the Exam results', async () => {
     await expect(page.getByTestId('exam-results-header')).toBeVisible();
     await expect(page.getByTestId('exam-results-message')).toBeVisible();
     await expect(
@@ -53,9 +60,7 @@ test.describe('Exam Results E2E Test Suite', () => {
     await expect(page.getByTestId('exit-exam')).toBeVisible();
   });
 
-  test('Exam Results When the User clicks on Download button', async ({
-    page
-  }) => {
+  test('Exam Results When the User clicks on Download button', async () => {
     const [downloadProm] = await Promise.all([
       page.waitForEvent('download'), // wait for download to start
       page.getByTestId('download-exam-results').click()
@@ -66,7 +71,7 @@ test.describe('Exam Results E2E Test Suite', () => {
     await expect(page).toHaveURL(examUrl);
   });
 
-  test('Exam Results when the User clicks on Exit button', async ({ page }) => {
+  test('Exam Results when the User clicks on Exit button', async () => {
     await page.getByTestId('exit-exam').click();
     await expect(page.getByTestId('exam-results-header')).not.toBeVisible();
     await expect(page).toHaveURL(exitUrl);
