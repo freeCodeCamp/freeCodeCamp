90f9f8a43ca134a889134476023d6f0c0d0659bf
Neetigya Poddar <neetigyapoddar1@gmail.com>
2023-10-09 22:17:23 +0530

    test(e2e): Added E2E tests for Header component

--- e2e/header.spec.ts
+++ e2e/header.spec.ts
@@ -1,14 +1,21 @@
 import { test, expect } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
 
 const headerComponentElements = {
   skipContent: 'header-skip-content',
   examNav: 'header-exam-nav',
+  examNavLogo: 'header-exam-nav-microsoft-logo',
   universalNav: 'header-universal-nav',
   universalNavLogo: 'header-universal-nav-logo'
 } as const;
 
+const examUrl =
+  '/learn/foundational-c-sharp-with-microsoft/foundational-c-sharp-with-microsoft-certification-exam/foundational-c-sharp-with-microsoft-certification-exam';
+
+test.use({ storageState: 'playwright/.auth/certified-user.json' });
+
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto(examUrl);
 });
 
 test.afterEach(async ({ page }) => {
@@ -18,6 +25,7 @@ test.afterEach(async ({ page }) => {
 test('Has link for skip content', async ({ page }) => {
   const skipContent = page.getByTestId(headerComponentElements.skipContent);
   await expect(skipContent).toBeVisible();
+  await expect(skipContent).toHaveAttribute('href', '#content-start');
 });
 
 test('Has nav for universal', async ({ page }) => {
@@ -29,4 +37,18 @@ test('Has nav for universal', async ({ page }) => {
   );
   await expect(universalNavigation).toBeVisible();
   await expect(universalNavigationLogo).toBeVisible();
+  await expect(universalNavigationLogo).toHaveAttribute('href', '/learn');
+});
+
+test('Has nav for exams', async ({ page }) => {
+  await page
+    .getByRole('button', {
+      name: translations.buttons['click-start-exam']
+    })
+    .click();
+  await expect(page).toHaveURL(examUrl);
+  await expect(page.getByTestId(headerComponentElements.examNav)).toBeVisible();
+  await expect(
+    page.getByTestId(headerComponentElements.examNavLogo)
+  ).toBeVisible();
 });
