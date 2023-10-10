690d6129a0253b33423710d647eb13c32881d854
AadhavanCnp <aadhavan313@gmail.com>
2023-10-10 19:47:41 +0530

    test(e2e,playwright): Header

--- e2e/header.spec.ts
+++ e2e/header.spec.ts
@@ -1,34 +1,34 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
 const headerComponentElements = {
-  headerLogo: "universal-nav-logo",
-  aisSearchBoxInput: "ais-SearchBox-input",
-  toggleLangButton: "toggle-lang-button",
-  toggleButtonNav: "toggle-button-nav",
-  navLangList: "nav-lang-list",
-  signupBtn: "signup-btn",
+  headerLogo: 'universal-nav-logo',
+  aisSearchBoxInput: 'ais-SearchBox-input',
+  toggleLangButton: 'toggle-lang-button',
+  toggleButtonNav: 'toggle-button-nav',
+  navLangList: 'nav-lang-list',
+  signupBtn: 'signup-btn'
 } as const;
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("/");
+  await page.goto('/');
 });
 
-test("should have a skip to content link", async ({ page }) => {
-  const skipToContentLink = await page.locator(".skip-to-content-button");
+test('should have a skip to content link', async ({ page }) => {
+  const skipToContentLink = page.locator('.skip-to-content-button');
   await expect(skipToContentLink).toBeVisible();
 });
 
-test("Has header logo", async ({ page }) => {
+test('Has header logo', async ({ page }) => {
   const headerLogo = page.getByTestId(headerComponentElements.headerLogo);
   await expect(headerLogo).toBeVisible();
 });
 
-test("should hide search box input on mobile (unless menu button is clicked) and show on desktop on navbar", async ({
+test('should hide search box input on mobile and show on desktop on navbar', async ({
   page,
-  isMobile,
+  isMobile
 }) => {
   const aisSearchBoxInput = page.getByTestId(
-    headerComponentElements.aisSearchBoxInput,
+    headerComponentElements.aisSearchBoxInput
   );
   const menuButton = page.getByTestId(headerComponentElements.toggleButtonNav);
   if (isMobile) {
@@ -40,51 +40,51 @@ test("should hide search box input on mobile (unless menu button is clicked) and
   }
 });
 
-test("Has toggle lang button", async ({ page }) => {
+test('Has toggle lang button', async ({ page }) => {
   const toggleLangButton = page.getByTestId(
-    headerComponentElements.toggleLangButton,
+    headerComponentElements.toggleLangButton
   );
   await expect(toggleLangButton).toBeVisible();
 });
 
-test("should display language list on lang button is clicked", async ({
-  page,
+test('should display language list on lang button is clicked', async ({
+  page
 }) => {
   const toggleLangButton = page.getByTestId(
-    headerComponentElements.toggleLangButton,
+    headerComponentElements.toggleLangButton
   );
   await toggleLangButton.click();
   const navLangList = page.getByTestId(headerComponentElements.navLangList);
   await expect(navLangList).toBeVisible();
-  const navLangListOptions = navLangList.locator(".nav-lang-list-option");
+  const navLangListOptions = navLangList.locator('.nav-lang-list-option');
   await expect(navLangListOptions).toHaveCount(8);
   for (const option of await navLangListOptions.all()) {
     await expect(option).toBeVisible();
   }
 });
 
-test("Has toggle button nav", async ({ page }) => {
+test('Has toggle button nav', async ({ page }) => {
   const toggleButtonNav = page.getByTestId(
-    headerComponentElements.toggleButtonNav,
+    headerComponentElements.toggleButtonNav
   );
   await expect(toggleButtonNav).toBeVisible();
 });
 
-test("should display menu list on menu button is clicked", async ({ page }) => {
+test('should display menu list on menu button is clicked', async ({ page }) => {
   const toggleButtonNav = page.getByTestId(
-    headerComponentElements.toggleButtonNav,
+    headerComponentElements.toggleButtonNav
   );
   await toggleButtonNav.click();
-  const displayMenu = page.locator(".display-menu");
+  const displayMenu = page.locator('.display-menu');
   await expect(displayMenu).toBeVisible();
-  const navLinks = displayMenu.locator(".nav-link");
+  const navLinks = displayMenu.locator('.nav-link');
   await expect(navLinks).toHaveCount(7);
   for (const link of await navLinks.all()) {
     await expect(link).toBeVisible();
   }
 });
 
-test("Has sign up button", async ({ page }) => {
+test('Has sign up button', async ({ page }) => {
   const signupBtn = page.getByTestId(headerComponentElements.signupBtn);
   await expect(signupBtn).toBeVisible();
 });
