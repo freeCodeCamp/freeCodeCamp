7ef4d850b2890c267017ffe587a2d4c473cd4946
Ann B Cherian <annbyjus@gmail.com>
2023-10-12 00:08:48 +0530

    fix: switch to beforeEach

--- e2e/challenge-description.spec.ts
+++ e2e/challenge-description.spec.ts
@@ -1,25 +1,22 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-let page: Page;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
   await page.goto(
     '/learn/foundational-c-sharp-with-microsoft/write-your-first-code-using-c-sharp/trophy-write-your-first-code-using-c-sharp'
   );
 });
 
-test.afterAll(async () => {
+test.afterEach(async ({ page }) => {
   await page.close();
 });
 
 test.describe('Challenge Description Component Tests', () => {
-  test('should be visible', async () => {
+  test('should be visible', async ({ page }) => {
     const challengeDescription = page.getByTestId('challenge-description');
     await expect(challengeDescription).toBeVisible();
   });
 
-  test('should contain text', async () => {
+  test('should contain text', async ({ page }) => {
     const challengeDescription = page.getByTestId('challenge-description');
     await expect(challengeDescription).toHaveText(/ */);
   });
