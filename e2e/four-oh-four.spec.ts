705cb836be391dc068df01f3abb32ca8c54d37bc
ArjunSingh-Rawat <arjunsinghrawat102@gmail.com>
2023-10-12 00:51:24 +0530

    chore: resolved changes requested

--- e2e/four-oh-four.spec.ts
+++ e2e/four-oh-four.spec.ts
@@ -1,19 +1,12 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
-let page: Page;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
   await page.goto('/404');
 });
 
-test.afterAll(async () => {
-  await page.close();
-});
-
 test.describe('test for FourOhFour component', () => {
-  test('should display 404 image', async () => {
+  test('should display 404 image', async ({ page }) => {
     const image = page.getByTestId('404-img');
     await expect(image).toBeVisible();
     await expect(image).toHaveAttribute(
@@ -22,13 +15,13 @@ test.describe('test for FourOhFour component', () => {
     );
   });
 
-  test('should display 404 heading', async () => {
+  test('should display 404 heading', async ({ page }) => {
     const heading = page.getByTestId('404-heading');
     await expect(heading).toBeVisible();
     await expect(heading).toContainText(translations['404']['page-not-found']);
   });
 
-  test('should display 404 heres-a-quote sentence', async () => {
+  test('should display 404 heres-a-quote sentence', async ({ page }) => {
     const heresAquote = page.getByTestId('404-heres-a-quote');
     await expect(heresAquote).toBeVisible();
     await expect(heresAquote).toContainText(
@@ -36,11 +29,11 @@ test.describe('test for FourOhFour component', () => {
     );
   });
 
-  test('should display 404 quote', async () => {
+  test('should display 404 quote', async ({ page }) => {
     await expect(page.getByTestId('404-quote')).toBeVisible();
   });
 
-  test('should display curriculum link', async () => {
+  test('should display curriculum link', async ({ page }) => {
     const curriculumLink = page.getByTestId('404-view-curriculum-link');
     await expect(curriculumLink).toBeVisible();
     await expect(curriculumLink).toHaveAttribute('href', '/learn');
