6f6b459b2f0dcac15520b5b9a9c269e54acec0b9
Sboonny <muhammedelruby@gmail.com>
2023-10-12 12:42:20 +0300

    add animation

--- tools/ui-components/tailwind.config.js
+++ tools/ui-components/tailwind.config.js
@@ -99,6 +99,20 @@ module.exports = {
       },
       zIndex: {
         2: '2'
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' }
+        }
+      },
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)'
       }
     }
   },
