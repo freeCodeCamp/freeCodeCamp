e27e484d6679b643f586b7a97ef71c2e7c5890e7
Rohit Rai <rohitraijob@gmail.com>
2023-10-13 00:56:53 +0530

    test(e2e): add E2E tests for video-player

---
+++ e2e/video-player.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(
+    '/learn/scientific-computing-with-python/python-for-everybody/introduction-why-program'
+  );
+});
+
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
+
+test.describe('Challenge Video Player Component Tests', () => {
+  test('should render video player and play button', async ({ page }) => {
+    await expect(
+      page.locator('iframe[title="YouTube video player"]')
+    ).toBeVisible();
+    await expect(
+      page
+        .frameLocator('.display-youtube-video')
+        .getByRole('button', { name: 'Play' })
+    ).toBeVisible();
+  });
+});
