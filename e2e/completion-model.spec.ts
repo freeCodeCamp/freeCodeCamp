2e721214fe8e8130cb3826da08e9b7b8f2ee8a68
Ann B Cherian <annbyjus@gmail.com>
2023-10-12 23:33:58 +0530

    fix: combine tests in one spec file

--- e2e/completion-model.spec.ts
+++ e2e/completion-model.spec.ts
@@ -15,7 +15,7 @@ test.afterEach(async ({ page }) => {
   await page.close();
 });
 
-test.describe('Challenge Completion Modal Tests', () => {
+test.describe('Challenge Completion Modal Tests (Signed Out)', () => {
   test('should render the modal correctly', async ({ page }) => {
     await expect(page.getByRole('heading')).toBeVisible();
     await expect(page.getByRole('button', { name: 'close' })).toBeVisible();
@@ -44,6 +44,37 @@ test.describe('Challenge Completion Modal Tests', () => {
   test('should redirect to next challenge', async ({ page }) => {
     await page
       .getByRole('button', { name: translations.buttons['go-to-next'] })
+      .click();
+    await expect(page).toHaveURL(
+      'http://localhost:8000/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator'
+    );
+  });
+});
+
+test.describe('Challenge Completion Modal Tests (Signed In)', () => {
+  test.use({ storageState: 'playwright/.auth/certified-user.json' });
+
+  test('should render the modal correctly', async ({ page }) => {
+    await expect(page.getByRole('heading')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'close' })).toBeVisible();
+    await expect(page.getByTestId('completion-success-icon')).toBeVisible();
+    await expect(page.getByTestId('progress-bar-container')).toBeVisible();
+    await expect(
+      page.getByRole('link', { name: translations.learn['sign-in-save'] })
+    ).not.toBeVisible();
+    await expect(
+      page.getByRole('button', { name: translations.buttons['submit-and-go'] })
+    ).toBeVisible();
+  });
+
+  test('should close the modal after user click on close', async ({ page }) => {
+    await page.getByRole('button', { name: 'close' }).click();
+    await expect(page.getByTestId('completion-success-icon')).not.toBeVisible();
+  });
+
+  test('should redirect to next challenge', async ({ page }) => {
+    await page
+      .getByRole('button', { name: translations.buttons['submit-and-go'] })
       .click();
     await expect(page).toHaveURL(
       'http://localhost:8000/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator'
