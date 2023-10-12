fd5b9628f4e7c3adc7d832fca8cffe83f3f1bde0
Igor Shcherba <igor.shcherba@callstack.com>
2023-10-12 17:18:00 +0200

    tests(e2e): do not declare global Page variable in the 404 page tests

--- e2e/404.spec.ts
+++ e2e/404.spec.ts
@@ -1,29 +1,26 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
-let page: Page;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
   await page.goto('/nonexistent-url');
 });
 
-test.afterAll(async () => {
+test.afterEach(async ({ page }) => {
   await page.close();
 });
 
 test.describe('404 Page Tests', () => {
-  test('The page should have correct heading', async () => {
+  test('The page should have correct heading', async ({ page }) => {
     const heading = page.getByTestId('404-heading');
     await expect(heading).toBeVisible();
     await expect(heading).toContainText(translations[404]['page-not-found']);
   });
 
-  test('The page should display the quote', async () => {
+  test('The page should display the quote', async ({ page }) => {
     await expect(page.getByTestId('404-quote')).toBeVisible();
   });
 
-  test('The page should have CTA link', async () => {
+  test('The page should have CTA link', async ({ page }) => {
     const cta = page.getByTestId('404-cta');
     await expect(cta).toBeVisible();
     await expect(cta).toContainText(translations.buttons['view-curriculum']);
