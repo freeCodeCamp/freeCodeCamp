a85ffc867708cae3a914fe6da65ec4185efce5a3
AMAN SINGH <amansingh33849@gmail.com>
2023-10-12 09:52:04 +0000

    changes

--- e2e/editor.spec.ts
+++ e2e/editor.spec.ts
@@ -1,20 +1,16 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-let page: Page;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
   await page.goto(
     '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
   );
 });
-
-test.afterAll(async () => {
+test.afterEach(async ({ page }) => {
   await page.close();
 });
 
 test.describe('Editor Component', () => {
-  test('Should be clicked and able to insert text', async () => {
+  test('Should be clicked and able to insert text', async ({ page }) => {
     const monacoEditor = page.getByLabel('Editor content');
     await monacoEditor.click();
     await page.keyboard.insertText('<h2>FreeCodeCamp</h2>');
