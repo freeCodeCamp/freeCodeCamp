e824a93fb52b1a0cedcd648f86aa2cf8706299f0
Mackensie Jack <kensiejack1@gmail.com>
2023-10-13 10:32:47 -0400

    test(e2e, playwright): email-sign-up

--- e2e/email-sign-up.spec.ts
+++ e2e/email-sign-up.spec.ts
@@ -5,30 +5,25 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Email sign-up page', () => {
-
   test('The page renders with correct title', async ({ page }) => {
-    await expect(page).toHaveTitle(
-        `Email Sign Up | freeCodeCamp.org`
-      );
+    await expect(page).toHaveTitle(`Email Sign Up | freeCodeCamp.org`);
   });
 
   test('The page renders the correct content', async ({ page }) => {
-      await expect(
-        page.getByText(
-          'freeCodeCamp is a proven path to your first software developer job.
-        )
-      ).toBeVisible();
-});
+    await expect(
+      page.getByText(
+        'freeCodeCamp is a proven path to your first software developer job.'
+      )
+    ).toBeVisible();
+  });
 
-  test('The sign up button renders and functions', async({ page }) => {
-    const button = page.getByTestId('email-sign-up-button')
+  test('The sign up button renders and functions', async ({ page }) => {
+    const button = page.getByTestId('email-sign-up-button');
 
-    await expect(button).toBeVisible()
+    await expect(button).toBeVisible();
 
-    await button.click()
+    await button.click();
 
     await expect(page).toHaveURL(/.*\/learn\/?$/);
-  })
-
-
+  });
 });
