7df7855b957db74a058e50eeff2409f5229992e2
airwaks <114548361+airwakz@users.noreply.github.com>
2023-10-13 21:18:13 +0530

    Update root-saga.js

--- client/src/redux/root-saga.js
+++ client/src/redux/root-saga.js
@@ -4,7 +4,9 @@ import { sagas as challengeSagas } from '../templates/Challenges/redux';
 import errorSagas from './error-saga';
 import { sagas as settingsSagas } from './settings';
 import { sagas as appSagas } from '.';
+import confettiSaga from './confetti-saga'; // Import the confettiSaga
 
 export default function* rootSaga() {
-  yield all([...errorSagas, ...appSagas, ...challengeSagas, ...settingsSagas,...confettiSaga]);
+  yield all([...errorSagas, ...appSagas, ...challengeSagas, ...settingsSagas, confettiSaga]); // Include the confettiSaga
 }
+
