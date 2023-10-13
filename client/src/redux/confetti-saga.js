68fa1becc6ef4ae288e5d50d9a2ab65e7d928c07
airwaks <114548361+airwakz@users.noreply.github.com>
2023-10-13 21:15:46 +0530

    Update confetti-saga.js

--- client/src/redux/confetti-saga.js
+++ client/src/redux/confetti-saga.js
@@ -1,2 +1,21 @@
-export const CONFETTI_START = 'CONFETTI_START';
-export const CONFETTI_STOP = 'CONFETTI_STOP';
+import { takeEvery, put } from 'redux-saga/effects';
+import { CONFETTI_ACTION, showConfetti, hideConfetti } from './actions'; // Import relevant actions
+
+function* confettiLogic() {
+  // Add the logic to show the confetti here, for example:
+  yield put(showConfetti());
+
+  // Simulate confetti for a few seconds
+  yield delay(5000); // You might need to import 'delay' from 'redux-saga/effects' or implement it differently
+
+  // Hide the confetti after a certain duration
+  yield put(hideConfetti());
+}
+
+function* watchConfettiAction() {
+  yield takeEvery(CONFETTI_ACTION, confettiLogic);
+}
+
+export default function* confettiSaga() {
+  yield all([watchConfettiAction()]);
+}
