1cb9d772d2f34ade4720c7ae8ceef9d4b0870bdf
EggSaled <adrian.escobar.104183@gmail.com>
2023-10-12 22:01:32 -0700

    feat: added GreenPass test and simple translation link test

--- e2e/challenge-title.spec.ts
+++ e2e/challenge-title.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(
@@ -15,5 +16,51 @@ test.describe('Challenge Title Component', () => {
     await expect(page.getByText('Developing a Port Scanner')).toBeVisible();
 
     await expect(page.getByLabel('Passed')).not.toBeVisible();
+  });
+
+  test("should not render the 'Please Help Us Translate' link", async ({
+    page
+  }) => {
+    // Placeholder code below
+    test.skip(
+      process.env.CLIENT_LOCALE != 'english',
+      'Current Locale not supported for this test.'
+    );
+
+    await expect(
+      page.getByText(translations['learn']['help-translate-link'])
+    ).not.toBeVisible();
+  });
+});
+
+test.describe('Challenge Title Component (signed in)', () => {
+  test.use({ storageState: 'playwright/.auth/certified-user.json' });
+
+  test('should display GreenPass after challenge completion', async ({
+    page
+  }) => {
+    await page
+      .getByText(
+        'If there is an error or if no host is found, .connect() raises an exception while .connect_ex() returns an error code.'
+      )
+      .click({ force: true });
+
+    await page
+      .getByText(translations['buttons']['check-answer'], { exact: true })
+      .click({ force: true });
+
+    await page
+      .getByText(translations['buttons']['go-to-next'])
+      .click({ force: true });
+
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.goto(
+      '/learn/information-security/python-for-penetration-testing/developing-a-port-scanner'
+    );
+
+    await page.waitForLoadState();
+
+    await expect(page.getByLabel('Passed')).toBeVisible();
   });
 });
