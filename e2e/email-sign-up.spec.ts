9391bb3df9e3618c422f3e7b6f3b40e33ad6a253
Kensie <94876562+kensiecodes@users.noreply.github.com>
2023-10-13 13:26:34 -0400

    Update e2e/email-sign-up.spec.ts
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>

--- e2e/email-sign-up.spec.ts
+++ e2e/email-sign-up.spec.ts
@@ -5,25 +5,16 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Email sign-up page', () => {
-  test('The page renders with correct title', async ({ page }) => {
+  test('should render correctly', async ({ page }) => {
     await expect(page).toHaveTitle(`Email Sign Up | freeCodeCamp.org`);
-  });
-
-  test('The page renders the correct content', async ({ page }) => {
     await expect(
       page.getByText(
         'freeCodeCamp is a proven path to your first software developer job.'
       )
     ).toBeVisible();
-  });
-
-  test('The sign up button renders and functions', async ({ page }) => {
     const button = page.getByTestId('email-sign-up-button');
-
     await expect(button).toBeVisible();
-
     await button.click();
-
     await expect(page).toHaveURL(/.*\/learn\/?$/);
   });
 });
