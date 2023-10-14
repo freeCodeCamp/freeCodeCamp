cd41e887b6f03ad01023172921f742d7a3e40f9b
khalid Tuhin <76039658+khalid-Ibne-Hussain@users.noreply.github.com>
2023-10-15 00:39:08 +0600

    Create twosum.js

---
+++ twosum.js
@@ -0,0 +1,16 @@
+var twoSum = function(nums, target) {
+        const indices = {};
+
+    nums.map((item, index) => {
+        indices[item] = index
+    });
+
+    for (let index = 0; index < nums.length; index++) {
+        const complement = target - nums[index];
+
+        if (indices[complement] !== undefined && indices[complement] !== index) {
+            return [index, indices[complement]]
+        }
+    }
+
+};
