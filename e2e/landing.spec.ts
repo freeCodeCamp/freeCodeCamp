be3b8a49f834d3d66cb5a0bf31e0fd0f42edd989
Ubuntu <ubuntu@ip-172-31-22-225.us-east-2.compute.internal>
2023-10-15 04:03:51 +0000

    refactor/simplify playwright test: landing.spec.ts

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, type Page } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
 
 const landingPageElements = {
   heading: 'landing-header',
@@ -30,6 +31,13 @@ const superBlocks = [
   'Project Euler'
 ];
 
+const testimonialEndorser = [
+  'testimonials-endorser-image-container',
+  'testimonials-endorser-location',
+  'testimonials-endorser-occupation',
+  'testimonials-endorser-testimony'
+];
+
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
   await page.goto('/');
@@ -37,18 +45,22 @@ test.beforeAll(async ({ browser }) => {
 
 test('The component Landing-top renders correctly', async () => {
   const landingHeading1 = page.getByTestId('landing-big-heading-1');
-  await expect(landingHeading1).toHaveText('Learn to code — for free.');
+  await expect(landingHeading1).toHaveText(
+    translations.landing['big-heading-1']
+  );
 
   const landingHeading2 = page.getByTestId('landing-big-heading-2');
-  await expect(landingHeading2).toHaveText('Build projects.');
+  await expect(landingHeading2).toHaveText(
+    translations.landing['big-heading-2']
+  );
 
   const landingHeading3 = page.getByTestId('landing-big-heading-3');
-  await expect(landingHeading3).toHaveText('Earn certifications.');
+  await expect(landingHeading3).toHaveText(
+    translations.landing['big-heading-3']
+  );
 
   const landingH2Heading = page.getByTestId('landing-h2-heading');
-  await expect(landingH2Heading).toHaveText(
-    'Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten jobs at tech companies including:'
-  );
+  await expect(landingH2Heading).toHaveText(translations.landing['h2-heading']);
 });
 
 test('Has 5 brand logos', async () => {
@@ -73,7 +85,9 @@ test('The campers landing page figure is visible on desktop and hidden on mobile
 
 test('The as seen in container is visible with featured logos', async () => {
   const asSeenInContainer = page.getByTestId('landing-as-seen-in-text');
-  await expect(asSeenInContainer).toHaveText('As seen in:');
+  await expect(asSeenInContainer).toHaveText(
+    translations.landing['as-seen-in']
+  );
 
   const featuredLogos = page.getByTestId('landing-as-seen-in-container-logos');
   await expect(featuredLogos).toBeVisible();
@@ -82,7 +96,7 @@ test('The as seen in container is visible with featured logos', async () => {
 test('Testimonial section has a header', async () => {
   const testimonialsHeader = page.getByTestId('testimonials-section-header');
   await expect(testimonialsHeader).toHaveText(
-    'Here is what our alumni say about freeCodeCamp:'
+    translations.landing.testimonials['heading']
   );
 });
 
@@ -93,18 +107,9 @@ test('Testimonial endorser people have images, occupation, location and testimon
 
   for (const card of await cards.all()) {
     await expect(card).toBeVisible();
-    await expect(
-      card.getByTestId('testimonials-endorser-image-container')
-    ).toBeVisible();
-    await expect(
-      card.getByTestId('testimonials-endorser-location')
-    ).toBeVisible();
-    await expect(
-      card.getByTestId('testimonials-endorser-occupation')
-    ).toBeVisible();
-    await expect(
-      card.getByTestId('testimonials-endorser-testimony')
-    ).toBeVisible();
+    for (let i = 0; i < testimonialEndorser.length; i++) {
+      await expect(card.getByTestId(testimonialEndorser[i])).toBeVisible();
+    }
   }
 });
 
