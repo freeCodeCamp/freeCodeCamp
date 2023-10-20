2a5e4517e693bca063e4653038e5c1b1a3f5bc9f
Lalitha Lakshmiraju <letterstocontact@gmail.com>
2023-10-20 17:21:28 +0530

    commit "edited commit"

--- e2e/email-signup.spec.ts
+++ e2e/email-signup.spec.ts
@@ -50,14 +50,7 @@ test('get danger zone link', async ({ page }) => {
 });
 
 test('get emails', async ({ page }) => {
-  /*await expect(page.getByTestId('misc-email-blast')).toHaveText(
-    translations.misc['email-blast']
-  ); */
-<<<<<<< HEAD
-  await expect(page.getByText(translations.misc['email-blast'])).toHaveScreenshot();
-=======
   await expect(page.getByTestId('misc-email-blast')).toHaveScreenshot(
     translations.misc['email-blast']
   );
->>>>>>> f0ebd9873f (commit "modified email-signup.spec.ts")
 });
