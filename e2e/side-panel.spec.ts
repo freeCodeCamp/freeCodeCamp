bad45c842a4af40982875a6616303e75ac1e8eba
Ankit Kumar <ankit07roykroos@gmail.com>
2023-10-22 23:34:00 +0530

    test(e2e,playwright): side-panel.tsx

---
+++ e2e/side-panel.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(
+    '/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-javascript-variables'
+  );
+});
+
+test.describe('Challenge Side Panel Component Tests', () => {
+  test('should render correctly', async ({ page }) => {
+    await expect(page.getByTestId('challenge-title')).toBeVisible();
+    await expect(page.getByTestId('challenge-description')).toBeVisible();
+    await expect(page.getByTestId('get-help-dropdown')).toBeVisible();
+    await expect(page.getByTestId('test-result')).toBeVisible();
+  });
+});
