b5a9b6201ba286e57daf3e5af9d8646f0a7d0520
weilirs <weilirs@umich.edu>
2023-10-25 11:23:38 -0500

    human-readable using locator and remove unnecessary comments

--- e2e/editor-tabs.spec.ts
+++ e2e/editor-tabs.spec.ts
@@ -5,78 +5,54 @@ test.beforeEach(async ({ page }) => {
     '/learn/2022/responsive-web-design/build-a-survey-form-project/build-a-survey-form'
   );
 });
-test.afterEach(async ({ page }) => {
-  await page.close();
-});
 
 test('should toggle editor visibility based on aria-expanded attribute', async ({
   page
 }) => {
-  // Find the first button that is not expanded (aria-expanded="false")
-  const notExpandedButton = await page.$('button[aria-expanded="false"]');
-
-  // If there's a button that's not expanded, click it
-  if (notExpandedButton) {
-    await notExpandedButton.click();
-
-    // After the click, it should now have aria-expanded="true"
-    const isExpandedAfterClick =
-      await notExpandedButton.getAttribute('aria-expanded');
+  const tabLocator = page.locator('div.monaco-editor-tabs button').first();
+  const isExpandedBeforeClick = await tabLocator.getAttribute('aria-expanded');
+  if (isExpandedBeforeClick === 'true') {
+    await tabLocator.click();
+    const isExpandedAfterClick = await tabLocator.getAttribute('aria-expanded');
+    expect(isExpandedAfterClick).toBe('false');
+  } else {
+    await tabLocator.click();
+    const isExpandedAfterClick = await tabLocator.getAttribute('aria-expanded');
     expect(isExpandedAfterClick).toBe('true');
-  }
-
-  // Find the first button that is expanded (aria-expanded="true")
-  const expandedButton = await page.$('button[aria-expanded="true"]');
-
-  // If there's a button that's expanded, click it
-  if (expandedButton) {
-    await expandedButton.click();
-
-    // After the click, it should now have aria-expanded="false"
-    const isNotExpandedAfterClick =
-      await expandedButton.getAttribute('aria-expanded');
-    expect(isNotExpandedAfterClick).toBe('false');
   }
 });
 
 test('toggling the button should toggle the appearance of the corresponding ReflexElement', async ({
   page
 }) => {
-  // Fetch all the editor tab buttons
-  const editorTabs = await page.$$('[data-playwright-test-label="editor-tab"]');
-
-  for (const tab of editorTabs) {
+  const tabsLocator = await page.locator('div.monaco-editor-tabs button').all();
+  for (const tab of tabsLocator) {
     const tabFileKey = await tab.getAttribute('data-cy');
     if (tabFileKey) {
       const fileKey = tabFileKey.replace('editor-tab-', '');
 
-      // Check the initial state of the button and the corresponding ReflexElement
       const ariaExpandedBeforeClick = await tab.getAttribute('aria-expanded');
-      const reflexElementBeforeClick = await page.$(
-        `[data-cy="editor-container-${fileKey}"]`
+      const reflexElementBeforeClick = page.locator(
+        `div[data-cy="editor-container-${fileKey}"]`
       );
 
-      // If ariaExpandedBeforeClick is "true", then reflexElementBeforeClick should be truthy, and vice versa.
       if (ariaExpandedBeforeClick === 'true') {
-        expect(reflexElementBeforeClick).toBeTruthy();
+        await expect(reflexElementBeforeClick).toBeVisible();
       } else {
-        expect(reflexElementBeforeClick).toBeFalsy();
+        await expect(reflexElementBeforeClick).toBeHidden();
       }
 
-      // Click the button to toggle its state
       await tab.click();
 
-      // Check the new state of the button and the corresponding ReflexElement
       const ariaExpandedAfterClick = await tab.getAttribute('aria-expanded');
-      const reflexElementAfterClick = await page.$(
-        `[data-cy="editor-container-${fileKey}"]`
+      const reflexElementAfterClick = page.locator(
+        `div[data-cy="editor-container-${fileKey}"]`
       );
 
-      // If ariaExpandedAfterClick is "true", then reflexElementAfterClick should be truthy, and vice versa.
       if (ariaExpandedAfterClick === 'true') {
-        expect(reflexElementAfterClick).toBeTruthy();
+        await expect(reflexElementAfterClick).toBeVisible();
       } else {
-        expect(reflexElementAfterClick).toBeFalsy();
+        await expect(reflexElementAfterClick).toBeHidden();
       }
     } else {
       throw new Error('Missing data-cy attribute on an editor tab button');
