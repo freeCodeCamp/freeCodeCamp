5f59ca9e3a68e7f33cbb927ea1951a5b389251ca
Ankit Kumar <ankit07roykroos@gmail.com>
2023-10-26 15:54:48 +0530

    fix: test tool panel is hidden on mobile

--- e2e/side-panel.spec.ts
+++ e2e/side-panel.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import translations from '../client/i18n/locales/english/translations.json';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(
@@ -7,10 +8,15 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Challenge Side Panel Component', () => {
-  test('should render correctly', async ({ page }) => {
+  test('should render correctly', async ({ page, isMobile }) => {
+    const toolPanelItem = page.getByText(translations.buttons['get-help']);
+    if (isMobile) {
+      await expect(toolPanelItem).not.toBeVisible();
+    } else {
+      await expect(toolPanelItem).toBeVisible();
+    }
     await expect(page.getByTestId('challenge-title')).toBeVisible();
     await expect(page.getByTestId('challenge-description')).toBeVisible();
-    await expect(page.getByTestId('get-help-dropdown')).toBeVisible();
     await expect(page.getByTestId('test-result')).toBeVisible();
   });
 });
