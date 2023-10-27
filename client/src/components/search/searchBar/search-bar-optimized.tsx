dd0274bad9a5f08c4697777f20bd9191943fad5e
Josh Alling <joshralling@gmail.com>
2023-10-27 01:23:57 -0700

    test(e2e,playwright): Search (#52034)

--- client/src/components/search/searchBar/search-bar-optimized.tsx
+++ client/src/components/search/searchBar/search-bar-optimized.tsx
@@ -54,8 +54,13 @@ const SearchBarOptimized = ({
               type='search'
               value={value}
               ref={inputElementRef}
+              data-playwright-test-label='fcc-search-input'
             />
-            <button className='ais-SearchBox-submit' type='submit'>
+            <button
+              className='ais-SearchBox-submit'
+              type='submit'
+              data-playwright-test-label='fcc-search-button'
+            >
               <Magnifier />
             </button>
             {value && (
@@ -63,6 +68,7 @@ const SearchBarOptimized = ({
                 className='ais-SearchBox-reset'
                 onClick={onClick}
                 type='button'
+                data-playwright-test-label='fcc-search-clear'
               >
                 <InputReset />
               </button>
