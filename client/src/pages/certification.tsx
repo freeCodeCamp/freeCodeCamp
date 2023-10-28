facc5d59fc3229b5c8430d8efa1fbbb23ad1c1aa
parakh2204 <parakh2204@gmail.com>
2023-10-28 22:38:11 +0530

    Update certification.tsx

--- client/src/pages/certification.tsx
+++ client/src/pages/certification.tsx
@@ -11,7 +11,6 @@ function Certification(): JSX.Element {
   return (
     <Router>
       <ShowCertification
-        // Error from installing @types/react-helmet and @types/react-redux
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         path={withPrefix('/certification/:username/:certSlug')}
