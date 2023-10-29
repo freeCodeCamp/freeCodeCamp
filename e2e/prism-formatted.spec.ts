b9d62b89a910136810abc447425888e143f6edd2
Radi Totev <raditotev@gmail.com>
2023-10-29 11:35:39 +0200

    Add e2e tests for prism-formatted

---
+++ e2e/prism-formatted.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Exercise description features syntax highlighting', async ({ page }) => {
+  await page.goto(
+    '/learn/javascript-algorithms-and-data-structures/basic-javascript/initializing-variables-with-the-assignment-operator'
+  );
+
+  await expect(page.getByTestId('challenge-description')).toHaveScreenshot();
+});
