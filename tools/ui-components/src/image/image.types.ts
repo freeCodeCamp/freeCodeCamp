4816aa972cacfe851260fe0ef7641a8a76367fe7
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2023-10-31 19:10:06 +0700

    fix(client): navigation profile image (#52087)

--- tools/ui-components/src/image/image.types.ts
+++ tools/ui-components/src/image/image.types.ts
@@ -2,4 +2,8 @@ import React from 'react';
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   alt: string;
   src: string;
+  /**
+   * Whether the image should be resized and contained within its parent.
+   */
+  responsive?: boolean;
 }
