bca4443a3406b14fc6c5b9005219d2f5858693fa
Nadav Chen <nadav2282@gmail.com>
2023-11-06 13:01:28 +0200

    Update cypress-install.js - Adding comments

--- cypress-install.js
+++ cypress-install.js
@@ -1,12 +1,17 @@
+// Import necessary dependencies
 const pkg = require('cypress/package.json');
 const execa = require('execa');
 
+// Get the Cypress version from the package.json
 const version = pkg.version;
 
+// Asynchronous function to install Cypress
 (async () => {
   console.log('Installing Cypress ' + version);
+  // Use the 'execa' library to run the 'cypress:install' script using 'pnpm'
   await execa('pnpm', ['run', 'cypress:install'], {
     env: { CYPRESS_INSTALL_BINARY: version }
   });
+  // Log a message indicating that Cypress has been successfully installed
   console.log('Cypress installed');
 })();
