9ecd5a785ec6bc81f43b242e0f4d8970fcf47b16
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-11-07 12:23:17 +0530

    fix(profile): user portfolio image size

--- client/src/components/helpers/avatar-renderer.tsx
+++ client/src/components/helpers/avatar-renderer.tsx
@@ -46,7 +46,7 @@ function AvatarRenderer({
       {isPlaceHolderImage ? (
         <DefaultAvatar className='avatar default-avatar' />
       ) : (
-        <Image alt='' src={picture} responsive />
+        <Image alt='' className='avatar' src={picture} />
       )}
     </div>
   );
