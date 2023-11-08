4fff48a087a00faadfb5ca036ee8709c146fc16c
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2023-11-09 05:52:53 +0700

    fix(client): profile image size and alignment (#52263)

--- tools/ui-components/src/image/image.tsx
+++ tools/ui-components/src/image/image.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { ImageProps } from './image.types';
 
 export const Image = React.forwardRef<HTMLImageElement, ImageProps>(
-  ({ alt, src, responsive, ...props }, ref): JSX.Element => {
+  ({ alt, src, responsive, className, ...props }, ref): JSX.Element => {
     // If `responsive` is `true`:
     // - Set `display` to `block`
-    // - Set a maximum relative to the parent
+    // - Set a maximum width relative to the parent
     // - Scale the height according to the width, otherwise the image is stretched
-    const className = responsive ? 'block max-w-full h-auto' : '';
+    const classes = responsive
+      ? [className, 'block max-w-full h-auto'].join(' ')
+      : className;
 
-    return (
-      <img ref={ref} alt={alt} src={src} className={className} {...props} />
-    );
+    return <img ref={ref} alt={alt} src={src} className={classes} {...props} />;
   }
 );
 
