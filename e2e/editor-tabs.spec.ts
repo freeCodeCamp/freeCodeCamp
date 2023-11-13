fcb523e545d6a5c78e50d4544826ba9e09ef7620
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2023-11-13 09:05:50 +0700

    refactor: merge tests and use playwright test IDs

--- e2e/editor-tabs.spec.ts
+++ e2e/editor-tabs.spec.ts
@@ -6,56 +6,25 @@ test.beforeEach(async ({ page }) => {
   );
 });
 
-test('should toggle editor visibility based on aria-expanded attribute', async ({
-  page
-}) => {
-  const tabLocator = page.locator('div.monaco-editor-tabs button').first();
-  const isExpandedBeforeClick = await tabLocator.getAttribute('aria-expanded');
-  if (isExpandedBeforeClick === 'true') {
-    await tabLocator.click();
-    const isExpandedAfterClick = await tabLocator.getAttribute('aria-expanded');
-    expect(isExpandedAfterClick).toBe('false');
-  } else {
-    await tabLocator.click();
-    const isExpandedAfterClick = await tabLocator.getAttribute('aria-expanded');
-    expect(isExpandedAfterClick).toBe('true');
-  }
-});
+test('should toggle editor visibility correctly', async ({ page }) => {
+  const htmlTabToggle = page.getByRole('button', { name: 'index.html Editor' });
+  const cssTabToggle = page.getByRole('button', { name: 'styles.css Editor' });
+  const htmlTab = page.getByTestId('editor-container-indexhtml');
+  const cssTab = page.getByTestId('editor-container-stylescss');
 
-test('toggling the button should toggle the appearance of the corresponding ReflexElement', async ({
-  page
-}) => {
-  const tabsLocator = await page.locator('div.monaco-editor-tabs button').all();
-  for (const tab of tabsLocator) {
-    const tabFileKey = await tab.getAttribute('data-cy');
-    if (tabFileKey) {
-      const fileKey = tabFileKey.replace('editor-tab-', '');
+  await expect(htmlTabToggle).toBeVisible();
+  // HTML tab is opened by default
+  await expect(htmlTabToggle).toHaveAttribute('aria-expanded', 'true');
+  await expect(htmlTab).toBeVisible();
+  await htmlTabToggle.click();
+  await expect(htmlTabToggle).toHaveAttribute('aria-expanded', 'false');
+  await expect(htmlTab).not.toBeVisible();
 
-      const ariaExpandedBeforeClick = await tab.getAttribute('aria-expanded');
-      const reflexElementBeforeClick = page.locator(
-        `div[data-cy="editor-container-${fileKey}"]`
-      );
-
-      if (ariaExpandedBeforeClick === 'true') {
-        await expect(reflexElementBeforeClick).toBeVisible();
-      } else {
-        await expect(reflexElementBeforeClick).toBeHidden();
-      }
-
-      await tab.click();
-
-      const ariaExpandedAfterClick = await tab.getAttribute('aria-expanded');
-      const reflexElementAfterClick = page.locator(
-        `div[data-cy="editor-container-${fileKey}"]`
-      );
-
-      if (ariaExpandedAfterClick === 'true') {
-        await expect(reflexElementAfterClick).toBeVisible();
-      } else {
-        await expect(reflexElementAfterClick).toBeHidden();
-      }
-    } else {
-      throw new Error('Missing data-cy attribute on an editor tab button');
-    }
-  }
+  await expect(cssTabToggle).toBeVisible();
+  // CSS tab is not opened by default
+  await expect(cssTabToggle).toHaveAttribute('aria-expanded', 'false');
+  await expect(cssTab).not.toBeVisible();
+  await cssTabToggle.click();
+  await expect(cssTabToggle).toHaveAttribute('aria-expanded', 'true');
+  await expect(cssTab).toBeVisible();
 });
