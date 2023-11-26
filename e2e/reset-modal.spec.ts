3cf79253bf9cd89320c37d07d4ca370b3510ca76
weilirs <weilirs@umich.edu>
2023-11-26 15:06:17 -0600

    uncomment

--- e2e/reset-modal.spec.ts
+++ e2e/reset-modal.spec.ts
@@ -37,89 +37,90 @@ test('Check the rendering of the reset modal', async ({ page }) => {
     })
   ).toBeVisible();
 });
-// test('User can reset challenge', async ({ page }) => {
-//   await page.goto(
-//     '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
-//   );
 
-//   const editorPaneLabel =
-//     'Editor content;Press Alt+F1 for Accessibility Options.';
-//   const challengeSolution = '<h2>Cat Photos</h2>';
+test('User can reset challenge', async ({ page }) => {
+  await page.goto(
+    '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
+  );
 
-//   await page.getByLabel(editorPaneLabel).fill(challengeSolution);
-//   await expect(
-//     page
-//       .frameLocator('iframe[title="challenge preview"]')
-//       .getByRole('heading', { name: 'Cat Photos' })
-//   ).toBeVisible();
+  const editorPaneLabel =
+    'Editor content;Press Alt+F1 for Accessibility Options.';
+  const challengeSolution = '<h2>Cat Photos</h2>';
 
-//   await page
-//     .getByRole('button', {
-//       name: translations.buttons['check-code']
-//     })
-//     .click();
+  await page.getByLabel(editorPaneLabel).fill(challengeSolution);
+  await expect(
+    page
+      .frameLocator('iframe[title="challenge preview"]')
+      .getByRole('heading', { name: 'Cat Photos' })
+  ).toBeVisible();
 
-//   await expect(
-//     page.getByText(translations.learn['sorry-keep-trying'])
-//   ).toBeVisible();
+  await page
+    .getByRole('button', {
+      name: translations.buttons['check-code']
+    })
+    .click();
 
-//   await page.getByTestId('lowerJaw-reset-button').click();
-//   await page
-//     .getByRole('button', { name: translations.buttons['reset-lesson'] })
-//     .click();
+  await expect(
+    page.getByText(translations.learn['sorry-keep-trying'])
+  ).toBeVisible();
 
-//   await expect(
-//     page
-//       .frameLocator('iframe[title="challenge preview"]')
-//       .getByRole('heading', { name: 'Cat Photos' })
-//   ).not.toBeVisible();
-//   await expect(
-//     page.getByText(translations.learn['sorry-keep-trying'])
-//   ).not.toBeVisible();
-// });
+  await page.getByTestId('lowerJaw-reset-button').click();
+  await page
+    .getByRole('button', { name: translations.buttons['reset-lesson'] })
+    .click();
 
-// test('User can reset classic challenge', async ({ page }) => {
-//   await page.goto(
-//     '/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code'
-//   );
+  await expect(
+    page
+      .frameLocator('iframe[title="challenge preview"]')
+      .getByRole('heading', { name: 'Cat Photos' })
+  ).not.toBeVisible();
+  await expect(
+    page.getByText(translations.learn['sorry-keep-trying'])
+  ).not.toBeVisible();
+});
 
-//   const editorPaneLabel =
-//     'Editor content;Press Alt+F1 for Accessibility Options.';
-//   const challengeSolution = '// This is in-line comment';
+test('User can reset classic challenge', async ({ page }) => {
+  await page.goto(
+    '/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code'
+  );
 
-//   await page.getByLabel(editorPaneLabel).fill(challengeSolution);
+  const editorPaneLabel =
+    'Editor content;Press Alt+F1 for Accessibility Options.';
+  const challengeSolution = '// This is in-line comment';
 
-//   const submitButton = page.getByLabel('Run the tests use shortcut Ctrl+enter');
-//   await submitButton.click();
+  await page.getByLabel(editorPaneLabel).fill(challengeSolution);
 
-//   await expect(
-//     page.locator('.view-lines').getByText(challengeSolution)
-//   ).toBeVisible();
-//   await expect(
-//     page.getByLabel(translations.icons.passed).locator('circle')
-//   ).toBeVisible();
-//   await expect(
-//     page.getByText(translations.learn['tests-completed'])
-//   ).toBeVisible();
+  const submitButton = page.getByLabel('Run the tests use shortcut Ctrl+enter');
+  await submitButton.click();
 
-//   await page
-//     .getByRole('button', { name: translations.buttons['reset-lesson'] })
-//     .click();
-//   await page
-//     .getByRole('button', { name: translations.buttons['reset-lesson'] })
-//     .click();
+  await expect(
+    page.locator('.view-lines').getByText(challengeSolution)
+  ).toBeVisible();
+  await expect(
+    page.getByLabel(translations.icons.passed).locator('circle')
+  ).toBeVisible();
+  await expect(
+    page.getByText(translations.learn['tests-completed'])
+  ).toBeVisible();
 
-//   await expect(
-//     page.locator('.view-lines').getByText(challengeSolution)
-//   ).not.toBeVisible();
-//   await expect(
-//     page.getByLabel(translations.icons.passed).locator('circle')
-//   ).not.toBeVisible();
-//   await expect(
-//     page.getByText(translations.learn['tests-completed'])
-//   ).not.toBeVisible();
-//   await expect(page.getByText(translations.learn['test-output'])).toBeVisible();
-// });
+  await page
+    .getByRole('button', { name: translations.buttons['reset-lesson'] })
+    .click();
+  await page
+    .getByRole('button', { name: translations.buttons['reset-lesson'] })
+    .click();
+
+  await expect(
+    page.locator('.view-lines').getByText(challengeSolution)
+  ).not.toBeVisible();
+  await expect(
+    page.getByLabel(translations.icons.passed).locator('circle')
+  ).not.toBeVisible();
+  await expect(
+    page.getByText(translations.learn['tests-completed'])
+  ).not.toBeVisible();
+  await expect(page.getByText(translations.learn['test-output'])).toBeVisible();
+});
 
 test('User can close the reset modal', async ({ page }) => {
   await page.goto(
