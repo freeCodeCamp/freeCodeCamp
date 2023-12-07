544ab2e438783a47e7bdda932860208cd0293e64
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-12-07 14:42:16 +0100

    fix: import xterm dynamically to allow building
    Since xterm makes reference to 'self' it cannot be statically imported
    during the build, so we have to use import()

--- client/src/templates/Challenges/classic/xterm.tsx
+++ client/src/templates/Challenges/classic/xterm.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-// TODO: use dynamic import to load xterm.js only when needed
-import { IDisposable, Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
+import type { IDisposable, Terminal } from 'xterm';
 
 import { getPythonWorker } from '../utils/python-worker-handler';
 
@@ -33,70 +31,79 @@ export const XtermTerminal = () => {
 
   useEffect(() => {
     void registerServiceWorker();
-    // TODO: copy over resetTerminal from python-runner
-    const disposables: IDisposable[] = [];
-    // Setting convertEol so that \n is converted to \r\n. Otherwise the terminal
-    // will interpret \n as line feed and just move the cursor to the next line.
-    // convertEol makes every \n a \r\n.
-    const term = new Terminal({ convertEol: true });
-    const fitAddon = new FitAddon();
-    term.loadAddon(fitAddon);
-    if (termRef.current) term.open(termRef.current);
-    fitAddon.fit();
 
-    const writeLine = (text: string) => term.writeln(`>>> ${text}`);
+    let term: Terminal | null;
 
-    // TODO: prevent user from moving cursor outside the current input line and
-    // handle insertion and deletion properly. While backspace and delete don't
-    // seem to work, we can use "\x1b[0K" to clear from the cursor to the end.
-    // Also, we should not add special characters to the userinput string.
-    const input = () => {
-      let userinput = '';
-      // Eslint is correct that this only gets assigned once, but we can't use
-      // const because the declaration (before keyListener is defined) and
-      // assignment (after keyListener is defined) must be separate.
-      // eslint-disable-next-line prefer-const
-      let disposable: IDisposable | undefined;
+    async function createTerminal() {
+      // TODO: copy over resetTerminal from python-runner
+      const disposables: IDisposable[] = [];
+      const { Terminal } = await import('xterm');
+      const { FitAddon } = await import('xterm-addon-fit');
+      // Setting convertEol so that \n is converted to \r\n. Otherwise the terminal
+      // will interpret \n as line feed and just move the cursor to the next line.
+      // convertEol makes every \n a \r\n.
+      term = new Terminal({ convertEol: true });
+      const fitAddon = new FitAddon();
+      term.loadAddon(fitAddon);
+      if (termRef.current) term.open(termRef.current);
+      fitAddon.fit();
 
-      const done = () => {
-        disposable?.dispose();
-        navigator.serviceWorker.controller?.postMessage(userinput);
+      const writeLine = (text: string) => term?.writeln(`>>> ${text}`);
+
+      // TODO: prevent user from moving cursor outside the current input line and
+      // handle insertion and deletion properly. While backspace and delete don't
+      // seem to work, we can use "\x1b[0K" to clear from the cursor to the end.
+      // Also, we should not add special characters to the userinput string.
+      const input = () => {
+        let userinput = '';
+        // Eslint is correct that this only gets assigned once, but we can't use
+        // const because the declaration (before keyListener is defined) and
+        // assignment (after keyListener is defined) must be separate.
+        // eslint-disable-next-line prefer-const
+        let disposable: IDisposable | undefined;
+
+        const done = () => {
+          disposable?.dispose();
+          navigator.serviceWorker.controller?.postMessage(userinput);
+        };
+
+        const keyListener = (key: string) => {
+          if (key === '\u007F' || key === '\b') {
+            // Backspace or delete key
+            term?.write('\b \b'); // Move cursor back, replace character with space, then move cursor back again
+            userinput = userinput.slice(0, -1); // Remove the last character from userinput
+          }
+          if (key == '\r') {
+            term?.write('\r\n');
+            done();
+          } else {
+            userinput += key;
+            term?.write(key);
+          }
+        };
+
+        disposable = term?.onData(keyListener); // Listen for key events and store the disposable
+        if (disposable) disposables.push(disposable);
       };
 
-      const keyListener = (key: string) => {
-        if (key === '\u007F' || key === '\b') {
-          // Backspace or delete key
-          term.write('\b \b'); // Move cursor back, replace character with space, then move cursor back again
-          userinput = userinput.slice(0, -1); // Remove the last character from userinput
-        }
-        if (key == '\r') {
-          term.write('\r\n');
-          done();
-        } else {
-          userinput += key;
-          term.write(key);
+      const pythonWorker = getPythonWorker();
+      pythonWorker.onmessage = event => {
+        console.log('pythonWorker.onmessage', event);
+
+        const { type, text } = event.data as { type: string; text: string };
+        if (type === 'print') {
+          writeLine(text);
+        } else if (type === 'input') {
+          writeLine(text);
+          input();
         }
       };
+    }
 
-      disposable = term.onData(keyListener); // Listen for key events and store the disposable
-      disposables.push(disposable);
-    };
-
-    const pythonWorker = getPythonWorker();
-    pythonWorker.onmessage = event => {
-      console.log('pythonWorker.onmessage', event);
-
-      const { type, text } = event.data as { type: string; text: string };
-      if (type === 'print') {
-        writeLine(text);
-      } else if (type === 'input') {
-        writeLine(text);
-        input();
-      }
-    };
+    void createTerminal();
 
     return () => {
-      term.dispose();
+      term?.dispose();
       // TODO: figure out if we need/want to terminate the worker
     };
   }, []);
