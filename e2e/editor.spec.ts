cccceb2e49f318b800bf1ce433e09e36e2af604e
Rahul Suresh <rahul.tech.work@gmail.com>
2023-12-08 16:58:27 -0600

    shifted from click to focus method

--- e2e/editor.spec.ts
+++ e2e/editor.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page }) => {
 test.describe('Editor Component', () => {
   test('Should be clicked and able to insert text', async ({ page }) => {
     const monacoEditor = page.getByLabel('Editor content');
-    await monacoEditor.click();
+    await monacoEditor.focus();
     await page.keyboard.insertText('<h2>FreeCodeCamp</h2>');
     const text = page.getByText('<h2>FreeCodeCamp</h2>');
     await expect(text).toBeVisible();
