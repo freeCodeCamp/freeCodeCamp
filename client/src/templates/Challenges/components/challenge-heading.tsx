b068b65ebfb6d8642c92b27bc85dcb18e927fe62
moT01 <20648924+moT01@users.noreply.github.com>
2023-12-19 09:38:34 -0600

    fix: remove i18nkey from challengheading

--- client/src/templates/Challenges/components/challenge-heading.tsx
+++ client/src/templates/Challenges/components/challenge-heading.tsx
@@ -6,20 +6,18 @@ import './challenge-heading.css';
 
 interface ChallengeHeadingProps {
   heading: string;
-  isI18nKey?: boolean;
   isCompleted?: boolean;
 }
 
 function ChallengeHeading({
   heading,
-  isI18nKey = true,
   isCompleted = false
 }: ChallengeHeadingProps): JSX.Element {
   const { t } = useTranslation();
 
   return (
     <div className='challenge-heading-wrap'>
-      <h2 className='challenge-heading'>{isI18nKey ? t(heading) : heading}</h2>
+      <h2 className='challenge-heading'>{t(heading)}</h2>
       {isCompleted && <GreenPass />}
     </div>
   );
