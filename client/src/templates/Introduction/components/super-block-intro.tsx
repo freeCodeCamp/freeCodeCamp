96d327c1909db471ec7d5a7fc323273a36b46fb2
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2023-12-19 21:50:33 +0300

    feat: add beta cert alert

--- client/src/templates/Introduction/components/super-block-intro.tsx
+++ client/src/templates/Introduction/components/super-block-intro.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Alert } from '@freecodecamp/ui';
 import { SuperBlocks } from '../../../../../shared/config/superblocks';
 import { SuperBlockIcon } from '../../../assets/icons/superblock-icon';
-import { Spacer } from '../../../components/helpers';
+import { Spacer, Link } from '../../../components/helpers';
 
 interface SuperBlockIntroProps {
   superBlock: SuperBlocks;
 }
+
+export const ConditionalDonationAlert = ({
+  superBlock
+}: {
+  superBlock: SuperBlocks;
+}): JSX.Element => {
+  const { t } = useTranslation();
+
+  if (
+    superBlock === SuperBlocks.JsAlgoDataStructNew ||
+    superBlock === SuperBlocks.A2English ||
+    superBlock === SuperBlocks.TheOdinProject ||
+    superBlock === SuperBlocks.UpcomingPython
+  )
+    return (
+      <Alert variant='info' className='annual-donation-alert'>
+        <p>{t('donate.beta-certification')}</p>
+        <hr />
+        <p className={'text-center'}>
+          <Link
+            className='btn'
+            key='donate'
+            sameTab={false}
+            to='/donate'
+            // onClick={onDonationAlertClick}
+          >
+            {t('buttons.donate-now')}
+          </Link>
+        </p>
+      </Alert>
+    );
+  return <></>;
+};
 
 function SuperBlockIntro(props: SuperBlockIntroProps): JSX.Element {
   const { t } = useTranslation();
@@ -46,6 +80,7 @@ function SuperBlockIntro(props: SuperBlockIntroProps): JSX.Element {
           {superBlockNoteText}
         </div>
       )}
+      <ConditionalDonationAlert superBlock={superBlock} />
     </>
   );
 }
