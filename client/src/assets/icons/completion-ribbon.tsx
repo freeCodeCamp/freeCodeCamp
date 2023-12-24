edd88033f83a6f49dea1546220febc123f8f27ed
Aaron Cottrill <a.rcottrill521@gmail.com>
2023-12-24 15:57:18 -0500

    Remove arrows and add back ribbons

--- client/src/assets/icons/completion-ribbon.tsx
+++ client/src/assets/icons/completion-ribbon.tsx
@@ -4,7 +4,7 @@ interface RibbonProps {
   value: number;
 }
 
-const RibbonIcon = ({ value }: RibbonProps): JSX.Element => {
+export const RibbonIcon = ({ value }: RibbonProps): JSX.Element => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -14,6 +14,14 @@ const RibbonIcon = ({ value }: RibbonProps): JSX.Element => {
       fill='none'
       aria-hidden='true'
     >
+      <path
+        d='M25 35.3418L35.4851 28L44.5957 41.0113L36.2658 39.7151L34.1106 48.353L25 35.3418Z'
+        className='map-icon'
+      />
+      <path
+        d='M9.11059 29L19.5957 36.3418L10.4851 49.353L8.85418 41.0821L-4.67677e-07 42.0113L9.11059 29Z'
+        className='map-icon'
+      />
       <circle cx='21.9999' cy='21' r='20' fill='black' />
       <circle
         cx='22.5'
@@ -39,6 +47,41 @@ const RibbonIcon = ({ value }: RibbonProps): JSX.Element => {
   );
 };
 
+export const IncompleteIcon = ({ value }: RibbonProps): JSX.Element => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='100%'
+      height='100%'
+      viewBox='0 0 45 50'
+      fill='none'
+      aria-hidden='true'
+    >
+      <circle cx='21.9999' cy='21' r='20' fill='grey' />
+      <circle
+        cx='22.5'
+        cy='21'
+        r='17.5'
+        fill='grey'
+        stroke='white'
+        strokeWidth='2'
+      />
+      <text
+        x='48%'
+        y='57%'
+        fontFamily='Verdana'
+        color='black'
+        fontSize='1.3rem'
+        fill='#fff'
+        textAnchor='middle'
+        alignmentBaseline='central'
+      >
+        {value}
+      </text>
+    </svg>
+  );
+};
+
 RibbonIcon.displayName = 'RibbonIcon';
 
-export default RibbonIcon;
+IncompleteIcon.displayName = 'IncompleteIcon';
