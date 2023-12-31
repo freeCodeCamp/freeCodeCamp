0e19c1d68230ec2aa60a2d3ecd326c6a9765b13a
Aaron Cottrill <a.rcottrill521@gmail.com>
2023-12-31 15:55:35 -0500

    Boolean the answers map

--- client/src/templates/Challenges/fill-in-the-blank/show.tsx
+++ client/src/templates/Challenges/fill-in-the-blank/show.tsx
@@ -180,8 +180,8 @@ class ShowFillInTheBlank extends Component<
 
     const blankAnswers = blanks.map(b => b.answer);
 
-    const newAnswersCorrect = userAnswers.map((userAnswer, i) =>
-      userAnswer && userAnswer.trim() === blankAnswers[i]
+    const newAnswersCorrect = userAnswers.map(
+      (userAnswer, i) => !!userAnswer && userAnswer.trim() === blankAnswers[i]
     );
 
     const hasWrongAnswer = newAnswersCorrect.some(a => a === false);
