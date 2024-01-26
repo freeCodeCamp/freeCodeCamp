c8155eb021aeb0dbcfa989204630aa6681f80520
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-01-26 17:28:10 +0100

    test(e2e): test modal in next python cert
    scientific-computing-with-python will stop accepting links as solutions
    soon, but data-analysis-with-python will accept them for now.

--- e2e/completion-modal.spec.ts
+++ e2e/completion-modal.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
 
-const timeCalculatorUrl =
-  '/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator';
+const nextChallengeURL =
+  '/learn/data-analysis-with-python/data-analysis-with-python-projects/demographic-data-analyzer';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(
-    '/learn/scientific-computing-with-python/scientific-computing-with-python-projects/arithmetic-formatter'
+    '/learn/data-analysis-with-python/data-analysis-with-python-projects/mean-variance-standard-deviation-calculator'
   );
   await page.getByLabel('Solution Link').fill('https://example.com');
   await page
@@ -44,7 +44,7 @@ test.describe('Challenge Completion Modal Tests (Signed Out)', () => {
     await page
       .getByRole('button', { name: translations.buttons['go-to-next'] })
       .click();
-    await expect(page).toHaveURL(timeCalculatorUrl);
+    await expect(page).toHaveURL(nextChallengeURL);
   });
 });
 
@@ -73,6 +73,6 @@ test.describe('Challenge Completion Modal Tests (Signed In)', () => {
     await page
       .getByRole('button', { name: translations.buttons['submit-and-go'] })
       .click();
-    await expect(page).toHaveURL(timeCalculatorUrl);
+    await expect(page).toHaveURL(nextChallengeURL);
   });
 });
