f26a82548657371a116b536759de3e870cacc34f
moT01 <20648924+moT01@users.noreply.github.com>
2024-01-30 14:37:40 -0600

    feat: add disable RDB growthbook flags

--- client/src/components/growth-book/codeally-down.tsx
+++ client/src/components/growth-book/codeally-down.tsx
@@ -6,9 +6,45 @@ import Spacer from '../../components/helpers/spacer';
 
 export function CodeAllyDown(): JSX.Element | null {
   const codeAllyDownFeature = useFeature('codeally_down');
+  const codeAllyDisablingSoonFeature = useFeature('codeally_disabling_soon');
+  const codeAllyDisabledFeature = useFeature('codeally_disabled');
   const { t } = useTranslation();
 
-  return codeAllyDownFeature.on ? (
+  return codeAllyDisabledFeature.on ? (
+    <Alert variant='danger'>
+      <p>
+        <Trans i18nKey='intro:misc-text.course-disabled'>
+          <a
+            href='https://www.freecodecamp.org/news/how-to-run-freecodecamps-relational-databases-curriculum-using-docker-vscode-and-coderoad'
+            rel='noreferrer'
+            target='_blank'
+          >
+            placeholder
+          </a>
+        </Trans>
+      </p>
+      <Spacer size='small' />
+      <p>{t('intro:misc-text.progress-wont-save')}</p>
+    </Alert>
+  ) : codeAllyDisablingSoonFeature.on ? (
+    <Alert variant='danger'>
+      <p>{t('intro:misc-text.course-disabling-soon')}</p>
+      <Spacer size='small' />
+      <p>
+        <Trans i18nKey='intro:misc-text.run-locally'>
+          <a
+            href='https://www.freecodecamp.org/news/how-to-run-freecodecamps-relational-databases-curriculum-using-docker-vscode-and-coderoad'
+            rel='noreferrer'
+            target='_blank'
+          >
+            placeholder
+          </a>
+        </Trans>
+      </p>
+      <Spacer size='small' />
+      <p>{t('intro:misc-text.progress-wont-save')}</p>
+    </Alert>
+  ) : codeAllyDownFeature.on ? (
     <Alert variant='danger'>
       <p>
         <Trans i18nKey='intro:misc-text.course-maintenance'>
