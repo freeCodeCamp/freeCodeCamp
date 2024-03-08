83d2cca0f32b7695ff8e609bcb93797706fe59a6
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-03-09 03:50:19 +0700

    wip

--- tools/ui-components/src/modal/modal.stories.tsx
+++ tools/ui-components/src/modal/modal.stories.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { StoryObj, StoryFn, Meta } from '@storybook/react';
 
 import { Button } from '../button';
-import { Modal, type ModalProps } from './modal';
+import { Modal } from './modal';
+import { type ModalProps } from './types';
 
 const story = {
   title: 'Example/Modal',
@@ -11,71 +12,161 @@ const story = {
 
 type Story = StoryObj<typeof Modal>;
 
+const Spacer = () => <div style={{ height: '5px', width: '100%' }} />;
+
 const AutoOpenTemplate: StoryFn<ModalProps> = args => {
-  return <Modal {...args} />;
+  return (
+    <Modal {...args}>
+      <Modal.Header>Edit profile</Modal.Header>
+      <Modal.Body>
+        <p>
+          Laboriosam autem non et nisi. Ut voluptatem sit beatae assumenda amet
+          aliquam corporis.
+        </p>
+        <p>
+          Dolores voluptas omnis et cupiditate ducimus delectus vel. Voluptas
+          atque cumque incidunt quia. A praesentium neque quis odit totam
+          praesentium illum est. Ut doloribus quisquam ut. Incidunt vel suscipit
+          accusamus consequuntur repellendus dolor sunt. Vel accusamus nesciunt
+          perspiciatis sunt est.
+        </p>
+        <p>
+          Tempore quis voluptas aut voluptatem praesentium nisi. Qui et quo ut
+          et vel dolores facilis dignissimos. Omnis facere quisquam recusandae
+          accusantium. Sit ut consectetur non id velit est odio. Laboriosam
+          soluta tenetur asperiores. Excepturi reprehenderit rerum sint tempore
+          molestiae vitae aliquid. Ea est sunt at atque ducimus doloribus quas
+          sit.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button block>Submit</Button>
+        <Spacer />
+        <Button block>Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 const WithTriggerTemplate: StoryFn<ModalProps> = args => {
   const [open, setOpen] = useState(false);
 
+  const handleClose = () => setOpen(false);
+
   return (
     <div>
       <Button onClick={() => setOpen(true)}>Open modal</Button>
-      <Modal {...args} open={open} onClose={() => setOpen(false)} />
+      <Modal {...args} open={open} onClose={handleClose}>
+        <Modal.Header>Edit profile</Modal.Header>
+        <Modal.Body>
+          <p>
+            Laboriosam autem non et nisi. Ut voluptatem sit beatae assumenda
+            amet aliquam corporis.
+          </p>
+          <p>
+            Dolores voluptas omnis et cupiditate ducimus delectus vel. Voluptas
+            atque cumque incidunt quia. A praesentium neque quis odit totam
+            praesentium illum est. Ut doloribus quisquam ut. Incidunt vel
+            suscipit accusamus consequuntur repellendus dolor sunt. Vel
+            accusamus nesciunt perspiciatis sunt est.
+          </p>
+          <p>
+            Tempore quis voluptas aut voluptatem praesentium nisi. Qui et quo ut
+            et vel dolores facilis dignissimos. Omnis facere quisquam recusandae
+            accusantium. Sit ut consectetur non id velit est odio. Laboriosam
+            soluta tenetur asperiores. Excepturi reprehenderit rerum sint
+            tempore molestiae vitae aliquid. Ea est sunt at atque ducimus
+            doloribus quas sit.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block>Submit</Button>
+          <Spacer />
+          <Button block onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
 
-export const AutoOpen: Story = {
+export const Default: Story = {
   render: AutoOpenTemplate,
   args: {
     open: true,
-    onClose: () => {},
-    children: (
-      <>
-        <Modal.Header>Edit profile</Modal.Header>
-        <Modal.Body>Content</Modal.Body>
-        <Modal.Footer>
-          <Button>Submit</Button>
-          <Button>Cancel</Button>
-        </Modal.Footer>
-      </>
-    )
+    onClose: () => {}
+  }
+};
+
+export const Large: Story = {
+  render: AutoOpenTemplate,
+  args: {
+    size: 'large',
+    open: true,
+    onClose: () => {}
+  }
+};
+
+export const Small: Story = {
+  render: AutoOpenTemplate,
+  args: {
+    size: 'small',
+    open: true,
+    onClose: () => {}
+  }
+};
+
+export const Danger: Story = {
+  render: AutoOpenTemplate,
+  args: {
+    open: true,
+    variant: 'danger',
+    onClose: () => {}
   }
 };
 
 export const WithoutCloseButton: Story = {
-  render: AutoOpenTemplate,
+  render: () => (
+    <Modal open onClose={() => {}}>
+      <Modal.Header showCloseButton={false}>Edit profile</Modal.Header>
+      <Modal.Body>
+        <p>
+          Laboriosam autem non et nisi. Ut voluptatem sit beatae assumenda amet
+          aliquam corporis.
+        </p>
+        <p>
+          Dolores voluptas omnis et cupiditate ducimus delectus vel. Voluptas
+          atque cumque incidunt quia. A praesentium neque quis odit totam
+          praesentium illum est. Ut doloribus quisquam ut. Incidunt vel suscipit
+          accusamus consequuntur repellendus dolor sunt. Vel accusamus nesciunt
+          perspiciatis sunt est.
+        </p>
+        <p>
+          Tempore quis voluptas aut voluptatem praesentium nisi. Qui et quo ut
+          et vel dolores facilis dignissimos. Omnis facere quisquam recusandae
+          accusantium. Sit ut consectetur non id velit est odio. Laboriosam
+          soluta tenetur asperiores. Excepturi reprehenderit rerum sint tempore
+          molestiae vitae aliquid. Ea est sunt at atque ducimus doloribus quas
+          sit.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button block>Submit</Button>
+        <Spacer />
+        <Button block>Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  ),
   args: {
     open: true,
-    onClose: () => {},
-    children: (
-      <>
-        <Modal.Header showCloseButton={false}>Edit profile</Modal.Header>
-        <Modal.Body>Content</Modal.Body>
-        <Modal.Footer>
-          <Button>Submit</Button>
-          <Button>Cancel</Button>
-        </Modal.Footer>
-      </>
-    )
+    onClose: () => {}
   }
 };
 
 export const WithTrigger: Story = {
   render: WithTriggerTemplate,
-  args: {
-    children: (
-      <>
-        <Modal.Header>Edit profile</Modal.Header>
-        <Modal.Body>Content</Modal.Body>
-        <Modal.Footer>
-          <Button>Submit</Button>
-          <Button>Cancel</Button>
-        </Modal.Footer>
-      </>
-    )
-  }
+  args: {}
 };
 
 export default story;
--- tools/ui-components/src/modal/modal.tsx
+++ tools/ui-components/src/modal/modal.tsx
@@ -1,55 +1,144 @@
-import React, { ReactNode, createContext, useContext } from 'react';
-import { Dialog } from '@headlessui/react';
+import React, {
+  type ReactNode,
+  createContext,
+  useContext,
+  Fragment
+} from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
 import { CloseButton } from '../close-button';
+import { type ModalProps, type HeaderProps } from './types';
 
-export type ModalProps = {
-  children: ReactNode;
-  open: boolean;
-  onClose: () => void;
-  size?: 'large' | 'small';
-  variant?: 'default' | 'danger';
-};
+// There is a close button on the right side of the modal title.
+// Some extra padding needs to be added to the left of the title text
+// so that the title is properly centered.
+// The value of the left padding is the width of the close button.
+const TITLE_LEFT_PADDING = 24;
 
-type HeaderProps = {
-  children: ReactNode;
-  showCloseButton?: boolean;
-};
+const PANEL_DEFAULT_CLASSES = [
+  'flex',
+  'flex-col',
+  'border-solid',
+  'border-1',
+  'border-foreground-secondary',
+  'bg-background-secondary'
+];
 
-const ModalContext = createContext<Pick<ModalProps, 'onClose'>>({
-  onClose: () => {}
+const HEADER_DEFAULT_CLASSES = [
+  'p-[15px]',
+  'border-b-1',
+  'border-solid',
+  'border-foreground-secondary'
+];
+
+const ModalContext = createContext<Pick<ModalProps, 'onClose' | 'variant'>>({
+  onClose: () => {},
+  variant: 'default'
 });
 
-export const Modal = ({ children, open, onClose }: ModalProps) => {
-  return (
-    <ModalContext.Provider value={{ onClose }}>
-      <Dialog open={open} onClose={onClose}>
-        <Dialog.Panel>{children}</Dialog.Panel>
-      </Dialog>
-    </ModalContext.Provider>
-  );
-};
-
 const Header = ({ children, showCloseButton = true }: HeaderProps) => {
-  const { onClose } = useContext(ModalContext);
+  const { onClose, variant } = useContext(ModalContext);
+
+  const classes = [...HEADER_DEFAULT_CLASSES];
+
+  if (variant === 'danger') {
+    classes.push('bg-foreground-danger');
+  }
 
   if (showCloseButton) {
+    classes.push(...['flex', 'items-center', 'justify-between']);
+
     return (
-      <div>
-        <Dialog.Title>{children}</Dialog.Title>
+      <div className={classes.join(' ')}>
+        <Dialog.Title
+          className={`m-0 pl-[${TITLE_LEFT_PADDING}px] flex-1 text-md text-center`}
+        >
+          {children}
+        </Dialog.Title>
         <CloseButton onClick={onClose} />
       </div>
     );
   }
 
-  return <Dialog.Title>{children}</Dialog.Title>;
+  return (
+    <div className={classes.join(' ')}>
+      <Dialog.Title className={'m-0 text-md text-center'}>
+        {children}
+      </Dialog.Title>
+    </div>
+  );
 };
 
 const Body = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className='p-[15px] border-b-1 border-solid border-foreground-secondary'>
+      {children}
+    </div>
+  );
 };
 
 const Footer = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className='p-[15px]'>{children}</div>;
+};
+
+export const Modal = ({
+  children,
+  open,
+  onClose,
+  size = 'medium',
+  variant = 'default'
+}: ModalProps) => {
+  const classes = [...PANEL_DEFAULT_CLASSES];
+
+  switch (size) {
+    case 'large':
+      classes.push('w-[900px]');
+      break;
+    case 'small':
+      classes.push('w-[300px]');
+      break;
+    default:
+      classes.push('w-[600px]');
+  }
+
+  switch (variant) {
+    case 'danger':
+      classes.push('text-background-danger');
+      break;
+    default:
+      classes.push('text-foreground-secondary');
+  }
+
+  return (
+    <ModalContext.Provider value={{ onClose, variant }}>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog onClose={onClose} className='relative z-50'>
+          {/* The backdrop, rendered as a fixed sibling to the panel container */}
+          <div
+            aria-hidden
+            className='fixed inset-0 bg-foreground-primary opacity-50'
+          />
+
+          {/* Full-screen container of the panel */}
+          <div className='fixed inset-0 w-screen flex items-start justify-center pt-[30px]'>
+            <Transition.Child
+              as={Fragment}
+              enter='transition-all duration-300 ease-out'
+              enterFrom='opacity-0 -translate-y-1/4'
+              enterTo='opacity-100 translate-y-0'
+              leave='transition-all duration-300 ease-out'
+              leaveFrom='opacity-100 translate-y-0'
+              leaveTo='opacity-0 -translate-y-1/4'
+            >
+              <Dialog.Panel className={classes.join(' ')}>
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </ModalContext.Provider>
+  );
 };
 
 Modal.Header = Header;
