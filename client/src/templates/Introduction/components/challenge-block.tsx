cf666b019d9bee8ebba1f331e265e0ce785a3a82
RavidYael <RavidYael.GitHubUser@gmail.com>
2024-03-08 12:00:04 +0200

    Add ChallengeBlock component to Introduction template

---
+++ client/src/templates/Introduction/components/challenge-block.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { withTranslation, useTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { Dispatch } from 'redux';
+import { executeGA } from '../../../redux/actions';
+import { ProgressionNodesWithCompleted } from '../../../redux/prop-types';
+import { SuperBlocks } from '../../../../../shared/config/superblocks';
+import ChallengeOrProjectNodesList from './challenge-or-project-nodes-list';
+import ProjectController from './project-controller';
+import ChallengeNodesList from './challenge-nodes-list';
+
+const getStepNumber = (dashedName: string) => {
+  // dashedName should be in the format 'step-1' or 'task-1'
+  const match = dashedName.match(/-(\d+)/);
+  return match ? match[1] : '';
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators({ executeGA }, dispatch);
+
+interface ChallengeBlock {
+  progressionNodes: ProgressionNodesWithCompleted[];
+  isProjectBlock: boolean;
+  isGridMap?: boolean;
+  blockTitle?: string | null;
+}
+
+function ChallengeBlock({
+  progressionNodes,
+  isProjectBlock,
+  isGridMap = false,
+  blockTitle
+}: ChallengeBlock): JSX.Element {
+  const { t } = useTranslation();
+
+  const firstIncompleteChallenge = progressionNodes.find(
+    node => !node.isCompleted
+  );
+
+  const isChallengeStarted = !!progressionNodes.find(node => node.isCompleted);
+
+  const buttons = firstIncompleteChallenge
+    ? [
+        {
+          to: firstIncompleteChallenge.fields.slug,
+          text: isChallengeStarted
+            ? t('buttons.resume-project')
+            : t('buttons.start-project'),
+          className: 'btn-primary'
+        }
+        // Enahnce: any additional buttons we need insert here-> Next up : Reset
+      ]
+    : [];
+
+  return isGridMap ? (
+    <>
+      {buttons.length > 0 && <ProjectController buttons={buttons} />}
+      <nav
+        aria-label={
+          blockTitle
+            ? progressionNodes[0].superBlock === SuperBlocks.A2English
+              ? t('aria.dialogues-and-tasks-for', { blockTitle })
+              : t('aria.steps-for', { blockTitle })
+            : t('aria.steps')
+        }
+      >
+        <ChallengeOrProjectNodesList
+          nodes={progressionNodes}
+          isProject={isProjectBlock}
+          getStepNumber={getStepNumber}
+          t={t}
+        />
+      </nav>
+    </>
+  ) : (
+    <ChallengeNodesList nodes={progressionNodes} isProject={isProjectBlock} />
+  );
+}
+
+ChallengeBlock.displayName = 'ChallengeBlock';
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(withTranslation()(ChallengeBlock));
