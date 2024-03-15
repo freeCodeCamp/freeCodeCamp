7f6d9cab70e5d4a55486c293d5498e7b174c4fe4
Sem Bauke <semboot699@gmail.com>
2024-03-15 10:00:00 +0100

    Apply suggestions from code review
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- cypress-install.js
+++ cypress-install.js
@@ -1,14 +1,13 @@
 const { spawn } = require('child_process');
-const pkg = require('cypress/package.json');
-
-console.log('Installing Cypress version:', pkg.version);
 
 const child = spawn('pnpm', ['run', 'cypress:install'], {
   stdio: 'inherit'
 });
 
 child.on('close', code => {
-  console.log('Cypress installation finished with code:', code);
+  if (code) {
+    console.error('Cypress installation failed with code:', code);
+  }
 });
 
 child.on('error', error => {
