d6ee06cef9576500a3a360e20118dbc9222a275b
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-03-21 12:04:44 +0700

    fix(ui-components): modal body alignment

--- tools/ui-components/src/modal/types.ts
+++ tools/ui-components/src/modal/types.ts
@@ -12,3 +12,13 @@ export interface HeaderProps {
   children: ReactNode;
   showCloseButton?: boolean;
 }
+
+export interface BodyProps {
+  children: ReactNode;
+
+  /**
+   * Use `start` for better right-to-left support.
+   * Use `left` if the modal body contains code blocks.
+   */
+  alignment?: 'center' | 'left' | 'start';
+}
