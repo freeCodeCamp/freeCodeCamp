2e12cc5a63095f31d5998f4c878309e64baaa531
RavidYael <RavidYael.GitHubUser@gmail.com>
2024-03-23 22:44:51 +0200

    Refactor getChallengeMapByBlock function and add blocksToChallengesService module

--- api-server/src/server/utils/get-blocks-challenges-mappings.js
+++ api-server/src/server/utils/get-blocks-challenges-mappings.js
@@ -1,38 +1,39 @@
 // eslint-disable-next-line import/no-unresolved
-import curriculum from '../../../../shared/config/curriculum.json';
+const curriculum = require('../../../../shared/config/curriculum.json');
 
-let challengeMap = null;
+const blocksToChallengesService = (() => {
+  let blocksToChallengesMap = null;
 
-export function getChallengeMapByBlock() {
-  if (challengeMap) {
-    return challengeMap;
+  function buildBlocksToChallengesMap() {
+    const map = {};
+    Object.values(curriculum).forEach(superBlock => {
+      if (superBlock.blocks) {
+        Object.entries(superBlock.blocks).forEach(([dashedName, block]) => {
+          if (block.challenges) {
+            map[dashedName] = block.challenges.map(challenge => challenge.id);
+          }
+        });
+      }
+    });
+    return map;
   }
 
-  challengeMap = {};
-
-  Object.values(curriculum).forEach(superBlock => {
-    if (superBlock.blocks) {
-      Object.entries(superBlock.blocks).forEach(([dashedName, block]) => {
-        if (block.challenges) {
-          challengeMap[dashedName] = block.challenges.map(
-            challenge => challenge.id
-          );
-        }
-      });
+  return {
+    getBlocksToChallengesMap() {
+      if (!blocksToChallengesMap) {
+        blocksToChallengesMap = buildBlocksToChallengesMap();
+      }
+      return blocksToChallengesMap;
+    },
+    getChallengeIdsForBlock(dashedName) {
+      const map = this.getBlocksToChallengesMap();
+      const challengeIds = map[dashedName] || [];
+      if (challengeIds.length === 0) {
+        throw new Error(`No challenges found for the block: ${dashedName}`);
+      }
+      return challengeIds;
     }
-  });
+  };
+})();
 
-  return challengeMap;
-}
-
-export function getChallengeIdsForBlock(dashedName) {
-  const localChallengeMap = getChallengeMapByBlock();
-
-  const challengeIds = localChallengeMap[dashedName] || [];
-
-  if (challengeIds.length === 0) {
-    throw new Error(`No block found with the name: ${dashedName}`);
-  }
-
-  return challengeIds;
-}
+module.exports = blocksToChallengesService;
