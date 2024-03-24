26d4c0625c0d1a1f0d89a55410bd2bb2e719d305
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-03-25 12:56:27 +1300

    feat(client): fallback image for portfolio page

--- client/src/components/profile/components/portfolio-projects.tsx
+++ client/src/components/profile/components/portfolio-projects.tsx
@@ -33,7 +33,8 @@ export const PortfolioProjects = ({
           </a>
 
           {image && (
-            <img alt='' className='portfolio-screen-shot' src={image} />
+            
+            <img alt='' className='portfolio-screen-shot' src={image} onError="this.onerror=null; this.src='https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg';"/>
           )}
         </div>
       ))}
