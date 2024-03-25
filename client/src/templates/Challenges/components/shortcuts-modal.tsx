105be37db829c855ec7f7bbd4235698e2ce70861
sembauke <semboot699@gmail.com>
2024-03-25 13:52:25 +0100

    fix: modal

--- client/src/templates/Challenges/components/shortcuts-modal.tsx
+++ client/src/templates/Challenges/components/shortcuts-modal.tsx
@@ -50,40 +50,42 @@ function ShortcutsModal({
         {t('shortcuts.title')}
       </Modal.Header>
       <Modal.Body>
-        <table>
-          <thead>
-            <tr>
-              <th scope='col'>{t('shortcuts.table-header-action')}</th>
-              <th scope='col'>{t('shortcuts.table-header-key')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{t('shortcuts.navigation-mode')}</td>
-              <td>ESC</td>
-            </tr>
-            <tr>
-              <td>{t('shortcuts.execute-challenge')}</td>
-              <td>CTRL/Command + Enter</td>
-            </tr>
-            <tr>
-              <td>{t('shortcuts.focus-editor')}</td>
-              <td>e</td>
-            </tr>
-            <tr>
-              <td>{t('shortcuts.focus-instructions-panel')}</td>
-              <td>r</td>
-            </tr>
-            <tr>
-              <td>{t('shortcuts.navigate-previous')}</td>
-              <td>p</td>
-            </tr>
-            <tr>
-              <td>{t('shortcuts.navigate-next')}</td>
-              <td>n</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className='shortcuts-modal-body'>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>{t('shortcuts.table-header-action')}</th>
+                <th scope='col'>{t('shortcuts.table-header-key')}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{t('shortcuts.navigation-mode')}</td>
+                <td>ESC</td>
+              </tr>
+              <tr>
+                <td>{t('shortcuts.execute-challenge')}</td>
+                <td>CTRL/Command + Enter</td>
+              </tr>
+              <tr>
+                <td>{t('shortcuts.focus-editor')}</td>
+                <td>e</td>
+              </tr>
+              <tr>
+                <td>{t('shortcuts.focus-instructions-panel')}</td>
+                <td>r</td>
+              </tr>
+              <tr>
+                <td>{t('shortcuts.navigate-previous')}</td>
+                <td>p</td>
+              </tr>
+              <tr>
+                <td>{t('shortcuts.navigate-next')}</td>
+                <td>n</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <KeyboardShortcutsSettings
