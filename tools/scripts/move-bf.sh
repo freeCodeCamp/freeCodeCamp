5c227a7fbc7ef6c319577aae06c1fa0059b0b74a
kiritrajput <164477029+kiritrajput@users.noreply.github.com>
2024-03-28 17:52:19 +0530

    Update move-bf.sh

--- tools/scripts/move-bf.sh
+++ tools/scripts/move-bf.sh
@@ -4,7 +4,7 @@
 source_file="curriculum/challenges/english/22-rosetta-code/rosetta-code-challenges/execute-brain.md"
 
 # Define the destination directory
-destination_dir="curriculum/challenges"
+destination_dir="curriculum"
 
 # Loop through all directories except _meta and english
 for dir in $(find "$destination_dir" -mindepth 1 -maxdepth 1 -type d ! -name '_meta' ! -name 'english'); do
