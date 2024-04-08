52532976f3881547eacbff51fbd30f74b6108406
Sem Bauke <semboot699@gmail.com>
2024-04-08 19:35:30 +0200

    refactor: remove component library as internal dependency (#54322)

--- tools/ui-components/src/image/image.types.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-  alt: string;
-  src: string;
-  /**
-   * Whether the image should be resized and contained within its parent.
-   */
-  responsive?: boolean;
-}
