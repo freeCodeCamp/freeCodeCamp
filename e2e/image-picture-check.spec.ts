c609129f3cffbc4977428488b50e064961ef2ade
Sem Bauke <semboot699@gmail.com>
2024-04-15 09:39:56 +0200

    Apply suggestions from code review
    Co-authored-by: Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>

--- e2e/image-picture-check.spec.ts
+++ e2e/image-picture-check.spec.ts
@@ -8,7 +8,7 @@ test.describe('Picture input field', () => {
   });
 
   test('Should be possible to type', async ({ page }) => {
-    const pictureInput = page.locator('#about-picture-input');
+    const pictureInput = page.getByLabel('Picture');
     await pictureInput.fill('');
     await pictureInput.fill('twaha');
     await expect(pictureInput).toHaveAttribute('value', 'twaha');
@@ -17,7 +17,7 @@ test.describe('Picture input field', () => {
   test('Show an error message if an incorrect url was submitted', async ({
     page
   }) => {
-    const pictureInput = page.locator('#about-picture-input');
+    const pictureInput = page.getByLabel('Picture');
     await pictureInput.fill('');
     await pictureInput.fill(
       'https://s3.amazonaws.com/freecodecamp/camper-image'
@@ -28,7 +28,7 @@ test.describe('Picture input field', () => {
   });
 
   test('Can submit a correct URL', async ({ page }) => {
-    const pictureInput = page.locator('#about-picture-input');
+    const pictureInput = page.getByLabel('Picture');
     await pictureInput.fill('');
     await pictureInput.fill(
       'https://s3.amazonaws.com/freecodecamp/camper-image-placeholder.png'
