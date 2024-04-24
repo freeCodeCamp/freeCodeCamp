f3cc5a2fab65cc9939c37903ee802c8d0deae8eb
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-04-24 15:34:52 +0200

    chore: stop skipping webkit

--- e2e/reset-modal.spec.ts
+++ e2e/reset-modal.spec.ts
@@ -2,13 +2,6 @@ import { test, expect } from '@playwright/test';
 
 import translations from '../client/i18n/locales/english/translations.json';
 
-test.beforeEach(({ browserName }) => {
-  test.skip(
-    browserName === 'webkit',
-    'Failing on webkit for no apparent reason. Can not reproduce locally.'
-  );
-});
-
 test('should render the modal content correctly', async ({ page }) => {
   await page.goto(
     '/learn/2022/responsive-web-design/learn-html-by-building-a-cat-photo-app/step-2'
