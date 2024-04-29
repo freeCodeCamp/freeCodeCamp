951ed8a4c8883491f32576b18d9cadfcc09b9cfb
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2024-04-29 23:23:36 +0300

    fix: map progress ui

--- client/src/assets/icons/completion-ribbon.tsx
+++ client/src/assets/icons/completion-ribbon.tsx
@@ -7,34 +7,27 @@ interface RibbonProps {
 }
 
 export const Arrow = () => (
-  <svg width={70} height={40} xmlns='http://www.w3.org/2000/svg'>
-    <line
-      x1={50}
-      y1={0}
-      x2={50}
-      y2={35}
-      stroke='black'
-      strokeWidth={3}
-      strokeDasharray='6,1.3'
-      className='map-arrow-icon'
+  <svg
+    width='14'
+    height='35'
+    viewBox='0 0 14 35'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <g filter='url(#filter0_d_108_7)'>
+      <path
+        d='M7 0V30'
+        stroke='var(--secondary-color)'
+        strokeDasharray='6 1.3'
+      />
+    </g>
+    <path
+      d='M7.71803 30.3038L1.57973 24.1655L0.87262 24.8726L7.01092 31.0109L7.71803 30.3038Z'
+      fill='var(--secondary-color)'
     />
-    <rect
-      x={36}
-      y={35}
-      width={15}
-      height={2}
-      fill='black'
-      transform='rotate(45, 50, 36)'
-      className='map-arrow-icon'
-    />
-    <rect
-      x={49}
-      y={35}
-      width={15}
-      height={2}
-      fill='black'
-      transform='rotate(-45, 50, 36)'
-      className='map-arrow-icon'
+    <path
+      d='M12.4658 24.12L6.29289 30.2929L7 31L13.1729 24.8271L12.4658 24.12Z'
+      fill='var(--secondary-color)'
     />
   </svg>
 );
@@ -45,46 +38,56 @@ export const RibbonIcon = ({
   isClaimed
 }: RibbonProps): JSX.Element => {
   const properClassName = completed ? 'completeIcon' : 'incompleteIcon';
-  const fillColor = completed ? 'black' : 'gray';
+  const textColor = completed
+    ? 'var(--secondary-background)'
+    : 'var(--secondary-color)';
+
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='75%'
-      height='75%'
+      width='45'
+      height='51'
       viewBox='0 0 45 50'
       fill='none'
+      xmlns='http://www.w3.org/2000/svg'
       className={properClassName}
-      aria-hidden='true'
     >
       {isClaimed && (
         <>
           <path
             d='M25 35.3418L35.4851 28L44.5957 41.0113L36.2658 39.7151L34.1106 48.353L25 35.3418Z'
-            className='map-icon'
+            fill='var(--secondary-color)'
           />
           <path
             d='M9.11059 29L19.5957 36.3418L10.4851 49.353L8.85418 41.0821L-4.67677e-07 42.0113L9.11059 29Z'
-            className='map-icon'
+            fill='var(--secondary-color)'
           />
         </>
       )}
 
-      <circle cx={21.9999} cy={21} r={20} fill={fillColor} />
       <circle
-        cx={22.5}
-        cy={21}
-        r={17.5}
-        fill={fillColor}
-        stroke='white'
-        strokeWidth={2}
+        cx='22'
+        cy='21'
+        r='20'
+        stroke='var(--secondary-color)'
+        strokeWidth='3'
       />
+      {completed && (
+        <circle
+          cx='22'
+          cy='21'
+          r='17.5'
+          fill='var(--secondary-color)'
+          stroke='var(--secondary-background)'
+          strokeWidth='3'
+        />
+      )}
       <text
-        x='50%'
-        y='50%'
-        fontFamily='Verdana'
-        color={fillColor}
+        x='48%'
+        y='40%'
+        fontFamily='lato'
+        color={textColor}
         fontSize='1.0rem'
-        fill='#fff'
+        fill={textColor}
         textAnchor='middle'
         alignmentBaseline='central'
       >
