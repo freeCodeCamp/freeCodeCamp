83741d12006746109c74ef98a86d15d89893148d
sembauke <semboot699@gmail.com>
2024-04-30 09:44:17 +0200

    fix: is this the real life or is this just fantasy?

--- client/src/components/profile/components/portfolio-projects.tsx
+++ client/src/components/profile/components/portfolio-projects.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import type { PortfolioProjectData } from '../../../redux/prop-types';
 import './portfolio-projects.css';
+import { FullWidthRow } from '../../helpers';
 
 interface PortfolioProjectsProps {
   portfolioProjects: PortfolioProjectData[];
@@ -17,50 +18,50 @@ export const PortfolioProjects = ({
     return null;
   }
   return (
-    <section aria-labelledby='portfolio-projects-header'>
-      <h2 id='portfolio-projects-header' className='text-center'>
-        {t('profile.projects')}
-      </h2>
-      <div className='portfolio-field'>
-        {portfolioProjects.map(({ title, url, image, description, id }) => (
-          <article className='portfolio-card' key={id}>
-            <img
-              alt={`Screenshot of project: ${title}`}
-              className='portfolio-card-image'
-              src={image}
-              onError={({ currentTarget }) => {
-                currentTarget.alt = 'Default project image not found';
-                currentTarget.src =
-                  'https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png';
-              }}
-            />
-            <div className='portfolio-card-description'>
-              <h3>{title}</h3>
-              <p className='portfolio-description-text'>{description}</p>
-              <div className='portfolio-button-container'>
-                <a
-                  href={url}
-                  rel='nofollow noopener noreferrer'
-                  target='_blank'
-                  className='portfolio-card-button'
-                  aria-label={`View ${title} (opens in a new window)`}
-                >
-                  {t('buttons.view')}
-                  <span className='sr-only'>
-                    {title}, {t('aria.opens-new-window')}
-                  </span>
-                  <FontAwesomeIcon
-                    id='link-icon'
-                    icon={faArrowUpRightFromSquare}
-                    aria-hidden='true'
-                  />
-                </a>
+    <FullWidthRow>
+      <h2 id='portfolio-projects-header'>{t('profile.projects')}</h2>
+      <section>
+        <div className='portfolio-field'>
+          {portfolioProjects.map(({ title, url, image, description, id }) => (
+            <article className='portfolio-card' key={id}>
+              <img
+                alt={`Screenshot of project: ${title}`}
+                className='portfolio-card-image'
+                src={image}
+                onError={({ currentTarget }) => {
+                  currentTarget.alt = 'Default project image not found';
+                  currentTarget.src =
+                    'https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png';
+                }}
+              />
+              <div className='portfolio-card-description'>
+                <h3>{title}</h3>
+                <p className='portfolio-description-text'>{description}</p>
+                <div className='portfolio-button-container'>
+                  <a
+                    href={url}
+                    rel='nofollow noopener noreferrer'
+                    target='_blank'
+                    className='portfolio-card-button'
+                    aria-label={`View ${title} (opens in a new window)`}
+                  >
+                    {t('buttons.view')}
+                    <span className='sr-only'>
+                      {title}, {t('aria.opens-new-window')}
+                    </span>
+                    <FontAwesomeIcon
+                      id='link-icon'
+                      icon={faArrowUpRightFromSquare}
+                      aria-hidden='true'
+                    />
+                  </a>
+                </div>
               </div>
-            </div>
-          </article>
-        ))}
-        <hr />
-      </div>
-    </section>
+            </article>
+          ))}
+          <hr />
+        </div>
+      </section>
+    </FullWidthRow>
   );
 };
