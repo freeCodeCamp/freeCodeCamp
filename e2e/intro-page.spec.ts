16262638083c56f2549dfd4c80099e71659dfabb
sembauke <semboot699@gmail.com>
2024-05-03 09:22:27 +0200

    fix: use right locator

--- e2e/intro-page.spec.ts
+++ e2e/intro-page.spec.ts
@@ -4,8 +4,8 @@ test.describe('Certification intro page', () => {
   test('Should render', async ({ page }) => {
     await page.goto('/learn/coding-interview-prep');
     await expect(
-      page.locator(
-        "text=If you're looking for free coding exercises to prepare for your next job interview, we've got you covered."
+      page.getByText(
+        "If you're looking for free coding exercises to prepare for your next job interview, we've got you covered."
       )
     ).toBeVisible();
     await expect(page).toHaveTitle('Coding Interview Prep | freeCodeCamp.org');
