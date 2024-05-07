6016c181d444d873fbd2929a4ea3992ed48510e6
Sem Bauke <semboot699@gmail.com>
2024-05-07 08:01:18 +0200

    feat: convert Sass test to Playwright test (#54618)

---
+++ e2e/sass.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Sass Challenge', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(
+      '/learn/front-end-development-libraries/sass/use-for-to-create-a-sass-loop'
+    );
+  });
+
+  test('should render the sass preview', async ({ page }) => {
+    const frame = page.frameLocator('.challenge-preview iframe');
+    expect(frame).not.toBeNull();
+
+    await expect(frame.locator('.text-1')).toBeVisible();
+    await expect(frame.locator('.text-2')).toBeVisible();
+    await expect(frame.locator('.text-3')).toBeVisible();
+    await expect(frame.locator('.text-4')).toBeVisible();
+    await expect(frame.locator('.text-5')).toBeVisible();
+  });
+});
