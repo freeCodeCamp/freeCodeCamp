895747dff87dd115d1803af20a9da766a3511e69
Niraj Nandish <nirajnandish@icloud.com>
2024-05-07 15:59:47 +0530

    chore: fix type errors in test file

--- e2e/mobile/mobile-learn.spec.ts
+++ e2e/mobile/mobile-learn.spec.ts
@@ -3,6 +3,23 @@ import { expect, test } from '@playwright/test';
 import currData from '../../shared/config/curriculum.json';
 import { orderedSuperBlockInfo } from '../../tools/scripts/build/build-external-curricula-data';
 
+interface Curriculum {
+  [key: string]: {
+    blocks: {
+      [key: string]: {
+        challenges: {
+          id: string;
+          title: string;
+          block: string;
+        }[];
+        meta: {
+          name: string;
+        };
+      };
+    };
+  };
+}
+
 const nonEditorSB = [
   'python-for-everybody',
   'data-analysis-with-python',
@@ -13,15 +30,18 @@ const publicSB = orderedSuperBlockInfo
   .filter(sb => sb.public === true && !nonEditorSB.includes(sb.dashedName))
   .map(sb => sb.dashedName);
 
+const typedCurriculum = currData as Curriculum;
+
 test.describe('Test challenges in mobile', () => {
   for (const superBlock of publicSB) {
-    for (const currBlock of Object.values(currData[superBlock]['blocks'])) {
+    for (const currBlock of Object.values(
+      typedCurriculum[superBlock]['blocks']
+    )) {
       test.describe(`SuperBlock: ${superBlock} - Block: ${currBlock['meta']['name']}`, () => {
         for (const currChallenge of currBlock['challenges']) {
           test(`Challenge: ${currChallenge['title']}(${currChallenge['id']})`, async ({
             page
           }) => {
-            // const consoleMsgPromise = page.waitForEvent('console');
             const logMsges: string[] = [];
             page.on('console', msg => {
               logMsges.push(msg.text());
