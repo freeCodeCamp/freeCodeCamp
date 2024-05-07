650e6d9cab61b087114c8c70bc12a90150af13fa
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2024-05-07 15:30:50 +0300

    fix: clean up

--- client/src/components/profile/components/portfolio-projects.tsx
+++ client/src/components/profile/components/portfolio-projects.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import type { PortfolioProjectData } from '../../../redux/prop-types';
 import './portfolio-projects.css';
 import { FullWidthRow } from '../../helpers';
@@ -19,9 +17,18 @@ export const PortfolioProjects = ({
   }
   return (
     <FullWidthRow>
-      <h2>{t('profile.projects')}</h2>
+      <h2 className='text-center'>{t('profile.projects')}</h2>
       {portfolioProjects.map(({ title, url, image, description, id }) => (
-        <div className='portfolio-card' key={id}>
+        <a
+          href={url}
+          rel='nofollow noopener noreferrer'
+          target='_blank'
+          className='portfolio-card'
+          key={id}
+        >
+          <span className='sr-only'>
+            {title}, {t('aria.opens-new-window')}
+          </span>
           {image && (
             <img
               alt=''
@@ -38,25 +45,8 @@ export const PortfolioProjects = ({
               <h3>{title}</h3>
               <p>{description}</p>
             </div>
-            <div className='portfolio-button-container'>
-              <a
-                href={url}
-                rel='nofollow noopener noreferrer'
-                target='_blank'
-                className='portfolio-card-button'
-              >
-                {t('buttons.view')}
-                <span className='sr-only'>
-                  {title}, {t('aria.opens-new-window')}
-                </span>
-                <FontAwesomeIcon
-                  id='link-icon'
-                  icon={faArrowUpRightFromSquare}
-                />
-              </a>
-            </div>
           </div>
-        </div>
+        </a>
       ))}
       <hr />
     </FullWidthRow>
