b373027828fac713d062c46564e1d7ca2af9b41a
sembauke <semboot699@gmail.com>
2024-05-09 10:34:31 +0200

    feat: check for flash message

--- e2e/report-user.spec.ts
+++ e2e/report-user.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 test.use({ storageState: 'playwright/.auth/certified-user.json' });
 
-//TODO: run email server?
+// To run this test you will need to run the email server.
+// To do this: https://contribute.freecodecamp.org/#/how-to-catch-outgoing-emails-locally?id=using-mailhog
 
 test('should be possible to report a user from their profile page', async ({
   page
@@ -9,7 +10,7 @@ test('should be possible to report a user from their profile page', async ({
   await page.goto('/twaha');
 
   // The following line is required if you're running the test in local development
-  // await page.getByRole('button', { name: 'Preview custom 404 page' }).click();
+  await page.getByRole('button', { name: 'Preview custom 404 page' }).click();
 
   await page.getByText("Flag This User's Account for Abuse").click();
 
@@ -20,4 +21,9 @@ test('should be possible to report a user from their profile page', async ({
   await page.getByRole('textbox').nth(1).fill('Some details');
   await page.getByRole('button', { name: 'Submit the report' }).click();
   await expect(page).toHaveURL('/learn');
+
+  await expect(page.getByTestId('flash-message')).toBeVisible();
+  await expect(page.getByTestId('flash-message')).toContainText(
+    'A report was sent to the team with foo@bar.com in copy'
+  );
 });
