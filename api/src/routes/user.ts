718aa6676503b807a228b0647ee3b74f02171f42
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-05-14 16:52:19 +0200

    feat: return public user properties

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,6 +1,6 @@
 import { type FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
 import { ObjectId } from 'mongodb';
-import _ from 'lodash';
+import { omit, pick } from 'lodash';
 
 import * as schemas from '../schemas';
 // Loopback creates a 64 character string for the user id, this customizes
@@ -23,6 +23,7 @@ import { encodeUserToken } from '../utils/tokens';
 import { trimTags } from '../utils/validation';
 import { generateReportEmail } from '../utils/email-templates';
 import { createResetProperties } from '../utils/create-user';
+import { challengeTypes } from '../../dist/shared/config/challenge-types';
 
 // user flags that the api-server returns as false if they're missing in the
 // user document. Since Prisma returns null for missing fields, we need to
@@ -542,13 +543,15 @@ export const userGetRoutes: FastifyPluginCallbackTypebox = (
           void res.code(500);
           return { user: {}, result: '' };
         }
+        // TODO: DRY this (the creation of the response body) and
+        // get-public-profile's response body creation.
 
         const encodedToken = userToken
           ? encodeUserToken(userToken.id)
           : undefined;
 
-        const flags = _.pick<typeof user, NullableFlag>(user, nullableFlags);
-        const rest = _.omit<typeof user, NullableFlag>(user, nullableFlags);
+        const flags = pick<typeof user, NullableFlag>(user, nullableFlags);
+        const rest = omit<typeof user, NullableFlag>(user, nullableFlags);
 
         const {
           username,
@@ -629,16 +632,37 @@ export const userPublicGetRoutes: FastifyPluginCallbackTypebox = (
       // TODO(Post-MVP): look for duplicates unless we can make username unique in the db.
       const user = await fastify.prisma.user.findFirst({
         where: { username: req.query.username }
+        // TODO: only select desired fields, then stop 'omit'ing the undesired
+        // ones.
       });
+
+      console.log('user', user);
 
       if (!user) {
         void reply.code(404);
         return reply.send({});
       }
 
-      const profileUI = normalizeProfileUI(user.profileUI);
-      if (profileUI.isLocked) {
-        void reply.code(200);
+      const publicUser = omit<typeof user, keyof typeof user>(user, [
+        'currentChallengeId',
+        'email',
+        'emailVerified',
+        'sendQuincyEmail',
+        'theme',
+        'keyboardShortcuts',
+        'acceptedPrivacyTerms',
+        'progressTimestamps',
+        'unsubscribeId',
+        'donationEmails',
+        'externalId',
+        'usernameDisplay',
+        'isBanned' // TODO: should this be omitted?
+      ]);
+
+      const normalizedProfileUI = normalizeProfileUI(user.profileUI);
+
+      void reply.code(200);
+      if (normalizedProfileUI.isLocked) {
         return reply.send({
           // TODO(Post-MVP): just return isLocked and an empty profileUI. No
           // need for entities or result.
@@ -646,8 +670,56 @@ export const userPublicGetRoutes: FastifyPluginCallbackTypebox = (
             user: {
               [user.username]: {
                 isLocked: true,
-                profileUI,
+                profileUI: normalizedProfileUI,
                 username: user.username
+              }
+            }
+          },
+          result: user.username
+        });
+      } else {
+        const normalizedChallenges = normalizedProfileUI.showTimeLine
+          ? normalizeChallenges(user.completedChallenges)
+          : [];
+        return reply.send({
+          entities: {
+            user: {
+              [user.username]: {
+                ...removeNulls(publicUser),
+                about: normalizedProfileUI.showAbout ? user.about : '',
+                calendar: normalizedProfileUI.showHeatMap
+                  ? getCalendar(
+                      user.progressTimestamps as ProgressTimestamp[] | null
+                    )
+                  : {},
+                completedChallenges: normalizedProfileUI.showCerts
+                  ? normalizedChallenges
+                  : normalizedChallenges.filter(
+                      ({ challengeType }) =>
+                        challengeType !== challengeTypes.step // AKA certifications
+                    ),
+                isDonating: normalizedProfileUI.showDonation
+                  ? user.isDonating
+                  : null,
+                joinDate: normalizedProfileUI.showAbout
+                  ? new ObjectId(user.id).getTimestamp().toISOString()
+                  : '',
+                location: normalizedProfileUI.showLocation ? user.location : '',
+                name: normalizedProfileUI.showName ? user.name : '',
+                points: normalizedProfileUI.showPoints
+                  ? getPoints(
+                      user.progressTimestamps as ProgressTimestamp[] | null
+                    )
+                  : 0,
+                portfolio: normalizedProfileUI.showPortfolio
+                  ? user.portfolio
+                  : [],
+                profileUI: normalizedProfileUI,
+                // TODO: should this always be returned? Shouldn't some privacy
+                // setting control it? Same applies to website, githubProfile,
+                // and linkedin.
+                twitter: normalizeTwitter(user.twitter),
+                yearsTopContributor: user.yearsTopContributor
               }
             }
           },
--- api/src/schemas/api/users/get-public-profile.ts
+++ api/src/schemas/api/users/get-public-profile.ts
@@ -6,19 +6,23 @@ export const getPublicProfile = {
     username: Type.String({ minLength: 1 })
   }),
   response: {
-    200: Type.Object({
-      entities: Type.Object({
-        user: Type.Record(
-          Type.String(),
-          Type.Object({
-            isLocked: Type.Boolean(),
-            profileUI,
-            username: Type.String()
-          })
-        )
+    200: Type.Union([
+      Type.Object({
+        entities: Type.Object({
+          user: Type.Record(
+            Type.String(),
+            Type.Object({
+              isLocked: Type.Boolean(),
+              profileUI,
+              username: Type.String()
+            })
+          )
+        }),
+        result: Type.String()
       }),
-      result: Type.String()
-    }),
+      // TODO: replace Any with real type.
+      Type.Object({ entities: Type.Any(), result: Type.String() })
+    ]),
     // We can't simply have Type.Object({}), even though that's correct, because
     // TypeScript will then accept all responses (since every object can be
     // assigned to {})
