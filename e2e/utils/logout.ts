a170d9f93677c70995b9380f96aa959b6c5ad2f7
Sem Bauke <semboot699@gmail.com>
2024-05-16 08:43:50 +0200

    Apply suggestions from code review
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- e2e/utils/logout.ts
+++ e2e/utils/logout.ts
@@ -1,13 +1,5 @@
-import { Page, expect } from '@playwright/test';
+import { Page } from '@playwright/test';
 
 export async function signout(page: Page) {
-  await page.goto('/');
-
-  const cookies = await page.context().cookies();
-  for (const cookie of cookies) {
-    await page.context().clearCookies(cookie);
-  }
-
-  await page.reload();
-  await expect(page.getByRole('link', { name: 'Sign in' })).toBeVisible();
+  await page.context().clearCookies({ name: 'jwt_access_token' });
 }
