47540d37c42386c6327ad7e3f555b46c77f566ea
sembauke <semboot699@gmail.com>
2024-05-16 08:44:58 +0200

    chore: remove signout spec

--- e2e/signout.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from '@playwright/test';
-import { signout } from './utils/logout';
-test.use({ storageState: 'playwright/.auth/certified-user.json' });
-test.describe('signing out', () => {
-  test('should have the ability singout', async ({ page }) => {
-    await signout(page);
-  });
-});
