8e2a3a47705572a4c40af6d5e307eacc0913d888
sembauke <semboot699@gmail.com>
2024-05-20 17:04:30 +0200

    feat: add useful comment on how to run the particular test

--- e2e/heading-challenge.spec.ts
+++ e2e/heading-challenge.spec.ts
@@ -1,5 +1,8 @@
 import test, { expect } from '@playwright/test';
 
+// To run this test locally you will need to run: pnpm run start-ci;
+// Also, make sure that you have pm2 installed globally via: pnpm install -g pm2
+
 test('Header to heading element redirect', async ({ page }) => {
   const basePath = '/learn/responsive-web-design/applied-visual-design';
 
