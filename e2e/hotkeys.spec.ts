14f6c46a1294a8ade0af7b50d63851d684dc5a04
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-05-29 11:50:43 -0700

    Apply suggestions from code review

--- e2e/hotkeys.spec.ts
+++ e2e/hotkeys.spec.ts
@@ -110,7 +110,7 @@ test('User can use shortcuts to navigate between frontend projects', async ({
   await page.waitForURL(links.frontEnd1);
 });
 
-test('User can use shortcuts to navigate backend projects', async ({
+test('User can use shortcuts to navigate between backend projects', async ({
   page
 }) => {
   await page.goto(links.backEnd1);
@@ -125,7 +125,7 @@ test('User can use shortcuts to navigate backend projects', async ({
   await page.waitForURL(links.backEnd1);
 });
 
-test('User can use shortcuts to navigate videos', async ({ page }) => {
+test('User can use shortcuts to navigate between video-based challenges', async ({ page }) => {
   await page.goto(links.video1);
   await expect(page.locator('#editor-layout')).toBeFocused();
   await page.keyboard.press('Escape');
