aadba3af53850017fba50ddeddd67af6822f9b94
sembauke <semboot699@gmail.com>
2024-05-31 10:52:24 +0200

    feat: convert thirdparty donation tests to Playwright

---
+++ e2e/third-party-donation.spec.ts
@@ -0,0 +1,101 @@
+import { expect, test } from '@playwright/test';
+
+// In order to run this test you will need a couple of keys
+// Meaning, Paypal, Stripe and Patreon.
+
+test.describe('third-party donation tests', () => {
+  test.use({ storageState: 'playwright/.auth/certified-user.json' });
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/donate');
+  });
+
+  test('Should have a donation button', async ({ page }) => {
+    await expect(
+      page.getByRole('button', { name: 'Donate', exact: true })
+    ).toBeVisible();
+  });
+
+  test('The donation widget should have four donation options', async ({
+    page
+  }) => {
+    await expect(page.locator('#radix-4-trigger-500')).toBeVisible();
+    await expect(page.locator('#radix-4-trigger-1000')).toBeVisible();
+    await expect(page.locator('#radix-4-trigger-2000')).toBeVisible();
+    await expect(page.locator('#radix-4-trigger-4000')).toBeVisible();
+  });
+
+  test('The donation widget should let you know what you"ll support per donation value', async ({
+    page
+  }) => {
+    await page.locator('#radix-4-trigger-500').click();
+    await expect(page.getByTestId('donation-tier-selector')).toContainText(
+      'Confirm your donation of $5 / month:'
+    );
+    await expect(page.locator('.tab-content').nth(0)).toContainText(
+      'Your $5 donation will provide 250 hours of learning to people around the world each month.'
+    );
+
+    await page.locator('#radix-4-trigger-1000').click();
+    await expect(page.getByTestId('donation-tier-selector')).toContainText(
+      'Confirm your donation of $10 / month:'
+    );
+    await expect(page.locator('.tab-content').nth(1)).toContainText(
+      'Your $10 donation will provide 500 hours of learning to people around the world each month.'
+    );
+
+    await page.locator('#radix-4-trigger-2000').click();
+    await expect(page.getByTestId('donation-tier-selector')).toContainText(
+      'Confirm your donation of $20 / month:'
+    );
+    await expect(page.locator('.tab-content').nth(2)).toContainText(
+      'Your $20 donation will provide 1,000 hours of learning to people around the world each month.'
+    );
+
+    await page.locator('#radix-4-trigger-4000').click();
+    await expect(page.getByTestId('donation-tier-selector')).toContainText(
+      'Confirm your donation of $40 / month:'
+    );
+    await expect(page.locator('.tab-content').nth(3)).toContainText(
+      'Your $40 donation will provide 2,000 hours of learning to people around the world each month.'
+    );
+  });
+
+  test('When the donation button is clicked it should tell you that it is secure', async ({
+    page
+  }) => {
+    await page.getByRole('button', { name: 'Donate', exact: true }).click();
+
+    await expect(page.getByText(' Secure donation')).toBeVisible();
+  });
+
+  test('It should have a Paypal and Patreon button', async ({ page }) => {
+    await page.getByRole('button', { name: 'Donate', exact: true }).click();
+    await expect(
+      page.locator('[data-paypal-smart-button-version="5.0.440"]')
+    ).toBeVisible();
+    await expect(
+      page.locator('[data-patreon-widget-type="become-patron-button"]')
+    ).toBeVisible();
+  });
+
+  test('It is possible to donate with a card', async ({ page }) => {
+    await page.getByRole('button', { name: 'Donate', exact: true }).click();
+    await expect(page.getByText('Or donate with card')).toBeVisible();
+
+    const cardNumberIframe = page
+      .frameLocator('iframe[src*="elements-inner-card"]')
+      .nth(0);
+
+    const cardExpiryIframe = page
+      .frameLocator('iframe[src*="elements-inner-card"]')
+      .nth(1);
+
+    await cardNumberIframe
+      .locator('input[data-elements-stable-field-name="cardNumber"]')
+      .fill('4242424242424242');
+
+    await cardExpiryIframe
+      .locator('input[data-elements-stable-field-name="cardExpiry"]')
+      .fill('1025');
+  });
+});
