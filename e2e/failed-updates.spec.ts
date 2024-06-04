533f98a6a61e6d194fa25171a07b62f95ee07d7c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-06-04 12:47:50 +0200

    test: update the store in the page's context

--- e2e/failed-updates.spec.ts
+++ e2e/failed-updates.spec.ts
@@ -1,6 +1,5 @@
 import { execSync } from 'child_process';
 import { test, expect } from '@playwright/test';
-import store from 'store';
 import { ChallengeData } from '../tools/challenge-editor/api/interfaces/challenge-data';
 
 const failedUpdates = [
@@ -15,7 +14,7 @@ const failedUpdates = [
     id: 'ea289e2f-a5d2-45e0-b795-0f9f4afc5124'
   }
 ];
-const failedUpdatesKey = 'fcc-failed-updates';
+const storeKey = 'fcc-failed-updates';
 
 function getCompletedIds(completedChallenges: ChallengeData[]): string[] {
   return completedChallenges.map((challenge: ChallengeData) => challenge.id);
@@ -31,47 +30,56 @@ test.afterAll(() => {
 });
 
 test.describe('failed update flushing', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('should resubmit failed updates, check they are stored, then flush', async ({
-    page
+  test('should resubmit failed updates to the api and clear the store', async ({
+    page,
+    request
   }) => {
-    store.set(failedUpdatesKey, failedUpdates);
-
-    const response = await page.request.get(
+    // Initially, the user has no completed challenges.
+    const userRes = await request.get(
       'http://localhost:3000/user/get-session-user'
     );
-    const body = await response.json();
-    const completedChallenges = body.user.developmentuser.completedChallenges;
+    const completedChallenges = (await userRes.json()).user.developmentuser
+      .completedChallenges;
     expect(completedChallenges).toEqual([]);
 
-    await page.route(
-      'http://localhost:3000/modern-challenge-completed',
-      async route => {
-        await route.continue();
-      }
-    );
-
-    expect(store.get(failedUpdatesKey)).toEqual(failedUpdates);
+    // It's necessary to wait until the page has loaded before setting the
+    // localStorage. Otherwise, evaluate fails with a permissions error (the
+    // store doesn't exist yet).
     await page.goto('/');
-    await page.waitForResponse(
-      'http://localhost:3000/modern-challenge-completed'
+    await page.evaluate(
+      ([failedUpdates, storeKey]) => {
+        localStorage.setItem(storeKey, JSON.stringify(failedUpdates));
+      },
+      [failedUpdates, storeKey] as const
     );
 
-    const updatedResponse = await page.request.get(
+    // The update epic sends two requests and this lets us wait for both.
+    const submitRes = page
+      .waitForResponse('http://localhost:3000/modern-challenge-completed')
+      .then(() =>
+        page.waitForResponse('http://localhost:3000/modern-challenge-completed')
+      );
+
+    await page.reload();
+    await submitRes;
+
+    const updatedUserRes = await request.get(
       'http://localhost:3000/user/get-session-user'
     );
-    const updatedBody = await updatedResponse.json();
-    const updatedCompletedChallenges =
-      updatedBody.user.developmentuser.completedChallenges;
+
+    // Now the user should have both completed challenges.
+    const updatedCompletedChallenges = (await updatedUserRes.json()).user
+      .developmentuser.completedChallenges;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     const completedIds = getCompletedIds(updatedCompletedChallenges);
 
-    for (const failedUpdate of failedUpdates) {
-      expect(completedIds).toContain(failedUpdate.payload.id);
+    for (const { payload } of failedUpdates) {
+      expect(completedIds).toContain(payload.id);
     }
-    expect(store.get(failedUpdatesKey)).toEqual([]);
+    const storedFailedUpdates = await page.evaluate(storeKey => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return JSON.parse(localStorage.getItem(storeKey) ?? '');
+    }, storeKey);
+    expect(storedFailedUpdates).toEqual([]);
   });
 });
