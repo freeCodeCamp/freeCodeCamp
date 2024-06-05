e2b331332108da26dc14ca624df82e934514a3c2
Asad Rahim <asad.rahim@arcticwolf.com>
2024-06-05 17:53:58 +0000

    typos

--- git_synch.sh
+++ git_synch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SOURCE_REPO=freeCodeCamp
-DST_REPO=FreeCodeCamp
+DST_REPO=FreeCodeCampCopy
 SOURCE_REPO_DIR=/home/runner/work/freeCodeCamp/${SOURCE_REPO}
-DST_REPO_DIR=/home/runner/work/freeCodeCamp/${DST_REPO}
+DST_REPO_DIR=/home/runner/work/FreeCodeCampCopy/${DST_REPO}
 TMP_REPO_DIR=/tmp/temp_repo
 
 rm -rf "${TMP_REPO_DIR}" 
