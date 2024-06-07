32eaa907c2a31a354e310c6d4e68338c91dad62e
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-06-07 14:30:57 +0700

    refactor: move search input query to a function

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -1,5 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 import translations from '../client/i18n/locales/english/translations.json';
+
+const getSearchInput = async ({
+  page,
+  isMobile
+}: {
+  page: Page;
+  isMobile: boolean;
+}) => {
+  if (isMobile) {
+    const menuButton = page.getByRole('button', {
+      name: translations.buttons.menu
+    });
+    await expect(menuButton).toBeVisible();
+    await menuButton.click();
+  }
+
+  return page.getByLabel('Search');
+};
 
 test.describe('Search', () => {
   test.beforeEach(async ({ page }) => {
@@ -7,29 +25,13 @@ test.describe('Search', () => {
   });
 
   test('should display correct placeholder', async ({ page, isMobile }) => {
-    if (isMobile) {
-      const menuButton = page.getByRole('button', {
-        name: translations.buttons.menu
-      });
-      await expect(menuButton).toBeVisible();
-      await menuButton.click();
+    const searchInput = await getSearchInput({ page, isMobile });
 
-      const searchInput = page.getByLabel(translations.search.label);
-
-      await expect(searchInput).toBeVisible();
-      await expect(searchInput).toHaveAttribute(
-        'placeholder',
-        'Search 9,000+ tutorials'
-      );
-    } else {
-      const searchInput = page.getByLabel(translations.search.label);
-
-      await expect(searchInput).toBeVisible();
-      await expect(searchInput).toHaveAttribute(
-        'placeholder',
-        'Search 10,700+ tutorials'
-      );
-    }
+    await expect(searchInput).toBeVisible();
+    await expect(searchInput).toHaveAttribute(
+      'placeholder',
+      'Search 10,700+ tutorials'
+    );
   });
 
   test('should return the search results when the user presses Enter', async ({
@@ -37,40 +39,19 @@ test.describe('Search', () => {
     page,
     isMobile
   }) => {
-    if (isMobile) {
-      const menuButton = page.getByRole('button', {
-        name: translations.buttons.menu
-      });
-      await menuButton.click();
+    const searchInput = await getSearchInput({ page, isMobile });
+    await expect(searchInput).toBeVisible();
 
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
+    await searchInput.fill('test');
+    await page.keyboard.press('Enter');
 
-      await searchInput.fill('test');
-      await page.keyboard.press('Enter');
+    // Wait for the new page to load.
+    const newPage = await context.waitForEvent('page');
+    await newPage.waitForLoadState();
 
-      // Wait for the new page to load.
-      const newPage = await context.waitForEvent('page');
-      await newPage.waitForLoadState();
-
-      expect(newPage.url()).toBe(
-        'https://www.freecodecamp.org/news/search/?query=test'
-      );
-    } else {
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
-
-      await searchInput.fill('test');
-      await page.keyboard.press('Enter');
-
-      // Wait for the new page to load.
-      const newPage = await context.waitForEvent('page');
-      await newPage.waitForLoadState();
-
-      expect(newPage.url()).toBe(
-        'https://www.freecodecamp.org/news/search/?query=test'
-      );
-    }
+    expect(newPage.url()).toBe(
+      'https://www.freecodecamp.org/news/search/?query=test'
+    );
   });
 
   test('should return the search results when the user clicks the search button', async ({
@@ -78,75 +59,35 @@ test.describe('Search', () => {
     page,
     isMobile
   }) => {
-    if (isMobile) {
-      const menuButton = page.getByRole('button', {
-        name: translations.buttons.menu
-      });
-      await menuButton.click();
+    const searchInput = await getSearchInput({ page, isMobile });
+    await expect(searchInput).toBeVisible();
 
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
+    await searchInput.fill('test');
+    await page
+      .getByRole('button', { name: translations.icons.magnifier })
+      .click();
 
-      await searchInput.fill('test');
-      await page
-        .getByRole('button', { name: translations.icons.magnifier })
-        .click();
+    // Wait for the new page to load.
+    const newPage = await context.waitForEvent('page');
+    await newPage.waitForLoadState();
 
-      // Wait for the new page to load.
-      const newPage = await context.waitForEvent('page');
-      await newPage.waitForLoadState();
-
-      expect(newPage.url()).toBe(
-        'https://www.freecodecamp.org/news/search/?query=test'
-      );
-    } else {
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
-
-      await searchInput.fill('test');
-      await page
-        .getByRole('button', { name: translations.icons.magnifier })
-        .click();
-
-      // Wait for the new page to load.
-      const newPage = await context.waitForEvent('page');
-      await newPage.waitForLoadState();
-
-      expect(newPage.url()).toBe(
-        'https://www.freecodecamp.org/news/search/?query=test'
-      );
-    }
+    expect(newPage.url()).toBe(
+      'https://www.freecodecamp.org/news/search/?query=test'
+    );
   });
 
   test('should clear the search input when the user clicks the clear button', async ({
     page,
     isMobile
   }) => {
-    if (isMobile) {
-      const menuButton = page.getByRole('button', {
-        name: translations.buttons.menu
-      });
-      await menuButton.click();
+    const searchInput = await getSearchInput({ page, isMobile });
+    await expect(searchInput).toBeVisible();
 
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
+    await searchInput.fill('test');
+    await page
+      .getByRole('button', { name: translations.icons['input-reset'] })
+      .click();
 
-      await searchInput.fill('test');
-      await page
-        .getByRole('button', { name: translations.icons['input-reset'] })
-        .click();
-
-      await expect(searchInput).toHaveValue('');
-    } else {
-      const searchInput = page.getByLabel(translations.search.label);
-      await expect(searchInput).toBeVisible();
-
-      await searchInput.fill('test');
-      await page
-        .getByRole('button', { name: translations.icons['input-reset'] })
-        .click();
-
-      await expect(searchInput).toHaveValue('');
-    }
+    await expect(searchInput).toHaveValue('');
   });
 });
