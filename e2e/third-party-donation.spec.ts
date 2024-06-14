1769126a1b5174037a58f43373b8028a2270a33d
sembauke <semboot699@gmail.com>
2024-06-14 09:19:43 +0200

    fix: test if all widgets are present with keys

--- e2e/third-party-donation.spec.ts
+++ e2e/third-party-donation.spec.ts
@@ -63,20 +63,48 @@ test.describe('third-party donation tests', () => {
     await expect(page.getByText('Secure donation')).toBeVisible();
   });
 
+  test('All elements are present in the widget', async ({ page }) => {
+    await page.getByRole('button', { name: 'Donate', exact: true }).click();
+
+    // Paypal button should be present
+
+    const paypalButtonIframe = page.frameLocator('.component-frame');
+    const paypalButton = paypalButtonIframe.getByRole('link');
+    await expect(paypalButton).toBeVisible();
+    await expect(paypalButton).toHaveAttribute('aria-label', 'PayPal');
+
+    // Patreon button should be present
+
+    const patreonButton = page.locator('.patreon-button');
+    await expect(patreonButton).toBeVisible();
+
+    // "Or dontate with card" button should be present
+
+    await expect(page.getByText('Or donate with card')).toBeVisible();
+
+    // The card inputs should be present (to check this we will try to fill them with some values)
+
+    const cardNumberIframe = page
+      .frameLocator('iframe[src*="elements-inner-card"]')
+      .nth(0);
+
+    const cardExpiryIframe = page
+      .frameLocator('iframe[src*="elements-inner-card"]')
+      .nth(1);
+
+    await cardNumberIframe
+      .locator('input[data-elements-stable-field-name="cardNumber"]')
+      .fill('4242424242424242');
+
+    await cardExpiryIframe
+      .locator('input[data-elements-stable-field-name="cardExpiry"]')
+      .fill('1025');
+  });
+
   // We do NOT want to test the implementation of the third-party services
   // We only want to test that the buttons are there (our buttons ^) and that they are visible
   // and that the donation widget works as expected if we do decide on testing the implementation
   // of the third-party services this will be a good starting point
-
-  // test('It should have a Paypal and Patreon button', async ({ page }) => {
-  //   await page.getByRole('button', { name: 'Donate', exact: true }).click();
-  //   await expect(
-  //     page.locator('[data-paypal-smart-button-version="5.0.440"]')
-  //   ).toBeVisible();
-  //   await expect(
-  //     page.locator('[data-patreon-widget-type="become-patron-button"]')
-  //   ).toBeVisible();
-  // });
 
   // test('It is possible to donate with a card', async ({ page }) => {
   //   await page.getByRole('button', { name: 'Donate', exact: true }).click();
