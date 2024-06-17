a0ee46abbd8d5409a53ffae25cdd28fe1159dd94
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-06-17 16:32:38 +0200

    fix: update schemas to match errors

--- api/src/schemas/challenge/save-challenge.ts
+++ api/src/schemas/challenge/save-challenge.ts
@@ -1,5 +1,5 @@
 import { Type } from '@fastify/type-provider-typebox';
-import { file, savedChallenge } from '../types';
+import { file, generic500, savedChallenge } from '../types';
 
 export const saveChallenge = {
   body: Type.Object({
@@ -15,11 +15,6 @@ export const saveChallenge = {
       savedChallenges: Type.Array(savedChallenge)
     }),
     403: Type.Literal('That challenge type is not saveable.'),
-    500: Type.Object({
-      type: Type.Literal('danger'),
-      message: Type.Literal(
-        'Oops! Something went wrong. Please try again in a moment or contact support@freecodecamp.org if the error persists.'
-      )
-    })
+    500: generic500
   }
 };
