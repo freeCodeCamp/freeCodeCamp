ffd76f6fa5c4fa8007a6d13c8f163ed08b87e7bd
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-06-25 00:22:34 +0700

    Revert "attempt to fix network error on firefox"
    This reverts commit 2e71d53428be23e687bde1206d130d0af5346efb.

--- e2e/update-email.spec.ts
+++ e2e/update-email.spec.ts
@@ -57,11 +57,11 @@ test.describe('The update-email page when the user is not signed in', () => {
     await page.goto('/update-email');
   });
 
-  test('should sign the user in and redirect to /learn', async ({ page }) => {
+  test('should redirect to the signin page', async ({ page }) => {
+    await page.waitForURL(/\/learn\/?/);
+
     await expect(
       page.getByRole('heading', { name: 'Welcome back, Full Stack User' })
     ).toBeVisible();
-
-    await expect(page).toHaveURL(/\/learn\/?/);
   });
 });
