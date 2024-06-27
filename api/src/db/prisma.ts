1a034516cb1dc65b1dcb3f0df98bc21ddd67f4cd
Shaun Hamilton <shauhami020@gmail.com>
2024-06-27 11:27:39 +0200

    feat(api): add lastUpdated field to user

--- api/src/db/prisma.ts
+++ api/src/db/prisma.ts
@@ -7,18 +7,12 @@ import { MONGOHQ_URL } from '../utils/env';
 
 declare module 'fastify' {
   interface FastifyInstance {
-    prisma: PrismaClient;
+    prisma: ReturnType<typeof getExtendedClient>;
   }
 }
 
 const prismaPlugin: FastifyPluginAsync = fp(async (server, _options) => {
-  const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: MONGOHQ_URL
-      }
-    }
-  });
+  const prisma = getExtendedClient();
 
   await prisma.$connect();
 
@@ -28,5 +22,60 @@ const prismaPlugin: FastifyPluginAsync = fp(async (server, _options) => {
     await server.prisma.$disconnect();
   });
 });
+
+// TODO: It would be nice to split this up into multiple update functions,
+//       but the types are a pain.
+// TODO: Multiple extended clients can be used for different restrictions (e.g. session vs non-session users)
+// TODO: Could be used to add other _easily forgotten_ fields like `progressTimestamp`
+function getExtendedClient() {
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: MONGOHQ_URL
+      }
+    }
+  }).$extends({
+    query: {
+      user: {
+        async update({ args, query }) {
+          args.data.lastUpdated = Date.now();
+          return query(args);
+        },
+        async updateMany({ args, query }) {
+          args.data.lastUpdated = Date.now();
+          return query(args);
+        },
+        async upsert({ args, query }) {
+          const lastUpdated = Date.now();
+          args.update.lastUpdated = lastUpdated;
+          args.create.lastUpdated = lastUpdated;
+          return query(args);
+        },
+        async create({ args, query }) {
+          args.data.lastUpdated = Date.now();
+          return query(args);
+        },
+        async createMany({ args, query }) {
+          const data = args.data;
+          const lastUpdated = Date.now();
+          if (Array.isArray(data)) {
+            data.forEach(data => {
+              data.lastUpdated = lastUpdated;
+            });
+          } else {
+            data.lastUpdated = lastUpdated;
+          }
+
+          return query(args);
+        }
+        // NOTE: raw ops are untouched, as it is meant to be a direct passthrough to mongodb
+        // async findRaw({ model, operation, args, query }) {}
+        // async aggregateRaw({ model, operation, args, query }) {}
+      }
+    }
+  });
+
+  return prisma;
+}
 
 export default prismaPlugin;
