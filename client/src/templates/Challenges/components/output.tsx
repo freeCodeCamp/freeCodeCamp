add29f5d73ad9ce5c343fbe8417bb98b064ceb1e
lasjorg <28780271+lasjorg@users.noreply.github.com>
2024-07-11 18:20:38 +0200

    fix(client): truncate outputs larger than 500_000

--- client/src/templates/Challenges/components/output.tsx
+++ client/src/templates/Challenges/components/output.tsx
@@ -11,17 +11,24 @@ interface OutputProps {
 }
 
 function Output({ defaultOutput, output }: OutputProps): JSX.Element {
-  const message = sanitizeHtml(
-    !isEmpty(output) ? output.join('\n') : defaultOutput,
-    {
-      allowedTags: ['b', 'i', 'em', 'strong', 'code', 'wbr']
-    }
-  );
+  const message = isEmpty(output) ? defaultOutput : output.join('\n');
+
+  const shouldTruncate = message?.length > 500_000;
+  const transformedMessage = shouldTruncate
+    ? `${message?.substring(0, 500_000)} Logs truncated. See browser console for more`
+    : message;
+
+  const sanitizedOutput = sanitizeHtml(transformedMessage, {
+    allowedTags: ['b', 'i', 'em', 'strong', 'code', 'wbr']
+  });
+
   return (
     <pre
       className='output-text'
       data-playwright-test-label='output-text'
-      dangerouslySetInnerHTML={{ __html: message }}
+      dangerouslySetInnerHTML={{
+        __html: sanitizedOutput
+      }}
       role='region'
       aria-label={i18next.t('learn.editor-tabs.console')}
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
