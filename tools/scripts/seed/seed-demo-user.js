17ac915616ff2d8044c47775673be755ba9bdba6
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-07-17 10:41:24 +0200

    refactor: use parseArgs to simplify parsing

--- tools/scripts/seed/seed-demo-user.js
+++ tools/scripts/seed/seed-demo-user.js
@@ -1,3 +1,5 @@
+const { parseArgs } = require('node:util');
+
 const path = require('path');
 const debug = require('debug');
 const { MongoClient } = require('mongodb');
@@ -11,23 +13,15 @@ const {
   userIds
 } = require('./user-data');
 
-const args = process.argv.slice(2);
+const options = {
+  donor: { type: 'boolean' },
+  'top-contributor': { type: 'boolean' },
+  'unset-privacy-terms': { type: 'boolean' },
+  'seed-trophy-challenges': { type: 'boolean' },
+  'certified-user': { type: 'boolean' }
+};
 
-const allowedArgs = [
-  '--donor',
-  '--top-contributor',
-  '--unset-privacy-terms',
-  '--seed-trophy-challenges',
-  'certified-user'
-];
-
-// Check for invalid arguments
-args.forEach(arg => {
-  if (!allowedArgs.includes(arg))
-    throw new Error(
-      `Invalid argument ${arg}. Allowed arguments are ${allowedArgs.join(', ')}`
-    );
-});
+const { values: argValues } = parseArgs({ options });
 
 const log = debug('fcc:tools:seedLocalAuthUser');
 const { MONGOHQ_URL } = process.env;
@@ -87,16 +81,16 @@ const trophyChallenges = [
 ];
 
 [demoUser, blankUser, fullyCertifiedUser].forEach(user => {
-  if (args.includes('--donor')) {
+  if (argValues.donor) {
     user.isDonating = true;
   }
-  if (args.includes('--top-contributor')) {
+  if (argValues['top-contributor']) {
     user.yearsTopContributor = ['2017', '2018', '2019'];
   }
-  if (args.includes('--unset-privacy-terms')) {
+  if (argValues['--unset-privacy-terms']) {
     user.acceptedPrivacyTerms = false;
   }
-  if (args.includes('--seed-trophy-challenges')) {
+  if (argValues['--seed-trophy-challenges']) {
     user.completedChallenges = trophyChallenges;
   }
 });
@@ -128,7 +122,7 @@ const run = async () => {
 
   await dropUserTokens();
   await dropUsers();
-  if (args.includes('certified-user')) {
+  if (argValues['certified-user']) {
     await user.insertOne(fullyCertifiedUser);
     await user.insertOne(blankUser);
     await user.insertOne(publicUser);
