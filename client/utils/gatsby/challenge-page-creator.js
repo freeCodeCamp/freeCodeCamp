4263ee533c42a9b091439ecafb32d1317070636b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-07-19 14:52:45 +0200

    feat: control preview via demoType

--- client/utils/gatsby/challenge-page-creator.js
+++ client/utils/gatsby/challenge-page-creator.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const { sortChallengeFiles } = require('../sort-challengefiles');
-const {
-  challengeTypes,
-  viewTypes
-} = require('../../../shared/config/challenge-types');
+const { viewTypes } = require('../../../shared/config/challenge-types');
 
 const backend = path.resolve(
   __dirname,
@@ -150,8 +147,7 @@ exports.createChallengePages = function (createPage) {
 // it during the curriculum build process and attach it to the first challenge?
 // That would remove the need to analyse allChallengeEdges.
 function getProjectPreviewConfig(challenge, allChallengeEdges) {
-  const { block, challengeOrder, challengeType, usesMultifileEditor } =
-    challenge;
+  const { block, demoType } = challenge;
 
   const challengesInBlock = allChallengeEdges
     .filter(({ node: { challenge } }) => challenge.block === block)
@@ -169,15 +165,7 @@ function getProjectPreviewConfig(challenge, allChallengeEdges) {
   }));
 
   return {
-    showProjectPreview:
-      challengeOrder === 0 &&
-      usesMultifileEditor &&
-      // TODO: handle the special cases better. Create a meta property for
-      // showProjectPreview, maybe? Then we can remove all the following cases
-      challengeType !== challengeTypes.multifileCertProject &&
-      challengeType !== challengeTypes.multifilePythonCertProject &&
-      challengeType !== challengeTypes.python &&
-      challengeType !== challengeTypes.js,
+    showProjectPreview: demoType === 'finished-project' || demoType === 'lab',
     challengeData: {
       challengeType: lastChallenge.challengeType,
       challengeFiles: projectPreviewChallengeFiles
