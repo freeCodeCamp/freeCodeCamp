4c4e26ba2e207c659004d2feb99ebb048b2ee7d9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-07-19 11:48:39 +0200

    feat: embed showDemo element in instructions

--- client/src/templates/Challenges/components/side-panel.tsx
+++ client/src/templates/Challenges/components/side-panel.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, ReactElement, ReactNode } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { Test } from '../../../redux/prop-types';
+import { useTranslation } from 'react-i18next';
 
+import { Test } from '../../../redux/prop-types';
 import { SuperBlocks } from '../../../../../shared/config/curriculum';
 import { initializeMathJax } from '../../../utils/math-jax';
 import { challengeTestsSelector } from '../redux/selectors';
@@ -21,6 +22,7 @@ interface SidePanelProps {
   challengeDescription: ReactElement;
   challengeTitle: ReactElement;
   instructionsPanelRef: React.RefObject<HTMLDivElement>;
+  hasDemo: boolean | null;
   toolPanel: ReactNode;
   superBlock: SuperBlocks;
   tests: Test[];
@@ -31,10 +33,12 @@ export function SidePanel({
   challengeDescription,
   challengeTitle,
   instructionsPanelRef,
+  hasDemo,
   toolPanel,
   superBlock,
   tests
 }: SidePanelProps): JSX.Element {
+  const { t } = useTranslation();
   useEffect(() => {
     const mathJaxChallenge =
       superBlock === SuperBlocks.RosettaCode ||
@@ -50,6 +54,17 @@ export function SidePanel({
       tabIndex={-1}
     >
       {challengeTitle}
+      {challengeDescription}
+      {hasDemo && (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+        <div
+          onClick={() => {
+            console.log('show demo');
+          }}
+        >
+          {t('buttons.show-demo')}
+        </div>
+      )}
       {challengeDescription}
       {toolPanel}
       <TestSuite tests={tests} />
