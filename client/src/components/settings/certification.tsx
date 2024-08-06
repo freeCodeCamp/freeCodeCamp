1e566b8e1a8b4c2ffb9ffcd3a6c205fe37c3f01a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-08-06 15:27:17 +0200

    refactor: use link helper over Gatsby's Link

--- client/src/components/settings/certification.tsx
+++ client/src/components/settings/certification.tsx
@@ -1,4 +1,4 @@
-import { Link, navigate } from 'gatsby';
+import { navigate } from 'gatsby';
 import { find } from 'lodash-es';
 import React, { MouseEvent, useState } from 'react';
 import { withTranslation } from 'react-i18next';
@@ -21,7 +21,7 @@ import {
 } from '../../../config/cert-and-project-map';
 import { FlashMessages } from '../Flash/redux/flash-messages';
 import ProjectModal from '../SolutionViewer/project-modal';
-import { FullWidthRow, Spacer } from '../helpers';
+import { FullWidthRow, Spacer, Link } from '../helpers';
 import { SolutionDisplayWidget } from '../solution-display-widget';
 import {
   Certification,
