cebb732c1dd52bf55f65f623b0ba20fd18d2b6be
Kevin Wu <kevinwu.858@gmail.com>
2024-08-15 19:30:15 +0000

    feat(curriculum): add test number enumeration

--- client/src/templates/Challenges/components/test-suite.tsx
+++ client/src/templates/Challenges/components/test-suite.tsx
@@ -43,7 +43,7 @@ function TestSuite({ tests }: TestSuiteProps): JSX.Element {
             pass && !err ? t('icons.passed') : t('icons.failed');
           // Remove opening/closing <p> so screen reader will read both
           // status message and test text as one block.
-          text = text.replace(/^<p>|<\/p>$/g, '');
+          text = `${index + 1}. ${text.replace(/^<p>|<\/p>$/g, '')}`;
           return (
             <li
               className='test-result'
