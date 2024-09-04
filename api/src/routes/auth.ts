838f30e2bee2509fdd946ddcaa2f511be3df1117
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-09-04 03:38:50 +0200

    feat(api): update mobile-login to match api-server (#55863)

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -3,25 +3,28 @@ import { FastifyPluginCallback, FastifyRequest } from 'fastify';
 import rateLimit from 'express-rate-limit';
 // @ts-expect-error - no types
 import MongoStoreRL from 'rate-limit-mongo';
+import isEmail from 'validator/lib/isEmail';
 
 import { AUTH0_DOMAIN, MONGOHQ_URL } from '../utils/env';
 import { auth0Client } from '../plugins/auth0';
+import { createAccessToken } from '../utils/tokens';
 import { findOrCreateUser } from './helpers/auth-helpers';
 
-const getEmailFromAuth0 = async (req: FastifyRequest) => {
+const getEmailFromAuth0 = async (
+  req: FastifyRequest
+): Promise<string | null> => {
   const auth0Res = await fetch(`https://${AUTH0_DOMAIN}/userinfo`, {
     headers: {
       Authorization: req.headers.authorization ?? ''
     }
   });
 
-  if (!auth0Res.ok) {
-    req.log.error(auth0Res);
-    throw new Error('Invalid Auth0 Access Token');
-  }
+  if (!auth0Res.ok) return null;
 
-  const { email } = (await auth0Res.json()) as { email: string };
-  return email;
+  // For now, we assume the response is a JSON object. If not, we can't proceed
+  // and the only safe thing to do is to throw.
+  const { email } = (await auth0Res.json()) as { email?: string };
+  return typeof email === 'string' ? email : null;
 };
 
 /**
@@ -60,10 +63,25 @@ export const mobileAuth0Routes: FastifyPluginCallback = (
   // all auth routes.
   fastify.addHook('onRequest', fastify.redirectIfSignedIn);
 
-  fastify.get('/mobile-login', async req => {
+  fastify.get('/mobile-login', async (req, reply) => {
     const email = await getEmailFromAuth0(req);
 
-    await findOrCreateUser(fastify, email);
+    if (!email) {
+      return reply.status(401).send({
+        message: 'We could not log you in, please try again in a moment.',
+        type: 'danger'
+      });
+    }
+    if (!isEmail(email)) {
+      return reply.status(400).send({
+        message: 'The email is incorrectly formatted',
+        type: 'danger'
+      });
+    }
+
+    const { id } = await findOrCreateUser(fastify, email);
+
+    reply.setAccessTokenCookie(createAccessToken(id));
   });
 
   done();
