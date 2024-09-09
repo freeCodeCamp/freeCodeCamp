a86a4a5fdc20a8811d6dbd7627c08c70290305a3
Ilenia <nethleen@gmail.com>
2024-09-09 08:54:17 +0000

    add duplicated id to the list

--- curriculum/test/utils/mongo-ids.js
+++ curriculum/test/utils/mongo-ids.js
@@ -13,7 +13,8 @@ const duplicatedProjectIds = [
   'a7f4d8f2483413a6ce226cac',
   '56533eb9ac21ba0edf2244e2',
   'aff0395860f5d3034dc0bfc9',
-  'aa2e6f85cab2ab736c9a9b24'
+  'aa2e6f85cab2ab736c9a9b24',
+  '5a8b073d06fa14fcfde687aa'
 ];
 
 class MongoIds {
