812a02bea587fc33323c2720e68ec0e46fd3d10a
Ilenia <nethleen@gmail.com>
2024-09-12 19:38:35 +0000

    fix(curriculum): add number guessing game lab to frontend cert

--- curriculum/test/utils/mongo-ids.js
+++ curriculum/test/utils/mongo-ids.js
@@ -14,7 +14,8 @@ const duplicatedProjectIds = [
   '56533eb9ac21ba0edf2244e2',
   'aff0395860f5d3034dc0bfc9',
   'aa2e6f85cab2ab736c9a9b24',
-  '5a8b073d06fa14fcfde687aa'
+  '5a8b073d06fa14fcfde687aa',
+  '602da04c22201c65d2a019f4'
 ];
 
 class MongoIds {
