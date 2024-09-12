302c7781eedfc34b83b9e320c8ac621de685715c
Ilenia <nethleen@gmail.com>
2024-09-12 17:13:21 +0000

    chore(curriculum): add salon appointment scheduler to frontend cert

--- curriculum/test/utils/mongo-ids.js
+++ curriculum/test/utils/mongo-ids.js
@@ -14,7 +14,8 @@ const duplicatedProjectIds = [
   '56533eb9ac21ba0edf2244e2',
   'aff0395860f5d3034dc0bfc9',
   'aa2e6f85cab2ab736c9a9b24',
-  '5a8b073d06fa14fcfde687aa'
+  '5a8b073d06fa14fcfde687aa',
+  '5f87ac112ae598023a42df1a'
 ];
 
 class MongoIds {
