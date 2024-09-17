42d9e1928b7b9100d8152f064f8da692141ac0d5
moT01 <20648924+moT01@users.noreply.github.com>
2024-09-17 11:04:30 -0500

    chore: rename file [skip ci]

--- tools/challenge-parser/parser/index.js
+++ tools/challenge-parser/parser/index.js
@@ -15,7 +15,7 @@ const replaceImports = require('./plugins/replace-imports');
 const restoreDirectives = require('./plugins/restore-directives');
 const tableAndStrikeThrough = require('./plugins/table-and-strikethrough');
 const addScene = require('./plugins/add-scene');
-const addQuiz = require('./plugins/add-quiz');
+const addQuizzes = require('./plugins/add-quizzes');
 
 // by convention, anything that adds to file.data has the name add<name>.
 const processor = unified()
@@ -51,7 +51,7 @@ const processor = unified()
   .use(addVideoQuestion)
   .use(addAssignment)
   .use(addScene)
-  .use(addQuiz)
+  .use(addQuizzes)
   .use(addTests)
   .use(addText, ['description', 'instructions', 'notes']);
 
