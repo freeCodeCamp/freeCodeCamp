c8f272d3c34a981337df4d6bc67708987df7aa50
Tom <20648924+moT01@users.noreply.github.com>
2024-09-24 10:48:17 -0500

    feat(challenge-parser/client): add quiz challenge type (#56058)
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- client/src/redux/prop-types.ts
+++ client/src/redux/prop-types.ts
@@ -201,6 +201,7 @@ export type ChallengeNode = {
     isPrivate: boolean;
     order: number;
     questions: Question[];
+    quizzes: Quiz[];
     assignments: string[];
     required: Required[];
     scene: FullScene;
@@ -222,6 +223,16 @@ export type ChallengeNode = {
     bilibiliIds?: BilibiliIds;
     videoUrl: string;
   };
+};
+
+export type Quiz = {
+  questions: QuizQuestion[];
+};
+
+export type QuizQuestion = {
+  question: string;
+  distractors: string[];
+  answer: string;
 };
 
 export type CertificateNode = {
---
+++ tools/challenge-parser/parser/plugins/add-quizzes.js
@@ -0,0 +1,102 @@
+const { root } = require('mdast-builder');
+const getAllBetween = require('./utils/between-headings');
+const mdastToHtml = require('./utils/mdast-to-html');
+
+const { splitOnThematicBreak } = require('./utils/split-on-thematic-break');
+
+function plugin() {
+  return transformer;
+  function transformer(tree, file) {
+    const quizzesNodes = getAllBetween(tree, `--quizzes--`);
+
+    if (quizzesNodes.length > 0) {
+      const quizzes = [];
+      const quizTrees = [];
+
+      quizzesNodes.forEach(quizNode => {
+        const isStartOfQuiz = quizNode.children?.[0]?.value === '--quiz--';
+        if (isStartOfQuiz) {
+          quizTrees.push([quizNode]);
+        } else {
+          quizTrees[quizTrees.length - 1].push(quizNode);
+        }
+      });
+
+      if (quizTrees.length === 0) {
+        throw Error(
+          'The --quizzes-- section should contain at least one quiz.'
+        );
+      }
+
+      quizTrees.forEach(allQuizNodes => {
+        const quizTree = root(allQuizNodes);
+        const quizNodes = getAllBetween(quizTree, `--quiz--`);
+        const quizQuestions = [];
+        const questionTrees = [];
+
+        quizNodes.forEach(quizNode => {
+          const isStartOfQuestion =
+            quizNode.children?.[0]?.value === '--question--';
+          if (isStartOfQuestion) {
+            questionTrees.push([quizNode]);
+          } else {
+            questionTrees[questionTrees.length - 1].push(quizNode);
+          }
+        });
+
+        if (questionTrees.length === 0) {
+          throw Error(
+            'The --quiz-- section should contain at least one question.'
+          );
+        }
+
+        questionTrees.forEach(singleQuestionNodes => {
+          const questionTree = root(singleQuestionNodes);
+
+          const questionNodes = getAllBetween(questionTree, '--question--');
+          const distractorNodes = getAllBetween(
+            questionTree,
+            '--distractors--'
+          );
+          const answerNodes = getAllBetween(questionTree, '--answer--');
+
+          quizQuestions.push(
+            getQuestion(questionNodes, distractorNodes, answerNodes)
+          );
+        });
+
+        quizzes.push({ questions: quizQuestions });
+      });
+
+      if (quizzes.length > 0) {
+        file.data.quizzes = quizzes;
+      }
+    }
+  }
+}
+
+function getQuestion(questionNodes, distractorNodes, answerNodes) {
+  const question = mdastToHtml(questionNodes);
+  const distractors = getDistractors(distractorNodes);
+  const answer = mdastToHtml(answerNodes);
+
+  if (!question) throw Error('--question-- is missing from the quiz');
+  if (!distractors)
+    throw Error('--distractors-- are missing from quiz question');
+  if (!answer) throw Error('--answer-- is missing from quiz question');
+
+  return { question, distractors, answer };
+}
+
+function getDistractors(distractorsNodes) {
+  const distractorsGroups = splitOnThematicBreak(distractorsNodes);
+
+  if (distractorsGroups.length !== 3)
+    throw Error('Three distractors are required per quiz-question');
+
+  return distractorsGroups.map(distractorsGroup => {
+    return mdastToHtml(distractorsGroup);
+  });
+}
+
+module.exports = plugin;
