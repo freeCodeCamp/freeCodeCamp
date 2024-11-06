c863950db65c1b2affa5ff7258a132294e1b6a1b
A Cottrill <a.rcottrill521@gmail.com>
2024-11-06 12:59:31 -0500

    add unsubscribe page

---
+++ client/src/pages/unsubscribed/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ShowUnsubscribed from '../../client-only-routes/show-unsubscribed';
+
+function UnsubscribedPage(): JSX.Element {
+  return <ShowUnsubscribed />;
+}
+
+export default UnsubscribedPage;
