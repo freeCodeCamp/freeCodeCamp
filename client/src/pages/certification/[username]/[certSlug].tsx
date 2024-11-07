5056bc7df8a9a298805d150ec849e7c73c6f7f16
A Cottrill <a.rcottrill521@gmail.com>
2024-11-07 16:45:51 -0500

    update prop order

--- client/src/pages/certification/[username]/[certSlug].tsx
+++ client/src/pages/certification/[username]/[certSlug].tsx
@@ -1,24 +1,21 @@
 /* eslint-disable filenames-simple/naming-convention */
+
 import React from 'react';
-
-import './certification.css';
 import ShowCertification from '../../../client-only-routes/show-certification';
+import './certification.css';
 
-interface CertificationProps {
+interface CertificationPageProps {
   params: {
     username: string;
     certSlug: string;
   };
 }
 
-const CertificationPage: React.FC<CertificationProps> = ({ params }) => {
+const CertificationPage: React.FC<CertificationPageProps> = ({ params }) => {
   const { username, certSlug } = params;
-
-  return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    <ShowCertification certSlug={certSlug} username={username} />
-  );
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return <ShowCertification username={username} certSlug={certSlug} />;
 };
 
 export default CertificationPage;
