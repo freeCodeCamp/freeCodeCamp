5edd236f1679f0518a9e44a9ff0b68d6b4b71f91
sembauke <semboot699@gmail.com>
2024-11-22 12:19:05 +0100

    test: seed certified user maybe

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -3,6 +3,10 @@ import { execSync } from 'child_process';
 import { test as setup } from '@playwright/test';
 
 setup.describe('certifieduser', () => {
+  setup.beforeAll(() => {
+    execSync('node ./tools/scripts/seed/seed-demo-user --certified-user');
+  });
+
   setup('can sign in', async ({ request }) => {
     await request.get(process.env.API_LOCATION + '/signin');
     await request.storageState({
@@ -15,6 +19,7 @@ setup.describe('developmentuser', () => {
   // We need to make sure the development user does not have any cookies from the certified user.
   // Ass the certified user now has the default storage state.
   setup.use({ storageState: { cookies: [], origins: [] } });
+
   // We can only sign in as a single user (one with email: 'foo@bar.com'), so
   // changing users means changing the record with that email in the database.
   setup.beforeAll(() => {
