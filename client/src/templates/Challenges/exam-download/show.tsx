f0d888cd9e55ab90bab3660216ff70092f98ecc5
sembauke <semboot699@gmail.com>
2024-12-02 10:23:05 +0100

    feat: add download capabilitie

--- client/src/templates/Challenges/exam-download/show.tsx
+++ client/src/templates/Challenges/exam-download/show.tsx
@@ -12,9 +12,52 @@ interface GitProps {
 
 function ShowExamDownload(): JSX.Element {
   const [latestVersion, setLatestVersion] = useState<string | null>(null);
+
+  const [downloadLink, setDownloadLink] = useState<string | undefined>('');
+
   const os = useDetectOS();
 
   const { t } = useTranslation();
+
+  const handleDownloadLink = (downloadLinks: string[]) => {
+    const win = downloadLinks.find(link => link.match(/\.msi/));
+    const macARM = downloadLinks.find(
+      link => link.match(/aarch64/) && link.match(/\.dmg/)
+    );
+    const macX64 = downloadLinks.find(
+      link => link.match(/x64/) && link.match(/\.dmg/)
+    );
+
+    const linuxARM = downloadLinks.find(
+      link => link.match(/aarch64/) && link.match(/tar\.gz/)
+    );
+
+    const linuxX64 = downloadLinks.find(
+      link => link.match(/amd64/) && link.match(/AppImage/)
+    );
+
+    if (os.os === 'WIN') {
+      return win;
+    }
+
+    if (os.os === 'MAC') {
+      if (os.architecture.toLowerCase() === 'arm') {
+        return macARM;
+      } else {
+        return macX64;
+      }
+    }
+
+    if (os.os === 'LINUX') {
+      if (os.architecture.toLowerCase() === 'arm') {
+        return linuxARM;
+      } else {
+        return linuxX64;
+      }
+    }
+
+    return '';
+  };
 
   useEffect(() => {
     const checkLatestVersion = async () => {
@@ -26,16 +69,20 @@ function ShowExamDownload(): JSX.Element {
       .then(response => {
         if (response.ok) {
           void response.json().then(data => {
-            const { tag_name } = data as GitProps;
+            const { tag_name, assets } = data as GitProps;
             setLatestVersion(tag_name);
+
+            setDownloadLink(
+              handleDownloadLink(
+                assets.map(links => links.browser_download_url)
+              )
+            );
           });
         }
       })
       .catch(() => {
         setLatestVersion('...');
       });
-
-    console.log(os);
   });
 
   return (
@@ -48,7 +95,14 @@ function ShowExamDownload(): JSX.Element {
           version: latestVersion || '...'
         })}
       </p>
-      <Button>{t('buttons.download-latest-version')}</Button>
+      <Button
+        disabled={!downloadLink}
+        aria-disabled={!downloadLink}
+        href={downloadLink}
+        download={downloadLink}
+      >
+        {t('buttons.download-latest-version')}
+      </Button>
       <Spacer size='l' />
       <strong>{t('exam.download-trouble')}</strong>
       <a href='mailto: support@freecodecamp.org'>support@freecodecamp.org</a>
