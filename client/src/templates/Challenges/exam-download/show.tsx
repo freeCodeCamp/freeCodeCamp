e72876df35660d65fe1056f1aa54d113c7bc530e
sembauke <semboot699@gmail.com>
2024-12-02 09:26:31 +0100

    feat: detect OS and Arch

--- client/src/templates/Challenges/exam-download/show.tsx
+++ client/src/templates/Challenges/exam-download/show.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Spacer } from '@freecodecamp/ui';
 import { useTranslation } from 'react-i18next';
 import { FullWidthRow } from '../../../components/helpers';
+import useDetectOS from '../utils/use-detect-os';
+interface GitProps {
+  tag_name: string;
+  assets: {
+    browser_download_url: string;
+  }[];
+}
 
 function ShowExamDownload(): JSX.Element {
-  // const handleCorrectDownloadLink = () => {};
-
-  // const checkLatestVersion = () => {};
+  const [latestVersion, setLatestVersion] = useState<string | null>(null);
+  const os = useDetectOS();
 
   const { t } = useTranslation();
+
+  useEffect(() => {
+    const checkLatestVersion = async () => {
+      return await fetch(
+        'https://api.github.com/repos/freeCodeCamp/exam-env/releases/latest'
+      );
+    };
+    checkLatestVersion()
+      .then(response => {
+        if (response.ok) {
+          void response.json().then(data => {
+            const { tag_name } = data as GitProps;
+            setLatestVersion(tag_name);
+          });
+        }
+      })
+      .catch(() => {
+        setLatestVersion('...');
+      });
+
+    console.log(os);
+  });
 
   return (
     <FullWidthRow>
@@ -17,7 +45,7 @@ function ShowExamDownload(): JSX.Element {
       <p>{t('exam.explanation')}</p>
       <p>
         {t('exam.version', {
-          version: 'v-TBD'
+          version: latestVersion || '...'
         })}
       </p>
       <Button>{t('buttons.download-latest-version')}</Button>
---
+++ client/src/templates/Challenges/utils/use-detect-os.ts
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+
+type UserOS = 'MAC' | 'WIN' | 'LINUX' | 'AIX' | 'OTHER' | 'LOADING';
+
+type UserOSState = {
+  os: UserOS;
+  architecture: string;
+};
+
+interface NavigatorUAData {
+  getHighEntropyValues(hints: string[]): Promise<Record<string, string>>;
+}
+
+interface Navigator {
+  userAgentData?: NavigatorUAData;
+}
+
+const getArchitecture = async (): Promise<string> => {
+  // Check for User-Agent Client Hints support
+  if ((navigator as Navigator).userAgentData?.getHighEntropyValues) {
+    try {
+      const { architecture } =
+        (await (navigator as Navigator).userAgentData?.getHighEntropyValues([
+          'architecture'
+        ])) ?? {};
+      if (architecture) {
+        return architecture;
+      }
+    } catch (error) {
+      console.error(
+        'Error fetching architecture via User-Agent Client Hints:',
+        error
+      );
+    }
+  }
+
+  // Fallback to userAgent parsing
+  const userAgent = navigator.userAgent;
+
+  // Detect architectures
+  if (/x86_64|Win64|WOW64|amd64/.test(userAgent)) {
+    return 'x86_64'; // 64-bit x86
+  } else if (/i686|x86|Win32/.test(userAgent)) {
+    return 'x86'; // 32-bit x86
+  } else if (/arm|aarch64/.test(userAgent)) {
+    return /aarch64/.test(userAgent) ? 'ARM64' : 'ARM'; // ARM
+  }
+
+  // Unknown architecture
+  return 'Unknown';
+};
+
+export const detectOsInUserAgent = (userAgent: string | undefined): UserOS => {
+  const osMatch = userAgent?.match(/(Win|Mac|Linux|AIX)/);
+  return osMatch ? (osMatch[1].toUpperCase() as UserOS) : 'OTHER';
+};
+
+export const detectOS = (): UserOS => detectOsInUserAgent(navigator?.userAgent);
+
+const useDetectOS = () => {
+  const [userOS, setUserOS] = useState<UserOSState>({
+    os: 'LOADING',
+    architecture: 'ARM'
+  });
+
+  useEffect(() => {
+    Promise.all([getArchitecture()])
+      .then(([arch]) => {
+        setUserOS({
+          os: detectOS(),
+          architecture: arch ? arch : ''
+        });
+      })
+      .catch(console.error);
+  }, []);
+
+  return userOS;
+};
+
+export default useDetectOS;
