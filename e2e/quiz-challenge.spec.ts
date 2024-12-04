a32f656d4da1411ac58a0ca705f3a1a3c434dd31
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-12-05 02:48:16 +0700

    test: user can complete if they pass the quiz

--- e2e/quiz-challenge.spec.ts
+++ e2e/quiz-challenge.spec.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'child_process';
 import { test, expect } from '@playwright/test';
 
 test.describe('Quiz challenge', () => {
@@ -6,10 +7,18 @@ test.describe('Quiz challenge', () => {
     'The FSD superblock is not available if SHOW_UPCOMING_CHANGES is false'
   );
 
+  test.use({ storageState: 'playwright/.auth/development-user.json' });
+
   test.beforeEach(async ({ page }) => {
+    execSync('node ./tools/scripts/seed/seed-demo-user');
+
     await page.goto(
       '/learn/full-stack-developer/quiz-basic-html/quiz-basic-html'
     );
+  });
+
+  test.afterAll(() => {
+    execSync('node ./tools/scripts/seed/seed-demo-user --certified-user');
   });
 
   test('should display a list of unanswered questions if user has not answered all questions', async ({
@@ -71,7 +80,7 @@ test.describe('Quiz challenge', () => {
 
     // The navigation should be blocked, the user should stay on the same page
     await expect(page).toHaveURL(
-      '/learn/front-end-development/quiz-basic-html/quiz-basic-html'
+      '/learn/full-stack-developer/quiz-basic-html/quiz-basic-html'
     );
 
     await expect(page.getByRole('dialog')).toBeVisible();
@@ -88,7 +97,7 @@ test.describe('Quiz challenge', () => {
       .getByRole('button', { name: 'Yes, I want to leave the quiz' })
       .click();
 
-    await page.waitForURL('/learn/front-end-development/#quiz-basic-html');
+    await page.waitForURL('/learn/full-stack-developer/#quiz-basic-html');
     await expect(
       page.getByRole('heading', { level: 3, name: 'Basic HTML Quiz' })
     ).toBeVisible();
@@ -107,7 +116,7 @@ test.describe('Quiz challenge', () => {
       .getByRole('button', { name: 'Yes, I want to leave the quiz' })
       .click();
 
-    await page.waitForURL('/learn/front-end-development/#quiz-basic-html');
+    await page.waitForURL('/learn/full-stack-developer/#quiz-basic-html');
     await expect(
       page.getByRole('heading', { level: 3, name: 'Basic HTML Quiz' })
     ).toBeVisible();
@@ -131,5 +140,69 @@ test.describe('Quiz challenge', () => {
     });
 
     await page.close({ runBeforeUnload: true });
+  });
+
+  test('should allow user to complete the block when they pass the quiz', async ({
+    page
+  }) => {
+    test.setTimeout(20000);
+
+    // Wait for the page content to render
+    await expect(page.getByRole('radiogroup')).toHaveCount(20);
+
+    const radioGroups = await page.getByRole('radiogroup').all();
+
+    // Answer 17 questions correctly.
+    // This is enough to pass the quiz, and also allowing us to test the quiz passing criteria.
+    for (let i = 0; i < radioGroups.length; i++) {
+      if (i <= 16) {
+        await radioGroups[i].locator("[role='radio'][data-value='4']").click();
+      } else {
+        await radioGroups[i].locator("[role='radio'][data-value='1']").click();
+      }
+    }
+
+    await page.getByRole('button', { name: 'Finish the quiz' }).click();
+    await page.getByRole('button', { name: 'Yes, I am finished' }).click();
+
+    // Wait for the finish quiz modal to close
+    await expect(
+      page.getByRole('dialog', { name: 'Finish Quiz' })
+    ).toBeHidden();
+
+    // Check that the completion modal shows up
+    await expect(
+      page
+        .getByRole('dialog')
+        .filter({ has: page.getByRole('button', { name: /submit and go/i }) })
+    ).toBeVisible();
+
+    await page
+      .getByRole('dialog')
+      .getByRole('button', { name: /close/i })
+      .click();
+
+    // Wait for the completion modal to close
+    await expect(page.getByRole('dialog')).toBeHidden();
+
+    // If the user closes the modal without submitting,
+    // the finish quiz button is replaced by the submit one.
+    await expect(
+      page.getByRole('button', { name: 'Finish the quiz' })
+    ).toBeHidden();
+    await expect(
+      page.getByRole('button', { name: /submit and go/i })
+    ).toBeVisible();
+
+    await expect(
+      page.getByText('You have 17 out of 20 questions correct.')
+    ).toBeVisible();
+
+    // Find all of the disabled radio elements on the page,
+    // and check if the count matches the total number of quiz answers (4 answers x 20 questions).
+    // This approach is much faster than querying each radio on the page and check if they are disabled.
+    await expect(
+      page.locator("[role='radio'][aria-disabled='true']")
+    ).toHaveCount(4 * 20);
   });
 });
