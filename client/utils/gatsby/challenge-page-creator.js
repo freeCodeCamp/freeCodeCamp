25de45da462403ea6a7dc28daff3f1b71fe4a41b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-12-05 09:55:00 +0100

    fix(client): remove video component (#57389)

--- client/utils/gatsby/challenge-page-creator.js
+++ client/utils/gatsby/challenge-page-creator.js
@@ -31,11 +31,6 @@ const quiz = path.resolve(
   '../../src/templates/Challenges/quiz/show.tsx'
 );
 
-const video = path.resolve(
-  __dirname,
-  '../../src/templates/Challenges/video/show.tsx'
-);
-
 const exam = path.resolve(
   __dirname,
   '../../src/templates/Challenges/exam/show.tsx'
@@ -62,7 +57,6 @@ const views = {
   modern: classic,
   frontend,
   quiz,
-  video,
   codeAlly,
   exam,
   msTrophy,
