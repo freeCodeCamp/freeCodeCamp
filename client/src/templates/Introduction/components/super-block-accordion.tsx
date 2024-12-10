c1be2768865d0d7e784a14002cdfa3800a2da1a9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-12-10 11:09:38 +0100

    feat: use Coming Soon badge for chapters/modules

--- client/src/templates/Introduction/components/super-block-accordion.tsx
+++ client/src/templates/Introduction/components/super-block-accordion.tsx
@@ -121,6 +121,7 @@ export const SuperBlockAccordion = ({
   superBlock,
   chosenBlock
 }: SuperBlockTreeViewProps) => {
+  const { t } = useTranslation();
   const { allChapters } = useMemo(() => {
     const populateBlocks = (blocks: { dashedName: string }[]) =>
       blocks.map(block => {
@@ -169,7 +170,8 @@ export const SuperBlockAccordion = ({
         } else if (chapter.modules.length === 0) {
           return (
             <li key={chapter.name}>
-              <Badge>Coming soon</Badge> Chapter name
+              <Badge>{t('misc.coming-soon')}</Badge>{' '}
+              {t(`intro:full-stack-developer.chapters.${chapter.name}`)}
             </li>
           );
         }
@@ -197,7 +199,8 @@ export const SuperBlockAccordion = ({
               } else if (module.blocks.length === 0) {
                 return (
                   <li key={chapter.name}>
-                    <Badge>Coming soon</Badge> Module name
+                    <Badge>{t('misc.coming-soon')}</Badge>{' '}
+                    {t(`intro:full-stack-developer.modules.${module.name}`)}
                   </li>
                 );
               }
