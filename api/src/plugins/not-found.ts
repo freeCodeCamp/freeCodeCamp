8970199e90d2c74b9f7d841fceb5a5889222e0c4
Amr Ashour <75447356+amrrashor@users.noreply.github.com>
2024-12-25 21:25:24 +0200

    Update not-found.ts

--- api/src/plugins/not-found.ts
+++ api/src/plugins/not-found.ts
@@ -15,7 +15,7 @@ const fourOhFour: FastifyPluginCallback = (fastify, _options, done) => {
   // If the request accepts JSON and does not specifically prefer text/html,
   // this will return a 404 JSON response. Everything else will be redirected.
   fastify.setNotFoundHandler((request, reply) => {
-    // @ts-expect-error
+    // @ts-expect-error : unreachable code error
     const accepted = request?.accepts().type(['json', 'html']);
 
     if (accepted == 'json') {
