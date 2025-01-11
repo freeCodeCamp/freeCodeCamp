47b59e0a434a806ba31042946420d137aa11e13d
A Cottrill <a.rcottrill521@gmail.com>
2025-01-11 10:11:31 -0500

    add learn client env to gitpod

--- tools/challenge-editor/client/src/vite-app-env.d.ts
+++ tools/challenge-editor/client/src/vite-app-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly CHALLENGE_EDITOR_FCC_LOCATION: string;
+  readonly CHALLENGE_EDITOR_LEARN_CLIENT_LOCATION: string;
 }
 
 interface ImportMeta {
