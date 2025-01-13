bc5fd0fdd4186887ce924dc512b3b699db59b409
Anna <a.rcottrill521@gmail.com>
2025-01-13 14:10:24 -0500

    feat(tools): add link to live version of challenge in challenge editor (#58027)
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

---
+++ tools/challenge-editor/client/src/utils/block-name-translator.ts
@@ -0,0 +1,28 @@
+export const superBlockNameMap: { [key: string]: string } = {
+  '01-responsive-web-design': 'responsive-web-design',
+  '02-javascript-algorithms-and-data-structures':
+    'javascript-algorithms-and-data-structures',
+  '03-front-end-development-libraries': 'front-end-development-libraries',
+  '04-data-visualization': 'data-visualization',
+  '05-back-end-development-and-apis': 'back-end-development-and-apis',
+  '06-quality-assurance': 'quality-assurance',
+  '07-scientific-computing-with-python': 'scientific-computing-with-python',
+  '08-data-analysis-with-python': 'data-analysis-with-python',
+  '09-information-security': 'information-security',
+  '10-coding-interview-prep': 'coding-interview-prep',
+  '11-machine-learning-with-python': 'machine-learning-with-python',
+  '13-relational-databases': 'relational-databases',
+  '14-responsive-web-design-22': '2022/responsive-web-design',
+  '15-javascript-algorithms-and-data-structures-22':
+    'javascript-algorithms-and-data-structures-v8',
+  '16-the-odin-project': 'the-odin-project',
+  '17-college-algebra-with-python': 'college-algebra-with-python',
+  '18-project-euler': 'project-euler',
+  '19-foundational-c-sharp-with-microsoft':
+    'foundational-c-sharp-with-microsoft',
+  '21-a2-english-for-developers': 'a2-english-for-developers',
+  '22-rosetta-code': 'rosetta-code',
+  '23-python-for-everybody': 'python-for-everybody',
+  '24-b1-english-for-developers': 'b1-english-for-developers',
+  '25-front-end-development': 'full-stack-developer'
+};
