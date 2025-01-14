71d04fa69012aad85d5c270f79a206513b8a2c39
JungLee-Dev <junghoonlee.dev@gmail.com>
2025-01-14 16:26:53 -0700

    fix: enable bash syntax highlighting

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -51,7 +51,8 @@ const config = {
           'sql',
           'svg',
           'typescript',
-          'xml'
+          'xml',
+          'bash'
         ],
         theme: 'default',
         css: true,
