34a2593e395bcfaae3904de11d7b1caebcd94df2
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-02-05 10:41:17 +0100

    fix: typo

--- api/src/routes/protected/challenge.ts
+++ api/src/routes/protected/challenge.ts
@@ -700,14 +700,14 @@ export const challengeRoutes: FastifyPluginCallbackTypebox = (
 
             // only update if it's a better result
             if (percentCorrect > oldResults.percentCorrect) {
-              const updatedChallege = {
+              const updatedChallenge = {
                 id,
                 challengeType: oldChallenge.challengeType,
                 completedDate: oldChallenge.completedDate,
                 examResults
               };
 
-              newCompletedChallenges[alreadyCompletedIndex] = updatedChallege;
+              newCompletedChallenges[alreadyCompletedIndex] = updatedChallenge;
 
               // TODO(Post-MVP): Try to DRY the updates.
               // updateUserChallengeData, for all its faults, handles the
