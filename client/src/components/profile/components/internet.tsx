8e12e6b92d9afc883f442df6f1b4377c99aa2084
sembauke <semboot699@gmail.com>
2025-02-06 09:00:50 +0100

    refactor(client): change internet presence from class component to functional component

--- client/src/components/profile/components/internet.tsx
+++ client/src/components/profile/components/internet.tsx
@@ -1,6 +1,6 @@
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { TFunction } from 'i18next';
 import { withTranslation } from 'react-i18next';
 import isURL from 'validator/lib/isURL';
@@ -31,11 +31,6 @@ interface InternetProps extends Socials {
   setIsEditing: (isEditing: boolean) => void;
 }
 
-type InternetState = {
-  formValues: Socials;
-  originalValues: Socials;
-};
-
 interface URLValidation {
   state: FormGroupProps['validationState'];
   message: string;
@@ -45,52 +40,40 @@ function Info({ message }: { message: string }) {
   return message ? <HelpBlock>{message}</HelpBlock> : null;
 }
 
-class InternetSettings extends Component<InternetProps, InternetState> {
-  static displayName: string;
-  constructor(props: InternetProps) {
-    super(props);
-    const {
-      githubProfile = '',
-      linkedin = '',
-      twitter = '',
-      website = ''
-    } = props;
+const InternetSettings = ({
+  githubProfile = '',
+  linkedin = '',
+  twitter = '',
+  website = '',
+  t,
+  updateSocials,
+  setIsEditing
+}: InternetProps) => {
+  const [formValues, setFormValues] = useState<Socials>({
+    githubProfile,
+    linkedin,
+    twitter,
+    website
+  });
+  const [originalValues, setOriginalValues] = useState<Socials>({
+    githubProfile,
+    linkedin,
+    twitter,
+    website
+  });
 
-    this.state = {
-      formValues: { githubProfile, linkedin, twitter, website },
-      originalValues: { githubProfile, linkedin, twitter, website }
-    };
-  }
-
-  toggleEditing = () => {
-    this.props.setIsEditing(false);
-  };
-
-  componentDidUpdate() {
-    const {
-      githubProfile = '',
-      linkedin = '',
-      twitter = '',
-      website = ''
-    } = this.props;
-
-    const { originalValues } = this.state;
-
+  useEffect(() => {
     if (
       githubProfile !== originalValues.githubProfile ||
       linkedin !== originalValues.linkedin ||
       twitter !== originalValues.twitter ||
       website !== originalValues.website
     ) {
-      return this.setState({
-        originalValues: { githubProfile, linkedin, twitter, website }
-      });
+      setOriginalValues({ githubProfile, linkedin, twitter, website });
     }
-    return null;
-  }
+  }, [githubProfile, linkedin, twitter, website, originalValues]);
 
-  getValidationStateFor(maybeURl = ''): URLValidation {
-    const { t } = this.props;
+  const getValidationStateFor = (maybeURl = ''): URLValidation => {
     if (!maybeURl || !maybeUrlRE.test(maybeURl)) {
       return {
         state: null,
@@ -107,46 +90,38 @@ class InternetSettings extends Component<InternetProps, InternetState> {
       state: 'error',
       message: t('validation.invalid-url')
     };
-  }
+  };
 
-  createHandleChange =
+  const createHandleChange =
     (key: keyof Socials) => (e: React.FormEvent<HTMLInputElement>) => {
       const value = (e.target as HTMLInputElement).value.slice(0);
-      return this.setState(state => ({
-        formValues: {
-          ...state.formValues,
-          [key]: value
-        }
-      }));
+      setFormValues({
+        ...formValues,
+        [key]: value
+      });
     };
 
-  isFormPristine = () => {
-    const { formValues, originalValues } = this.state;
+  const isFormPristine = () => {
     return (Object.keys(originalValues) as Array<keyof Socials>).every(
       key => originalValues[key] === formValues[key]
     );
   };
 
-  isFormValid = (): boolean =>
-    Object.values(this.state.formValues).every(
+  const isFormValid = (): boolean =>
+    Object.values(formValues).every(
       (value: string) => value === '' || isURL(value)
     );
 
-  handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!this.isFormPristine() && this.isFormValid()) {
-      // // Only submit the form if is has changed, and if it is valid
-      const { formValues } = this.state;
-
-      const { updateSocials } = this.props;
-      this.toggleEditing();
-      return updateSocials({ ...formValues });
+    if (!isFormPristine() && isFormValid()) {
+      // Only submit the form if is has changed, and if it is valid
+      updateSocials({ ...formValues });
     }
-    this.toggleEditing();
-    return null;
+    setIsEditing(false);
   };
 
-  renderCheck = (
+  const renderCheck = (
     url: string,
     validation: FormGroupProps['validationState'],
     dataPlaywrightTestLabel: string
@@ -163,136 +138,130 @@ class InternetSettings extends Component<InternetProps, InternetState> {
       </FormControl.Feedback>
     ) : null;
 
-  render() {
-    const { t } = this.props;
-    const {
-      formValues: { githubProfile, linkedin, twitter, website }
-    } = this.state;
+  const {
+    state: githubProfileValidation,
+    message: githubProfileValidationMessage
+  } = getValidationStateFor(formValues.githubProfile);
 
-    const {
-      state: githubProfileValidation,
-      message: githubProfileValidationMessage
-    } = this.getValidationStateFor(githubProfile);
+  const { state: linkedinValidation, message: linkedinValidationMessage } =
+    getValidationStateFor(formValues.linkedin);
 
-    const { state: linkedinValidation, message: linkedinValidationMessage } =
-      this.getValidationStateFor(linkedin);
+  const { state: twitterValidation, message: twitterValidationMessage } =
+    getValidationStateFor(formValues.twitter);
 
-    const { state: twitterValidation, message: twitterValidationMessage } =
-      this.getValidationStateFor(twitter);
+  const { state: websiteValidation, message: websiteValidationMessage } =
+    getValidationStateFor(formValues.website);
+  const isDisabled = isFormPristine() || !isFormValid();
 
-    const { state: websiteValidation, message: websiteValidationMessage } =
-      this.getValidationStateFor(website);
-    const isDisabled = this.isFormPristine() || !this.isFormValid();
-    return (
-      <>
-        <SectionHeader>{t('settings.headings.internet')}</SectionHeader>
-        <FullWidthRow>
-          <form
-            id='internet-presence'
-            onSubmit={this.handleSubmit}
-            data-playwright-test-label='internet-presence'
-          >
-            <div role='group' aria-label={t('settings.headings.internet')}>
-              <FormGroup
-                controlId='internet-github'
-                validationState={githubProfileValidation}
-              >
-                <ControlLabel htmlFor='internet-github-input'>
-                  GitHub
-                </ControlLabel>
-                <FormControl
-                  data-playwright-test-label='internet-github-input'
-                  onChange={this.createHandleChange('githubProfile')}
-                  placeholder='https://github.com/user-name'
-                  type='url'
-                  value={githubProfile}
-                  id='internet-github-input'
-                />
-                {this.renderCheck(
-                  githubProfile,
-                  githubProfileValidation,
-                  'internet-github-check'
-                )}
-                <Info message={githubProfileValidationMessage} />
-              </FormGroup>
-              <FormGroup
-                controlId='internet-linkedin'
-                validationState={linkedinValidation}
-              >
-                <ControlLabel htmlFor='internet-linkedin-input'>
-                  LinkedIn
-                </ControlLabel>
-                <FormControl
-                  onChange={this.createHandleChange('linkedin')}
-                  placeholder='https://www.linkedin.com/in/user-name'
-                  type='url'
-                  value={linkedin}
-                  id='internet-linkedin-input'
-                />
-                {this.renderCheck(
-                  linkedin,
-                  linkedinValidation,
-                  'internet-linkedin-check'
-                )}
-                <Info message={linkedinValidationMessage} />
-              </FormGroup>
-              <FormGroup
-                controlId='internet-twitter'
-                validationState={twitterValidation}
-              >
-                <ControlLabel htmlFor='internet-twitter-input'>
-                  Twitter
-                </ControlLabel>
-                <FormControl
-                  onChange={this.createHandleChange('twitter')}
-                  placeholder='https://twitter.com/user-name'
-                  type='url'
-                  value={twitter}
-                  id='internet-twitter-input'
-                />
-                {this.renderCheck(
-                  twitter,
-                  twitterValidation,
-                  'internet-twitter-check'
-                )}
-                <Info message={twitterValidationMessage} />
-              </FormGroup>
-              <FormGroup
-                controlId='internet-website'
-                validationState={websiteValidation}
-              >
-                <ControlLabel htmlFor='internet-website-input'>
-                  {t('settings.labels.personal')}
-                </ControlLabel>
-                <FormControl
-                  onChange={this.createHandleChange('website')}
-                  placeholder='https://example.com'
-                  type='url'
-                  value={website}
-                  id='internet-website-input'
-                />
-                {this.renderCheck(
-                  website,
-                  websiteValidation,
-                  'internet-website-check'
-                )}
-                <Info message={websiteValidationMessage} />
-              </FormGroup>
-            </div>
-            <BlockSaveButton
-              disabled={isDisabled}
-              bgSize='large'
-              {...(isDisabled && { tabIndex: -1 })}
+  return (
+    <>
+      <SectionHeader>{t('settings.headings.internet')}</SectionHeader>
+      <FullWidthRow>
+        <form
+          id='internet-presence'
+          onSubmit={handleSubmit}
+          data-playwright-test-label='internet-presence'
+        >
+          <div role='group' aria-label={t('settings.headings.internet')}>
+            <FormGroup
+              controlId='internet-github'
+              validationState={githubProfileValidation}
             >
-              {t('buttons.save')}{' '}
-              <span className='sr-only'>{t('settings.headings.internet')}</span>
-            </BlockSaveButton>
-          </form>
-        </FullWidthRow>
-      </>
-    );
-  }
-}
+              <ControlLabel htmlFor='internet-github-input'>
+                GitHub
+              </ControlLabel>
+              <FormControl
+                data-playwright-test-label='internet-github-input'
+                onChange={createHandleChange('githubProfile')}
+                placeholder=''
+                type='url'
+                value={formValues.githubProfile}
+                id='internet-github-input'
+              />
+              {renderCheck(
+                formValues.githubProfile,
+                githubProfileValidation,
+                'internet-github-check'
+              )}
+              <Info message={githubProfileValidationMessage} />
+            </FormGroup>
+            <FormGroup
+              controlId='internet-linkedin'
+              validationState={linkedinValidation}
+            >
+              <ControlLabel htmlFor='internet-linkedin-input'>
+                LinkedIn
+              </ControlLabel>
+              <FormControl
+                onChange={createHandleChange('linkedin')}
+                placeholder=''
+                type='url'
+                value={formValues.linkedin}
+                id='internet-linkedin-input'
+              />
+              {renderCheck(
+                formValues.linkedin,
+                linkedinValidation,
+                'internet-linkedin-check'
+              )}
+              <Info message={linkedinValidationMessage} />
+            </FormGroup>
+            <FormGroup
+              controlId='internet-twitter'
+              validationState={twitterValidation}
+            >
+              <ControlLabel htmlFor='internet-twitter-input'>
+                Twitter
+              </ControlLabel>
+              <FormControl
+                onChange={createHandleChange('twitter')}
+                placeholder=''
+                type='url'
+                value={formValues.twitter}
+                id='internet-twitter-input'
+              />
+              {renderCheck(
+                formValues.twitter,
+                twitterValidation,
+                'internet-twitter-check'
+              )}
+              <Info message={twitterValidationMessage} />
+            </FormGroup>
+            <FormGroup
+              controlId='internet-website'
+              validationState={websiteValidation}
+            >
+              <ControlLabel htmlFor='internet-website-input'>
+                {t('settings.labels.personal')}
+              </ControlLabel>
+              <FormControl
+                onChange={createHandleChange('website')}
+                placeholder=''
+                type='url'
+                value={formValues.website}
+                id='internet-website-input'
+              />
+              {renderCheck(
+                formValues.website,
+                websiteValidation,
+                'internet-website-check'
+              )}
+              <Info message={websiteValidationMessage} />
+            </FormGroup>
+          </div>
+          <BlockSaveButton
+            disabled={isDisabled}
+            bgSize='large'
+            {...(isDisabled && { tabIndex: -1 })}
+          >
+            {t('buttons.save')}{' '}
+            <span className='sr-only'>{t('settings.headings.internet')}</span>
+          </BlockSaveButton>
+        </form>
+      </FullWidthRow>
+    </>
+  );
+};
 
 InternetSettings.displayName = 'InternetSettings';
 
