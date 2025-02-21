67261d15c57c7e926f75e1779cec0f8dc365fd36
clarencepenz <clarencedespenz@gmail.com>
2025-02-21 18:25:37 +0100

    chore: input-reset - improve fragment readability

--- client/src/assets/icons/input-reset.tsx
+++ client/src/assets/icons/input-reset.tsx
@@ -5,7 +5,7 @@ const InputReset = (): JSX.Element => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <React.Fragment>
       <span className='sr-only'>{t('icons.input-reset')}</span>
       <svg
         className='ais-SearchBox-resetIcon'
@@ -16,7 +16,7 @@ const InputReset = (): JSX.Element => {
       >
         <path d='M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z'></path>
       </svg>
-    </>
+    </React.Fragment>
   );
 };
 
