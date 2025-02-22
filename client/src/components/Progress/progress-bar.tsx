191011696f35736f28094a909cfdc2ea30ef72ae
clarencepenz <clarencedespenz@gmail.com>
2025-02-22 11:59:18 +0100

    chore: progress-bar - improve fragment readability

--- client/src/components/Progress/progress-bar.tsx
+++ client/src/components/Progress/progress-bar.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export function ProgressBar({ now }: { now: number }) {
   return (
-    <>
+    <React.Fragment>
       <div className='progress-bar-background'></div>
       <div
         className='progress-bar-percent'
         data-testid='fcc-progress-bar-percent'
         style={{ width: `${now}%` }}
       ></div>
-    </>
+    </React.Fragment>
   );
 }
