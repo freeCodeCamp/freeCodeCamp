ba290460e931ad1c92d99445af3fd521e44bc46f
Anna <a.rcottrill521@gmail.com>
2025-03-05 17:14:48 -0500

    Use the whole word
    Co-authored-by: Naomi <accounts+github@nhcarrigan.com>

--- tools/challenge-parser/parser/plugins/add-video-question.js
+++ tools/challenge-parser/parser/plugins/add-video-question.js
@@ -10,7 +10,7 @@ const { splitOnThematicBreak } = require('./utils/split-on-thematic-break');
 function plugin() {
   return transformer;
   function transformer(tree, file) {
-    const description = getSection(tree, '--desc--');
+    const description = getSection(tree, '--description--');
 
     if (getSection(description, '--transcript--').length > 0) {
       throw Error('transcript should not be nested inside of the description');
