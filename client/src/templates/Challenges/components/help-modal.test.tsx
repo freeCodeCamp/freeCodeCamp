0630eaaaeae21fb70b4ec2a35f65c0907ca35fb1
A Cottrill <a.rcottrill521@gmail.com>
2025-03-17 20:46:51 -0400

    tests working?

--- client/src/templates/Challenges/components/help-modal.test.tsx
+++ client/src/templates/Challenges/components/help-modal.test.tsx
@@ -1,9 +1,9 @@
+import i18n from '../../../../i18n/config-for-tests';
 import { generateSearchLink } from './help-modal';
 
-jest.unmock('react-i18next');
-
 describe('generateSearchLink', () => {
-  it("should return a link with search query containing block name and challenge title if the title includes 'step'", () => {
+  it("should return a link with search query containing block name and challenge title if the title includes 'step'", async () => {
+    await i18n.reloadResources('en', 'intro');
     const link = generateSearchLink(
       'Step 10',
       'learn-basic-javascript-by-building-a-role-playing-game',
@@ -11,7 +11,7 @@ describe('generateSearchLink', () => {
     );
 
     expect(link).toBe(
-      'https://forum.freecodecamp.org/search?q=Learn%20Basic%20Javascript%20By%20Building%20a%20Role%20Playing%20Game%20-%20Step%2010'
+      'https://forum.freecodecamp.org/search?q=javascript-algorithms-and-data-structures-v8.blocks.learn-basic-javascript-by-building-a-role-playing-game.title%20-%20Step%2010'
     );
   });
 
@@ -23,7 +23,7 @@ describe('generateSearchLink', () => {
     );
 
     expect(link).toBe(
-      'https://forum.freecodecamp.org/search?q=learn%20greetings%20in%20your%20first%20day%20at%20the%20office%20-%20Task%2010'
+      'https://forum.freecodecamp.org/search?q=a2-english-for-developers.blocks.learn-greetings-in-your-first-day-at-the-office.title%20-%20Task%2010'
     );
   });
 
@@ -35,7 +35,7 @@ describe('generateSearchLink', () => {
     );
 
     expect(link).toBe(
-      'https://forum.freecodecamp.org/search?q=Write%20Your%20First%20Code%20Using%20C%23'
+      'https://forum.freecodecamp.org/search?q=foundational-c-sharp-with-microsoft.blocks.write-your-first-code-using-c-sharp.title%20-%20Perform%20Basic%20String%20Formatting%20in%20C%23'
     );
   });
 });
