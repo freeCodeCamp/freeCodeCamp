362555290884a7f5b5b1df19d39833da8b6e6523
c0d1ng_ma5ter <joshua-pim2223@makademy.com.mx>
2025-03-25 08:56:06 -0600

    Fix CodeFactor

--- client/src/templates/Challenges/components/reset-modal.tsx
+++ client/src/templates/Challenges/components/reset-modal.tsx
@@ -58,7 +58,7 @@ function ResetModal({
         <p>
           {canSaveToDB(challengeType)
             ? t('learn.revert-warn')
-            : t('learn.reset-warn')}
+            : t('learn.reset-warn', {title: lessonTitle})}
         </p>
         <p>
           <em>{t('learn.reset-warn-2')}</em>
