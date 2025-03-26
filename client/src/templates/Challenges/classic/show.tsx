89091eae10f889b9270532caa37b3aada0c3121e
c0d1ng_ma5ter <joshua-pim2223@makademy.com.mx>
2025-03-26 11:43:26 -0600

    whoops, typo

--- client/src/templates/Challenges/classic/show.tsx
+++ client/src/templates/Challenges/classic/show.tsx
@@ -524,7 +524,7 @@ function ShowClassic({
         <CompletionModal />
         <HelpModal challengeTitle={title} challengeBlock={blockName} />
         <VideoModal videoUrl={videoUrl} />
-        <ResetModal challengeType={challengeType} lessonTitle={challengeTitle}/>
+        <ResetModal challengeType={challengeType} lessonTitle={title}/>
         <ProjectPreviewModal
           challengeData={challengeData}
           closeText={t('buttons.start-coding')}
