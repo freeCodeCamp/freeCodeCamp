aa5d60b6ed035b65f35991af476588f70a27f814
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-04-10 12:57:25 +0200

    feat: remove deprecated endpoints

--- api-server/src/server/boot/randomAPIs.js
+++ api-server/src/server/boot/randomAPIs.js
@@ -16,8 +16,6 @@ module.exports = function (app) {
   router.get('/unsubscribe/:email', unsubscribeDeprecated);
   router.get('/ue/:unsubscribeId', unsubscribeById);
   router.get('/resubscribe/:unsubscribeId', resubscribe);
-  router.get('/api/users/get-public-profile', deprecatedEndpoint);
-  router.get('/api/users/exists', deprecatedEndpoint);
   router.get('/users/get-public-profile', blockUserAgent, getPublicProfile);
   const getUserExists = createGetUserExists(app);
   router.get('/users/exists', getUserExists);
