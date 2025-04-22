1e747c924529caf485537295d754ecc6f56b2a75
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-04-22 15:14:28 +0200

    feat: upgrade cors logs to info
    Co-authored-by: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>

--- api/src/plugins/cors.ts
+++ api/src/plugins/cors.ts
@@ -23,7 +23,7 @@ const cors: FastifyPluginCallback = (fastify, _options, done) => {
       void reply.header('Access-Control-Allow-Origin', HOME_LOCATION);
 
       if (!req.url?.startsWith('/status/')) {
-        logger.debug(`Received request from disallowed origin: ${origin}`);
+        logger.info(`Received request from disallowed origin: ${origin}`);
       }
     }
 
