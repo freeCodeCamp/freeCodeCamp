cd05a28ea1fecfb85c94a1c210224d127e34c55f
A Cottrill <a.rcottrill521@gmail.com>
2025-06-05 15:19:08 -0400

    move to challenge helper scripts

--- client/i18n/locales.test.ts
+++ client/i18n/locales.test.ts
@@ -19,10 +19,6 @@ interface Intro {
   };
 }
 
-interface ChallengeMeta {
-  name: string;
-}
-
 const filesThatShouldExist = [
   {
     name: 'translations.json'
@@ -86,42 +82,4 @@ describe('Intro file structure tests:', () => {
       expect(typedIntro[superBlock].blocks[block].intro).toBeInstanceOf(Array);
     });
   }
-});
-
-describe('Intro file name test:', () => {
-  const typedIntro = intro as unknown as Intro;
-  test('Every block listed in intro.json has a challenge meta file', () => {
-    const superblocks = Object.values(SuperBlocks);
-    for (const superBlock of superblocks) {
-      const blocks = Object.keys(typedIntro[superBlock].blocks);
-      blocks.forEach(block => {
-        if (typedIntro[superBlock].blocks[block].intro.length > 0) {
-          const exists = fs.existsSync(
-            `${__dirname}/../../curriculum/challenges/_meta/${block}/meta.json`
-          );
-          expect(exists).toBeTruthy();
-        }
-      });
-    }
-  });
-
-  test('Every block name matches in the meta.json and intro.json', () => {
-    const typedIntro = intro as unknown as Intro;
-    const superblocks = Object.values(SuperBlocks);
-    for (const superBlock of superblocks) {
-      const blocks = Object.keys(typedIntro[superBlock].blocks);
-      blocks.forEach(block => {
-        if (typedIntro[superBlock].blocks[block].intro.length > 0) {
-          const metaContent = fs.readFileSync(
-            `${__dirname}/../../curriculum/challenges/_meta/${block}/meta.json`,
-            { encoding: 'utf-8' }
-          );
-          const metaJSON = JSON.parse(metaContent) as ChallengeMeta;
-          expect(metaJSON.name).toBe(
-            typedIntro[superBlock].blocks[block].title
-          );
-        }
-      });
-    }
-  });
 });
