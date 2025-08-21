415128ebc7aaf7d7cceb9e1826f292641aa8da5c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-08-21 09:47:54 +0200

    refactor: remove config, the defaults seem fine for now

--- api/vitest.setup.ts
+++
@@ -1,7 +0,0 @@
-// This file runs before all tests to set up the environment
-import { beforeAll } from 'vitest';
-
-// Setup any global configurations if needed
-beforeAll(() => {
-  // Global test setup can go here
-});
