29b7490be12708c4a477fde2d446ad137ea3f16d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-08-22 09:17:25 +0200

    Revert "DEBUG: definitely not for production"
    This reverts commit fdbbd5b397157d01aec16fad386656253cae8c5a.

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -166,10 +166,6 @@ export const MONGOHQ_URL =
       )
     : process.env.MONGOHQ_URL;
 
-console.log('NODE_ENV', process.env.NODE_ENV);
-console.log('VITEST_WORKER_ID', process.env.VITEST_WORKER_ID);
-console.log('MONGOHQ_URL', MONGOHQ_URL);
-
 export const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 export const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
