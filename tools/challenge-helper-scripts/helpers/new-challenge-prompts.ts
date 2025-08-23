1bf7fb6e04a5365db897b06dc1061d69f83e4036
Ajay A <aji478514@gmail.com>
2025-08-23 07:25:28 +0000

    fix :add @inquirer/prompts in helper(new challenge/task)-prompts

--- tools/challenge-helper-scripts/helpers/new-challenge-prompts.ts
+++ tools/challenge-helper-scripts/helpers/new-challenge-prompts.ts
@@ -1,4 +1,4 @@
-import { prompt } from 'inquirer';
+import { input, select } from '@inquirer/prompts';
 import { challengeTypes } from '../../../shared/config/challenge-types';
 import { getLastStep } from './get-last-step-file-number';
 
@@ -7,22 +7,18 @@ export const newChallengePrompts = async (): Promise<{
   dashedName: string;
   challengeType: string;
 }> => {
-  const challengeType = await prompt<{ value: string }>({
-    name: 'value',
+  const challengeType = await select({
     message: 'What type of challenge is this?',
-    type: 'list',
     choices: Object.entries(challengeTypes).map(([key, value]) => ({
       name: key,
-      value
+      value: String(value)
     }))
   });
 
   const lastStep = getLastStep().stepNum;
   const defaultTitle = `Step ${lastStep + 1}`;
   const defaultDashedName = `step-${lastStep + 1}`;
-
-  const dashedName = await prompt<{ value: string }>({
-    name: 'value',
+  const dashedName = await input({
     message: 'What is the dashed name (in kebab-case) for this challenge?',
     validate: (block: string) => {
       if (!block.length) {
@@ -33,20 +29,20 @@ export const newChallengePrompts = async (): Promise<{
       }
       return true;
     },
-    filter: (block: string) => {
+    default: defaultDashedName,
+    transformer: (block: string) => {
       return block.toLowerCase();
-    },
-    default: defaultDashedName
+    }
   });
-  const title = await prompt<{ value: string }>({
-    name: 'value',
+
+  const title = await input({
     message: 'What is the title of this challenge?',
     default: defaultTitle
   });
 
   return {
-    title: title.value,
-    dashedName: dashedName.value,
-    challengeType: challengeType.value
+    title,
+    dashedName,
+    challengeType
   };
 };
