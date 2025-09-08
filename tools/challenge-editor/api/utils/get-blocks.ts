3351d1af2a3b06ec26d05259386cd5f92fd56d82
Anna <a.rcottrill521@gmail.com>
2025-09-08 18:28:04 -0400

    make a list

--- tools/challenge-editor/api/utils/get-blocks.ts
+++ tools/challenge-editor/api/utils/get-blocks.ts
@@ -10,6 +10,8 @@ type Block = {
   path: string;
 };
 
+const chapterBasedSuperBlocks = ['full-stack-developer'];
+
 export const getBlocks = async (sup: string): Promise<Block[]> => {
   const superBlockDataPath = join(SUPERBLOCK_META_DIR, sup + '.json');
   const superBlockMetaFile = await readFile(superBlockDataPath, {
@@ -18,31 +20,13 @@ export const getBlocks = async (sup: string): Promise<Block[]> => {
   const superBlockMeta = JSON.parse(superBlockMetaFile) as SuperBlockMeta;
   let blocks: { name: string; path: string }[] = [];
 
-  if (sup === 'full-stack-developer') {
+  if (chapterBasedSuperBlocks.includes(sup)) {
     blocks = superBlockMeta.chapters!.map(chapter => {
       return {
         name: chapter.dashedName,
         path: 'chapters/' + chapter.dashedName
       };
     });
-    /*
-    const moduleBlockData = await Promise.all(
-      superBlockMeta.chapters!.flatMap(async chapter => {
-        return await Promise.all(
-          chapter.modules.flatMap(async module => {
-            return module.blocks!.flatMap(block => {
-              const filePath = join(CHALLENGE_DIR, block);
-              return {
-                name: block,
-                path: filePath
-              };
-            });
-          })
-        );
-      })
-    );
-    blocks = moduleBlockData.flat().flat();
-    */
   } else {
     blocks = await Promise.all(
       superBlockMeta.blocks!.map(async block => {
