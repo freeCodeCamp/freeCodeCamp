3dd8c6b1cae73b66015a28d679191d80b6722206
Niraj Nandish <nirajnandish@icloud.com>
2025-09-12 07:11:16 +0530

    feat: chapter based structure

--- shared/config/curriculum.ts
+++ shared/config/curriculum.ts
@@ -346,7 +346,8 @@ Object.freeze(notAuditedSuperBlocks);
 export const chapterBasedSuperBlocks = [
   SuperBlocks.FullStackDeveloper,
   SuperBlocks.FullStackOpen,
-  SuperBlocks.A1Spanish
+  SuperBlocks.A1Spanish,
+  SuperBlocks.RespWebDesign2025
 ];
 Object.freeze(chapterBasedSuperBlocks);
 
