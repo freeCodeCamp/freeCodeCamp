2f72669dbc9371e48c19b44364dfb01387815898
Sem Bauke <sem@freecodecamp.org>
2025-09-19 09:22:54 +0200

    Apply suggestions from code review
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- shared/vitest.config.mjs
+++ shared/vitest.config.mjs
@@ -3,8 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['**/*.test.ts'],
-    exclude: ['node_modules'],
-    environment: 'node',
-    globals: true
   }
 });
