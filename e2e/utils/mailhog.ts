61922778f4bf524d93c25e7b4725ee2a713a3eca
Mrugesh Mohapatra <noreply@mrugesh.dev>
2025-10-05 16:32:41 +0530

    fix(tools): consolidate docker compose setup

--- e2e/utils/mailhog.ts
+++ e2e/utils/mailhog.ts
@@ -11,7 +11,12 @@ type AllEmails = {
   count: number;
 };
 
-const host = process.env.MAILHOG_HOST || 'localhost';
+// TODO: Remove MAILHOG_HOST in a few months
+// We renamed MailHog to MailPit, but kept the same port and API
+// This is to keep backward compatibility with existing setups
+// that might still use MAILHOG_HOST environment variable
+const host =
+  process.env.MAILPIT_HOST || process.env.MAILHOG_HOST || 'localhost';
 
 export const getAllEmails = async (): Promise<AllEmails> => {
   const res = await fetch(`http://${host}:8025/api/v1/messages`);
