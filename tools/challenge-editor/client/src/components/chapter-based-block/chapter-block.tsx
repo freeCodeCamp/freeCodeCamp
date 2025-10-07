20a53830ed255d30519a7a988d1e585a213a5c09
sembauke <semboot699@gmail.com>
2025-10-07 15:38:54 +0200

    fix(tools): adjust challenge editor terminal path to match new curriculum structure

--- tools/challenge-editor/client/src/components/chapter-based-block/chapter-block.tsx
+++ tools/challenge-editor/client/src/components/chapter-based-block/chapter-block.tsx
@@ -106,8 +106,8 @@ const ChapterBasedBlock = () => {
           <p>
             Looking to add or remove challenges? Navigate to <br />
             <code>
-              freeCodeCamp/curriculum/challenges/english
-              {`/${params.block}/`}
+              curriculum/challenges/english/blocks
+              {`/${params.block}`}
             </code>
             <br />
             in your terminal and run the following commands:
@@ -144,8 +144,8 @@ const ChapterBasedBlock = () => {
           <p>
             Looking to add or remove challenges? Navigate to <br />
             <code>
-              freeCodeCamp/curriculum/challenges/english
-              {`/${params.superblock}/${params.block}/`}
+              curriculum/challenges/english/blocks
+              {`/${params.block}`}
             </code>
             <br />
             in your terminal and run the following commands:
