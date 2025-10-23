3042c1e37175af804d9e014888611dff974a52ca
Shaun Hamilton <shauhami020@gmail.com>
2025-10-23 20:25:46 +0200

    refactor(api): plain js type instead of prisma
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- api/src/schemas/user/get-session-user.ts
+++ api/src/schemas/user/get-session-user.ts
@@ -1,13 +1,9 @@
 import { Type } from '@fastify/type-provider-typebox';
-import { DailyCodingChallengeLanguage } from '@prisma/client';
 import { examResults, profileUI, savedChallenge } from '../types.js';
 
-const languages = Object.values(DailyCodingChallengeLanguage).map(k =>
-  Type.Literal(k)
-) as [
-  ReturnType<typeof Type.Literal<'javascript'>>,
-  ReturnType<typeof Type.Literal<'python'>>
-];
+const languages = Type.Array(
+  Type.Union([Type.Literal('javascript'), Type.Literal('python')])
+);
 
 export const getSessionUser = {
   response: {
@@ -61,7 +57,7 @@ export const getSessionUser = {
             Type.Object({
               id: Type.String(),
               completedDate: Type.Number(),
-              languages: Type.Array(Type.Union(languages))
+              languages
             })
           ),
           currentChallengeId: Type.String(),
