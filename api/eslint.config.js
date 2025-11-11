3a5a7b44e33551a44bf36b95318703365a4e01b7
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-11-11 16:32:30 +0100

    refactor: create new, simpler, base config (use in api)

---
+++ api/eslint.config.js
@@ -0,0 +1,8 @@
+import { config } from '@freecodecamp/eslint-config/base';
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export default config;
