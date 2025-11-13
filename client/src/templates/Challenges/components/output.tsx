4b14a51d8b41f33b05729540b4e0003c0e7521b9
Anna Cottrill <a.rcottrill521@gmail.com>
2025-11-13 17:08:02 -0500

    fix(ui): directly display output in output.tsx

--- client/src/templates/Challenges/components/output.tsx
+++ client/src/templates/Challenges/components/output.tsx
@@ -14,16 +14,18 @@ function Output({ defaultOutput, output }: OutputProps): JSX.Element {
   const message = sanitizeHtml(!isEmpty(output) ? output : defaultOutput, {
     allowedTags: ['b', 'i', 'em', 'strong', 'code', 'wbr']
   });
+
   return (
     <pre
       className='output-text'
       data-playwright-test-label='output-text'
-      dangerouslySetInnerHTML={{ __html: message }}
       role='region'
       aria-label={i18next.t('learn.editor-tabs.console')}
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
       tabIndex={0}
-    />
+    >
+      <p>{message}</p>
+    </pre>
   );
 }
 
