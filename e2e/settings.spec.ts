807c7963de3ccf850898f3f6b6b58a7d9e8839ed
sembauke <semboot699@gmail.com>
2025-11-14 08:43:38 +0100

    fix: test

--- e2e/settings.spec.ts
+++ e2e/settings.spec.ts
@@ -232,7 +232,7 @@ test.describe('Settings - Certified User', () => {
     }
 
     // Danger Zone
-    await expect(page.getByText('Danger Zone')).toBeVisible();
+    await expect(page.getByText('Danger Zone').nth(1)).toBeVisible();
     await expect(
       page.getByText(
         'Please be careful. Changes in this section are permanent.'
