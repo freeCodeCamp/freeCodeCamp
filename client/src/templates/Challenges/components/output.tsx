094911d0d39c56c41f33bd78fc5c98da22cde0fd
Anna Cottrill <a.rcottrill521@gmail.com>
2025-11-19 18:28:47 -0500

    dangerously use html with replacers

--- client/src/templates/Challenges/components/output.tsx
+++ client/src/templates/Challenges/components/output.tsx
@@ -10,6 +10,24 @@ interface OutputProps {
   output: string;
 }
 
+/**
+ *
+ * @param message Sanitized HTML code
+ * @returns Html code that is capable of correctly displaying escaped html entities
+ */
+function reformatHTMLEntities(message: string) {
+  console.log(message);
+  const reformattedHTML = message
+    .replace(/&amp;/g, '&amp;amp;') // `&amp;amp;` becomes `&amp;` in rendered html
+    .replace(/&apos;/g, '&amp;apos;') // `&amp;apos;` becomes `&apos;` in rendered html
+    .replace(/&quot;/g, '&amp;quot;') // `&amp;quot;` becomes `&quot;` in rendered html
+    .replace(/&gt;/g, '&amp;gt;') // `&amp;gt;` becomes `&gt;` in rendered html
+    .replace(/&lt;/g, '&amp;lt;'); // `&amp;lt;` becomes `&lt;` in rendered html
+
+  console.log(reformattedHTML);
+  return reformattedHTML;
+}
+
 function Output({ defaultOutput, output }: OutputProps): JSX.Element {
   const message = sanitizeHtml(!isEmpty(output) ? output : defaultOutput, {
     allowedTags: ['b', 'i', 'em', 'strong', 'code', 'wbr']
@@ -21,11 +39,10 @@ function Output({ defaultOutput, output }: OutputProps): JSX.Element {
       data-playwright-test-label='output-text'
       role='region'
       aria-label={i18next.t('learn.editor-tabs.console')}
+      dangerouslySetInnerHTML={{ __html: reformatHTMLEntities(message) }}
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
       tabIndex={0}
-    >
-      <p>{message}</p>
-    </pre>
+    ></pre>
   );
 }
 
