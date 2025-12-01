56403ca7561addf37d093579b7ee312c644fa9c1
sembauke <semboot699@gmail.com>
2025-12-01 12:24:58 +0100

    feat: add chapter icons for A1 Chinese and A1 Spanish chapters

--- client/src/assets/chapter-icon.tsx
+++ client/src/assets/chapter-icon.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { FsdChapters } from '../../../shared-dist/config/chapters';
+import {
+  EsA1SpanishChapters,
+  FsdChapters,
+  ZhA1ChineseChapters
+} from '../../../shared-dist/config/chapters';
 import DatabaseIcon from './icons/database';
 import JavaScriptIcon from './icons/javascript';
 import ReactIcon from './icons/react';
@@ -10,6 +14,15 @@ import Css from './icons/css';
 import NodeIcon from './icons/node';
 import Python from './icons/python';
 import Graduation from './icons/graduation';
+import {
+  faComments,
+  faCubes,
+  faDoorOpen,
+  faHands,
+  faIdCard,
+  faPeopleGroup
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const iconMap = {
   [FsdChapters.Welcome]: FreeCodeCampIcon,
@@ -29,7 +42,16 @@ const iconMap = {
   [FsdChapters.RdbExam]: Graduation,
   [FsdChapters.Bed]: NodeIcon,
   [FsdChapters.BedExam]: Graduation,
-  [FsdChapters.FsdExam]: Graduation
+  [FsdChapters.FsdExam]: Graduation,
+  [ZhA1ChineseChapters.zhA1Welcome]: faDoorOpen,
+  [ZhA1ChineseChapters.zhA1PinYin]: faCubes,
+  [ZhA1ChineseChapters.zhA1Greetings]: faComments,
+  [ZhA1ChineseChapters.zhA1Family]: faPeopleGroup,
+  [ZhA1ChineseChapters.zhA1Expressing]: faHands,
+  [EsA1SpanishChapters.esA1Welcome]: faDoorOpen,
+  [EsA1SpanishChapters.esA1Fundamentals]: faCubes,
+  [EsA1SpanishChapters.esA1Greetings]: faComments,
+  [EsA1SpanishChapters.esA1Details]: faIdCard
 };
 
 type ChapterIconProps = {
@@ -39,6 +61,10 @@ type ChapterIconProps = {
 export function ChapterIcon(props: ChapterIconProps): JSX.Element {
   const { chapter, ...iconProps } = props;
   const Icon = iconMap[chapter] ?? ResponsiveDesign;
+
+  if (typeof Icon === 'object') {
+    return <FontAwesomeIcon icon={Icon} />;
+  }
 
   return <Icon {...iconProps} />;
 }
