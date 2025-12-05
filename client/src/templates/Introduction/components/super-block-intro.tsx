697a6a1b39d13aa8d4a8a6e398f6fd9325a15211
moT01 <20648924+moT01@users.noreply.github.com>
2025-12-05 12:53:26 -0600

    fix: remove finished certs

--- client/src/templates/Introduction/components/super-block-intro.tsx
+++ client/src/templates/Introduction/components/super-block-intro.tsx
@@ -39,8 +39,6 @@ export const ConditionalDonationAlert = ({
     SuperBlocks.A2Chinese,
     SuperBlocks.A1Chinese,
     SuperBlocks.FrontEndDevLibsV9,
-    SuperBlocks.PythonV9,
-    SuperBlocks.RelationalDbV9,
     SuperBlocks.BackEndDevApisV9,
     SuperBlocks.FullStackDeveloperV9
   ];
