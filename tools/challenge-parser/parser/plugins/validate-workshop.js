d8535be635c90a725d2b285b6eaf977496c6bc66
Ali Farooqi <ali.farooqi@kmon.net>
2025-12-16 11:29:28 +0800

    fix(tools): fixed the typo

--- tools/challenge-parser/parser/plugins/validate-workshop.js
+++ tools/challenge-parser/parser/plugins/validate-workshop.js
@@ -2,7 +2,7 @@ const { findAll } = require('./utils/find-all');
 const { isMarker } = require('./utils/get-section');
 
 const FCC_EDITABLE_REGION = '--fcc-editable-region--';
-const SOLITIONS_HEADER = '--solutions--';
+const SOLUTIONS_HEADER = '--solutions--';
 
 const isEditableMarker = node => {
   if (node.children && node.children[0]) {
@@ -33,12 +33,12 @@ function validateWorkshop() {
     // 2) Ensure solutions header only in last step.
     for (let i = 0; i < allMarkers.length; i++) {
       const markerText = allMarkers[i].children[0].value;
-      if (markerText === SOLITIONS_HEADER && i !== allMarkers.length - 1) {
+      if (markerText === SOLUTIONS_HEADER && i !== allMarkers.length - 1) {
         throw new Error(
           `Validation error: "Solutions" header appears before the last step. The Solutions header must only appear in the final step.`
         );
       } else if (
-        markerText !== SOLITIONS_HEADER &&
+        markerText !== SOLUTIONS_HEADER &&
         i === allMarkers.length - 1
       ) {
         throw new Error(
