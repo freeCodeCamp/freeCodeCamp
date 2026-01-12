477f6fa1bec67090f6cb6a8a027c75f08b4db00b
Steven Boutcher <boutchersj@gmail.com>
2026-01-12 07:35:03 -0600

    remove redundant not-undefined condition
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- api/src/routes/protected/settings.ts
+++ api/src/routes/protected/settings.ts
@@ -525,7 +525,7 @@ ${isLinkSentWithinLimitTTL}`
       const logger = fastify.log.child({ req, res: reply });
       
       // No need to validate if picture is being deleted.
-      if (req.body.picture !== undefined) {
+      if (req.body.picture) {
         const currentUser = await fastify.prisma.user.findUnique({
           where: { id: req.user?.id },
           select: { picture: true }
