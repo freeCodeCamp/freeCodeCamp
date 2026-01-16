601f218da564fd7138eafbcd8a10095963f454c5
Naomi Carrigan <commits+fcc@nhcarrigan.com>
2026-01-16 08:16:32 -0800

    revert: just make description optional

--- curriculum/schema/challenge-schema.js
+++ curriculum/schema/challenge-schema.js
@@ -174,14 +174,11 @@ const schema = Joi.object().keys({
       challengeTypes.step,
       challengeTypes.video,
       challengeTypes.multipleChoice,
-      challengeTypes.fillInTheBlank
+      challengeTypes.fillInTheBlank,
+      challengeTypes.review
     ],
     then: Joi.string().allow(''),
-    otherwise: Joi.when('challengeType', {
-      is: challengeTypes.review,
-      then: Joi.string().allow(''),
-      otherwise: Joi.string().required()
-    })
+    otherwise: Joi.string().required()
   }),
   disableLoopProtectTests: Joi.boolean().required(),
   disableLoopProtectPreview: Joi.boolean().required(),
@@ -191,72 +188,23 @@ const schema = Joi.object().keys({
   }),
   challengeFiles: Joi.array().items(fileJoi),
   // TODO: Consider renaming to something else. Stuff show.tsx knows how to render in order
-  nodules: Joi.when('challengeType', {
-    is: challengeTypes.review,
-    then: Joi.when('description', {
-      is: '',
-      then: Joi.array()
-        .items(
+  nodules: Joi.array().items(
+    Joi.object().keys({
+      type: Joi.valid('paragraph', 'interactiveEditor').required(),
+      data: Joi.when('type', {
+        is: ['interactiveEditor'],
+        then: Joi.array().items(
           Joi.object().keys({
-            type: Joi.valid('paragraph', 'interactiveEditor').required(),
-            data: Joi.when('type', {
-              is: ['interactiveEditor'],
-              then: Joi.array().items(
-                Joi.object().keys({
-                  ext: Joi.string().required(),
-                  name: Joi.string().required(),
-                  contents: Joi.string().required(),
-                  contentsHtml: Joi.string().required()
-                })
-              ),
-              otherwise: Joi.string().required()
-            })
+            ext: Joi.string().required(),
+            name: Joi.string().required(),
+            contents: Joi.string().required(),
+            contentsHtml: Joi.string().required()
           })
-        )
-        .min(1)
-        .required()
-        .messages({
-          'array.min':
-            'Review challenges must have interactive content (--interactive--) when description is empty',
-          'any.required':
-            'Review challenges must have interactive content (--interactive--) when description is empty'
-        }),
-      otherwise: Joi.array().items(
-        Joi.object().keys({
-          type: Joi.valid('paragraph', 'interactiveEditor').required(),
-          data: Joi.when('type', {
-            is: ['interactiveEditor'],
-            then: Joi.array().items(
-              Joi.object().keys({
-                ext: Joi.string().required(),
-                name: Joi.string().required(),
-                contents: Joi.string().required(),
-                contentsHtml: Joi.string().required()
-              })
-            ),
-            otherwise: Joi.string().required()
-          })
-        })
-      )
-    }),
-    otherwise: Joi.array().items(
-      Joi.object().keys({
-        type: Joi.valid('paragraph', 'interactiveEditor').required(),
-        data: Joi.when('type', {
-          is: ['interactiveEditor'],
-          then: Joi.array().items(
-            Joi.object().keys({
-              ext: Joi.string().required(),
-              name: Joi.string().required(),
-              contents: Joi.string().required(),
-              contentsHtml: Joi.string().required()
-            })
-          ),
-          otherwise: Joi.string().required()
-        })
+        ),
+        otherwise: Joi.string().required()
       })
-    )
-  }),
+    })
+  ),
   hasEditableBoundaries: Joi.boolean(),
   helpCategory: Joi.valid(
     'JavaScript',
