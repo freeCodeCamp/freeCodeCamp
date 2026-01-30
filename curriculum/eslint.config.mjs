77f3828bb525a64dfbc65a1aa2ce217c0f60816f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-01-30 15:19:55 +0100

    fix(tools): ignore files that should not be linted (#65594)

--- curriculum/eslint.config.mjs
+++ curriculum/eslint.config.mjs
@@ -1,5 +1,5 @@
 import { config } from '@freecodecamp/eslint-config/base';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 
 /**
@@ -7,20 +7,28 @@ import globals from 'globals';
  *
  * @type {import("eslint").Linter.Config[]}
  * */
-export default defineConfig({
-  extends: [config],
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.mocha,
-      ...globals.node,
-      Promise: true,
-      window: true,
-      $: true,
-      ga: true,
-      jQuery: true,
-      router: true,
-      globalThis: true
+export default defineConfig(
+  globalIgnores([
+    './src/test/stubs/js',
+    './src/test/blocks-generated',
+    './challenges',
+    './structure'
+  ]),
+  {
+    extends: [config],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        ...globals.node,
+        Promise: true,
+        window: true,
+        $: true,
+        ga: true,
+        jQuery: true,
+        router: true,
+        globalThis: true
+      }
     }
   }
-});
+);
