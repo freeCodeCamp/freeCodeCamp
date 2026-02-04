a83ce6a4f2f523f70af33e8578cfe75c03f0692b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-02-04 11:42:21 +0100

    refactor: lint challenges as part of curriculum, not root (#65665)
    Co-authored-by: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>

--- packages/challenge-linter/src/linter/index.js
+++ packages/challenge-linter/src/linter/index.js
@@ -1,26 +1,29 @@
-const markdownlint = require('markdownlint');
+import markdownlint from 'markdownlint';
 
-const lintPrism = require('./markdown-prism');
-const lintYAML = require('./markdown-yaml');
-const fencedCodeBlock = require('./fenced-code-block');
+import * as lintPrism from './markdown-prism.js';
+import * as lintYAML from './markdown-yaml.js';
+import * as fencedCodeBlock from './fenced-code-block.js';
 
-function linter(rules) {
+export function linter(rules) {
   const lint = (file, next) => {
     const options = {
       files: [file.path],
       config: rules,
       customRules: [lintYAML, lintPrism, fencedCodeBlock]
     };
+
     markdownlint(options, function callback(err, result) {
       const resultString = (result || '').toString();
       if (resultString) {
         process.exitCode = 1;
         console.log(resultString);
       }
+      if (err) {
+        process.exitCode = 1;
+        console.error(err);
+      }
       if (next) next(err, file);
     });
   };
   return lint;
 }
-
-module.exports = linter;
