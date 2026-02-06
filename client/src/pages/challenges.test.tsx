425094b030cd8d5ff387217d9266905da5c955f3
sembauke <semboot699@gmail.com>
2026-02-06 16:46:34 +0100

    fix: client tests

--- client/src/pages/challenges.test.tsx
+++ client/src/pages/challenges.test.tsx
@@ -1,59 +1,36 @@
-import {
-  createHistory,
-  createMemorySource,
-  LocationProvider
-} from '@gatsbyjs/reach-router';
 import { render } from '@testing-library/react';
 import { navigate, withPrefix } from 'gatsby';
 import React from 'react';
 import { describe, it, expect } from 'vitest';
 
 import Challenges from './challenges/index';
+import ChallengesRedirect from './challenges/[...]';
 
 describe('Challenges', () => {
-  // Source: https://testing-library.com/docs/example-reach-router/
-  function renderWithRouterWrapper({
-    route = '/',
-    history = createHistory(createMemorySource(route))
-  } = {}) {
-    return {
-      ...render(
-        <LocationProvider history={history}>
-          <Challenges />
-        </LocationProvider>
-      ),
-      // adding `history` to the returned utilities to allow us
-      // to reference it in our tests (just try to avoid using
-      // this to test implementation details).
-      history
-    };
-  }
-
-  const challenges = withPrefix('/challenges');
   const learn = withPrefix('/learn');
 
   it('should handle redirect to /learn', () => {
-    renderWithRouterWrapper({ route: challenges });
+    render(<Challenges />);
 
     expect(navigate).toHaveBeenLastCalledWith(learn);
   });
 
   it('should handle redirect to /learn/:super-block', () => {
-    renderWithRouterWrapper({ route: `${challenges}/super-block` });
+    render(<ChallengesRedirect params={{ '*': 'super-block' }} />);
 
     expect(navigate).toHaveBeenLastCalledWith(`${learn}/super-block`);
   });
 
   it('should handle redirect to /learn/:super-block/:block', () => {
-    renderWithRouterWrapper({ route: `${challenges}/super-block/block` });
+    render(<ChallengesRedirect params={{ '*': 'super-block/block' }} />);
 
     expect(navigate).toHaveBeenLastCalledWith(`${learn}/super-block/block`);
   });
 
   it('should handle redirect to /learn/:super-block/:block/:challenge', () => {
-    renderWithRouterWrapper({
-      route: `${challenges}/super-block/block/challenge`
-    });
+    render(
+      <ChallengesRedirect params={{ '*': 'super-block/block/challenge' }} />
+    );
 
     expect(navigate).toHaveBeenLastCalledWith(
       `${learn}/super-block/block/challenge`
