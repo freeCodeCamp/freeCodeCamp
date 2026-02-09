2e0bb54d7f5be8fe100358f12ae08b57277eb97d
sembauke <semboot699@gmail.com>
2026-02-09 12:39:40 +0100

    fix: handle 404 profile page

---
+++ client/src/pages/[maybeUser].tsx
@@ -0,0 +1,20 @@
+/* eslint-disable filenames-simple/naming-convention */
+import React from 'react';
+import ShowProfileOrFourOhFour from '../client-only-routes/show-profile-or-four-oh-four';
+
+interface ProfilePageProps {
+  params: {
+    maybeUser: string;
+  };
+}
+
+const ProfilePage: React.FC<ProfilePageProps> = ({ params }) => {
+  const { maybeUser } = params;
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore - Redux connect() provides remaining props
+    <ShowProfileOrFourOhFour maybeUser={maybeUser} />
+  );
+};
+
+export default ProfilePage;
