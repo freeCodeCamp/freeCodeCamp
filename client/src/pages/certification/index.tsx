6cbfb7012096a818908cef09df8c3d6df7173708
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-02-11 11:03:51 +0100

    refactor: remove unused css import

--- client/src/pages/certification/index.tsx
+++ client/src/pages/certification/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import RedirectHome from '../../components/redirect-home';
 
-import './certification.css';
-
 // Redirect to home if no username/certSlug provided
 const Certification = () => <RedirectHome />;
 
