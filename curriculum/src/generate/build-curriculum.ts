0f0e4a0a72658632a3d84b509490ff73209fcb32
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-02-13 19:04:19 +0100

    refactor: use pre-built curriculum when starting client

--- curriculum/src/generate/build-curriculum.ts
+++ curriculum/src/generate/build-curriculum.ts
@@ -2,10 +2,18 @@ import fs from 'fs';
 import path from 'path';
 
 import { getChallengesForLang } from '../get-challenges';
+import assert from 'assert';
 
 const globalConfigPath = path.resolve(__dirname, '../../generated/');
 
-void getChallengesForLang('english')
+const curriculumLocale = process.env.CURRICULUM_LOCALE;
+
+assert(
+  curriculumLocale,
+  'CURRICULUM_LOCALE environment variable is required to build curriculum'
+);
+
+void getChallengesForLang(curriculumLocale)
   .then(JSON.stringify)
   .then(json => {
     fs.mkdirSync(globalConfigPath, { recursive: true });
