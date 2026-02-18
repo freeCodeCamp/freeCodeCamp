03b0888dc74e9d31de5af950ac1b54f69534a3e6
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-02-18 14:27:55 +0100

    fix(client): daily-coding-challenge routing
    Turns out mixing two types of routing for the same page (gatsby + reach) doesn't really work. This remove the reach router code and lets Gatsby handle it

--- client/src/pages/learn/daily-coding-challenge/[...].tsx
+++ client/src/pages/learn/daily-coding-challenge/[...].tsx
@@ -1,30 +1,16 @@
 /* eslint-disable filenames-simple/naming-convention */
-import { Router } from '@gatsbyjs/reach-router';
-import { withPrefix } from 'gatsby';
 import React from 'react';
 
 import ShowDailyCodingChallenge from '../../../client-only-routes/show-daily-coding-challenge';
-import RedirectToArchive from '../../../components/redirect-daily-challenge-archive';
 
-const inlineStyles = {
-  minHeight: 0,
-  height: '100%'
-};
+interface Props {
+  params: {
+    '*': string;
+  };
+}
 
-function DailyCodingChallengeAll(): JSX.Element {
-  return (
-    // Router adds an element around the editor, messing with the layout because the editor is a flex item
-    // These few inline styles fix it.
-    <Router style={inlineStyles}>
-      <ShowDailyCodingChallenge
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        path={withPrefix('/learn/daily-coding-challenge/:date')}
-      />
-
-      <RedirectToArchive default />
-    </Router>
-  );
+function DailyCodingChallengeAll(props: Props): JSX.Element {
+  return <ShowDailyCodingChallenge date={props.params['*']} />;
 }
 
 DailyCodingChallengeAll.displayName = 'DailyCodingChallengeAll';
