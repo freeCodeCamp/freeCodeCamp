19cdd12b6e11daa947a88a85c5a198839da1f113
moT01 <20648924+moT01@users.noreply.github.com>
2026-02-18 10:47:59 -0600

    fix: reimplement redirect logic

--- client/src/pages/learn/daily-coding-challenge/[...].tsx
+++ client/src/pages/learn/daily-coding-challenge/[...].tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 
 import ShowDailyCodingChallenge from '../../../client-only-routes/show-daily-coding-challenge';
+import RedirectToArchive from '../../../components/redirect-daily-challenge-archive';
+import { isValidDateString } from '../../../components/daily-coding-challenge/helpers';
 
 interface Props {
   params: {
@@ -10,6 +12,10 @@ interface Props {
 }
 
 function DailyCodingChallengeAll(props: Props): JSX.Element {
+  if (!isValidDateString(props.params['*'])) {
+    return <RedirectToArchive />;
+  }
+
   return <ShowDailyCodingChallenge date={props.params['*']} />;
 }
 
