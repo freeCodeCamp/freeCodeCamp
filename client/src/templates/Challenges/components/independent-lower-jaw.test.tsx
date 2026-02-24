ab82310e6c84a3dcacc599b6e2637d5a09c693f2
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2026-02-24 15:03:15 +0300

    fix: add store to test

--- client/src/templates/Challenges/components/independent-lower-jaw.test.tsx
+++ client/src/templates/Challenges/components/independent-lower-jaw.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '../../../../utils/test-utils';
 import type { ChallengeMeta, Test } from '../../../redux/prop-types';
 import { SuperBlocks } from '@freecodecamp/shared/config/curriculum';
 import { IndependentLowerJaw } from './independent-lower-jaw';
+import { createStore } from '../../../redux/create-store';
+
+vi.mock('../../../components/Progress');
+vi.mock('../../../analytics');
+vi.mock('../../../utils/get-words');
 
 const baseChallengeMeta: ChallengeMeta = {
   block: 'test-block',
@@ -16,7 +21,7 @@ const baseChallengeMeta: ChallengeMeta = {
 };
 
 const passingTests: Test[] = [{ pass: true, text: 'test', testString: 'test' }];
-
+const store = createStore();
 const baseProps = {
   openHelpModal: vi.fn(),
   openResetModal: vi.fn(),
@@ -33,8 +38,7 @@ const baseProps = {
 
 describe('<IndependentLowerJaw />', () => {
   it('shows share buttons when the block is completed on the last step', () => {
-    render(<IndependentLowerJaw {...baseProps} />);
-
+    render(<IndependentLowerJaw {...baseProps} />, store);
     expect(screen.getByTestId('share-on-x')).toBeInTheDocument();
     expect(screen.getByTestId('share-on-bluesky')).toBeInTheDocument();
     expect(screen.getByTestId('share-on-threads')).toBeInTheDocument();
@@ -46,7 +50,8 @@ describe('<IndependentLowerJaw />', () => {
         {...baseProps}
         completedPercent={50}
         completedChallengeIds={['id-1']}
-      />
+      />,
+      store
     );
 
     expect(screen.queryByTestId('share-on-x')).not.toBeInTheDocument();
@@ -58,7 +63,8 @@ describe('<IndependentLowerJaw />', () => {
         {...baseProps}
         currentBlockIds={[baseChallengeMeta.id, 'id-2']}
         completedChallengeIds={[baseChallengeMeta.id, 'id-2']}
-      />
+      />,
+      store
     );
 
     expect(screen.queryByTestId('share-on-x')).not.toBeInTheDocument();
