ec967e52607d1fd0681485a367bfc02fd0e76cfe
sembauke <semboot699@gmail.com>
2026-03-12 10:39:22 +0100

    refactor(client): migrate Gatsby setup files to TypeScript

--- client/gatsby-browser.tsx
+++ client/gatsby-browser.tsx
@@ -1,6 +1,6 @@
+import type { GatsbyBrowser } from 'gatsby';
 import { LocationProvider } from '@gatsbyjs/reach-router';
 import cookies from 'browser-cookies';
-import PropTypes from 'prop-types';
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
@@ -15,7 +15,9 @@ import GrowthBookProvider from './src/components/growth-book/growth-book-wrapper
 
 const store = createStore();
 
-export const wrapRootElement = ({ element }) => {
+export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({
+  element
+}) => {
   return (
     <LocationProvider>
       <Provider store={store}>
@@ -31,19 +33,16 @@ export const wrapRootElement = ({ element }) => {
   );
 };
 
-wrapRootElement.propTypes = {
-  element: PropTypes.any
-};
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = layoutSelector;
 
-export const wrapPageElement = layoutSelector;
+export const disableCorePrefetching: GatsbyBrowser['disableCorePrefetching'] =
+  () => true;
 
-export const disableCorePrefetching = () => true;
-
-export const onRouteUpdate = () => {
+export const onRouteUpdate: GatsbyBrowser['onRouteUpdate'] = () => {
   store.dispatch({ type: 'app.routeUpdated' });
 };
 
-export const onClientEntry = () => {
+export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
   // Letting the users' browsers expire the cookie seems to have caused issues
   // for some users. Until we have time to investigate further, we should remove
   // the cookie on every page load.
