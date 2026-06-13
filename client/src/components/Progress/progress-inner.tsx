7da147c8b936db56dfcae3c708100246b98d61d7
Jose-Renny <joserennyt@gmail.com>
2026-06-13 11:16:33 -0500

    .

--- client/src/components/Progress/progress-inner.tsx
+++ client/src/components/Progress/progress-inner.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+ import React, { useState, useEffect, useRef, useMemo } from 'react';
 
 import BezierEasing from 'bezier-easing';
 import { ProgressBar } from './progress-bar';
