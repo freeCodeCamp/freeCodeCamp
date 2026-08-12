54bd0a1d27e291ed8277930f190f6b5917a898c8
Sem Bauke <sem@freecodecamp.org>
2026-08-12 17:16:47 +0200

    Update client/src/components/signout-modal/signout-modal.test.tsx
    Co-authored-by: Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
    Signed-off-by: Sem Bauke <sem@freecodecamp.org>

--- client/src/components/signout-modal/signout-modal.test.tsx
+++ client/src/components/signout-modal/signout-modal.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { I18nextProvider } from 'react-i18next';
 import { describe, it, expect, beforeAll, vi } from 'vitest';
