7809e582ec7bf73df87632d69d01ac8432a0e4c7
Shaun Hamilton <shauhami020@gmail.com>
2026-08-26 11:13:00 +0200

    fix(api): handle concurrent/parallel get-hint requests

--- api/vitest.utils.ts
+++ api/vitest.utils.ts
@@ -101,6 +101,7 @@ type IndexData = {
     key: Record<string, 1>;
     name: string;
     expireAfterSeconds?: number;
+    unique?: boolean;
   }[];
 };
 const indexData: IndexData[] = [
@@ -129,7 +130,8 @@ const indexData: IndexData[] = [
     indexes: [
       {
         key: { userId: 1, date: 1 },
-        name: 'userId_date_unique'
+        name: 'userId_date_unique',
+        unique: true
       }
     ]
   },
