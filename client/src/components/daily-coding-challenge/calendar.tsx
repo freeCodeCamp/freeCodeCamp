9a35049be61274655b4d2467a855a5c415012111
hr1378 <143756273+hr1378@users.noreply.github.com>
2026-08-27 20:41:47 -0400

    fix(client): fix daily challenge sign-in button width

--- client/src/components/daily-coding-challenge/calendar.tsx
+++ client/src/components/daily-coding-challenge/calendar.tsx
@@ -287,7 +287,7 @@ function DailyCodingChallengeCalendar({
       {!isSignedIn && (
         <Col md={8} mdOffset={2} sm={10} smOffset={1} xs={12}>
           <Spacer size='m' />
-          <div className='completion-modal-login-btn'>
+          <div className='daily-coding-challenge-login-btn'>
             <Login block={true}>{t('buttons.logged-out-cta-btn')}</Login>
           </div>
         </Col>
