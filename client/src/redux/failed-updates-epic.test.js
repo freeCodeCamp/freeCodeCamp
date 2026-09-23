984779e6df94a3a5208a660ff25c9df3d742555f
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-09-23 17:00:52 +0200

    test: improve test coverage for flushUpdates

--- client/src/redux/failed-updates-epic.test.js
+++ client/src/redux/failed-updates-epic.test.js
@@ -1,7 +1,9 @@
+// @vitest-environment jsdom
+
 import { ActionsObservable, StateObservable } from 'redux-observable';
 import { Subject } from 'rxjs';
 import store from 'store';
-import { describe, it, expect, vi } from 'vitest';
+import { afterEach, beforeEach, describe, it, expect, vi } from 'vitest';
 import { actionTypes } from './action-types';
 import failedUpdatesEpic from './failed-updates-epic';
 
@@ -10,18 +12,76 @@ vi.mock('../analytics');
 const key = 'fcc-failed-updates';
 
 describe('failed-updates-epic', () => {
+  const action$ = ActionsObservable.of({
+    type: actionTypes.updateComplete
+  });
+
+  const failRes = () =>
+    new Response(JSON.stringify({ message: 'flash.generic-error' }), {
+      status: 500
+    });
+
+  beforeEach(() => {
+    vi.spyOn(console, 'info').mockImplementation(() => {});
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.clearAllTimers();
+    vi.useRealTimers();
+    vi.restoreAllMocks();
+    store.remove(key);
+  });
+
   it('should remove faulty backend challenges from localStorage', async () => {
     store.set(key, failedSubmissions);
 
-    const action$ = ActionsObservable.of({
-      type: actionTypes.updateComplete
-    });
     const state$ = new StateObservable(new Subject(), initialState);
     const epic$ = failedUpdatesEpic(action$, state$);
 
     await epic$.toPromise();
 
     expect(store.get(key)).toEqual(submitableChallenges);
+  });
+
+  it('should remove successfully retried failures from storage (even if the retry fails)', async () => {
+    const fetchSpy = vi
+      .spyOn(globalThis, 'fetch')
+      .mockResolvedValueOnce(new Response('{}', { status: 200 }))
+      .mockResolvedValueOnce(failRes())
+      .mockResolvedValueOnce(failRes());
+
+    store.set(key, validSubmissions);
+
+    const state$ = new StateObservable(new Subject(), initialState);
+    const epic$ = failedUpdatesEpic(action$, state$);
+
+    await epic$.toPromise();
+    // first confirm that the submissions are all valid:
+    expect(store.get(key)).toEqual(validSubmissions);
+    await vi.runAllTimersAsync();
+
+    expect(fetchSpy).toHaveBeenCalledTimes(validSubmissions.length);
+    expect(store.get(key)).toEqual([]);
+  });
+
+  it('should NOT remove updates that do not reach the server', async () => {
+    const fetchSpy = vi
+      .spyOn(globalThis, 'fetch')
+      .mockResolvedValueOnce(new Response('{}', { status: 200 }))
+      .mockRejectedValue(new TypeError());
+
+    store.set(key, validSubmissions);
+
+    const state$ = new StateObservable(new Subject(), initialState);
+    const epic$ = failedUpdatesEpic(action$, state$);
+
+    await epic$.toPromise();
+    expect(store.get(key)).toEqual(validSubmissions);
+    await vi.runAllTimersAsync();
+
+    expect(fetchSpy).toHaveBeenCalledTimes(validSubmissions.length);
+    expect(store.get(key)).toEqual(validSubmissions.slice(1));
   });
 });
 
@@ -60,5 +120,10 @@ const failedSubmissions = [
     }
   }
 ];
+
+const validSubmissions = failedSubmissions.map(submission => ({
+  ...submission,
+  payload: { ...submission.payload, solution: 'https://example.com/' }
+}));
 
 const submitableChallenges = failedSubmissions.slice(1);
